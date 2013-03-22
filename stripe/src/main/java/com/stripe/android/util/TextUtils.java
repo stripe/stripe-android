b1171ca4b186ddbca105ef10de53f833bb143a32
Chiu-Ki Chan <chiuki@gmail.com>
2013-03-22 16:13:13 -0700

    Add TextUtilsTest

--- stripe/src/main/java/com/stripe/android/util/TextUtils.java
+++ stripe/src/main/java/com/stripe/android/util/TextUtils.java
@@ -2,6 +2,9 @@ package com.stripe.android.util;
 
 public class TextUtils {
     public static boolean hasAnyPrefix(String number, String... prefixes) {
+        if (number == null) {
+            return false;
+        }
         for (String prefix : prefixes) {
             if (number.startsWith(prefix)) {
                 return true;
@@ -11,6 +14,9 @@ public class TextUtils {
     }
 
     public static boolean isWholePositiveNumber(String value) {
+        if (value == null) {
+            return false;
+        }
         for (char c : value.toCharArray()) {
             if (!Character.isDigit(c)) {
                 return false;
---
+++ stripe/src/test/java/com/stripe/android/test/TextUtilsTest.java
@@ -0,0 +1,156 @@
+package com.stripe.android.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.stripe.android.util.TextUtils;
+
+public class TextUtilsTest {
+    @Test
+    public void hasAnyPrefixShouldFailIfNull() {
+        assertFalse(TextUtils.hasAnyPrefix(null));
+    }
+
+    @Test
+    public void hasAnyPrefixShouldFailIfEmpty() {
+        assertFalse(TextUtils.hasAnyPrefix(""));
+    }
+
+    @Test
+    public void hasAnyPrefixShouldFailWithNullAndEmptyPrefix() {
+        assertFalse(TextUtils.hasAnyPrefix(null, ""));
+    }
+
+    @Test
+    public void hasAnyPrefixShouldFailWithNullAndSomePrefix() {
+        assertFalse(TextUtils.hasAnyPrefix(null, "1"));
+    }
+
+    @Test
+    public void hasAnyPrefixShouldMatch() {
+        assertTrue(TextUtils.hasAnyPrefix("1234", "12"));
+    }
+
+    @Test
+    public void hasAnyPrefixShouldMatchMultiple() {
+        assertTrue(TextUtils.hasAnyPrefix("1234", "12", "1"));
+    }
+
+    @Test
+    public void hasAnyPrefixShouldMatchSome() {
+        assertTrue(TextUtils.hasAnyPrefix("abcd", "bc", "ab"));
+    }
+
+    @Test
+    public void hasAnyPrefixShouldNotMatch() {
+        assertFalse(TextUtils.hasAnyPrefix("1234", "23"));
+    }
+
+    @Test
+    public void hasAnyPrefixShouldNotMatchWithSpace() {
+        assertFalse(TextUtils.hasAnyPrefix("xyz", " x"));
+    }
+
+    @Test
+    public void wholePositiveNumberShouldFailNull() {
+        assertFalse(TextUtils.isWholePositiveNumber(null));
+    }
+
+    @Test
+    public void wholePositiveNumberShouldPassIfEmpty() {
+        assertTrue(TextUtils.isWholePositiveNumber(""));
+    }
+
+    @Test
+    public void wholePositiveNumberShouldPass() {
+        assertTrue(TextUtils.isWholePositiveNumber("123"));
+    }
+
+    @Test
+    public void wholePositiveNumberShouldPassWithLeadingZero() {
+        assertTrue(TextUtils.isWholePositiveNumber("000"));
+    }
+
+    @Test
+    public void wholePositiveNumberShouldFailIfNegative() {
+        assertFalse(TextUtils.isWholePositiveNumber("-1"));
+    }
+
+    @Test
+    public void wholePositiveNumberShouldFailIfLetters() {
+        assertFalse(TextUtils.isWholePositiveNumber("1a"));
+    }
+
+    @Test
+    public void testNullIfBlankNullShouldBeNull() {
+        assertEquals(null, TextUtils.nullIfBlank(null));
+    }
+
+    @Test
+    public void testNullIfBlankEmptyShouldBeNull() {
+        assertEquals(null, TextUtils.nullIfBlank(""));
+    }
+
+    @Test
+    public void testNullIfBlankSpaceShouldBeNull() {
+        assertEquals(null, TextUtils.nullIfBlank(" "));
+    }
+
+    @Test
+    public void testNullIfBlankSpacesShouldBeNull() {
+        assertEquals(null, TextUtils.nullIfBlank("     "));
+    }
+
+    @Test
+    public void testNullIfBlankTabShouldBeNull() {
+        assertEquals(null, TextUtils.nullIfBlank("	"));
+    }
+
+    @Test
+    public void testNullIfBlankNumbersShouldNotBeNull() {
+        assertEquals("0", TextUtils.nullIfBlank("0"));
+    }
+
+    @Test
+    public void testNullIfBlankLettersShouldNotBeNull() {
+        assertEquals("abc", TextUtils.nullIfBlank("abc"));
+    }
+
+    @Test
+    public void isBlankShouldPassIfNull() {
+        assertTrue(TextUtils.isBlank(null));
+    }
+
+    @Test
+    public void isBlankShouldPassIfEmpty() {
+        assertTrue(TextUtils.isBlank(""));
+    }
+
+    @Test
+    public void isBlankShouldPassIfSpace() {
+        assertTrue(TextUtils.isBlank(" "));
+    }
+
+    @Test
+    public void isBlankShouldPassIfSpaces() {
+        assertTrue(TextUtils.isBlank("     "));
+    }
+
+    @Test
+    public void isBlankShouldPassIfTab() {
+        assertTrue(TextUtils.isBlank("	"));
+    }
+
+    @Test
+    public void isBlankShouldFailIfNumber() {
+        assertFalse(TextUtils.isBlank("0"));
+    }
+
+    @Test
+    public void isBlankShouldFailIfLetters() {
+        assertFalse(TextUtils.isBlank("abc"));
+    }
+}
