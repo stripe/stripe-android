fd558b8c7196e407e9579d829e679d9bd4dc0ce9
Michael McDuffee <mrmcduff@stripe.com>
2016-11-29 14:51:00 -0800

    updating the example application to be a single fragment-less activity

--- example/src/main/java/com/stripe/example/TokenList.java
+++
@@ -1,7 +0,0 @@
-package com.stripe.example;
-
-import com.stripe.android.model.Token;
-
-public interface TokenList {
-    public void addToList(Token token);
-}
--- example/src/main/java/com/stripe/example/activity/PaymentActivity.java
+++ example/src/main/java/com/stripe/example/activity/PaymentActivity.java
@@ -3,16 +3,25 @@ package com.stripe.example.activity;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 
 import com.stripe.android.Stripe;
 import com.stripe.android.TokenCallback;
 import com.stripe.android.model.Card;
 import com.stripe.android.model.Token;
-import com.stripe.example.PaymentForm;
 import com.stripe.example.R;
-import com.stripe.example.TokenList;
 import com.stripe.example.dialog.ErrorDialogFragment;
 import com.stripe.example.dialog.ProgressDialogFragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class PaymentActivity extends FragmentActivity {
@@ -22,9 +31,22 @@ public class PaymentActivity extends FragmentActivity {
      *
      * You can get your key here: https://manage.stripe.com/account/apikeys
      */
-    public static final String PUBLISHABLE_KEY = "pk_test_6pRNASCoBOKtIshFeQd4XMUh";
+    private static final String PUBLISHABLE_KEY = "pk_test_6pRNASCoBOKtIshFeQd4XMUh";
+    private static final String CURRENCY_UNSPECIFIED = "Unspecified";
 
     private ProgressDialogFragment progressFragment;
+
+    private Button saveButton;
+    private EditText cardNumberEditText;
+    private EditText cvcEditText;
+    private Spinner monthSpinner;
+    private Spinner yearSpinner;
+    private Spinner currencySpinner;
+
+    private ListView listView;
+    private SimpleAdapter simpleAdapter;
+    private List<Map<String, String>> cardTokens = new ArrayList<Map<String, String>>();
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,42 +54,79 @@ public class PaymentActivity extends FragmentActivity {
         setContentView(R.layout.payment_activity);
 
         progressFragment = ProgressDialogFragment.newInstance(R.string.progressMessage);
+
+        this.saveButton = (Button) findViewById(R.id.save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveCreditCard();
+            }
+        });
+
+        this.cardNumberEditText = (EditText) findViewById(R.id.number);
+        this.cvcEditText = (EditText) findViewById(R.id.cvc);
+        this.monthSpinner = (Spinner) findViewById(R.id.expMonth);
+        this.yearSpinner = (Spinner) findViewById(R.id.expYear);
+        this.currencySpinner = (Spinner) findViewById(R.id.currency);
+        this.listView = (ListView) findViewById(R.id.listview);
+        initListView();
     }
 
-    public void saveCreditCard(PaymentForm form) {
+    public void saveCreditCard() {
+        String cardNumber = cardNumberEditText.getText().toString();
+        String cvc = cvcEditText.getText().toString();
 
-        Card card = new Card(
-                form.getCardNumber(),
-                form.getExpMonth(),
-                form.getExpYear(),
-                form.getCvc());
-        card.setCurrency(form.getCurrency());
+        int expMonth = getIntegerFromSpinner(monthSpinner);
+        int expYear = getIntegerFromSpinner(yearSpinner);
 
-        boolean validation = card.validateCard();
-        if (validation) {
+        String currency = getCurrency();
+        Card cardToSave = new Card(cardNumber, expMonth, expYear, cvc);
+        cardToSave.setCurrency(currency);
+
+        if (validateCard(cardToSave)) {
             startProgress();
             new Stripe().createToken(
-                    card,
+                    cardToSave,
                     PUBLISHABLE_KEY,
                     new TokenCallback() {
-                    public void onSuccess(Token token) {
-                        getTokenList().addToList(token);
-                        finishProgress();
+                        public void onSuccess(Token token) {
+                            addToList(token);
+                            finishProgress();
                         }
-                    public void onError(Exception error) {
+                        public void onError(Exception error) {
                             handleError(error.getLocalizedMessage());
                             finishProgress();
                         }
                     });
-        } else if (!card.validateNumber()) {
-        	handleError("The card number that you entered is invalid");
-        } else if (!card.validateExpiryDate()) {
-        	handleError("The expiration date that you entered is invalid");
-        } else if (!card.validateCVC()) {
-        	handleError("The CVC code that you entered is invalid");
-        } else {
-        	handleError("The card details that you entered are invalid");
         }
+    }
+
+    private void addToList(Token token) {
+        String endingIn = getResources().getString(R.string.endingIn);
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("last4", endingIn + " " + token.getCard().getLast4());
+        map.put("tokenId", token.getId());
+        cardTokens.add(map);
+        simpleAdapter.notifyDataSetChanged();
+    }
+
+    private boolean validateCard(Card card) {
+        boolean valid = true;
+        if (!card.validateNumber()) {
+            handleError("The card number that you entered is invalid");
+            valid = false;
+        } else if (!card.validateExpiryDate()) {
+            handleError("The expiration date that you entered is invalid");
+            valid = false;
+        } else if (!card.validateCVC()) {
+            handleError("The CVC code that you entered is invalid");
+            valid = false;
+        } else if (!card.validateCard()){
+            handleError("The card details that you entered are invalid");
+            valid = false;
+        }
+
+        return valid;
     }
 
     private void startProgress() {
@@ -78,12 +137,40 @@ public class PaymentActivity extends FragmentActivity {
         progressFragment.dismiss();
     }
 
+    public String getCurrency() {
+        if (currencySpinner.getSelectedItemPosition() == 0) {
+            return null;
+        }
+
+        String selected = (String) currencySpinner.getSelectedItem();
+
+        if (selected.equals(CURRENCY_UNSPECIFIED)) {
+            return null;
+        }
+
+        return selected.toLowerCase();
+    }
+
+    private int getIntegerFromSpinner(Spinner spinner) {
+        try {
+            return Integer.parseInt(spinner.getSelectedItem().toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
     private void handleError(String error) {
         DialogFragment fragment = ErrorDialogFragment.newInstance(R.string.validationErrors, error);
         fragment.show(getSupportFragmentManager(), "error");
     }
 
-    private TokenList getTokenList() {
-        return (TokenList)(getSupportFragmentManager().findFragmentById(R.id.token_list));
+    private void initListView() {
+        simpleAdapter = new SimpleAdapter(
+                this,
+                cardTokens,
+                R.layout.list_item_layout,
+                new String[]{"last4", "tokenId"},
+                new int[]{R.id.last4, R.id.tokenId});
+        listView.setAdapter(simpleAdapter);
     }
 }
