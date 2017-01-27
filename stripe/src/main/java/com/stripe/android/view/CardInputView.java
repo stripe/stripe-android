dab81bd98b33b86aa2c8b0232a1f9fd215bf1134
Michael McDuffee <mrmcduff@stripe.com>
2017-01-27 13:28:17 -0800

    adding a clarifying comment

--- stripe/src/main/java/com/stripe/android/view/CardInputView.java
+++ stripe/src/main/java/com/stripe/android/view/CardInputView.java
@@ -281,6 +281,14 @@ public class CardInputView extends FrameLayout {
         }
     }
 
+    /**
+     * Class used to encapsulate the functionality of "backing up" via the delete/backspace key
+     * from one text field to the previous. We use this to simulate multiple fields being all part
+     * of the same EditText, so a delete key entry from field N+1 deletes the last character in
+     * field N. Each BackUpFieldDeleteListener is attached to the N+1 field, from which it gets
+     * its {@link #onDeleteEmpty()} call, and given a reference to the N field, upon which
+     * it will be acting.
+     */
     private class BackUpFieldDeleteListener implements StripeEditText.DeleteEmptyListener {
 
         private StripeEditText backUpTarget;
