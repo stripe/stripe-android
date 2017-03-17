830731a99c6da8d9f0fdef4baf26fb32f9582d69
Michael McDuffee <mrmcduff@stripe.com>
2017-03-16 19:10:49 -0700

    making the application look a little prettier

--- example/src/main/java/com/stripe/example/activity/PollingActivity.java
+++ example/src/main/java/com/stripe/example/activity/PollingActivity.java
@@ -1,5 +1,6 @@
 package com.stripe.example.activity;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,15 +10,20 @@ import android.view.View;
 import android.widget.Button;
 
 import com.stripe.android.Stripe;
+import com.stripe.android.exception.StripeException;
 import com.stripe.android.model.Card;
 import com.stripe.android.model.Source;
 import com.stripe.android.model.SourceParams;
+import com.stripe.android.net.PollingResponse;
+import com.stripe.android.net.PollingResponseHandler;
 import com.stripe.android.view.CardInputWidget;
 import com.stripe.example.R;
 import com.stripe.example.adapter.PollingAdapter;
 import com.stripe.example.controller.ErrorDialogHandler;
+import com.stripe.example.controller.PollingDialogController;
 import com.stripe.example.controller.ProgressDialogController;
 
+import java.util.Locale;
 import java.util.concurrent.Callable;
 
 import rx.Observable;
@@ -28,18 +34,25 @@ import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
+/**
+ * Activity that lets you poll for a 3DS source update.
+ */
 public class PollingActivity extends AppCompatActivity {
 
     private static final String FUNCTIONAL_SOURCE_PUBLISHABLE_KEY =
             "pk_test_vOo1umqsYxSrP5UXfOeL3ecm";
+    private static final String RETURN_URL = "stripe://example";
+
+    private static final String QUERY_CLIENT_SECRET = "client_secret";
+    private static final String QUERY_SOURCE_ID = "source";
 
     private CardInputWidget mCardInputWidget;
     private CompositeSubscription mCompositeSubscription;
     private PollingAdapter mPollingAdapter;
     private ErrorDialogHandler mErrorDialogHandler;
+    private PollingDialogController mPollingDialogController;
     private ProgressDialogController mProgressDialogController;
     private Stripe mStripe;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +60,10 @@ public class PollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_polling);
 
         mCompositeSubscription = new CompositeSubscription();
-        mCardInputWidget = (CardInputWidget) findViewById(R.id.card_input_widget);
+        mCardInputWidget = (CardInputWidget) findViewById(R.id.card_widget_three_d);
         mErrorDialogHandler = new ErrorDialogHandler(this.getSupportFragmentManager());
         mProgressDialogController = new ProgressDialogController(this.getSupportFragmentManager());
+        mPollingDialogController = new PollingDialogController(this);
         mStripe = new Stripe(this);
 
         Button threeDSecureButton = (Button) findViewById(R.id.btn_three_d_secure);
@@ -68,6 +82,19 @@ public class PollingActivity extends AppCompatActivity {
         mPollingAdapter = new PollingAdapter();
         recyclerView.setAdapter(mPollingAdapter);
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent.getData() != null && intent.getData().getQuery() != null) {
+            String clientSecret = intent.getData().getQueryParameter(QUERY_CLIENT_SECRET);
+            String sourceId = intent.getData().getQueryParameter(QUERY_SOURCE_ID);
+            if (clientSecret != null && sourceId != null) {
+                addNewSource(sourceId, clientSecret);
+            }
+            mPollingDialogController.dismissDialog();
+        }
     }
 
     @Override
@@ -100,14 +127,15 @@ public class PollingActivity extends AppCompatActivity {
                                         FUNCTIONAL_SOURCE_PUBLISHABLE_KEY);
                             }
                         });
+
         mCompositeSubscription.add(cardSourceObservable
                 .subscribeOn(Schedulers.io())
-                .doOnSubscribe(
-                        new Action0() {
-                            @Override
-                            public void call() {
-                                mProgressDialogController.startProgress();
-                            }
+                .doOnSubscribe(new Action0() {
+                    @Override
+                    public void call() {
+                        mProgressDialogController.setMessageResource(R.string.createSource);
+                        mProgressDialogController.startProgress();
+                    }
                 })
                 .doOnUnsubscribe(new Action0() {
                     @Override
@@ -117,16 +145,18 @@ public class PollingActivity extends AppCompatActivity {
                 })
                 .flatMap(
                         new Func1<Source, Observable<Source>>() {
+                            // This mapping converts the card source into a 3DS source
                             @Override
                             public Observable<Source> call(Source source) {
                                 if (source == null || !Source.CARD.equals(source.getType())) {
                                     return null;
                                 }
 
+                                // This represents a request for a 3DS purchase of 10.00 euro.
                                 final SourceParams threeDParams = SourceParams.createThreeDSecureParams(
                                         1000L,
                                         "EUR",
-                                        "example://return",
+                                        RETURN_URL,
                                         source.getId());
 
                                 return Observable.fromCallable(
@@ -142,10 +172,12 @@ public class PollingActivity extends AppCompatActivity {
                         })
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
+                        // Because we've made the mapping above, we're now subscribing
+                        // to the result of creating a 3DS Source
                         new Action1<Source>() {
                             @Override
                             public void call(Source source) {
-                                addNewSource(source);
+                                showDialog(source);
                             }
                         },
                         new Action1<Throwable>() {
@@ -154,11 +186,67 @@ public class PollingActivity extends AppCompatActivity {
                                 mErrorDialogHandler.showError(throwable.getMessage());
                             }
                         }
-
                 ));
     }
 
-    void addNewSource(Source source) {
-        mPollingAdapter.addItem(source.getType(), source.getStatus(), source.getRedirect().getUrl());
+    void showDialog(final Source source) {
+        mPollingDialogController.showDialog(source.getRedirect().getUrl());
+    }
+
+    void addNewSource(String sourceId, String clientSecret) {
+        mProgressDialogController.setMessageResource(R.string.pollingSource);
+        mProgressDialogController.startProgress();
+        mStripe.pollSource(
+                sourceId,
+                clientSecret,
+                FUNCTIONAL_SOURCE_PUBLISHABLE_KEY,
+                new PollingResponseHandler() {
+                    int count = 0;
+                    @Override
+                    public void onPollingResponse(PollingResponse pollingResponse) {
+                        count++;
+                        mProgressDialogController.finishProgress();
+                        if (pollingResponse.isSuccess()) {
+                            Source source = pollingResponse.getSource();
+                            if (source == null) {
+                                return;
+                            }
+                            mPollingAdapter.addItem(
+                                    getCountString(count),
+                                    source.getStatus(),
+                                    source.getId());
+                        } else if (pollingResponse.isExpired()){
+                            Source source = pollingResponse.getSource();
+                            mPollingAdapter.addItem(
+                                    getCountString(count),
+                                    source.getStatus(),
+                                    "Polling Expired");
+                        } else {
+                            StripeException stripeEx = pollingResponse.getStripeException();
+                            Source source = pollingResponse.getSource();
+                            if (stripeEx != null) {
+                                mPollingAdapter.addItem(
+                                        getCountString(count),
+                                        "error",
+                                        stripeEx.getMessage());
+                            } else {
+                                mPollingAdapter.addItem(
+                                        getCountString(count),
+                                        source.getStatus(),
+                                        source.getId());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onRetry(int millis) {
+                        count++;
+                    }
+                },
+                null);
+    }
+
+    private static String getCountString(int count) {
+        return String.format(Locale.ENGLISH, "API Queries: %d", count);
     }
 }
--- example/src/main/java/com/stripe/example/controller/ProgressDialogController.java
+++ example/src/main/java/com/stripe/example/controller/ProgressDialogController.java
@@ -1,6 +1,7 @@
 package com.stripe.example.controller;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.v4.app.FragmentManager;
 
 import com.stripe.example.R;
@@ -17,6 +18,14 @@ public class ProgressDialogController {
     public ProgressDialogController(@NonNull FragmentManager fragmentManager) {
         mFragmentManager = fragmentManager;
         mProgressFragment = ProgressDialogFragment.newInstance(R.string.progressMessage);
+    }
+
+    public void setMessageResource(@StringRes int resId) {
+        if (mProgressFragment.isVisible()) {
+            mProgressFragment.dismiss();
+            mProgressFragment = null;
+        }
+        mProgressFragment = ProgressDialogFragment.newInstance(resId);
     }
 
     public void startProgress() {
