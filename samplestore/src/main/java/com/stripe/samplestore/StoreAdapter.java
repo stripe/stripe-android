d78acb92ede9a448f29bd7b96a8e256eae1024bc
Michael McDuffee <mrmcduff@stripe.com>
2017-04-03 16:53:16 -0700

    showing an unformatted list of emoji and prices

--- samplestore/src/main/java/com/stripe/samplestore/StoreAdapter.java
+++ samplestore/src/main/java/com/stripe/samplestore/StoreAdapter.java
@@ -6,12 +6,13 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.util.Currency;
 import java.util.Locale;
-
 
 public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> {
 
-    static final int[] EMOJI_CLOTHES = {
+    private static final int[] EMOJI_CLOTHES = {
             0x1F455,
             0x1F456,
             0x1F457,
@@ -28,7 +29,7 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
             0x1F462
     };
 
-    static final int[] EMOJI_PRICES = {
+    private static final int[] EMOJI_PRICES = {
             2000,
             4000,
             3000,
@@ -46,32 +47,45 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
     };
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+        private Currency mCurrency;
         private TextView mEmojiTextView;
         private TextView mPriceTextView;
 
-        ViewHolder(LinearLayout pollingLayout) {
+        ViewHolder(LinearLayout pollingLayout, Currency currency) {
             super(pollingLayout);
             mEmojiTextView = (TextView) pollingLayout.findViewById(R.id.tv_emoji);
             mPriceTextView = (TextView) pollingLayout.findViewById(R.id.tv_price);
+            mCurrency = currency;
         }
 
         void setEmoji(int emojiUnicode) {
             mEmojiTextView.setText(getEmojiByUnicode(emojiUnicode));
         }
 
-        void setPrice(int priceInPennies) {
-            mPriceTextView.setText(getPriceString(priceInPennies));
+        void setPrice(int price) {
+            mPriceTextView.setText(getPriceString(price, mCurrency));
+        }
+    }
+
+    private Currency mCurrency;
+
+    public StoreAdapter(String currencyCode) {
+        try {
+            mCurrency = Currency.getInstance(currencyCode);
+        } catch (IllegalArgumentException badCurrencyCode) {
+            mCurrency = Currency.getInstance(Locale.US);
         }
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder.setEmoji(EMOJI_CLOTHES[position]);
+        holder.setPrice(EMOJI_PRICES[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return EMOJI_CLOTHES.length;
     }
 
     @Override
@@ -79,7 +93,7 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
         LinearLayout pollingView = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.store_item, parent, false);
 
-        ViewHolder vh = new ViewHolder(pollingView);
+        ViewHolder vh = new ViewHolder(pollingView, mCurrency);
         return vh;
     }
 
@@ -87,9 +101,39 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
         return new String(Character.toChars(unicode));
     }
 
-    static String getPriceString(int priceInPennies) {
-        int cents = priceInPennies % 100;
-        int dollars = priceInPennies / 100;
-        return String.format(Locale.ENGLISH, "%d.%d", dollars, cents);
-    };
+    static String getPriceString(int price, Currency currency) {
+        int fractionDigits = currency.getDefaultFractionDigits();
+        int totalLength = String.valueOf(price).length();
+        StringBuilder builder = new StringBuilder();
+        builder.append('\u00A4');
+
+        if (fractionDigits == 0) {
+            for (int i = 0; i < totalLength; i++) {
+                builder.append('#');
+            }
+            DecimalFormat noDecimalCurrencyFormat = new DecimalFormat(builder.toString());
+            noDecimalCurrencyFormat.setCurrency(currency);
+            return noDecimalCurrencyFormat.format(price);
+        }
+
+        int beforeDecimal = totalLength - fractionDigits;
+        for (int i = 0; i < beforeDecimal; i++) {
+            builder.append('#');
+        }
+        // So we display "$0.55" instead of "$.55"
+        if (totalLength <= fractionDigits) {
+            builder.append('0');
+        }
+        builder.append('.');
+        for (int i = 0; i < fractionDigits; i++) {
+            builder.append('0');
+        }
+        double modBreak = Math.pow(10, fractionDigits);
+        double decimalPrice = price / modBreak;
+
+        DecimalFormat decimalFormat = new DecimalFormat(builder.toString());
+        decimalFormat.setCurrency(currency);
+
+        return decimalFormat.format(decimalPrice);
+    }
 }
