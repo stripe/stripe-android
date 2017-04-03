9389117b48021a0cddeb23ded1cdefc6a762c8f6
Michael McDuffee <mrmcduff@stripe.com>
2017-04-03 15:03:49 -0700

    adding boilerplate and constants for store application

--- samplestore/src/main/java/com/stripe/samplestore/StoreActivity.java
+++ samplestore/src/main/java/com/stripe/samplestore/StoreActivity.java
@@ -1,24 +1,70 @@
 package com.stripe.samplestore;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 public class StoreActivity extends AppCompatActivity {
-
+    
+    static final int PURCHASE_REQUEST = 37;
+    
+    private static final String EXTRA_EMOJI_PURCHASED = "EXTRA_EMOJI_PURCHASED";
+    private static final String EXTRA_PRICE_PAID = "EXTRA_PRICE_PAID";
+    
+    public static Intent createPurchaseCompleteIntent(int emojiUnicode, long amount) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(EXTRA_EMOJI_PURCHASED, emojiUnicode);
+        returnIntent.putExtra(EXTRA_PRICE_PAID, amount);
+        return returnIntent;
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
-
-        StoreAdapter adapter = new StoreAdapter("USD");
+        
+        StoreAdapter adapter = new StoreAdapter(this);
+        ItemDivider dividerDecoration = new ItemDivider(this, R.drawable.item_divider);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_store_items);
-
+        
         RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(linearLayoutManager);
-
+        recyclerView.addItemDecoration(dividerDecoration);
         recyclerView.setAdapter(adapter);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        if (requestCode == PURCHASE_REQUEST && resultCode == RESULT_OK) {
+            int emojiUnicode = data.getExtras().getInt(EXTRA_EMOJI_PURCHASED, -1);
+            long price = data.getExtras().getLong(EXTRA_PRICE_PAID, -1L);
+            if (emojiUnicode != -1 && price != -1L) {
+                displayPurchase(emojiUnicode, price);
+            }
+        }
+    }
+    
+    private void displayPurchase(int emojiUnicode, long price) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = LayoutInflater.from(this)
+        .inflate(R.layout.purchase_complete_notification, null);
+        
+        TextView emojiView = (TextView) dialogView.findViewById(R.id.dlg_emoji_display);
+        emojiView.setText(StoreUtils.getEmojiByUnicode(emojiUnicode));
+        TextView priceView = (TextView) dialogView.findViewById(R.id.dlg_price_display);
+        priceView.setText(StoreUtils.getPriceString(price, null));
+        
+        builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- samplestore/src/main/java/com/stripe/samplestore/StoreAdapter.java
+++ samplestore/src/main/java/com/stripe/samplestore/StoreAdapter.java
@@ -1,140 +1,119 @@
 package com.stripe.samplestore;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.text.DecimalFormat;
 import java.util.Currency;
-import java.util.Locale;
 
 public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> {
-
+    
     private static final int[] EMOJI_CLOTHES = {
-            0x1F455,
-            0x1F456,
-            0x1F457,
-            0x1F458,
-            0x1F459,
-            0x1F45A,
-            0x1F45B,
-            0x1F45C,
-            0x1F45D,
-            0x1F45E,
-            0x1F45F,
-            0x1F460,
-            0x1F461,
-            0x1F462
+    0x1F455,
+    0x1F456,
+    0x1F457,
+    0x1F458,
+    0x1F459,
+    0x1F45A,
+    0x1F45B,
+    0x1F45C,
+    0x1F45D,
+    0x1F45E,
+    0x1F45F,
+    0x1F460,
+    0x1F461,
+    0x1F462
     };
-
+    
     private static final int[] EMOJI_PRICES = {
-            2000,
-            4000,
-            3000,
-            700,
-            600,
-            1000,
-            2000,
-            2500,
-            800,
-            3000,
-            2000,
-            5000,
-            5500,
-            6000
+    2000,
+    4000,
+    3000,
+    700,
+    600,
+    1000,
+    2000,
+    2500,
+    800,
+    3000,
+    2000,
+    5000,
+    5500,
+    6000
     };
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    
+    class ViewHolder extends RecyclerView.ViewHolder {
         private Currency mCurrency;
         private TextView mEmojiTextView;
         private TextView mPriceTextView;
-
-        ViewHolder(LinearLayout pollingLayout, Currency currency) {
+        
+        ViewHolder(final LinearLayout pollingLayout, Currency currency) {
             super(pollingLayout);
             mEmojiTextView = (TextView) pollingLayout.findViewById(R.id.tv_emoji);
             mPriceTextView = (TextView) pollingLayout.findViewById(R.id.tv_price);
             mCurrency = currency;
+            pollingLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    v.setPressed(true);
+                    launchPurchaseActivityForIndex(getAdapterPosition());
+                }
+            });
         }
-
+        
         void setEmoji(int emojiUnicode) {
-            mEmojiTextView.setText(getEmojiByUnicode(emojiUnicode));
+            mEmojiTextView.setText(StoreUtils.getEmojiByUnicode(emojiUnicode));
         }
-
+        
         void setPrice(int price) {
-            mPriceTextView.setText(getPriceString(price, mCurrency));
+            mPriceTextView.setText(StoreUtils.getPriceString(price, mCurrency));
         }
     }
-
+    
+    // Storing an activity here only so we can launch for result
+    private Activity mActivity;
     private Currency mCurrency;
-
-    public StoreAdapter(String currencyCode) {
-        try {
-            mCurrency = Currency.getInstance(currencyCode);
-        } catch (IllegalArgumentException badCurrencyCode) {
-            mCurrency = Currency.getInstance(Locale.US);
-        }
+    
+    public StoreAdapter(Activity activity) {
+        mActivity = activity;
+        // Note: our sample backend assumes USD as currency. This code would be
+        // otherwise functional if you switched that assumption on the backend and passed
+        // currency code as a parameter.
+        mCurrency = Currency.getInstance("USD");
     }
-
+    
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.setEmoji(EMOJI_CLOTHES[position]);
         holder.setPrice(EMOJI_PRICES[position]);
     }
-
+    
     @Override
     public int getItemCount() {
         return EMOJI_CLOTHES.length;
     }
-
+    
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LinearLayout pollingView = (LinearLayout) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.store_item, parent, false);
-
+        .inflate(R.layout.store_item, parent, false);
+        
         ViewHolder vh = new ViewHolder(pollingView, mCurrency);
         return vh;
     }
-
-    static String getEmojiByUnicode(int unicode){
-        return new String(Character.toChars(unicode));
-    }
-
-    static String getPriceString(int price, Currency currency) {
-        int fractionDigits = currency.getDefaultFractionDigits();
-        int totalLength = String.valueOf(price).length();
-        StringBuilder builder = new StringBuilder();
-        builder.append('\u00A4');
-        builder.append(' ');
-
-        if (fractionDigits == 0) {
-            for (int i = 0; i < totalLength; i++) {
-                builder.append('#');
-            }
-            DecimalFormat noDecimalCurrencyFormat = new DecimalFormat(builder.toString());
-            noDecimalCurrencyFormat.setCurrency(currency);
-            return noDecimalCurrencyFormat.format(price);
-        }
-
-        int beforeDecimal = totalLength - fractionDigits;
-        for (int i = 0; i < beforeDecimal; i++) {
-            builder.append('#');
-        }
-        // So we display "$0.55" instead of "$.55"
-        if (totalLength <= fractionDigits) {
-            builder.append('0');
-        }
-        builder.append('.');
-        for (int i = 0; i < fractionDigits; i++) {
-            builder.append('0');
-        }
-        double modBreak = Math.pow(10, fractionDigits);
-        double decimalPrice = price / modBreak;
-
-        DecimalFormat decimalFormat = new DecimalFormat(builder.toString());
-        decimalFormat.setCurrency(currency);
-
-        return decimalFormat.format(decimalPrice);
+    
+    private void launchPurchaseActivityForIndex(int index) {
+        Intent paymentLaunchIntent = PaymentActivity.createIntent(
+                                                                  mActivity,
+                                                                  EMOJI_CLOTHES[index],
+                                                                  EMOJI_PRICES[index],
+                                                                  mCurrency);
+        mActivity.startActivityForResult(
+                                         paymentLaunchIntent, StoreActivity.PURCHASE_REQUEST);
     }
 }
