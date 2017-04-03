b22d76b0787fcfae3397dd412030c00596ec90c7
Michael McDuffee <mrmcduff@stripe.com>
2017-04-03 15:03:49 -0700

    adding boilerplate and constants for store application

---
+++ samplestore/src/main/java/com/stripe/samplestore/StoreActivity.java
@@ -0,0 +1,13 @@
+package com.stripe.samplestore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
