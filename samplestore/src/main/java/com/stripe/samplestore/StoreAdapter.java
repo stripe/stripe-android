273fcb1de0ac0bfd088fa9061d0c17a4e10d2a0a
Michael McDuffee <mrmcduff@stripe.com>
2017-04-03 16:53:16 -0700

    showing an unformatted list of emoji and prices

--- samplestore/src/main/java/com/stripe/samplestore/StoreAdapter.java
+++ samplestore/src/main/java/com/stripe/samplestore/StoreAdapter.java
@@ -12,46 +12,46 @@ import android.widget.TextView;
 import java.util.Currency;
 
 public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> {
-    
+
     private static final int[] EMOJI_CLOTHES = {
-    0x1F455,
-    0x1F456,
-    0x1F457,
-    0x1F458,
-    0x1F459,
-    0x1F45A,
-    0x1F45B,
-    0x1F45C,
-    0x1F45D,
-    0x1F45E,
-    0x1F45F,
-    0x1F460,
-    0x1F461,
-    0x1F462
+            0x1F455,
+            0x1F456,
+            0x1F457,
+            0x1F458,
+            0x1F459,
+            0x1F45A,
+            0x1F45B,
+            0x1F45C,
+            0x1F45D,
+            0x1F45E,
+            0x1F45F,
+            0x1F460,
+            0x1F461,
+            0x1F462
     };
-    
+
     private static final int[] EMOJI_PRICES = {
-    2000,
-    4000,
-    3000,
-    700,
-    600,
-    1000,
-    2000,
-    2500,
-    800,
-    3000,
-    2000,
-    5000,
-    5500,
-    6000
+            2000,
+            4000,
+            3000,
+            700,
+            600,
+            1000,
+            2000,
+            2500,
+            800,
+            3000,
+            2000,
+            5000,
+            5500,
+            6000
     };
-    
+
     class ViewHolder extends RecyclerView.ViewHolder {
         private Currency mCurrency;
         private TextView mEmojiTextView;
         private TextView mPriceTextView;
-        
+
         ViewHolder(final LinearLayout pollingLayout, Currency currency) {
             super(pollingLayout);
             mEmojiTextView = (TextView) pollingLayout.findViewById(R.id.tv_emoji);
@@ -65,20 +65,20 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
                 }
             });
         }
-        
+
         void setEmoji(int emojiUnicode) {
             mEmojiTextView.setText(StoreUtils.getEmojiByUnicode(emojiUnicode));
         }
-        
+
         void setPrice(int price) {
             mPriceTextView.setText(StoreUtils.getPriceString(price, mCurrency));
         }
     }
-    
+
     // Storing an activity here only so we can launch for result
     private Activity mActivity;
     private Currency mCurrency;
-    
+
     public StoreAdapter(Activity activity) {
         mActivity = activity;
         // Note: our sample backend assumes USD as currency. This code would be
@@ -86,34 +86,34 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
         // currency code as a parameter.
         mCurrency = Currency.getInstance("USD");
     }
-    
+
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.setEmoji(EMOJI_CLOTHES[position]);
         holder.setPrice(EMOJI_PRICES[position]);
     }
-    
+
     @Override
     public int getItemCount() {
         return EMOJI_CLOTHES.length;
     }
-    
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LinearLayout pollingView = (LinearLayout) LayoutInflater.from(parent.getContext())
-        .inflate(R.layout.store_item, parent, false);
-        
+                .inflate(R.layout.store_item, parent, false);
+
         ViewHolder vh = new ViewHolder(pollingView, mCurrency);
         return vh;
     }
-    
+
     private void launchPurchaseActivityForIndex(int index) {
         Intent paymentLaunchIntent = PaymentActivity.createIntent(
-                                                                  mActivity,
-                                                                  EMOJI_CLOTHES[index],
-                                                                  EMOJI_PRICES[index],
-                                                                  mCurrency);
+                mActivity,
+                EMOJI_CLOTHES[index],
+                EMOJI_PRICES[index],
+                mCurrency);
         mActivity.startActivityForResult(
-                                         paymentLaunchIntent, StoreActivity.PURCHASE_REQUEST);
+                paymentLaunchIntent, StoreActivity.PURCHASE_REQUEST);
     }
 }
--- samplestore/src/test/java/com/stripe/samplestore/StoreAdapterTest.java
+++ samplestore/src/test/java/com/stripe/samplestore/StoreAdapterTest.java
@@ -10,33 +10,33 @@ import java.util.Currency;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Test class for {@link StoreAdapter}.
+ * Test class for {@link StoreUtils}.
  */
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = 23)
-public class StoreAdapterTest {
-
+public class StoreUtilsTest {
+    
     @Test
     public void getPriceString_withUSD_returnsExpectedValues() {
         Currency dollars = Currency.getInstance("USD");
-        String firstPrice = StoreAdapter.getPriceString(1000, dollars);
-        String secondPrice = StoreAdapter.getPriceString(55, dollars);
-        String tinyPrice = StoreAdapter.getPriceString(7, dollars);
-        String bigPrice = StoreAdapter.getPriceString(1234567890, dollars);
-        assertEquals("$ 10.00", firstPrice);
-        assertEquals("$ 0.55", secondPrice);
-        assertEquals("$ 0.07", tinyPrice);
-        assertEquals("$ 12345678.90", bigPrice);
+        String firstPrice = StoreUtils.getPriceString(1000, dollars);
+        String secondPrice = StoreUtils.getPriceString(55, dollars);
+        String tinyPrice = StoreUtils.getPriceString(7, dollars);
+        String bigPrice = StoreUtils.getPriceString(1234567890, dollars);
+        assertEquals("$10.00", firstPrice);
+        assertEquals("$0.55", secondPrice);
+        assertEquals("$0.07", tinyPrice);
+        assertEquals("$12345678.90", bigPrice);
     }
-
+    
     @Test
     public void getPriceString_withYen_returnsExpectedValues() {
         Currency yen = Currency.getInstance("JPY");
-        String firstPrice = StoreAdapter.getPriceString(1000, yen);
-        String secondPrice = StoreAdapter.getPriceString(55, yen);
-        String bigPrice = StoreAdapter.getPriceString(1234567890, yen);
-        assertEquals("JPY 1000", firstPrice);
-        assertEquals("JPY 55", secondPrice);
-        assertEquals("JPY 1234567890", bigPrice);
+        String firstPrice = StoreUtils.getPriceString(1000, yen);
+        String secondPrice = StoreUtils.getPriceString(55, yen);
+        String bigPrice = StoreUtils.getPriceString(1234567890, yen);
+        assertEquals("JPY1000", firstPrice);
+        assertEquals("JPY55", secondPrice);
+        assertEquals("JPY1234567890", bigPrice);
     }
 }
