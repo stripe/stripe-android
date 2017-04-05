31ff76bdf0be31057157791de0abb2d8ec8d1b00
Michael McDuffee <mrmcduff@stripe.com>
2017-04-05 14:35:51 -0700

    adding stripe sample store with full connection for non-3ds charges

--- samplestore/src/main/java/com/stripe/samplestore/StripeService.java
+++ samplestore/src/main/java/com/stripe/samplestore/StripeService.java
@@ -2,14 +2,18 @@ package com.stripe.samplestore;
 
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 import rx.Observable;
 
+/**
+ * The {@link retrofit2.Retrofit} interface that creates our API service.
+ */
 public interface StripeService {
 
+    @FormUrlEncoded
     @POST("create_charge")
-    Observable<String> createCharge(@Body ChargeParams params);
-
-    @POST("charge")
-    Observable<String> createSimpleCharge(@Body ChargeParams params);
+    Observable<Void> createQueryCharge(
+            @Field("amount") long amount,
+            @Field("source") String source);
 }
