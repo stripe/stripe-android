64d2b7e2218d60373fa609bae0f5bb99c7447a1e
Michael McDuffee <mrmcduff@stripe.com>
2017-04-18 10:20:55 -0700

    bumping the version in the Emoji Store app

--- stripe/src/test/java/com/stripe/android/view/CardInputWidgetTest.java
+++ stripe/src/test/java/com/stripe/android/view/CardInputWidgetTest.java
@@ -19,7 +19,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
