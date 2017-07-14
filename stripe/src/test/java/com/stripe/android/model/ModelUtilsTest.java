d64943363c38595589e0a6a90949b2dc7db3e322
Kathy Sun <ksun@stripe.com>
2017-07-13 20:26:17 -0700

    test for normalize year method

--- stripe/src/test/java/com/stripe/android/model/ModelUtilsTest.java
+++ stripe/src/test/java/com/stripe/android/model/ModelUtilsTest.java
@@ -5,8 +5,12 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.util.Calendar;
+
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Test class for {@link ModelUtils}.
@@ -43,5 +47,21 @@ public class ModelUtilsTest {
     @Test
     public void wholePositiveNumberShouldFailIfLetters() {
         assertFalse(ModelUtils.isWholePositiveNumber("1a"));
+    }
+
+    @Test
+    public void normalizeSameCenturyShouldPass() {
+        Calendar now = Calendar.getInstance();
+        int year = 1997;
+        now.set(Calendar.YEAR, year);
+        assertEquals(ModelUtils.normalizeYear(97, now), year);
+    }
+
+    @Test
+    public void normalizeDifferentCenturyShouldFail() {
+        Calendar now = Calendar.getInstance();
+        int year = 1997;
+        now.set(Calendar.YEAR, year);
+        assertNotEquals(ModelUtils.normalizeYear(97, now), 2097);
     }
 }
