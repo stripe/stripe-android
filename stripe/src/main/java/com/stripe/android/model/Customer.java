4085fab5241268c88a54f622cb14342262531190
Michael McDuffee <mrmcduff@stripe.com>
2017-10-09 15:41:18 -0700

    removing unused function

--- stripe/src/main/java/com/stripe/android/model/Customer.java
+++ stripe/src/main/java/com/stripe/android/model/Customer.java
@@ -2,7 +2,6 @@ package com.stripe.android.model;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.VisibleForTesting;
 
 import com.stripe.android.StripeNetworkUtils;
 
@@ -82,16 +81,6 @@ public class Customer extends StripeJsonModel {
 
     public String getUrl() {
         return mUrl;
-    }
-
-    @VisibleForTesting
-    void addSource(@NonNull CustomerSource customerSource) {
-        mSources.add(customerSource);
-        if (mTotalCount == null) {
-            mTotalCount = 1;
-        } else {
-            mTotalCount++;
-        }
     }
 
     @Nullable
