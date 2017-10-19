d9e9d6985a839a261fbb49838034e1c38aa98ec8
Kathy Sun <ksun@stripe.com>
2017-10-19 14:01:34 -0700

    strings

--- scripts/pull_translations.rb
+++ scripts/pull_translations.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'set'
 require 'fileutils'
 # Usage:
-# $ ./scripts/pull_translations.rb
+# $ ./scripts/pull_translations.rb `fetch-password PhraseApp-access-token`
 # (running fetch-password from within this script causes strange dependency
 # issues)
 #
@@ -30,16 +30,16 @@ def remove_translation_and_comment(node)
   node.unlink
 end
 
-def remove_excess_strings(filename, included_strings)
-  puts '▸ Removing translations for unused strings'
+def remove_excess_strings(filename, included_strings, show_info)
+  puts '▸ Removing unused translated strings'
   strings_doc = File.open(filename) { |f| Nokogiri::XML(f) }
   translation_nodes = strings_doc.xpath('//string').to_a
   all_strings = strings_doc.xpath('//string/@name').to_a.to_set(&:text)
   excess_strings = all_strings - included_strings
   translation_nodes.each do |translation|
     if excess_strings.include? translation['name']
-      puts 'translation ' + translation
       remove_translation_and_comment(translation)
+      puts "Removed translation %s" % translation if show_info
     end
   end
   File.write(filename, strings_doc.to_xml)
@@ -65,35 +65,34 @@ def make_translations_hash(filename, included_strings)
       translations_hash[translation['name']] = Struct::Translation.new(
         translation.content, translation_comment, translation
       )
-    else
-      puts 'not adding' + translation['name']
     end
   end
   translations_hash
 end
 
-def replace_updated_translations(filename, translations_hash)
+def replace_updated_translations(filename, translations_hash, show_info)
   puts '▸ Updating strings that have changed'
   strings_doc = File.open(filename) { |f| Nokogiri::XML(f) }
   translation_nodes = strings_doc.xpath('//string').to_a
   translation_nodes.each do |translation|
-    translation_comment
     next unless translations_hash.key?(translation['name'])
     key = translation['name']
     next unless !translation.previous.previous.nil? &&
                 translation.previous.previous.comment?
     translation_comment = translation.previous.previous
     unless translation_comment.text.eql? translations_hash[key].description_comment.text
+      puts "Updating translation comment for %s" % translation_comment if show_info
       translation_comment.replace(translations_hash[key].description_comment)
     end
     unless translation.content.eql? translations_hash[key].translated_string
+      puts "Updating translation to %s" % translation if show_info
       translation.content = translations_hash[key].translated_string
     end
   end
   File.write(filename, strings_doc.to_xml)
 end
 
-def add_new_translations(filename, translations_hash)
+def add_new_translations(filename, translations_hash, show_info)
   puts '▸ Adding new translations'
   strings_doc = File.open(filename) { |f| Nokogiri::XML(f) }
   translation_nodes = strings_doc.xpath('//string')
@@ -111,12 +110,13 @@ def add_new_translations(filename, translations_hash)
     end
     strings_doc.root.add_child(new_line_node.dup)
     strings_doc.root.add_child(translation.string_node)
+    puts "Added translation for %s" % translation.string_node if show_info
   end
   strings_doc.root.add_child(last_newline_node)
   File.write(filename, strings_doc.to_xml)
 end
 
-def update_translations(default_strings_file, supported_languages)
+def update_translations(default_strings_file, supported_languages, show_info=true)
   puts '▸ Building set of locally added strings'
   default_strings_doc = File.open(default_strings_file) { |f| Nokogiri::XML(f) }
   included_strings = default_strings_doc.xpath('//string/@name')
@@ -126,7 +126,10 @@ def update_translations(default_strings_file, supported_languages)
   puts '▸ Removing translations for languages we do not yet support'
   language_dirs = Dir.glob 'stripe/res/values-*'
   language_dirs.each do |dir|
-    FileUtils.rm_rf(dir) unless supported_languages.include? dir.split('-')[1]
+    unless supported_languages.include? dir.split('-')[1]
+      puts 'Ignoring ' + dir + ' because it is not a supported language' if show_info
+      FileUtils.rm_rf(dir) 
+    end
   end
 
   supported_languages.each do |lang|
@@ -139,14 +142,26 @@ def update_translations(default_strings_file, supported_languages)
       included_strings
     )
     current_translations_filename = 'stripe/res/values-' + lang + '/strings.xml'
-    remove_excess_strings(current_translations_filename, included_strings)
-    replace_updated_translations(current_translations_filename, translations_hash)
-    add_new_translations(current_translations_filename, translations_hash)
+    remove_excess_strings(
+      current_translations_filename,
+      included_strings,
+      show_info
+    )
+    replace_updated_translations(
+      current_translations_filename, 
+      translations_hash,
+      show_info
+    )
+    add_new_translations(
+      current_translations_filename,
+      translations_hash,
+      show_info
+    )
     File.delete(downloaded_filename)
   end
 end
 
-DEFAULT_STRINGS_FILE = 'stripe/res/values/strings.xml'.freeze
+DEFAULT_STRINGS_FILE = 'stripe/res/values/strings.xml'
 @langs = %w(de es fr it ja nl zh)
 
 update_translations(DEFAULT_STRINGS_FILE, @langs)
