b3a25c997ab6d3afce5aa5ddda9fdbba3580f0b9
Michael McDuffee <mrmcduff@stripe.com>
2017-10-31 10:45:00 -0700

    addressing review and rebasing

--- example/src/main/java/com/stripe/example/activity/PaymentSessionActivity.java
+++ example/src/main/java/com/stripe/example/activity/PaymentSessionActivity.java
@@ -15,6 +15,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.stripe.android.CustomerSession;
+import com.stripe.android.PayWithGoogleUtils;
 import com.stripe.android.PaymentSession;
 import com.stripe.android.PaymentSessionConfig;
 import com.stripe.android.PaymentSessionData;
@@ -226,7 +227,9 @@ public class PaymentSessionActivity extends AppCompatActivity {
             stringBuilder.append(data.getShippingMethod()).append('\n');
             if (data.getShippingTotal() > 0) {
                 stringBuilder.append("Shipping total: ")
-                        .append(getPriceString(data.getShippingTotal(), currency));
+                        .append(PayWithGoogleUtils.getPriceString(
+                                data.getShippingTotal(),
+                                currency));
             }
         }
 
@@ -252,60 +255,6 @@ public class PaymentSessionActivity extends AppCompatActivity {
         super.onDestroy();
         mPaymentSession.onDestroy();
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
-
-    }
-
-    /**
-     * Converts an integer price in the lowest currency denomination to a Google string value.
-     * For instance (100, USD) -> "1.00", but (100, JPY) -> "100"
-     * @param price the price in the lowest available currency denomination
-     * @param currency the {@link Currency} used to determine how many digits after the decimal
-     * @return a String that can be used as an Android Pay price string
-     */
-    @NonNull
-    public static String getPriceString(Long price, @NonNull Currency currency) {
-        if (price == null) {
-            return "";
-        }
-
-        int fractionDigits = currency.getDefaultFractionDigits();
-        int totalLength = String.valueOf(price).length();
-        StringBuilder builder = new StringBuilder();
-
-        if (fractionDigits == 0) {
-            for (int i = 0; i < totalLength; i++) {
-                builder.append('#');
-            }
-            DecimalFormat noDecimalCurrencyFormat = new DecimalFormat(builder.toString());
-            noDecimalCurrencyFormat.setCurrency(currency);
-            noDecimalCurrencyFormat.setGroupingUsed(false);
-            return noDecimalCurrencyFormat.format(price);
-        }
-
-        int beforeDecimal = totalLength - fractionDigits;
-        for (int i = 0; i < beforeDecimal; i++) {
-            builder.append('#');
-        }
-
-        // So we display "0.55" instead of ".55"
-        if (totalLength <= fractionDigits) {
-            builder.append('0');
-        }
-        builder.append('.');
-        for (int i = 0; i < fractionDigits; i++) {
-            builder.append('0');
-        }
-        double modBreak = Math.pow(10, fractionDigits);
-        double decimalPrice = price / modBreak;
-
-        // No matter the Locale, Android Pay requires a dot for the decimal separator.
-        DecimalFormatSymbols symbolOverride = new DecimalFormatSymbols();
-        symbolOverride.setDecimalSeparator('.');
-        DecimalFormat decimalFormat = new DecimalFormat(builder.toString(), symbolOverride);
-        decimalFormat.setCurrency(currency);
-        decimalFormat.setGroupingUsed(false);
-
-        return decimalFormat.format(decimalPrice);
     }
 
     private ShippingInformation getExampleShippingInfo() {
