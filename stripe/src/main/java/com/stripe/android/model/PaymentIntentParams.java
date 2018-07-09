8ee507927cb04a44488f8dfa5e1da2d87c8b7fc2
ksun-stripe <ksun@stripe.com>
2018-07-09 13:43:39 -0700

    params (#600)

---
+++ stripe/src/main/java/com/stripe/android/model/PaymentIntentParams.java
@@ -0,0 +1,212 @@
+package com.stripe.android.model;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaymentIntentParams {
+
+    static final String API_PARAM_SOURCE_DATA = "source_data";
+    static final String API_PARAM_SOURCE_ID = "source_id";
+    static final String API_PARAM_RETURN_URL = "return_url";
+    static final String API_PARAM_CLIENT_SECRET = "client_secret";
+
+    private Map<String, Object> mExtraParams;
+    private SourceParams mSourceParams;
+    private String mSourceId;
+    private String mClientSecret;
+    private String mReturnUrl;
+
+    private PaymentIntentParams() {
+    }
+
+    /**
+     * Create a custom {@link PaymentIntentParams}. Incorrect attributes may result in errors
+     * when connecting to Stripe's API.
+     *
+     * @return an empty Params object. Call the setter methods on this class to specific values on
+     * the params
+     */
+    public static PaymentIntentParams createCustomParams() {
+        return new PaymentIntentParams();
+    }
+
+    /**
+     * Create the parameters necessary for confirming a PaymentIntent while attaching Source data
+     *
+     * @param sourceParams params for the source that will be attached to this payment intent
+     * @param clientSecret client secret from the PaymentIntent that is to be confirmed
+     * @param returnUrl the URL the customer should be redirected to after the authorization
+     *                  process
+     * @return params that can be use to confirm a PaymentIntent
+     */
+    @NonNull
+    public static PaymentIntentParams createConfirmPaymentIntentWithSourceData(
+            @Nullable SourceParams sourceParams,
+            @NonNull String clientSecret,
+            @Nullable String returnUrl) {
+        return new PaymentIntentParams()
+                .setSourceParams(sourceParams)
+                .setClientSecret(clientSecret)
+                .setReturnUrl(returnUrl);
+    }
+
+    /**
+     * Create the parameters necessary for confirming a PaymentIntent while attaching a source that
+     * already exits.
+     *
+     * @param sourceId the ID of the source that is being attached to the PaymentIntent being
+     *                 confirmed
+     * @param clientSecret client secret from the PaymentIntent being confirmed
+     * @param returnUrl the URL the customer should be redirected to after the authorization
+     *                  process
+     * @return params that can be use to confirm a PaymentIntent
+     */
+    @NonNull
+    public static PaymentIntentParams createConfirmPaymentIntentWithSourceId(
+            @Nullable String sourceId,
+            @NonNull String clientSecret,
+            @Nullable String returnUrl) {
+        return new PaymentIntentParams()
+                .setSourceId(sourceId)
+                .setClientSecret(clientSecret)
+                .setReturnUrl(returnUrl);
+    }
+
+    /**
+     * Create the parameters necessary for retrieving the details of PaymentIntent
+     *
+     * @param clientSecret client secret from the PaymentIntent that is being retrieved
+     * @return params that can be used to retrieve a PaymentIntent
+     */
+    @NonNull
+    public static PaymentIntentParams createRetrievePaymentIntent(
+            @NonNull String clientSecret) {
+        return new PaymentIntentParams().setClientSecret(clientSecret);
+    }
+
+    /**
+     * Sets the source data that will be included with this PaymentIntent
+     *
+     * @param sourceParams params for the source that will be attached to this payment intent. Only
+     *                     one of SourceParam and SourceId should be used at at time
+     * @return {@code this}, for chaining purposes
+     */
+    @NonNull
+    public PaymentIntentParams setSourceParams(@NonNull SourceParams sourceParams) {
+        mSourceParams = sourceParams;
+        return this;
+    }
+
+    /**
+     * Sets a pre-existing source that will be attached to this PaymentIntent
+     * @param sourceId the ID of the source that is being attached to this PaymentIntent. Only
+     *                     one of SourceParam and SourceId should be used at at time
+     * @return {@code this}, for chaining purposes
+     */
+    @NonNull
+    public PaymentIntentParams setSourceId(@NonNull String sourceId) {
+        mSourceId = sourceId;
+        return this;
+    }
+
+    /**
+     * Sets the client secret that is used to authenticate actions on this PaymentIntent
+     * @param clientSecret client secret associated with this PaymentIntent
+     * @return {@code this}, for chaining purposes
+     */
+    public PaymentIntentParams setClientSecret(@NonNull String clientSecret) {
+        mClientSecret = clientSecret;
+        return this;
+    }
+
+    /**
+     * @param returnUrl the URL the customer should be redirected to after the authorization
+     *                  process
+     * @return {@code this}, for chaining purposes
+     */
+    @NonNull
+    public PaymentIntentParams setReturnUrl(@NonNull String returnUrl) {
+        mReturnUrl = returnUrl;
+        return this;
+    }
+
+    /**
+     * @param extraParams params that will be included in the request. Incorrect params may result
+     *                    in errors when connecting to Stripe's API.
+     * @return {@code this}, for chaining purposes
+     */
+    @NonNull
+    public PaymentIntentParams setExtraParams(@NonNull Map<String, Object> extraParams) {
+        mExtraParams = extraParams;
+        return this;
+    }
+
+    /**
+     * Create a string-keyed map representing this object that is
+     * ready to be sent over the network.
+     *
+     * @return a String-keyed map
+     */
+    @NonNull
+    public Map<String, Object> toParamMap() {
+        Map<String, Object> networkReadyMap = new HashMap<>();
+        if (mSourceParams != null) {
+            networkReadyMap.put(API_PARAM_SOURCE_DATA, mSourceParams.toParamMap());
+        } else if (mSourceId != null) {
+            networkReadyMap.put(API_PARAM_SOURCE_ID, mSourceId);
+        }
+        if (mReturnUrl != null) {
+            networkReadyMap.put(API_PARAM_RETURN_URL, mReturnUrl);
+        }
+        networkReadyMap.put(API_PARAM_CLIENT_SECRET, mClientSecret);
+        if (mExtraParams != null) {
+            networkReadyMap.putAll(mExtraParams);
+        }
+        return networkReadyMap;
+    }
+
+    /**
+     * @return client secret associated with the PaymentIntent, used to identify the PaymentIntent
+     * and authenticate actions.
+     */
+    @Nullable
+    public String getClientSecret() {
+        return mClientSecret;
+    }
+
+    /**
+     * @return extraparams that will be included in the request
+     */
+    @Nullable
+    public Map<String, Object> getExtraParams() {
+        return mExtraParams;
+    }
+
+    /**
+     * @return params for the source that will be attached to this payment intent
+     */
+    @Nullable
+    public SourceParams getSourceParams() {
+        return mSourceParams;
+    }
+
+    /**
+     * @return the ID of the existing source that is being attached to this PaymentIntent.
+     */
+    @Nullable
+    public String getSourceId() {
+        return mSourceId;
+    }
+
+    /**
+     * @return  the URL the customer should be redirected to after the authorization process
+     */
+    @Nullable
+    public String getReturnUrl() {
+        return mReturnUrl;
+    }
+
+}
---
+++ stripe/src/test/java/com/stripe/android/model/PaymentIntentParamsTest.java
@@ -0,0 +1,115 @@
+package com.stripe.android.model;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.stripe.android.view.CardInputTestActivity.VALID_VISA_NO_SPACES;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 25)
+public class PaymentIntentParamsTest {
+    private static Card FULL_FIELDS_VISA_CARD =
+            new Card(VALID_VISA_NO_SPACES,
+                    12,
+                    2050,
+                    "123",
+                    "Captain Cardholder",
+                    "1 ABC Street",
+                    "Apt. 123",
+                    "San Francisco",
+                    "CA",
+                    "94107",
+                    "US",
+                    "usd");
+
+    private static String TEST_CLIENT_SECRET =
+            "pi_1CkiBMLENEVhOs7YMtUehLau_secret_s4O8SDh7s6spSmHDw1VaYPGZA";
+
+    private static String TEST_RETURN_URL = "stripe://return_url";
+    private static String TEST_SOURCE_ID = "src_123testsourceid";
+
+    @Test
+    public void createConfirmPaymentIntentWithSourceData_withAllFields_hasExpectedFields() {
+        SourceParams sourceParams = SourceParams.createCardParams(FULL_FIELDS_VISA_CARD);
+        PaymentIntentParams params = PaymentIntentParams.createConfirmPaymentIntentWithSourceData(
+                sourceParams,
+                TEST_CLIENT_SECRET,
+                TEST_RETURN_URL);
+
+        Assert.assertEquals(TEST_CLIENT_SECRET, params.getClientSecret());
+        Assert.assertEquals(TEST_RETURN_URL, params.getReturnUrl());
+        Assert.assertEquals(sourceParams, params.getSourceParams());
+    }
+
+    @Test
+    public void createConfirmPaymentIntentWithSourceId_withAllFields_hasExpectedFields() {
+        PaymentIntentParams params = PaymentIntentParams.createConfirmPaymentIntentWithSourceId(
+                TEST_SOURCE_ID,
+                TEST_CLIENT_SECRET,
+                TEST_RETURN_URL);
+
+        Assert.assertEquals(TEST_CLIENT_SECRET, params.getClientSecret());
+        Assert.assertEquals(TEST_RETURN_URL, params.getReturnUrl());
+        Assert.assertEquals(TEST_SOURCE_ID, params.getSourceId());
+    }
+
+    @Test
+    public void createRetrievePaymentIntentWithSourceId_hasExpectedFields() {
+        PaymentIntentParams params = PaymentIntentParams.createRetrievePaymentIntent(
+                TEST_CLIENT_SECRET);
+
+        Assert.assertEquals(TEST_CLIENT_SECRET, params.getClientSecret());
+        Assert.assertNull(params.getReturnUrl());
+        Assert.assertNull(params.getExtraParams());
+        Assert.assertNull(params.getSourceId());
+        Assert.assertNull(params.getSourceParams());
+    }
+
+    @Test
+    public void createCustomParam_toParamMap_createsExpectedMap() {
+        PaymentIntentParams paymentIntentParams = PaymentIntentParams.createCustomParams();
+        paymentIntentParams
+                .setReturnUrl(TEST_RETURN_URL)
+                .setClientSecret(TEST_CLIENT_SECRET)
+                .setSourceId(TEST_SOURCE_ID);
+
+        Map<String, Object> paramMap = paymentIntentParams.toParamMap();
+
+        Assert.assertEquals(paramMap.get(PaymentIntentParams.API_PARAM_SOURCE_ID), TEST_SOURCE_ID);
+        Assert.assertEquals(
+                paramMap.get(PaymentIntentParams.API_PARAM_CLIENT_SECRET), TEST_CLIENT_SECRET);
+        Assert.assertEquals(
+                paramMap.get(PaymentIntentParams.API_PARAM_RETURN_URL), TEST_RETURN_URL);
+    }
+
+    @Test
+    public void toParamMap_whenExtraParamsProvided_createsExpectedMap() {
+        PaymentIntentParams paymentIntentParams = PaymentIntentParams.createRetrievePaymentIntent(
+                TEST_CLIENT_SECRET);
+        Map<String, Object> extraParams = new HashMap<>();
+        String extraParamKey1 = "extra_param_key_1";
+        String extraParamKey2 = "extra_param_key_2";
+        String extraParamValue1 = "extra_param_value_1";
+        String extraParamValue2 = "extra_param_value_2";
+        extraParams.put(extraParamKey1, extraParamValue1);
+        extraParams.put(extraParamKey2, extraParamValue2);
+        paymentIntentParams.setExtraParams(extraParams);
+
+        Map<String, Object> paramMap = paymentIntentParams.toParamMap();
+
+        Assert.assertEquals(
+                paramMap.get(PaymentIntentParams.API_PARAM_CLIENT_SECRET), TEST_CLIENT_SECRET);
+        Assert.assertEquals(
+                paramMap.get(extraParamKey1), extraParamValue1);
+        Assert.assertEquals(
+                paramMap.get(extraParamKey2), extraParamValue2);
+    }
+
+}
