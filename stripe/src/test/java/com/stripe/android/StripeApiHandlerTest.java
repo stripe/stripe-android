46987a4759c9b24fe706f168c9840906d4efaf23
Michael Shafrir <mshafrir@stripe.com>
2019-04-22 11:46:09 -0400

    Create StripeApiHandler.start3ds2Auth()
    `StripeApiHandler` method to do 3ds2 authentication

--- stripe/src/test/java/com/stripe/android/StripeApiHandlerTest.java
+++ stripe/src/test/java/com/stripe/android/StripeApiHandlerTest.java
@@ -30,6 +30,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.UUID;
 
 import static com.stripe.android.StripeApiHandler.POST;
 import static org.junit.Assert.assertEquals;
@@ -212,6 +213,29 @@ public class StripeApiHandlerTest {
 
         // Check that we get a source back; we don't care about its fields for this test.
         assertNotNull(source);
+    }
+
+    @Test
+    public void start3ds2Auth_withInvalidSource_shouldThrowInvalidRequestException()
+            throws APIConnectionException, APIException, CardException,
+            AuthenticationException {
+        final Stripe3DS2AuthParams authParams = new Stripe3DS2AuthParams(
+                "src_invalid",
+                "1.0.0",
+                UUID.randomUUID().toString(),
+                "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiUlNBLU9BRVAtMjU2In0.nid2Q-Ii21cSPHBaszR5KSXz866yX9I7AthLKpfWZoc7RIfz11UJ1EHuvIRDIyqqJ8txNUKKoL4keqMTqK5Yc5TqsxMn0nML8pZaPn40nXsJm_HFv3zMeOtRR7UTewsDWIgf5J-A6bhowIOmvKPCJRxspn_Cmja-YpgFWTp08uoJvqgntgg1lHmI1kh1UV6DuseYFUfuQlICTqC3TspAzah2CALWZORF_QtSeHc_RuqK02wOQMs-7079jRuSdBXvI6dQnL5ESH25wHHosfjHMZ9vtdUFNJo9J35UI1sdWFDzzj8k7bt0BupZhyeU0PSM9EHP-yv01-MQ9eslPTVNbFJ9YOHtq8WamvlKDr1sKxz6Ac_gUM8NgEcPP9SafPVxDd4H1Fwb5-4NYu2AD4xoAgMWE-YtzvfIFXZcU46NDoi6Xum3cHJqTH0UaOhBoqJJft9XZXYW80fjts-v28TkA76-QPF7CTDM6KbupvBkSoRq218eJLEywySXgCwf-Q95fsBtnnyhKcvfRaByq5kT7PH3DYD1rCQLexJ76A79kurre9pDjTKAv85G9DNkOFuVUYnNB3QGFReCcF9wzkGnZXdfkgN2BkB6n94bbkEyjbRb5r37XH6oRagx2fWLVj7kC5baeIwUPVb5kV_x4Kle7C-FPY1Obz4U7s6SVRnLGXY.IP9OcQx5uZxBRluOpn1m6Q.w-Ko5Qg6r-KCmKnprXEbKA7wV-SdLNDAKqjtuku6hda_0crOPRCPU4nn26Yxj7EG.p01pl8CKukuXzjLeY3a_Ew",
+                "{\"kty\":\"EC\",\"use\":\"sig\",\"crv\":\"P-256\",\"kid\":\"b23da28b-d611-46a8-93af-44ad57ce9c9d\",\"x\":\"hSwyaaAp3ppSGkpt7d9G8wnp3aIXelsZVo05EPpqetg\",\"y\":\"OUVOv9xPh5RYWapla0oz3vCJWRRXlDmppy5BGNeSl-A\"}",
+                "2.1.0",
+                10
+        );
+
+        try {
+            mApiHandler.start3ds2Auth(authParams, "pk_test_fdjfCYpGSwAX24KUEiuaAAWX");
+            fail("Expected InvalidRequestException");
+        } catch (InvalidRequestException e) {
+            assertEquals("source", e.getParam());
+            assertEquals("resource_missing", e.getErrorCode());
+        }
     }
 
     @Test
