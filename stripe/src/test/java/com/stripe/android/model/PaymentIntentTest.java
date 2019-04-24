7ccd133976224d6b77e83b31a83a2d294f506863
Michael Shafrir <mshafrir@stripe.com>
2019-04-24 09:42:58 -0400

    Create PaymentIntent.RedirectData
    PaymentIntent's `next_action.redirect_to_url` hash includes both
    `url` and `return_url`. Capture both of these fields in `RedirectData`.

--- stripe/src/test/java/com/stripe/android/model/PaymentIntentTest.java
+++ stripe/src/test/java/com/stripe/android/model/PaymentIntentTest.java
@@ -59,8 +59,10 @@ public class PaymentIntentTest {
             "  \"livemode\": false,\n" +
             "  \"next_source_action\": {" +
             "       \"type\": \"authorize_with_url\"," +
-            "           authorize_with_url: {" +
-            "           url: \""+ BAD_URL +"\" } " +
+            "           \"authorize_with_url\": {" +
+            "             \"url\": \""+ BAD_URL +"\"," +
+            "             \"return_url\": \"yourapp://post-authentication-return-url\"" +
+            "           } " +
             "       },\n" +
             "  \"receipt_email\": null,\n" +
             "  \"shipping\": null,\n" +
@@ -133,7 +135,8 @@ public class PaymentIntentTest {
             "\t\"next_action\": {\n" +
             "\t\t\"type\": \"redirect_to_url\",\n" +
             "\t\t\"redirect_to_url\": {\n" +
-            "\t\t\t\"url\": \"https://example.com/redirect\"\n" +
+            "\t\t\t\"url\": \"https://example.com/redirect\",\n" +
+            "\t\t\t\"return_url\": \"yourapp://post-authentication-return-url\"\n" +
             "\t\t}\n" +
             "\t}\n" +
             "}";
@@ -145,7 +148,8 @@ public class PaymentIntentTest {
             "\t\"next_action\": {\n" +
             "\t\t\"type\": \"authorize_with_url\",\n" +
             "\t\t\"authorize_with_url\": {\n" +
-            "\t\t\t\"url\": \"https://example.com/redirect\"\n" +
+            "\t\t\t\"url\": \"https://example.com/redirect\",\n" +
+            "\t\t\t\"return_url\": \"yourapp://post-authentication-return-url\"\n" +
             "\t\t}\n" +
             "\t}\n" +
             "}";
