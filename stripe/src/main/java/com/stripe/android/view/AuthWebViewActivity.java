2be612e185954906c1dcacb8d306b9a5d7d6cd6d
Michael Shafrir <mshafrir@stripe.com>
2019-04-24 09:42:58 -0400

    Create AuthWebViewActivity and AuthWebView
    These classes handle showing an authentication page in-app,
    then redirecting the customer back to the app after authentication
    is complete.
    
    MOBILE3DS2-359

---
+++ stripe/src/main/java/com/stripe/android/view/AuthWebViewActivity.java
@@ -0,0 +1,22 @@
+package com.stripe.android.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.stripe.android.R;
+
+public class AuthWebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.auth_web_view_layout);
+
+        final String returnUrl = getIntent().getStringExtra(AuthWebViewStarter.EXTRA_RETURN_URL);
+
+        final AuthWebView webView = findViewById(R.id.auth_web_view);
+        webView.init(this, returnUrl);
+        webView.loadUrl(getIntent().getStringExtra(AuthWebViewStarter.EXTRA_AUTH_URL));
+    }
+}
