ce078015908eb11f796ea70871c4c7c5dcf8a8e6
keith siilats <keith@siilats.com>
2019-04-28 03:28:16 -0400

    Update PaymentSession.java

--- stripe/src/main/java/com/stripe/android/PaymentSession.java
+++ stripe/src/main/java/com/stripe/android/PaymentSession.java
@@ -12,6 +12,7 @@ import com.stripe.android.model.Customer;
 import com.stripe.android.view.PaymentFlowActivity;
 import com.stripe.android.view.PaymentMethodsActivity;
 import com.stripe.android.view.PaymentMethodsActivityStarter;
+import com.stripe.android.PaymentConfiguration;
 
 import java.lang.ref.WeakReference;
 
