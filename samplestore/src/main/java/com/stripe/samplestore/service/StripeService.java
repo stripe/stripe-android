0e3ce3049229ce444a8f8436d99d8869e38fe0f4
Michael Shafrir <mshafrir@stripe.com>
2019-05-02 13:26:59 -0400

    Add support for /confirm_payment endpoint
    This endpoint already exists on the example backend:
    https://github.com/stripe/example-ios-backend/blob/master/web.rb#L69

--- samplestore/src/main/java/com/stripe/samplestore/service/StripeService.java
+++ samplestore/src/main/java/com/stripe/samplestore/service/StripeService.java
@@ -17,6 +17,16 @@ public interface StripeService {
     @POST("capture_payment")
     Observable<ResponseBody> capturePayment(@Body Map<String, Object> params);
 
+    /**
+     * Used for Payment Intent Manual confirmation
+     *
+     * @see <a
+     * href="https://stripe.com/docs/payments/payment-intents/quickstart#manual-confirmation-flow">
+     * Manual Confirmation Flow</a>
+     */
+    @POST("confirm_payment")
+    Observable<ResponseBody> confirmPayment(@Body Map<String, Object> params);
+
     @FormUrlEncoded
     @POST("ephemeral_keys")
     Observable<ResponseBody> createEphemeralKey(@FieldMap Map<String, String> apiVersionMap);
