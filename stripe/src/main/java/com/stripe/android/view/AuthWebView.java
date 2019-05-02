b7908f17db041def9f0e82d7a8f15d03c0525565
Jason Emerick <jemerick-c@stripe.com>
2019-05-02 17:25:12 -0400

    add auth web view progress dialog

--- stripe/src/main/java/com/stripe/android/view/AuthWebView.java
+++ stripe/src/main/java/com/stripe/android/view/AuthWebView.java
@@ -3,8 +3,10 @@ package com.stripe.android.view;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -14,10 +16,12 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.stripe.android.R;
+
 /**
  * A {@link WebView} used for authenticating payment and deep-linking the user back to the
  * PaymentIntent's return_url[0].
- *
+ * <p>
  * [0] https://stripe.com/docs/api/payment_intents/confirm#confirm_payment_intent-return_url
  */
 class AuthWebView extends WebView {
@@ -51,19 +55,20 @@ class AuthWebView extends WebView {
     static class AuthWebViewClient extends WebViewClient {
         @NonNull private final Activity mActivity;
         @NonNull private final Uri mReturnUrl;
+        @NonNull private final ProgressDialog mProgressDialog;
 
         AuthWebViewClient(@NonNull Activity activity, @NonNull String returnUrl) {
             mActivity = activity;
             mReturnUrl = Uri.parse(returnUrl);
+            mProgressDialog = new AuthWebViewProgressDialog(mActivity);
         }
 
         @Override
         public boolean shouldOverrideUrlLoading(@NonNull WebView view,
                                                 @NonNull String urlString) {
-            final Uri uri = Uri.parse(urlString);
             if (isReturnUrl(urlString)) {
                 mActivity.startActivity(new Intent(Intent.ACTION_VIEW)
-                        .setData(uri));
+                        .setData(Uri.parse(urlString)));
                 mActivity.finish();
                 return true;
             }
@@ -84,6 +89,37 @@ class AuthWebView extends WebView {
                     mReturnUrl.getScheme().equals(uri.getScheme()) &&
                     mReturnUrl.getHost() != null &&
                     mReturnUrl.getHost().equals(uri.getHost());
+        }
+
+        @Override
+        public void onPageStarted(@NonNull WebView view, @NonNull String url,
+                                  @Nullable Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            if (!mProgressDialog.isShowing()) {
+                mProgressDialog.show();
+            }
+        }
+
+        @Override
+        public void onPageFinished(@NonNull WebView view, @NonNull String url) {
+            super.onPageFinished(view, url);
+            if (mProgressDialog.isShowing()) {
+                mProgressDialog.dismiss();
+            }
+        }
+    }
+
+    private static class AuthWebViewProgressDialog extends ProgressDialog {
+        AuthWebViewProgressDialog(@NonNull Context context) {
+            super(context);
+            setIndeterminate(true);
+            setCancelable(false);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            setContentView(R.layout.auth_web_view_dialog_layout);
         }
     }
 }
