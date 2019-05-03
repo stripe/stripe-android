c60cb1c26879e7d8b25d37949931449e8b7485fd
jemerick-stripe <48026502+jemerick-stripe@users.noreply.github.com>
2019-05-03 11:43:04 -0400

    Auth Web View Progress Indicator (#933)
    * add auth web view progress dialog
    
    * switch to progress bar

--- stripe/src/main/java/com/stripe/android/view/AuthWebView.java
+++ stripe/src/main/java/com/stripe/android/view/AuthWebView.java
@@ -5,19 +5,24 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import com.stripe.android.R;
 
 /**
  * A {@link WebView} used for authenticating payment and deep-linking the user back to the
  * PaymentIntent's return_url[0].
- *
+ * <p>
  * [0] https://stripe.com/docs/api/payment_intents/confirm#confirm_payment_intent-return_url
  */
 class AuthWebView extends WebView {
@@ -40,12 +45,34 @@ class AuthWebView extends WebView {
 
     void init(@NonNull Activity activity, @NonNull String returnUrl) {
         setWebViewClient(new AuthWebViewClient(activity, returnUrl));
+        setWebChromeClient(new AuthWebViewChromeClient(activity));
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     private void configureSettings() {
         getSettings().setJavaScriptEnabled(true);
         getSettings().setAllowContentAccess(false);
+    }
+
+    static class AuthWebViewChromeClient extends WebChromeClient {
+        @NonNull private final ProgressBar mProgressBar;
+
+        AuthWebViewChromeClient(@NonNull Activity activity) {
+            mProgressBar = activity.findViewById(R.id.auth_web_view_progress_bar);
+        }
+
+        @Override
+        public void onProgressChanged(@NonNull WebView view, int progress) {
+            if (progress < 100) {
+                if (mProgressBar.getVisibility() == GONE) {
+                    mProgressBar.setVisibility(VISIBLE);
+                }
+                mProgressBar.setProgress(progress);
+            } else if (progress == 100) {
+                mProgressBar.setVisibility(GONE);
+                mProgressBar.setProgress(0);
+            }
+        }
     }
 
     static class AuthWebViewClient extends WebViewClient {
@@ -60,10 +87,9 @@ class AuthWebView extends WebView {
         @Override
         public boolean shouldOverrideUrlLoading(@NonNull WebView view,
                                                 @NonNull String urlString) {
-            final Uri uri = Uri.parse(urlString);
             if (isReturnUrl(urlString)) {
                 mActivity.startActivity(new Intent(Intent.ACTION_VIEW)
-                        .setData(uri));
+                        .setData(Uri.parse(urlString)));
                 mActivity.finish();
                 return true;
             }
