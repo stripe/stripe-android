82016d65aa6b6f8bacd0b920db14cbba5c338e71
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2019-05-14 20:56:37 -0400

    Update StripeNetworkUtils#addUidParamsToPaymentIntent to support payment_method_data (#947)

--- stripe/src/main/java/com/stripe/android/StripeNetworkUtils.java
+++ stripe/src/main/java/com/stripe/android/StripeNetworkUtils.java
@@ -6,6 +6,7 @@ import android.support.annotation.VisibleForTesting;
 
 import com.stripe.android.model.BankAccount;
 import com.stripe.android.model.Card;
+import com.stripe.android.model.PaymentIntentParams;
 import com.stripe.android.model.Token;
 
 import java.util.AbstractMap;
@@ -148,8 +149,17 @@ public class StripeNetworkUtils {
     }
 
     void addUidParamsToPaymentIntent(@NonNull Map<String, Object> params) {
-        if (params.containsKey("source_data") && params.get("source_data") instanceof Map) {
-            addUidParams((Map) params.get("source_data"));
+        final Object sourceData = params
+                .get(PaymentIntentParams.API_PARAM_SOURCE_DATA);
+        if (sourceData instanceof Map) {
+            addUidParams((Map) sourceData);
+            return;
+        }
+
+        final Object paymentMethodData = params
+                .get(PaymentIntentParams.API_PARAM_PAYMENT_METHOD_DATA);
+        if (paymentMethodData instanceof Map) {
+            addUidParams((Map) paymentMethodData);
         }
     }
 
@@ -159,14 +169,13 @@ public class StripeNetworkUtils {
             return;
         }
 
-        String hashGuid = StripeTextUtils.shaHashInput(guid);
-        String muid = mPackageName + guid;
-        String hashMuid = StripeTextUtils.shaHashInput(muid);
-
+        final String hashGuid = StripeTextUtils.shaHashInput(guid);
         if (!StripeTextUtils.isBlank(hashGuid)) {
             params.put(GUID, hashGuid);
         }
 
+        final String muid = mPackageName + guid;
+        final String hashMuid = StripeTextUtils.shaHashInput(muid);
         if (!StripeTextUtils.isBlank(hashMuid)) {
             params.put(MUID, hashMuid);
         }
