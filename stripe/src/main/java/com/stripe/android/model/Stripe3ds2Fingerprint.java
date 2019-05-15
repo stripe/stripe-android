e5d7b63641594609b59201da9f1aeb67a9ae3d12
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2019-05-15 15:59:27 -0400

    Create Stripe3ds2Fingerprint.DirectoryServerName @StringDef (#949)

--- stripe/src/main/java/com/stripe/android/model/Stripe3ds2Fingerprint.java
+++ stripe/src/main/java/com/stripe/android/model/Stripe3ds2Fingerprint.java
@@ -1,9 +1,14 @@
 package com.stripe.android.model;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 class Stripe3ds2Fingerprint {
     private static final String FIELD_TYPE = "type";
@@ -14,7 +19,7 @@ class Stripe3ds2Fingerprint {
     private static final String TYPE = "stripe_3ds2_fingerprint";
 
     @NonNull final String source;
-    @NonNull final String directoryServerName;
+    @Nullable @DirectoryServerName final String directoryServerName;
     @NonNull final String serverTransactionId;
 
     @NonNull
@@ -27,16 +32,43 @@ class Stripe3ds2Fingerprint {
         }
 
         final String source = json.getString(FIELD_THREE_D_SECURE_2_SOURCE);
-        final String directoryServerName = json.getString(FIELD_DIRECTORY_SERVER_NAME);
+        @DirectoryServerName final String directoryServerName =
+                toDirectoryServerName(json.getString(FIELD_DIRECTORY_SERVER_NAME));
         final String serverTransactionId = json.getString(FIELD_SERVER_TRANSACTION_ID);
         return new Stripe3ds2Fingerprint(source, directoryServerName, serverTransactionId);
     }
 
+    @Nullable
+    @DirectoryServerName
+    private static String toDirectoryServerName(@Nullable String code) {
+        if (DirectoryServerName.AMERICAN_EXPRESS.equals(code)) {
+            return DirectoryServerName.AMERICAN_EXPRESS;
+        } else if (DirectoryServerName.MASTERCARD.equals(code)) {
+            return DirectoryServerName.MASTERCARD;
+        } else if (DirectoryServerName.VISA.equals(code)) {
+            return DirectoryServerName.VISA;
+        } else {
+            return null;
+        }
+    }
+
     private Stripe3ds2Fingerprint(@NonNull String source,
-                                  @NonNull String directoryServerName,
+                                  @Nullable @DirectoryServerName String directoryServerName,
                                   @NonNull String serverTransactionId) {
         this.source = source;
         this.directoryServerName = directoryServerName;
         this.serverTransactionId = serverTransactionId;
+    }
+
+    @Retention(RetentionPolicy.SOURCE)
+    @StringDef({
+            DirectoryServerName.AMERICAN_EXPRESS,
+            DirectoryServerName.MASTERCARD,
+            DirectoryServerName.VISA
+    })
+    @interface DirectoryServerName {
+        String AMERICAN_EXPRESS = "american_express";
+        String MASTERCARD = "mastercard";
+        String VISA = "visa";
     }
 }
