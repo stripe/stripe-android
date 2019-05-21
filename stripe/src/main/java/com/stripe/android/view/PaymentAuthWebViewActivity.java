c6836f778ebfe686fd55fea365b0d06c07fc4681
jemerick-stripe <48026502+jemerick-stripe@users.noreply.github.com>
2019-05-21 16:24:03 -0400

    Add toolbar to payment auth web view activity (#966)
    * add toolbar to payment auth web view activity
    
    * Hardcode strings until translations

--- stripe/src/main/java/com/stripe/android/view/PaymentAuthWebViewActivity.java
+++ stripe/src/main/java/com/stripe/android/view/PaymentAuthWebViewActivity.java
@@ -3,6 +3,9 @@ package com.stripe.android.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.stripe.android.R;
 
@@ -12,6 +15,9 @@ public class PaymentAuthWebViewActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment_auth_web_view_layout);
+
+        final Toolbar toolbar = findViewById(R.id.payment_auth_web_view_toolbar);
+        setSupportActionBar(toolbar);
 
         final String returnUrl = getIntent()
                 .getStringExtra(PaymentAuthWebViewStarter.EXTRA_RETURN_URL);
@@ -25,5 +31,21 @@ public class PaymentAuthWebViewActivity extends AppCompatActivity {
     public void onBackPressed() {
         setResult(RESULT_CANCELED);
         super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.payment_auth_web_view_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_close) {
+            setResult(RESULT_CANCELED);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
