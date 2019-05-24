0532ed0067b4da09d9ae97345d335f0dbc3f0487
Michael Shafrir <mshafrir@stripe.com>
2019-05-23 23:42:09 -0400

    Pass PaymentIntent object to Stripe3ds2CompletionStarter

---
+++ stripe/src/test/java/com/stripe/android/Stripe3ds2CompletionStarterTest.java
@@ -0,0 +1,46 @@
+package com.stripe.android;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.stripe.android.model.PaymentIntentFixtures;
+import com.stripe.android.view.PaymentAuthenticationExtras;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@RunWith(RobolectricTestRunner.class)
+public class Stripe3ds2CompletionStarterTest {
+
+    private Stripe3ds2CompletionStarter mStarter;
+
+    @Mock private Activity mActivity;
+
+    @Captor private ArgumentCaptor<Intent> mIntentArgumentCaptor;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mStarter = new Stripe3ds2CompletionStarter(mActivity, 500);
+    }
+
+    @Test
+    public void start_shouldAddPaymentIntentClientSecretToIntent() {
+        mStarter.start(Stripe3ds2CompletionStarter.StartData.createForComplete(
+                PaymentIntentFixtures.PI_REQUIRES_3DS2, "Y"));
+        verify(mActivity).startActivityForResult(mIntentArgumentCaptor.capture(), eq(500));
+        final Intent intent = mIntentArgumentCaptor.getValue();
+        assertEquals(PaymentIntentFixtures.PI_REQUIRES_3DS2.getClientSecret(),
+                intent.getStringExtra(PaymentAuthenticationExtras.CLIENT_SECRET));
+    }
+}
