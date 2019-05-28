5a534c33f68b2ab785dcfa92f496ab846fe3213c
Jason Emerick <jemerick-c@stripe.com>
2019-05-28 16:19:14 -0400

    remove import

--- stripe/src/main/java/com/stripe/android/view/PaymentAuthWebView.java
+++ stripe/src/main/java/com/stripe/android/view/PaymentAuthWebView.java
@@ -5,7 +5,6 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
