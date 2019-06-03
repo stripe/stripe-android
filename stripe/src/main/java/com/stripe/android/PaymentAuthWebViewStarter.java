a1ee8de36f471852206a0bd61e43bf1a073bfcc5
Jason Emerick <jemerick-c@stripe.com>
2019-06-03 15:32:10 -0400

    add ability to start payment auth web view with stripe toolbar customization

--- stripe/src/main/java/com/stripe/android/PaymentAuthWebViewStarter.java
+++ stripe/src/main/java/com/stripe/android/PaymentAuthWebViewStarter.java
@@ -3,8 +3,10 @@ package com.stripe.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.stripe.android.model.PaymentIntent;
+import com.stripe.android.stripe3ds2.init.ui.StripeToolbarCustomization;
 import com.stripe.android.view.ActivityStarter;
 import com.stripe.android.view.PaymentAuthWebViewActivity;
 
@@ -15,24 +17,33 @@ import com.stripe.android.view.PaymentAuthWebViewActivity;
 public class PaymentAuthWebViewStarter implements ActivityStarter<PaymentIntent.RedirectData> {
     public static final String EXTRA_AUTH_URL = "auth_url";
     public static final String EXTRA_RETURN_URL = "return_url";
+    public static final String EXTRA_UI_CUSTOMIZATION = "ui_customization";
 
     @NonNull private final Activity mActivity;
     private final int mRequestCode;
+    @Nullable private final StripeToolbarCustomization mToolbarCustomization;
 
     PaymentAuthWebViewStarter(@NonNull Activity activity, int requestCode) {
+        this(activity, requestCode, null);
+    }
+
+    PaymentAuthWebViewStarter(@NonNull Activity activity, int requestCode,
+                              @Nullable StripeToolbarCustomization toolbarCustomization) {
         mActivity = activity;
         mRequestCode = requestCode;
+        mToolbarCustomization = toolbarCustomization;
     }
 
     /**
      * @param redirectData typically obtained through {@link PaymentIntent#getRedirectData()}
-     *
      */
     public void start(@NonNull PaymentIntent.RedirectData redirectData) {
         final Intent intent = new Intent(mActivity, PaymentAuthWebViewActivity.class)
                 .putExtra(EXTRA_AUTH_URL, redirectData.url.toString())
                 .putExtra(EXTRA_RETURN_URL, redirectData.returnUrl != null ?
-                        redirectData.returnUrl.toString() : null);
+                        redirectData.returnUrl.toString() : null)
+                .putExtra(EXTRA_UI_CUSTOMIZATION, mToolbarCustomization);
+
         mActivity.startActivityForResult(intent, mRequestCode);
     }
 }
--- stripe/src/test/java/com/stripe/android/PaymentAuthWebViewStarterTest.java
+++ stripe/src/test/java/com/stripe/android/PaymentAuthWebViewStarterTest.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.stripe.android.model.PaymentIntentFixtures;
+import com.stripe.android.stripe3ds2.init.ui.StripeToolbarCustomization;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +18,7 @@ import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.verify;
 
 @RunWith(RobolectricTestRunner.class)
@@ -40,6 +42,21 @@ public class PaymentAuthWebViewStarterTest {
         final Intent intent = mIntentArgumentCaptor.getValue();
         final Bundle extras = intent.getExtras();
         assertNotNull(extras);
-        assertEquals(2, extras.size());
+        assertNull(extras.getParcelable(PaymentAuthWebViewStarter.EXTRA_UI_CUSTOMIZATION));
+        assertEquals(3, extras.size());
+    }
+
+    @Test
+    public void start_startsWithCorrectIntentAndRequestCodeAndCustomization() {
+        new PaymentAuthWebViewStarter(mActivity, 50000,
+                new StripeToolbarCustomization()).start(PaymentIntentFixtures.REDIRECT_DATA);
+        verify(mActivity).startActivityForResult(mIntentArgumentCaptor.capture(),
+                mRequestCodeCaptor.capture());
+
+        final Intent intent = mIntentArgumentCaptor.getValue();
+        final Bundle extras = intent.getExtras();
+        assertNotNull(extras);
+        assertNotNull(extras.getParcelable(PaymentAuthWebViewStarter.EXTRA_UI_CUSTOMIZATION));
+        assertEquals(3, extras.size());
     }
 }
