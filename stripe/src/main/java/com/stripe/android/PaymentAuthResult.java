fdcb1077fecad7ddafc837cca0a6f097f4d0922d
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2019-06-04 16:19:17 -0400

    Create PaymentAuthResult.Status to signal information about auth result (#1052)
    A user can inspect the value of this field in `PaymentAuthResult` to
    make a better determination of how to react to the state of the
    PaymentIntent.

--- stripe/src/main/java/com/stripe/android/PaymentAuthResult.java
+++ stripe/src/main/java/com/stripe/android/PaymentAuthResult.java
@@ -1,10 +1,14 @@
 package com.stripe.android;
 
 import android.app.Activity;
+import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 
 import com.stripe.android.model.PaymentIntent;
 import com.stripe.android.model.PaymentIntentParams;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * A model representing the result of a payment authentication attempt via
@@ -15,17 +19,35 @@ import com.stripe.android.model.PaymentIntentParams;
  */
 public final class PaymentAuthResult {
     @NonNull public final PaymentIntent paymentIntent;
+    @Status public final int status;
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({Status.UNKNOWN, Status.SUCCEEDED, Status.FAILED, Status.CANCELED})
+    public @interface Status {
+        int UNKNOWN = 0;
+        int SUCCEEDED = 1;
+        int FAILED = 2;
+        int CANCELED = 3;
+    }
 
     private PaymentAuthResult(@NonNull Builder builder) {
         this.paymentIntent = builder.mPaymentIntent;
+        this.status = builder.mStatus;
     }
 
     static final class Builder {
         private PaymentIntent mPaymentIntent;
+        @Status private int mStatus;
 
         @NonNull
         public Builder setPaymentIntent(@NonNull PaymentIntent paymentIntent) {
             mPaymentIntent = paymentIntent;
+            return this;
+        }
+
+        @NonNull
+        public Builder setStatus(@Status int status) {
+            mStatus = status;
             return this;
         }
 
--- stripe/src/main/java/com/stripe/android/view/PaymentAuthenticationExtras.java
+++ stripe/src/main/java/com/stripe/android/view/PaymentAuthenticationExtras.java
@@ -1,8 +1,19 @@
 package com.stripe.android.view;
 
+import com.stripe.android.model.PaymentIntent;
+
 public final class PaymentAuthenticationExtras {
+    /**
+     * Should be a {@link PaymentIntent#getClientSecret()}
+     */
     public static final String CLIENT_SECRET = "client_secret";
+
     public static final String AUTH_EXCEPTION = "exception";
+
+    /**
+     * See {@link com.stripe.android.PaymentAuthResult.Status} for possible values
+     */
+    public static final String AUTH_STATUS = "status";
 
     private PaymentAuthenticationExtras() {
     }
