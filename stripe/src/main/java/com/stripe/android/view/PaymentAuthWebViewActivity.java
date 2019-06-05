948df716d080d3c15ab30a5a3f453f3a811f11d6
jemerick-stripe <48026502+jemerick-stripe@users.noreply.github.com>
2019-06-05 09:59:37 -0400

    Customize Payment auth web view toolbar (#1039)
    * add ability to start payment auth web view with stripe toolbar customization
    
    * customize toolbar
    
    * bump 3ds2 library dependency
    
    * remove duplicate null check
    
    * checkstyle

--- stripe/src/main/java/com/stripe/android/view/PaymentAuthWebViewActivity.java
+++ stripe/src/main/java/com/stripe/android/view/PaymentAuthWebViewActivity.java
@@ -1,6 +1,10 @@
 package com.stripe.android.view;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,8 +13,13 @@ import android.view.MenuItem;
 
 import com.stripe.android.PaymentAuthWebViewStarter;
 import com.stripe.android.R;
+import com.stripe.android.StripeTextUtils;
+import com.stripe.android.stripe3ds2.init.ui.ToolbarCustomization;
+import com.stripe.android.stripe3ds2.utils.CustomizeUtils;
 
 public class PaymentAuthWebViewActivity extends AppCompatActivity {
+
+    @Nullable private ToolbarCustomization mToolbarCustomization;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +28,9 @@ public class PaymentAuthWebViewActivity extends AppCompatActivity {
 
         final Toolbar toolbar = findViewById(R.id.payment_auth_web_view_toolbar);
         setSupportActionBar(toolbar);
+        mToolbarCustomization =
+                getIntent().getParcelableExtra(PaymentAuthWebViewStarter.EXTRA_UI_CUSTOMIZATION);
+        customizeToolbar(toolbar);
 
         final String returnUrl = getIntent()
                 .getStringExtra(PaymentAuthWebViewStarter.EXTRA_RETURN_URL);
@@ -37,6 +49,13 @@ public class PaymentAuthWebViewActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.payment_auth_web_view_menu, menu);
+
+        if (mToolbarCustomization != null &&
+                !StripeTextUtils.isBlank(mToolbarCustomization.getButtonText())) {
+            final MenuItem closeMenuItem = menu.findItem(R.id.action_close);
+            closeMenuItem.setTitle(mToolbarCustomization.getButtonText());
+        }
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -48,5 +67,24 @@ public class PaymentAuthWebViewActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    void customizeToolbar(@NonNull Toolbar toolbar) {
+        if (mToolbarCustomization != null) {
+            if (!StripeTextUtils.isBlank(mToolbarCustomization.getHeaderText())) {
+                toolbar.setTitle(CustomizeUtils.buildStyledText(this,
+                        mToolbarCustomization.getHeaderText(), mToolbarCustomization));
+            }
+
+            if (mToolbarCustomization.getBackgroundColor() != null) {
+                @ColorInt final int backgroundColor =
+                        Color.parseColor(mToolbarCustomization.getBackgroundColor());
+                toolbar.setBackgroundColor(backgroundColor);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(CustomizeUtils.darken(backgroundColor));
+                }
+            }
+        }
     }
 }
