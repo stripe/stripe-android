95c7c3b43a7eb5e107ad8c1524ad30076f4254f1
Jason Emerick <jemerick-c@stripe.com>
2019-06-28 13:22:48 -0400

    add setup future usage to payment intent

--- stripe/src/main/java/com/stripe/android/model/StripeModel.java
+++ stripe/src/main/java/com/stripe/android/model/StripeModel.java
@@ -3,7 +3,12 @@ package com.stripe.android.model;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 /**
  * Represents a JSON model used in the Stripe Api.
@@ -18,4 +23,19 @@ public abstract class StripeModel {
 
     @Override
     public abstract boolean equals(@Nullable Object obj);
+
+    @NonNull
+    static List<String> jsonArrayToList(@Nullable JSONArray jsonArray) {
+        final List<String> list = new ArrayList<>();
+        if (jsonArray != null) {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                try {
+                    list.add(jsonArray.getString(i));
+                } catch (JSONException ignored) {
+                }
+            }
+        }
+
+        return list;
+    }
 }
