e856f50ef29cf3b105d83985e2a497cdbef6a1e3
Jason Emerick <jemerick-c@stripe.com>
2019-07-01 17:39:03 -0400

    Add example app implementation of setup intents

--- example/src/main/java/com/stripe/example/activity/PaymentAuthActivity.java
+++ example/src/main/java/com/stripe/example/activity/PaymentAuthActivity.java
@@ -15,9 +15,12 @@ import com.stripe.android.ApiResultCallback;
 import com.stripe.android.PaymentAuthConfig;
 import com.stripe.android.PaymentConfiguration;
 import com.stripe.android.PaymentIntentResult;
+import com.stripe.android.SetupIntentResult;
 import com.stripe.android.Stripe;
 import com.stripe.android.model.PaymentIntent;
 import com.stripe.android.model.PaymentIntentParams;
+import com.stripe.android.model.SetupIntent;
+import com.stripe.android.model.SetupIntentParams;
 import com.stripe.example.R;
 import com.stripe.example.module.RetrofitFactory;
 import com.stripe.example.service.StripeService;
@@ -46,6 +49,8 @@ public class PaymentAuthActivity extends AppCompatActivity {
      */
     private static final String PAYMENT_METHOD_3DS2_REQUIRED = "pm_card_threeDSecure2Required";
     private static final String PAYMENT_METHOD_3DS_REQUIRED = "pm_card_threeDSecureRequired";
+    private static final String PAYMENT_METHOD_AUTH_REQUIRED_ON_SETUP =
+            "pm_card_authenticationRequiredOnSetup";
 
     private static final String RETURN_URL = "stripe://payment_auth";
 
@@ -58,6 +63,7 @@ public class PaymentAuthActivity extends AppCompatActivity {
     private StripeService mStripeService;
     private TextView mStatusTextView;
     private Button mBuyButton;
+    private Button mSetupButton;
     private ProgressBar mProgressBar;
 
     @Override
@@ -85,6 +91,9 @@ public class PaymentAuthActivity extends AppCompatActivity {
         mBuyButton = findViewById(R.id.buy_button);
         mBuyButton.setOnClickListener((v) -> createPaymentIntent());
 
+        mSetupButton = findViewById(R.id.setup_button);
+        mSetupButton.setOnClickListener((v) -> createSetupIntent());
+
         mProgressBar = findViewById(R.id.progress_bar);
     }
 
@@ -97,14 +106,25 @@ public class PaymentAuthActivity extends AppCompatActivity {
                         RETURN_URL));
     }
 
+    private void confirmSetupIntent(@NonNull String setupIntentClientSecret) {
+        mStatusTextView.append("\n\nStarting setup intent authentication");
+        mStripe.confirmSetupIntent(this,
+                SetupIntentParams.createConfirmSetupIntenParamsWithPaymentMethodId(
+                        PAYMENT_METHOD_AUTH_REQUIRED_ON_SETUP,
+                        setupIntentClientSecret,
+                        RETURN_URL));
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         mProgressBar.setVisibility(View.VISIBLE);
         mStatusTextView.append("\n\nPayment authentication completed, getting result");
-        mStripe.onPaymentResult(requestCode, resultCode, data,
-                new AuthResultListener(this));
+
+        mStripe.onPaymentResult(requestCode, resultCode, data, new AuthResultListener(this));
+
+        mStripe.onSetupResult(requestCode, resultCode, data, new SetupAuthResultListener(this));
     }
 
     @Override
@@ -133,9 +153,24 @@ public class PaymentAuthActivity extends AppCompatActivity {
                         .doOnSubscribe((d) -> {
                             mProgressBar.setVisibility(View.VISIBLE);
                             mBuyButton.setEnabled(false);
+                            mSetupButton.setEnabled(false);
                             mStatusTextView.setText(R.string.creating_payment_intent);
                         })
                         .subscribe(this::handleCreatePaymentIntentResponse));
+    }
+
+    private void createSetupIntent() {
+        mCompositeSubscription.add(
+                mStripeService.createSetupIntent(new HashMap<>(0))
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .doOnSubscribe((d) -> {
+                            mProgressBar.setVisibility(View.VISIBLE);
+                            mBuyButton.setEnabled(false);
+                            mSetupButton.setEnabled(false);
+                            mStatusTextView.setText(R.string.creating_setup_intent);
+                        })
+                        .subscribe(this::handleCreateSetupIntentResponse));
     }
 
     private void handleCreatePaymentIntentResponse(@NonNull ResponseBody responseBody) {
@@ -151,8 +186,22 @@ public class PaymentAuthActivity extends AppCompatActivity {
         }
     }
 
+    private void handleCreateSetupIntentResponse(@NonNull ResponseBody responseBody) {
+        try {
+            final JSONObject responseData = new JSONObject(responseBody.string());
+            mStatusTextView.append("\n\n" +
+                    getString(R.string.setup_intent_status,
+                            responseData.getString("status")));
+            final String secret = responseData.getString("secret");
+            confirmSetupIntent(secret);
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void onAuthComplete() {
         mBuyButton.setEnabled(true);
+        mSetupButton.setEnabled(true);
         mProgressBar.setVisibility(View.INVISIBLE);
     }
 
@@ -182,6 +231,38 @@ public class PaymentAuthActivity extends AppCompatActivity {
             final PaymentIntent paymentIntent = paymentIntentResult.getIntent();
             activity.mStatusTextView.append("\n\n" +
                     activity.getString(R.string.payment_intent_status, paymentIntent.getStatus()));
+            activity.onAuthComplete();
+        }
+
+        @Override
+        public void onError(@NonNull Exception e) {
+            final PaymentAuthActivity activity = mActivityRef.get();
+            if (activity == null) {
+                return;
+            }
+
+            activity.mStatusTextView.append("\n\nException: " + e.getMessage());
+            activity.onAuthComplete();
+        }
+    }
+
+    private static class SetupAuthResultListener implements ApiResultCallback<SetupIntentResult> {
+        @NonNull private final WeakReference<PaymentAuthActivity> mActivityRef;
+
+        private SetupAuthResultListener(@NonNull PaymentAuthActivity activity) {
+            this.mActivityRef = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void onSuccess(@NonNull SetupIntentResult setupIntentResult) {
+            final PaymentAuthActivity activity = mActivityRef.get();
+            if (activity == null) {
+                return;
+            }
+
+            final SetupIntent setupIntent = setupIntentResult.getIntent();
+            activity.mStatusTextView.append("\n\n" +
+                    activity.getString(R.string.setup_intent_status, setupIntent.getStatus()));
             activity.onAuthComplete();
         }
 
---
+++ example/src/main/java/com/stripe/example/activity/SetupIntentActivity.java
@@ -0,0 +1,231 @@
+package com.stripe.example.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.Stripe;
+import com.stripe.android.model.Card;
+import com.stripe.android.model.PaymentMethod;
+import com.stripe.android.model.PaymentMethodCreateParams;
+import com.stripe.android.model.SetupIntent;
+import com.stripe.android.model.SetupIntentParams;
+import com.stripe.android.view.CardInputWidget;
+import com.stripe.example.R;
+import com.stripe.example.controller.ErrorDialogHandler;
+import com.stripe.example.controller.ProgressDialogController;
+import com.stripe.example.module.RetrofitFactory;
+import com.stripe.example.service.StripeService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
+import retrofit2.Retrofit;
+
+public class SetupIntentActivity extends AppCompatActivity {
+    private static final String TAG = SetupIntentActivity.class.getName();
+
+    private static final String RETURN_URL = "stripe://setup_intent_return";
+
+    @NonNull
+    private final CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+
+    private ProgressDialogController mProgressDialogController;
+    private ErrorDialogHandler mErrorDialogHandler;
+    private Stripe mStripe;
+    private StripeService mStripeService;
+    private String mClientSecret;
+    private Button mCreatePaymentMethod;
+    private Button mConfirmSetupIntent;
+    private Button mRetrieveSetupIntent;
+    private CardInputWidget mCardInputWidget;
+    private TextView mSetupIntentValue;
+    private PaymentMethod mPaymentMethod;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_intent_demo);
+        final Button createSetupIntent = findViewById(R.id.btn_create_setup_intent);
+        mCreatePaymentMethod = findViewById(R.id.btn_create_payment_method);
+        mRetrieveSetupIntent = findViewById(R.id.btn_retrieve_setup_intent);
+        mConfirmSetupIntent = findViewById(R.id.btn_confirm_setup_intent);
+        mSetupIntentValue = findViewById(R.id.setup_intent_value);
+        mCardInputWidget = findViewById(R.id.card_input_widget);
+
+        mProgressDialogController = new ProgressDialogController(getSupportFragmentManager(),
+                getResources());
+        mErrorDialogHandler = new ErrorDialogHandler(this);
+        mStripe = new Stripe(getApplicationContext(),
+                PaymentConfiguration.getInstance().getPublishableKey());
+        final Retrofit retrofit = RetrofitFactory.getInstance();
+        mStripeService = retrofit.create(StripeService.class);
+
+        createSetupIntent.setOnClickListener(v -> createSetupIntent());
+        mCreatePaymentMethod.setOnClickListener(v -> {
+            final Card card = mCardInputWidget.getCard();
+            if (card != null) {
+                createPaymentMethod(card);
+            }
+        });
+        mRetrieveSetupIntent.setOnClickListener(v -> retrieveSetupIntent());
+        mConfirmSetupIntent.setOnClickListener(v -> {
+            confirmSetupIntent();
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        mCompositeDisposable.dispose();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+
+        if (intent.getData() != null && intent.getData().getQuery() != null) {
+            Toast.makeText(SetupIntentActivity.this,
+                    "Retrieving SetupIntent after authorizing",
+                    Toast.LENGTH_SHORT)
+                    .show();
+            mClientSecret = intent.getData().getQueryParameter(
+                    "setup_intent_client_secret");
+            retrieveSetupIntent();
+        }
+    }
+
+    void createSetupIntent() {
+        final Disposable disposable = mStripeService.createSetupIntent(new HashMap<>())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe((d) ->
+                        mProgressDialogController.show(R.string.creating_setup_intent))
+                .doOnComplete(() ->
+                        mProgressDialogController.dismiss())
+
+                // Because we've made the mapping above, we're now subscribing
+                // to the result of creating a 3DS Source
+                .subscribe(this::onCreatedSetupIntent,
+                        throwable -> mErrorDialogHandler.show(throwable.getLocalizedMessage())
+                );
+        mCompositeDisposable.add(disposable);
+    }
+
+    private void onCreatedSetupIntent(@NonNull ResponseBody responseBody) {
+        try {
+            final JSONObject jsonObject = new JSONObject(responseBody.string());
+            mSetupIntentValue.setText(jsonObject.toString());
+            mClientSecret = jsonObject.getString("secret");
+            mCreatePaymentMethod.setEnabled(mClientSecret != null);
+        } catch (IOException | JSONException exception) {
+            Log.e(TAG, exception.toString());
+        }
+    }
+
+    private void retrieveSetupIntent() {
+        final Observable<SetupIntent> setupIntentObservable = Observable.fromCallable(
+                () -> mStripe.retrieveSetupIntentSynchronous(
+                        SetupIntentParams.createRetrieveSetupIntentParams(mClientSecret)));
+
+        final Disposable disposable = setupIntentObservable
+                .subscribeOn(Schedulers.io())
+                .doOnSubscribe((d) ->
+                        mProgressDialogController.show(R.string.retrieving_setup_intent))
+                .doOnComplete(() -> mProgressDialogController.dismiss())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        setupIntent -> mSetupIntentValue.setText(setupIntent != null ?
+                                new JSONObject(setupIntent.toMap()).toString() :
+                                getString(R.string.error_while_retrieving_setup_intent)),
+                        throwable -> Log.e(TAG, throwable.toString())
+                );
+        mCompositeDisposable.add(disposable);
+    }
+
+    private void createPaymentMethod(@NonNull final Card card) {
+        final PaymentMethodCreateParams paymentMethodCreateParams =
+                PaymentMethodCreateParams.create(card.toPaymentMethodParamsCard(), null);
+
+        final Observable<PaymentMethod> paymentMethodObservable = Observable.fromCallable(
+                () -> mStripe.createPaymentMethodSynchronous(paymentMethodCreateParams,
+                        PaymentConfiguration.getInstance().getPublishableKey()));
+
+        final Disposable disposable = paymentMethodObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe((d) ->
+                        mProgressDialogController.show(R.string.creating_payment_method))
+                .doOnComplete(() ->
+                        mProgressDialogController.dismiss())
+                .subscribe(
+                        paymentMethod -> {
+                            if (paymentMethod != null) {
+                                mSetupIntentValue
+                                        .setText(paymentMethod.id);
+
+                                mPaymentMethod = paymentMethod;
+                                mConfirmSetupIntent.setEnabled(mPaymentMethod != null);
+                                mRetrieveSetupIntent.setEnabled(mPaymentMethod != null);
+                            }
+                        },
+                        throwable -> Log.e(TAG, throwable.toString())
+                );
+        mCompositeDisposable.add(disposable);
+    }
+
+    private void confirmSetupIntent() {
+
+        final Observable<SetupIntent> setupIntentObservable = Observable.fromCallable(
+                () -> {
+                    final SetupIntentParams setupIntentParams = SetupIntentParams
+                            .createConfirmSetupIntenParamsWithPaymentMethodId(
+                                    mPaymentMethod.id, mClientSecret, RETURN_URL);
+                    return mStripe.confirmSetupIntentSynchronous(setupIntentParams,
+                            PaymentConfiguration.getInstance().getPublishableKey());
+                });
+
+        final Disposable disposable = setupIntentObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe((d) ->
+                        mProgressDialogController.show(R.string.confirm_setup_intent))
+                .doOnComplete(() ->
+                        mProgressDialogController.dismiss())
+                .subscribe(
+                        setupIntent -> {
+                            if (setupIntent != null) {
+                                mSetupIntentValue
+                                        .setText(new JSONObject(setupIntent.toMap()).toString());
+
+                                if (setupIntent.requiresAction()) {
+                                    Toast.makeText(SetupIntentActivity.this,
+                                            "Redirecting to redirect URL",
+                                            Toast.LENGTH_SHORT)
+                                            .show();
+                                    startActivity(new Intent(Intent.ACTION_VIEW,
+                                            setupIntent.getRedirectUrl()));
+                                }
+                            }
+                        },
+                        throwable -> Log.e(TAG, throwable.toString())
+                );
+        mCompositeDisposable.add(disposable);
+    }
+}
