d12abcbcbd8cc07763d435d7bd67155244e307d0
Jason Emerick <jemerick-c@stripe.com>
2019-07-10 13:26:50 -0400

    update tests

--- stripe/src/test/java/com/stripe/android/model/Stripe3ds2FingerprintTest.java
+++ stripe/src/test/java/com/stripe/android/model/Stripe3ds2FingerprintTest.java
@@ -2,26 +2,18 @@ package com.stripe.android.model;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 @RunWith(RobolectricTestRunner.class)
 public class Stripe3ds2FingerprintTest {
 
     private static final String VISA_DS_JSON = "{\n" +
-            "\t\"type\": \"stripe_3ds2_fingerprint\",\n" +
-            "\t\"three_d_secure_2_source\": \"src_1EZl3YCRMbs6FrXfYgJXF46w\",\n" +
-            "\t\"directory_server_name\": \"visa\",\n" +
-            "\t\"server_transaction_id\": \"b31807ca-e7d6-4685-a7e3-53fbd2962135\",\n" +
-            "\t\"three_ds_method_url\": \"\"\n" +
-            "}";
-
-    private static final String VISA_DS_JSON_WITH_DSE = "{\n" +
             "\t\"type\": \"stripe_3ds2_fingerprint\",\n" +
             "\t\"three_d_secure_2_source\": \"src_1EZl3YCRMbs6FrXfYgJXF46w\",\n" +
             "\t\"directory_server_name\": \"visa\",\n" +
@@ -69,12 +61,12 @@ public class Stripe3ds2FingerprintTest {
                 stripe3ds2Fingerprint.directoryServer);
         assertEquals("e64bb72f-60ac-4845-b8b6-47cfdb0f73aa",
                 stripe3ds2Fingerprint.serverTransactionId);
-        assertNull(stripe3ds2Fingerprint.directoryServerEncryption);
+        assertNotNull(stripe3ds2Fingerprint.directoryServerEncryption);
     }
 
     @Test
     public void create_with3ds2SdkDataWithDSE_shouldCreateObject() {
-        final PaymentIntent.SdkData sdkData = PaymentIntentFixtures.PI_REQUIRES_VISA_3DS2_WITH_DSE
+        final PaymentIntent.SdkData sdkData = PaymentIntentFixtures.PI_REQUIRES_VISA_3DS2
                 .getStripeSdkData();
         assertNotNull(sdkData);
         final Stripe3ds2Fingerprint stripe3ds2Fingerprint = Stripe3ds2Fingerprint.create(sdkData);
@@ -102,22 +94,9 @@ public class Stripe3ds2FingerprintTest {
     }
 
     @Test
-    public void create_withVisaDsJson_shouldCreateObject() throws JSONException {
-        final Stripe3ds2Fingerprint stripe3ds2Fingerprint =
-                Stripe3ds2Fingerprint.create(new JSONObject(VISA_DS_JSON));
-        assertEquals("src_1EZl3YCRMbs6FrXfYgJXF46w",
-                stripe3ds2Fingerprint.source);
-        assertEquals(Stripe3ds2Fingerprint.DirectoryServer.Visa,
-                stripe3ds2Fingerprint.directoryServer);
-        assertEquals("b31807ca-e7d6-4685-a7e3-53fbd2962135",
-                stripe3ds2Fingerprint.serverTransactionId);
-        assertNull(stripe3ds2Fingerprint.directoryServerEncryption);
-    }
-
-    @Test
     public void create_withVisaDsJsonWithDSE_shouldCreateObject() throws JSONException {
         final Stripe3ds2Fingerprint stripe3ds2Fingerprint =
-                Stripe3ds2Fingerprint.create(new JSONObject(VISA_DS_JSON_WITH_DSE));
+                Stripe3ds2Fingerprint.create(new JSONObject(VISA_DS_JSON));
         assertEquals("src_1EZl3YCRMbs6FrXfYgJXF46w",
                 stripe3ds2Fingerprint.source);
         assertEquals(Stripe3ds2Fingerprint.DirectoryServer.Visa,
