b06160c1d56b2aa8499256e470d11d095ba0f7cf
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2019-08-05 12:51:18 -0400

    Enable stricter StrictMode in sample apps (#1301)

--- example/src/main/java/com/stripe/example/ExampleApplication.kt
+++ example/src/main/java/com/stripe/example/ExampleApplication.kt
@@ -5,20 +5,34 @@ import android.support.multidex.MultiDexApplication
 
 import com.facebook.stetho.Stetho
 import com.squareup.leakcanary.LeakCanary
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ExampleApplication : MultiDexApplication() {
 
     override fun onCreate() {
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectAll()
+                .penaltyLog()
+                .build())
+
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build())
+
         super.onCreate()
 
-        StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
-            .detectLeakedSqlLiteObjects()
-            .detectLeakedClosableObjects()
-            .penaltyLog()
-            .penaltyDeath()
-            .build())
-
-        Stetho.initializeWithDefaults(this)
+        CoroutineScope(Dispatchers.IO).launch {
+            Stetho.initializeWithDefaults(this@ExampleApplication)
+        }
 
         if (BuildConfig.DEBUG && LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
--- samplestore/src/main/java/com/stripe/samplestore/SampleStoreApplication.kt
+++ samplestore/src/main/java/com/stripe/samplestore/SampleStoreApplication.kt
@@ -1,16 +1,38 @@
 package com.stripe.samplestore
 
+import android.os.StrictMode
 import android.support.multidex.MultiDexApplication
 
 import com.facebook.stetho.Stetho
 import com.squareup.leakcanary.LeakCanary
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SampleStoreApplication : MultiDexApplication() {
 
     override fun onCreate() {
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectAll()
+                .penaltyLog()
+                .build())
+
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build())
+
         super.onCreate()
 
-        Stetho.initializeWithDefaults(this)
+        CoroutineScope(Dispatchers.IO).launch {
+            Stetho.initializeWithDefaults(this@SampleStoreApplication)
+        }
 
         if (BuildConfig.DEBUG && LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
