25b5e17555626d7d863eada4dd3ccf3195c4fe07
Michael Shafrir <mshafrir@stripe.com>
2019-09-13 13:18:23 -0400

    Add Connect Account Id support to GooglePayConfig
    ANDROID-411

--- stripe/src/main/java/com/stripe/android/GooglePayConfig.java
+++ stripe/src/main/java/com/stripe/android/GooglePayConfig.java
@@ -3,6 +3,9 @@ package com.stripe.android;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Locale;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,18 +14,32 @@ import org.json.JSONObject;
 public final class GooglePayConfig {
     @NonNull private final String mPublishableKey;
     @NonNull private final String mApiVersion;
+    @Nullable private final String mConnectedAccountId;
 
     /**
      * Instantiate with {@link PaymentConfiguration}. {@link PaymentConfiguration} must be
      * initialized.
      */
     public GooglePayConfig(@NonNull Context context) {
-        this(PaymentConfiguration.getInstance(context).getPublishableKey());
+        this(PaymentConfiguration.getInstance(context).getPublishableKey(), null);
+    }
+
+    /**
+     * Instantiate with {@link PaymentConfiguration} and Connect Account Id.
+     * {@link PaymentConfiguration} must be initialized.
+     */
+    public GooglePayConfig(@NonNull Context context, @Nullable String connectedAccountId) {
+        this(PaymentConfiguration.getInstance(context).getPublishableKey(), connectedAccountId);
     }
 
     public GooglePayConfig(@NonNull String publishableKey) {
+        this(publishableKey, null);
+    }
+
+    public GooglePayConfig(@NonNull String publishableKey, @Nullable String connectedAccountId) {
         mPublishableKey = ApiKeyValidator.get().requireValid(publishableKey);
         mApiVersion = ApiVersion.get().getCode();
+        mConnectedAccountId = connectedAccountId;
     }
 
     /**
@@ -39,7 +56,16 @@ public final class GooglePayConfig {
                         new JSONObject()
                                 .put("gateway", "stripe")
                                 .put("stripe:version", mApiVersion)
-                                .put("stripe:publishableKey", mPublishableKey)
+                                .put("stripe:publishableKey", getKey())
                 );
+    }
+
+    @NonNull
+    private String getKey() {
+        if (mConnectedAccountId != null) {
+            return String.format(Locale.ROOT, "%s/%s", mPublishableKey, mConnectedAccountId);
+        } else {
+            return mPublishableKey;
+        }
     }
 }
--- stripe/src/test/java/com/stripe/android/GooglePayConfigTest.java
+++ stripe/src/test/java/com/stripe/android/GooglePayConfigTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 public class GooglePayConfigTest {
 
     @Test
-    public void getTokenizationSpecification() throws JSONException {
+    public void getTokenizationSpecification_withoutConnectedAccount() throws JSONException {
         PaymentConfiguration.init(ApplicationProvider.getApplicationContext(),
                 ApiKeyFixtures.FAKE_PUBLISHABLE_KEY);
         final JSONObject tokenizationSpec =
@@ -26,6 +26,20 @@ public class GooglePayConfigTest {
         assertEquals(ApiVersion.get().getCode(),
                 params.getString("stripe:version"));
         assertEquals(ApiKeyFixtures.FAKE_PUBLISHABLE_KEY,
+                params.getString("stripe:publishableKey"));
+    }
+
+    @Test
+    public void getTokenizationSpecification_withConnectedAccount() throws JSONException {
+        final JSONObject tokenizationSpec =
+                new GooglePayConfig(ApiKeyFixtures.FAKE_PUBLISHABLE_KEY, "acct_1234")
+                        .getTokenizationSpecification();
+        final JSONObject params = tokenizationSpec.getJSONObject("parameters");
+        assertEquals("stripe",
+                params.getString("gateway"));
+        assertEquals(ApiVersion.get().getCode(),
+                params.getString("stripe:version"));
+        assertEquals("pk_test_123/acct_1234",
                 params.getString("stripe:publishableKey"));
     }
 }
