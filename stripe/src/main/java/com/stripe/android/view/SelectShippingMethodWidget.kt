b75627ca1626bf8350747a2da6ade72ead18a12d
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2019-09-17 13:08:21 -0400

    Convert SelectShippingMethodWidget to Kotlin (#1576)

---
+++ stripe/src/main/java/com/stripe/android/view/SelectShippingMethodWidget.kt
@@ -0,0 +1,46 @@
+package com.stripe.android.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.stripe.android.R
+import com.stripe.android.model.ShippingMethod
+import kotlinx.android.synthetic.main.select_shipping_method_widget.view.*
+
+/**
+ * A widget that allows the user to select a shipping method.
+ */
+internal class SelectShippingMethodWidget @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    internal val shippingMethodAdapter = ShippingMethodAdapter()
+
+    /**
+     * @return The [ShippingMethod] selected by the customer or `null` if no option is
+     * selected.
+     */
+    val selectedShippingMethod: ShippingMethod?
+        get() = shippingMethodAdapter.selectedShippingMethod
+
+    init {
+        View.inflate(context, R.layout.select_shipping_method_widget, this)
+        rv_shipping_methods_ssmw.setHasFixedSize(true)
+        rv_shipping_methods_ssmw.adapter = shippingMethodAdapter
+        rv_shipping_methods_ssmw.layoutManager = LinearLayoutManager(context)
+    }
+
+    /**
+     * Specify the shipping methods to show.
+     */
+    fun setShippingMethods(
+        shippingMethods: List<ShippingMethod>?,
+        defaultShippingMethod: ShippingMethod?
+    ) {
+        shippingMethodAdapter.setShippingMethods(shippingMethods, defaultShippingMethod)
+    }
+}
---
+++ stripe/src/test/java/com/stripe/android/view/SelectShippingMethodWidgetTest.kt
@@ -0,0 +1,53 @@
+package com.stripe.android.view
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.stripe.android.model.ShippingMethod
+import java.util.Locale
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Test for [SelectShippingMethodWidget]
+ */
+@RunWith(RobolectricTestRunner::class)
+class SelectShippingMethodWidgetTest {
+
+    private lateinit var shippingMethodAdapter: ShippingMethodAdapter
+
+    @Before
+    fun setup() {
+        Locale.setDefault(Locale.US)
+        val selectShippingMethodWidget =
+            SelectShippingMethodWidget(ApplicationProvider.getApplicationContext<Context>())
+        selectShippingMethodWidget.setShippingMethods(listOf(UPS, FEDEX), UPS)
+        shippingMethodAdapter = selectShippingMethodWidget.shippingMethodAdapter
+    }
+
+    @Test
+    fun selectShippingMethodWidget_whenSelected_selectionChanges() {
+        assertEquals(shippingMethodAdapter.selectedShippingMethod, UPS)
+        shippingMethodAdapter.onShippingMethodSelected(1)
+        assertEquals(shippingMethodAdapter.selectedShippingMethod, FEDEX)
+    }
+
+    companion object {
+        private val UPS = ShippingMethod(
+            "UPS Ground",
+            "ups-ground",
+            "Arrives in 3-5 days",
+            0,
+            "USD"
+        )
+        private val FEDEX = ShippingMethod(
+            "FedEx",
+            "fedex",
+            "Arrives tomorrow",
+            599,
+            "USD"
+        )
+    }
+}
