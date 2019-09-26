a8fa5ccf9ebb6a06a704cce055b53bd4b91f29b3
Michael Shafrir <mshafrir@stripe.com>
2019-09-26 13:24:13 -0400

    Convert CardUtils to Kotlin

---
+++ stripe/src/main/java/com/stripe/android/CardUtils.kt
@@ -0,0 +1,152 @@
+package com.stripe.android
+
+import com.stripe.android.model.Card
+import com.stripe.android.model.Card.CardBrand
+
+/**
+ * Utility class for functions to do with cards.
+ */
+object CardUtils {
+
+    private const val LENGTH_COMMON_CARD = 16
+    private const val LENGTH_AMERICAN_EXPRESS = 15
+    private const val LENGTH_DINERS_CLUB = 14
+
+    /**
+     * Returns a [CardBrand] corresponding to a partial card number,
+     * or [Card.CardBrand.UNKNOWN] if the card brand can't be determined from the input value.
+     *
+     * @param cardNumber a credit card number or partial card number
+     * @return the [Card.CardBrand] corresponding to that number,
+     * or [CardBrand.UNKNOWN] if it can't be determined
+     */
+    @JvmStatic
+    @Card.CardBrand
+    fun getPossibleCardType(cardNumber: String?): String {
+        return getPossibleCardType(cardNumber, true)
+    }
+
+    /**
+     * Checks the input string to see whether or not it is a valid card number, possibly
+     * with groupings separated by spaces or hyphens.
+     *
+     * @param cardNumber a String that may or may not represent a valid card number
+     * @return `true` if and only if the input value is a valid card number
+     */
+    @JvmStatic
+    fun isValidCardNumber(cardNumber: String?): Boolean {
+        val normalizedNumber = StripeTextUtils.removeSpacesAndHyphens(cardNumber)
+        return isValidLuhnNumber(normalizedNumber) && isValidCardLength(normalizedNumber)
+    }
+
+    /**
+     * Checks the input string to see whether or not it is a valid Luhn number.
+     *
+     * @param cardNumber a String that may or may not represent a valid Luhn number
+     * @return `true` if and only if the input value is a valid Luhn number
+     */
+    internal fun isValidLuhnNumber(cardNumber: String?): Boolean {
+        if (cardNumber == null) {
+            return false
+        }
+
+        var isOdd = true
+        var sum = 0
+
+        for (index in cardNumber.length - 1 downTo 0) {
+            val c = cardNumber[index]
+            if (!Character.isDigit(c)) {
+                return false
+            }
+
+            var digitInteger = Character.getNumericValue(c)
+            isOdd = !isOdd
+
+            if (isOdd) {
+                digitInteger *= 2
+            }
+
+            if (digitInteger > 9) {
+                digitInteger -= 9
+            }
+
+            sum += digitInteger
+        }
+
+        return sum % 10 == 0
+    }
+
+    /**
+     * Checks to see whether the input number is of the correct length, after determining its brand.
+     * This function does not perform a Luhn check.
+     *
+     * @param cardNumber the card number with no spaces or dashes
+     * @return `true` if the card number is of known type and the correct length
+     */
+    internal fun isValidCardLength(cardNumber: String?): Boolean {
+        return cardNumber != null && isValidCardLength(cardNumber,
+            getPossibleCardType(cardNumber, false))
+    }
+
+    /**
+     * Checks to see whether the input number is of the correct length, given the assumed brand of
+     * the card. This function does not perform a Luhn check.
+     *
+     * @param cardNumber the card number with no spaces or dashes
+     * @param cardBrand a [Card.CardBrand] used to get the correct size
+     * @return `true` if the card number is the correct length for the assumed brand
+     */
+    internal fun isValidCardLength(
+        cardNumber: String?,
+        @CardBrand cardBrand: String
+    ): Boolean {
+        if (cardNumber == null || CardBrand.UNKNOWN == cardBrand) {
+            return false
+        }
+
+        val length = cardNumber.length
+        return when (cardBrand) {
+            CardBrand.AMERICAN_EXPRESS -> {
+                length == LENGTH_AMERICAN_EXPRESS
+            }
+            CardBrand.DINERS_CLUB -> {
+                length == LENGTH_DINERS_CLUB
+            }
+            else -> {
+                length == LENGTH_COMMON_CARD
+            }
+        }
+    }
+
+    @CardBrand
+    private fun getPossibleCardType(cardNumber: String?, shouldNormalize: Boolean): String {
+        if (StripeTextUtils.isBlank(cardNumber)) {
+            return CardBrand.UNKNOWN
+        }
+
+        val spacelessCardNumber =
+            if (shouldNormalize) {
+                StripeTextUtils.removeSpacesAndHyphens(cardNumber)
+            } else {
+                cardNumber
+            }
+
+        return when {
+            StripeTextUtils.hasAnyPrefix(spacelessCardNumber, *Card.PREFIXES_AMERICAN_EXPRESS) ->
+                CardBrand.AMERICAN_EXPRESS
+            StripeTextUtils.hasAnyPrefix(spacelessCardNumber, *Card.PREFIXES_DISCOVER) ->
+                CardBrand.DISCOVER
+            StripeTextUtils.hasAnyPrefix(spacelessCardNumber, *Card.PREFIXES_JCB) ->
+                CardBrand.JCB
+            StripeTextUtils.hasAnyPrefix(spacelessCardNumber, *Card.PREFIXES_DINERS_CLUB) ->
+                CardBrand.DINERS_CLUB
+            StripeTextUtils.hasAnyPrefix(spacelessCardNumber, *Card.PREFIXES_VISA) ->
+                CardBrand.VISA
+            StripeTextUtils.hasAnyPrefix(spacelessCardNumber, *Card.PREFIXES_MASTERCARD) ->
+                CardBrand.MASTERCARD
+            StripeTextUtils.hasAnyPrefix(spacelessCardNumber, *Card.PREFIXES_UNIONPAY) ->
+                CardBrand.UNIONPAY
+            else -> CardBrand.UNKNOWN
+        }
+    }
+}
---
+++ stripe/src/test/java/com/stripe/android/CardUtilsTest.kt
@@ -0,0 +1,199 @@
+package com.stripe.android
+
+import com.stripe.android.model.Card
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Test class for [CardUtils].
+ */
+class CardUtilsTest {
+
+    @Test
+    fun getPossibleCardType_withEmptyCard_returnsUnknown() {
+        assertEquals(Card.CardBrand.UNKNOWN, CardUtils.getPossibleCardType("   "))
+    }
+
+    @Test
+    fun getPossibleCardType_withNullCardNumber_returnsUnknown() {
+        assertEquals(Card.CardBrand.UNKNOWN, CardUtils.getPossibleCardType(null))
+    }
+
+    @Test
+    fun getPossibleCardType_withVisaPrefix_returnsVisa() {
+        assertEquals(Card.CardBrand.VISA, CardUtils.getPossibleCardType("4899 99"))
+        assertEquals(Card.CardBrand.VISA, CardUtils.getPossibleCardType("4"))
+    }
+
+    @Test
+    fun getPossibleCardType_withAmexPrefix_returnsAmex() {
+        assertEquals(Card.CardBrand.AMERICAN_EXPRESS, CardUtils.getPossibleCardType("345"))
+        assertEquals(Card.CardBrand.AMERICAN_EXPRESS, CardUtils.getPossibleCardType("37999999999"))
+    }
+
+    @Test
+    fun getPossibleCardType_withJCBPrefix_returnsJCB() {
+        assertEquals(Card.CardBrand.JCB, CardUtils.getPossibleCardType("3535 3535"))
+    }
+
+    @Test
+    fun getPossibleCardType_withMasterCardPrefix_returnsMasterCard() {
+        assertEquals(Card.CardBrand.MASTERCARD, CardUtils.getPossibleCardType("2222 452"))
+        assertEquals(Card.CardBrand.MASTERCARD, CardUtils.getPossibleCardType("5050"))
+    }
+
+    @Test
+    fun getPossibleCardType_withDinersClubPrefix_returnsDinersClub() {
+        assertEquals(Card.CardBrand.DINERS_CLUB, CardUtils.getPossibleCardType("303922 2234"))
+        assertEquals(Card.CardBrand.DINERS_CLUB, CardUtils.getPossibleCardType("36778 9098"))
+    }
+
+    @Test
+    fun getPossibleCardType_withDiscoverPrefix_returnsDiscover() {
+        assertEquals(Card.CardBrand.DISCOVER, CardUtils.getPossibleCardType("60355"))
+        assertEquals(Card.CardBrand.DISCOVER, CardUtils.getPossibleCardType("6433 8 90923"))
+        // This one has too many numbers on purpose. Checking for length is not part of the
+        // function under test.
+        assertEquals(Card.CardBrand.DISCOVER, CardUtils.getPossibleCardType("6523452309209340293423"))
+    }
+
+    @Test
+    fun getPossibleCardType_withUnionPayPrefix_returnsUnionPay() {
+        assertEquals(Card.CardBrand.UNIONPAY, CardUtils.getPossibleCardType("62"))
+    }
+
+    @Test
+    fun getPossibleCardType_withNonsenseNumber_returnsUnknown() {
+        assertEquals(Card.CardBrand.UNKNOWN, CardUtils.getPossibleCardType("1234567890123456"))
+        assertEquals(Card.CardBrand.UNKNOWN, CardUtils.getPossibleCardType("9999 9999 9999 9999"))
+        assertEquals(Card.CardBrand.UNKNOWN, CardUtils.getPossibleCardType("3"))
+    }
+
+    @Test
+    fun isValidCardLength_whenValidVisaNumber_returnsTrue() {
+        assertTrue(CardUtils.isValidCardLength("4242424242424242"))
+    }
+
+    @Test
+    fun isValidCardLength_whenValidJCBNumber_returnsTrue() {
+        assertTrue(CardUtils.isValidCardLength("3530111333300000"))
+    }
+
+    @Test
+    fun isValidCardLength_whenValidDiscover_returnsTrue() {
+        assertTrue(CardUtils.isValidCardLength("6011000990139424"))
+    }
+
+    @Test
+    fun isValidCardLength_whenValidDinersClub_returnsTrue() {
+        assertTrue(CardUtils.isValidCardLength("30569309025904"))
+    }
+
+    @Test
+    fun isValidCardLength_whenValidMasterCard_returnsTrue() {
+        assertTrue(CardUtils.isValidCardLength("5555555555554444"))
+    }
+
+    @Test
+    fun isValidCardLength_whenValidAmEx_returnsTrue() {
+        assertTrue(CardUtils.isValidCardLength("378282246310005"))
+    }
+
+    @Test
+    fun isValidCardLength_whenValidUnionPay_returnsTrue() {
+        assertTrue(CardUtils.isValidCardLength("6200000000000005"))
+    }
+
+    @Test
+    fun isValidCardLength_whenNull_returnsFalse() {
+        assertFalse(CardUtils.isValidCardLength(null))
+    }
+
+    @Test
+    fun isValidCardLength_whenVisaStyleNumberButDinersClubLength_returnsFalse() {
+        assertFalse(CardUtils.isValidCardLength("42424242424242"))
+    }
+
+    @Test
+    fun isValidCardLength_whenVisaStyleNumberButAmExLength_returnsFalse() {
+        assertFalse(CardUtils.isValidCardLength("424242424242424"))
+    }
+
+    @Test
+    fun isValidCardLength_whenAmExStyleNumberButVisaLength_returnsFalse() {
+        assertFalse(CardUtils.isValidCardLength("3782822463100050"))
+    }
+
+    @Test
+    fun isValidCardLength_whenAmExStyleNumberButDinersClubLength_returnsFalse() {
+        assertFalse(CardUtils.isValidCardLength("37828224631000"))
+    }
+
+    @Test
+    fun isValidCardLength_whenDinersClubStyleNumberButVisaLength_returnsFalse() {
+        assertFalse(CardUtils.isValidCardLength("3056930902590400"))
+    }
+
+    @Test
+    fun isValidCardLength_whenDinersClubStyleNumberStyleNumberButAmexLength_returnsFalse() {
+        assertFalse(CardUtils.isValidCardLength("305693090259040"))
+    }
+
+    @Test
+    fun isValidCardLengthWithBrand_whenBrandUnknown_alwaysReturnsFalse() {
+        val validVisa = "4242424242424242"
+        // Adding this check to ensure the input number is correct
+        assertTrue(CardUtils.isValidCardLength(validVisa))
+        assertFalse(CardUtils.isValidCardLength(validVisa, Card.CardBrand.UNKNOWN))
+    }
+
+    @Test
+    fun isValidLuhnNumber_whenValidVisaNumber_returnsTrue() {
+        assertTrue(CardUtils.isValidLuhnNumber("4242424242424242"))
+    }
+
+    @Test
+    fun isValidLuhnNumber_whenValidJCBNumber_returnsTrue() {
+        assertTrue(CardUtils.isValidLuhnNumber("3530111333300000"))
+    }
+
+    @Test
+    fun isValidLuhnNumber_whenValidDiscover_returnsTrue() {
+        assertTrue(CardUtils.isValidLuhnNumber("6011000990139424"))
+    }
+
+    @Test
+    fun isValidLuhnNumber_whenValidDinersClub_returnsTrue() {
+        assertTrue(CardUtils.isValidLuhnNumber("30569309025904"))
+    }
+
+    @Test
+    fun isValidLuhnNumber_whenValidMasterCard_returnsTrue() {
+        assertTrue(CardUtils.isValidLuhnNumber("5555555555554444"))
+    }
+
+    @Test
+    fun isValidLuhnNumber_whenValidAmEx_returnsTrue() {
+        assertTrue(CardUtils.isValidLuhnNumber("378282246310005"))
+    }
+
+    @Test
+    fun isValidLunhNumber_whenNumberIsInvalid_returnsFalse() {
+        assertFalse(CardUtils.isValidLuhnNumber("4242424242424243"))
+    }
+
+    @Test
+    fun isValidLuhnNumber_whenInputIsNull_returnsFalse() {
+        assertFalse(CardUtils.isValidLuhnNumber(null))
+    }
+
+    @Test
+    fun isValidLuhnNumber_whenInputIsNotNumeric_returnsFalse() {
+        assertFalse(CardUtils.isValidLuhnNumber("abcdefg"))
+        // Note: it is not the job of this function to de-space the card number, nor de-hyphen it
+        assertFalse(CardUtils.isValidLuhnNumber("4242 4242 4242 4242"))
+        assertFalse(CardUtils.isValidLuhnNumber("4242-4242-4242-4242"))
+    }
+}
