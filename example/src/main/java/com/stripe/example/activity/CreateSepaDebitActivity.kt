7a4a7837643b05d5973d94d90b2d9b26c33154e8
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2019-10-11 16:46:51 -0400

    Create SEPA Debit example (#1695)

---
+++ example/src/main/java/com/stripe/example/activity/CreateSepaDebitActivity.kt
@@ -0,0 +1,66 @@
+package com.stripe.example.activity
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.stripe.android.ApiResultCallback
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.Stripe
+import com.stripe.android.model.PaymentMethod
+import com.stripe.android.model.PaymentMethodCreateParams
+import com.stripe.example.R
+
+class CreateSepaDebitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_sepa_debit_pm_layout)
+        setTitle(R.string.launch_create_pm_sepa_debit)
+
+        val stripe = Stripe(this,
+            PaymentConfiguration.getInstance(this).publishableKey)
+
+        val progressBar: ProgressBar = findViewById(R.id.progress_bar)
+        val ibanInput: EditText = findViewById(R.id.iban_input)
+        val button: Button = findViewById(R.id.create_sepa_debit_button)
+        button.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+
+            stripe.createPaymentMethod(
+                PaymentMethodCreateParams.create(
+                    PaymentMethodCreateParams.SepaDebit.Builder()
+                        .setIban(ibanInput.text.toString())
+                        .build(),
+                    PaymentMethod.BillingDetails.Builder()
+                        .setName(CUSTOMER_NAME)
+                        .setEmail(CUSTOMER_EMAIl)
+                        .build()
+                ),
+                object : ApiResultCallback<PaymentMethod> {
+                    override fun onSuccess(result: PaymentMethod) {
+                        progressBar.visibility = View.INVISIBLE
+                        Toast.makeText(
+                            this@CreateSepaDebitActivity,
+                            "Created payment method: " + result.id,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onError(e: Exception) {
+                        progressBar.visibility = View.INVISIBLE
+                        e.printStackTrace()
+                    }
+                }
+            )
+        }
+    }
+
+    companion object {
+        private const val CUSTOMER_NAME = "Jenny Rosen"
+        private const val CUSTOMER_EMAIl = "jrosen@example.com"
+    }
+}
