ffa565b73a74317a1ebeb040f0603ddaa56b4fdf
Michael Shafrir <mshafrir@stripe.com>
2019-11-12 18:00:43 -0500

    Fix test failure

--- stripe/src/test/java/com/stripe/android/view/PaymentMethodsActivityTest.kt
+++ stripe/src/test/java/com/stripe/android/view/PaymentMethodsActivityTest.kt
@@ -1,5 +1,6 @@
 package com.stripe.android.view
 
+import android.app.Activity.RESULT_CANCELED
 import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
@@ -188,8 +189,7 @@ class PaymentMethodsActivityTest : BaseViewTest<PaymentMethodsActivity>(PaymentM
         listenerArgumentCaptor.firstValue
             .onPaymentMethodsRetrieved(PaymentMethodFixtures.CARD_PAYMENT_METHODS)
         val paymentMethodsAdapter =
-            recyclerView.adapter as PaymentMethodsAdapter?
-        assertNotNull(paymentMethodsAdapter)
+            recyclerView.adapter as PaymentMethodsAdapter
         paymentMethodsAdapter.selectedPaymentMethodId =
             PaymentMethodFixtures.CARD_PAYMENT_METHODS[0].id
 
@@ -198,12 +198,12 @@ class PaymentMethodsActivityTest : BaseViewTest<PaymentMethodsActivity>(PaymentM
         // Now it should be gone.
         assertEquals(View.GONE, progressBar.visibility)
         assertTrue(paymentMethodsActivity.isFinishing)
-        assertEquals(RESULT_OK, shadowActivity.resultCode)
-        val intent = shadowActivity.resultIntent
-        assertNotNull(intent)
+
+        // `resultCode` is `RESULT_CANCELED` because back was pressed
+        assertEquals(RESULT_CANCELED, shadowActivity.resultCode)
 
         val result =
-            PaymentMethodsActivityStarter.Result.fromIntent(intent)
+            PaymentMethodsActivityStarter.Result.fromIntent(shadowActivity.resultIntent)
         assertEquals(PaymentMethodFixtures.CARD_PAYMENT_METHODS[0], result?.paymentMethod)
     }
 }
