4b9689d3f488dcd29bc504d142064a8627015040
Michael Shafrir <mshafrir@stripe.com>
2019-11-25 15:44:06 -0500

    Simplify CreateCardTokenActivity
    - Convert to `RecyclerView`
    - Use asynchronous `Stripe#createCardToken()` method
    - Use ViewModel to manage Activity lifecycle
    - Delete helper "Controller" classes as they are no longer needed
    - Update `CreateCardTokenActivityTest`

--- example/src/androidTestDebug/java/com/stripe/example/activity/CreateCardTokenActivityTest.kt
+++ example/src/androidTestDebug/java/com/stripe/example/activity/CreateCardTokenActivityTest.kt
@@ -1,22 +1,17 @@
 package com.stripe.example.activity
 
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.stripe.example.R
-import org.hamcrest.CoreMatchers.anything
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -36,7 +31,7 @@ class CreateCardTokenActivityTest {
     }
 
     @After
-    fun intentsTeardown() {
+    fun cleanup() {
         Intents.release()
     }
 
@@ -56,15 +51,14 @@ class CreateCardTokenActivityTest {
             .perform(typeText("111"))
 
         // click create card button
-        onView(withId(R.id.create_token_rxjava))
+        onView(withId(R.id.create_token_button))
             .perform(click())
 
         // don't use Thread.sleep in Espresso tests - figure out how to use IdlingResource
         Thread.sleep(2000)
 
         // check that card token info has been added to the tokens list
-        onData(anything()).inAdapterView(withId(R.id.tokens_list))
-            .atPosition(0)
-            .check(matches(hasDescendant(withText("Card ending in 4242"))))
+        onView(withId(R.id.tokens_list))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
     }
 }
--- example/src/main/java/com/stripe/example/activity/CreateCardTokenActivity.kt
+++ example/src/main/java/com/stripe/example/activity/CreateCardTokenActivity.kt
@@ -1,32 +1,134 @@
 package com.stripe.example.activity
 
+import android.app.Activity
+import android.app.Application
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.stripe.android.ApiResultCallback
 import com.stripe.android.PaymentConfiguration
+import com.stripe.android.Stripe
+import com.stripe.android.model.Card
+import com.stripe.android.model.Token
 import com.stripe.example.R
-import com.stripe.example.module.DependencyHandler
+import kotlinx.android.synthetic.main.card_token_activity.*
 
 class CreateCardTokenActivity : AppCompatActivity() {
-
-    private lateinit var dependencyHandler: DependencyHandler
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.card_token_activity)
 
-        dependencyHandler = DependencyHandler(
-            this,
-            findViewById(R.id.card_input_widget),
-            findViewById(R.id.tokens_list),
-            PaymentConfiguration.getInstance(this).publishableKey
-        )
+        val adapter = Adapter(this)
+        tokens_list.adapter = adapter
+        tokens_list.layoutManager = LinearLayoutManager(this)
+            .apply {
+                orientation = LinearLayoutManager.VERTICAL
+            }
 
-        dependencyHandler.attachAsyncTaskTokenController(findViewById(R.id.create_token_asynctask))
-        dependencyHandler.attachRxTokenController(findViewById(R.id.create_token_rxjava))
+        val viewModel = ViewModelProviders.of(
+            this
+        )[CreateCardTokenViewModel::class.java]
+
+        create_token_button.setOnClickListener {
+            val card = card_input_widget.card
+
+            if (card != null) {
+                onRequestStart()
+                viewModel.createCardToken(card).observe(this, Observer {
+                    onRequestEnd()
+                    adapter.addToken(it)
+                })
+            } else {
+                Snackbar.make(
+                    findViewById(android.R.id.content),
+                    R.string.invalid_card_details,
+                    Snackbar.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
     }
 
-    override fun onDestroy() {
-        dependencyHandler.clearReferences()
-        super.onDestroy()
+    private fun onRequestStart() {
+        progress_bar.visibility = View.VISIBLE
+        create_token_button.isEnabled = false
+    }
+
+    private fun onRequestEnd() {
+        progress_bar.visibility = View.INVISIBLE
+        create_token_button.isEnabled = true
+    }
+
+    internal class Adapter(
+        private val activity: Activity
+    ) : RecyclerView.Adapter<Adapter.TokenViewHolder>() {
+        private val tokens: MutableList<Token> = mutableListOf()
+
+        override fun getItemCount(): Int {
+            return tokens.size
+        }
+
+        override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): TokenViewHolder {
+            val root = activity.layoutInflater
+                .inflate(R.layout.token_item, viewGroup, false)
+            return TokenViewHolder(root)
+        }
+
+        override fun onBindViewHolder(holder: TokenViewHolder, position: Int) {
+            holder.update(tokens[position])
+        }
+
+        internal fun addToken(token: Token) {
+            tokens.add(0, token)
+            notifyItemInserted(0)
+        }
+
+        internal class TokenViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val last4TextView: TextView = itemView.findViewById(R.id.last4)
+            private val tokenIdTextView: TextView = itemView.findViewById(R.id.tokenId)
+
+            fun update(token: Token) {
+                last4TextView.text = token.card?.last4
+                tokenIdTextView.text = token.id
+            }
+        }
+    }
+
+    internal class CreateCardTokenViewModel(
+        application: Application
+    ) : AndroidViewModel(application) {
+        private val context = application.applicationContext
+        private val stripe = Stripe(
+            context,
+            PaymentConfiguration.getInstance(context).publishableKey
+        )
+
+        fun createCardToken(card: Card): LiveData<Token> {
+            val data = MutableLiveData<Token>()
+
+            stripe.createCardToken(card, callback = object : ApiResultCallback<Token> {
+                override fun onSuccess(result: Token) {
+                    data.value = result
+                }
+
+                override fun onError(e: Exception) {
+                    Log.e("StripeExample", "Error while creating card token", e)
+                }
+            })
+
+            return data
+        }
     }
 }
