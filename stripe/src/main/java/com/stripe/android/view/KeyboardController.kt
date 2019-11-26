a8af3ee195278c2bacdc0415645c23bb5817f881
Michael Shafrir <mshafrir@stripe.com>
2019-11-26 10:17:34 -0500

    Create KeyboardController
    Used to hide virtual keyboard

---
+++ stripe/src/main/java/com/stripe/android/view/KeyboardController.kt
@@ -0,0 +1,24 @@
+package com.stripe.android.view
+
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+
+internal class KeyboardController(
+    private val activity: Activity
+) {
+    private val inputMethodManager: InputMethodManager =
+        activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    /**
+     * Hide virtual keyboard
+     */
+    @JvmSynthetic
+    internal fun hide() {
+        if (inputMethodManager.isAcceptingText) {
+            inputMethodManager.hideSoftInputFromWindow(
+                activity.currentFocus?.windowToken, 0
+            )
+        }
+    }
+}
