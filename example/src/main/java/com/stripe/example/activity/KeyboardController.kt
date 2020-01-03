ee8130b8c405edc57667a917fafa071f87066268
Michael Shafrir <mshafrir@stripe.com>
2020-01-03 12:18:36 -0500

    Improve CreateCardSourceActivity example
    - Use Stripe#authenticateSource() instead of manually handling
      browser redirect
    - Use `SourceViewModel` to create a Source
    - Simplify `SourcesAdapter` to use actual `Source` objects as
      backing data
    - Remove `<intent-filter>` rules in `AndroidManifest.xml` for
      `CreateCardSourceActivity`

---
+++ example/src/main/java/com/stripe/example/activity/KeyboardController.kt
@@ -0,0 +1,23 @@
+package com.stripe.example.activity
+
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+
+internal class KeyboardController(
+    private val activity: Activity
+) {
+    private val inputMethodManager: InputMethodManager =
+        activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    /**
+     * Hide virtual keyboard
+     */
+    internal fun hide() {
+        if (inputMethodManager.isAcceptingText) {
+            inputMethodManager.hideSoftInputFromWindow(
+                activity.currentFocus?.windowToken, 0
+            )
+        }
+    }
+}
