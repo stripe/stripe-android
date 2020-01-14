b6270f446946bf591422c62cacd634a5b804c2ec
Michael Shafrir <mshafrir@stripe.com>
2020-01-14 16:42:21 -0500

    Fix lint failure in SnackbarController

--- example/src/main/java/com/stripe/example/activity/SnackbarController.kt
+++ example/src/main/java/com/stripe/example/activity/SnackbarController.kt
@@ -1,12 +1,13 @@
 package com.stripe.example.activity
 
+import android.annotation.SuppressLint
 import android.view.View
-import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
 internal class SnackbarController internal constructor(val view: View) {
+    @SuppressLint("WrongConstant)")
     fun show(message: String) {
-        Snackbar.make(view, message, BaseTransientBottomBar.LENGTH_SHORT)
+        Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
             .show()
     }
 }
