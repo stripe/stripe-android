6b2c094cfb3487fbec795cb4135a5886a4885521
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-01-16 15:41:38 -0500

    Make AddPaymentMethodRowView a concrete class (#2072)
    Summary
    - Consolidate layouts to a single `add_payment_method_row`
      layout.
    - Also fix bug introduced by enabling `focusableInTouchMode`
    
    Testing
    Testing with TalkBack on for both cards and FPX

--- stripe/src/main/java/com/stripe/android/view/AddPaymentMethodRowView.kt
+++ stripe/src/main/java/com/stripe/android/view/AddPaymentMethodRowView.kt
@@ -1,26 +1,81 @@
 package com.stripe.android.view
 
 import android.app.Activity
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.annotation.IdRes
-import androidx.annotation.LayoutRes
+import androidx.annotation.StringRes
+import com.stripe.android.R
+import com.stripe.android.model.PaymentMethod
+import kotlinx.android.synthetic.main.add_payment_method_row.view.*
 
-internal abstract class AddPaymentMethodRowView(
-    activity: Activity,
-    @LayoutRes layoutId: Int,
+@SuppressWarnings("ViewConstructor")
+internal class AddPaymentMethodRowView private constructor(
+    context: Context,
     @IdRes idRes: Int,
-    args: AddPaymentMethodActivityStarter.Args
-) : FrameLayout(activity) {
+    @StringRes private val labelRes: Int,
+    private val args: AddPaymentMethodActivityStarter.Args
+) : FrameLayout(context) {
+
     init {
-        View.inflate(activity, layoutId, this)
+        View.inflate(context, R.layout.add_payment_method_row, this)
         id = idRes
-        setOnClickListener { AddPaymentMethodActivityStarter(activity).startForResult(args) }
         layoutParams = ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
 
         isFocusable = true
         isClickable = true
-        isFocusableInTouchMode = true
+
+        contentDescription = context.getString(labelRes)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        label.setText(labelRes)
+
+        (context as? Activity)?.let { activity ->
+            setOnClickListener {
+                AddPaymentMethodActivityStarter(activity).startForResult(args)
+            }
+        }
+    }
+
+    internal companion object {
+        internal fun createCard(
+            activity: Activity,
+            args: PaymentMethodsActivityStarter.Args
+        ): AddPaymentMethodRowView {
+            return AddPaymentMethodRowView(
+                activity,
+                R.id.stripe_payment_methods_add_card,
+                R.string.payment_method_add_new_card,
+                AddPaymentMethodActivityStarter.Args.Builder()
+                    .setBillingAddressFields(args.billingAddressFields)
+                    .setShouldAttachToCustomer(true)
+                    .setIsPaymentSessionActive(args.isPaymentSessionActive)
+                    .setPaymentMethodType(PaymentMethod.Type.Card)
+                    .setAddPaymentMethodFooter(args.addPaymentMethodFooterLayoutId)
+                    .setPaymentConfiguration(args.paymentConfiguration)
+                    .setWindowFlags(args.windowFlags)
+                    .build()
+            )
+        }
+
+        internal fun createFpx(
+            activity: Activity,
+            args: PaymentMethodsActivityStarter.Args
+        ): AddPaymentMethodRowView {
+            return AddPaymentMethodRowView(
+                activity,
+                R.id.stripe_payment_methods_add_fpx,
+                R.string.payment_method_add_new_fpx,
+                AddPaymentMethodActivityStarter.Args.Builder()
+                    .setIsPaymentSessionActive(args.isPaymentSessionActive)
+                    .setPaymentMethodType(PaymentMethod.Type.Fpx)
+                    .setPaymentConfiguration(args.paymentConfiguration)
+                    .build()
+            )
+        }
     }
 }
