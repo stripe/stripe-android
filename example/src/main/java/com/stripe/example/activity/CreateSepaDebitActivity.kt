a72fe602df9b1e445309e961f8e8744ac46a0366
Michael Shafrir <mshafrir@stripe.com>
2020-01-27 12:20:44 -0500

    Improve SEPA Debit example

--- example/src/main/java/com/stripe/example/activity/CreateSepaDebitActivity.kt
+++ example/src/main/java/com/stripe/example/activity/CreateSepaDebitActivity.kt
@@ -1,39 +1,81 @@
 package com.stripe.example.activity
 
+import android.app.Application
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.stripe.android.ApiResultCallback
 import com.stripe.android.PaymentConfiguration
 import com.stripe.android.Stripe
 import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.PaymentMethodCreateParams
 import com.stripe.example.R
+import kotlinx.android.synthetic.main.create_sepa_debit_pm_layout.*
 
+/**
+ * See [SEPA Direct Debit payments](https://stripe.com/docs/payments/sepa-debit) for more
+ * details.
+ */
 class CreateSepaDebitActivity : AppCompatActivity() {
+    private val viewModel: SepaDebitViewModel by lazy {
+        ViewModelProviders.of(
+            this,
+            SepaDebitViewModel.Factory(application)
+        )[SepaDebitViewModel::class.java]
+    }
+
+    private val snackbarController: SnackbarController by lazy {
+        SnackbarController(findViewById(android.R.id.content))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_sepa_debit_pm_layout)
         setTitle(R.string.launch_create_pm_sepa_debit)
 
-        val stripe = Stripe(this,
-            PaymentConfiguration.getInstance(this).publishableKey)
+        iban_input.setText(TEST_ACCOUNT_NUMBER)
 
-        val progressBar: ProgressBar = findViewById(R.id.progress_bar)
-        val ibanInput: EditText = findViewById(R.id.iban_input)
-        val button: Button = findViewById(R.id.create_sepa_debit_button)
-        button.setOnClickListener {
-            progressBar.visibility = View.VISIBLE
+        create_sepa_debit_button.setOnClickListener {
+            progress_bar.visibility = View.VISIBLE
 
+            viewModel.createPaymentMethod(iban_input.text.toString())
+                .observe(this, Observer {
+                    when (it) {
+                        is SepaDebitViewModel.Result.Success -> {
+                            val paymentMethod = it.paymentMethod
+                            progress_bar.visibility = View.INVISIBLE
+                            snackbarController.show("Created payment method: ${paymentMethod.id}")
+                        }
+                        is SepaDebitViewModel.Result.Error -> {
+                            val exception = it.exception
+                            progress_bar.visibility = View.INVISIBLE
+                            snackbarController.show("Created payment method: ${exception.message}")
+                        }
+                    }
+                })
+        }
+    }
+
+    internal class SepaDebitViewModel(
+        application: Application
+    ) : AndroidViewModel(application) {
+        private val context = application.applicationContext
+        private val stripe: Stripe =
+            Stripe(context, PaymentConfiguration.getInstance(context).publishableKey)
+
+        fun createPaymentMethod(accountNumber: String): LiveData<Result> {
+            val resultData = MutableLiveData<Result>()
             stripe.createPaymentMethod(
                 PaymentMethodCreateParams.create(
                     PaymentMethodCreateParams.SepaDebit(
-                        iban = ibanInput.text.toString()
+                        iban = accountNumber
                     ),
                     PaymentMethod.BillingDetails(
                         name = CUSTOMER_NAME,
@@ -42,25 +84,36 @@ class CreateSepaDebitActivity : AppCompatActivity() {
                 ),
                 callback = object : ApiResultCallback<PaymentMethod> {
                     override fun onSuccess(result: PaymentMethod) {
-                        progressBar.visibility = View.INVISIBLE
-                        Toast.makeText(
-                            this@CreateSepaDebitActivity,
-                            "Created payment method: " + result.id,
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        resultData.value = Result.Success(result)
                     }
 
                     override fun onError(e: Exception) {
-                        progressBar.visibility = View.INVISIBLE
-                        e.printStackTrace()
+                        resultData.value = Result.Error(e)
                     }
                 }
             )
+
+            return resultData
+        }
+
+        internal sealed class Result {
+            data class Success(val paymentMethod: PaymentMethod) : Result()
+            data class Error(val exception: Exception) : Result()
+        }
+
+        internal class Factory(
+            private val application: Application
+        ) : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return SepaDebitViewModel(application) as T
+            }
         }
     }
 
     private companion object {
         private const val CUSTOMER_NAME = "Jenny Rosen"
         private const val CUSTOMER_EMAIL = "jrosen@example.com"
+
+        private const val TEST_ACCOUNT_NUMBER = "DE89370400440532013000"
     }
 }
