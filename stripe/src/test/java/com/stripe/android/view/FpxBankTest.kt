988e2b6563dccf0c73a2b860abad5a4560a65605
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-02-10 11:32:19 -0500

    Fix FPX bank order (#2156)
    ANDROID-482

--- stripe/src/test/java/com/stripe/android/view/FpxBankTest.kt
+++ stripe/src/test/java/com/stripe/android/view/FpxBankTest.kt
@@ -1,5 +1,6 @@
 package com.stripe.android.view
 
+import com.google.common.truth.Truth.assertThat
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
@@ -13,5 +14,32 @@ class FpxBankTest {
     @Test
     fun testGet_withInvalidBank() {
         assertNull(FpxBank.get("not_a_bank"))
+    }
+
+    @Test
+    fun testOrder() {
+        val expected = listOf(
+            "Maybank2U",
+            "CIMB Clicks",
+            "Public Bank",
+            "RHB Bank",
+            "Hong Leong Bank",
+            "AmBank",
+            "Affin Bank",
+            "Alliance Bank",
+            "Bank Islam",
+            "Bank Muamalat",
+            "Bank Rakyat",
+            "BSN",
+            "HSBC Bank",
+            "KFH",
+            "Maybank2E",
+            "OCBC Bank",
+            "Standard Chartered",
+            "UOB"
+        )
+        val actual = FpxBank.values().map { it.displayName }
+        assertThat(expected)
+            .isEqualTo(actual)
     }
 }
