3c884cddd618a9ff9b891c751423c17512fae41e
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-02-19 16:26:35 -0500

    Create CardParams (#2201)
    Motivation
    Create a new class, `CardParams`, to represent the parameters for
    [creating a card](https://stripe.com/docs/api/cards/create). This
    was previously done with `Card`, which was used for both creating
    a card and representing a card object.
    
    `CardParams` is currently internal only, but will be made available
    in a future PR.
    
    Testing
    Create a unit test

---
+++ stripe/src/test/java/com/stripe/android/model/CardParamsTest.kt
@@ -0,0 +1,41 @@
+package com.stripe.android.model
+
+import com.google.common.truth.Truth.assertThat
+import com.stripe.android.CardNumberFixtures
+import kotlin.test.Test
+
+class CardParamsTest {
+
+    @Test
+    fun toParamMap_shouldCreateExpectedMap() {
+        val actualParams = CardParams(
+            number = CardNumberFixtures.VALID_VISA_NO_SPACES,
+            expMonth = 12,
+            expYear = 2025,
+            cvc = "123",
+            name = "Jenny Rosen",
+            address = AddressFixtures.ADDRESS,
+            currency = "usd"
+        ).toParamMap()
+
+        val expectedParams = mapOf(
+            "card" to mapOf(
+                "number" to "4242424242424242",
+                "exp_month" to 12,
+                "exp_year" to 2025,
+                "cvc" to "123",
+                "name" to "Jenny Rosen",
+                "currency" to "usd",
+                "address_line1" to "123 Market St",
+                "address_line2" to "#345",
+                "address_city" to "San Francisco",
+                "address_state" to "CA",
+                "address_zip" to "94107",
+                "address_country" to "US"
+            )
+        )
+
+        assertThat(actualParams)
+            .isEqualTo(expectedParams)
+    }
+}
