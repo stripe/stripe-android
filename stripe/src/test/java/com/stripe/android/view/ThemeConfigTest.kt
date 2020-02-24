24b0568cedf88770e515b43f91aafdb6cd248cd4
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-02-24 14:28:25 -0500

    Simplify MaskedCardView tinting logic (#2218)
    Previously, `MaskedCardView` image tinting logic was applied every time
    the image drawable was updated. Tinting only needs to be applied once,
    so apply it only when the view is instantiated.

---
+++ stripe/src/test/java/com/stripe/android/view/ThemeConfigTest.kt
@@ -0,0 +1,26 @@
+package com.stripe.android.view
+
+import androidx.core.graphics.ColorUtils
+import androidx.test.core.app.ApplicationProvider
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ThemeConfigTest {
+
+    private val themeConfig = ThemeConfig(ApplicationProvider.getApplicationContext())
+
+    @Test
+    fun textColorValues_shouldBeExpectedValues() {
+        val alpha = 204 // 80% of 255
+        val colorValues = themeConfig.textColorValues
+        // The colors are arranged [selected, selectedLowAlpha, unselected, unselectedLowAlpha
+        assertEquals(4, colorValues.size)
+        assertEquals(colorValues[1],
+            ColorUtils.setAlphaComponent(colorValues[0], alpha))
+        assertEquals(colorValues[3],
+            ColorUtils.setAlphaComponent(colorValues[2], alpha))
+    }
+}
