6006977c7edcc2646e710754426b5be02d29e6c2
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-03-04 17:09:40 -0500

    Migrate PaymentFlowActivity to use ViewBinding (#2252)

--- stripe/src/main/java/com/stripe/android/view/PaymentFlowPage.kt
+++ stripe/src/main/java/com/stripe/android/view/PaymentFlowPage.kt
@@ -1,13 +1,11 @@
 package com.stripe.android.view
 
-import androidx.annotation.LayoutRes
 import androidx.annotation.StringRes
 import com.stripe.android.R
 
 internal enum class PaymentFlowPage(
-    @param:StringRes @field:StringRes @get:StringRes val titleResId: Int,
-    @param:LayoutRes @field:LayoutRes @get:LayoutRes val layoutResId: Int
+    @param:StringRes @field:StringRes @get:StringRes val titleResId: Int
 ) {
-    SHIPPING_INFO(R.string.title_add_an_address, R.layout.activity_enter_shipping_info),
-    SHIPPING_METHOD(R.string.title_select_shipping_method, R.layout.activity_select_shipping_method)
+    ShippingInfo(R.string.title_add_an_address),
+    ShippingMethod(R.string.title_select_shipping_method)
 }
