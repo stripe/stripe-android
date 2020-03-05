cdf6afe2a3ea6acf86757a3985b9baf0ec8ef204
Michael Shafrir <mshafrir@stripe.com>
2020-03-05 11:16:05 -0500

    Fix Google Pay example

--- example/src/main/java/com/stripe/example/activity/GooglePayButton.kt
+++ example/src/main/java/com/stripe/example/activity/GooglePayButton.kt
@@ -2,9 +2,9 @@ package com.stripe.example.activity
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.view.LayoutInflater
 import android.widget.FrameLayout
-import com.stripe.example.R
+import com.stripe.example.databinding.GooglePayButtonBinding
 
 class GooglePayButton @JvmOverloads constructor(
     context: Context,
@@ -13,6 +13,10 @@ class GooglePayButton @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
     init {
-        View.inflate(context, R.layout.google_pay_button, this)
+        GooglePayButtonBinding.inflate(
+            LayoutInflater.from(context),
+            this,
+            true
+        )
     }
 }
