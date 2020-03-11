a5676973651c5d3890dfffe6e71f98b6262e46fa
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-03-11 16:07:31 -0400

    Create BecsDebitMandateAcceptanceFactory (#2285)
    Class to create the BECS mandate acceptance copy that must be shown
    with the BECS Debit element.

---
+++ stripe/src/test/java/com/stripe/android/view/BecsDebitMandateAcceptanceFactoryTest.kt
@@ -0,0 +1,22 @@
+package com.stripe.android.view
+
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import java.util.Locale
+import kotlin.test.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class BecsDebitMandateAcceptanceFactoryTest {
+    private val factory = BecsDebitMandateAcceptanceFactory(
+        ApplicationProvider.getApplicationContext()
+    )
+
+    @Test
+    fun testCreate() {
+        Locale.setDefault(Locale.US)
+        assertThat(factory.create("Rocketship Inc."))
+            .isEqualTo("By providing your bank account details and confirming this payment, you agree to this Direct Debit Request and the Direct Debit Request service agreement, and authorise Stripe Payments Australia Pty Ltd ACN 160 180 343 Direct Debit User ID number 507156 (“Stripe”) to debit your account through the Bulk Electronic Clearing System (BECS) on behalf of Rocketship Inc. (the Merchant) for any amounts separately communicated to you by the Merchant. You certify that you are either an account holder or an authorised signatory on the account listed above.")
+    }
+}
