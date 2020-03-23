fbd89502458b935d80578dbcc62ca8b5a56c4c80
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-03-23 13:29:15 -0400

    Create StripeConnection interface (#2309)
    Move existing `StripeConnection` implementation to
    `StripeConnection.Default`

--- stripe/src/main/java/com/stripe/android/StripeConnection.kt
+++ stripe/src/main/java/com/stripe/android/StripeConnection.kt
@@ -12,67 +12,72 @@ import javax.net.ssl.HttpsURLConnection
  * A wrapper for accessing a [HttpURLConnection]. Implements [Closeable] to simplify closing related
  * resources.
  */
-internal class StripeConnection internal constructor(
-    private val conn: HttpsURLConnection
-) : Closeable {
-    internal val responseCode: Int
-        @JvmSynthetic
-        get() {
-            return conn.responseCode
-        }
+internal interface StripeConnection : Closeable {
+    val responseCode: Int
+    val response: StripeResponse
 
-    internal val response: StripeResponse
-        @Throws(IOException::class)
-        @JvmSynthetic
-        get() {
-            // trigger the request
-            val responseCode = this.responseCode
-            return StripeResponse(
-                code = responseCode,
-                body = responseBody,
-                headers = conn.headerFields
-            )
-        }
-
-    private val responseBody: String?
-        @Throws(IOException::class)
-        get() {
-            return getResponseBody(responseStream)
-        }
-
-    private val responseStream: InputStream?
-        @Throws(IOException::class)
-        get() {
-            return if (responseCode in 200..299) {
-                conn.inputStream
-            } else {
-                conn.errorStream
+    class Default internal constructor(
+        private val conn: HttpsURLConnection
+    ) : StripeConnection {
+        override val responseCode: Int
+            @JvmSynthetic
+            get() {
+                return conn.responseCode
             }
+
+        override val response: StripeResponse
+            @Throws(IOException::class)
+            @JvmSynthetic
+            get() {
+                // trigger the request
+                val responseCode = this.responseCode
+                return StripeResponse(
+                    code = responseCode,
+                    body = responseBody,
+                    headers = conn.headerFields
+                )
+            }
+
+        private val responseBody: String?
+            @Throws(IOException::class)
+            get() {
+                return getResponseBody(responseStream)
+            }
+
+        private val responseStream: InputStream?
+            @Throws(IOException::class)
+            get() {
+                return if (responseCode in 200..299) {
+                    conn.inputStream
+                } else {
+                    conn.errorStream
+                }
+            }
+
+        @Throws(IOException::class)
+        private fun getResponseBody(responseStream: InputStream?): String? {
+            if (responseStream == null) {
+                return null
+            }
+
+            // \A is the beginning of the stream boundary
+            val scanner = Scanner(responseStream, CHARSET).useDelimiter("\\A")
+            val responseBody = if (scanner.hasNext()) {
+                scanner.next()
+            } else {
+                null
+            }
+            responseStream.close()
+            return responseBody
         }
 
-    @Throws(IOException::class)
-    private fun getResponseBody(responseStream: InputStream?): String? {
-        if (responseStream == null) {
-            return null
+        override fun close() {
+            responseStream?.close()
+            conn.disconnect()
         }
 
-        // \A is the beginning of the stream boundary
-        val scanner = Scanner(responseStream, CHARSET).useDelimiter("\\A")
-        val responseBody = if (scanner.hasNext()) {
-            scanner.next()
-        } else {
-            null
+        private companion object {
+            private val CHARSET = StandardCharsets.UTF_8.name()
         }
-        responseStream.close()
-        return responseBody
-    }
-
-    override fun close() {
-        responseStream?.close()
-        conn.disconnect()
-    }
-
-    private companion object {
-        private val CHARSET = StandardCharsets.UTF_8.name()
     }
 }
