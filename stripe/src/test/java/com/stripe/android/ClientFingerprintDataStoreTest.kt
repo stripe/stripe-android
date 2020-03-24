08456ac922958a87f8dfcd5b4a50c5ea30b4e63d
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-03-24 17:03:43 -0400

    Add sid to TelemetryClientUtil (#2320)

--- stripe/src/test/java/com/stripe/android/ClientFingerprintDataStoreTest.kt
+++ stripe/src/test/java/com/stripe/android/ClientFingerprintDataStoreTest.kt
@@ -1,7 +1,10 @@
 package com.stripe.android
 
+import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import java.util.Calendar
+import java.util.concurrent.TimeUnit
 import kotlin.test.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -9,13 +12,37 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class ClientFingerprintDataStoreTest {
 
-    private val store = ClientFingerprintDataStore.Default(
-        ApplicationProvider.getApplicationContext()
-    )
+    private val context: Context = ApplicationProvider.getApplicationContext()
+    private val store = ClientFingerprintDataStore.Default(context)
 
     @Test
     fun getMuid_shouldReturnSameValue() {
         assertThat(store.getMuid())
             .isEqualTo(store.getMuid())
+    }
+
+    @Test
+    fun getSid_whenSameSession_shouldReturnSameValue() {
+        assertThat(store.getSid())
+            .isEqualTo(store.getSid())
+    }
+
+    @Test
+    fun getSid_whenDifferentSession_shouldReturnDifferentValue() {
+        var callbacks = 0
+        val store = ClientFingerprintDataStore.Default(
+            context = context,
+            timestampSupplier = {
+                val currentTime = Calendar.getInstance().timeInMillis
+                // simulate different sessions
+                val timestamp = currentTime + TimeUnit.HOURS.toMillis(callbacks.toLong())
+                callbacks++
+                timestamp
+            }
+        )
+        val firstSid = store.getSid()
+        val secondSid = store.getSid()
+        assertThat(firstSid)
+            .isNotEqualTo(secondSid)
     }
 }
--- stripe/src/test/java/com/stripe/android/FakeClientFingerprintDataStore.kt
+++ stripe/src/test/java/com/stripe/android/FakeClientFingerprintDataStore.kt
@@ -3,7 +3,9 @@ package com.stripe.android
 import java.util.UUID
 
 internal class FakeClientFingerprintDataStore(
-    private val muid: UUID = UUID.randomUUID()
+    private val muid: UUID = UUID.randomUUID(),
+    private val sid: UUID = UUID.randomUUID()
 ) : ClientFingerprintDataStore {
     override fun getMuid(): String = muid.toString()
+    override fun getSid(): String = sid.toString()
 }
