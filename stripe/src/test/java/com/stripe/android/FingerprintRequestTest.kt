c7ebe7fa71df3ded44157f70a6259de8feaf1d84
Michael Shafrir <mshafrir@stripe.com>
2020-03-27 11:03:34 -0400

    Fix FingerprintRequestTest

--- stripe/src/test/java/com/stripe/android/FingerprintRequestTest.kt
+++ stripe/src/test/java/com/stripe/android/FingerprintRequestTest.kt
@@ -3,6 +3,7 @@ package com.stripe.android
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import java.io.ByteArrayOutputStream
+import java.util.UUID
 import kotlin.test.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -13,19 +14,25 @@ class FingerprintRequestTest {
         ApplicationProvider.getApplicationContext()
     )
 
+    private val request =
+        FingerprintRequest(
+            params = fingerprintRequestParamsFactory.createParams(),
+            guid = GUID
+        )
+
     @Test
     fun contentType_shouldBeApplicationJson() {
-        assertThat(FingerprintRequest(params = emptyMap(), guid = "guid").contentType)
+        assertThat(request.contentType)
             .isEqualTo("application/json; charset=UTF-8")
     }
 
     @Test
     fun headers_shouldReturnExpectedMap() {
-        assertThat(FingerprintRequest(emptyMap(), "guid").headers)
+        assertThat(request.headers)
             .isEqualTo(
                 mapOf(
-                    "Cookie" to "m=guid",
-                    "User-Agent" to "Stripe/v1 AndroidBindings/14.2.0",
+                    "Cookie" to "m=$GUID",
+                    "User-Agent" to RequestHeadersFactory.getUserAgent(),
                     "Accept-Charset" to "UTF-8"
                 )
             )
@@ -34,13 +41,13 @@ class FingerprintRequestTest {
     @Test
     fun writeBody_shouldWriteNonEmptyBytes() {
         ByteArrayOutputStream().use {
-            FingerprintRequest(
-                params = fingerprintRequestParamsFactory.createParams(),
-                guid = "guid"
-            )
-                .writeBody(it)
+            request.writeBody(it)
             assertThat(it.size())
                 .isGreaterThan(0)
         }
+    }
+
+    private companion object {
+        private val GUID = UUID.randomUUID().toString()
     }
 }
