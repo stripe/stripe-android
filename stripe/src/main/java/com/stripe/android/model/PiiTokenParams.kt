4b4116bcb3beb1921aaf9b3a2914fa01d11d13e1
Michael Shafrir <mshafrir@stripe.com>
2020-04-06 11:24:51 -0400

    Update visibility of a few classes

--- stripe/src/main/java/com/stripe/android/model/PiiTokenParams.kt
+++ stripe/src/main/java/com/stripe/android/model/PiiTokenParams.kt
@@ -2,8 +2,11 @@ package com.stripe.android.model
 
 import kotlinx.android.parcel.Parcelize
 
+/**
+ * [TokenParams] for creating a PII token.
+ */
 @Parcelize
-data class PiiTokenParams(
+internal data class PiiTokenParams(
     private val personalId: String
 ) : TokenParams(Token.TokenType.PII) {
     override fun toParamMap(): Map<String, Any> {
--- stripe/src/test/java/com/stripe/android/model/Stripe3dsRedirectTest.kt
+++ stripe/src/test/java/com/stripe/android/model/Stripe3dsRedirectTest.kt
@@ -1,7 +1,7 @@
 package com.stripe.android.model
 
+import com.google.common.truth.Truth.assertThat
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
@@ -9,13 +9,13 @@ import org.robolectric.RobolectricTestRunner
 class Stripe3dsRedirectTest {
 
     @Test
-    fun testCreate() {
+    fun create_shouldReturnExpectedObject() {
         val redirect = Stripe3dsRedirect.create(
             requireNotNull(PaymentIntentFixtures.PI_REQUIRES_3DS1.stripeSdkData)
         )
-        assertEquals(
-            "https://hooks.stripe.com/3d_secure_2_eap/begin_test/src_1Ecve7CRMbs6FrXfm8AxXMIh/src_client_secret_F79yszOBAiuaZTuIhbn3LPUW",
-            redirect.url
-        )
+        assertThat(redirect.url)
+            .isEqualTo(
+                "https://hooks.stripe.com/3d_secure_2_eap/begin_test/src_1Ecve7CRMbs6FrXfm8AxXMIh/src_client_secret_F79yszOBAiuaZTuIhbn3LPUW"
+            )
     }
 }
