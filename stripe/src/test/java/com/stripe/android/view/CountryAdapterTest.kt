5e8bd09970558b5439f54614e71b66334b543d3b
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-04-07 12:50:57 -0400

    Add unit tests to Github Actions config

--- stripe/src/test/java/com/stripe/android/view/CountryAdapterTest.kt
+++ stripe/src/test/java/com/stripe/android/view/CountryAdapterTest.kt
@@ -2,14 +2,11 @@ package com.stripe.android.view
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
 import java.util.Locale
 import kotlin.test.BeforeTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 import org.junit.runner.RunWith
-import org.mockito.MockitoAnnotations
 import org.robolectric.RobolectricTestRunner
 
 /**
@@ -18,8 +15,15 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class CountryAdapterTest {
 
-    private lateinit var countryAdapter: CountryAdapter
-    private lateinit var orderedCountries: List<Country>
+    private val countryAdapter: CountryAdapter by lazy {
+        CountryAdapter(
+            ApplicationProvider.getApplicationContext<Context>(),
+            orderedCountries
+        )
+    }
+    private val orderedCountries: List<Country> by lazy {
+        CountryUtils.getOrderedCountries(Locale.getDefault())
+    }
 
     private val suggestions: List<Country>
         get() {
@@ -30,92 +34,85 @@ class CountryAdapterTest {
 
     @BeforeTest
     fun setup() {
-        MockitoAnnotations.initMocks(this)
         Locale.setDefault(Locale.US)
-
-        orderedCountries = CountryUtils.getOrderedCountries(Locale.getDefault())
-        countryAdapter = CountryAdapter(
-            ApplicationProvider.getApplicationContext<Context>(),
-            orderedCountries
-        )
     }
 
     @Test
     fun filter_whenEmptyConstraint_showsAllResults() {
         countryAdapter.filter.filter("")
-        assertEquals(
-            orderedCountries,
-            suggestions
-        )
+        assertThat(suggestions)
+            .isEqualTo(orderedCountries)
     }
 
     @Test
     fun filter_whenCountryInputNoMatch_showsAllResults() {
         countryAdapter.filter.filter("NONEXISTENT COUNTRY")
-        assertEquals(
-            orderedCountries,
-            suggestions
-        )
+        assertThat(suggestions)
+            .isEqualTo(orderedCountries)
     }
 
     @Test
     fun filter_whenCountryInputMatches_filters() {
         countryAdapter.filter.filter("United")
-        assertEquals(
-            listOf(
-                "United States",
-                "United Arab Emirates",
-                "United Kingdom",
-                "United States Minor Outlying Islands"
-            ),
-            suggestions.map { it.name }
-        )
+        assertThat(suggestions.map { it.name })
+            .isEqualTo(
+                listOf(
+                    "United States",
+                    "United Arab Emirates",
+                    "United Kingdom",
+                    "United States Minor Outlying Islands"
+                )
+            )
     }
 
     @Test
     fun filter_whenCountryInputMatchesExactly_showsAllResults() {
         countryAdapter.filter.filter("Uganda")
-        assertEquals(
-            orderedCountries,
-            suggestions
-        )
+        assertThat(suggestions)
+            .isEqualTo(orderedCountries)
     }
 
     @Test
     fun updateUnfilteredCountries_withPopulatedSet_shouldUpdateSuggestions() {
-        assertEquals("US", countryAdapter.firstItem.code)
-        assertTrue(countryAdapter.updateUnfilteredCountries(setOf("fr", "de")))
-        assertEquals("FR", countryAdapter.firstItem.code)
+        assertThat(countryAdapter.firstItem.code)
+            .isEqualTo("US")
+        assertThat(countryAdapter.updateUnfilteredCountries(setOf("fr", "de")))
+            .isTrue()
+        assertThat(countryAdapter.firstItem.code)
+            .isEqualTo("FR")
     }
 
     @Test
     fun updateUnfilteredCountries_withEmptySet_shouldNotUpdateSuggestions() {
-        assertEquals("US", countryAdapter.firstItem.code)
-        assertFalse(countryAdapter.updateUnfilteredCountries(emptySet()))
-        assertEquals("US", countryAdapter.firstItem.code)
+        assertThat(countryAdapter.firstItem.code)
+            .isEqualTo("US")
+        assertThat(countryAdapter.updateUnfilteredCountries(emptySet()))
+            .isFalse()
+        assertThat(countryAdapter.firstItem.code)
+            .isEqualTo("US")
     }
 
     @Test
     fun updateUnfilteredCountries_shouldUpdateFilter() {
         countryAdapter.filter.filter("United")
-        assertEquals(
-            listOf(
-                "United States",
-                "United Arab Emirates",
-                "United Kingdom",
-                "United States Minor Outlying Islands"
-            ),
-            suggestions.map { it.name }
-        )
+        assertThat(suggestions.map { it.name })
+            .isEqualTo(
+                listOf(
+                    "United States",
+                    "United Arab Emirates",
+                    "United Kingdom",
+                    "United States Minor Outlying Islands"
+                )
+            )
 
         countryAdapter.updateUnfilteredCountries(setOf("gb", "ae"))
         countryAdapter.filter.filter("United")
-        assertEquals(
-            listOf(
-                "United Arab Emirates",
-                "United Kingdom"
-            ),
-            suggestions.map { it.name }
-        )
+        assertThat(suggestions.map { it.name })
+            .isEqualTo(
+                listOf(
+                    "United Arab Emirates",
+                    "United Kingdom"
+                )
+            )
     }
 }
--- stripe/src/test/java/com/stripe/android/view/PaymentUtilsTest.kt
+++ stripe/src/test/java/com/stripe/android/view/PaymentUtilsTest.kt
@@ -1,10 +1,10 @@
 package com.stripe.android.view
 
+import com.google.common.truth.Truth.assertThat
 import java.util.Currency
 import java.util.Locale
 import kotlin.test.BeforeTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
@@ -20,71 +20,71 @@ class PaymentUtilsTest {
 
     @Test
     fun formatPriceStringUsingFree_whenZero_rendersFree() {
-        assertEquals(
+        assertThat(
             PaymentUtils.formatPriceStringUsingFree(
                 0,
-                Currency.getInstance("USD"),
-                "Free"),
-            "Free")
+                USD,
+                "Free"
+            )
+        ).isEqualTo("Free")
     }
 
     @Test
     fun formatPriceString_whenUSLocale_rendersCorrectSymbols() {
         Locale.setDefault(Locale.US)
-        assertEquals(
-            PaymentUtils.formatPriceString(12300.0, Currency.getInstance("USD")),
-            "$123.00"
-        )
+        assertThat(PaymentUtils.formatPriceString(12300.0, USD))
+            .isEqualTo("$123.00")
 
-        val euro = Currency.getInstance(Locale.GERMANY)
-        assertEquals(PaymentUtils.formatPriceString(12300.0, euro), "EUR123.00")
+        assertThat(PaymentUtils.formatPriceString(12300.0, EURO))
+            .isEqualTo("EUR123.00")
 
         val canadianDollar = Currency.getInstance(Locale.CANADA)
-        assertEquals(
-            PaymentUtils.formatPriceString(12300.0, canadianDollar),
-            "CAD123.00"
-        )
+        assertThat(PaymentUtils.formatPriceString(12300.0, canadianDollar))
+            .isEqualTo("CAD123.00")
 
         val britishPound = Currency.getInstance(Locale.UK)
-        assertEquals(PaymentUtils.formatPriceString(10000.0, britishPound), "GBP100.00")
+        assertThat(PaymentUtils.formatPriceString(10000.0, britishPound))
+            .isEqualTo("GBP100.00")
     }
 
     @Test
     fun formatPriceString_whenInternationalLocale_rendersCorrectSymbols() {
         Locale.setDefault(Locale.GERMANY)
-        val euro = Currency.getInstance(Locale.GERMANY)
-        assertEquals(PaymentUtils.formatPriceString(10000.0, euro), "100,00 €")
+        assertThat(PaymentUtils.formatPriceString(10000.0, EURO))
+            .isEqualTo("100,00 €")
 
         Locale.setDefault(Locale.JAPAN)
         val yen = Currency.getInstance(Locale.JAPAN)
         // Japan's native local uses narrow yen symbol (there is also a wide yen symbol)
-        assertEquals(PaymentUtils.formatPriceString(100.0, yen), "￥100")
+        assertThat(PaymentUtils.formatPriceString(100.0, yen))
+            .isEqualTo("￥100")
 
         Locale.setDefault(Locale("ar", "JO")) // Jordan
         val jordanianDinar = Currency.getInstance("JOD")
-        assertEquals(PaymentUtils.formatPriceString(100123.0, jordanianDinar), jordanianDinar.symbol + " 100.123")
+        assertThat(PaymentUtils.formatPriceString(100123.0, jordanianDinar))
+            .isEqualTo("${jordanianDinar.symbol} 100.123")
 
         Locale.setDefault(Locale.UK)
         val britishPound = Currency.getInstance(Locale.UK)
-        assertEquals(PaymentUtils.formatPriceString(10000.0, britishPound), "£100.00")
+        assertThat(PaymentUtils.formatPriceString(10000.0, britishPound))
+            .isEqualTo("£100.00")
 
         Locale.setDefault(Locale.CANADA)
         val canadianDollar = Currency.getInstance(Locale.CANADA)
-        assertEquals(
-            PaymentUtils.formatPriceString(12300.0, canadianDollar),
-            "$123.00"
-        )
+        assertThat(PaymentUtils.formatPriceString(12300.0, canadianDollar))
+            .isEqualTo("$123.00")
     }
 
     @Test
     fun formatPriceString_whenDecimalAmounts_rendersCorrectDigits() {
-        assertEquals(
-            "$100.12",
-            PaymentUtils.formatPriceString(10012.0, Currency.getInstance("USD"))
-        )
-        assertEquals(
-            "$0.12",
-            PaymentUtils.formatPriceString(12.0, Currency.getInstance("USD"))
-        )
+        assertThat(PaymentUtils.formatPriceString(10012.0, USD))
+            .isEqualTo("$100.12")
+        assertThat(PaymentUtils.formatPriceString(12.0, USD))
+            .isEqualTo("$0.12")
+    }
+
+    private companion object {
+        private val USD = Currency.getInstance("USD")
+        private val EURO = Currency.getInstance(Locale.GERMANY)
     }
 }
