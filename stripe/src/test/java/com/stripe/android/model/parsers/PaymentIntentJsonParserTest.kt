18bee370b7ae16292966b6a59bf8df779caa08dd
Michael Shafrir <mshafrir@stripe.com>
2020-04-16 15:53:53 -0400

    Add shipping property to PaymentIntent
    Add support for
    https://stripe.com/docs/api/payment_intents/object#payment_intent_object-shipping
    
    Added test

--- stripe/src/test/java/com/stripe/android/model/parsers/PaymentIntentJsonParserTest.kt
+++ stripe/src/test/java/com/stripe/android/model/parsers/PaymentIntentJsonParserTest.kt
@@ -1,6 +1,8 @@
 package com.stripe.android.model.parsers
 
 import com.google.common.truth.Truth.assertThat
+import com.stripe.android.model.Address
+import com.stripe.android.model.PaymentIntent
 import com.stripe.android.model.PaymentIntentFixtures
 import kotlin.test.Test
 import org.junit.runner.RunWith
@@ -11,11 +13,36 @@ class PaymentIntentJsonParserTest {
     @Test
     fun parse_withExpandedPaymentMethod_shouldCreateExpectedObject() {
         val paymentIntent = PaymentIntentJsonParser().parse(
-            PaymentIntentFixtures.EXPANDED_PAYMENT_METHOD
+            PaymentIntentFixtures.EXPANDED_PAYMENT_METHOD_JSON
         )
         assertThat(paymentIntent?.paymentMethodId)
             .isEqualTo("pm_1GSTxOCRMbs6FrXfYCosDqyr")
         assertThat(paymentIntent?.paymentMethod?.id)
             .isEqualTo("pm_1GSTxOCRMbs6FrXfYCosDqyr")
+    }
+
+    @Test
+    fun parse_withShipping_shouldCreateExpectedObject() {
+        val paymentIntent = PaymentIntentJsonParser().parse(
+            PaymentIntentFixtures.PI_WITH_SHIPPING_JSON
+        )
+
+        assertThat(paymentIntent?.shipping)
+            .isEqualTo(
+                PaymentIntent.Shipping(
+                    address = Address(
+                        line1 = "123 Market St",
+                        line2 = "#345",
+                        city = "San Francisco",
+                        state = "CA",
+                        postalCode = "94107",
+                        country = "US"
+                    ),
+                    carrier = "UPS",
+                    name = "Jenny Rosen",
+                    phone = "1-800-555-1234",
+                    trackingNumber = "12345"
+                )
+            )
     }
 }
