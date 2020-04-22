6d0779f7578dd73fa11324590ec5f7d71f164383
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-04-22 13:36:58 -0400

    Make StripeIntentResult implement Parcelable (#2412)

--- stripe/src/main/java/com/stripe/android/SetupIntentResult.kt
+++ stripe/src/main/java/com/stripe/android/SetupIntentResult.kt
@@ -1,8 +1,27 @@
 package com.stripe.android
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.stripe.android.model.SetupIntent
 
 class SetupIntentResult internal constructor(
     setupIntent: SetupIntent,
     @Outcome outcome: Int = 0
-) : StripeIntentResult<SetupIntent>(setupIntent, outcome)
+) : StripeIntentResult<SetupIntent>(setupIntent, outcome) {
+    internal constructor(parcel: Parcel) : this(
+        setupIntent = requireNotNull(
+            parcel.readParcelable<SetupIntent>(SetupIntent::class.java.classLoader)
+        ),
+        outcome = parcel.readInt()
+    )
+
+    companion object CREATOR : Parcelable.Creator<SetupIntentResult> {
+        override fun createFromParcel(parcel: Parcel): SetupIntentResult {
+            return SetupIntentResult(parcel)
+        }
+
+        override fun newArray(size: Int): Array<SetupIntentResult?> {
+            return arrayOfNulls<SetupIntentResult?>(size)
+        }
+    }
+}
