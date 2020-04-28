e15ef74ba403a9dc510425e155e5f0e4e88a7720
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-04-28 15:23:25 -0400

    Add advancedFraudSignalsEnabled property (#2436)
    This allows you to control whether the Stripe SDK is collecting
    additional device data for fraud prevention.
    
    For more details on the information we collect, visit
    https://stripe.com/docs/disputes/prevention/advanced-fraud-detection

--- stripe/src/test/java/com/stripe/android/FingerprintDataRepositoryTest.kt
+++ stripe/src/test/java/com/stripe/android/FingerprintDataRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.stripe.android
 
 import android.content.Context
+import android.os.Handler
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.KArgumentCaptor
@@ -8,11 +9,13 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.doNothing
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import java.util.Calendar
 import java.util.UUID
 import java.util.concurrent.TimeUnit
+import kotlin.test.AfterTest
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -23,6 +26,11 @@ class FingerprintDataRepositoryTest {
     private val context = ApplicationProvider.getApplicationContext<Context>()
     private val fingerprintRequestExecutor: FingerprintRequestExecutor = mock()
     private val requestExecutorCallback: KArgumentCaptor<(FingerprintData?) -> Unit> = argumentCaptor()
+
+    @AfterTest
+    fun after() {
+        Stripe.advancedFraudSignalsEnabled = true
+    }
 
     @Test
     fun roundtrip_shouldReturnOriginalObject() {
@@ -75,6 +83,26 @@ class FingerprintDataRepositoryTest {
                 currentTime = fingerprintData.timestamp + TimeUnit.MINUTES.toMillis(31L)
             )
         ).isTrue()
+    }
+
+    @Test
+    fun refresh_whenAdvancedFraudSignalsDisabled_shouldNotFetchFingerprintData() {
+        Stripe.advancedFraudSignalsEnabled = false
+
+        val store: FingerprintDataStore = mock()
+        val fingerprintRequestFactory: FingerprintRequestFactory = mock()
+        val handler: Handler = mock()
+        val repository = FingerprintDataRepository.Default(
+            store = store,
+            fingerprintRequestFactory = fingerprintRequestFactory,
+            fingerprintRequestExecutor = fingerprintRequestExecutor
+        )
+        repository.refresh()
+
+        verify(store, never()).save(any())
+        verify(handler, never()).post(any())
+        verify(fingerprintRequestFactory, never()).create(any())
+        verify(fingerprintRequestExecutor, never()).execute(any(), any())
     }
 
     private companion object {
