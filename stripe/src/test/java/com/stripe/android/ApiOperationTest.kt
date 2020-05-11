d8a15c08a9d931eb9681b5e349ea60a0f38c0590
Michael Shafrir <mshafrir@stripe.com>
2020-05-11 16:34:16 -0400

    Refactor ApiOperation to use kotlin.Result
    - Delete unused `ResultWrapper`
    - Add tests to `ApiOperationTest`

--- stripe/src/test/java/com/stripe/android/ApiOperationTest.kt
+++ stripe/src/test/java/com/stripe/android/ApiOperationTest.kt
@@ -1,22 +1,83 @@
 package com.stripe.android
 
 import com.nhaarman.mockitokotlin2.argWhere
+import com.nhaarman.mockitokotlin2.isA
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.stripe.android.exception.APIException
 import com.stripe.android.exception.InvalidRequestException
+import com.stripe.android.exception.RateLimitException
+import com.stripe.android.model.PaymentIntent
+import com.stripe.android.model.PaymentIntentFixtures
+import java.lang.RuntimeException
 import kotlin.test.Test
 import kotlinx.coroutines.MainScope
+import org.json.JSONException
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class ApiOperationTest {
 
-    private val callback: ApiResultCallback<String> = mock()
+    private val callback: ApiResultCallback<PaymentIntent> = mock()
 
     @Test
-    fun getResult_whenException_shouldInvokeCallbackOnError() {
-        FakeApiOperation(callback).execute()
+    fun getResult_whenSuccess_shouldInvokeCallbackOnSuccess() {
+        FakeApiOperation(
+            {
+                PaymentIntentFixtures.PI_REQUIRES_3DS1
+            },
+            callback
+        ).execute()
+        verify(callback)
+            .onSuccess(PaymentIntentFixtures.PI_REQUIRES_3DS1)
+    }
+
+    @Test
+    fun getResult_whenNull_shouldInvokeCallbackOnError() {
+        FakeApiOperation(
+            {
+                null
+            },
+            callback
+        ).execute()
+        verify(callback).onError(
+            argWhere {
+                (it as? RuntimeException)?.message == "The API operation returned neither a result or exception"
+            }
+        )
+    }
+
+    @Test
+    fun getResult_whenJsonException_shouldInvokeCallbackOnError() {
+        FakeApiOperation(
+            {
+                throw JSONException("")
+            },
+            callback
+        ).execute()
+        verify(callback).onError(isA<APIException>())
+    }
+
+    @Test
+    fun getResult_whenRateLimitException_shouldInvokeCallbackOnError() {
+        FakeApiOperation(
+            {
+                throw RateLimitException()
+            },
+            callback
+        ).execute()
+        verify(callback).onError(isA<RateLimitException>())
+    }
+
+    @Test
+    fun getResult_whenIllegalArgumentException_shouldInvokeCallbackOnError() {
+        FakeApiOperation(
+            {
+                throw IllegalArgumentException("Illegal argument!")
+            },
+            callback
+        ).execute()
         verify(callback).onError(
             argWhere {
                 (it as? InvalidRequestException)?.message == "Illegal argument!"
@@ -25,13 +86,12 @@ class ApiOperationTest {
     }
 
     internal class FakeApiOperation(
-        callback: ApiResultCallback<String>
-    ) : ApiOperation<String>(
+        private val resultSupplier: () -> PaymentIntent?,
+        callback: ApiResultCallback<PaymentIntent>
+    ) : ApiOperation<PaymentIntent>(
         workScope = MainScope(),
         callback = callback
     ) {
-        override suspend fun getResult(): String? {
-            throw IllegalArgumentException("Illegal argument!")
-        }
+        override suspend fun getResult(): PaymentIntent? = resultSupplier()
     }
 }
