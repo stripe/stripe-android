ab48e6cec59c26cdf41c8ab1f2552a9dc2665490
Samuel Maskell <smaskell@stripe.com>
2020-05-14 17:24:51 -0700

    Add Either class

--- stripe/src/main/java/com/stripe/android/utils/Either.kt
+++ stripe/src/main/java/com/stripe/android/utils/Either.kt
@@ -1,6 +1,6 @@
 package com.stripe.android.utils
 
 sealed class Either<A, B> {
-    data class Left<A, B>(val left: A): Either<A, B>()
-    data class Right<A, B>(val right: B): Either<A, B>()
+    data class Left<A, B>(val left: A) : Either<A, B>()
+    data class Right<A, B>(val right: B) : Either<A, B>()
 }
