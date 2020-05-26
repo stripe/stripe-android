2fe0337c84e70b27c0e6ebf9491801d8dd795c27
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-05-26 15:02:15 -0400

    Use liveData builder in example app (#2520)

--- example/src/main/java/com/stripe/example/activity/BaseViewModel.kt
+++ example/src/main/java/com/stripe/example/activity/BaseViewModel.kt
@@ -5,20 +5,20 @@ import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import com.stripe.example.module.BackendApiFactory
 import com.stripe.example.service.BackendApi
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancelChildren
+import kotlin.coroutines.CoroutineContext
 
 abstract class BaseViewModel(
     application: Application
 ) : AndroidViewModel(application) {
     protected val context: Context = application.applicationContext
-    protected val workScope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    protected val workContext: CoroutineContext = Dispatchers.IO + SupervisorJob()
     protected val backendApi: BackendApi = BackendApiFactory(application.applicationContext).create()
 
     override fun onCleared() {
         super.onCleared()
-        workScope.coroutineContext.cancelChildren()
+        workContext.cancelChildren()
     }
 }
