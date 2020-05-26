9774cb127124da670b89b8475fd028b739d133e0
Samuel Maskell <smaskell@stripe.com>
2020-05-26 14:47:38 -0700

    Add JvmSynthetic to packageInfo property

--- stripe/src/main/java/com/stripe/android/model/ContextUtils.kt
+++ stripe/src/main/java/com/stripe/android/model/ContextUtils.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageInfo
 
 internal object ContextUtils {
     internal val Context.packageInfo: PackageInfo?
+        @JvmSynthetic
         get() = runCatching {
             packageManager.getPackageInfo(packageName, 0)
         }.getOrNull()
