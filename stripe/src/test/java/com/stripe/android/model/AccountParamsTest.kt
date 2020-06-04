016bf6ef4d559017e6d405a87e6de7e0d40ebcae
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-06-04 14:37:09 -0400

    Remove deprecated AccountParams.create() method (#2545)

--- stripe/src/test/java/com/stripe/android/model/AccountParamsTest.kt
+++ stripe/src/test/java/com/stripe/android/model/AccountParamsTest.kt
@@ -1,39 +1,40 @@
 package com.stripe.android.model
 
+import com.google.common.truth.Truth.assertThat
 import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 
 class AccountParamsTest {
 
     @Test
     fun toParamMap_withBusinessData() {
         val company = AccountParams.BusinessTypeParams.Company(name = "Stripe")
-        val params = AccountParams.create(
-            true,
-            company
-        )
-            .toParamMap()
 
-        val accountData = params["account"] as Map<String, *>?
-        assertNotNull(accountData)
-        assertEquals(3, accountData.size)
-        assertEquals(java.lang.Boolean.TRUE, accountData[AccountParams.PARAM_TOS_SHOWN_AND_ACCEPTED])
-        assertEquals(AccountParams.BusinessType.Company.code,
-            accountData[AccountParams.PARAM_BUSINESS_TYPE])
-        assertEquals(
-            company.toParamMap(),
-            accountData[AccountParams.BusinessType.Company.code]
+        assertThat(
+            AccountParams.create(
+                true,
+                company
+            ).toParamMap()
+        ).isEqualTo(
+            mapOf(
+                "account" to mapOf(
+                    "tos_shown_and_accepted" to true,
+                    "business_type" to "company",
+                    "company" to company.toParamMap()
+                )
+            )
         )
     }
 
     @Test
     fun toParamMap_withNoBusinessData() {
-        val params = AccountParams.create(true).toParamMap()
-
-        val accountData = params["account"] as Map<String, *>?
-        assertNotNull(accountData)
-        assertEquals(1, accountData.size)
-        assertEquals(java.lang.Boolean.TRUE, accountData[AccountParams.PARAM_TOS_SHOWN_AND_ACCEPTED])
+        assertThat(
+            AccountParams.create(true).toParamMap()
+        ).isEqualTo(
+            mapOf(
+                "account" to mapOf(
+                    "tos_shown_and_accepted" to true
+                )
+            )
+        )
     }
 }
