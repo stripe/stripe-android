4f8d828796d41521d94dec112e4f9e619a705080
Michael Shafrir <mshafrir@stripe.com>
2020-06-17 09:25:42 -0400

    Use coroutines in StripeEditText
    Replace Handler with coroutine
    
    Add test

--- stripe/src/main/java/com/stripe/android/view/StripeEditText.kt
+++ stripe/src/main/java/com/stripe/android/view/StripeEditText.kt
@@ -2,7 +2,6 @@ package com.stripe.android.view
 
 import android.content.Context
 import android.content.res.ColorStateList
-import android.os.Handler
 import android.text.Editable
 import android.util.AttributeSet
 import android.view.KeyEvent
@@ -15,6 +14,13 @@ import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.stripe.android.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Extension of [TextInputEditText] that listens for users pressing the delete key when
@@ -26,7 +32,8 @@ import com.stripe.android.R
 open class StripeEditText @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = androidx.appcompat.R.attr.editTextStyle
+    defStyleAttr: Int = androidx.appcompat.R.attr.editTextStyle,
+    private val workScope: CoroutineScope = CoroutineScope(Dispatchers.IO + Job())
 ) : TextInputEditText(context, attrs, defStyleAttr) {
 
     protected var isLastKeyDelete: Boolean = false
@@ -70,10 +77,10 @@ open class StripeEditText @JvmOverloads constructor(
 
     @ColorInt
     private var defaultErrorColor: Int = 0
+
     @ColorInt
     private var errorColor: Int? = null
 
-    private val hintHandler: Handler = Handler()
     private var errorMessageListener: ErrorMessageListener? = null
 
     /**
@@ -158,9 +165,13 @@ open class StripeEditText @JvmOverloads constructor(
      * @param delayMilliseconds a delay period, measured in milliseconds
      */
     fun setHintDelayed(hint: CharSequence, delayMilliseconds: Long) {
-        hintHandler.postDelayed({
-            setHintSafely(hint)
-        }, delayMilliseconds)
+        workScope.launch {
+            delay(delayMilliseconds)
+
+            withContext(Dispatchers.Main) {
+                setHintSafely(hint)
+            }
+        }
     }
 
     /**
@@ -168,9 +179,10 @@ open class StripeEditText @JvmOverloads constructor(
      * [known issue on Samsung devices](https://issuetracker.google.com/issues/37127697).
      */
     private fun setHintSafely(hint: CharSequence) {
-        try {
+        runCatching {
             setHint(hint)
-        } catch (e: NullPointerException) {
+        }.recover {
+            it
         }
     }
 
@@ -183,8 +195,7 @@ open class StripeEditText @JvmOverloads constructor(
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        // Passing a null token removes all callbacks and messages to the handler.
-        hintHandler.removeCallbacksAndMessages(null)
+        workScope.cancel()
     }
 
     private fun determineDefaultErrorColor() {
