a2fb1591fd4d04995e88af964a59787da25c22e6
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-07-31 13:23:33 -0400

    Make CardParams extend TokenParams (#2670)

--- stripe/src/test/java/com/stripe/android/model/CardParamsTest.kt
+++ stripe/src/test/java/com/stripe/android/model/CardParamsTest.kt
@@ -20,19 +20,21 @@ class CardParamsTest {
 
         assertThat(actualParams)
             .isEqualTo(
-                mapOf(
-                    "number" to "4242424242424242",
-                    "exp_month" to 12,
-                    "exp_year" to 2025,
-                    "cvc" to "123",
-                    "name" to "Jenny Rosen",
-                    "currency" to "usd",
-                    "address_line1" to "123 Market St",
-                    "address_line2" to "#345",
-                    "address_city" to "San Francisco",
-                    "address_state" to "CA",
-                    "address_zip" to "94107",
-                    "address_country" to "US"
+                mapOf("card" to
+                    mapOf(
+                        "number" to "4242424242424242",
+                        "exp_month" to 12,
+                        "exp_year" to 2025,
+                        "cvc" to "123",
+                        "name" to "Jenny Rosen",
+                        "currency" to "usd",
+                        "address_line1" to "123 Market St",
+                        "address_line2" to "#345",
+                        "address_city" to "San Francisco",
+                        "address_state" to "CA",
+                        "address_zip" to "94107",
+                        "address_country" to "US"
+                    )
                 )
             )
     }
