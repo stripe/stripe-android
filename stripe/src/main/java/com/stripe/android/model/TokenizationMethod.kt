e766dcc980922db086cabcb011c54b30c4757e1d
Michael Shafrir <mshafrir@stripe.com>
2020-08-03 13:40:09 -0400

    Add missing TokenizationMethod values

--- stripe/src/main/java/com/stripe/android/model/TokenizationMethod.kt
+++ stripe/src/main/java/com/stripe/android/model/TokenizationMethod.kt
@@ -1,11 +1,15 @@
 package com.stripe.android.model
 
 /**
- * If a card number is tokenized, this is the method that was used. Can be apple_pay or google_pay.
+ * If a card number is tokenized, this is the method that was used.
+ *
+ * See [tokenization_method](https://stripe.com/docs/api/cards/object#card_object-tokenization_method)
  */
 enum class TokenizationMethod(val code: String) {
     ApplePay("apple_pay"),
-    GooglePay("google_pay");
+    GooglePay("google_pay"),
+    Masterpass("masterpass"),
+    VisaCheckout("visa_checkout");
 
     internal companion object {
         internal fun fromCode(code: String?): TokenizationMethod? {
