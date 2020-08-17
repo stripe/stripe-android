ad78ae62f3668af7a7714c17d9131a514d623155
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-08-17 14:17:56 -0400

    Implement DefaultCardAccountRangeRepository (#2719)

--- stripe/src/main/java/com/stripe/android/cards/DefaultCardAccountRangeRepository.kt
+++ stripe/src/main/java/com/stripe/android/cards/DefaultCardAccountRangeRepository.kt
@@ -3,13 +3,23 @@ package com.stripe.android.cards
 import com.stripe.android.model.CardMetadata
 
 internal class DefaultCardAccountRangeRepository(
+    private val inMemoryCardAccountRangeSource: CardAccountRangeSource,
     private val localCardAccountRangeSource: CardAccountRangeSource,
     private val remoteCardAccountRangeSource: CardAccountRangeSource
 ) : CardAccountRangeRepository {
     override suspend fun getAccountRange(
         cardNumber: String
     ): CardMetadata.AccountRange? {
-        // TODO(mshafrir-stripe): implement
-        return null
+        val bin = cardNumber
+            .take(CardAccountRangeSource.BIN_LENGTH)
+            .takeIf {
+                it.length == CardAccountRangeSource.BIN_LENGTH
+            }
+
+        return bin?.let {
+            inMemoryCardAccountRangeSource.getAccountRange(cardNumber)
+                ?: remoteCardAccountRangeSource.getAccountRange(cardNumber)
+                ?: localCardAccountRangeSource.getAccountRange(cardNumber)
+        }
     }
 }
