5bf6ee212ce282b39a35a1c13ea31758652a11ee
Samuel Maskell <smaskell@stripe.com>
2020-09-01 17:05:37 -0700

    Show bottom sheet in MC

--- stripe/src/main/java/com/stripe/android/view/CheckoutActivity.kt
+++ stripe/src/main/java/com/stripe/android/view/CheckoutActivity.kt
@@ -1,15 +1,78 @@
 package com.stripe.android.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN
 import com.stripe.android.databinding.ActivityCheckoutBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 internal class CheckoutActivity : AppCompatActivity() {
     private val viewBinding by lazy {
         ActivityCheckoutBinding.inflate(layoutInflater)
     }
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+
+        // Handle taps outside of bottom sheet
+        viewBinding.root.setOnClickListener {
+            animateOut()
+        }
+
+        setupBottomSheet()
+    }
+
+    private fun setupBottomSheet() {
+        bottomSheetBehavior = BottomSheetBehavior.from(viewBinding.bottomSheet)
+        bottomSheetBehavior.peekHeight = BottomSheetBehavior.PEEK_HEIGHT_AUTO
+        bottomSheetBehavior.isHideable = true
+        // Start hidden and then animate in after delay
+        bottomSheetBehavior.state = STATE_HIDDEN
+
+        lifecycleScope.launch {
+            delay(ANIMATE_IN_DELAY)
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            bottomSheetBehavior.addBottomSheetCallback(
+                object : BottomSheetBehavior.BottomSheetCallback() {
+                    override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    }
+
+                    override fun onStateChanged(bottomSheet: View, newState: Int) {
+                        if (newState == STATE_HIDDEN) {
+                            finish()
+                        }
+                    }
+                }
+            )
+        }
+    }
+
+    private fun animateOut() {
+        // When the bottom sheet finishes animating to its new state,
+        // the callback will finish the activity
+        bottomSheetBehavior.state = STATE_HIDDEN
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(0, 0)
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            super.onBackPressed()
+        } else {
+            animateOut()
+        }
+    }
+
+    private companion object {
+        private const val ANIMATE_IN_DELAY = 300L
     }
 }
