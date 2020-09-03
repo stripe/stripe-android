567e0408e9df802c73c481872690a6e523eb585b
smaskell-stripe <47332718+smaskell-stripe@users.noreply.github.com>
2020-09-03 14:18:25 -0700

    Add mc payment methods view (#2794)
    * Add initial mc view model
    
    * Add empty MC payment methods list fragment
    
    * Add mc payment methods view
    
    * Review feedback

---
+++ stripe/src/main/java/com/stripe/android/view/CheckoutPaymentMethodsAdapter.kt
@@ -0,0 +1,66 @@
+package com.stripe.android.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.stripe.android.R
+import com.stripe.android.databinding.LayoutCheckoutPaymentMethodItemBinding
+import com.stripe.android.model.PaymentMethod
+import java.lang.IllegalStateException
+
+internal class CheckoutPaymentMethodsAdapter(val paymentMethods: List<PaymentMethod>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return paymentMethods[position].hashCode().toLong()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        // TODO: Support AddCard, GooglePay
+        return when (ViewType.values()[viewType]) {
+            ViewType.Card -> CardViewHolder(parent)
+            else -> throw IllegalStateException("Unsupported view type")
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return paymentMethods.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        // TODO: Support AddCard, GooglePay
+        return ViewType.Card.ordinal
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is CardViewHolder) {
+            val paymentMethod = paymentMethods[position]
+            holder.setPaymentMethod(paymentMethod)
+        }
+    }
+
+    private class CardViewHolder(private val binding: LayoutCheckoutPaymentMethodItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        constructor(parent: ViewGroup) : this(
+            LayoutCheckoutPaymentMethodItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+
+        fun setPaymentMethod(method: PaymentMethod) {
+            // TODO: Communicate error if card data not present
+            method.card?.let { card ->
+                // TODO: Get updated card brand icons
+                binding.brandIcon.setImageResource(card.brand.icon)
+                binding.cardNumber.text = itemView.context
+                    .getString(R.string.checkout_payment_method_item_card_number, card.last4)
+            }
+        }
+    }
+
+    private enum class ViewType {
+        Card,
+        AddCard,
+        GooglePay
+    }
+}
