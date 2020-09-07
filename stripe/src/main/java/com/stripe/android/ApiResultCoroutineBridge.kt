e5e3673579e39803e54daeee493cbabfdb0d240d
Tiger Oakes <contact@tigeroakes.com>
2020-09-06 22:48:15 -0700

    Add bridge class for callbacks

---
+++ stripe/src/main/java/com/stripe/android/ApiResultCoroutineBridge.kt
@@ -0,0 +1,37 @@
+package com.stripe.android
+
+import com.stripe.android.model.StripeModel
+import kotlinx.coroutines.Deferred
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+/**
+ * Generic class to bridge an API operation callback with coroutines.
+ *
+ * @param continuation [Continuation] obtained from [suspendCoroutine]
+ */
+class ApiResultCoroutineBridge<in ResultType : StripeModel>(
+    private val continuation: Continuation<ResultType>
+) : ApiResultCallback<ResultType> {
+
+    override fun onSuccess(result: ResultType) {
+        continuation.resume(result)
+    }
+
+    override fun onError(e: Exception) {
+        continuation.resumeWithException(e)
+    }
+}
+
+/**
+ * Suspend the currently running coroutine until one of the [ApiResultCallback] methods are invoked.
+ */
+suspend inline fun <ResultType : StripeModel> suspendApiResultCoroutine(
+    crossinline block: (callback: ApiResultCallback<ResultType>) -> Unit
+): ResultType {
+    return suspendCoroutine { cont ->
+        block(ApiResultCoroutineBridge(cont))
+    }
+}
---
+++ stripe/src/test/java/com/stripe/android/ApiResultCoroutineBridgeTest.kt
@@ -0,0 +1,64 @@
+package com.stripe.android
+
+import com.nhaarman.mockitokotlin2.mock
+import com.stripe.android.model.StripeModel
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineScope
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertSame
+
+@ExperimentalCoroutinesApi
+@RunWith(RobolectricTestRunner::class)
+class ApiResultCoroutineBridgeTest {
+
+    private val scope = TestCoroutineScope()
+
+    @Test
+    fun onSuccess_shouldResumeCoroutine() {
+        var callback: ApiResultCallback<StripeModel>? = null
+        val deferred = scope.async<StripeModel> {
+            suspendApiResultCoroutine { callback = it }
+        }
+
+        assertFalse(deferred.isCompleted)
+
+        val result = mock<StripeModel>()
+        callback!!.onSuccess(result)
+
+        assertTrue(deferred.isCompleted)
+
+        runBlocking {
+            assertEquals(result, deferred.await())
+        }
+    }
+
+    @Test
+    fun onError_shouldResumeCoroutineWithException() {
+        var callback: ApiResultCallback<StripeModel>? = null
+        val deferred = scope.async<StripeModel> {
+            suspendApiResultCoroutine { callback = it }
+        }
+
+        assertFalse(deferred.isCompleted)
+
+        callback!!.onError(IllegalStateException("hello world"))
+
+        assertTrue(deferred.isCompleted)
+
+        runBlocking {
+            val result = runCatching { deferred.await() }
+            val error = result.exceptionOrNull()
+            assertTrue(error is IllegalStateException)
+            assertEquals("hello world", error?.message)
+        }
+    }
+}
