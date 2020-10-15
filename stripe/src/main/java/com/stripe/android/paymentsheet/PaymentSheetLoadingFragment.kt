034b71e43a77f09bc4e139ce33931b707d8e536f
Samuel Maskell <smaskell@stripe.com>
2020-10-14 17:45:45 -0700

    Add PaymentSheetLoadingFragment

---
+++ stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetLoadingFragment.kt
@@ -0,0 +1,6 @@
+package com.stripe.android.paymentsheet
+
+import androidx.fragment.app.Fragment
+import com.stripe.android.R
+
+class PaymentSheetLoadingFragment : Fragment(R.layout.fragment_payment_sheet_loading)
