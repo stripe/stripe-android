f9d02bc7379595945acaf17c7c1ff9e2e9b4f34b
smaskell-stripe <47332718+smaskell-stripe@users.noreply.github.com>
2020-10-16 17:40:12 -0700

    Show keyboard when entering add card screen in MC (#2941)

--- stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetAddCardFragment.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetAddCardFragment.kt
@@ -2,6 +2,8 @@ package com.stripe.android.paymentsheet
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.stripe.android.R
@@ -32,6 +34,15 @@ internal class PaymentSheetAddCardFragment : Fragment(R.layout.fragment_payments
                 null
             }
             activityViewModel.updateSelection(selection)
+        }
+
+        // If we're in the full expanded mode, request focus on the card number field
+        // and show the keyboard automatically
+        if (activityViewModel.sheetMode.value == PaymentSheetViewModel.SheetMode.Full) {
+            binding.cardMultilineWidget.cardNumberEditText.requestFocus()
+            getSystemService(requireContext(), InputMethodManager::class.java)?.apply {
+                showSoftInput(binding.cardMultilineWidget.cardNumberEditText, InputMethodManager.SHOW_IMPLICIT)
+            }
         }
     }
 }
