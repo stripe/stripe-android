666169f69ad2f5d945211cf62384d55cae9fdebb
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-10-20 19:22:38 -0400

    Create BuyButton custom view for PaymentSheet (#2953)
    Custom view representing buy button in PaymentSheet
    Will handle animations

---
+++ stripe/src/main/java/com/stripe/android/paymentsheet/BuyButton.kt
@@ -0,0 +1,48 @@
+package com.stripe.android.paymentsheet
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.stripe.android.R
+import com.stripe.android.databinding.PaymentSheetBuyButtonBinding
+
+/**
+ * Buy button for PaymentSheet.
+ */
+internal class BuyButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    internal val viewBinding = PaymentSheetBuyButtonBinding.inflate(
+        LayoutInflater.from(context),
+        this
+    )
+
+    init {
+        setBackgroundResource(R.drawable.stripe_paymentsheet_buy_button_default_background)
+
+        isClickable = true
+        isEnabled = true
+    }
+
+    fun updateText(text: String) {
+        viewBinding.label.text = text
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+
+        viewBinding.label.alpha = if (isEnabled) {
+            ALPHA_ENABLED
+        } else {
+            ALPHA_DISABLED
+        }
+    }
+
+    private companion object {
+        private const val ALPHA_ENABLED = 1.0f
+        private const val ALPHA_DISABLED = 0.5f
+    }
+}
