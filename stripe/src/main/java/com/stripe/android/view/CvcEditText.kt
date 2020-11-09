2866508a289a62db0752f963fae0f9c802a1e921
Shengwei Wu <shengwei@stripe.com>
2020-11-09 10:49:44 +0800

    Fix CVC not masked like password issue
    Remove extra empty line

--- stripe/src/main/java/com/stripe/android/view/CvcEditText.kt
+++ stripe/src/main/java/com/stripe/android/view/CvcEditText.kt
@@ -3,7 +3,8 @@ package com.stripe.android.view
 import android.content.Context
 import android.os.Build
 import android.text.InputFilter
-import android.text.InputType
+import android.text.InputType.TYPE_CLASS_NUMBER
+import android.text.InputType.TYPE_NUMBER_VARIATION_PASSWORD
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.widget.doAfterTextChanged
@@ -52,7 +53,7 @@ class CvcEditText @JvmOverloads constructor(
         maxLines = 1
         filters = createFilters(CardBrand.Unknown)
 
-        inputType = InputType.TYPE_CLASS_NUMBER
+        inputType = TYPE_CLASS_NUMBER or TYPE_NUMBER_VARIATION_PASSWORD
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             setAutofillHints(View.AUTOFILL_HINT_CREDIT_CARD_SECURITY_CODE)
