46c1dfdccef02840010f003b6b17fcabce49d81b
Michael Shafrir <mshafrir@stripe.com>
2020-11-18 15:19:33 -0500

    Create PaymentOptionsViewModel.TransitionTarget

--- stripe/src/main/java/com/stripe/android/paymentsheet/PaymentOptionsViewModel.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/PaymentOptionsViewModel.kt
@@ -16,9 +16,32 @@ internal class PaymentOptionsViewModel(
 ) : ViewModel() {
     private val mutableError = MutableLiveData<Throwable>()
     private val mutableSheetMode = MutableLiveData<SheetMode>()
+    private val mutableTransition = MutableLiveData<TransitionTarget>()
 
     internal val error: LiveData<Throwable> = mutableError
     internal val sheetMode: LiveData<SheetMode> = mutableSheetMode.distinctUntilChanged()
+    internal val transition: LiveData<TransitionTarget> = mutableTransition
+
+    fun transitionTo(target: TransitionTarget) {
+        mutableTransition.postValue(target)
+    }
+
+    fun updateMode(mode: SheetMode) {
+        mutableSheetMode.postValue(mode)
+    }
+
+    internal enum class TransitionTarget(
+        val sheetMode: SheetMode
+    ) {
+        // User has saved PM's and is selected
+        SelectSavedPaymentMethod(SheetMode.Wrapped),
+
+        // User has saved PM's and is adding a new one
+        AddPaymentMethodFull(SheetMode.Full),
+
+        // User has no saved PM's
+        AddPaymentMethodSheet(SheetMode.FullCollapsed)
+    }
 
     internal class Factory(
         private val applicationSupplier: () -> Application,
