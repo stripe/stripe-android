3ff0513ee38196bc26fa634829451ff2e1db3c9e
Michael Shafrir <mshafrir@stripe.com>
2020-11-18 15:01:11 -0500

    Implement basic UI interactions in PaymentOptionsActivity
    This is mostly reproducing `PaymentSheetActivity`

--- stripe/src/main/java/com/stripe/android/paymentsheet/PaymentOptionsActivity.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/PaymentOptionsActivity.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.stripe.android.databinding.StripeActivityPaymentOptionsBinding
 import com.stripe.android.paymentsheet.ui.BasePaymentSheetActivity
+import com.stripe.android.paymentsheet.ui.SheetMode
+import com.stripe.android.paymentsheet.ui.Toolbar
 
 /**
  * An `Activity` for selecting a payment option.
@@ -59,6 +61,55 @@ internal class PaymentOptionsActivity : BasePaymentSheetActivity<PaymentOptionRe
         }
 
         setContentView(viewBinding.root)
+
+        // Handle taps outside of bottom sheet
+        viewBinding.root.setOnClickListener {
+            onUserCancel()
+        }
+        viewModel.error.observe(this) {
+            animateOut(
+                PaymentOptionResult.Failed(it)
+            )
+        }
+        viewModel.sheetMode.observe(this) { mode ->
+            when (mode) {
+                SheetMode.Full,
+                SheetMode.FullCollapsed -> {
+                    viewBinding.toolbar.showBack()
+                }
+                SheetMode.Wrapped -> {
+                    viewBinding.toolbar.showClose()
+                }
+                else -> {
+                    // mode == null
+                }
+            }
+
+            viewBinding.bottomSheet.layoutParams = viewBinding.bottomSheet.layoutParams.apply {
+                height = mode.height
+            }
+
+            bottomSheetController.updateState(mode)
+        }
+        bottomSheetController.shouldFinish.observe(this) { shouldFinish ->
+            if (shouldFinish) {
+                finish()
+            }
+        }
+        bottomSheetController.setup()
+
+        viewBinding.toolbar.action.observe(this) { action ->
+            if (action != null) {
+                when (action) {
+                    Toolbar.Action.Close -> {
+                        onUserCancel()
+                    }
+                    Toolbar.Action.Back -> {
+                        // TODO: implement back
+                    }
+                }
+            }
+        }
     }
 
     override fun setActivityResult(result: PaymentOptionResult) {
