a63eef0083dda4ffc6102a19ba5fc4cdba1ae88e
Michael Shafrir <mshafrir@stripe.com>
2020-11-23 16:00:47 -0500

    Delete Either class

--- stripe/src/main/java/com/stripe/android/utils/Either.kt
+++
@@ -1,6 +0,0 @@
-package com.stripe.android.utils
-
-sealed class Either<A, B> {
-    data class Left<A, B>(val left: A) : Either<A, B>()
-    data class Right<A, B>(val right: B) : Either<A, B>()
-}
