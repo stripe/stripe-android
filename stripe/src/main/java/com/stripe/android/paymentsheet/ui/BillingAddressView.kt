10dcfb325ff7d8e6d53beb1b12a00e80aa957456
Michael Shafrir <mshafrir@stripe.com>
2020-11-24 15:40:18 -0500

    Update country autocomplete logic

--- stripe/src/main/java/com/stripe/android/paymentsheet/ui/BillingAddressView.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/ui/BillingAddressView.kt
@@ -3,8 +3,10 @@ package com.stripe.android.paymentsheet.ui
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import androidx.annotation.VisibleForTesting
 import androidx.core.content.ContextCompat
 import androidx.core.os.ConfigurationCompat
 import androidx.core.view.ViewCompat
@@ -13,6 +15,9 @@ import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.ShapeAppearanceModel
 import com.stripe.android.R
 import com.stripe.android.databinding.StripeBillingAddressLayoutBinding
+import com.stripe.android.view.Country
+import com.stripe.android.view.CountryAdapter
+import com.stripe.android.view.CountryAutoCompleteTextViewValidator
 import com.stripe.android.view.CountryUtils
 
 internal class BillingAddressView @JvmOverloads constructor(
@@ -25,8 +30,17 @@ internal class BillingAddressView @JvmOverloads constructor(
         this
     )
 
+    private val countryAdapter = CountryAdapter(
+        context,
+        CountryUtils.getOrderedCountries(
+            ConfigurationCompat.getLocales(context.resources.configuration)[0]
+        )
+    )
+
     internal val countryView = viewBinding.country
     private val postalCodeView = viewBinding.postalCode
+
+    private var selectedCountry: Country? = null
 
     init {
         orientation = VERTICAL
@@ -55,13 +69,58 @@ internal class BillingAddressView @JvmOverloads constructor(
     }
 
     private fun configureCountryAutoComplete() {
-        val countries = CountryUtils.getOrderedCountries(
-            ConfigurationCompat.getLocales(context.resources.configuration)[0]
-        ).map { it.name }
-
-        val adapter = ArrayAdapter(context, R.layout.stripe_country_dropdown_item, countries)
         countryView.threshold = 0
-        countryView.setAdapter(adapter)
-        countryView.setText(countries.first())
+        countryView.setAdapter(countryAdapter)
+        countryView.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                updatedSelectedCountryCode(countryAdapter.getItem(position))
+            }
+        countryView.onFocusChangeListener = OnFocusChangeListener { _, focused ->
+            if (focused) {
+                countryView.showDropDown()
+            } else {
+                val countryEntered = countryView.text.toString()
+                updateUiForCountryEntered(countryEntered)
+            }
+        }
+
+        selectedCountry = countryAdapter.firstItem
+        updateInitialCountry()
+
+        countryView.validator = CountryAutoCompleteTextViewValidator(
+            countryAdapter
+        ) { country ->
+            selectedCountry = country
+        }
+    }
+
+    private fun updateInitialCountry() {
+        val initialCountry = countryAdapter.firstItem
+        countryView.setText(initialCountry.name)
+        selectedCountry = initialCountry
+    }
+
+    @VisibleForTesting
+    internal fun updateUiForCountryEntered(displayCountryEntered: String) {
+        val country = CountryUtils.getCountryByName(displayCountryEntered)
+
+        // If the user-typed country matches a valid country, update the selected country
+        // Otherwise, revert back to last valid country if country is not recognized.
+        val displayCountry = country?.let {
+            updatedSelectedCountryCode(it)
+            displayCountryEntered
+        } ?: selectedCountry?.name
+
+        countryView.setText(displayCountry)
+    }
+
+    private fun updatedSelectedCountryCode(country: Country) {
+        if (selectedCountry != country) {
+            selectedCountry = country
+        }
+    }
+
+    internal fun validateCountry() {
+        countryView.performValidation()
     }
 }
