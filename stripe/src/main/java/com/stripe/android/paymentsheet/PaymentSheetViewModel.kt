a6d65211637d58902b732c9eda26563712c101a5
Michael Shafrir <mshafrir@stripe.com>
2020-12-01 16:06:29 -0500

    Add GooglePayButton to PaymentSheetAddCardFragment
    - Show `GooglePayButton` and `GooglePayDivider` instead of header if
      Google Pay is enabled and there are no saved payment methods.
    - Create `AddPaymentMethodConfig` model for configuration data needed by
      `PaymentSheetAddCardFragment`.
    - Move data fetching logic from `PaymentSheetLoadingFragment` to
      `PaymentSheetActivity`.

--- stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetViewModel.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetViewModel.kt
@@ -5,9 +5,11 @@ import android.app.Application
 import android.content.Intent
 import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.stripe.android.ApiResultCallback
@@ -21,6 +23,7 @@ import com.stripe.android.model.PaymentMethod
 import com.stripe.android.networking.ApiRequest
 import com.stripe.android.networking.StripeApiRepository
 import com.stripe.android.networking.StripeRepository
+import com.stripe.android.paymentsheet.model.AddPaymentMethodConfig
 import com.stripe.android.paymentsheet.model.ConfirmParamsFactory
 import com.stripe.android.paymentsheet.model.ViewState
 import com.stripe.android.paymentsheet.ui.SheetMode
@@ -48,6 +51,41 @@ internal class PaymentSheetViewModel internal constructor(
 
     private val mutablePaymentIntent = MutableLiveData<PaymentIntent?>()
     internal val paymentIntent: LiveData<PaymentIntent?> = mutablePaymentIntent
+
+    private val mutableIsGooglePayReady = MutableLiveData<Boolean>()
+    internal val isGooglePayReady: LiveData<Boolean> = mutableIsGooglePayReady.distinctUntilChanged()
+
+    fun fetchAddPaymentMethodConfig() = liveData {
+        emitSource(
+            MediatorLiveData<AddPaymentMethodConfig?>().also { configLiveData ->
+                listOf(paymentIntent, paymentMethods, isGooglePayReady).forEach { source ->
+                    configLiveData.addSource(source) {
+                        configLiveData.value = createAddPaymentMethodConfig()
+                    }
+                }
+            }.distinctUntilChanged()
+        )
+    }
+
+    private fun createAddPaymentMethodConfig(): AddPaymentMethodConfig? {
+        val paymentIntentValue = paymentIntent.value
+        val paymentMethodsValue = paymentMethods.value
+        val isGooglePayReadyValue = isGooglePayReady.value
+
+        return if (
+            paymentIntentValue != null &&
+            paymentMethodsValue != null &&
+            isGooglePayReadyValue != null
+        ) {
+            AddPaymentMethodConfig(
+                paymentIntent = paymentIntentValue,
+                paymentMethods = paymentMethodsValue,
+                isGooglePayReady = isGooglePayReadyValue
+            )
+        } else {
+            null
+        }
+    }
 
     fun updatePaymentMethods() {
         when (args) {
@@ -146,8 +184,14 @@ internal class PaymentSheetViewModel internal constructor(
         }
     }
 
-    fun isGooglePayReady() = liveData {
-        emit(googlePayRepository.isReady().filterNotNull().first())
+    fun fetchIsGooglePayReady() {
+        viewModelScope.launch {
+            withContext(workContext) {
+                mutableIsGooglePayReady.postValue(
+                    googlePayRepository.isReady().filterNotNull().first()
+                )
+            }
+        }
     }
 
     @VisibleForTesting
@@ -161,7 +205,7 @@ internal class PaymentSheetViewModel internal constructor(
         stripeAccountId: String? = this.stripeAccountId
     ) {
         viewModelScope.launch {
-            val result = withContext(workContext) {
+            withContext(workContext) {
                 runCatching {
                     stripeRepository.getPaymentMethods(
                         ListPaymentMethodsParams(
@@ -173,8 +217,7 @@ internal class PaymentSheetViewModel internal constructor(
                         ApiRequest.Options(ephemeralKey, stripeAccountId)
                     )
                 }
-            }
-            result.fold(
+            }.fold(
                 onSuccess = this@PaymentSheetViewModel::setPaymentMethods,
                 onFailure = {
                     onError(it)
