5cd080aa446061b4394ae4b5d6ce19055b085cfc
Michael Shafrir <mshafrir@stripe.com>
2020-12-03 12:18:24 -0500

    PaymentSheet requires PaymentIntent with automatic confirmation

--- stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetViewModel.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetViewModel.kt
@@ -117,23 +117,36 @@ internal class PaymentSheetViewModel internal constructor(
                 }
             }
             result.fold(
-                onSuccess = { paymentIntent ->
-                    mutablePaymentIntent.value = paymentIntent
-
-                    val amount = paymentIntent.amount
-                    val currencyCode = paymentIntent.currency
-                    if (amount != null && currencyCode != null) {
-                        mutableViewState.value = ViewState.Ready(amount, currencyCode)
-                    } else {
-                        // TODO(mshafrir-stripe): improve error message
-                        onError(IllegalStateException("PaymentIntent is invalid."))
-                    }
-                },
+                onSuccess = ::onPaymentIntentResponse,
                 onFailure = {
                     mutablePaymentIntent.value = null
 
                     onError(it)
                 }
+            )
+        }
+    }
+
+    private fun onPaymentIntentResponse(paymentIntent: PaymentIntent) {
+        if (paymentIntent.confirmationMethod == PaymentIntent.ConfirmationMethod.Automatic) {
+            mutablePaymentIntent.value = paymentIntent
+
+            val amount = paymentIntent.amount
+            val currencyCode = paymentIntent.currency
+            if (amount != null && currencyCode != null) {
+                mutableViewState.value = ViewState.Ready(amount, currencyCode)
+            } else {
+                // TODO(mshafrir-stripe): improve error message
+                onError(IllegalStateException("PaymentIntent is invalid."))
+            }
+        } else {
+            onError(
+                IllegalArgumentException(
+                    """
+                    PaymentIntent with confirmation_method='automatic' is required.
+                    See https://stripe.com/docs/api/payment_intents/object#payment_intent_object-confirmation_method.
+                    """.trimIndent()
+                )
             )
         }
     }
