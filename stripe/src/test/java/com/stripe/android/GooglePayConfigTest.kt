42f16986e5db77487a427fc63347edea10bd24cf
Michael Shafrir <mshafrir@stripe.com>
2020-12-11 12:25:46 -0500

    Update GooglePayConfig constructor
    Extract `connectedAccountId` from `PaymentConfiguration` if using
    constructor with `Context`.

--- stripe/src/test/java/com/stripe/android/GooglePayConfigTest.kt
+++ stripe/src/test/java/com/stripe/android/GooglePayConfigTest.kt
@@ -1,10 +1,10 @@
 package com.stripe.android
 
 import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 @RunWith(RobolectricTestRunner::class)
 class GooglePayConfigTest {
@@ -19,18 +19,12 @@ class GooglePayConfigTest {
             GooglePayConfig(ApplicationProvider.getApplicationContext())
                 .tokenizationSpecification
         val params = tokenizationSpec.getJSONObject("parameters")
-        assertEquals(
-            "stripe",
-            params.getString("gateway")
-        )
-        assertEquals(
-            ApiVersion.get().code,
-            params.getString("stripe:version")
-        )
-        assertEquals(
-            ApiKeyFixtures.FAKE_PUBLISHABLE_KEY,
-            params.getString("stripe:publishableKey")
-        )
+        assertThat(params.getString("gateway"))
+            .isEqualTo("stripe")
+        assertThat(params.getString("stripe:version"))
+            .isEqualTo(ApiVersion.get().code)
+        assertThat(params.getString("stripe:publishableKey"))
+            .isEqualTo(ApiKeyFixtures.FAKE_PUBLISHABLE_KEY)
     }
 
     @Test
@@ -38,17 +32,11 @@ class GooglePayConfigTest {
         val tokenizationSpec = GooglePayConfig(ApiKeyFixtures.FAKE_PUBLISHABLE_KEY, "acct_1234")
             .tokenizationSpecification
         val params = tokenizationSpec.getJSONObject("parameters")
-        assertEquals(
-            "stripe",
-            params.getString("gateway")
-        )
-        assertEquals(
-            ApiVersion.get().code,
-            params.getString("stripe:version")
-        )
-        assertEquals(
-            "pk_test_123/acct_1234",
-            params.getString("stripe:publishableKey")
-        )
+        assertThat(params.getString("gateway"))
+            .isEqualTo("stripe")
+        assertThat(params.getString("stripe:version"))
+            .isEqualTo(ApiVersion.get().code)
+        assertThat(params.getString("stripe:publishableKey"))
+            .isEqualTo("pk_test_123/acct_1234")
     }
 }
