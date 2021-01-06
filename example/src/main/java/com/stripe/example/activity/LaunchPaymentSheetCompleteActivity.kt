0eb9ba94ba59f576dd7f998d0629c66fc0609f3f
Michael Shafrir <mshafrir@stripe.com>
2021-01-06 16:26:06 -0500

    Create preferences screen to configure payment sheet

--- example/src/main/java/com/stripe/example/activity/LaunchPaymentSheetCompleteActivity.kt
+++ example/src/main/java/com/stripe/example/activity/LaunchPaymentSheetCompleteActivity.kt
@@ -1,13 +1,17 @@
 package com.stripe.example.activity
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import com.stripe.android.paymentsheet.PaymentResult
 import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.example.R
 import com.stripe.example.databinding.ActivityPaymentSheetCompleteBinding
-import com.stripe.example.paymentsheet.EphemeralKey
 import com.stripe.example.paymentsheet.PaymentSheetViewModel
 
 class LaunchPaymentSheetCompleteActivity : AppCompatActivity() {
@@ -22,7 +26,9 @@ class LaunchPaymentSheetCompleteActivity : AppCompatActivity() {
         )
     }
 
-    private lateinit var ephemeralKey: EphemeralKey
+    private val prefsManager: SharedPreferences by lazy {
+        PreferenceManager.getDefaultSharedPreferences(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,46 +39,101 @@ class LaunchPaymentSheetCompleteActivity : AppCompatActivity() {
         viewModel.inProgress.observe(this) {
             viewBinding.progressBar.visibility = if (it) View.VISIBLE else View.INVISIBLE
             viewBinding.launch.isEnabled = !it
-            viewBinding.clear.isEnabled = !it
         }
         viewModel.status.observe(this) {
             viewBinding.status.text = it
         }
 
-        viewBinding.clear.setOnClickListener {
+        viewBinding.launch.setOnClickListener {
+            val isCustomerEnabled = prefsManager.getBoolean("enable_customer", true)
+
+            if (isCustomerEnabled) {
+                fetchEphemeralKey { customerConfig ->
+                    createPaymentIntent(paymentSheet, customerConfig)
+                }
+            } else {
+                createPaymentIntent(paymentSheet, null)
+            }
+        }
+    }
+
+    private fun createPaymentIntent(
+        paymentSheet: PaymentSheet,
+        customerConfig: PaymentSheet.CustomerConfiguration?
+    ) {
+        viewModel.createPaymentIntent(
+            "us",
+            customerId = customerConfig?.id
+        ).observe(this) {
+            it.fold(
+                onSuccess = { json ->
+                    val clientSecret = json.getString("secret")
+
+                    onPaymentIntent(
+                        paymentSheet,
+                        clientSecret,
+                        customerConfig
+                    )
+                },
+                onFailure = {
+                    viewModel.status.postValue(viewModel.status.value + "\nFailed: ${it.message}")
+                }
+            )
+        }
+    }
+
+    private fun onPaymentIntent(
+        paymentSheet: PaymentSheet,
+        paymentIntentClientSecret: String,
+        customerConfig: PaymentSheet.CustomerConfiguration?
+    ) {
+        viewModel.inProgress.postValue(false)
+
+        val isGooglePayEnabled = prefsManager.getBoolean("enable_googlepay", true)
+        val merchantName = prefsManager.getString("merchant_name", null) ?: "Widget Store"
+        val billingAddressCollection = when (prefsManager.getBoolean("require_billing_address", false)) {
+            true -> PaymentSheet.BillingAddressCollectionLevel.Required
+            false -> PaymentSheet.BillingAddressCollectionLevel.Automatic
+        }
+
+        paymentSheet.present(
+            paymentIntentClientSecret,
+            PaymentSheet.Configuration(
+                merchantDisplayName = merchantName,
+                customer = customerConfig,
+                googlePay = when (isGooglePayEnabled) {
+                    true -> {
+                        PaymentSheet.GooglePayConfiguration(
+                            environment = PaymentSheet.GooglePayConfiguration.Environment.Test,
+                            countryCode = "US"
+                        )
+                    }
+                    false -> null
+                },
+
+                billingAddressCollection = billingAddressCollection
+            )
+        )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.payment_sheet, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.config) {
+            PaymentSheetConfigBottomSheet().show(
+                supportFragmentManager,
+                PaymentSheetConfigBottomSheet.TAG
+            )
+            return true
+        } else if (item.itemId == R.id.refresh_key) {
             viewModel.clearKeys()
             fetchEphemeralKey()
         }
-        viewBinding.launch.setOnClickListener {
-            viewModel.createPaymentIntent("us", ephemeralKey.customer).observe(this) {
-                it.fold(
-                    onSuccess = { json ->
-                        viewModel.inProgress.postValue(false)
-                        val secret = json.getString("secret")
-                        paymentSheet.present(
-                            secret,
-                            PaymentSheet.Configuration(
-                                merchantDisplayName = "Widget Store",
-                                customer = PaymentSheet.CustomerConfiguration(
-                                    id = ephemeralKey.customer,
-                                    ephemeralKeySecret = ephemeralKey.key
-                                ),
-                                googlePay = PaymentSheet.GooglePayConfiguration(
-                                    environment = PaymentSheet.GooglePayConfiguration.Environment.Test,
-                                    countryCode = "US"
-                                ),
-                                billingAddressCollection = PaymentSheet.BillingAddressCollectionLevel.Automatic
-                            )
-                        )
-                    },
-                    onFailure = {
-                        viewModel.status.postValue(viewModel.status.value + "\nFailed: ${it.message}")
-                    }
-                )
-            }
-        }
-
-        fetchEphemeralKey()
+        return super.onOptionsItemSelected(item)
     }
 
     private fun onPaymentSheetResult(
@@ -92,11 +153,18 @@ class LaunchPaymentSheetCompleteActivity : AppCompatActivity() {
         viewModel.status.value = viewModel.status.value + "\n\n$statusString"
     }
 
-    private fun fetchEphemeralKey() {
+    private fun fetchEphemeralKey(
+        onSuccess: (PaymentSheet.CustomerConfiguration) -> Unit = {}
+    ) {
         viewModel.fetchEphemeralKey()
             .observe(this) { newEphemeralKey ->
                 if (newEphemeralKey != null) {
-                    ephemeralKey = newEphemeralKey
+                    onSuccess(
+                        PaymentSheet.CustomerConfiguration(
+                            id = newEphemeralKey.customer,
+                            ephemeralKeySecret = newEphemeralKey.key
+                        )
+                    )
                 }
             }
     }
--- example/src/main/java/com/stripe/example/activity/PaymentSheetConfigBottomSheet.kt
+++ example/src/main/java/com/stripe/example/activity/PaymentSheetConfigBottomSheet.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.stripe.example.R
 import com.stripe.example.databinding.PaymentSheetConfigBottomSheetBinding
 
 class PaymentSheetConfigBottomSheet : BottomSheetDialogFragment() {
@@ -26,5 +28,23 @@ class PaymentSheetConfigBottomSheet : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        childFragmentManager
+            .beginTransaction()
+            .replace(
+                viewBinding.fragmentContainer.id,
+                PreferenceFragment()
+            )
+            .commit()
+    }
+
+    internal class PreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.payment_sheet_preferences, rootKey)
+        }
+    }
+
+    internal companion object {
+        const val TAG = "PaymentSheetConfigBottomSheet"
     }
 }
