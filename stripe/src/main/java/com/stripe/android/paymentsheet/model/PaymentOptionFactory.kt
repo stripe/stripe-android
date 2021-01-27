ef727bba23bb02c9c6ef3b1345779c37ce3c899f
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2021-01-27 14:16:12 -0500

    Update PaymentOptionFactory for saved cards (#3298)
    Use last 4 instead of brand name as label

--- stripe/src/main/java/com/stripe/android/paymentsheet/model/PaymentOptionFactory.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/model/PaymentOptionFactory.kt
@@ -1,10 +1,13 @@
 package com.stripe.android.paymentsheet.model
 
+import android.content.res.Resources
 import com.stripe.android.R
 import com.stripe.android.model.CardBrand
 import com.stripe.android.model.PaymentMethod
 
-internal class PaymentOptionFactory {
+internal class PaymentOptionFactory(
+    private val resources: Resources
+) {
     fun create(selection: PaymentSelection): PaymentOption? {
         return when (selection) {
             PaymentSelection.GooglePay -> {
@@ -17,11 +20,10 @@ internal class PaymentOptionFactory {
             is PaymentSelection.Saved -> {
                 when (selection.paymentMethod.type) {
                     PaymentMethod.Type.Card -> {
-                        val brand = selection.paymentMethod.card?.brand
-                        brand?.let {
+                        selection.paymentMethod.card?.let { card ->
                             PaymentOption(
-                                drawableResourceId = it.icon,
-                                label = it.displayName
+                                drawableResourceId = getIcon(card.brand),
+                                label = createCardLabel(card.last4)
                             )
                         }
                     }
@@ -34,19 +36,30 @@ internal class PaymentOptionFactory {
             is PaymentSelection.New.Card -> {
                 val brand = selection.brand
                 PaymentOption(
-                    drawableResourceId = when (brand) {
-                        CardBrand.Visa -> R.drawable.stripe_ic_paymentsheet_card_visa
-                        CardBrand.AmericanExpress -> R.drawable.stripe_ic_paymentsheet_card_amex
-                        CardBrand.Discover -> R.drawable.stripe_ic_paymentsheet_card_discover
-                        CardBrand.JCB -> R.drawable.stripe_ic_paymentsheet_card_jcb
-                        CardBrand.DinersClub -> R.drawable.stripe_ic_paymentsheet_card_dinersclub
-                        CardBrand.MasterCard -> R.drawable.stripe_ic_paymentsheet_card_mastercard
-                        CardBrand.UnionPay -> R.drawable.stripe_ic_paymentsheet_card_unionpay
-                        CardBrand.Unknown -> R.drawable.stripe_ic_paymentsheet_card_unknown
-                    },
-                    label = brand.displayName
+                    drawableResourceId = getIcon(brand),
+                    label = createCardLabel(selection.paymentMethodCreateParams.card?.last4)
                 )
             }
         }
+    }
+
+    private fun createCardLabel(last4: String?): String {
+        return last4?.let {
+            resources.getString(
+                R.string.paymentsheet_payment_method_item_card_number,
+                last4
+            )
+        }.orEmpty()
+    }
+
+    private fun getIcon(brand: CardBrand) = when (brand) {
+        CardBrand.Visa -> R.drawable.stripe_ic_paymentsheet_card_visa
+        CardBrand.AmericanExpress -> R.drawable.stripe_ic_paymentsheet_card_amex
+        CardBrand.Discover -> R.drawable.stripe_ic_paymentsheet_card_discover
+        CardBrand.JCB -> R.drawable.stripe_ic_paymentsheet_card_jcb
+        CardBrand.DinersClub -> R.drawable.stripe_ic_paymentsheet_card_dinersclub
+        CardBrand.MasterCard -> R.drawable.stripe_ic_paymentsheet_card_mastercard
+        CardBrand.UnionPay -> R.drawable.stripe_ic_paymentsheet_card_unionpay
+        CardBrand.Unknown -> R.drawable.stripe_ic_paymentsheet_card_unknown
     }
 }
