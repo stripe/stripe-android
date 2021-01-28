f1e022ea52f399322e3cfc5b1ba41fbffddc13ac
Michael Shafrir <mshafrir@stripe.com>
2021-01-28 11:52:05 -0500

    Cleanup TestFocusChangeListener

--- stripe/src/test/java/com/stripe/android/testharness/TestFocusChangeListener.kt
+++ stripe/src/test/java/com/stripe/android/testharness/TestFocusChangeListener.kt
@@ -2,25 +2,16 @@ package com.stripe.android.testharness
 
 import android.view.View
 import android.view.ViewTreeObserver
-import androidx.annotation.IdRes
 
 /**
  * Test class that listens for global focus changes and stores the transition.
  */
-class TestFocusChangeListener : ViewTreeObserver.OnGlobalFocusChangeListener {
-    private var oldFocus: View? = null
-    private var newFocus: View? = null
-
-    val oldFocusId: Int
-        @IdRes
-        get() = oldFocus?.id ?: 0
-
-    val newFocusId: Int
-        @IdRes
-        get() = newFocus?.id ?: 0
+internal class TestFocusChangeListener : ViewTreeObserver.OnGlobalFocusChangeListener {
+    var oldFocusId = 0
+    var newFocusId = 0
 
     override fun onGlobalFocusChanged(oldFocus: View?, newFocus: View?) {
-        this.oldFocus = oldFocus
-        this.newFocus = newFocus
+        oldFocusId = oldFocus?.id ?: 0
+        newFocusId = newFocus?.id ?: 0
     }
 }
