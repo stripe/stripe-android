f2d9fe68a8ba69b37597d5e07405a5b33fd08f68
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2021-02-01 16:11:35 -0500

    Use Robolectric's idleMainLooper() (#3321)

--- stripe/src/test/java/com/stripe/android/utils/TestUtils.kt
+++ stripe/src/test/java/com/stripe/android/utils/TestUtils.kt
@@ -1,13 +1,12 @@
 package com.stripe.android.utils
 
-import android.os.Looper
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import org.robolectric.Shadows
+import org.robolectric.shadows.ShadowLooper.idleMainLooper
 
 internal object TestUtils {
     @JvmStatic
-    fun idleLooper() = Shadows.shadowOf(Looper.getMainLooper()).idle()
+    fun idleLooper() = idleMainLooper()
 
     fun viewModelFactoryFor(viewModel: ViewModel) = object : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
