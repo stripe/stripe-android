2284e851bd34998bc4c4a0a417110c78ddbdc210
Michael Shafrir <mshafrir@stripe.com>
2021-02-04 12:14:03 -0500

    Update PaymentOption
    Make public and add comments.

--- stripe/src/main/java/com/stripe/android/paymentsheet/model/PaymentOption.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/model/PaymentOption.kt
@@ -2,7 +2,19 @@ package com.stripe.android.paymentsheet.model
 
 import androidx.annotation.DrawableRes
 
-internal data class PaymentOption(
+/**
+ * The customer's selected payment option.
+ */
+data class PaymentOption(
+    /**
+     * The drawable resource id of the icon that represents the payment option.
+     */
     @DrawableRes val drawableResourceId: Int,
+
+    /**
+     * A label that describes the payment option.
+     *
+     * For example, "····4242" for a Visa ending in 4242.
+     */
     val label: String
 )
