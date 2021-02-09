aa741eb5fecca2117e362b0bda3966dbadc4f5fb
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2021-02-09 11:02:17 -0500

    Update payment sheet examples (#3360)

--- example/src/main/java/com/stripe/example/activity/BasePaymentSheetActivity.kt
+++ example/src/main/java/com/stripe/example/activity/BasePaymentSheetActivity.kt
@@ -7,6 +7,8 @@ import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
+import com.stripe.android.paymentsheet.PaymentResult
+import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.example.R
 import com.stripe.example.paymentsheet.PaymentSheetViewModel
 
@@ -28,6 +30,27 @@ internal abstract class BasePaymentSheetActivity : AppCompatActivity() {
     protected val isCustomerEnabled: Boolean
         get() = prefsManager.getBoolean("enable_customer", true)
 
+    protected val billingAddressCollection: PaymentSheet.BillingAddressCollectionLevel
+        get() {
+            return when (prefsManager.getBoolean("require_billing_address", false)) {
+                true -> PaymentSheet.BillingAddressCollectionLevel.Required
+                false -> PaymentSheet.BillingAddressCollectionLevel.Automatic
+            }
+        }
+
+    protected val googlePayConfig: PaymentSheet.GooglePayConfiguration?
+        get() {
+            return when (prefsManager.getBoolean("enable_googlepay", true)) {
+                true -> {
+                    PaymentSheet.GooglePayConfiguration(
+                        environment = PaymentSheet.GooglePayConfiguration.Environment.Test,
+                        countryCode = "US"
+                    )
+                }
+                false -> null
+            }
+        }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.payment_sheet, menu)
@@ -46,6 +69,38 @@ internal abstract class BasePaymentSheetActivity : AppCompatActivity() {
             onRefreshEphemeralKey()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    protected fun fetchEphemeralKey(
+        onSuccess: (PaymentSheet.CustomerConfiguration) -> Unit = {}
+    ) {
+        viewModel.fetchEphemeralKey()
+            .observe(this) { newEphemeralKey ->
+                if (newEphemeralKey != null) {
+                    onSuccess(
+                        PaymentSheet.CustomerConfiguration(
+                            id = newEphemeralKey.customer,
+                            ephemeralKeySecret = newEphemeralKey.key
+                        )
+                    )
+                }
+            }
+    }
+
+    protected fun onPaymentSheetResult(
+        paymentResult: PaymentResult
+    ) {
+        viewModel.status.value = paymentResult.toString()
+    }
+
+    protected fun onError(error: Throwable) {
+        viewModel.status.postValue(
+            """
+            ${viewModel.status.value}
+            
+            Failed: ${error.message}
+            """.trimIndent()
+        )
     }
 
     abstract fun onRefreshEphemeralKey()
