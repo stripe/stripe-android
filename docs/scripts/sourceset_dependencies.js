a0924b70c105837c85cddb906d0b830c5135a3c1
Michelle Brubaker <michelleb@stripe.com>
2021-02-24 07:19:00 -0500

    Update documentation.

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":stripe/main": []}'
+sourceset_dependencies = '{":stripe:dokkaHtml/androidTestRelease": [], ":stripe:dokkaHtml/debug": [], ":stripe:dokkaHtml/main": [], ":stripe:dokkaHtml/release": []}'
