592d8dd371500a1fcb813d69927b9f26d12baae1
Michael Shafrir <mshafrir@stripe.com>
2021-02-25 08:28:59 -0500

    Increase timeout for end-to-end tests

--- stripe-test-e2e/src/test/java/com/stripe/android/test/e2e/ServiceFactory.kt
+++ stripe-test-e2e/src/test/java/com/stripe/android/test/e2e/ServiceFactory.kt
@@ -1,13 +1,19 @@
 package com.stripe.android.test.e2e
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ServiceFactory {
 
     fun create(
         baseUrl: String
     ): Service {
+        OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build()
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(MoshiConverterFactory.create())
