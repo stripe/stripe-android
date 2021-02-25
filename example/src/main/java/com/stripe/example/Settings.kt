b0701f124ef3bb7620059f9688dd8f2feef031d9
Michael Shafrir <mshafrir@stripe.com>
2021-02-25 15:42:37 -0500

    Test

--- example/src/main/java/com/stripe/example/Settings.kt
+++ example/src/main/java/com/stripe/example/Settings.kt
@@ -9,23 +9,23 @@ import android.content.pm.PackageManager
  */
 class Settings(context: Context) {
     private val appContext = context.applicationContext
-    private val backendMetadata = getMetadata(METADATA_KEY_BACKEND_URL_KEY)
-    private val publishableKeyMetadata = getMetadata(METADATA_KEY_PUBLISHABLE_KEY)
-    private val stripeAccountIdMetadata = getMetadata(METADATA_KEY_STRIPE_ACCOUNT_ID)
+//    private val backendMetadata = getMetadata(METADATA_KEY_BACKEND_URL_KEY)
+//    private val publishableKeyMetadata = getMetadata(METADATA_KEY_PUBLISHABLE_KEY)
+//    private val stripeAccountIdMetadata = getMetadata(METADATA_KEY_STRIPE_ACCOUNT_ID)
 
     val backendUrl: String
         get() {
-            return backendMetadata ?: BASE_URL
+            return BASE_URL
         }
 
     val publishableKey: String
         get() {
-            return publishableKeyMetadata ?: PUBLISHABLE_KEY
+            return PUBLISHABLE_KEY
         }
 
     val stripeAccountId: String?
         get() {
-            return stripeAccountIdMetadata ?: STRIPE_ACCOUNT_ID
+            return STRIPE_ACCOUNT_ID
         }
 
     private fun getMetadata(key: String): String? {
@@ -47,14 +47,14 @@ class Settings(context: Context) {
          * [example-mobile-backend](https://github.com/stripe/example-mobile-backend),
          * the URL will be something like `https://hidden-beach-12345.herokuapp.com/`.
          */
-        private const val BASE_URL = "put your base url here"
+        private const val BASE_URL = "https://mshafrir-mobile-backend.herokuapp.com/"
 
         /**
          * Note: only necessary if not configured via `gradle.properties`.
          *
          * Set to publishable key from https://dashboard.stripe.com/test/apikeys
          */
-        private const val PUBLISHABLE_KEY = "pk_test_your_key_goes_here"
+        private const val PUBLISHABLE_KEY = "pk_test_brpLutVVvqMRjOedg8SUJr9O"
 
         /**
          * Note: only necessary if not configured via `gradle.properties`.
