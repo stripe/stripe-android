4b4c59b583d987093addbc82ac82d7b943cd7dd9
Michelle Brubaker <michelleb@stripe.com>
2021-03-02 06:55:47 -0500

    Make ViewState generic.

--- stripe/src/main/java/com/stripe/android/paymentsheet/model/ViewState.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/model/ViewState.kt
@@ -1,7 +1,5 @@
 package com.stripe.android.paymentsheet.model
 
-import com.stripe.android.PaymentIntentResult
-
 sealed class ViewState {
     // Might consider this just taking a string for the button
     data class Ready(
@@ -10,8 +8,7 @@ sealed class ViewState {
 
     object Confirming : ViewState()
 
-    // Might consider removing paymentIntentResult from the button
-    data class Completed(
-        val paymentIntentResult: PaymentIntentResult
+    data class Completed<T>(
+        val result: T
     ) : ViewState()
 }
