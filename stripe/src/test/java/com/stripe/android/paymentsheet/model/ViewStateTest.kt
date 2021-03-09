51ff7e0d5854bf0759cb2aab205acf28920d199b
Michael Shafrir <mshafrir@stripe.com>
2021-03-09 15:39:47 -0500

    Add tests

---
+++ stripe/src/test/java/com/stripe/android/paymentsheet/model/ViewStateTest.kt
@@ -0,0 +1,27 @@
+package com.stripe.android.paymentsheet.model
+
+import com.google.common.truth.Truth.assertThat
+import kotlin.test.Test
+
+class ViewStateTest {
+    @Test
+    fun `FinishProcessing equals() with FinishProcessing param should return true`() {
+        assertThat(ViewState.PaymentSheet.FinishProcessing {})
+            .isEqualTo(ViewState.PaymentSheet.FinishProcessing {})
+    }
+
+    @Test
+    fun `FinishProcessing equals() with not FinishProcessing param should return true`() {
+        assertThat(ViewState.PaymentSheet.FinishProcessing {})
+            .isNotEqualTo(ViewState.PaymentSheet.StartProcessing)
+    }
+
+    @Test
+    fun `FinishProcessing hashCode() should return different values`() {
+        val viewState1 = ViewState.PaymentSheet.FinishProcessing {}
+        val viewState2 = ViewState.PaymentSheet.FinishProcessing {}
+
+        assertThat(viewState1.hashCode())
+            .isNotEqualTo(viewState2.hashCode())
+    }
+}
