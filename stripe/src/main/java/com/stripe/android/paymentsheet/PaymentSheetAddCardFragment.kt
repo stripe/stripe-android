693900f3029a8aba29a661c66a1b896b1a84a8a8
michelleb-stripe <michellbe-stripe@gmail.com>
2021-03-15 08:47:49 -0400

    On Payment options remove google pay from add card.

--- stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetAddCardFragment.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/PaymentSheetAddCardFragment.kt
@@ -1,9 +1,15 @@
 package com.stripe.android.paymentsheet
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import com.stripe.android.R
+import com.stripe.android.databinding.FragmentPaymentsheetAddCardBinding
 import com.stripe.android.paymentsheet.analytics.EventReporter
 import com.stripe.android.paymentsheet.model.FragmentConfig
+import com.stripe.android.paymentsheet.model.PaymentSelection
+import com.stripe.android.paymentsheet.ui.BaseSheetActivity
 import java.util.Currency
 import java.util.Locale
 
@@ -21,8 +27,35 @@ internal class PaymentSheetAddCardFragment(
         )
     }
 
-    override fun onGooglePaySelected() {
-        sheetViewModel.checkout()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val config = arguments?.getParcelable<FragmentConfig>(
+            BaseSheetActivity.EXTRA_FRAGMENT_CONFIG
+        )
+        val shouldShowGooglePayButton = config?.let {
+            config.isGooglePayReady && config.paymentMethods.isEmpty()
+        } ?: false
+
+        val viewBinding = FragmentPaymentsheetAddCardBinding.bind(view)
+        val googlePayButton = viewBinding.googlePayButton
+        val addCardHeader = viewBinding.addCardHeader
+        val googlePayDivider = viewBinding.googlePayDivider
+
+        googlePayButton.setOnClickListener {
+            sheetViewModel.updateSelection(PaymentSelection.GooglePay)
+        }
+
+        googlePayButton.isVisible = shouldShowGooglePayButton
+
+        googlePayDivider.isVisible = shouldShowGooglePayButton
+        addCardHeader.isVisible = !shouldShowGooglePayButton
+
+        sheetViewModel.selection.observe(viewLifecycleOwner) { paymentSelection ->
+            if (paymentSelection == PaymentSelection.GooglePay) {
+                sheetViewModel.checkout()
+            }
+        }
     }
 
     override fun createHeaderText(
