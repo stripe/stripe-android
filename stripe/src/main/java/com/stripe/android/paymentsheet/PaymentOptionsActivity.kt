793812d6ca1d69cc14b073389bec6f4c9d7bb31b
michelleb-stripe <michellbe-stripe@gmail.com>
2021-03-15 08:18:04 -0400

    Payment Options won't jump to add card if google pay is ready.

--- stripe/src/main/java/com/stripe/android/paymentsheet/PaymentOptionsActivity.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/PaymentOptionsActivity.kt
@@ -142,7 +142,7 @@ internal class PaymentOptionsActivity : BaseSheetActivity<PaymentOptionResult>()
                     // where we also jump to a new unsaved card. However this move require
                     // the transition target to specify when to and when not to add things to the
                     // backstack.
-                    if (starterArgs.paymentMethods.isEmpty()) {
+                    if (starterArgs.paymentMethods.isEmpty() && viewModel.isGooglePayReady.value?.not() == true) {
                         PaymentOptionsViewModel.TransitionTarget.AddPaymentMethodSheet(config)
                     } else {
                         PaymentOptionsViewModel.TransitionTarget.SelectSavedPaymentMethod(config)
