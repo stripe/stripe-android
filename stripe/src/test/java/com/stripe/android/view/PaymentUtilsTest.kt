e0159c2a9627225c323a2a52c03e51947655d35d
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2021-03-22 09:40:30 -0400

    Add support for Java 11 in CI (#3497)
    Additionally, upgrade Robolectric to target API 30.

--- stripe/src/test/java/com/stripe/android/view/PaymentUtilsTest.kt
+++ stripe/src/test/java/com/stripe/android/view/PaymentUtilsTest.kt
@@ -1,12 +1,12 @@
 package com.stripe.android.view
 
+import com.google.common.truth.Truth.assertThat
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import java.util.Currency
 import java.util.Locale
 import kotlin.test.BeforeTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 /**
  * Test class for [PaymentUtils]
@@ -20,73 +20,80 @@ class PaymentUtilsTest {
 
     @Test
     fun formatPriceStringUsingFree_whenZero_rendersFree() {
-        assertEquals(
+        assertThat(
             PaymentUtils.formatPriceStringUsingFree(
                 0,
                 Currency.getInstance("USD"),
                 "Free"
-            ),
-            "Free"
-        )
+            )
+        ).isEqualTo("Free")
     }
 
     @Test
     fun formatPriceString_whenUSLocale_rendersCorrectSymbols() {
         Locale.setDefault(Locale.US)
-        assertEquals(
-            PaymentUtils.formatPriceString(12300.0, Currency.getInstance("USD")),
-            "$123.00"
-        )
+        assertThat(
+            PaymentUtils.formatPriceString(
+                12300.0,
+                Currency.getInstance("USD")
+            )
+        ).isEqualTo("$123.00")
 
         val euro = Currency.getInstance(Locale.GERMANY)
-        assertEquals(PaymentUtils.formatPriceString(12300.0, euro), "EUR123.00")
+        assertThat(PaymentUtils.formatPriceString(12300.0, euro))
+            .isEqualTo("€123.00")
 
         val canadianDollar = Currency.getInstance(Locale.CANADA)
-        assertEquals(
-            PaymentUtils.formatPriceString(12300.0, canadianDollar),
-            "CAD123.00"
-        )
+        assertThat(PaymentUtils.formatPriceString(12300.0, canadianDollar))
+            .isEqualTo("CA$123.00")
 
         val britishPound = Currency.getInstance(Locale.UK)
-        assertEquals(PaymentUtils.formatPriceString(10000.0, britishPound), "GBP100.00")
+        assertThat(PaymentUtils.formatPriceString(10000.0, britishPound))
+            .isEqualTo("£100.00")
     }
 
     @Test
     fun formatPriceString_whenInternationalLocale_rendersCorrectSymbols() {
         Locale.setDefault(Locale.GERMANY)
         val euro = Currency.getInstance(Locale.GERMANY)
-        assertEquals(PaymentUtils.formatPriceString(10000.0, euro), "100,00 €")
+        assertThat(PaymentUtils.formatPriceString(10000.0, euro))
+            .isEqualTo("100,00 €")
 
         Locale.setDefault(Locale.JAPAN)
         val yen = Currency.getInstance(Locale.JAPAN)
         // Japan's native local uses narrow yen symbol (there is also a wide yen symbol)
-        assertEquals(PaymentUtils.formatPriceString(100.0, yen), "￥100")
+        assertThat(PaymentUtils.formatPriceString(100.0, yen))
+            .isEqualTo("￥100")
 
         Locale.setDefault(Locale("ar", "JO")) // Jordan
         val jordanianDinar = Currency.getInstance("JOD")
-        assertEquals(PaymentUtils.formatPriceString(100123.0, jordanianDinar), jordanianDinar.symbol + " 100.123")
+        assertThat(PaymentUtils.formatPriceString(100123.0, jordanianDinar))
+            .isEqualTo("د.أ.\u200F ١٠٠٫١٢٣")
 
         Locale.setDefault(Locale.UK)
         val britishPound = Currency.getInstance(Locale.UK)
-        assertEquals(PaymentUtils.formatPriceString(10000.0, britishPound), "£100.00")
+        assertThat(PaymentUtils.formatPriceString(10000.0, britishPound))
+            .isEqualTo("£100.00")
 
         Locale.setDefault(Locale.CANADA)
         val canadianDollar = Currency.getInstance(Locale.CANADA)
-        assertEquals(
-            PaymentUtils.formatPriceString(12300.0, canadianDollar),
-            "$123.00"
-        )
+        assertThat(PaymentUtils.formatPriceString(12300.0, canadianDollar))
+            .isEqualTo("$123.00")
     }
 
     @Test
     fun formatPriceString_whenDecimalAmounts_rendersCorrectDigits() {
-        assertEquals(
-            "$100.12",
-            PaymentUtils.formatPriceString(10012.0, Currency.getInstance("USD"))
-        )
-        assertEquals(
-            "$0.12",
-            PaymentUtils.formatPriceString(12.0, Currency.getInstance("USD"))
-        )
+        assertThat(
+            PaymentUtils.formatPriceString(
+                10012.0,
+                Currency.getInstance("USD")
+            )
+        ).isEqualTo("$100.12")
+        assertThat(
+            PaymentUtils.formatPriceString(
+                12.0,
+                Currency.getInstance("USD")
+            )
+        ).isEqualTo("$0.12")
     }
 }
