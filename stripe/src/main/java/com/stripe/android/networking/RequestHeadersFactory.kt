1054078a56d587945b758426a46f3cadcbe956fb
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2021-04-01 16:44:33 -0400

    Create StripeClientUserAgentHeaderFactory (#3550)
    Abstract logic for creating the `X-Stripe-Client-User-Agent`

--- stripe/src/main/java/com/stripe/android/networking/RequestHeadersFactory.kt
+++ stripe/src/main/java/com/stripe/android/networking/RequestHeadersFactory.kt
@@ -1,10 +1,8 @@
 package com.stripe.android.networking
 
-import android.os.Build
 import com.stripe.android.ApiVersion
 import com.stripe.android.AppInfo
 import com.stripe.android.Stripe
-import org.json.JSONObject
 import java.util.Locale
 
 internal sealed class RequestHeadersFactory {
@@ -25,10 +23,14 @@ internal sealed class RequestHeadersFactory {
         private val options: ApiRequest.Options,
         private val appInfo: AppInfo? = null,
         private val locale: Locale = Locale.getDefault(),
-        private val systemPropertySupplier: (String) -> String = DEFAULT_SYSTEM_PROPERTY_SUPPLIER,
+        systemPropertySupplier: (String) -> String = DEFAULT_SYSTEM_PROPERTY_SUPPLIER,
         private val apiVersion: String = ApiVersion.get().code,
         private val sdkVersion: String = Stripe.VERSION
     ) : RequestHeadersFactory() {
+        private val stripeClientUserAgentHeaderFactory = StripeClientUserAgentHeaderFactory(
+            systemPropertySupplier
+        )
+
         private val languageTag: String?
             get() {
                 return locale.toString().replace("_", "-")
@@ -47,9 +49,10 @@ internal sealed class RequestHeadersFactory {
             get() {
                 return mapOf(
                     "Accept" to "application/json",
-                    ApiRequest.HEADER_STRIPE_CLIENT_USER_AGENT to createStripeClientUserAgent(appInfo),
                     "Stripe-Version" to apiVersion,
                     "Authorization" to "Bearer ${options.apiKey}"
+                ).plus(
+                    stripeClientUserAgentHeaderFactory.create(appInfo)
                 ).plus(
                     options.stripeAccount?.let {
                         mapOf("Stripe-Account" to it)
@@ -63,25 +66,7 @@ internal sealed class RequestHeadersFactory {
                 )
             }
 
-        private fun createStripeClientUserAgent(appInfo: AppInfo? = null): String {
-            return JSONObject(
-                mapOf(
-                    "os.name" to "android",
-                    "os.version" to Build.VERSION.SDK_INT.toString(),
-                    "bindings.version" to Stripe.VERSION_NAME,
-                    "lang" to "Java",
-                    "publisher" to "Stripe",
-                    "http.agent" to systemPropertySupplier(PROP_USER_AGENT)
-                ).plus(
-                    appInfo?.createClientHeaders().orEmpty()
-                )
-            ).toString()
-        }
-
         private companion object {
-            // this is the default user agent set by the system
-            private const val PROP_USER_AGENT = "http.agent"
-
             private val DEFAULT_SYSTEM_PROPERTY_SUPPLIER = { name: String ->
                 System.getProperty(name).orEmpty()
             }
@@ -106,7 +91,9 @@ internal sealed class RequestHeadersFactory {
     }
 
     internal companion object {
-        internal fun getUserAgent(sdkVersion: String = Stripe.VERSION) = "Stripe/v1 $sdkVersion"
+        internal fun getUserAgent(
+            sdkVersion: String = Stripe.VERSION
+        ) = "Stripe/v1 $sdkVersion"
 
         private const val HEADER_USER_AGENT = "User-Agent"
         private const val HEADER_ACCEPT_CHARSET = "Accept-Charset"
