2098d818149b9c85496363e8516e70c78227c687
Michelle Brubaker <michelleb@stripe.com>
2021-04-20 15:32:58 -0400

    Make the checkout identifier internal

--- stripe/src/main/java/com/stripe/android/paymentsheet/CheckoutIdentifier.kt
+++ stripe/src/main/java/com/stripe/android/paymentsheet/CheckoutIdentifier.kt
@@ -1,6 +1,6 @@
 package com.stripe.android.paymentsheet
 
-enum class CheckoutIdentifier {
+internal enum class CheckoutIdentifier {
     ADD_FRAGMENT_TOP_GOOGLE_PAY,
     SHEET_BOTTOM_GOOGLE_PAY,
     SHEET_BOTTOM_BUY,
