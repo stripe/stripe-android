bc4991c649ce85eb4c966d656fae600172a4c52c
Michael Shafrir <mshafrir@stripe.com>
2021-04-27 12:23:02 -0400

    Fix test

--- stripe/src/test/java/com/stripe/android/AnalyticsRequestFactoryTest.kt
+++ stripe/src/test/java/com/stripe/android/AnalyticsRequestFactoryTest.kt
@@ -334,7 +334,7 @@ class AnalyticsRequestFactoryTest {
                 )
             )
         assertThat(analyticsRequest.url)
-            .isEqualTo("https://q.stripe.com?publishable_key=pk_test_123&app_version=0&bindings_version=$sdkVersion&os_version=30&os_release=11&device_type=robolectric_robolectric_robolectric&source_type=card&app_name=com.stripe.android.test&analytics_ua=analytics.stripe_android-1.0&os_name=REL&event=stripe_android.payment_method_creation&request_id=req_123")
+            .isEqualTo("https://q.stripe.com?publishable_key=pk_abc123&app_version=0&bindings_version=$sdkVersion&os_version=30&os_release=11&device_type=robolectric_robolectric_robolectric&source_type=card&app_name=com.stripe.android.test&analytics_ua=analytics.stripe_android-1.0&os_name=REL&event=stripe_android.payment_method_creation&request_id=req_123")
     }
 
     private companion object {
