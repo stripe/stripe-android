f2d74582fbac846b4af6e8d06847af957083aad2
Chen Cen <ccen@stripe.com>
2021-04-29 12:12:28 -0700

    lint and dump

--- stripe/src/main/java/com/stripe/android/model/WeChatPayNextAction.kt
+++ stripe/src/main/java/com/stripe/android/model/WeChatPayNextAction.kt
@@ -1,8 +1,5 @@
 package com.stripe.android.model
 
-import com.stripe.android.model.PaymentIntent
-import com.stripe.android.model.StripeModel
-import com.stripe.android.model.WeChat
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
