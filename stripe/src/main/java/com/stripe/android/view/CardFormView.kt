65bb06d533d448b6391e50ef6d17d00df24eda68
Chen Cen <ccen@stripe.com>
2021-04-30 20:44:05 -0700

    remote log cat

--- stripe/src/main/java/com/stripe/android/view/CardFormView.kt
+++ stripe/src/main/java/com/stripe/android/view/CardFormView.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.os.Parcelable
 import android.text.Editable
 import android.util.AttributeSet
-import android.util.Log
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.inputmethod.EditorInfo
@@ -460,7 +459,6 @@ internal class CardFormView @JvmOverloads constructor(
     }
 
     private fun updateErrorsView(errorMessage: String?) {
-        Log.d("MLGB", "errorMessage: $errorMessage")
         errors.text = errorMessage
         errors.isVisible = errorMessage != null
     }
