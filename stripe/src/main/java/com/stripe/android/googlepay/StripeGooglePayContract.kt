346d9280bf793ee6308009dcee28df49f25050e9
michelleb-stripe <77996191+michelleb-stripe@users.noreply.github.com>
2021-05-05 08:48:20 -0700

    Display a google pay error in case of failure. (#3673)
    GooglePayResult could be one of these from these two sources:
    - https://developers.google.com/android/reference/com/google/android/gms/common/api/CommonStatusCodes
    - WalletConstants
    
    In case of a network error this is null:
    googlePayResult.googlePayStatus?.statusMessage

--- stripe/src/main/java/com/stripe/android/googlepay/StripeGooglePayContract.kt
+++ stripe/src/main/java/com/stripe/android/googlepay/StripeGooglePayContract.kt
@@ -8,7 +8,9 @@ import android.os.Parcelable
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.annotation.ColorInt
 import androidx.core.os.bundleOf
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Status
+import com.stripe.android.R
 import com.stripe.android.model.PaymentIntent
 import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.ShippingInformation
@@ -124,6 +126,34 @@ internal class StripeGooglePayContract :
                         "Error while processing result from Google Pay."
                     )
                 )
+            }
+        }
+    }
+}
+
+internal fun Status.getErrorResourceID(): Int? {
+    return when (this) {
+        Status.RESULT_SUCCESS -> null
+        Status.RESULT_INTERNAL_ERROR, Status.RESULT_CANCELED, Status.RESULT_DEAD_CLIENT -> R.string.stripe_google_pay_error_internal
+        else -> {
+            when (this.statusCode) {
+                CommonStatusCodes.API_NOT_CONNECTED, // "The client attempted to call a method from an API that failed to connect."
+                CommonStatusCodes.CANCELED, // -> "The result was canceled either due to client disconnect or PendingResult.cancel()."
+                CommonStatusCodes.DEVELOPER_ERROR, // -> "The application is misconfigured."
+                CommonStatusCodes.ERROR, // -> "The operation failed with no more detailed information."
+                CommonStatusCodes.INTERRUPTED, // -> "A blocking call was interrupted while waiting and did not run to completion."
+                CommonStatusCodes.INVALID_ACCOUNT, // -> "The client attempted to connect to the service with an invalid account name specified."
+                CommonStatusCodes.SERVICE_DISABLED, // -> "This constant is deprecated. This case handled during connection, not during API requests. No results should be returned with this status code."
+                CommonStatusCodes.SERVICE_VERSION_UPDATE_REQUIRED, // -> " This constant is deprecated.This case handled during connection, not during API requests . No results should be returned with this status code."
+                CommonStatusCodes.SUCCESS, // -> "The operation was successful.SUCCESS_CACHE The operation was successful, but was used the device's cache."
+                CommonStatusCodes.INTERNAL_ERROR -> R.string.stripe_google_pay_error_internal
+                CommonStatusCodes.RESOLUTION_REQUIRED -> R.string.stripe_google_pay_error_resolution_required
+                CommonStatusCodes.NETWORK_ERROR -> R.string.stripe_failure_connection_error
+                CommonStatusCodes.SIGN_IN_REQUIRED -> R.string.stripe_failure_reason_authentication
+                CommonStatusCodes.TIMEOUT -> R.string.stripe_failure_reason_timed_out
+                else -> {
+                    R.string.stripe_google_pay_error_internal
+                }
             }
         }
     }
