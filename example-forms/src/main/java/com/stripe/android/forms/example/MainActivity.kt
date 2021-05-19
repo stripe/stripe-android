eb721d301ea45170d3af9034a6479147a4b0548a
Michelle Brubaker <michelleb@stripe.com>
2021-05-19 17:19:31 -0400

    Fixes to get it working.

--- example-forms/src/main/java/com/stripe/android/forms/example/MainActivity.kt
+++ example-forms/src/main/java/com/stripe/android/forms/example/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.stripe.android.forms.example
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.stripe.android.compose.StripeTheme
-import com.stripe.android.compose.forms.SofortForm
-import com.stripe.android.compose.forms.SofortFormViewModel
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            StripeTheme {
+            MaterialTheme {
                 Column(Modifier.fillMaxSize()) {
                     Text("Here is where we add some text.")
                 }
