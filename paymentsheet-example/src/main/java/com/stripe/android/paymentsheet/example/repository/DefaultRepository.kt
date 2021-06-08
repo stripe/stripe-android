906176e8d8c0567123528cf158840cdd37112baf
Bruno R. Nunes <77990083+brnunes-stripe@users.noreply.github.com>
2021-06-08 11:28:35 -0700

    Create Payment Sheet playground in example app (#3806)

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/repository/DefaultRepository.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/repository/DefaultRepository.kt
@@ -2,23 +2,13 @@ package com.stripe.android.paymentsheet.example.repository
 
 import com.stripe.android.paymentsheet.example.service.CheckoutBackendApi
 import com.stripe.android.paymentsheet.example.service.CheckoutRequest
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
 
 internal class DefaultRepository(
-    private val checkoutBackendApi: CheckoutBackendApi,
-    private val workContext: CoroutineContext
+    private val checkoutBackendApi: CheckoutBackendApi
 ) : Repository {
     override suspend fun checkout(
         customer: Repository.CheckoutCustomer,
         currency: Repository.CheckoutCurrency,
         mode: Repository.CheckoutMode
-    ) = withContext(workContext) {
-        flowOf(
-            kotlin.runCatching {
-                checkoutBackendApi.checkout(CheckoutRequest(customer.value, currency.value, mode.value))
-            }
-        )
-    }
+    ) = checkoutBackendApi.checkout(CheckoutRequest(customer.value, currency.value, mode.value))
 }
