7920e4fa649b0a6792f21022977b88ba7d715616
Michelle Brubaker <michelleb@stripe.com>
2021-06-09 14:44:18 -0400

    Revert "Error State shakeup"
    This reverts commit c9302fbc

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/common/TextFieldElementState.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/common/TextFieldElementState.kt
@@ -5,45 +5,36 @@ package com.stripe.android.paymentsheet.elements.common
  * in each of these cases there might be a reason to show the
  * error in a different way
  */
-internal sealed class TextFieldElementState {
+internal abstract class TextFieldElementState {
 
-    abstract class Valid(val resId: Int) : TextFieldElementState() {
-        abstract fun isFull(): Boolean?
+    /**
+     * This indicate the Element state has reached the max number of characters
+     * and focus should shift forward.
+     */
+    abstract fun isFull(): Boolean
+
+    /**
+     * Indicates element is valid and field extraction can happen
+     * and be used to create PaymentMethod Parameters
+     */
+    abstract fun isValid(): Boolean
+
+    /**
+     * If in a state where isValid() returns false it indicates the element cannot be
+     * used for extraction.  This function should return the error message.
+     */
+    abstract fun getErrorMessageResId(): Int?
+
+    abstract class TextFieldElementStateValid : TextFieldElementState() {
+        override fun isValid(): Boolean = true
+        override fun isFull(): Boolean = false
+        override fun getErrorMessageResId(): Int? = null
     }
 
-    abstract class Invalid(val resId: Int) : TextFieldElementState() {
-        abstract fun shouldShowError(state: Invalid, hasFocus: Boolean): Boolean
-        abstract fun getErrorMessageResId(): Int
+    abstract class TextFieldElementStateError(private val stringResId: Int) :
+        TextFieldElementState() {
+        override fun isValid(): Boolean = false
+        override fun isFull(): Boolean = false
+        override fun getErrorMessageResId(): Int? = stringResId
     }
-
-//    /**
-//     * This indicate the Element state has reached the max number of characters
-//     * and focus should shift forward.
-//     */
-//    abstract fun isFull(): Boolean
-//
-//    /**
-//     * Indicates element is valid and field extraction can happen
-//     * and be used to create PaymentMethod Parameters
-//     */
-//    abstract fun isValid(): Boolean
-//
-//    /**
-//     * If in a state where isValid() returns false it indicates the element cannot be
-//     * used for extraction.  This function should return the error message.
-//     */
-//    abstract fun getErrorMessageResId(): Int?
-
-//    abstract class TextFieldElementStateValid : TextFieldElementState() {
-//        fun isValid(): Boolean = true
-//        fun isFull(): Boolean = false
-//        fun getErrorMessageResId(): Int? = null
-//    }
-//
-//    abstract class TextFieldElementStateError(private val stringResId: Int) :
-//        TextFieldElementState() {
-//        fun isValid(): Boolean = false
-//        fun isFull(): Boolean = false
-//        fun getErrorMessageResId(): Int? = stringResId
-//    }
 }
