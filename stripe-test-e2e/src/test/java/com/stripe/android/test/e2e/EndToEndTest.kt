60d4b2f7bee4fc48b407113b9ef7e0b60d4ee51c
Michael Shafrir <mshafrir@stripe.com>
2021-06-09 13:00:36 -0400

    Fix test

--- stripe-test-e2e/src/test/java/com/stripe/android/test/e2e/EndToEndTest.kt
+++ stripe-test-e2e/src/test/java/com/stripe/android/test/e2e/EndToEndTest.kt
@@ -43,10 +43,12 @@ internal class EndToEndTest {
         // Confirm the PaymentIntent using a test card
         val confirmedPaymentIntent = requireNotNull(
             stripe.confirmPaymentIntentSynchronous(
-                ConfirmPaymentIntentParams.createWithPaymentMethodCreateParams(
-                    clientSecret = newPaymentIntent.clientSecret,
-                    paymentMethodCreateParams = PAYMENT_METHOD_CREATE_PARAMS
-                )
+                ConfirmPaymentIntentParams
+                    .createWithPaymentMethodCreateParams(
+                        clientSecret = newPaymentIntent.clientSecret,
+                        paymentMethodCreateParams = PAYMENT_METHOD_CREATE_PARAMS
+                    )
+                    .withShouldUseStripeSdk(true)
             )
         )
 
