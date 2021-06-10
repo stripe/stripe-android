8aae5ed4f780a1aca4c78981cad10f9733f81b70
Bruno R. Nunes <brnunes@stripe.com>
2021-06-09 17:39:41 -0700

    Cleanup fake code

--- payments-core/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/IdealDataCollectionFragment.kt
+++ payments-core/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/IdealDataCollectionFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.appcompat.view.ContextThemeWrapper
 import com.stripe.android.R
-import com.stripe.android.databinding.FragmentPaymentsheetAddIdealBinding
 
 internal class IdealDataCollectionFragment : BasePaymentDataCollectionFragment() {
 
@@ -24,24 +22,5 @@ internal class IdealDataCollectionFragment : BasePaymentDataCollectionFragment()
             container,
             false
         )
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val viewBinding = FragmentPaymentsheetAddIdealBinding.bind(view)
-
-        ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_spinner_item,
-            arrayOf(
-                "Select", "Bank 1", "Bank 2", "Bank 3"
-            )
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            viewBinding.spinner.adapter = adapter
-        }
     }
 }
