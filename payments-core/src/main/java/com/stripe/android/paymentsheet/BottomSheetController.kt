579e90d0ff8325a6bba5658d60fb4118af3d432c
Bruno R. Nunes <brnunes@stripe.com>
2021-06-10 16:49:20 -0700

    Reorder

--- payments-core/src/main/java/com/stripe/android/paymentsheet/BottomSheetController.kt
+++ payments-core/src/main/java/com/stripe/android/paymentsheet/BottomSheetController.kt
@@ -49,14 +49,14 @@ internal class BottomSheetController(
     }
 
     fun hide() {
-        if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_HIDDEN) {
-            // When the bottom sheet finishes animating to its new state,
-            // the callback will finish the activity.
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-        } else {
+        if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN) {
             // If the state is already hidden, setting the state to hidden is a no-op,
             // so we need to finish it now.
             _shouldFinish.value = true
+        } else {
+            // When the bottom sheet finishes animating to its new state,
+            // the callback will finish the activity.
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         }
     }
 }
