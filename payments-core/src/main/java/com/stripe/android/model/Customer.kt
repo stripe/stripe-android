c9660bcedfe82f98a73746bb508943d02972444c
Michael Shafrir <mshafrir@stripe.com>
2021-06-14 10:30:33 -0400

    Update Customer and CustomerSource
    - Remove `Customer.fromString()` and `Customer.fromJson()`
    - Rename `CustomerSource` to `CustomerPaymentSource` and make
      a sealed class

--- payments-core/src/main/java/com/stripe/android/model/Customer.kt
+++ payments-core/src/main/java/com/stripe/android/model/Customer.kt
@@ -1,8 +1,6 @@
 package com.stripe.android.model
 
-import com.stripe.android.model.parsers.CustomerJsonParser
 import kotlinx.parcelize.Parcelize
-import org.json.JSONObject
 
 /**
  * Model for a Stripe Customer object
@@ -12,7 +10,7 @@ data class Customer internal constructor(
     val id: String?,
     val defaultSource: String?,
     val shippingInformation: ShippingInformation?,
-    val sources: List<CustomerSource>,
+    val sources: List<CustomerPaymentSource>,
     val hasMore: Boolean,
     val totalCount: Int?,
     val url: String?,
@@ -20,27 +18,7 @@ data class Customer internal constructor(
     val email: String?,
     val liveMode: Boolean
 ) : StripeModel {
-
-    fun getSourceById(sourceId: String): CustomerSource? {
+    fun getSourceById(sourceId: String): CustomerPaymentSource? {
         return sources.firstOrNull { it.id == sourceId }
-    }
-
-    companion object {
-        @JvmStatic
-        fun fromString(jsonString: String?): Customer? {
-            if (jsonString == null) {
-                return null
-            }
-
-            return runCatching { JSONObject(jsonString) }
-                .getOrNull()?.let {
-                    fromJson(it)
-                }
-        }
-
-        @JvmStatic
-        fun fromJson(jsonObject: JSONObject): Customer? {
-            return CustomerJsonParser().parse(jsonObject)
-        }
     }
 }
