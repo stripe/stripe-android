d4d2a58da152989747fb6bd7ae95265136d53966
Michelle Brubaker <michelleb@stripe.com>
2021-06-17 21:40:29 -0400

    Cleanup the populate from form field values.

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PopulateFormFromFormFieldValues.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PopulateFormFromFormFieldValues.kt
@@ -5,21 +5,16 @@ import com.stripe.android.paymentsheet.elements.common.FormElement
 internal class PopulateFormFromFormFieldValues(
     val elements: List<FormElement>
 ) {
-    // TODO: This can be very much simplified.
-    // This maps the field type to the controller
-    private val idControllerMap = elements
-        .filter { it.controller != null }
-        .associate { Pair(it.identifier, it.controller!!) }
-
     fun populateWith(
         formFieldValues: FormFieldValues
     ) {
-        val formFieldValueMap = formFieldValues.getMap()
-        idControllerMap.entries.forEach { entry ->
-            elements.filter { it. }
-            formFieldValueMap[entry.key]?.let {
-                entry.value.onValueChange(it)
+        val formFieldValueMap = formFieldValues.fieldValuePairs
+        elements
+            .filter { it.controller != null }
+            .forEach {
+                formFieldValueMap[it.identifier]?.let { input ->
+                    it.controller?.onValueChange(input)
+                }
             }
-        }
     }
 }
