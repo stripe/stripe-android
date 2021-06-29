4bdfab614090a745046205b4deea4000a35011e6
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2021-06-28 22:38:41 -0400

    Create GooglePayPaymentMethodLauncher (#3891)
    As opposed to `GooglePayLauncher`, will be used for returning a
    `PaymentMethod` instead of confirming a `PaymentIntent` or
    `SetupIntent`.

---
+++ payments-core/src/main/java/com/stripe/android/googlepaylauncher/GooglePayPaymentMethodLauncherContract.kt
@@ -0,0 +1,62 @@
+package com.stripe.android.googlepaylauncher
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Parcelable
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.os.bundleOf
+import kotlinx.parcelize.Parcelize
+
+internal class GooglePayPaymentMethodLauncherContract :
+    ActivityResultContract<GooglePayPaymentMethodLauncherContract.Args, GooglePayPaymentMethodLauncher.Result>() {
+
+    override fun createIntent(context: Context, input: Args): Intent {
+        val statusBarColor = when (context) {
+            is Activity -> context.window?.statusBarColor
+            else -> null
+        }
+
+        val extras = input.toBundle().apply {
+            if (statusBarColor != null) {
+                putInt(EXTRA_STATUS_BAR_COLOR, statusBarColor)
+            }
+        }
+
+        // TODO(mshafrir-stripe): update target activity
+        return Intent(context, StripeGooglePayActivity::class.java)
+            .putExtras(extras)
+    }
+
+    override fun parseResult(
+        resultCode: Int,
+        intent: Intent?
+    ): GooglePayPaymentMethodLauncher.Result {
+        return intent?.getParcelableExtra(EXTRA_RESULT)
+            ?: GooglePayPaymentMethodLauncher.Result.Failed(
+                IllegalArgumentException("Could not parse a valid result.")
+            )
+    }
+
+    @Parcelize
+    data class Args(
+        internal val config: GooglePayPaymentMethodLauncher.Config,
+        internal val currencyCode: String,
+        internal val amount: Int?
+    ) : Parcelable {
+        fun toBundle() = bundleOf("" to this)
+
+        internal companion object {
+            private const val EXTRA_ARGS = "extra_args"
+
+            fun fromIntent(intent: Intent): Args? {
+                return intent.getParcelableExtra(EXTRA_ARGS)
+            }
+        }
+    }
+
+    internal companion object {
+        internal const val EXTRA_RESULT = "extra_result"
+        internal const val EXTRA_STATUS_BAR_COLOR = "extra_status_bar_color"
+    }
+}
---
+++ payments-core/src/test/java/com/stripe/android/googlepaylauncher/GooglePayPaymentMethodLauncherTest.kt
@@ -0,0 +1,152 @@
+package com.stripe.android.googlepaylauncher
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.app.ActivityOptionsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import com.google.common.truth.Truth.assertThat
+import com.stripe.android.model.PaymentMethodFixtures
+import com.stripe.android.paymentsheet.GooglePayRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.AfterTest
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+@ExperimentalCoroutinesApi
+@RunWith(RobolectricTestRunner::class)
+class GooglePayPaymentMethodLauncherTest {
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
+
+    private val scenario = launchFragmentInContainer(initialState = Lifecycle.State.CREATED) {
+        TestFragment()
+    }
+
+    private val readyCallbackInvocations = mutableListOf<Boolean>()
+    private val results = mutableListOf<GooglePayPaymentMethodLauncher.Result>()
+
+    private val readyCallback =
+        GooglePayPaymentMethodLauncher.ReadyCallback(readyCallbackInvocations::add)
+    private val resultCallback = GooglePayPaymentMethodLauncher.ResultCallback(results::add)
+
+    @AfterTest
+    fun cleanup() {
+        testDispatcher.cleanupTestCoroutines()
+        testScope.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun `present() should successfully return a result when Google Pay is available`() {
+        val result = GooglePayPaymentMethodLauncher.Result.Completed(
+            PaymentMethodFixtures.CARD_PAYMENT_METHOD
+        )
+        scenario.onFragment { fragment ->
+            val launcher = GooglePayPaymentMethodLauncher(
+                testScope,
+                CONFIG,
+                { FakeGooglePayRepository(true) },
+                readyCallback,
+                fragment.registerForActivityResult(
+                    GooglePayPaymentMethodLauncherContract(),
+                    FakeActivityResultRegistry(result)
+                ) {
+                    resultCallback.onResult(it)
+                }
+            )
+            scenario.moveToState(Lifecycle.State.RESUMED)
+
+            assertThat(readyCallbackInvocations)
+                .containsExactly(true)
+
+            launcher.present(
+                currencyCode = "usd"
+            )
+
+            assertThat(results)
+                .containsExactly(result)
+        }
+    }
+
+    @Test
+    fun `present() should throw IllegalStateException when Google Pay is not available`() {
+        scenario.onFragment { fragment ->
+            val launcher = GooglePayPaymentMethodLauncher(
+                testScope,
+                CONFIG,
+                { FakeGooglePayRepository(false) },
+                readyCallback,
+                fragment.registerForActivityResult(
+                    GooglePayPaymentMethodLauncherContract(),
+                    FakeActivityResultRegistry(
+                        GooglePayPaymentMethodLauncher.Result.Completed(
+                            PaymentMethodFixtures.CARD_PAYMENT_METHOD
+                        )
+                    )
+                ) {
+                    resultCallback.onResult(it)
+                }
+            )
+            scenario.moveToState(Lifecycle.State.RESUMED)
+
+            assertThat(readyCallbackInvocations)
+                .containsExactly(false)
+
+            assertFailsWith<IllegalStateException> {
+                launcher.present(
+                    currencyCode = "usd"
+                )
+            }
+        }
+    }
+
+    private class FakeGooglePayRepository(
+        private var isReady: Boolean
+    ) : GooglePayRepository {
+        override fun isReady(): Flow<Boolean> = flowOf(isReady)
+    }
+
+    private class FakeActivityResultRegistry(
+        private val result: GooglePayPaymentMethodLauncher.Result
+    ) : ActivityResultRegistry() {
+        override fun <I, O> onLaunch(
+            requestCode: Int,
+            contract: ActivityResultContract<I, O>,
+            input: I,
+            options: ActivityOptionsCompat?
+        ) {
+            dispatchResult(
+                requestCode,
+                result
+            )
+        }
+    }
+
+    internal class TestFragment : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View = FrameLayout(inflater.context)
+    }
+
+    private companion object {
+        val CONFIG = GooglePayPaymentMethodLauncher.Config(
+            GooglePayEnvironment.Test,
+            merchantCountryCode = "US",
+            merchantName = "Widget Store"
+        )
+    }
+}
