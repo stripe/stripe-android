5c2bb0d5408a1f6660987884ea83749d7e237800
Michelle Brubaker <michelleb@stripe.com>
2021-06-30 13:30:33 -0400

    Bancontact requires a mandate.

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/specifications/BancontactSpec.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/specifications/BancontactSpec.kt
@@ -1,5 +1,7 @@
 package com.stripe.android.paymentsheet.specifications
 
+import androidx.compose.ui.graphics.Color
+import com.stripe.android.paymentsheet.R
 import com.stripe.android.paymentsheet.specifications.FormItemSpec.SaveForFutureUseSpec
 
 internal val bancontactParamKey: MutableMap<String, Any?> = mutableMapOf(
@@ -11,6 +13,11 @@ internal val bancontactNameSection =
     FormItemSpec.SectionSpec(IdentifierSpec("name"), SectionFieldSpec.Name)
 internal val bancontactEmailSection =
     FormItemSpec.SectionSpec(IdentifierSpec("email"), SectionFieldSpec.Email)
+internal val bancontactMandate = FormItemSpec.MandateTextSpec(
+    IdentifierSpec("mandate"),
+    R.string.sofort_mandate,
+    Color.Gray
+)
 val bancontact = FormSpec(
     LayoutSpec(
         listOf(
@@ -18,7 +25,7 @@ val bancontact = FormSpec(
             bancontactEmailSection,
             SaveForFutureUseSpec(
                 listOf(
-                    bancontactEmailSection
+                    bancontactEmailSection, bancontactMandate
                 )
             )
         )
