2fcfd2520a733f567ae23de53e634bd80387b6ec
Michelle Brubaker <michelleb@stripe.com>
2021-07-07 09:06:48 -0400

    Restrict classes to internal

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/common/Controller.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/common/Controller.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 /**
  * This class provides the logic behind the fields.
  */
-sealed interface Controller {
+internal sealed interface Controller {
     val label: Int
     val fieldValue: Flow<String>
     val isComplete: Flow<Boolean>
