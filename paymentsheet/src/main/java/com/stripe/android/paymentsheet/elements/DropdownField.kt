5a94d0389486baf4b11bed132a288b6fda13980e
Michelle Brubaker <michelleb@stripe.com>
2021-07-08 21:43:22 -0400

    ktlintFormat

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/DropdownField.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/DropdownField.kt
@@ -13,16 +13,12 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
@@ -32,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.asLiveData
 
@@ -103,10 +98,11 @@ internal fun DropDown(
 
 /**
  * This will create the label for the DropdownTextField.
- * Copied from androidx.compose.material.TextFieldImpl
+ *
+ * Copied logic from androidx.compose.material.TextFieldImpl
  */
 @Composable
-fun DropdownLabel(
+internal fun DropdownLabel(
     label: Int,
 ) {
     val interactionSource = remember { MutableInteractionSource() }
