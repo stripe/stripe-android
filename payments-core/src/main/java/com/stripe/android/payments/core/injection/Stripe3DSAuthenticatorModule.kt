7ce661441ea8e70cbdc8dfc0f59c839670e27f2c
Michael Shafrir <mshafrir@stripe.com>
2021-07-12 16:54:55 -0400

    Respond to comments

--- payments-core/src/main/java/com/stripe/android/payments/core/injection/Stripe3DSAuthenticatorModule.kt
+++ payments-core/src/main/java/com/stripe/android/payments/core/injection/Stripe3DSAuthenticatorModule.kt
@@ -3,18 +3,11 @@ package com.stripe.android.payments.core.injection
 import com.stripe.android.PaymentAuthConfig
 import com.stripe.android.model.StripeIntent
 import com.stripe.android.model.StripeIntent.NextActionData
-import com.stripe.android.networking.AnalyticsRequestExecutor
-import com.stripe.android.networking.AnalyticsRequestFactory
-import com.stripe.android.networking.RetryDelaySupplier
-import com.stripe.android.networking.StripeRepository
 import com.stripe.android.payments.core.authentication.PaymentAuthenticator
-import com.stripe.android.payments.core.authentication.threeds2.DefaultStripe3ds2ChallengeResultProcessor
 import com.stripe.android.payments.core.authentication.threeds2.Stripe3DS2Authenticator
-import com.stripe.android.payments.core.authentication.threeds2.Stripe3ds2ChallengeResultProcessor
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
-import kotlinx.coroutines.Dispatchers
 import javax.inject.Named
 
 /**
@@ -35,24 +28,6 @@ internal class Stripe3DSAuthenticatorModule {
             PaymentAuthConfig.get(),
             enableLogging,
             threeDs1IntentReturnUrlMap
-        )
-    }
-
-    @Provides
-    internal fun provideStripe3ds2ChallengeResultProcessor(
-        stripeRepository: StripeRepository,
-        analyticsRequestExecutor: AnalyticsRequestExecutor,
-        analyticsRequestFactory: AnalyticsRequestFactory,
-        retryDelaySupplier: RetryDelaySupplier,
-        @Named(ENABLE_LOGGING) enableLogging: Boolean
-    ): Stripe3ds2ChallengeResultProcessor {
-        return DefaultStripe3ds2ChallengeResultProcessor(
-            stripeRepository,
-            analyticsRequestExecutor,
-            analyticsRequestFactory,
-            retryDelaySupplier,
-            enableLogging,
-            Dispatchers.IO
         )
     }
 }
