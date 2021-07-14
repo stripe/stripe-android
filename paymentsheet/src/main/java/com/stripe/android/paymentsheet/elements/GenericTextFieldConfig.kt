4773209574c0cc0b9eb9f412347e71d0e8b18579
Michelle Brubaker <michelleb@stripe.com>
2021-07-14 15:50:23 -0400

    Fix error

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/GenericTextFieldConfig.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/GenericTextFieldConfig.kt
@@ -1,5 +1,6 @@
 package com.stripe.android.paymentsheet.elements
 
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import com.stripe.android.paymentsheet.ElementType
 
@@ -7,7 +8,8 @@ internal class GenericTextFieldConfig(
     override val debugLabel: String = "generic_text",
     override val label: Int,
     override val keyboard: KeyboardType = KeyboardType.Text,
-    override val elementType: ElementType = ElementType.GenericText
+    override val elementType: ElementType = ElementType.GenericText,
+    override val capitalization: KeyboardCapitalization = KeyboardCapitalization.None
 ) : TextFieldConfig {
     override fun determineState(input: String): TextFieldState = object : TextFieldState {
         override fun shouldShowError(hasFocus: Boolean) = false
