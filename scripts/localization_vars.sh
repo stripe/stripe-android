d79eae4a9b8b5f6b05a572a12f97045189caf142
Michelle Brubaker <michelleb@stripe.com>
2021-07-20 11:40:21 -0400

    Add the lokalize vars.

---
+++ scripts/localization_vars.sh
@@ -0,0 +1,5 @@
+# Shared variables used in our localization scripts
+
+# API token should be retrieved from: https://app.lokalise.com/profile#apitokens
+API_TOKEN=
+PROJECT_ID=747824695e51bc2f4aa912.89576472
