ab528c9290835c73be3dca7f7206e0319301e064
Michelle Brubaker <michelleb@stripe.com>
2021-07-21 17:01:00 -0400

    KtLint

--- payments-core/src/test/java/com/stripe/android/view/BecsDebitMandateAcceptanceTextViewTest.kt
+++ payments-core/src/test/java/com/stripe/android/view/BecsDebitMandateAcceptanceTextViewTest.kt
@@ -22,6 +22,5 @@ class BecsDebitMandateAcceptanceTextViewTest {
 
         assertThat(textView.text.toString().trim())
             .isEqualTo("By providing your bank account details and confirming this payment, you agree to this Direct Debit Request and the Direct Debit Request service agreement, and authorise Stripe Payments Australia Pty Ltd ACN 160 180 343 Direct Debit User ID number 507156 (“Stripe”) to debit your account through the Bulk Electronic Clearing System (BECS) on behalf of Rocketship Inc. (the \"Merchant\") for any amounts separately communicated to you by the Merchant. You certify that you are either an account holder or an authorised signatory on the account listed above.")
-
     }
 }
