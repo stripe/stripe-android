421efdca3012822a72543209ca350dac182cabff
Chen Cen <ccen@stripe.com>
2021-08-05 17:38:45 -0700

    change

--- payments-core/src/main/java/com/stripe/android/paymentsheet/flowcontroller/DefaultFlowControllerInitializer.kt
+++ payments-core/src/main/java/com/stripe/android/paymentsheet/flowcontroller/DefaultFlowControllerInitializer.kt
@@ -1,7 +1,10 @@
 package com.stripe.android.paymentsheet.flowcontroller
 
+import com.stripe.android.googlepaylauncher.GooglePayEnvironment
+import com.stripe.android.googlepaylauncher.GooglePayRepository
 import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.StripeIntent
+import com.stripe.android.payments.core.injection.IOContext
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PrefsRepository
 import com.stripe.android.paymentsheet.model.ClientSecret
@@ -12,13 +15,17 @@ import com.stripe.android.paymentsheet.model.StripeIntentValidator
 import com.stripe.android.paymentsheet.model.SupportedPaymentMethod
 import com.stripe.android.paymentsheet.repositories.PaymentMethodsRepository
 import com.stripe.android.paymentsheet.repositories.StripeIntentRepository
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
 import kotlin.coroutines.CoroutineContext
 
-internal class DefaultFlowControllerInitializer(
-    private val prefsRepositoryFactory: (String) -> PrefsRepository,
-    private val isGooglePayReadySupplier: suspend (PaymentSheet.GooglePayConfiguration.Environment?) -> Boolean,
-    private val workContext: CoroutineContext
+@Singleton
+internal class DefaultFlowControllerInitializer @Inject constructor(
+    private val prefsRepositoryFactory: @JvmSuppressWildcards (PaymentSheet.CustomerConfiguration?) -> PrefsRepository,
+    private val googlePayRepositoryFactory: @JvmSuppressWildcards (GooglePayEnvironment) -> GooglePayRepository,
+    @IOContext private val workContext: CoroutineContext
 ) : FlowControllerInitializer {
     private val stripeIntentValidator = StripeIntentValidator()
 
@@ -34,10 +41,7 @@ internal class DefaultFlowControllerInitializer(
         this@DefaultFlowControllerInitializer.stripeIntentRepository = stripeIntentRepository
         this@DefaultFlowControllerInitializer.paymentMethodsRepository = paymentMethodsRepository
 
-        val isGooglePayReady =
-            clientSecret is PaymentIntentClientSecret && paymentSheetConfiguration?.let {
-                isGooglePayReadySupplier(it.googlePay?.environment)
-            } ?: false
+        val isGooglePayReady = isGooglePayReady(clientSecret, paymentSheetConfiguration)
         paymentSheetConfiguration?.customer?.let { customerConfig ->
             createWithCustomer(
                 clientSecret,
@@ -52,15 +56,33 @@ internal class DefaultFlowControllerInitializer(
         )
     }
 
+    private suspend fun isGooglePayReady(
+        clientSecret: ClientSecret,
+        paymentSheetConfiguration: PaymentSheet.Configuration?
+    ): Boolean {
+        return clientSecret is PaymentIntentClientSecret &&
+            paymentSheetConfiguration?.googlePay?.environment.let { environment ->
+                val googlePayRepository = environment?.let {
+                    googlePayRepositoryFactory(
+                        when (environment) {
+                            PaymentSheet.GooglePayConfiguration.Environment.Production ->
+                                GooglePayEnvironment.Production
+                            PaymentSheet.GooglePayConfiguration.Environment.Test ->
+                                GooglePayEnvironment.Test
+                        }
+                    )
+                } ?: GooglePayRepository.Disabled
+                googlePayRepository.isReady().first()
+            }
+    }
+
     private suspend fun createWithCustomer(
         clientSecret: ClientSecret,
         customerConfig: PaymentSheet.CustomerConfiguration,
         config: PaymentSheet.Configuration?,
         isGooglePayReady: Boolean
     ): FlowControllerInitializer.InitResult {
-        val prefsRepository = prefsRepositoryFactory(
-            customerConfig.id
-        )
+        val prefsRepository = prefsRepositoryFactory(customerConfig)
 
         return runCatching {
             retrieveStripeIntent(clientSecret)
