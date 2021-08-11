ffb52425027a40c8f1c947dcbe20782eb92ac271
michelleb-stripe <77996191+michelleb-stripe@users.noreply.github.com>
2021-08-11 08:56:23 -0700

    Add comments

--- payments-core/src/main/java/com/stripe/android/paymentsheet/PaymentSheetPaymentController.kt
+++ payments-core/src/main/java/com/stripe/android/paymentsheet/PaymentSheetPaymentController.kt
@@ -11,12 +11,25 @@ import com.stripe.android.paymentsheet.model.StripeIntentValidator
 import com.stripe.android.view.AuthActivityStarterHost
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * This sealed interface is the result of startAndConfirm it can either be an Error, Fatal, success, or a StripeIntent that is still ready for a new selection
+ */
 sealed interface StartAndConfirmResult {
+    /**
+      *This is an error that can be recovered, the payment intent can be used with a different
+      * payment method.
+      */
     data class ErrorStripeIntentReady(val stripeIntentResult: StripeIntentResult<StripeIntent>) :
         StartAndConfirmResult
 
+    /**
+     * This is an error that cannot be recovered,  new payment intent must be created.
+     */
     data class StripeResultError(val throwable: Throwable) : StartAndConfirmResult
     data class Fatal(val throwable: Throwable) : StartAndConfirmResult
+    /**
+      * The confirm and Auth was successful.
+      */
     data class Success(val stripeIntentResult: StripeIntentResult<StripeIntent>) :
         StartAndConfirmResult
 }
