9d5365a4ab7ebfe7f4be63ec70b4333f35bb2def
Chen Cen <79880926+ccen-stripe@users.noreply.github.com>
2021-08-17 12:07:58 -0700

    Reimplement `InjectorRegistry` with a map (#4112)
    * reiplement InjectorRegistry with map
    
    * dump

--- payments-core/src/test/java/com/stripe/android/payments/core/injection/WeakMapInjectorRegistryTest.kt
+++ payments-core/src/test/java/com/stripe/android/payments/core/injection/WeakMapInjectorRegistryTest.kt
@@ -12,29 +12,29 @@ import kotlin.test.assertNull
 
 @RunWith(RobolectricTestRunner::class)
 @ExperimentalCoroutinesApi
-class WeakSetInjectorRegistryTest {
+class WeakMapInjectorRegistryTest {
 
     @Before
     fun clearStaticCache() {
-        WeakSetInjectorRegistry.staticCacheSet.clear()
+        WeakMapInjectorRegistry.staticCacheMap.clear()
     }
 
     @Test
     fun verifyRegistryRetrievesCorrectObject() {
         val injector1 = TestInjector()
-        val keyForInjector1 = WeakSetInjectorRegistry.nextKey()
+        val keyForInjector1 = WeakMapInjectorRegistry.nextKey()
 
         val injector2 = TestInjector()
-        val keyForInjector2 = WeakSetInjectorRegistry.nextKey()
+        val keyForInjector2 = WeakMapInjectorRegistry.nextKey()
 
-        WeakSetInjectorRegistry.register(injector1, keyForInjector1)
-        WeakSetInjectorRegistry.register(injector2, keyForInjector2)
+        WeakMapInjectorRegistry.register(injector1, keyForInjector1)
+        WeakMapInjectorRegistry.register(injector2, keyForInjector2)
 
-        assertEquals(injector1, WeakSetInjectorRegistry.retrieve(keyForInjector1))
-        assertEquals(injector2, WeakSetInjectorRegistry.retrieve(keyForInjector2))
+        assertEquals(injector1, WeakMapInjectorRegistry.retrieve(keyForInjector1))
+        assertEquals(injector2, WeakMapInjectorRegistry.retrieve(keyForInjector2))
         assertNotEquals(
-            WeakSetInjectorRegistry.retrieve(keyForInjector1),
-            WeakSetInjectorRegistry.retrieve(keyForInjector2)
+            WeakMapInjectorRegistry.retrieve(keyForInjector1),
+            WeakMapInjectorRegistry.retrieve(keyForInjector2)
         )
     }
 
@@ -42,21 +42,21 @@ class WeakSetInjectorRegistryTest {
     @Test
     fun verifyCacheIsClearedOnceWeakReferenceIsDeReferenced() {
         var injector1: Injector? = TestInjector()
-        val keyForInjector1 = WeakSetInjectorRegistry.nextKey()
-        WeakSetInjectorRegistry.register(injector1!!, keyForInjector1)
+        val keyForInjector1 = WeakMapInjectorRegistry.nextKey()
+        WeakMapInjectorRegistry.register(injector1!!, keyForInjector1)
 
-        assertNotNull(WeakSetInjectorRegistry.retrieve(keyForInjector1))
+        assertNotNull(WeakMapInjectorRegistry.retrieve(keyForInjector1))
 
         var retry = 10
         // de-reference the injector and hint System.gc to trigger removal
         injector1 = null
-        while (WeakSetInjectorRegistry.retrieve(keyForInjector1) != null && retry > 0) {
+        while (WeakMapInjectorRegistry.retrieve(keyForInjector1) != null && retry > 0) {
             System.gc()
             retry--
         }
 
         if (retry == 0) {
-            assertNull(WeakSetInjectorRegistry.retrieve(keyForInjector1))
+            assertNull(WeakMapInjectorRegistry.retrieve(keyForInjector1))
         }
         // otherwise WeakSetInjectorRegistry.retrieve(keyForInjector1) == null,
         // indicating the entry is already cleared
@@ -65,17 +65,8 @@ class WeakSetInjectorRegistryTest {
     // calling whenever() on a mock will end up holding the mock instance, therefore a real
     // Injector instance is needed to ensure System.gc() works correctly.
     internal class TestInjector : Injector {
-
-        var key: Int? = null
-
         override fun inject(injectable: Injectable) {
             // no - op
-        }
-
-        override fun getInjectorKey(): Int? = key
-
-        override fun setInjectorKey(injectorKey: Int) {
-            key = injectorKey
         }
     }
 }
