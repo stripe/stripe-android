feeb4c0c6dbc4f948420fcff087b603fc0e88412
Chen Cen <ccen@stripe.com>
2021-08-18 18:06:17 -0700

    add doc to create functions

--- payments-core/src/main/java/com/stripe/android/payments/paymentlauncher/PaymentLauncher.kt
+++ payments-core/src/main/java/com/stripe/android/payments/paymentlauncher/PaymentLauncher.kt
@@ -2,6 +2,7 @@ package com.stripe.android.payments.paymentlauncher
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.fragment.app.Fragment
@@ -42,6 +43,12 @@ interface PaymentLauncher {
     }
 
     companion object {
+        /**
+         * Create a [PaymentLauncher] instance with [ComponentActivity].
+         *
+         * This API registers an [ActivityResultLauncher] into the [ComponentActivity],  it needs
+         * to be called before the [ComponentActivity] is created.
+         */
         fun create(
             activity: ComponentActivity,
             publishableKey: String,
@@ -49,6 +56,13 @@ interface PaymentLauncher {
             callback: PaymentResultCallback
         ) = PaymentLauncherFactory(activity, callback).create(publishableKey, stripeAccountId)
 
+
+        /**
+         * Create a [PaymentLauncher] instance with [Fragment].
+         *
+         * This API registers an [ActivityResultLauncher] into the [Fragment]'s hosting Activity, it
+         * needs to be called before the [Fragment] is created.
+         */
         fun create(
             fragment: Fragment,
             publishableKey: String,
@@ -56,6 +70,13 @@ interface PaymentLauncher {
             callback: PaymentResultCallback
         ) = PaymentLauncherFactory(fragment, callback).create(publishableKey, stripeAccountId)
 
+        /**
+         * Create a [PaymentLauncher] used for Jetpack Compose.
+         *
+         * This API uses Compose specific API [rememberLauncherForActivityResult] to register a
+         * [ActivityResultLauncher] into current activity, it should be called as part of Compose
+         * initialization path.
+         */
         @Composable
         fun createForCompose(
             publishableKey: String,
