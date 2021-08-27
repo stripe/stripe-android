a1a2133625880241f92bf533919c46d188f461e9
Chen Cen <79880926+ccen-stripe@users.noreply.github.com>
2021-08-27 15:10:59 -0700

    Make `StripePaymentLauncher` assisted injectable so that it can share the dependencies from PaymentSheet (#4140)
    * change
    
    * resolve comments
    
    * change to lambda
    
    * restrictTo
    
    * update comments

--- payments-core/src/main/java/com/stripe/android/payments/paymentlauncher/PaymentLauncherFactory.kt
+++ payments-core/src/main/java/com/stripe/android/payments/paymentlauncher/PaymentLauncherFactory.kt
@@ -3,13 +3,20 @@ package com.stripe.android.payments.paymentlauncher
 import android.content.Context
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
+import androidx.annotation.RestrictTo
 import androidx.fragment.app.Fragment
-import com.stripe.android.payments.core.injection.WeakMapInjectorRegistry
+import com.stripe.android.BuildConfig
+import com.stripe.android.networking.AnalyticsRequestFactory
+import com.stripe.android.networking.StripeApiRepository
+import kotlinx.coroutines.Dispatchers
 
 /**
- * Factory to create a [PaymentLauncher].
+ * Factory to create a [PaymentLauncher], initialize all required dependencies.
+ *
+ * Used when [PaymentLauncher] is used as a standalone API.
  */
-internal class PaymentLauncherFactory(
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+class PaymentLauncherFactory(
     private val context: Context,
     private val hostActivityLauncher: ActivityResultLauncher<PaymentLauncherContract.Args>
 ) {
@@ -39,17 +46,21 @@ internal class PaymentLauncherFactory(
     fun create(
         publishableKey: String,
         stripeAccountId: String? = null
-    ): PaymentLauncher {
-        val injectorKey = WeakMapInjectorRegistry.nextKey()
-        val paymentLauncher =
-            StripePaymentLauncher(
-                hostActivityLauncher,
-                context,
-                publishableKey,
-                stripeAccountId,
-                injectorKey
-            )
-        WeakMapInjectorRegistry.register(paymentLauncher, injectorKey)
-        return paymentLauncher
-    }
+    ): PaymentLauncher = StripePaymentLauncher(
+        { publishableKey },
+        { stripeAccountId },
+        hostActivityLauncher,
+        context,
+        BuildConfig.DEBUG,
+        Dispatchers.IO,
+        Dispatchers.Main,
+        StripeApiRepository(
+            context,
+            { publishableKey }
+        ),
+        AnalyticsRequestFactory(
+            context,
+            { publishableKey }
+        )
+    )
 }
