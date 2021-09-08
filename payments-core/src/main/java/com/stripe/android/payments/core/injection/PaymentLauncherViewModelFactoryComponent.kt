94eb34da1e2a3a1c4ee5bd07cd47028f6e5168b4
Chen Cen <ccen@stripe.com>
2021-09-08 16:16:23 -0700

    change

---
+++ payments-core/src/main/java/com/stripe/android/payments/core/injection/PaymentLauncherViewModelFactoryComponent.kt
@@ -0,0 +1,50 @@
+package com.stripe.android.payments.core.injection
+
+import android.content.Context
+import com.stripe.android.payments.paymentlauncher.PaymentLauncherViewModel
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Named
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Component to inject [PaymentLauncherViewModel.Factory] when the app process is killed and
+ * there is no [Injector] available.
+ */
+@Singleton
+@Component(
+    modules = [
+        PaymentLauncherModule::class,
+        StripeRepositoryModule::class
+    ]
+)
+internal interface PaymentLauncherViewModelFactoryComponent {
+    fun inject(factory: PaymentLauncherViewModel.Factory)
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun context(context: Context): Builder
+
+        @BindsInstance
+        fun enableLogging(@Named(ENABLE_LOGGING) enableLogging: Boolean): Builder
+
+        @BindsInstance
+        fun ioContext(@IOContext workContext: CoroutineContext): Builder
+
+        @BindsInstance
+        fun uiContext(@UIContext uiContext: CoroutineContext): Builder
+
+        @BindsInstance
+        fun publishableKeyProvider(@Named(PUBLISHABLE_KEY) publishableKeyProvider: () -> String): Builder
+
+        @BindsInstance
+        fun stripeAccountIdProvider(@Named(STRIPE_ACCOUNT_ID) stripeAccountIdProvider: () -> String?): Builder
+
+        @BindsInstance
+        fun productUsage(@Named(PRODUCT_USAGE) productUsage: Set<String>): Builder
+
+        fun build(): PaymentLauncherViewModelFactoryComponent
+    }
+}
