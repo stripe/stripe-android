648596b06efafa136644552cc8fb84d23d271b6c
Michelle Brubaker <michelleb@stripe.com>
2021-09-09 11:37:33 -0400

    Add in authorization.

--- paymentsheet-example/src/androidTestDebug/java/SofortFormTest.kt
+++ paymentsheet-example/src/androidTestDebug/java/SofortFormTest.kt
@@ -17,6 +17,11 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.Until
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.paymentsheet.example.R
 import com.stripe.android.paymentsheet.example.activity.PaymentSheetPlaygroundActivity
@@ -30,16 +35,21 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.security.InvalidParameterException
 import java.util.concurrent.Semaphore
+import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
 internal class SofortFormTest {
     @get:Rule
     val composeTestRule = createEmptyComposeRule()
 
+    private lateinit var device: UiDevice
+
     @Before
     fun before() {
         androidx.test.espresso.intent.Intents.init()
+        device = UiDevice.getInstance(getInstrumentation())
     }
 
     @After
@@ -58,7 +68,6 @@ internal class SofortFormTest {
                 Checkout.Pay,
                 Billing.Off
             ),
-            completed = true,
         ) {
             try {
                 onView(withId(com.stripe.android.paymentsheet.R.id.recycler))
@@ -77,15 +86,19 @@ internal class SofortFormTest {
 
     @Test
     fun newUserComplete() {
+        val testParameters = TestParameters(
+            Customer.New,
+            GooglePayState.Off,
+            Currency.EUR,
+            Checkout.Pay,
+            Billing.Off,
+            AuthorizationParameters(
+                Browser.Opera,
+                AuthorizeAction.Cancel
+            )
+        )
         runComplete(
-            TestParameters(
-                Customer.New,
-                GooglePayState.Off,
-                Currency.EUR,
-                Checkout.Pay,
-                Billing.Off
-            ),
-            completed = true,
+            testParameters,
         ) {
             try {
                 onView(withId(com.stripe.android.paymentsheet.R.id.payment_methods_recycler))
@@ -100,6 +113,7 @@ internal class SofortFormTest {
                 // Item is already selected.
             }
 
+            // TODO: Need to fill in these fields based on form spec.
             composeTestRule.onNodeWithText("Name").performTextInput("My name")
             composeTestRule.onNodeWithText("Email").performTextInput("email@email.com")
 
@@ -107,7 +121,7 @@ internal class SofortFormTest {
         }
     }
 
-    private fun runComplete(testParameters: TestParameters, completed: Boolean, logic: () -> Unit) {
+    private fun runComplete(testParameters: TestParameters, logic: () -> Unit) {
         val scenario = launch(PaymentSheetPlaygroundActivity::class.java)
         var resultValue: String? = null
         val callbackLock = Semaphore(1)
@@ -130,74 +144,72 @@ internal class SofortFormTest {
         IdlingRegistry.getInstance().register(multiStepUIIdlingResource)
         IdlingRegistry.getInstance().register(transitionFragmentResource)
 
-        LabelIdButton(R.string.reload_paymentsheet).click()
+        EspressoLabelIdButton(R.string.reload_paymentsheet).click()
         onView(withText(R.string.checkout_complete)).perform(click())
 
         logic()
+
+
+        onView(withId(R.id.buy_button)).perform(click())
+
+        // TODO: Need to rid of this sleep
+        TimeUnit.SECONDS.sleep(1)
+        if (SelectBrowserWindow.exists(device)) {
+            testParameters.authorization.browser.click(device)
+        }
+
+        if (AuthorizeWindow.exists(device, testParameters.authorization.browser)) {
+            AuthorizePageLoaded.blockUntilLoaded(device)
+            when (testParameters.authorization.action) {
+                AuthorizeAction.Authorize, AuthorizeAction.Fail ->
+                    testParameters.authorization.action.click(device)
+                AuthorizeAction.Cancel ->
+                    device.pressBack()
+            }
+        }
+
+        // TODO: Need to handle cancel/fail authorization - do it again but press success this time?
 
         IdlingRegistry.getInstance().unregister(singleStepUIIdlingResource)
         IdlingRegistry.getInstance().unregister(multiStepUIIdlingResource)
         IdlingRegistry.getInstance().unregister(transitionFragmentResource)
 
-        onView(withId(R.id.buy_button)).perform(click())
-
-        if (completed) {
-            callbackLock.acquire()
-            assertThat(resultValue).isEqualTo("Completed")
-            callbackLock.release()
-        }
+        callbackLock.acquire()
+        assertThat(resultValue).isEqualTo("Completed")
+        callbackLock.release()
     }
 
-    /**
-    //     * Perform action of waiting until the element is accessible & not shown.
-    //     * @param viewId The id of the view to wait for.
-    //     * @param millis The timeout of until when to wait for.
-    //     */
-//    fun waitUntilShown(viewId: Int, millis: Long): ViewAction? {
-//        return object : ViewAction {
-//            override fun getConstraints(): Matcher<View> {
-//                return isRoot()
-//            }
-//
-//            override fun getDescription(): String {
-//                return "wait for a specific view with id <$viewId> is shown during $millis millis."
-//            }
-//
-//            override fun perform(uiController: UiController, view: View?) {
-//                uiController.loopMainThreadUntilIdle()
-//                val startTime = System.currentTimeMillis()
-//                val endTime = startTime + millis
-//                val viewMatcher: Matcher<View> = withId(viewId)
-//                do {
-//                    for (child in TreeIterables.breadthFirstViewTraversal(view)) {
-//                        // found view with required ID
-//                        if (viewMatcher.matches(child) && child.isShown) {
-//                            return
-//                        }
-//                    }
-//                    uiController.loopMainThreadForAtLeast(50)
-//                } while (System.currentTimeMillis() < endTime)
-//                throw PerformException.Builder()
-//                    .withActionDescription(this.description)
-//                    .withViewDescription(HumanReadables.describe(view))
-//                    .withCause(TimeoutException())
-//                    .build()
-//            }
-//        }
-//    }
-    open class LabelIdButton(@StringRes val label: Int) {
+    open class EspressoLabelIdButton(@StringRes val label: Int) {
         fun click() {
             onView(withText(label)).perform(ViewActions.click())
         }
     }
 
-    abstract class IdButton(@IntegerRes val id: Int) {
+    abstract class EspressoIdButton(@IntegerRes val id: Int) {
         fun click() {
             onView(withId(id)).perform(ViewActions.click())
         }
     }
 
-    sealed class PaymentSelection(@StringRes id: Int) : LabelIdButton(id) {
+    open class UiAutomatorText(
+        private val label: String,
+        className: String = "android.widget.TextView"
+    ) {
+        private val selector = UiSelector().textContains(label)
+            .className(className)
+
+        fun click(device: UiDevice) {
+            if (!exists(device)) {
+                throw InvalidParameterException("Text button not found: $label")
+            }
+            device.findObject(selector).click()
+        }
+
+        fun exists(device: UiDevice) =
+            device.findObject(selector).exists()
+    }
+
+    sealed class PaymentSelection(@StringRes id: Int) : EspressoLabelIdButton(id) {
         object Card : PaymentSelection(R.string.stripe_paymentsheet_payment_method_card)
         object Sofort : PaymentSelection(R.string.stripe_paymentsheet_payment_method_sofort)
         object iDEAL : PaymentSelection(R.string.stripe_paymentsheet_payment_method_ideal)
@@ -211,33 +223,82 @@ internal class SofortFormTest {
         val googlePayState: GooglePayState,
         val currency: Currency,
         val checkout: Checkout,
-        val billing: Billing
+        val billing: Billing,
+        val authorization: AuthorizationParameters = AuthorizationParameters(
+            Browser.Chrome,
+            AuthorizeAction.Authorize
+        )
     )
 
-    sealed class Billing(@IntegerRes id: Int) : IdButton(id) {
+    data class AuthorizationParameters(
+        val browser: Browser,
+        val action: AuthorizeAction
+    )
+
+    sealed class Billing(@IntegerRes id: Int) : EspressoIdButton(id) {
         object On : Billing(R.id.default_billing_on_button)
         object Off : Billing(R.id.default_billing_off_button)
     }
 
-    sealed class Checkout(@StringRes id: Int) : LabelIdButton(id) {
+    sealed class Checkout(@StringRes id: Int) : EspressoLabelIdButton(id) {
         object Pay : Checkout(R.string.payment)
         object PayWithSetup : Checkout(R.string.payment_with_setup)
         object Setup : Checkout(R.string.setup)
     }
 
-    sealed class Currency(@StringRes id: Int) : LabelIdButton(id) {
+    sealed class Currency(@StringRes id: Int) : EspressoLabelIdButton(id) {
         object USD : Currency(R.string.currency_usd)
         object EUR : Currency(R.string.currency_eur)
     }
 
-    sealed class GooglePayState(@IntegerRes id: Int) : IdButton(id) {
+    sealed class GooglePayState(@IntegerRes id: Int) : EspressoIdButton(id) {
         object On : GooglePayState(R.id.google_pay_on_button)
         object Off : GooglePayState(R.id.google_pay_off_button)
     }
 
-    sealed class Customer(@StringRes id: Int) : LabelIdButton(id) {
+    sealed class Customer(@StringRes id: Int) : EspressoLabelIdButton(id) {
         object Guest : Customer(R.string.customer_guest)
         object New : Customer(R.string.customer_new)
         object Returning : Customer(R.string.customer_returning)
+    }
+
+    // No 3DS2 support
+    sealed class AuthorizeAction(
+        name: String,
+        className: String
+    ) : UiAutomatorText(name, className) {
+        object Authorize : AuthorizeAction("AUTHORIZE TEST PAYMENT", "android.widget.Button")
+        object Cancel : AuthorizeAction("", "")
+        object Fail : AuthorizeAction("FAIL TEST PAYMENT", "android.widget.Button")
+    }
+
+    object SelectBrowserWindow : UiAutomatorText("Verify your payment")
+    object AuthorizeWindow {
+        fun exists(device: UiDevice, browser: Browser?) =
+            device.findObject(getSelector(browser)).exists()
+
+        private fun getSelector(browser: Browser?) = UiSelector()
+            .packageName(browser?.packageName)
+            .resourceId(browser?.resourdID)
+    }
+
+    object AuthorizePageLoaded {
+        fun blockUntilLoaded(device: UiDevice) {
+            device.wait(
+                Until.findObject(
+                    By.textContains("test payment page")
+                ),
+                10000
+            )
+        }
+    }
+
+    sealed class Browser(name: String, val packageName: String) :
+        UiAutomatorText(name) {
+        val resourdID = "$packageName:id/action_bar_root"
+
+        object Chrome : Browser("Chrome", "com.android.chrome")
+        object Opera : Browser("Opera", "com.opera.browser")
+        object Firefox : Browser("Firefox", "org.mozilla.firefox")
     }
 }
