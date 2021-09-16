06400ddcb993bf8b2ec9c5fb6996fed200dc17e8
Chen Cen <ccen@stripe.com>
2021-09-16 12:30:55 -0700

    move PaymentSheetViewModel injection to PaymentSheetLauncher

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/injection/PaymentSheetLauncherModule.kt
@@ -0,0 +1,30 @@
+package com.stripe.android.paymentsheet.injection
+
+import android.app.Application
+import android.content.Context
+import com.stripe.android.payments.core.injection.PRODUCT_USAGE
+import com.stripe.android.paymentsheet.analytics.EventReporter
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module(
+    subcomponents = [PaymentSheetViewModelSubcomponent::class]
+)
+internal abstract class PaymentSheetLauncherModule {
+    @Binds
+    abstract fun bindsApplicationForContext(application: Application): Context
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideEventReporterMode(): EventReporter.Mode = EventReporter.Mode.Complete
+
+        @Provides
+        @Singleton
+        @Named(PRODUCT_USAGE)
+        fun provideProductUsageTokens() = setOf("PaymentSheet")
+    }
+}
