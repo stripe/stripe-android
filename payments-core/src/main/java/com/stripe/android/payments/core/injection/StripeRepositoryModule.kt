dc22391a7fe368f49c5652ceac2bd97d0ad75b37
Chen Cen <ccen@stripe.com>
2021-09-28 18:24:18 -0700

    inject StripeApiRepository

--- payments-core/src/main/java/com/stripe/android/payments/core/injection/StripeRepositoryModule.kt
+++ payments-core/src/main/java/com/stripe/android/payments/core/injection/StripeRepositoryModule.kt
@@ -4,16 +4,11 @@ import android.content.Context
 import androidx.annotation.RestrictTo
 import com.stripe.android.Logger
 import com.stripe.android.networking.AnalyticsRequestExecutor
-import com.stripe.android.networking.AnalyticsRequestFactory
 import com.stripe.android.networking.DefaultAnalyticsRequestExecutor
 import com.stripe.android.networking.StripeApiRepository
 import com.stripe.android.networking.StripeRepository
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
-import javax.inject.Named
-import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 /**
  * A [Module] to provide [StripeRepository] and its corresponding dependencies.
@@ -28,25 +23,8 @@ abstract class StripeRepositoryModule {
         default: DefaultAnalyticsRequestExecutor
     ): AnalyticsRequestExecutor
 
-    companion object {
-        @Provides
-        @Singleton
-        internal fun provideStripeRepository(
-            appContext: Context,
-            @Named(PUBLISHABLE_KEY) publishableKeyProvider: () -> String,
-            @IOContext workContext: CoroutineContext,
-            @Named(PRODUCT_USAGE) productUsageTokens: Set<String>,
-            analyticsRequestFactory: AnalyticsRequestFactory,
-            analyticsRequestExecutor: AnalyticsRequestExecutor,
-            logger: Logger
-        ): StripeRepository = StripeApiRepository(
-            appContext,
-            publishableKeyProvider,
-            logger = logger,
-            workContext = workContext,
-            productUsageTokens = productUsageTokens,
-            analyticsRequestFactory = analyticsRequestFactory,
-            analyticsRequestExecutor = analyticsRequestExecutor
-        )
-    }
+    @Binds
+    internal abstract fun bindsStripeRepository(
+        stripeApiRepository: StripeApiRepository
+    ): StripeRepository
 }
