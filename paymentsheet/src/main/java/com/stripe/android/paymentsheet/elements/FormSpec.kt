437c3ca1153ce060445b2c9a78bdee8aa78dd37f
Michelle Brubaker <michelleb@stripe.com>
2021-10-05 09:27:29 -0400

    With PIRequirement, SI Requirement, and ReuseRequirement

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/FormSpec.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/FormSpec.kt
@@ -53,84 +53,32 @@ internal data class Blah(
 internal data class PaymentMethodFormSpec(
     val paramKey: MutableMap<String, Any?>,
 
-    val PIRequirement: Set<PIRequirement>, // shipping, delayed
-    val PIRequirementWithSFU: Set<PIWithSFURequirement>,
-    val SIRequirement: Set<SIRequirement>,
-    val customerRequirement: Set<CustomerCardRequirement>,
+    val PIRequirement: Set<PIRequirement>?,
+    val SIRequirement: Set<SIRequirement>?,
 
-    /** Here consider the type of the saved payment method and what it's requirements are
-     * for instance after saving Sofort, Bancontact, and iDEAL it appears as a SEPA payment
-     * method which has different effects from it's original form
-     */
-    val reuseRequirement: Set<MandateRequirement>, // madate, delayed
+    // Cannot be null if you want to use setup intent, or sfu set by merchant
+    // or by user.  It can be an empty set if you have no requirements but want
+    // these scenarios supported.
+    val customerSavedPMRequirement: Set<CustomerSavedPMRequirement>?,
 
     /** Unordered list of specs **/
     val layoutSpec: LayoutSpec
 )
 
-/**
- * This is the list of requirements for the form
- */
-internal data class FormRequirement(
-    val intentRequirement: IntentRequirement,
-    val sdkRequirements: Set<SdkRequirement>
-) {
-    val requirements
-        get() = intentRequirement.requirements.plus(sdkRequirements)
-}
+sealed interface Requirement
+sealed interface PIRequirement : Requirement
+sealed interface PIWithSFURequirement : Requirement
+sealed interface SIRequirement : Requirement
+sealed interface CustomerSavedPMRequirement : Requirement
 
-sealed class IntentRequirement(
-    val requirements: Set<Requirement>
-) {
-    internal class SetupIntentRequirement(
-        val mandate: MandateRequirement
-    ) : IntentRequirement(setOf(mandate))
-
-    internal data class PaymentIntentRequirement(
-        val shipping: Set<ShippingIntentRequirement> = emptySet(),
-        val mandate: MandateRequirement
-    ) : IntentRequirement(shipping.plus(mandate))
-}
-
-
-sealed class Requirement
-sealed interface PIRequirement
-sealed interface PIWithSFURequirement
-sealed interface SIRequirement
-sealed interface CustomerCardRequirement
-
-object Mandate : PIRequirement, PIWithSFURequirement, SIRequirement, CustomerCardRequirement
-object Shipping : PIRequirement
-object Delayed : PIRequirement, PIWithSFURequirement, SIRequirement, CustomerCardRequirement
+object Mandate : PIRequirement, PIWithSFURequirement, SIRequirement, CustomerSavedPMRequirement
+object Delayed : PIRequirement, PIWithSFURequirement, SIRequirement, CustomerSavedPMRequirement
 object AfterpayCancel : PIRequirement
-
-
-internal class SetupFutureUsageRequirement private constructor(
-    val set: Boolean,
-    val modifiable: Boolean
-) {
-    companion object {
-        fun createSet() = SetupFutureUsageRequirement(set = true, modifiable = false)
-        fun createNotSet(modifiable: Boolean) = SetupFutureUsageRequirement(set = false, modifiable)
-    }
-}
-
-sealed class MandateRequirement : Requirement() {
-    object Required : MandateRequirement()
-    object NotRequired : MandateRequirement()
-}
-
-sealed class SdkRequirement : Requirement() {
-    object AfterpayCancelSupport : SdkRequirement()
-    object AllowDelayedPaymentMethods : SdkRequirement()
-}
-
-sealed class ShippingIntentRequirement : Requirement() {
+sealed class ShippingIntentRequirement : PIRequirement {
     /**
      * Different payment method might require some subset of the address
      * fields, so they must be individually declared
      */
-    object NotRequired : ShippingIntentRequirement()
     object AddressLine1 : ShippingIntentRequirement()
     object AddressLine2 : ShippingIntentRequirement()
     object AddressCountry : ShippingIntentRequirement()
--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PaymentMethodSpecKtx.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PaymentMethodSpecKtx.kt
@@ -6,12 +6,14 @@ import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.SetupIntent
 import com.stripe.android.model.StripeIntent
 import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.elements.AfterpayCancel
 import com.stripe.android.paymentsheet.elements.Blah
-import com.stripe.android.paymentsheet.elements.FormRequirement
-import com.stripe.android.paymentsheet.elements.MandateRequirement
+import com.stripe.android.paymentsheet.elements.CustomerSavedPMRequirement
+import com.stripe.android.paymentsheet.elements.Delayed
+import com.stripe.android.paymentsheet.elements.Mandate
+import com.stripe.android.paymentsheet.elements.PIRequirement
 import com.stripe.android.paymentsheet.elements.PaymentMethodFormSpec
-import com.stripe.android.paymentsheet.elements.Requirement
-import com.stripe.android.paymentsheet.elements.SdkRequirement
+import com.stripe.android.paymentsheet.elements.SIRequirement
 import com.stripe.android.paymentsheet.elements.ShippingIntentRequirement
 import com.stripe.android.paymentsheet.model.SupportedPaymentMethod
 
@@ -21,55 +23,48 @@ internal data class SavedCustomerCardRequirementMatcher(
 ) {
     val mode: Mode = Mode.SavedCustomerCard
 
-    /**
-     * Get the cards that are supported given the current cpabilities.  For saved cards, this
-     * should never be cards that are only offered for one time use.
-     */
-    internal fun getSupportedSavedCustomerCards(
-        stripeIntent: StripeIntent,
-        config: PaymentSheet.Configuration?
-    ) = when(stripeIntent){
-        is PaymentIntent -> {
-
-        }
-        is SetupIntent -> {
-
-        }
-    }
 }
 
 internal data class AddNewRequirementMatcher(
     val stripeIntent: StripeIntent,
     val config: PaymentSheet.Configuration?
 ) {
-    val mode: Mode = Mode.AddNew
+    /**
+     * Get the LPMS that are supported given the current cpabilities.
+     */
+    internal fun getSupportedSavedCustomerPMs() =
+        stripeIntent.paymentMethodTypes.asSequence().mapNotNull {
+            SupportedPaymentMethod.fromCode(it)
+        }.filter {
+            checkCustomerSavedRequirements(it.formSpec.customerSavedPMRequirement)
+        }.filter {
+            // Here SFU doesn't matter because it is already saved.
+            when (stripeIntent) {
+                is PaymentIntent -> checkPIRequirement(it.formSpec.PIRequirement)
+                is SetupIntent -> checkSIRequirement(it.formSpec.SIRequirement)
+            }
+        }
 
     /**
      * This will get the form layout for the supported method that matches the top pick for the
      * payment method.  It should be known that this payment method has a form
      * that matches the capabilities already.
      */
-    internal fun getForm(spec: PaymentMethodFormSpec) = getSpecWithFullfilledRequirements(spec)
+    internal fun getPMAddForm(spec: PaymentMethodFormSpec) = getSpecWithFullfilledRequirements(spec)
 
     /**
      * This will return a list of payment methods that have a supported form given the capabilities.
      */
     @VisibleForTesting
-    internal fun getSupportedPaymentMethods(
-    ): List<SupportedPaymentMethod> {
-        return stripeIntent.paymentMethodTypes.asSequence().mapNotNull {
-            SupportedPaymentMethod.fromCode(it)
-        }.filter { supportedPaymentMethod ->
-            satisfiedBy(supportedPaymentMethod.formSpec)
-        }//TODO.filter { it == SupportedPaymentMethod.Card }
-            .toList()
-    }
+    internal fun getPMsToAdd(
+    ) = stripeIntent.paymentMethodTypes.asSequence().mapNotNull {
+        SupportedPaymentMethod.fromCode(it)
+    }.filter { supportedPaymentMethod ->
+        getSpecWithFullfilledRequirements(supportedPaymentMethod.formSpec) != null
+    }//TODO.filter { it == SupportedPaymentMethod.Card }
+        .toList()
 
-    fun satisfiedBy(
-        spec: PaymentMethodFormSpec
-    ) = getSpecWithFullfilledRequirements(spec) != null
-
-    fun getSpecWithFullfilledRequirements(spec: PaymentMethodFormSpec): Blah? {
+    private fun getSpecWithFullfilledRequirements(spec: PaymentMethodFormSpec): Blah? {
         val oneTimeUse = Blah(spec.layoutSpec, false, false)
         val merchantRequestedSave = Blah(spec.layoutSpec, false, true)
         val userSelectableSave = Blah(spec.layoutSpec, true, null)
@@ -80,22 +75,26 @@ internal data class AddNewRequirementMatcher(
                 ) {
                     if (config?.customer != null &&
                         allHaveKnownReuseSupport(stripeIntent.paymentMethodTypes) &&
-                        fullfilledRequirements(spec.PIRequirementWithSave)
+                        checkPIRequirement(spec.PIRequirement) &&
+                        checkCustomerSavedRequirements(spec.customerSavedPMRequirement)
                     ) {
                         userSelectableSave
-                    } else if (fullfilledRequirements(spec.PIRequirement)) {
+                    } else if (checkPIRequirement(spec.PIRequirement)) {
                         oneTimeUse
                     } else {
                         null
                     }
-                } else if (fullfilledRequirements(spec.PIRequirementWithSave)) {
+                } else if (
+                    checkPIRequirement(spec.PIRequirement) &&
+                    checkCustomerSavedRequirements(spec.customerSavedPMRequirement)
+                ) {
                     merchantRequestedSave
                 } else {
                     null
                 }
             }
             is SetupIntent -> {
-                if (fullfilledRequirements(spec.SIRequirement)) {
+                if (checkSIRequirement(spec.SIRequirement)) {
                     merchantRequestedSave
                 } else {
                     null
@@ -104,119 +103,52 @@ internal data class AddNewRequirementMatcher(
         }
     }
 
-    /**
-     * Capabilities are fulfilled only if all requirements are in the set of capabilities
-     */
-    private fun fullfilledRequirements(
-        requirements: Set<Requirement>
-    ) = !requirements.map {
+    private fun checkPIRequirement(requirements: Set<PIRequirement>?) = requirements?.map {
         when (it) {
-            is MandateRequirement -> it.isRequirementMet(stripeIntent, config, mode)
-            is SdkRequirement -> it.isRequirementMet(stripeIntent, config, mode)
-            is ShippingIntentRequirement -> it.isRequirementMet(stripeIntent, config, mode)
+            AfterpayCancel -> false
+            Delayed -> config?.allowsDelayedPaymentMethods == true
+            Mandate -> true
+            is ShippingIntentRequirement -> it.isRequirementMet(stripeIntent)
         }
-    }.contains(false)
-}
+    }?.contains(false) == false
 
+    private fun checkSIRequirement(requirements: Set<SIRequirement>?) = requirements?.map {
+        when (it) {
+            Mandate -> true
+            Delayed -> config?.allowsDelayedPaymentMethods == true
+        }
+    }?.contains(false) == false
 
-//private fun PaymentMethodFormSpec.getSpecWithFullfilledRequirementssdfsdfsdf(
-//)  : Blah? {
-//    val oneTimeUse = Blah(layoutSpec, false, false)
-//    val merchantRequestedSave = Blah(layoutSpec, false, true)
-//    val userSelectableSave = Blah(layoutSpec, true, null)
-//
-//    when (stripeIntent) {
-//        is PaymentIntent -> {
-//            if ((stripeIntent.setupFutureUsage == null || stripeIntent.setupFutureUsage == StripeIntent.Usage.OneTime)
-//            ) {
-//                if (config?.customer != null &&
-//                    allHaveKnownReuseSupport(stripeIntent.paymentMethodTypes) &&
-//                    fullfilledRequirements(this.PIRequirementWithSave, stripeIntent, config, mode)
-//                ) {
-//                    userSelectableSave
-//                } else if(fullfilledRequirements(this.PIRequirement, stripeIntent, config, mode)){
-//                    oneTimeUse
-//                }
-//            } else if(fullfilledRequirements(this.PIRequirementWithSave, stripeIntent, config, mode)){
-//                merchantRequestedSave
-//            }
-//            else {
-//                null
-//            }
-//        }
-//        is SetupIntent -> {
-//            if(fullfilledRequirements(this.SIWithSave, stripeIntent, config, mode)) {
-//                merchantRequestedSave
-//            }
-//            else {
-//                null
-//            }
-//        }
-//    }
-//}
+    private fun checkCustomerSavedRequirements(requirements: Set<CustomerSavedPMRequirement>?) =
+        requirements?.map {
+            when (it) {
+                Mandate -> false
+                Delayed -> config?.allowsDelayedPaymentMethods == true
+            }
+        }?.contains(false) == false
 
-internal enum class Mode {
-    AddNew,
-    SavedCustomerCard
-}
-
-
-private fun isSetupIntentOrSetupFutureUsageSet(stripeIntent: StripeIntent) =
-    when (stripeIntent) {
-        is PaymentIntent -> stripeIntent.setupFutureUsage != null && stripeIntent.setupFutureUsage != StripeIntent.Usage.OneTime
-        is SetupIntent -> true
+    private fun allHaveKnownReuseSupport(paymentMethodsInIntent: List<String?>): Boolean {
+        // The following PaymentMethods are know to work when
+        // PaymentIntent.setup_future_usage = on/off session
+        // This list is different from the PaymentMethod.Type.isReusable
+        // It is expected that this check will be removed soon
+        val knownReusable = setOf(
+            PaymentMethod.Type.Alipay.code,
+            PaymentMethod.Type.Card.code,
+            PaymentMethod.Type.SepaDebit.code,
+            PaymentMethod.Type.AuBecsDebit.code,
+            PaymentMethod.Type.Bancontact.code,
+            PaymentMethod.Type.Sofort.code,
+            PaymentMethod.Type.BacsDebit.code,
+            PaymentMethod.Type.Ideal.code
+        )
+        return paymentMethodsInIntent.filterNot { knownReusable.contains(it) }.isEmpty()
     }
-
-
-private fun allHaveKnownReuseSupport(paymentMethodsInIntent: List<String?>): Boolean {
-    // The following PaymentMethods are know to work when
-    // PaymentIntent.setup_future_usage = on/off session
-    // This list is different from the PaymentMethod.Type.isReusable
-    // It is expected that this check will be removed soon
-    val knownReusable = setOf(
-        PaymentMethod.Type.Alipay.code,
-        PaymentMethod.Type.Card.code,
-        PaymentMethod.Type.SepaDebit.code,
-        PaymentMethod.Type.AuBecsDebit.code,
-        PaymentMethod.Type.Bancontact.code,
-        PaymentMethod.Type.Sofort.code,
-        PaymentMethod.Type.BacsDebit.code,
-        PaymentMethod.Type.Ideal.code
-    )
-    return paymentMethodsInIntent.filterNot { knownReusable.contains(it) }.isEmpty()
-}
-
-
-/**
- * Capabilities is used to refer to what is supported by the combination of the code base,
- * configuration, and values in the StripeIntent. (i.e. SetupIntent, PaymentIntent w/SFU,
- * customer, allowedDelayedSettlement)
- */
-internal fun getAllCapabilities(
-    stripeIntent: StripeIntent,
-    config: PaymentSheet.Configuration?
-) = FormRequirement(
-    getIntentRequirements(stripeIntent, config),
-    setOfNotNull(
-        SdkRequirement.AllowDelayedPaymentMethods.takeIf { config?.allowsDelayedPaymentMethods == true },
-    )
-)
-
-internal fun MandateRequirement.isRequirementMet(
-    stripeIntent: StripeIntent,
-    config: PaymentSheet.Configuration?,
-    mode: Mode
-) = when (this) {
-    MandateRequirement.NotRequired -> true
-    MandateRequirement.Required ->
-        mode != Mode.SavedCustomerCard && !isSetupIntentOrSetupFutureUsageSet(stripeIntent)
 }
 
 
 internal fun ShippingIntentRequirement.isRequirementMet(
-    stripeIntent: StripeIntent,
-    config: PaymentSheet.Configuration?,
-    mode: Mode,
+    stripeIntent: StripeIntent
 ) {
     when (stripeIntent) {
         is PaymentIntent -> {
@@ -246,83 +178,3 @@ internal fun ShippingIntentRequirement.isRequirementMet(
         }
     }
 }
-
-internal fun SdkRequirement.isRequirementMet(
-    stripeIntent: StripeIntent,
-    config: PaymentSheet.Configuration?,
-    mode: Mode
-) {
-    when (this) {
-        SdkRequirement.AfterpayCancelSupport -> false
-        SdkRequirement.AllowDelayedPaymentMethods -> config?.allowsDelayedPaymentMethods == true
-    }
-}
-
-/**
- * These are the capabilities that define if the intent or configuration requires one time use
- * merchant requres save through SetupIntent or PaymentIntent with setup_future_usage = on/off session
- */
-//private fun getIntentRequirements(
-//    stripeIntent: StripeIntent,
-//    config: PaymentSheet.Configuration?,
-//) = when (stripeIntent) {
-//    is PaymentIntent -> {
-//        val shippingRequirement = intentShippingCapabilities(stripeIntent)
-//        if (stripeIntent.setupFutureUsage == null || stripeIntent.setupFutureUsage == StripeIntent.Usage.OneTime) {
-//            if (config?.customer != null &&
-//                allHaveKnownReuseSupport(stripeIntent.paymentMethodTypes)
-//            ) {
-//                IntentRequirement.PaymentIntentRequirement(
-//                    SetupFutureUsageRequirement.createNotSet(true),
-//                    shippingRequirement,
-//                    MandateRequirement.Required or NotRquired
-//                )
-//            } else {
-//                IntentRequirement.PaymentIntentRequirement(
-//                    SetupFutureUsageRequirement.createNotSet(false),
-//                    shippingRequirement,
-//                    MandateRequirement.NotRequired
-//                )
-//            }
-//        } else {
-//            IntentRequirement.PaymentIntentRequirement(
-//                SetupFutureUsageRequirement.createSet(),
-//                shippingRequirement,
-//                MandateRequirement.NotRequired
-//            )
-//        }
-//    }
-//    is SetupIntent -> {
-//        IntentRequirement.SetupIntentRequirement(
-//            MandateRequirement.Required
-//        )
-//    }
-//}
-
-
-/**
- * This gets the specific shipping capabilities in the intent
- */
-//private fun intentShippingCapabilities(stripeIntent: PaymentIntent?) =
-//    stripeIntent?.let { paymentIntent ->
-//        setOfNotNull(
-//            ShippingIntentRequirement.Name.takeIf {
-//                paymentIntent.shipping?.name != null
-//            },
-//            ShippingIntentRequirement.AddressLine1.takeIf {
-//                paymentIntent.shipping?.address?.line1 != null
-//            },
-//            ShippingIntentRequirement.AddressLine2.takeIf {
-//                paymentIntent.shipping?.address?.line1 != null
-//            },
-//            ShippingIntentRequirement.AddressCountry.takeIf {
-//                paymentIntent.shipping?.address?.country != null
-//            },
-//            ShippingIntentRequirement.AddressState.takeIf {
-//                paymentIntent.shipping?.address?.state != null
-//            },
-//            ShippingIntentRequirement.AddressPostal.takeIf {
-//                paymentIntent.shipping?.address?.postalCode != null
-//            },
-//        )
-//    } ?: emptySet()
