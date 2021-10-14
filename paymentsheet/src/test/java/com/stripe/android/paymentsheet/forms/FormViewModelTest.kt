643b96352c69646e9a670f628e4846ab9ac7bcef
Michelle Brubaker <michelleb@stripe.com>
2021-10-14 11:56:45 -0400

    ktformat

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/forms/FormViewModelTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/forms/FormViewModelTest.kt
@@ -148,9 +148,9 @@ internal class FormViewModelTest {
         val args = COMPOSE_FRAGMENT_ARGS
         val formViewModel = FormViewModel(
             LayoutSpec.create(
-                    emailSection,
-                    countrySection,
-                    SaveForFutureUseSpec(listOf(emailSection))
+                emailSection,
+                countrySection,
+                SaveForFutureUseSpec(listOf(emailSection))
             ),
             args,
             resourceRepository = resourceRepository,
