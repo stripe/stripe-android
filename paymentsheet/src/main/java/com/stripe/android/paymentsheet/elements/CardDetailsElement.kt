c41ec5d9d70907afabfa6f701a1715bfdd0213ee
Michelle Brubaker <michelleb@stripe.com>
2021-11-02 17:37:48 -0400

    Set the expiration month and date correctly in the formFieldValues

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/CardDetailsElement.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/CardDetailsElement.kt
@@ -1,7 +1,9 @@
 package com.stripe.android.paymentsheet.elements
 
+import com.stripe.android.paymentsheet.R
 import com.stripe.android.paymentsheet.paymentdatacollection.FormFragmentArguments
 import kotlinx.coroutines.flow.combine
+import java.util.Calendar
 
 internal class CardDetailsElement(
     identifier: IdentifierSpec,
@@ -23,20 +25,53 @@ internal class CardDetailsElement(
         controller.cvcElement.controller.formFieldValue,
         controller.expirationDateElement.controller.formFieldValue
     ) { number, cvc, expirationDate ->
-        val month = ""
-        if (expirationDate.value?.getOrNull(0) == '0' || expirationDate.value?.getOrNull(0) == '1') {
-            value = expirationDate.value?.take(2)
-
+        var month = -1
+        var year = -1
+        expirationDate.value?.let { date ->
+            // TODO: Duplicate of DateConfig
+            val newString =
+                if ((
+                        date.isNotBlank() &&
+                            !(date[0] == '0' || date[0] == '1')
+                        ) ||
+                    (
+                        (date.length > 1) &&
+                            (date[0] == '1' && requireNotNull(date[1].digitToInt()) > 2)
+                        )
+                ) {
+                    "0$date"
+                } else {
+                    date
+                }
+            if (newString.length == 4) {
+                month = requireNotNull(newString.take(2).toIntOrNull())
+                year = requireNotNull(newString.takeLast(2).toIntOrNull())
+                val yearMinus1900 = year + (2000 - 1900)
+                val currentYear = Calendar.getInstance().get(Calendar.YEAR) - 1900
+                val currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1
+                if ((yearMinus1900 - currentYear) < 0) {
+                    TextFieldStateConstants.Error.Invalid(R.string.invalid_expiry_year_past)
+                } else if ((yearMinus1900 - currentYear) > 50) {
+                    TextFieldStateConstants.Error.Invalid(R.string.invalid_expiry_year)
+                } else if ((yearMinus1900 - currentYear) == 0 && currentMonth > month) {
+                    TextFieldStateConstants.Error.Invalid(R.string.invalid_expiry_year_past)
+                } else if (month !in 1..12) {
+                    TextFieldStateConstants.Error.Incomplete(R.string.invalid_expiry_month)
+                } else {
+                    TextFieldStateConstants.Valid.Full
+                }
+            }
         }
+
 
         listOf(
             controller.numberElement.identifier to number,
             controller.cvcElement.identifier to cvc,
             IdentifierSpec.Generic("exp_month") to expirationDate.copy(
-                value = expirationDate.value?.take(2)
+                value = month.toString()
             ),
             IdentifierSpec.Generic("exp_year") to expirationDate.copy(
-                value = expirationDate.value?.takeLast(2)
+                value = year.toString()
             )
         )
     }
