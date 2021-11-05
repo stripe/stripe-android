370748f2bfb4f50812654dab079fc3f011b2735b
Skyler Reimer <skyler@stripe.com>
2021-11-05 09:32:09 -0700

    address comments, disable Klarna until release

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/KlarnaCountrySpec.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/elements/KlarnaCountrySpec.kt
@@ -2,8 +2,5 @@ package com.stripe.android.paymentsheet.elements
 
 /**
  * This is the specification for a klarna country field
- * @property onlyShowCountryCodes: a list of country code that should be shown.  If empty all
- * countries will be shown.
  */
-internal data class KlarnaCountrySpec(val onlyShowCountryCodes: Set<String> = emptySet()) :
-    SectionFieldSpec(IdentifierSpec.Country)
+internal class KlarnaCountrySpec : SectionFieldSpec(IdentifierSpec.Country)
