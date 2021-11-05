df7d925286ff7389da6bc71507cdad2417b33d4c
Skyler Reimer <skyler@stripe.com>
2021-11-05 09:32:09 -0700

    address comments, disable Klarna until release

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/model/SupportedPaymentMethod.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/model/SupportedPaymentMethod.kt
@@ -329,9 +329,7 @@ internal enum class SupportedPaymentMethod(
          * This is a list of the payment methods that we are allowing in the release
          */
         @VisibleForTesting
-        internal val exposedPaymentMethods = listOf(
-            Card, Bancontact, Sofort, Ideal, SepaDebit, Klarna
-        )
+        internal val exposedPaymentMethods = listOf(Card, Bancontact, Sofort, Ideal, SepaDebit)
 
         /**
          * This will use only those payment methods that are allowed in the release
