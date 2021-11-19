1d4d8ff0717c8c8dc7fd8272970d00cc8b8d6fa4
Skyler Reimer <skyler@stripe.com>
2021-11-19 10:50:32 -0800

    address comments

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PaypalSpec.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PaypalSpec.kt
@@ -1,20 +1,25 @@
 package com.stripe.android.paymentsheet.forms
 
 import com.stripe.android.paymentsheet.elements.LayoutSpec
-import com.stripe.android.paymentsheet.elements.billingParams
 
 /**
  * This defines the requirements for usage as a Payment Method.
  */
 internal val PaypalRequirement = PaymentMethodRequirements(
     piRequirements = emptySet(),
+
+    /**
+     * SetupIntents are not supported by this payment method. Currently Paypal should only be set up
+     * one time, but the Stripe API will let you keep doing it resulting in several paypals being
+     * listed. It's better to leave this disabled than to enable it, let users save multiple
+     * paypals, and then fix it leaving the users in a weird state. See oof #5
+     */
     siRequirements = null,
     confirmPMFromCustomer = null
 )
 
 internal val PaypalParamKey: MutableMap<String, Any?> = mutableMapOf(
     "type" to "paypal",
-    "billing_details" to billingParams
 )
 
 internal val PaypalForm = LayoutSpec.create()
