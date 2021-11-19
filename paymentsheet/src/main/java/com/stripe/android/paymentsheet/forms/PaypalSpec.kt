389280e10bd858b96c97ff25d449aef852281f6c
Skyler Reimer <skyler@stripe.com>
2021-11-19 14:08:08 -0800

    update documentation

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PaypalSpec.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PaypalSpec.kt
@@ -9,10 +9,9 @@ internal val PaypalRequirement = PaymentMethodRequirements(
     piRequirements = emptySet(),
 
     /**
-     * SetupIntents are not supported by this payment method. Currently Paypal should only be set up
-     * one time, but the Stripe API will let you keep doing it resulting in several paypals being
-     * listed. It's better to leave this disabled than to enable it, let users save multiple
-     * paypals, and then fix it leaving the users in a weird state. See oof #5
+     * SetupIntents are not supported by this payment method. Currently, for paypal (and others see
+     * oof #5) customers are not able to set up saved payment methods for reuse. The API errors if
+     * confirming PI+SFU or SI with these methods.
      */
     siRequirements = null,
     confirmPMFromCustomer = null
