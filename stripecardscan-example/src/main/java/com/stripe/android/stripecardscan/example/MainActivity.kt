9822b4da699987da56e847834227ce339549777d
Adam Wushensky <awush@stripe.com>
2021-11-22 16:58:16 -0800

    Clean up the stripecardscan example

--- stripecardscan-example/src/main/java/com/stripe/android/stripecardscan/example/MainActivity.kt
+++ stripecardscan-example/src/main/java/com/stripe/android/stripecardscan/example/MainActivity.kt
@@ -2,9 +2,17 @@ package com.stripe.android.stripecardscan.example
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.stripe.android.stripecardscan.example.activity.LaunchCardImageVerificationSheetCompleteActivity
 import com.stripe.android.stripecardscan.example.activity.PARAM_PUBLISHABLE_KEY
+import com.stripe.android.stripecardscan.example.activity.PARAM_VERIFICATION_RESULT
 import com.stripe.android.stripecardscan.example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +21,50 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val requestQueue = Volley.newRequestQueue(this)
+
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        val civLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            viewBinding.scanResultText.text = result.data?.getStringExtra(PARAM_VERIFICATION_RESULT)
+        }
+
+        viewBinding.getPublishableKeyButton.setOnClickListener {
+            requestQueue.add(
+                JsonObjectRequest(
+                    "https://stripe-card-scan-civ-example-app.glitch.me/publishable_key",
+                    { response ->
+                        viewBinding.stripePublishableKey.setText(
+                            response.get("publishable_key") as String
+                        )
+                        viewBinding.launchCompleteButton.isEnabled = true
+                    },
+                    { error ->
+                        Toast.makeText(
+                            this,
+                            "Error getting publishable key: ${error.message}",
+                            Toast.LENGTH_SHORT,
+                        )
+                            .show()
+                    }
+                )
+            )
+        }
+
+        viewBinding.stripePublishableKey.addTextChangedListener(
+            { _, _, _, _ -> /* before change */ },
+            { _, _, _, _ -> /* on change */ },
+            { s ->
+                viewBinding.launchCompleteButton.isEnabled = !s.isNullOrEmpty()
+            }
+        )
+
         viewBinding.launchCompleteButton.setOnClickListener {
-            startActivity(
+            civLauncher.launch(
                 Intent(this, LaunchCardImageVerificationSheetCompleteActivity::class.java)
                     .putExtra(
                         PARAM_PUBLISHABLE_KEY,
--- stripecardscan-example/src/main/java/com/stripe/android/stripecardscan/example/activity/LaunchCardImageVerificationSheetCompleteActivity.kt
+++ stripecardscan-example/src/main/java/com/stripe/android/stripecardscan/example/activity/LaunchCardImageVerificationSheetCompleteActivity.kt
@@ -1,21 +1,34 @@
 package com.stripe.android.stripecardscan.example.activity
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.stripe.android.stripecardscan.cardimageverification.CardImageVerificationSheet
 import com.stripe.android.stripecardscan.cardimageverification.CardImageVerificationSheetResult
 import com.stripe.android.stripecardscan.example.databinding.ActivityLaunchCardImageVerificationSheetCompleteBinding
+import org.json.JSONObject
 
 internal const val PARAM_PUBLISHABLE_KEY = "stripePublishableKey"
+internal const val PARAM_VERIFICATION_RESULT = "verificationResult"
 
 internal class LaunchCardImageVerificationSheetCompleteActivity : AppCompatActivity() {
     private val viewBinding by lazy {
         ActivityLaunchCardImageVerificationSheetCompleteBinding.inflate(layoutInflater)
     }
 
+    private lateinit var requestQueue: RequestQueue
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestQueue = Volley.newRequestQueue(this)
+
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
 
@@ -28,10 +41,54 @@ internal class LaunchCardImageVerificationSheetCompleteActivity : AppCompatActiv
         val cardImageVerificationSheet =
             CardImageVerificationSheet.create(this, stripePublishableKey)
 
+        viewBinding.generateCivIntent.setOnClickListener {
+            requestQueue.add(
+                JsonObjectRequest(
+                    Request.Method.POST,
+                    "https://stripe-card-scan-civ-example-app.glitch.me/card-set/checkout",
+                    JSONObject().apply {
+                        put("expected_card[iin]", viewBinding.requiredIin.text.toString())
+                        put("expected_card[last4]", viewBinding.requiredLast4.text.toString())
+                    },
+                    { response ->
+                        viewBinding.civIdText.setText(response.get("id") as String)
+                        viewBinding.civSecretText.setText(response.get("client_secret") as String)
+                        viewBinding.launchScanButton.isEnabled = true
+                    },
+                    { error ->
+                        Toast.makeText(
+                            this,
+                            "Error generating CIV: ${error.message}",
+                            Toast.LENGTH_SHORT,
+                        )
+                            .show()
+                    },
+                )
+            )
+        }
+
+        viewBinding.civIdText.addTextChangedListener(
+            { _, _, _, _ -> /* before change */ },
+            { _, _, _, _ -> /* on change */ },
+            { s ->
+                viewBinding.launchScanButton.isEnabled =
+                    !s.isNullOrEmpty() && !viewBinding.civSecretText.text.isNullOrEmpty()
+            }
+        )
+
+        viewBinding.civSecretText.addTextChangedListener(
+            { _, _, _, _ -> /* before change */ },
+            { _, _, _, _ -> /* on change */ },
+            { s ->
+                viewBinding.launchScanButton.isEnabled =
+                    !s.isNullOrEmpty() && !viewBinding.civIdText.text.isNullOrEmpty()
+            }
+        )
+
         viewBinding.launchScanButton.setOnClickListener {
             cardImageVerificationSheet.present(
-                viewBinding.civId.text.toString(),
-                viewBinding.civSecret.text.toString(),
+                viewBinding.civIdText.text.toString(),
+                viewBinding.civSecretText.text.toString(),
                 this::onScanFinished,
             )
         }
@@ -39,6 +96,30 @@ internal class LaunchCardImageVerificationSheetCompleteActivity : AppCompatActiv
 
     private fun onScanFinished(result: CardImageVerificationSheetResult) {
         Toast.makeText(this, result.toString(), Toast.LENGTH_LONG).show()
-        finish()
+
+        requestQueue.add(
+            JsonObjectRequest(
+                Request.Method.POST,
+                "https://stripe-card-scan-civ-example-app.glitch.me/verify",
+                JSONObject().apply {
+                    put("civ_id", viewBinding.civIdText.text.toString())
+                },
+                { response ->
+                    setResult(
+                        Activity.RESULT_OK,
+                        Intent().putExtra(PARAM_VERIFICATION_RESULT, response.toString()),
+                    )
+                    finish()
+                },
+                { error ->
+                    Toast.makeText(
+                        this,
+                        "Error getting validation payload: ${error.message}",
+                        Toast.LENGTH_SHORT,
+                    )
+                        .show()
+                },
+            )
+        )
     }
 }
