7bbd9cee2201e16bb1bc78cf6743b91447eacd4c
Adam Wushensky <84421010+awush-stripe@users.noreply.github.com>
2021-11-23 14:20:13 -0800

    Clean up stripecardscan example (#4402)
    * Return the pan on a successful scan
    
    * Remove a test CIV
    
    * Update API format for verify_frames
    
    * Add jpg and correct values
    
    * Return the pan on a successful scan
    
    * Rename module and classes
    
    * Fix lint
    
    * Fix manifests
    
    * Clean up the stripecardscan example
    
    * Do not crash when no IIN + Last4 is provided
    
    * Use fuel and snackbar

--- stripecardscan-example/src/main/java/com/stripe/android/stripecardscan/example/MainActivity.kt
+++ stripecardscan-example/src/main/java/com/stripe/android/stripecardscan/example/MainActivity.kt
@@ -2,14 +2,35 @@ package com.stripe.android.stripecardscan.example
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.result.Result
 import com.stripe.android.stripecardscan.example.activity.LaunchCardImageVerificationSheetCompleteActivity
 import com.stripe.android.stripecardscan.example.activity.PARAM_PUBLISHABLE_KEY
+import com.stripe.android.stripecardscan.example.activity.PARAM_VERIFICATION_RESULT
+import com.stripe.android.stripecardscan.example.activity.SnackbarController
 import com.stripe.android.stripecardscan.example.databinding.ActivityMainBinding
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
     private val viewBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val snackbarController: SnackbarController by lazy {
+        SnackbarController(viewBinding.coordinator)
+    }
+
+    private val json by lazy {
+        Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            encodeDefaults = true
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +38,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(viewBinding.root)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        val civLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            viewBinding.scanResultText.text = result.data?.getStringExtra(PARAM_VERIFICATION_RESULT)
+        }
+
+        viewBinding.getPublishableKeyButton.setOnClickListener {
+            Fuel.get("https://stripe-card-scan-civ-example-app.glitch.me/publishable_key")
+                .responseString { _, _, result ->
+                    when (result) {
+                        is Result.Failure -> runOnUiThread {
+                            snackbarController
+                                .show("Error getting key: ${result.getException().message}")
+                        }
+                        is Result.Success -> runOnUiThread {
+                            json.decodeFromString(PubKeyResponse.serializer(), result.get())
+                                .let {
+                                    viewBinding.stripePublishableKey.setText(it.publishableKey)
+                                    viewBinding.launchCompleteButton.isEnabled = true
+                                }
+                        }
+                    }
+                }
+        }
+
+        viewBinding.stripePublishableKey.addTextChangedListener(
+            { _, _, _, _ -> /* before change */ },
+            { _, _, _, _ -> /* on change */ },
+            { s ->
+                viewBinding.launchCompleteButton.isEnabled = !s.isNullOrEmpty()
+            }
+        )
+
         viewBinding.launchCompleteButton.setOnClickListener {
-            startActivity(
+            civLauncher.launch(
                 Intent(this, LaunchCardImageVerificationSheetCompleteActivity::class.java)
                     .putExtra(
                         PARAM_PUBLISHABLE_KEY,
@@ -27,4 +81,9 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
+
+    @Serializable
+    data class PubKeyResponse(
+        @SerialName("publishable_key") val publishableKey: String,
+    )
 }
