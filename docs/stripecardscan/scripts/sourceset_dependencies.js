64b52309ceb291549f22a460cc47c51e3d2067b8
Adam Wushensky <awush@stripe.com>
2021-12-10 00:53:41 -0800

    Remove manual docs creation

--- docs/stripecardscan/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":stripecardscan:dokkaHtml/androidTestRelease":[],":stripecardscan:dokkaHtml/debug":[],":stripecardscan:dokkaHtml/main":[],":stripecardscan:dokkaHtml/release":[]}'
