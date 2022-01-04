3376bbd728d2645778a10484bf50f1d46480bd60
Skyler Reimer <skyler@stripe.com>
2022-01-04 14:21:52 -0800

    update requirements for newly enabled payment methods

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PaymentMethodRequirements.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/forms/PaymentMethodRequirements.kt
@@ -187,28 +187,13 @@ internal val SepaDebitRequirement = PaymentMethodRequirements(
 )
 
 internal val EpsRequirement = PaymentMethodRequirements(
-
-    /**
-     * Disabling this support so that it doesn't negatively impact our ability
-     * to save cards when the user selects SFU set and the PI has PM that don't support
-     * SFU to be set.
-     *
-     * When supported there are no known pi requirements and can be set to an empty set.
-     */
-    piRequirements = null,
+    piRequirements = emptySet(),
     siRequirements = null, // this is not supported by this payment method
     confirmPMFromCustomer = null
 )
 
 internal val P24Requirement = PaymentMethodRequirements(
-    /**
-     * Disabling this support so that it doesn't negatively impact our ability
-     * to save cards when the user selects SFU set and the PI has PM that don't support
-     * SFU to be set.
-     *
-     * When supported there are no known PI requirements and can be set to an empty set.
-     */
-    piRequirements = null,
+    piRequirements = emptySet(),
     siRequirements = null, // this is not supported by this payment method
 
     /**
@@ -218,14 +203,7 @@ internal val P24Requirement = PaymentMethodRequirements(
 )
 
 internal val GiropayRequirement = PaymentMethodRequirements(
-    /**
-     * Disabling this support so that it doesn't negatively impact our ability
-     * to save cards when the user selects SFU set and the PI has PM that don't support
-     * SFU to be set.
-     *
-     * When supported there are no known PI requirements and can be set to an empty set.
-     */
-    piRequirements = null,
+    piRequirements = emptySet(),
     siRequirements = null, // this is not supported by this payment method
     confirmPMFromCustomer = null
 )
