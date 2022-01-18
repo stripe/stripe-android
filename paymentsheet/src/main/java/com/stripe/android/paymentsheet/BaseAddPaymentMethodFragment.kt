a8435a2b208d12bc0100a4b59c9432e06e94b9f0
Skyler Reimer <skyler@stripe.com>
2022-01-18 10:37:59 -0800

    lint

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/BaseAddPaymentMethodFragment.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/BaseAddPaymentMethodFragment.kt
@@ -254,8 +254,8 @@ internal abstract class BaseAddPaymentMethodFragment(
                 merchantName = merchantName,
                 amount = amount,
                 billingDetails = config?.defaultBillingDetails,
-                requiresUserOptInToSavePaymentMethod
-                    = config?.requiresUserOptInToSavePaymentMethod ?: false,
+                requiresUserOptInToSavePaymentMethod =
+                config?.requiresUserOptInToSavePaymentMethod ?: false,
                 injectorKey = injectorKey
             )
         }
