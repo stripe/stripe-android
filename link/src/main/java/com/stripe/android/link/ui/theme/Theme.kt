ee6787a607ea3480b103c41247ad0990d5a88b5a
Bruno R. Nunes <brnunes@stripe.com>
2022-02-01 20:01:27 -0300

    ktlint

--- link/src/main/java/com/stripe/android/link/ui/theme/Theme.kt
+++ link/src/main/java/com/stripe/android/link/ui/theme/Theme.kt
@@ -2,7 +2,6 @@ package com.stripe.android.link.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
