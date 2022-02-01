81e3753f76079e40dd2e224d585916ed6d42bd84
Bruno R. Nunes <77990083+brnunes-stripe@users.noreply.github.com>
2022-02-01 13:51:57 -0800

    Revert "Generate dokka for 19.1.1 (#4537)"
    This reverts commit f604fc2ed32fc944ffb0aa2e6297488a98c1b979.

--- 3ds2sdk/src/main/kotlin/com/stripe/android/stripe3ds2/SdkVersion.kt
+++
@@ -1,7 +0,0 @@
-package com.stripe.android.stripe3ds2
-
-internal object SdkVersion {
-    // update these values when publishing a new release
-    internal const val VERSION_NAME = "6.1.2"
-    internal const val VERSION_CODE = 19
-}
--- 3ds2sdk/src/main/kotlin/com/stripe/android/stripe3ds2/utils/Factory0.kt
+++
@@ -1,5 +0,0 @@
-package com.stripe.android.stripe3ds2.utils
-
-fun interface Factory0<T> {
-    fun create(): T
-}
