a387ed23020542092a2b57325ed20cd9657bcbb7
Elena Pan <epan@stripe.com>
2022-02-03 10:49:13 -0800

    Create CardScanFragment and ScanFragment

---
+++ stripecardscan/src/main/java/com/stripe/android/stripecardscan/cardscan/CardScanFragment.kt
@@ -0,0 +1,316 @@
+package com.stripe.android.stripecardscan.cardscan
+
+import android.annotation.SuppressLint
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.PointF
+import android.os.Bundle
+import android.util.Size
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.os.bundleOf
+import androidx.core.view.updateMargins
+import androidx.fragment.app.setFragmentResult
+import com.stripe.android.camera.CameraPreviewImage
+import com.stripe.android.camera.framework.Stats
+import com.stripe.android.stripecardscan.R
+import com.stripe.android.stripecardscan.cardscan.exception.InvalidStripePublishableKeyException
+import com.stripe.android.stripecardscan.cardscan.exception.UnknownScanException
+import com.stripe.android.stripecardscan.cardscan.result.MainLoopAggregator
+import com.stripe.android.stripecardscan.cardscan.result.MainLoopState
+import com.stripe.android.stripecardscan.databinding.FragmentCardscanBinding
+import com.stripe.android.stripecardscan.framework.api.dto.ScanStatistics
+import com.stripe.android.stripecardscan.framework.api.uploadScanStats
+import com.stripe.android.stripecardscan.framework.util.AppDetails
+import com.stripe.android.stripecardscan.framework.util.Device
+import com.stripe.android.stripecardscan.payment.card.ScannedCard
+import com.stripe.android.stripecardscan.scanui.CancellationReason
+import com.stripe.android.stripecardscan.scanui.ScanErrorListener
+import com.stripe.android.stripecardscan.scanui.ScanFragment
+import com.stripe.android.stripecardscan.scanui.ScanState
+import com.stripe.android.stripecardscan.scanui.SimpleScanStateful
+import com.stripe.android.stripecardscan.scanui.util.asRect
+import com.stripe.android.stripecardscan.scanui.util.getColorByRes
+import com.stripe.android.stripecardscan.scanui.util.getFloatResource
+import com.stripe.android.stripecardscan.scanui.util.startAnimation
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.math.min
+import kotlin.math.roundToInt
+
+private val MINIMUM_RESOLUTION = Size(1067, 600) // minimum size of OCR
+const val CARD_SCAN_FRAGMENT_REQUEST_KEY = "CardScanRequestKey"
+const val CARD_SCAN_FRAGMENT_BUNDLE_KEY = "CardScanBundleKey"
+
+class CardScanFragment : ScanFragment(), SimpleScanStateful<CardScanState> {
+
+    override val minimumAnalysisResolution = MINIMUM_RESOLUTION
+
+    private lateinit var viewBinding: FragmentCardscanBinding
+
+    override val instructionsText: TextView by lazy { viewBinding.instructions }
+
+    override val previewFrame: ViewGroup by lazy {
+        viewBinding.previewFrame
+    }
+
+    private val params: CardScanSheetParams by lazy {
+        requireActivity().intent.getParcelableExtra(INTENT_PARAM_REQUEST)
+            ?: CardScanSheetParams("")
+    }
+
+    private val hasPreviousValidResult = AtomicBoolean(false)
+
+    override var scanState: ScanState = CardScanState.NotFound
+
+    override var scanStatePrevious: ScanState? = null
+
+    override val scanErrorListener: ScanErrorListener = ScanErrorListener()
+
+    /**
+     * The listener which handles results from the scan.
+     */
+    override val resultListener: CardScanResultListener =
+        object : CardScanResultListener {
+
+            override fun cardScanComplete(card: ScannedCard) {
+                setFragmentResult(
+                    CARD_SCAN_FRAGMENT_REQUEST_KEY,
+                    bundleOf(
+                        CARD_SCAN_FRAGMENT_BUNDLE_KEY to CardScanSheetResult.Completed(
+                            ScannedCard(
+                                pan = card.pan
+                            )
+                        )
+                    )
+                )
+                closeScanner()
+            }
+
+            override fun userCanceled(reason: CancellationReason) {
+                setFragmentResult(
+                    CARD_SCAN_FRAGMENT_REQUEST_KEY,
+                    bundleOf(
+                        CARD_SCAN_FRAGMENT_BUNDLE_KEY to CardScanSheetResult.Canceled(reason)
+                    )
+                )
+            }
+
+            override fun failed(cause: Throwable?) {
+                setFragmentResult(
+                    CARD_SCAN_FRAGMENT_REQUEST_KEY,
+                    bundleOf(
+                        CARD_SCAN_FRAGMENT_BUNDLE_KEY to
+                            CardScanSheetResult.Failed(
+                                cause ?: UnknownScanException()
+                            )
+                    )
+                )
+            }
+        }
+
+    /**
+     * The flow used to scan an item.
+     */
+    private val scanFlow: CardScanFlow by lazy {
+        object : CardScanFlow(scanErrorListener) {
+            /**
+             * A final result was received from the aggregator. Set the result from this activity.
+             */
+            override suspend fun onResult(
+                result: MainLoopAggregator.FinalResult,
+            ) {
+                super.onResult(result)
+
+                launch(Dispatchers.Main) {
+                    changeScanState(CardScanState.Correct)
+                    cameraAdapter.unbindFromLifecycle(requireActivity())
+                    resultListener.cardScanComplete(ScannedCard(result.pan))
+                }.let { }
+            }
+
+            /**
+             * An interim result was received from the result aggregator.
+             */
+            override suspend fun onInterimResult(
+                result: MainLoopAggregator.InterimResult,
+            ) = launch(Dispatchers.Main) {
+                if (
+                    result.state is MainLoopState.OcrFound &&
+                    !hasPreviousValidResult.getAndSet(true)
+                ) {
+                    scanStat.trackResult("ocr_pan_observed")
+                }
+
+                when (result.state) {
+                    is MainLoopState.Initial -> changeScanState(CardScanState.NotFound)
+                    is MainLoopState.OcrFound -> changeScanState(CardScanState.Found)
+                    is MainLoopState.Finished -> changeScanState(CardScanState.Correct)
+                }
+            }.let { }
+
+            override suspend fun onReset() = launch(Dispatchers.Main) {
+                changeScanState(CardScanState.NotFound)
+            }.let { }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        if (!ensureValidParams()) {
+//            return
+//        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = FragmentCardscanBinding.inflate(inflater, container, false)
+
+        setupViewFinderConstraints()
+
+        viewBinding.closeButton.setOnClickListener {
+            userClosedScanner()
+        }
+        viewBinding.viewFinderBorder.setOnTouchListener { _, e ->
+            setFocus(
+                PointF(
+                    e.x + viewBinding.viewFinderWindow.left,
+                    e.y + viewBinding.viewFinderWindow.top
+                )
+            )
+            true
+        }
+
+        displayState(scanState, scanStatePrevious)
+        return viewBinding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        scanState = CardScanState.NotFound
+    }
+
+    override fun onDestroy() {
+        scanFlow.cancelFlow()
+        super.onDestroy()
+    }
+
+    /**
+     * Set up viewFinderWindowView and viewFinderBorderView centered with predefined margins
+     */
+    private fun setupViewFinderConstraints() {
+
+        val screenSize = Resources.getSystem().displayMetrics.let {
+            Size(it.widthPixels, it.heightPixels)
+        }
+        val viewFinderMargin = (
+            min(screenSize.width, screenSize.height) *
+                requireActivity().getFloatResource(R.dimen.stripeViewFinderMargin)
+            ).roundToInt()
+
+        listOf(viewBinding.viewFinderWindow, viewBinding.viewFinderBorder).forEach { view ->
+            (view.layoutParams as ViewGroup.MarginLayoutParams)
+                .updateMargins(
+                    viewFinderMargin, viewFinderMargin, viewFinderMargin, viewFinderMargin
+                )
+        }
+
+        viewBinding.viewFinderBackground.setViewFinderRect(viewBinding.viewFinderWindow.asRect())
+    }
+
+    override fun onFlashSupported(supported: Boolean) {}
+
+    override fun onSupportsMultipleCameras(supported: Boolean) {}
+
+    /**
+     * Prepare to start the camera. Once the camera is ready, [onCameraReady] must be called.
+     */
+    override fun prepareCamera(onCameraReady: () -> Unit) {
+        viewBinding.previewFrame.post {
+            viewBinding.viewFinderBackground
+                .setViewFinderRect(viewBinding.viewFinderWindow.asRect())
+            onCameraReady()
+        }
+    }
+
+    /**
+     * Once the camera stream is available, start processing images.
+     */
+    override suspend fun onCameraStreamAvailable(cameraStream: Flow<CameraPreviewImage<Bitmap>>) {
+        scanFlow.startFlow(
+            context = requireActivity(),
+            imageStream = cameraStream,
+            viewFinder = viewBinding.viewFinderWindow.asRect(),
+            lifecycleOwner = this,
+            coroutineScope = this,
+            parameters = null
+        )
+    }
+
+    /**
+     * Called when the flashlight state has changed.
+     */
+    override fun onFlashlightStateChanged(flashlightOn: Boolean) {}
+
+    private fun ensureValidParams() = when {
+        params.stripePublishableKey.isEmpty() -> {
+            scanFailure(InvalidStripePublishableKeyException("Missing publishable key"))
+            false
+        }
+        else -> true
+    }
+
+    override fun displayState(newState: ScanState, previousState: ScanState?) {
+        when (newState) {
+            is CardScanState.NotFound -> {
+                viewBinding.viewFinderBackground
+                    .setBackgroundColor(
+                        requireActivity().getColorByRes(R.color.stripeNotFoundBackground)
+                    )
+                viewBinding.viewFinderWindow
+                    .setBackgroundResource(R.drawable.stripe_card_background_not_found)
+                viewBinding.viewFinderBorder.startAnimation(R.drawable.stripe_card_border_not_found)
+            }
+            is CardScanState.Found -> {
+                viewBinding.viewFinderBackground
+                    .setBackgroundColor(
+                        requireActivity().getColorByRes(R.color.stripeFoundBackground)
+                    )
+                viewBinding.viewFinderWindow
+                    .setBackgroundResource(R.drawable.stripe_card_background_found)
+                viewBinding.viewFinderBorder.startAnimation(R.drawable.stripe_card_border_found)
+            }
+            is CardScanState.Correct -> {
+                viewBinding.viewFinderBackground
+                    .setBackgroundColor(
+                        requireActivity().getColorByRes(R.color.stripeCorrectBackground)
+                    )
+                viewBinding.viewFinderWindow
+                    .setBackgroundResource(R.drawable.stripe_card_background_correct)
+                viewBinding.viewFinderBorder.startAnimation(R.drawable.stripe_card_border_correct)
+            }
+        }
+    }
+
+    override fun closeScanner() {
+        uploadScanStats(
+            stripePublishableKey = params.stripePublishableKey,
+            civId = "",
+            civSecret = "",
+            instanceId = Stats.instanceId,
+            scanId = Stats.scanId,
+            device = Device.fromContext(requireActivity()),
+            appDetails = AppDetails.fromContext(requireActivity()),
+            scanStatistics = ScanStatistics.fromStats()
+        )
+        super.closeScanner()
+    }
+}
---
+++ stripecardscan/src/main/java/com/stripe/android/stripecardscan/scanui/ScanFragment.kt
@@ -0,0 +1,274 @@
+package com.stripe.android.stripecardscan.scanui
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.PointF
+import android.util.Log
+import android.util.Size
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.stripe.android.stripecardscan.R
+import com.stripe.android.camera.CameraAdapter
+import com.stripe.android.camera.CameraPreviewImage
+import com.stripe.android.stripecardscan.camera.getCameraAdapter
+import com.stripe.android.stripecardscan.framework.Config
+import com.stripe.android.camera.framework.Stats
+import com.stripe.android.stripecardscan.framework.StorageFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
+
+private const val PERMISSION_RATIONALE_SHOWN = "permission_rationale_shown"
+
+abstract class ScanFragment : Fragment(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = Dispatchers.Main
+
+    internal val scanStat = Stats.trackTask("scan_activity")
+    private val permissionStat = Stats.trackTask("camera_permission")
+
+    protected var isFlashlightOn: Boolean = false
+        private set
+
+    internal val cameraAdapter by lazy { buildCameraAdapter() }
+    private val cameraErrorListener by lazy {
+        CameraErrorListenerImpl(requireActivity()) { t -> scanFailure(t) }
+    }
+
+    /**
+     * The listener which will handle the results from the scan.
+     */
+    internal abstract val resultListener: ScanResultListener
+
+    private val storage by lazy {
+        StorageFactory.getStorageInstance(requireActivity(), "scan_camera_permissions")
+    }
+
+    private val cameraPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                launch { permissionStat.trackResult("success") }
+                prepareCamera { onCameraReady() }
+            } else {
+                launch { permissionStat.trackResult("failure") }
+                showPermissionDenied()
+            }
+        }
+
+    override fun onStart() {
+        super.onStart()
+        Stats.startScan()
+
+        if (!CameraAdapter.isCameraSupported(requireActivity())) {
+            showCameraNotSupported()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+
+        launch {
+            delay(1500)
+            hideSystemUi()
+        }
+
+        if (!cameraAdapter.isBoundToLifecycle()) {
+            ensurePermissionAndStartCamera()
+        }
+    }
+
+    protected open fun hideSystemUi() {
+        // Prevent screenshots and keep the screen on while scanning.
+        requireActivity().window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE +
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+            WindowManager.LayoutParams.FLAG_SECURE +
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        setFlashlightState(false)
+    }
+
+    /**
+     * Ensure that the camera permission is available. If so, start the camera. If not, request it.
+     */
+    protected open fun ensurePermissionAndStartCamera() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.CAMERA,
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                launch { permissionStat.trackResult("success") }
+                prepareCamera { onCameraReady() }
+            }
+            storage.getBoolean(
+                PERMISSION_RATIONALE_SHOWN,
+                false,
+            ) -> showPermissionDenied()
+            else -> requestCameraPermission()
+        }
+    }
+
+    /**
+     * Show text explaining that the camera is not available.
+     */
+    protected open fun showCameraNotSupported() {
+        instructionsText.visibility = View.VISIBLE
+        instructionsText.setText(R.string.stripe_error_camera_unsupported)
+        scanFailure()
+    }
+
+    /**
+     * Show text  for why we are requesting camera permissions when the permission
+     * has been denied.
+     */
+    protected open fun showPermissionDenied() {
+        instructionsText.visibility = View.VISIBLE
+        instructionsText.setText(R.string.stripe_camera_permission_settings_message)
+        storage.storeValue(PERMISSION_RATIONALE_SHOWN, true)
+    }
+
+    /**
+     * Request permission to use the camera.
+     */
+    protected open fun requestCameraPermission() {
+        cameraPermissionLauncher.launch(
+            Manifest.permission.CAMERA
+        )
+    }
+
+    /**
+     * Turn the flashlight on or off.
+     */
+    protected open fun toggleFlashlight() {
+        isFlashlightOn = !isFlashlightOn
+        setFlashlightState(isFlashlightOn)
+        // TODO: this should be reported as part of scanstats, but is not yet supported
+    }
+
+    /**
+     * Toggle between available cameras.
+     */
+    protected open fun toggleCamera() {
+        cameraAdapter.changeCamera()
+        // TODO: this should probably be reported as part of scanstats, but is not yet supported
+    }
+
+    /**
+     * Called when the flashlight state has changed.
+     */
+    protected abstract fun onFlashlightStateChanged(flashlightOn: Boolean)
+
+    /**
+     * Turn the flashlight on or off.
+     */
+    private fun setFlashlightState(on: Boolean) {
+        cameraAdapter.setTorchState(on)
+        isFlashlightOn = on
+        onFlashlightStateChanged(on)
+    }
+
+    /**
+     * Cancel scanning due to a camera error.
+     */
+    protected open fun scanFailure(cause: Throwable? = null) {
+        Log.e(Config.logTag, "Canceling scan due to error", cause)
+        runBlocking { scanStat.trackResult("scan_failure") }
+        resultListener.failed(cause)
+    }
+
+    /**
+     * The scan has been closed by the user.
+     */
+    protected open fun userClosedScanner() {
+        runBlocking { scanStat.trackResult("user_canceled") }
+        resultListener.userCanceled(CancellationReason.Closed)
+        closeScanner()
+    }
+
+    /**
+     * Close the scanner.
+     */
+    protected open fun closeScanner() {
+        setFlashlightState(false)
+//        finish()
+    }
+
+    /**
+     * Prepare to start the camera. Once the camera is ready, [onCameraReady] must be called.
+     */
+    protected abstract fun prepareCamera(onCameraReady: () -> Unit)
+
+    protected open fun onCameraReady() {
+        instructionsText.visibility = View.GONE
+        cameraAdapter.bindToLifecycle(this)
+
+        val torchStat = Stats.trackTask("torch_supported")
+        cameraAdapter.withFlashSupport {
+            launch { torchStat.trackResult(if (it) "supported" else "unsupported") }
+            setFlashlightState(cameraAdapter.isTorchOn())
+            onFlashSupported(it)
+        }
+
+        // TODO: this should probably be reported as part of scanstats, but is not yet supported
+        cameraAdapter.withSupportsMultipleCameras {
+            onSupportsMultipleCameras(it)
+        }
+
+        launch { onCameraStreamAvailable(cameraAdapter.getImageStream()) }
+    }
+
+    /**
+     * Perform an action when the flash is supported
+     */
+    protected abstract fun onFlashSupported(supported: Boolean)
+
+    /**
+     * Perform an action when the camera support is determined
+     */
+    protected abstract fun onSupportsMultipleCameras(supported: Boolean)
+
+    protected open fun setFocus(point: PointF) {
+        cameraAdapter.setFocus(point)
+    }
+
+    /**
+     * Generate a camera adapter
+     */
+    internal open fun buildCameraAdapter(): CameraAdapter<CameraPreviewImage<Bitmap>> =
+        getCameraAdapter(
+            activity = requireActivity(),
+            previewView = previewFrame,
+            minimumResolution = minimumAnalysisResolution,
+            cameraErrorListener = cameraErrorListener,
+        )
+
+    protected abstract val previewFrame: ViewGroup
+
+    protected abstract val instructionsText: TextView
+
+    protected abstract val minimumAnalysisResolution: Size
+
+    /**
+     * A stream of images from the camera is available to be processed.
+     */
+    protected abstract suspend fun onCameraStreamAvailable(
+        cameraStream: Flow<CameraPreviewImage<Bitmap>>,
+    )
+}
