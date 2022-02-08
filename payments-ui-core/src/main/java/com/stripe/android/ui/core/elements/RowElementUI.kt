78ebcf7be74b1e54baa96e33634e40aa223a05de
Michelle Brubaker <michelleb@stripe.com>
2022-02-08 08:01:48 -0500

    Vertical line not working

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/RowElementUI.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/RowElementUI.kt
@@ -3,9 +3,11 @@ package com.stripe.android.ui.core.elements
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
@@ -24,6 +26,7 @@ internal fun RowElementUI(
     val fields = controller.fields
     Row(
         Modifier
+//            .height(IntrinsicSize.Min)
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
