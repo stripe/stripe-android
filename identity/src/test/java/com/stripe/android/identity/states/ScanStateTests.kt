800943b8b15a040586ec61d99e2cd804c856b16d
Chen Cen <ccen@stripe.com>
2022-02-07 17:40:29 -0800

    add timeout to Satisfied

--- identity/src/test/java/com/stripe/android/identity/states/ScanStateTests.kt
+++ identity/src/test/java/com/stripe/android/identity/states/ScanStateTests.kt
@@ -1,11 +1,15 @@
 package com.stripe.android.identity.states
 
 import com.google.common.truth.Truth.assertThat
+import com.stripe.android.camera.framework.time.ClockMark
+import com.stripe.android.camera.framework.time.milliseconds
 import com.stripe.android.identity.ml.AnalyzerOutput
 import com.stripe.android.identity.ml.BoundingBox
 import com.stripe.android.identity.ml.Category
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
@@ -31,9 +35,25 @@ class ScanStateTests {
     // TODO(ccen) add test for Found -> Found when #moreResultsRequired is implemented
 
     @Test
-    fun `Satisfied automatically transitions Finished`() {
+    fun `Satisfied transitions to Satisfied before timeout`() {
+        val mockClockMark: ClockMark = mock()
+        whenever(mockClockMark.elapsedSince()).thenReturn(DURATION_BEFORE_TIMEOUT)
+
         val initialScanType = ScanState.ScanType.ID_FRONT
-        val initialState = ScanState.Satisfied(initialScanType)
+        val initialState = ScanState.Satisfied(initialScanType, mockClockMark)
+        val resultState = initialState.consumeTransition(ID_FRONT_OUTPUT)
+
+        assertThat(resultState).isSameInstanceAs(initialState)
+        assertThat(resultState.type).isEqualTo(initialScanType)
+    }
+
+    @Test
+    fun `Satisfied transitions to Finished after timeout`() {
+        val mockClockMark: ClockMark = mock()
+        whenever(mockClockMark.elapsedSince()).thenReturn(DURATION_AFTER_TIMEOUT)
+
+        val initialScanType = ScanState.ScanType.ID_FRONT
+        val initialState = ScanState.Satisfied(initialScanType, mockClockMark)
         val resultState = initialState.consumeTransition(ID_FRONT_OUTPUT)
 
         assertThat(resultState).isInstanceOf(ScanState.Finished::class.java)
@@ -61,5 +81,7 @@ class ScanStateTests {
             Category.ID_BACK,
             0f
         )
+        val DURATION_BEFORE_TIMEOUT = 499.milliseconds
+        val DURATION_AFTER_TIMEOUT = 501.milliseconds
     }
 }
