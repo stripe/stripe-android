176195d7497263202a82d87a72532e9e4ccf1252
Elena Pan <epan@stripe.com>
2022-02-07 16:36:55 -0800

    Add RestrictTo

--- stripecardscan/src/main/java/com/stripe/android/stripecardscan/scanui/SimpleScanStateful.kt
+++ stripecardscan/src/main/java/com/stripe/android/stripecardscan/scanui/SimpleScanStateful.kt
@@ -1,9 +1,11 @@
 package com.stripe.android.stripecardscan.scanui
 
 import android.util.Log
+import androidx.annotation.RestrictTo
 import com.stripe.android.camera.framework.AnalyzerLoopErrorListener
 import com.stripe.android.stripecardscan.framework.Config
 
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 abstract class ScanState(val isFinal: Boolean)
 
 internal interface SimpleScanStateful<State : ScanState> {
@@ -26,6 +28,7 @@ internal interface SimpleScanStateful<State : ScanState> {
     }
 }
 
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 class ScanErrorListener : AnalyzerLoopErrorListener {
     override fun onAnalyzerFailure(t: Throwable): Boolean {
         Log.e(Config.logTag, "Error executing analyzer", t)
