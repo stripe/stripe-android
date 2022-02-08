e2f39c444b1e0f971b54a30b2651c3b0a7bf5c35
Michelle Brubaker <michelleb@stripe.com>
2022-02-08 07:48:57 -0500

    Cleanup

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/CardBillingSpec.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/CardBillingSpec.kt
@@ -1,6 +1,14 @@
 package com.stripe.android.ui.core.elements
 
+import com.stripe.android.ui.core.address.AddressFieldElementRepository
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-internal object CardBillingSpec : SectionFieldSpec(IdentifierSpec.Generic("card_billing"))
+internal object CardBillingSpec : SectionFieldSpec(IdentifierSpec.Generic("card_billing")) {
+    fun transform(
+        addressRepository: AddressFieldElementRepository
+    ) = CardBillingElement(
+        IdentifierSpec.Generic("credit_billing"),
+        addressRepository
+    )
+}
