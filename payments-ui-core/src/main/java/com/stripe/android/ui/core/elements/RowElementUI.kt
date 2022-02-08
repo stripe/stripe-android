7370c495a66dd40d27fda0a1682acd45011be4f6
Michelle Brubaker <michelleb@stripe.com>
2022-02-08 08:41:23 -0500

    Vertical row divider working with trailing icon

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/RowElementUI.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/RowElementUI.kt
@@ -2,20 +2,14 @@ package com.stripe.android.ui.core.elements
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 
 @Composable
 internal fun RowElementUI(
@@ -24,46 +18,54 @@ internal fun RowElementUI(
     hiddenIdentifiers: List<IdentifierSpec>
 ) {
     val fields = controller.fields
-    Row(
-        Modifier
-//            .height(IntrinsicSize.Min)
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        fields.forEachIndexed { index, field ->
-            SectionFieldElementUI(
-                enabled,
-                field,
-                Modifier.fillMaxWidth(
-                    (1f / fields.size.toFloat()).takeIf { index != (fields.size - 1) } ?: 1f
-                ),
-                hiddenIdentifiers
-            )
-            if (index != (fields.size - 1)) {
-                val cardStyle = CardStyle(isSystemInDarkTheme())
-                VeriticalDivider(
-                    color = cardStyle.cardBorderColor,
-                    thickness = cardStyle.cardBorderWidth,
-                    modifier = Modifier
-                        .padding(
-                            horizontal = cardStyle.cardBorderWidth
-                        )
-                )
-            }
-        }
-    }
-}
 
-@Composable
-internal fun VeriticalDivider(
-    color: Color,
-    modifier: Modifier = Modifier,
-    thickness: Dp = 1.dp,
-) {
-    Box(
-        modifier = modifier
-            .fillMaxHeight()
-            .width(thickness)
-            .background(color)
-    )
+    // An attempt was made to do this with a row, and a vertical divider created with a box.
+    // The row had a height of IntrinsicSize.Min, and the box/vertical divider filled the height
+    // when adding in the trailing icon this broke and caused the overall height of the row to
+    // increase.  The approach only supports two items in the row.
+    ConstraintLayout {
+        // Create references for the composables to constrain
+        val (field1Ref, dividerRef, field2Ref) = createRefs()
+
+        val field1 = fields[0]
+        SectionFieldElementUI(
+            enabled,
+            field1,
+            Modifier
+                .constrainAs(field1Ref) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                }
+                .fillMaxWidth(0.5f),
+            hiddenIdentifiers
+        )
+        val cardStyle = CardStyle(isSystemInDarkTheme())
+
+        Divider(
+            modifier = Modifier
+                .constrainAs(dividerRef) {
+                    start.linkTo(field1Ref.end)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    height = (Dimension.fillToConstraints)
+                }
+                .padding(
+                    horizontal = cardStyle.cardBorderWidth
+                )
+                .width(cardStyle.cardBorderWidth)
+                .background(cardStyle.cardBorderColor)
+        )
+        val field2 = fields[1]
+        SectionFieldElementUI(
+            enabled,
+            field2,
+            Modifier
+                .constrainAs(field2Ref) {
+                    start.linkTo(dividerRef.end)
+                    top.linkTo(parent.top)
+                }
+                .fillMaxWidth(0.5f),
+            hiddenIdentifiers
+        )
+    }
 }
