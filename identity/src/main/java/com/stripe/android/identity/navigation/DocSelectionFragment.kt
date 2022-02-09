a616a3a6fe1b81732eb0ba1cbfe06cbb6d4b5485
Chen Cen <79880926+ccen-stripe@users.noreply.github.com>
2022-02-08 17:51:24 -0800

    doc selection page (#4558)

--- identity/src/main/java/com/stripe/android/identity/navigation/DocSelectionFragment.kt
+++ identity/src/main/java/com/stripe/android/identity/navigation/DocSelectionFragment.kt
@@ -5,29 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.stripe.android.identity.R
-import com.stripe.android.identity.viewmodel.DocSelectionViewModel
+import com.stripe.android.identity.databinding.DocSelectionFragmentBinding
 
-class DocSelectionFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = DocSelectionFragment()
-    }
-
-    private lateinit var viewModel: DocSelectionViewModel
+/**
+ * Screen to select type of ID to scan.
+ */
+internal class DocSelectionFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.doc_selection_fragment, container, false)
-    }
+    ): View {
+        val binding = DocSelectionFragmentBinding.inflate(inflater, container, false)
+        binding.dl.setOnClickListener {
+            findNavController().navigate(R.id.action_docSelectionFragment_to_driverLicenseScanFragment)
+        }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DocSelectionViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.id.setOnClickListener {
+            findNavController().navigate(R.id.action_docSelectionFragment_to_IDScanFragment)
+        }
+
+        binding.passport.setOnClickListener {
+            findNavController().navigate(R.id.action_docSelectionFragment_to_passportScanFragment)
+        }
+
+        return binding.root
     }
 }
--- identity/src/main/java/com/stripe/android/identity/viewmodel/DocSelectionViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.stripe.android.identity.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class DocSelectionViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
