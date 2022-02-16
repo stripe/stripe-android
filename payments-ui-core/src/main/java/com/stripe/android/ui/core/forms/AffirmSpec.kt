f5153c42a668d4d364a64a0c4e4ab16f642f920e
Maxwell Clemens <maxwellclemens@stripe.com>
2022-02-16 15:22:09 -0700

    remove unused imports

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/forms/AffirmSpec.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/forms/AffirmSpec.kt
@@ -2,15 +2,9 @@ package com.stripe.android.ui.core.forms
 
 import androidx.annotation.RestrictTo
 import com.stripe.android.ui.core.R
-import com.stripe.android.ui.core.elements.AddressSpec
-import com.stripe.android.ui.core.elements.EmailSpec
 import com.stripe.android.ui.core.elements.IdentifierSpec
-import com.stripe.android.ui.core.elements.KlarnaCountrySpec
-import com.stripe.android.ui.core.elements.KlarnaHelper
 import com.stripe.android.ui.core.elements.LayoutSpec
-import com.stripe.android.ui.core.elements.SectionSpec
 import com.stripe.android.ui.core.elements.StaticTextSpec
-import com.stripe.android.ui.core.elements.billingParams
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 val AffirmParamKey: MutableMap<String, Any?> = mutableMapOf(
