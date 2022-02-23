ad3c191646ba86f0063dc0ca050225ca620a029b
Chen Cen <79880926+ccen-stripe@users.noreply.github.com>
2022-02-23 14:24:37 -0800

    [Identity] Introudce CameraPermissionFragment (#4612)

--- identity/src/main/java/com/stripe/android/identity/navigation/IdentityFragmentFactory.kt
+++ identity/src/main/java/com/stripe/android/identity/navigation/IdentityFragmentFactory.kt
@@ -2,6 +2,7 @@ package com.stripe.android.identity.navigation
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import com.stripe.android.camera.AppSettingsOpenable
 import com.stripe.android.camera.CameraPermissionEnsureable
 import com.stripe.android.identity.viewmodel.CameraViewModel
 
@@ -9,7 +10,8 @@ import com.stripe.android.identity.viewmodel.CameraViewModel
  * Factory for creating Identity fragments.
  */
 internal class IdentityFragmentFactory(
-    private val cameraPermissionEnsureable: CameraPermissionEnsureable
+    private val cameraPermissionEnsureable: CameraPermissionEnsureable,
+    private val appSettingsOpenable: AppSettingsOpenable
 ) : FragmentFactory() {
     private val cameraViewModelFactory = CameraViewModel.CameraViewModelFactory()
 
@@ -26,6 +28,9 @@ internal class IdentityFragmentFactory(
             PassportScanFragment::class.java.name -> PassportScanFragment(
                 cameraPermissionEnsureable,
                 cameraViewModelFactory
+            )
+            CameraPermissionDeniedFragment::class.java.name -> CameraPermissionDeniedFragment(
+                appSettingsOpenable
             )
             else -> super.instantiate(classLoader, className)
         }
