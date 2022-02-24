f652e3bb2cc849cb1fea8671f8c8454c8327caa6
Yiman Liu <yiman@stripe.com>
2022-02-24 13:42:43 -0800

    Add tests for bsbConfig

--- payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/AuBankAccountNumberConfigTest.kt
+++ payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/AuBankAccountNumberConfigTest.kt
@@ -7,6 +7,12 @@ class AuBankAccountNumberConfigTest {
     private val auBankAccountNumberConfig = AuBankAccountNumberConfig()
 
     @Test
+    fun `verify only numbers are allowed in the field`() {
+        Truth.assertThat(auBankAccountNumberConfig.filter("12345hell6789"))
+            .isEqualTo("123456789")
+    }
+
+    @Test
     fun `verify blank bank account number returns blank state`() {
         Truth.assertThat(auBankAccountNumberConfig.determineState(""))
             .isEqualTo(TextFieldStateConstants.Error.Blank)
@@ -19,7 +25,10 @@ class AuBankAccountNumberConfigTest {
 
         Truth.assertThat(auBankAccountNumberConfig.determineState("123456"))
             .isInstanceOf(TextFieldStateConstants.Error.Incomplete::class.java)
+    }
 
+    @Test
+    fun `verify invalid bank account number is in invalid state`() {
         Truth.assertThat(auBankAccountNumberConfig.determineState("1234567890"))
             .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
     }
---
+++ payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/BsbConfigTest.kt
@@ -0,0 +1,62 @@
+package com.stripe.android.ui.core.elements
+
+import androidx.compose.ui.text.AnnotatedString
+import com.google.common.truth.Truth
+import com.stripe.android.view.BecsDebitBanks
+import org.junit.Test
+
+class BsbConfigTest {
+    // TODO: remove this list, not sure if we will remove the manually added bank list in BsbSpec.
+    private val banks: List<BecsDebitBanks.Bank> = listOf(
+        BecsDebitBanks.Bank(
+            "00",
+            "STRIPE",
+            "Stripe Test Bank"
+        )
+    )
+    private val bsbConfig = BsbConfig(banks)
+
+    @Test
+    fun `verify visualTransformation formats entered value`() {
+        Truth.assertThat(bsbConfig.visualTransformation.filter(AnnotatedString("123456")).text)
+            .isEqualTo(AnnotatedString("123-456"))
+    }
+
+    @Test
+    fun `verify only numbers are allowed in the field`() {
+        Truth.assertThat(bsbConfig.filter("123h456"))
+            .isEqualTo("123456")
+    }
+
+    @Test
+    fun `verify blank BSB returns blank state`() {
+        Truth.assertThat(bsbConfig.determineState(""))
+            .isEqualTo(TextFieldStateConstants.Error.Blank)
+    }
+
+    @Test
+    fun `verify incomplete BSB is in incomplete state`() {
+        Truth.assertThat(bsbConfig.determineState("123"))
+            .isInstanceOf(TextFieldStateConstants.Error.Incomplete::class.java)
+
+        Truth.assertThat(bsbConfig.determineState("12345"))
+            .isInstanceOf(TextFieldStateConstants.Error.Incomplete::class.java)
+    }
+
+    @Test
+    fun `verify invalid BSB is in invalid state`() {
+        // input length > LENGTH
+        Truth.assertThat(bsbConfig.determineState("1234567"))
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+
+        // invalid BSB prefix
+        Truth.assertThat(bsbConfig.determineState("891200"))
+            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
+    }
+
+    @Test
+    fun `verify valid BSB is in valid state`() {
+        Truth.assertThat(bsbConfig.determineState("001234"))
+            .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
+    }
+}
