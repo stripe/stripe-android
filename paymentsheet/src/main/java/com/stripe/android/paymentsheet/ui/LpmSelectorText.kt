a1dc06170bb29f450126f7e25e18f620d7dee571
Skyler <89166418+skyler-stripe@users.noreply.github.com>
2022-02-25 14:36:41 -0500

    Convert Payment Sheet RecylerViewHolders to Compose (#4616)
    * Convert Payment Sheet RecylerViewHolders to Compose
    
    * Update paymentsheet/src/main/java/com/stripe/android/paymentsheet/AddPaymentMethodsAdapter.kt
    
    Co-authored-by: michelleb-stripe <77996191+michelleb-stripe@users.noreply.github.com>
    
    * address comments and fix dark mode
    
    * merge
    
    * api dump
    
    * address comments
    
    * Update build.gradle
    
    * fix cutoff text
    
    Co-authored-by: michelleb-stripe <77996191+michelleb-stripe@users.noreply.github.com>

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/LpmSelectorText.kt
@@ -0,0 +1,25 @@
+package com.stripe.android.paymentsheet.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+import com.stripe.android.paymentsheet.R
+
+@Composable
+internal fun LpmSelectorText(text: String, modifier: Modifier, isEnabled: Boolean) {
+    val textColor = colorResource(R.color.stripe_paymentsheet_title_text)
+    Text(
+        text = text,
+        fontSize = 13.sp,
+        fontWeight = FontWeight.Bold,
+        color = if (isEnabled) textColor else textColor.copy(alpha = 0.6f),
+        lineHeight = 1.sp,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        modifier = modifier
+    )
+}
