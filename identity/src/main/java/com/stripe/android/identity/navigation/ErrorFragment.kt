cdd19f7032874bd1f77b7ad47d02eaa61f4522d2
Chen Cen <ccen@stripe.com>
2022-03-05 18:29:34 -0800

    add a convenient method to navigate to error fragment

--- identity/src/main/java/com/stripe/android/identity/navigation/ErrorFragment.kt
+++ identity/src/main/java/com/stripe/android/identity/navigation/ErrorFragment.kt
@@ -1,12 +1,17 @@
 package com.stripe.android.identity.navigation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.stripe.android.identity.R
 import com.stripe.android.identity.databinding.ErrorFragmentBinding
+import com.stripe.android.identity.networking.models.VerificationPageDataRequirementError
 
 /**
  * Fragment to show generic error.
@@ -22,7 +27,6 @@ internal class ErrorFragment : Fragment() {
         val args = requireNotNull(arguments)
         binding.errorTitle.text = args[ARG_ERROR_TITLE] as String
         binding.errorContent.text = args[ARG_ERROR_CONTENT] as String
-
         if (args.getInt(ARG_GO_BACK_BUTTON_DESTINATION) != UNSET_DESTINATION) {
             binding.goBack.text = args[ARG_GO_BACK_BUTTON_TEXT] as String
             binding.goBack.visibility = View.VISIBLE
@@ -43,5 +47,29 @@ internal class ErrorFragment : Fragment() {
         const val ARG_GO_BACK_BUTTON_TEXT = "goBackButtonText"
         const val ARG_GO_BACK_BUTTON_DESTINATION = "goBackButtonDestination"
         private const val UNSET_DESTINATION = 0
+
+        fun NavController.navigateToErrorFragmentWithRequirementError(requirementError: VerificationPageDataRequirementError) {
+            navigate(
+                R.id.action_global_errorFragment,
+                bundleOf(
+                    ARG_ERROR_TITLE to requirementError.title,
+                    ARG_ERROR_CONTENT to requirementError.body,
+                    ARG_GO_BACK_BUTTON_DESTINATION to R.id.action_errorFragment_to_consentFragment,
+                    ARG_GO_BACK_BUTTON_TEXT to requirementError.buttonText,
+                )
+            )
+        }
+
+        fun NavController.navigateToErrorFragmentWithDefaultValues(context: Context) {
+            navigate(
+                R.id.action_global_errorFragment,
+                bundleOf(
+                    ARG_ERROR_TITLE to context.getString(R.string.error),
+                    ARG_ERROR_CONTENT to context.getString(R.string.unexpected_error_try_again),
+                    ARG_GO_BACK_BUTTON_DESTINATION to R.id.action_errorFragment_to_consentFragment,
+                    ARG_GO_BACK_BUTTON_TEXT to context.getString(R.string.go_back),
+                )
+            )
+        }
     }
 }
