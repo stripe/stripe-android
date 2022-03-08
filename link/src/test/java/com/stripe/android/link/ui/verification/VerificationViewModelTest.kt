cf83c942944a420fcdf76bda330cf06e2c8e19c6
Bruno R. Nunes <brnunes@stripe.com>
2022-03-08 16:31:55 -0300

    injectionParams nullable

--- link/src/test/java/com/stripe/android/link/ui/verification/VerificationViewModelTest.kt
+++ link/src/test/java/com/stripe/android/link/ui/verification/VerificationViewModelTest.kt
@@ -8,7 +8,6 @@ import com.stripe.android.core.Logger
 import com.stripe.android.core.injection.Injectable
 import com.stripe.android.core.injection.Injector
 import com.stripe.android.core.injection.WeakMapInjectorRegistry
-import com.stripe.android.link.LinkActivityContract
 import com.stripe.android.link.LinkScreen
 import com.stripe.android.link.account.LinkAccountManager
 import com.stripe.android.link.injection.SignedInViewModelSubcomponent
