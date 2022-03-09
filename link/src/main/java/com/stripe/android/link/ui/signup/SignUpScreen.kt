aa7b74b886c447358759a3479931eaec546b747e
Bruno R. Nunes <brnunes@stripe.com>
2022-03-09 15:11:11 -0300

    lint

--- link/src/main/java/com/stripe/android/link/ui/signup/SignUpScreen.kt
+++ link/src/main/java/com/stripe/android/link/ui/signup/SignUpScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.stripe.android.core.injection.Injector
-import com.stripe.android.link.LinkActivityContract
 import com.stripe.android.link.R
 import com.stripe.android.link.theme.DefaultLinkTheme
 import com.stripe.android.link.theme.linkTextFieldColors
