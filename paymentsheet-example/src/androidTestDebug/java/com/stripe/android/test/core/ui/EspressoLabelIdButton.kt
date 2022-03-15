ef264c9dfc4eb418e6042e43efcc91667e758784
Michelle Brubaker <michelleb@stripe.com>
2022-03-15 18:05:19 -0400

    Integrate with browserstack.

--- paymentsheet-example/src/androidTestDebug/java/com/stripe/android/test/core/ui/EspressoLabelIdButton.kt
+++ paymentsheet-example/src/androidTestDebug/java/com/stripe/android/test/core/ui/EspressoLabelIdButton.kt
@@ -1,9 +1,7 @@
 package com.stripe.android.test.core.ui
 
-import android.util.Log
 import androidx.annotation.StringRes
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
@@ -11,18 +9,10 @@ import java.security.InvalidParameterException
 
 open class EspressoLabelIdButton(@StringRes val label: Int) {
     fun click() {
-        var attempts = 0;
-        while(attempts < 3) {
-            attempts++
-            try {
-                Espresso.onView(ViewMatchers.withText(label))
-                    .perform(ViewActions.scrollTo())
-                    .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-                    .perform(ViewActions.click())
-            } catch (e: Exception) {
-                Log.e("Stripe", "Error finding playground complete button, attempt: $attempts")
-            }
-        }
+        Espresso.onView(ViewMatchers.withText(label))
+            .perform(ViewActions.scrollTo())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.click())
     }
 
     fun exists(): Boolean {
---
+++ scripts/browserstack.py
@@ -0,0 +1,61 @@
+#!/bin/python
+import requests
+import time
+import os
+from requests.auth import HTTPBasicAuth
+from requests_toolbelt.multipart import encoder
+
+user=os.getenv('BROWSERSTACK_USERNAME')
+authKey=os.getenv('BROWSERSTACK_ACCESS_KEY')
+apk="../paymentsheet-example/build/outputs/apk/debug/paymentsheet-example-debug.apk"
+espressoApk="../paymentsheet-example/build/outputs/apk/androidTest/debug/paymentsheet-example-debug-androidTest.apk"
+
+# curl -u "user:authKey" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@apk"
+# RETURN: {"app_url":"bs://527636cd103e394cb10e57d3ef0d57bcd0e0f433"}
+print("Uploading the file: " + apk)
+url = "https://api-cloud.browserstack.com/app-automate/upload"
+files = { 'file': ('paymentSheet.apk', open(apk, 'rb')), }
+response = requests.post(url, files=files, auth=(user, authKey))
+appUrl = response.json()["app_url"]
+print("App url: " + appUrl)
+
+# curl -u "user:authKey" -X POST "https://api-cloud.browserstack.com/app-automate/espresso/test-suite" -F "file=@espressoApk"
+# RETURN:  {"test_url":"bs://xxx"}
+print("Uploading the file: " + apk)
+url = "https://api-cloud.browserstack.com/app-automate/espresso/test-suite"
+files = { 'file': ('paymentSheet-espresso.apk', open(espressoApk, 'rb')), }
+response = requests.post(url, files=files, auth=(user, authKey))
+testUrl = response.json()["test_url"]
+print("test_url: " + testUrl)
+
+# curl -u ""user:authKey"" \
+#   -X POST "https://api-cloud.browserstack.com/app-automate/espresso/v2/build" \
+#   -d '{"app": "bs://xxx",
+# "testSuite": "bs://xxx",
+#  "devices": ["Samsung Galaxy S9 Plus-9.0"]}' \
+#   -H "Content-Type: application/json"
+print("Execute the tests: " + apk)
+url="https://api-cloud.browserstack.com/app-automate/espresso/v2/build"
+# firefox doesn't work on this samsung: Samsung Galaxy S9 Plus-9.0"]
+response = requests.post(url, json={"app": appUrl, "devices": ["Google Pixel 3-9.0"], "testSuite": testUrl}, auth=(user, authKey))
+jsonResponse = response.json()
+print(jsonResponse)
+if(jsonResponse["message"] == "Success"):
+   buildId = jsonResponse["build_id"]
+   url="https://api-cloud.browserstack.com/app-automate/espresso/v2/builds/"+ buildId
+   responseStatus="running"
+   print("Waiting for tests to complete.")
+   while(responseStatus == "running"):
+       time.sleep(60)
+       print(".")
+       response = requests.get(url, auth=(user, authKey))
+       responseStatus = response.json()["status"]
+   print("DONE.")
+
+   # No uploaded files need to be deleted.
+
+   sys.exit(0)
+else:
+   sys.exit(1)
+
+
