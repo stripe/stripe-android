b6f22dbb9921e39778f118f9994de9fc396aae94
Chen Cen <ccen@stripe.com>
2022-03-16 11:37:25 -0700

    change

--- identity/src/main/java/com/stripe/android/identity/ml/IDDetectorAnalyzer.kt
+++ identity/src/main/java/com/stripe/android/identity/ml/IDDetectorAnalyzer.kt
@@ -92,11 +92,11 @@ internal class IDDetectorAnalyzer(modelFile: File) :
     internal class Factory(
         private val modelFile: File
     ) : AnalyzerFactory<
-        AnalyzerInput,
-        IdentityScanState,
-        AnalyzerOutput,
-        Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput>
-        > {
+            AnalyzerInput,
+            IdentityScanState,
+            AnalyzerOutput,
+            Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput>
+            > {
         override suspend fun newInstance(): Analyzer<AnalyzerInput, IdentityScanState, AnalyzerOutput> {
             return IDDetectorAnalyzer(modelFile)
         }
