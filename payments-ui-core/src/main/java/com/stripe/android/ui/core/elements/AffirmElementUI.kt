c31561240affe9f0d7388cc229e124c95a73ff95
michelleb-stripe <77996191+michelleb-stripe@users.noreply.github.com>
2022-03-22 12:46:50 -0700

    Pre-release polish of affirm LPM in paymentsheet (#4738)

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/AffirmElementUI.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/AffirmElementUI.kt
@@ -1,43 +1,21 @@
 package com.stripe.android.ui.core.elements
 
 import androidx.annotation.RestrictTo
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
-import com.google.accompanist.flowlayout.FlowRow
-import com.stripe.android.ui.core.PaymentsTheme
 import com.stripe.android.ui.core.R
 
 @Composable
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 fun AffirmElementUI() {
-    FlowRow(
-        modifier = Modifier.padding(4.dp, 8.dp, 4.dp, 4.dp),
-        crossAxisAlignment = FlowCrossAxisAlignment.Center
-    ) {
-        Text(
-            stringResource(R.string.affirm_buy_now_pay_later),
-            Modifier
-                .padding(end = 4.dp)
-                .padding(top = 6.dp),
-            color = PaymentsTheme.colors.colorTextSecondary
-        )
-        Image(
-            painter = if (isSystemInDarkTheme()) {
-                painterResource(R.drawable.stripe_ic_affirm_logo_dark)
-            } else {
-                painterResource(R.drawable.stripe_ic_affirm_logo_light)
-            },
-            contentDescription = stringResource(
-                R.string.affirm_buy_now_pay_later
-            )
-        )
-    }
+    HtmlText(
+        html = stringResource(id = R.string.affirm_buy_now_pay_later),
+        imageGetter = mapOf(
+            "affirm" to R.drawable.stripe_ic_affirm_logo
+        ),
+        modifier = Modifier.padding(bottom = 4.dp)
+    )
 }
