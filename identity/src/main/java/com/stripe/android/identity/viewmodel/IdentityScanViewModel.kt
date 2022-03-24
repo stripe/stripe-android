e15d6d0a9e90c475e16b1bd8443bf718abace400
Chen Cen <ccen@stripe.com>
2022-03-23 17:13:28 -0700

    remove bracket

--- identity/src/main/java/com/stripe/android/identity/viewmodel/IdentityScanViewModel.kt
+++ identity/src/main/java/com/stripe/android/identity/viewmodel/IdentityScanViewModel.kt
@@ -4,14 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.stripe.android.identity.states.IdentityScanState
 
-internal class IdentityScanViewModel() : CameraViewModel() {
+internal class IdentityScanViewModel : CameraViewModel() {
 
     /**
      * The target ScanType of current scan.
      */
     internal var targetScanType: IdentityScanState.ScanType? = null
 
-    internal class IdentityScanViewModelFactory() : ViewModelProvider.Factory {
+    internal class IdentityScanViewModelFactory : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return IdentityScanViewModel() as T
