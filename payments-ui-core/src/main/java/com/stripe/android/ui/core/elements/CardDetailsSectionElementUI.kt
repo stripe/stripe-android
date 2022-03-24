c8b37794d81a52c91846010e66dda56189678dc1
Elena Pan <epan@stripe.com>
2022-03-24 08:06:28 -0700

    Update camera icon design

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/CardDetailsSectionElementUI.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/CardDetailsSectionElementUI.kt
@@ -78,6 +78,7 @@ fun CardDetailsSectionElementUI(
                     }
                 }
             Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.clickable(
                     indication = null,
                     interactionSource = remember { MutableInteractionSource() },
