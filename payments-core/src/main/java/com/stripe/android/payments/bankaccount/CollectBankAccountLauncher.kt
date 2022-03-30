87a5b9e4c0cebb6b675dab9c624c8a7b16fe029c
Carlos M <99293320+carlosmuvi-stripe@users.noreply.github.com>
2022-03-30 11:27:26 -0700

    Adds CollectBankAccountLauncher contract (#4756)
    * Adds CollectBankAccountLauncher.
    
    * Regenerates api.
    
    * Adds tests.
    
    * Style fixes.
    
    * Renames to configuration.
    
    * Removes duplicated confirm and exposes confirm setup.
    
    * Merge fixes.
    
    * Updates sample app.
    
    * Updates sample app.
    
    * Uses secret field from sample backend.
    
    Co-authored-by: jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
    
    Co-authored-by: jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>

--- payments-core/src/main/java/com/stripe/android/payments/bankaccount/CollectBankAccountLauncher.kt
+++ payments-core/src/main/java/com/stripe/android/payments/bankaccount/CollectBankAccountLauncher.kt
@@ -1,10 +1,101 @@
 package com.stripe.android.payments.bankaccount
 
 import android.os.Parcelable
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.fragment.app.Fragment
+import com.stripe.android.payments.bankaccount.navigation.CollectBankAccountContract
+import com.stripe.android.payments.bankaccount.navigation.CollectBankAccountResult
 import kotlinx.parcelize.Parcelize
 
-// TODO coming in a different PR.
-interface CollectBankAccountLauncher
+/**
+ * API to collect bank account information for a given PaymentIntent or SetupIntent.
+ *
+ * use [CollectBankAccountLauncher.create] to instantiate this object.
+ */
+interface CollectBankAccountLauncher {
+
+    fun presentWithPaymentIntent(
+        publishableKey: String,
+        clientSecret: String,
+        configuration: CollectBankAccountConfiguration
+    )
+
+    fun presentWithSetupIntent(
+        publishableKey: String,
+        clientSecret: String,
+        configuration: CollectBankAccountConfiguration
+    )
+
+    companion object {
+        /**
+         * Create a [CollectBankAccountLauncher] instance with [ComponentActivity].
+         *
+         * This API registers an [ActivityResultLauncher] into the [ComponentActivity],  it needs
+         * to be called before the [ComponentActivity] is created.
+         */
+        fun create(
+            activity: ComponentActivity,
+            callback: (CollectBankAccountResult) -> Unit
+        ): CollectBankAccountLauncher {
+            return StripeCollectBankAccountLauncher(
+                activity.registerForActivityResult(CollectBankAccountContract()) {
+                    callback(it)
+                },
+            )
+        }
+
+        /**
+         * Create a [CollectBankAccountLauncher] instance with [Fragment].
+         *
+         * This API registers an [ActivityResultLauncher] into the [Fragment],  it needs
+         * to be called before the [Fragment] is created.
+         */
+        fun create(
+            fragment: Fragment,
+            callback: (CollectBankAccountResult) -> Unit
+        ): CollectBankAccountLauncher {
+            return StripeCollectBankAccountLauncher(
+                fragment.registerForActivityResult(CollectBankAccountContract()) {
+                    callback(it)
+                },
+            )
+        }
+    }
+}
+
+internal class StripeCollectBankAccountLauncher constructor(
+    private val hostActivityLauncher: ActivityResultLauncher<CollectBankAccountContract.Args>,
+) : CollectBankAccountLauncher {
+
+    override fun presentWithPaymentIntent(
+        publishableKey: String,
+        clientSecret: String,
+        configuration: CollectBankAccountConfiguration
+    ) {
+        hostActivityLauncher.launch(
+            CollectBankAccountContract.Args.ForPaymentIntent(
+                publishableKey = publishableKey,
+                clientSecret = clientSecret,
+                configuration = configuration
+            )
+        )
+    }
+
+    override fun presentWithSetupIntent(
+        publishableKey: String,
+        clientSecret: String,
+        configuration: CollectBankAccountConfiguration
+    ) {
+        hostActivityLauncher.launch(
+            CollectBankAccountContract.Args.ForSetupIntent(
+                publishableKey = publishableKey,
+                clientSecret = clientSecret,
+                configuration = configuration
+            )
+        )
+    }
+}
 
 sealed class CollectBankAccountConfiguration : Parcelable {
     @Parcelize
