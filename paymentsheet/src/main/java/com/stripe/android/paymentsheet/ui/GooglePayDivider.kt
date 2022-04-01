0f7fc5aaa83e3d0bd4ec489f59fbf07990722fae
Michelle Brubaker <michelleb@stripe.com>
2022-03-31 20:12:13 -0400

    Fix display of the divider

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/GooglePayDivider.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/GooglePayDivider.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -18,15 +19,29 @@ internal fun GooglePayDividerUi(
     text: String = stringResource(R.string.stripe_paymentsheet_or_pay_with_card)
 ) {
     Box(
-        Modifier
-            .background(PaymentsTheme.colors.material.surface)
-            .padding(horizontal = 8.dp)
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
+        GooglePayDividerLine()
         Text(
             text = text,
             style = PaymentsTheme.typography.body1,
-            color = PaymentsTheme.colors.subtitle
+            color = PaymentsTheme.colors.subtitle,
+            modifier = Modifier
+                .background(PaymentsTheme.colors.material.surface)
+                .padding(horizontal = 8.dp)
         )
     }
+}
+
+@Composable
+internal fun GooglePayDividerLine() {
+    Box(
+        Modifier
+            .background(PaymentsTheme.colors.colorComponentBorder)
+            .height(PaymentsTheme.shapes.borderStrokeWidth)
+            .fillMaxWidth()
+    )
 }
