b22ac04456d58a35d97c7a33543b31effcb83d2f
Adam Wushensky <awush@stripe.com>
2022-04-06 14:31:36 -0700

    Make ScanConfig internal

--- stripecardscan/src/main/java/com/stripe/android/stripecardscan/framework/util/ScanConfig.kt
+++ stripecardscan/src/main/java/com/stripe/android/stripecardscan/framework/util/ScanConfig.kt
@@ -1,6 +1,6 @@
 package com.stripe.android.stripecardscan.framework.util
 
-data class ScanConfig(
+internal data class ScanConfig(
     /**
      * The number of strict mode frames used in a given scan.
      */
