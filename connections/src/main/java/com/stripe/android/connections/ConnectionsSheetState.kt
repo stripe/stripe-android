a63b71c9c08d906937d76f9ae581eeeb7a0d0181
Carlos Munoz <carlosmuvi@stripe.com>
2022-04-07 11:35:14 -0700

    Saves manifest on savedStateHandle and restores it on process recreation.

--- connections/src/main/java/com/stripe/android/connections/ConnectionsSheetState.kt
+++ connections/src/main/java/com/stripe/android/connections/ConnectionsSheetState.kt
@@ -1,5 +1,6 @@
 package com.stripe.android.connections
 
+import androidx.lifecycle.SavedStateHandle
 import com.stripe.android.connections.model.LinkAccountSessionManifest
 
 /**
@@ -9,7 +10,29 @@ internal data class ConnectionsSheetState(
     val activityRecreated: Boolean = false,
     val manifest: LinkAccountSessionManifest? = null,
     val authFlowActive: Boolean = false
-)
+) {
+
+    /**
+     * Restores existing persisted fields into the current [ConnectionsSheetState]
+     */
+    internal fun from(savedStateHandle: SavedStateHandle): ConnectionsSheetState {
+        return copy(
+            manifest = savedStateHandle.get(KEY_MANIFEST)
+        )
+    }
+
+    /**
+     * Saves the persistable fields of this state that changed to the given [SavedStateHandle]
+     */
+    internal fun to(savedStateHandle: SavedStateHandle, previousValue: ConnectionsSheetState) {
+        if (previousValue.manifest != manifest) savedStateHandle.set(KEY_MANIFEST, manifest)
+    }
+
+    companion object {
+        private const val KEY_MANIFEST = "key_manifest"
+    }
+}
+
 
 /**
  *  Class containing all side effects intended to be run by the view.
