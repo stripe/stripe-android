d7f7e246877aa9be32628bf75a36f7fd9a98467e
Carlos Munoz <carlosmuvi@stripe.com>
2022-04-13 11:40:48 -0700

    Regenerates API.

--- connections/src/main/java/com/stripe/android/connections/model/serializer/JsonAsStringSerializer.kt
+++ connections/src/main/java/com/stripe/android/connections/model/serializer/JsonAsStringSerializer.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.JsonTransformingSerializer
 
-object JsonAsStringSerializer :
+internal object JsonAsStringSerializer :
     JsonTransformingSerializer<String>(tSerializer = String.serializer()) {
     override fun transformDeserialize(element: JsonElement): JsonElement {
         return JsonPrimitive(value = element.toString())
