132cf3c5e48850d9a81c0a8bc477fa78e8f8f288
Carlos Munoz <carlosmuvi@stripe.com>
2022-04-13 17:03:31 -0700

    Update sample app backend url.

--- connections-example/src/main/java/com/stripe/android/connections/example/data/BackendRepository.kt
+++ connections-example/src/main/java/com/stripe/android/connections/example/data/BackendRepository.kt
@@ -1,6 +1,6 @@
 package com.stripe.android.connections.example.data
 
-private const val BASE_URL = "https://desert-instinctive-eoraptor.glitch.me/"
+private const val BASE_URL = "https://stripe-mobile-connections-example.glitch.me/"
 
 class BackendRepository(
     private val backendService: BackendApiService = BackendApiFactory(BASE_URL).create()
