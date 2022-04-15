6ccbdb67026aafdc0221713d60a62822ce63a20e
Skyler Reimer <skyler@stripe.com>
2022-04-15 13:44:14 -0700

    fix postal code callback not firing when enabled

--- example/src/main/java/com/stripe/example/activity/CreateCardSourceActivity.kt
+++ example/src/main/java/com/stripe/example/activity/CreateCardSourceActivity.kt
@@ -57,6 +57,9 @@ class CreateCardSourceActivity : AppCompatActivity() {
             } ?: showSnackbar("Enter a valid card.")
         }
 
+        viewBinding.cardWidget.postalCodeEnabled = true
+        viewBinding.cardWidget.postalCodeRequired = true
+
         viewBinding.cardWidget.setCardValidCallback { isValid, invalidFields -> // We will not call cardParams unless it is valid because
             // this will cause the inFocus field to change.
             if (isValid) {
