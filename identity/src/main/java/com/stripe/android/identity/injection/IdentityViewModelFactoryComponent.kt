11e25318463e4a13aeb260e08edd7faf1d2a0e37
Chen Cen <ccen@stripe.com>
2022-04-28 14:11:12 -0700

    [Identity] Add dependency injection

---
+++ identity/src/main/java/com/stripe/android/identity/injection/IdentityViewModelFactoryComponent.kt
@@ -0,0 +1,25 @@
+package com.stripe.android.identity.injection
+
+import android.content.Context
+import com.stripe.android.identity.viewmodel.IdentityViewModel
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        IdentityCommonModule::class
+    ]
+)
+internal interface IdentityViewModelFactoryComponent {
+    fun inject(factory: IdentityViewModel.IdentityViewModelFactory)
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun context(context: Context): Builder
+
+        fun build(): IdentityViewModelFactoryComponent
+    }
+}
