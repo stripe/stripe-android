46b587f7549b5a7253ae57e2425709bfcc8e1a03
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2022-05-09 09:24:51 -0700

    Add ability to reinstate saved payment method for us_bank_account

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/ach/USBankAccountFormScreenState.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/ach/USBankAccountFormScreenState.kt
@@ -1,39 +1,77 @@
 package com.stripe.android.paymentsheet.paymentdatacollection.ach
 
 import androidx.annotation.StringRes
+import com.stripe.android.financialconnections.model.BankAccount
+import com.stripe.android.financialconnections.model.FinancialConnectionsAccount
 import com.stripe.android.model.ConfirmStripeIntentParams
 import com.stripe.android.paymentsheet.model.PaymentSelection
 
-sealed class USBankAccountFormScreenState {
-    class NameAndEmailCollection(
+internal sealed class USBankAccountFormScreenState {
+    data class NameAndEmailCollection(
         @StringRes val error: Int? = null,
-        val primaryButtonText: String?,
-        val primaryButtonOnClick: () -> Unit,
-    ) : USBankAccountFormScreenState()
+        val name: String,
+        val email: String?,
+        val primaryButtonText: String?
+    ) : USBankAccountFormScreenState() {
+        override fun updateInputs(name: String, email: String?) =
+            this.copy(name = name, email = email)
+    }
 
     data class MandateCollection(
-        val bankName: String?,
-        val displayName: String?,
-        val last4: String?,
+        val name: String,
+        val email: String?,
+        val paymentAccount: FinancialConnectionsAccount,
+        val financialConnectionsSessionId: String,
+        val intentId: String,
         val primaryButtonText: String?,
-        val primaryButtonOnClick: () -> Unit,
-        val mandateText: String
-    ) : USBankAccountFormScreenState()
+        val mandateText: String,
+    ) : USBankAccountFormScreenState() {
+        override fun updateInputs(name: String, email: String?) =
+            this.copy(name = name, email = email)
+    }
 
     data class VerifyWithMicrodeposits(
-        val bankName: String?,
-        val displayName: String?,
+        val name: String,
+        val email: String?,
+        val paymentAccount: BankAccount,
+        val financialConnectionsSessionId: String,
+        val intentId: String,
+        val primaryButtonText: String?,
+        val mandateText: String,
+    ) : USBankAccountFormScreenState() {
+        override fun updateInputs(name: String, email: String?) =
+            this.copy(name = name, email = email)
+    }
+
+    data class SavedAccount(
+        val name: String,
+        val email: String?,
+        val financialConnectionsSessionId: String?,
+        val intentId: String,
+        val bankName: String,
         val last4: String?,
         val primaryButtonText: String?,
-        val primaryButtonOnClick: () -> Unit,
-        val mandateText: String
-    ) : USBankAccountFormScreenState()
+        val mandateText: String,
+    ) : USBankAccountFormScreenState() {
+        override fun updateInputs(name: String, email: String?) =
+            this.copy(name = name, email = email)
+    }
 
     data class ConfirmIntent(
         val confirmIntentParams: ConfirmStripeIntentParams
-    ) : USBankAccountFormScreenState()
+    ) : USBankAccountFormScreenState() {
+        override fun updateInputs(name: String, email: String?) = this
+    }
 
     data class Finished(
-        val paymentSelection: PaymentSelection
-    ) : USBankAccountFormScreenState()
+        val paymentSelection: PaymentSelection,
+        val financialConnectionsSessionId: String,
+        val intentId: String,
+        val last4: String,
+        val bankName: String
+    ) : USBankAccountFormScreenState() {
+        override fun updateInputs(name: String, email: String?) = this
+    }
+
+    abstract fun updateInputs(name: String, email: String?): USBankAccountFormScreenState
 }
