b1f0ccaa9b782ccab7c92295e44599a6fb2824d6
Chen Cen <ccen@stripe.com>
2022-05-10 16:48:56 -0700

    add transitionFromSatisified and transitionFromUnsatisified

--- identity/src/main/java/com/stripe/android/identity/states/IdentityScanStateTransitioner.kt
+++ identity/src/main/java/com/stripe/android/identity/states/IdentityScanStateTransitioner.kt
@@ -3,6 +3,8 @@ package com.stripe.android.identity.states
 import com.stripe.android.identity.ml.AnalyzerOutput
 import com.stripe.android.identity.states.IdentityScanState.Found
 import com.stripe.android.identity.states.IdentityScanState.Initial
+import com.stripe.android.identity.states.IdentityScanState.Satisfied
+import com.stripe.android.identity.states.IdentityScanState.Unsatisfied
 
 /**
  * Interface to determine how to transition between [IdentityScanState]s.
@@ -15,6 +17,16 @@ internal interface IdentityScanStateTransitioner {
 
     fun transitionFromFound(
         foundState: Found,
+        analyzerOutput: AnalyzerOutput
+    ): IdentityScanState
+
+    fun transitionFromSatisfied(
+        satisfiedState: Satisfied,
+        analyzerOutput: AnalyzerOutput
+    ): IdentityScanState
+
+    fun transitionFromUnsatisfied(
+        unsatisfiedState: Unsatisfied,
         analyzerOutput: AnalyzerOutput
     ): IdentityScanState
 }
