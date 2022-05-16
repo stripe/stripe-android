07880273e476e5b4ea631610c3a2f1f75aa6cf69
Michelle Brubaker <michelleb@stripe.com>
2022-05-16 13:48:48 -0400

    Revert change

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/DropdownItemSpec.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/DropdownItemSpec.kt
@@ -1,12 +1,11 @@
 package com.stripe.android.ui.core.elements
 
-import android.os.Parcelable
 import androidx.annotation.RestrictTo
-import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
-@Parcelize
+@Serializable
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 data class DropdownItemSpec(
     val api_value: String?,
     val display_text: String
-) : Parcelable
+)
