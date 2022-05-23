fc7b2aaed0713e4d22c875d494d3aa5457625194
Bruno R. Nunes <77990083+brnunes-stripe@users.noreply.github.com>
2022-05-23 16:03:26 -0400

    Create `flagMode` for country dropdown. (#5047)

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/DropdownConfig.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/DropdownConfig.kt
@@ -7,19 +7,22 @@ sealed interface DropdownConfig {
     /** This is a label for debug logs **/
     val debugLabel: String
 
-    /** This is the label to describe the field */
+    /** This is the label to describe the field **/
     val label: Int
 
     /** This is the list of displayable items to show in the drop down **/
-    fun getDisplayItems(): List<String>
+    val displayItems: List<String>
 
-    /**
-     * This will convert the field to a raw value to use in the parameter map
-     */
+    /** Whether the dropdown menu should be shown in a small form when collapsed **/
+    val tinyMode: Boolean
+        get() = false
+
+    /** The label identifying the selected item used when the dropdown menu is collapsed **/
+    fun getSelectedItemLabel(index: Int): String
+
+    /** This will convert from a raw value used in the parameter map to a display value **/
     fun convertFromRaw(rawValue: String): String
 
-    /**
-     * This will convert from a raw value used in the parameter map to a disiplayValue
-     */
+    /** This will convert the field to a raw value to use in the parameter map **/
     fun convertToRaw(displayName: String): String?
 }
