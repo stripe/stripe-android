63f3b8451db215645620ff56d60e0986ed2258f5
Michelle Brubaker <michelleb@stripe.com>
2022-05-24 19:12:14 -0400

    Run checks

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/StaticTextSpec.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/StaticTextSpec.kt
@@ -14,7 +14,7 @@ internal data class StaticTextSpec(
     @StringRes val stringResId: Int
 ) : FormItemSpec(), RequiredItemSpec {
     fun transform(): FormElement =
-    // It could be argued that the static text should have a controller, but
+        // It could be argued that the static text should have a controller, but
         // since it doesn't provide a form field we leave it out for now
         StaticTextElement(
             this.api_path,
--- payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/LpmSerializerTest.kt
+++ payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/LpmSerializerTest.kt
@@ -166,6 +166,6 @@ class LpmSerializerTest {
                     ]
                   }
                ]
-            """.trimIndent()
+        """.trimIndent()
     }
 }
