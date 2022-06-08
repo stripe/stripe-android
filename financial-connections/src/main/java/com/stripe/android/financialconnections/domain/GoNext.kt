e300c3847278ee8101f287abf780c42d26c6b2dc
Carlos Munoz <carlosmuvi@stripe.com>
2022-06-08 16:02:00 -0700

    Detekt fixes.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/domain/GoNext.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/domain/GoNext.kt
@@ -12,23 +12,26 @@ internal class GoNext @Inject constructor(
 
     operator fun invoke(manifest: FinancialConnectionsSessionManifest) {
         navigationManager.navigate(
-            when (manifest.nextPane) {
-                NextPane.ACCOUNT_PICKER -> TODO()
-                NextPane.ATTACH_LINKED_PAYMENT_ACCOUNT -> TODO()
-                NextPane.AUTH_OPTIONS -> TODO()
-                NextPane.CONSENT -> NavigationDirections.consent
-                NextPane.INSTITUTION_PICKER -> NavigationDirections.bankPicker
-                NextPane.LINK_CONSENT -> TODO()
-                NextPane.LINK_LOGIN -> TODO()
-                NextPane.MANUAL_ENTRY -> TODO()
-                NextPane.MANUAL_ENTRY_SUCCESS -> TODO()
-                NextPane.NETWORKING_LINK_LOGIN_WARMUP -> TODO()
-                NextPane.NETWORKING_LINK_SIGNUP_PANE -> TODO()
-                NextPane.NETWORKING_LINK_VERIFICATION -> TODO()
-                NextPane.PARTNER_AUTH -> TODO()
-                NextPane.SUCCESS -> TODO()
-                NextPane.UNEXPECTED_ERROR -> TODO()
-            }
+            manifest.nextPane.toNavigationCommand()
         )
+    }
+
+    @Suppress("ComplexMethod")
+    private fun NextPane.toNavigationCommand() = when (this) {
+        NextPane.ACCOUNT_PICKER -> TODO()
+        NextPane.ATTACH_LINKED_PAYMENT_ACCOUNT -> TODO()
+        NextPane.AUTH_OPTIONS -> TODO()
+        NextPane.CONSENT -> NavigationDirections.consent
+        NextPane.INSTITUTION_PICKER -> NavigationDirections.bankPicker
+        NextPane.LINK_CONSENT -> TODO()
+        NextPane.LINK_LOGIN -> TODO()
+        NextPane.MANUAL_ENTRY -> TODO()
+        NextPane.MANUAL_ENTRY_SUCCESS -> TODO()
+        NextPane.NETWORKING_LINK_LOGIN_WARMUP -> TODO()
+        NextPane.NETWORKING_LINK_SIGNUP_PANE -> TODO()
+        NextPane.NETWORKING_LINK_VERIFICATION -> TODO()
+        NextPane.PARTNER_AUTH -> TODO()
+        NextPane.SUCCESS -> TODO()
+        NextPane.UNEXPECTED_ERROR -> TODO()
     }
 }
