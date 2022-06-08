f1a214739088b026e4c1c1b0c10273dcdd63c33f
Carlos Munoz <carlosmuvi@stripe.com>
2022-06-08 11:02:15 -0700

    Adds navigation logic.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/domain/GoNext.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/domain/GoNext.kt
@@ -1,6 +1,8 @@
 package com.stripe.android.financialconnections.domain
 
 import com.stripe.android.financialconnections.model.FinancialConnectionsSessionManifest
+import com.stripe.android.financialconnections.model.FinancialConnectionsSessionManifest.NextPane
+import com.stripe.android.financialconnections.navigation.NavigationDirections
 import com.stripe.android.financialconnections.navigation.NavigationManager
 import javax.inject.Inject
 
@@ -8,9 +10,25 @@ internal class GoNext @Inject constructor(
     private val navigationManager: NavigationManager
 ) {
 
-    operator fun invoke() {
-    }
-
     operator fun invoke(manifest: FinancialConnectionsSessionManifest) {
+        navigationManager.navigate(
+            when (manifest.nextPane) {
+                NextPane.ACCOUNT_PICKER -> TODO()
+                NextPane.ATTACH_LINKED_PAYMENT_ACCOUNT -> TODO()
+                NextPane.AUTH_OPTIONS -> TODO()
+                NextPane.CONSENT -> NavigationDirections.consent
+                NextPane.INSTITUTION_PICKER -> NavigationDirections.bankPicker
+                NextPane.LINK_CONSENT -> TODO()
+                NextPane.LINK_LOGIN -> TODO()
+                NextPane.MANUAL_ENTRY -> TODO()
+                NextPane.MANUAL_ENTRY_SUCCESS -> TODO()
+                NextPane.NETWORKING_LINK_LOGIN_WARMUP -> TODO()
+                NextPane.NETWORKING_LINK_SIGNUP_PANE -> TODO()
+                NextPane.NETWORKING_LINK_VERIFICATION -> TODO()
+                NextPane.PARTNER_AUTH -> TODO()
+                NextPane.SUCCESS -> TODO()
+                NextPane.UNEXPECTED_ERROR -> TODO()
+            }
+        )
     }
 }
