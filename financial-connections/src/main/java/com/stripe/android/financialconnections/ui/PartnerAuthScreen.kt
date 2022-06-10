4544c97eaaa04842177cb0c6cf744c8d917ea99c
Carlos Munoz <carlosmuvi@stripe.com>
2022-06-10 09:39:06 -0700

    Adds sample partner auth screen that opens custom tab.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/ui/PartnerAuthScreen.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/ui/PartnerAuthScreen.kt
@@ -1,36 +1,32 @@
 package com.stripe.android.financialconnections.ui
 
-import android.annotation.SuppressLint
-import android.view.ViewGroup
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.content.Context
+import android.net.Uri
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksActivityViewModel
+import com.stripe.android.financialconnections.presentation.CreateBrowserIntentForUrl
 import com.stripe.android.financialconnections.presentation.FinancialConnectionsSheetNativeViewModel
 
 @Composable
 fun PartnerAuthScreen() {
     val activityViewModel: FinancialConnectionsSheetNativeViewModel = mavericksActivityViewModel()
     val url = activityViewModel.collectAsState(mapper = { it.authorizationSession?.url })
-    PartnerAuthContent(url.value ?: "")
+    val context = LocalContext.current
+    OpenBrowserLauncherEffect(url.value, context)
 }
 
-@SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun PartnerAuthContent(url: String) {
-    AndroidView(factory = {
-        WebView(it).apply {
-            settings.javaScriptEnabled = true
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            webViewClient = WebViewClient()
-            loadUrl(url)
+private fun OpenBrowserLauncherEffect(
+    url: String?,
+    context: Context
+) {
+    LaunchedEffect(url) {
+        if (url != null) {
+            val intent = CreateBrowserIntentForUrl(context, Uri.parse(url))
+            context.startActivity(intent)
         }
-    }, update = {
-        it.loadUrl(url)
-    })
+    }
 }
