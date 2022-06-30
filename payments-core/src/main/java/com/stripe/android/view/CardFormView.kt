4d84dd4de96a521be8c2e7abcd10f221a62da4c8
Skyler <89166418+skyler-stripe@users.noreply.github.com>
2022-06-30 15:14:28 -0700

    Expose the current card brand in CardFormView, CardInputWidget, and CardMultiLineWidget (#5238)
    * expose the current card brand in CardFormView, CardInputWidget, and CardMultiLineWidget
    
    * changelog

--- payments-core/src/main/java/com/stripe/android/view/CardFormView.kt
+++ payments-core/src/main/java/com/stripe/android/view/CardFormView.kt
@@ -24,6 +24,7 @@ import com.stripe.android.databinding.StripeCardFormViewBinding
 import com.stripe.android.databinding.StripeHorizontalDividerBinding
 import com.stripe.android.databinding.StripeVerticalDividerBinding
 import com.stripe.android.model.Address
+import com.stripe.android.model.CardBrand
 import com.stripe.android.model.CardParams
 import com.stripe.android.view.CardFormView.Style
 import com.stripe.android.view.CardValidCallback.Fields
@@ -99,6 +100,14 @@ class CardFormView @JvmOverloads constructor(
         Standard(0),
         Borderless(1)
     }
+
+    /**
+     * A [CardBrand] matching the current card number inputted by the user.
+     */
+    val brand: CardBrand
+        get() {
+            return cardMultilineWidget.brand
+        }
 
     /**
      * Retrieve a [CardParams] representing the card details if all these fields are valid:
