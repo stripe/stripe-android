76708742a4ad7db08b670ca24447584c952dea6e
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2022-06-30 13:36:58 -0700

    Add Google to Stripe Address transformer (#5236)

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/autocomplete/model/Place.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/autocomplete/model/Place.kt
@@ -1,6 +1,8 @@
 package com.stripe.android.ui.core.elements.autocomplete.model
 
 import android.text.SpannableString
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 internal data class FindAutocompletePredictionsResponse(
     val autocompletePredictions: List<AutocompletePrediction>
@@ -16,8 +18,9 @@ internal data class FetchPlaceResponse(
     val place: Place
 )
 
+@Serializable
 internal data class Place(
-    val addressComponents: List<AddressComponent>?
+    @SerialName("address_components") val addressComponents: List<AddressComponent>?
 ) {
     enum class Type(val value: String) {
         ADMINISTRATIVE_AREA_LEVEL_1("administrative_area_level_1"),
@@ -33,16 +36,18 @@ internal data class Place(
         ROUTE("route"),
         STREET_NUMBER("street_number"),
         SUBLOCALITY("sublocality"),
+        SUBLOCALITY_LEVEL_1("sublocality_level_1"),
         SUBLOCALITY_LEVEL_2("sublocality_level_2"),
         SUBLOCALITY_LEVEL_3("sublocality_level_3"),
         SUBLOCALITY_LEVEL_4("sublocality_level_4"),
     }
 }
 
+@Serializable
 internal data class AddressComponent(
-    val shortName: String?,
-    val longName: String,
-    val types: List<String>
+    @SerialName("short_name") val shortName: String?,
+    @SerialName("long_name") val longName: String,
+    @SerialName("types") val types: List<String>
 ) {
     fun contains(type: Place.Type): Boolean {
         return types.contains(type.value)
