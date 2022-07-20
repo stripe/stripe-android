d34d17dc8c82c5f7e8cb3038d858edfdd758c81f
Chen Cen <ccen@stripe.com>
2022-07-19 18:22:29 -0700

    Add PluginDetector

---
+++ stripe-core/src/main/java/com/stripe/android/core/utils/PluginDetector.kt
@@ -0,0 +1,35 @@
+package com.stripe.android.core.utils
+
+import androidx.annotation.RestrictTo
+
+/**
+ * A detector using reflection to check which plugin the SDK is being used from.
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+object PluginDetector {
+    /**
+     * Loop through all possible [PluginType]s and found the first one that exists in class path,
+     * return null if none of the class is found.
+     */
+    val pluginType: String? =
+        PluginType.values().firstOrNull {
+            isPlugin(it.className)
+        }?.pluginName
+
+    private fun isPlugin(className: String): Boolean = try {
+        Class.forName(className)
+        true
+    } catch (e: Exception) {
+        false
+    }
+
+    enum class PluginType(
+        val className: String,
+        val pluginName: String
+    ) {
+        ReactNative("com.facebook.proguard.annotations.DoNotStrip", "react-native"),
+        Flutter("io.flutter.embedding.engine.FlutterEngine", "flutter"),
+        Cordova("org.apache.cordova.CordovaActivity", "cordova"),
+        Unity("com.unity3d.player.UnityPlayerActivity", "unity")
+    }
+}
