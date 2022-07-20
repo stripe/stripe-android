ca4f63e33ed6bfac405a30c6cca243284b759a3f
Michelle Brubaker <michelleb@stripe.com>
2022-07-20 15:29:48 -0400

    apiDump

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/forms/resources/LpmSerializeFailureEvent.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/forms/resources/LpmSerializeFailureEvent.kt
@@ -2,6 +2,6 @@ package com.stripe.android.ui.core.forms.resources
 
 import com.stripe.android.core.networking.AnalyticsEvent
 
-class LpmSerializeFailureEvent : AnalyticsEvent {
+internal class LpmSerializeFailureEvent : AnalyticsEvent {
     override val eventName: String = "luxe_serialize_failure"
 }
