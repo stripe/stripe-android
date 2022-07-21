a4ab64191460ab157eab5403733c89c61e2ef030
Skyler <89166418+skyler-stripe@users.noreply.github.com>
2022-07-21 16:54:14 -0700

    Parse appearance config from merchant in Address Element (#5311)

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/addresselement/AddressElementPrimaryButton.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/addresselement/AddressElementPrimaryButton.kt
@@ -1,13 +1,14 @@
 package com.stripe.android.paymentsheet.addresselement
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
@@ -16,9 +17,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import com.stripe.android.ui.core.PaymentsTheme
 import com.stripe.android.ui.core.getBackgroundColor
+import com.stripe.android.ui.core.getBorderStrokeColor
 import com.stripe.android.ui.core.getOnBackgroundColor
 
 @Composable
@@ -27,9 +32,24 @@ internal fun AddressElementPrimaryButton(
     text: String,
     onButtonClick: () -> Unit
 ) {
+    // We need to use PaymentsTheme.primaryButtonStyle instead of MaterialTheme
+    // because of the rules API for primary button.
     val context = LocalContext.current
     val background = Color(PaymentsTheme.primaryButtonStyle.getBackgroundColor(context))
     val onBackground = Color(PaymentsTheme.primaryButtonStyle.getOnBackgroundColor(context))
+    val borderStroke = BorderStroke(
+        PaymentsTheme.primaryButtonStyle.shape.borderStrokeWidth.dp,
+        Color(PaymentsTheme.primaryButtonStyle.getBorderStrokeColor(context))
+    )
+    val shape = RoundedCornerShape(
+        PaymentsTheme.primaryButtonStyle.shape.cornerRadius
+    )
+    val fontFamily = PaymentsTheme.primaryButtonStyle.typography.fontFamily
+    val textStyle = TextStyle(
+        fontFamily = if (fontFamily != null) FontFamily(Font(fontFamily)) else FontFamily.Default,
+        fontSize = PaymentsTheme.primaryButtonStyle.typography.fontSize
+    )
+
     CompositionLocalProvider(
         LocalContentAlpha provides if (isEnabled) ContentAlpha.high else ContentAlpha.disabled
     ) {
@@ -45,7 +65,8 @@ internal fun AddressElementPrimaryButton(
                     .fillMaxWidth()
                     .height(44.dp),
                 enabled = isEnabled,
-                shape = MaterialTheme.shapes.medium,
+                shape = shape,
+                border = borderStroke,
                 colors = ButtonDefaults.buttonColors(
                     backgroundColor = background,
                     disabledBackgroundColor = background
@@ -53,7 +74,8 @@ internal fun AddressElementPrimaryButton(
             ) {
                 Text(
                     text = text,
-                    color = onBackground.copy(alpha = LocalContentAlpha.current)
+                    color = onBackground.copy(alpha = LocalContentAlpha.current),
+                    style = textStyle
                 )
             }
         }
