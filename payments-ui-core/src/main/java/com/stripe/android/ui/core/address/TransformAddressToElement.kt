5f3ed3769ecda2e283cd7076013225c0d435db0b
Michelle Brubaker <michelleb@stripe.com>
2022-07-25 14:39:09 -0400

    In a form that I like.

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/address/TransformAddressToElement.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/address/TransformAddressToElement.kt
@@ -12,15 +12,9 @@ import com.stripe.android.ui.core.elements.SectionSingleFieldElement
 import com.stripe.android.ui.core.elements.SimpleTextElement
 import com.stripe.android.ui.core.elements.SimpleTextFieldConfig
 import com.stripe.android.ui.core.elements.SimpleTextFieldController
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
 import java.io.InputStream
 import java.util.UUID
