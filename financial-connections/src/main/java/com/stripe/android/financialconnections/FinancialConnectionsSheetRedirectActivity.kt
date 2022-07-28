24a566c9e3963a5f185b2a51e08e1263c8b7acbe
Carlos Munoz <carlosmuvi@stripe.com>
2022-07-28 10:02:10 +0200

    Merges redirect activities.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/FinancialConnectionsSheetRedirectActivity.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/FinancialConnectionsSheetRedirectActivity.kt
@@ -1,13 +1,14 @@
 package com.stripe.android.financialconnections
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.stripe.android.financialconnections.ui.FinancialConnectionsSheetNativeActivity
 
 /**
- * This Activity handles redirects from the ChromeCustomTab hosting the AuthFlow.
- * It'll process the result url in [Intent.getData] and pass them back to the opening activity,
- * [FinancialConnectionsSheetActivity].
+ * This Activity handles redirects from the Institution authorization flow hand-off from native.
+ * It'll process the result url in [Intent.getData] and pass them back to the opening activity.
  */
 class FinancialConnectionsSheetRedirectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +17,49 @@ class FinancialConnectionsSheetRedirectActivity : AppCompatActivity() {
          * Used together, FLAG_ACTIVITY_SINGLE_TOP and FLAG_ACTIVITY_CLEAR_TOP
          * clear everything on the stack above the opening activity, including CCT.
          */
-        Intent(this, FinancialConnectionsSheetActivity::class.java)
-            .setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            .also { it.data = intent.data }
-            .let { startActivity(it) }
+        intent.data
+            ?.let { uri ->
+                uri.toIntent()
+                    ?.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    ?.also { it.data = intent.data }
+                    ?.let { startActivity(it) }
+            }
         finish()
+    }
+
+    /**
+     * @return Intent, or null if deeplink cannot be mapped to an Intent (in case of unknown deep links).
+     */
+    private fun Uri.toIntent(): Intent? =
+        when {
+            isFinancialConnectionsUrl().not() -> null
+            else -> {
+                when {
+                    path?.contains(PATH_CANCEL) == true ||
+                        path?.contains(PATH_SUCCESS) == true -> Intent(
+                        this@FinancialConnectionsSheetRedirectActivity,
+                        FinancialConnectionsSheetActivity::class.java
+                    )
+                    // TODO@carlosmuvi - temporary deep link for native.
+                    path?.contains(PATH_LOGIN) == true -> Intent(
+                        this@FinancialConnectionsSheetRedirectActivity,
+                        FinancialConnectionsSheetNativeActivity::class.java
+                    )
+                    else -> null
+                }
+            }
+        }
+
+    private fun Uri.isFinancialConnectionsUrl(): Boolean {
+        return this.scheme == SCHEME && this.host == HOST
+    }
+
+    private companion object {
+        private const val SCHEME = "stripe-auth"
+        private const val HOST = "link-accounts"
+
+        private const val PATH_CANCEL = "cancel"
+        private const val PATH_SUCCESS = "success"
+        private const val PATH_LOGIN = "login"
     }
 }
