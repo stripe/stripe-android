60da53ec1bce06c412e5c0dfcabebedc2dd40fca
Carlos Munoz <carlosmuvi@stripe.com>
2022-07-28 22:44:40 +0200

    Extracts retry logic from usecase.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/domain/PollAuthorizationSessionOAuthResults.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/domain/PollAuthorizationSessionOAuthResults.kt
@@ -1,12 +1,11 @@
 package com.stripe.android.financialconnections.domain
 
-import com.stripe.android.core.exception.StripeException
 import com.stripe.android.financialconnections.FinancialConnectionsSheet
 import com.stripe.android.financialconnections.model.FinancialConnectionsSessionManifest.FinancialConnectionsAuthorizationSession
 import com.stripe.android.financialconnections.model.MixedOAuthParams
 import com.stripe.android.financialconnections.repository.FinancialConnectionsRepository
-import kotlinx.coroutines.delay
-import java.net.HttpURLConnection.HTTP_ACCEPTED
+import com.stripe.android.financialconnections.utils.retryOnException
+import com.stripe.android.financialconnections.utils.shouldRetry
 import javax.inject.Inject
 
 /**
@@ -21,45 +20,21 @@ internal class PollAuthorizationSessionOAuthResults @Inject constructor(
 
     suspend operator fun invoke(
         session: FinancialConnectionsAuthorizationSession,
-        pollingTime: Long = POLLING_TIME_MS,
-        maxTries: Int = MAX_TRIES
     ): MixedOAuthParams {
-        var result: MixedOAuthParams?
-        do result = getOAuthResults(session).fold(
-            onSuccess = { it },
-            onFailure = { error ->
-                if (error.shouldRetry) {
-                    delay(pollingTime)
-                    null
-                } else throw error
-            }
-        )
-        while (result == null)
-        return result
-    }
-
-    /**
-     * @return null if should keep polling, actual result otherwise.
-     */
-
-    private suspend fun getOAuthResults(
-        session: FinancialConnectionsAuthorizationSession
-    ): Result<MixedOAuthParams> =
-        kotlin.runCatching {
+        return retryOnException(
+            times = MAX_TRIES,
+            delayMilliseconds = POLLING_TIME_MS,
+            retryCondition = { exception -> exception.shouldRetry }
+        ) {
             repository.postAuthorizationSessionOAuthResults(
                 clientSecret = configuration.financialConnectionsSessionClientSecret,
                 sessionId = session.id
             )
         }
-
-    private val Throwable.shouldRetry: Boolean
-        get() {
-            val statusCode: Int? = (this as? StripeException)?.statusCode
-            return statusCode == HTTP_ACCEPTED
-        }
+    }
 
     companion object {
-        private const val POLLING_TIME_MS = 3000L
+        private const val POLLING_TIME_MS = 2000L
         private const val MAX_TRIES = 10
     }
 }
