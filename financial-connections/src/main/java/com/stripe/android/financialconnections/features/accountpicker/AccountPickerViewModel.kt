5edc1b14de2528c6b7a8cf0cf76e2a6b2d734179
Carlos Munoz <carlosmuvi@stripe.com>
2022-07-31 19:05:50 +0200

    WIP

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/accountpicker/AccountPickerViewModel.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/accountpicker/AccountPickerViewModel.kt
@@ -7,6 +7,7 @@ import com.airbnb.mvrx.MavericksViewModelFactory
 import com.airbnb.mvrx.Uninitialized
 import com.airbnb.mvrx.ViewModelContext
 import com.stripe.android.financialconnections.domain.PollAuthorizationSessionAccounts
+import com.stripe.android.financialconnections.model.PartnerAccount
 import com.stripe.android.financialconnections.model.PartnerAccountsList
 import com.stripe.android.financialconnections.ui.FinancialConnectionsSheetNativeActivity
 import javax.inject.Inject
@@ -20,6 +21,16 @@ internal class AccountPickerViewModel @Inject constructor(
         suspend {
             pollAuthorizationSessionAccounts(authSessionId)
         }.execute { copy(accounts = it) }
+    }
+
+    fun onAccountClicked(account: PartnerAccount) {
+        withState {
+            if (it.selectedIds.contains(account.id)) {
+                setState { copy(selectedIds = selectedIds - account.id) }
+            } else {
+                setState { copy(selectedIds = selectedIds + account.id) }
+            }
+        }
     }
 
     companion object :
@@ -41,5 +52,10 @@ internal class AccountPickerViewModel @Inject constructor(
 }
 
 internal data class AccountPickerState(
-    val accounts: Async<PartnerAccountsList> = Uninitialized
-) : MavericksState
+    val accounts: Async<PartnerAccountsList> = Uninitialized,
+    val selectedIds: Set<String> = emptySet()
+) : MavericksState {
+
+    val PartnerAccount.isSelected
+        get() = selectedIds.contains(id)
+}
