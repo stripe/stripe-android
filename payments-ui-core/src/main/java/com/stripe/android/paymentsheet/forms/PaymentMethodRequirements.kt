dd71b667cf9ecf25cfb3df14ba7a5af51d3d9a84
Michelle Brubaker <michelleb@stripe.com>
2022-08-15 14:30:51 -0400

    Keep affirm requirements the same.

--- payments-ui-core/src/main/java/com/stripe/android/paymentsheet/forms/PaymentMethodRequirements.kt
+++ payments-ui-core/src/main/java/com/stripe/android/paymentsheet/forms/PaymentMethodRequirements.kt
@@ -273,7 +273,7 @@ internal val PaypalRequirement = PaymentMethodRequirements(
  * This defines the requirements for usage as a Payment Method.
  */
 internal val AffirmRequirement = PaymentMethodRequirements(
-    piRequirements = emptySet(),//setOf(ShippingAddress),
+    piRequirements = setOf(ShippingAddress),
     siRequirements = null,
     confirmPMFromCustomer = null
 )
