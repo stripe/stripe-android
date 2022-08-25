467eff1bb79d75cef8a9ba59764ea3337da9d417
Till Hellmund <110940675+tillh-stripe@users.noreply.github.com>
2022-08-25 16:09:12 -0400

    Refactor Link bottom sheet (#5455)
    * Create and switch to reusable LinkMenu
    
    * Add tests for WalletPaymentMethodMenu
    
    * Update API
    
    * Address code review feedback
    
    `destructive` → `isDestructive`

---
+++ link/src/main/java/com/stripe/android/link/ui/wallet/WalletPaymentMethodMenu.kt
@@ -0,0 +1,55 @@
+package com.stripe.android.link.ui.wallet
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import com.stripe.android.link.R
+import com.stripe.android.link.model.removeLabel
+import com.stripe.android.link.ui.menus.LinkMenu
+import com.stripe.android.link.ui.menus.LinkMenuItem
+import com.stripe.android.model.ConsumerPaymentDetails
+
+internal sealed class WalletPaymentMethodMenuItem(
+    override val textResId: Int,
+    override val isDestructive: Boolean = false
+) : LinkMenuItem {
+
+    data class RemoveItem(
+        @StringRes override val textResId: Int
+    ) : WalletPaymentMethodMenuItem(textResId)
+
+    object EditCard : WalletPaymentMethodMenuItem(
+        textResId = R.string.wallet_update_card
+    )
+
+    object Cancel : WalletPaymentMethodMenuItem(
+        textResId = R.string.cancel
+    )
+}
+
+@Composable
+internal fun WalletPaymentMethodMenu(
+    paymentDetails: ConsumerPaymentDetails.PaymentDetails,
+    onEditClick: () -> Unit,
+    onRemoveClick: () -> Unit,
+    onCancelClick: () -> Unit
+) {
+    val items = buildList {
+        if (paymentDetails is ConsumerPaymentDetails.Card) {
+            add(WalletPaymentMethodMenuItem.EditCard)
+        }
+
+        add(WalletPaymentMethodMenuItem.RemoveItem(textResId = paymentDetails.removeLabel))
+        add(WalletPaymentMethodMenuItem.Cancel)
+    }
+
+    LinkMenu(
+        items = items,
+        onItemPress = { item ->
+            when (item) {
+                is WalletPaymentMethodMenuItem.EditCard -> onEditClick()
+                is WalletPaymentMethodMenuItem.RemoveItem -> onRemoveClick()
+                is WalletPaymentMethodMenuItem.Cancel -> onCancelClick()
+            }
+        }
+    )
+}
