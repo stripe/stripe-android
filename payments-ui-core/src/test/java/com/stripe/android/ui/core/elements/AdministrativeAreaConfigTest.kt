025a6b4183e78b01d5ec9fc85fe8cb71717b125a
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2022-09-07 09:57:48 -0700

    Add administrative area dropdown for US, CA (#5518)
    * Add administrative area dropdown for US, CA

---
+++ payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/AdministrativeAreaConfigTest.kt
@@ -0,0 +1,42 @@
+package com.stripe.android.ui.core.elements
+
+import com.google.common.truth.Truth
+import org.junit.Test
+
+class AdministrativeAreaConfigTest {
+    @Test
+    fun `display values are full names of the state`() {
+        val config = AdministrativeAreaConfig(
+            AdministrativeAreaConfig.Country.US()
+        )
+        Truth.assertThat(config.convertFromRaw("CA"))
+            .isEqualTo("California")
+    }
+
+    @Test
+    fun `us displays state`() {
+        val config = AdministrativeAreaConfig(
+            AdministrativeAreaConfig.Country.US()
+        )
+        Truth.assertThat(config.label)
+            .isEqualTo(com.stripe.android.core.R.string.address_label_state)
+    }
+
+    @Test
+    fun `canada displays province`() {
+        val config = AdministrativeAreaConfig(
+            AdministrativeAreaConfig.Country.Canada()
+        )
+        Truth.assertThat(config.label)
+            .isEqualTo(com.stripe.android.core.R.string.address_label_province)
+    }
+
+    @Test
+    fun `dropdown defaults to first element if raw value doesn't exist`() {
+        val config = AdministrativeAreaConfig(
+            AdministrativeAreaConfig.Country.US()
+        )
+        Truth.assertThat(config.convertFromRaw("ABC"))
+            .isEqualTo("Alabama")
+    }
+}
