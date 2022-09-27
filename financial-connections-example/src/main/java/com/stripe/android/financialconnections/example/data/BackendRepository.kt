bfb4fbf9e42b5fb7e29e47f91764a9a05e81d30a
Carlos Munoz <carlosmuvi@stripe.com>
2022-09-27 11:36:57 -0700

    Test maestro on CI.

--- financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/data/BackendRepository.kt
+++ financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/data/BackendRepository.kt
@@ -1,6 +1,7 @@
 package com.stripe.android.financialconnections.example.data
 
-private const val BASE_URL = "https://stripe-mobile-connections-example.glitch.me/"
+//private const val BASE_URL = "https://stripe-mobile-connections-example.glitch.me/"
+private const val BASE_URL = "https://night-discreet-femur.glitch.me/"
 
 class BackendRepository(
     private val backendService: BackendApiService = BackendApiFactory(BASE_URL).create()
