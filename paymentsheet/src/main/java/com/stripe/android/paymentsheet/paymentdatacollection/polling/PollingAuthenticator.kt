6e6b522fa91088bb168181947b846b00617d39ab
Till Hellmund <110940675+tillh-stripe@users.noreply.github.com>
2022-09-30 13:44:40 -0400

    Open polling sheet on `upi_await_notification` action (#5620)
    * Add UpiAwaitNotification next action type
    
    * Update transformation to payment selection
    
    * Add redirect to polling bottom sheet
    
    * Fix lint
    
    * Remove one-off logic in form collection screen
    
    * Fix detekt issues
    
    * Update API
    
    * Undo change to codestyles file
    
    * Remove code that's not needed yet
    
    * Suppress unused class warning
    
    * Add launcher unregister and status bar color
    
    * Update hashCode() in NextActionData subclasses

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/polling/PollingAuthenticator.kt
@@ -0,0 +1,44 @@
+package com.stripe.android.paymentsheet.paymentdatacollection.polling
+
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultCaller
+import androidx.activity.result.ActivityResultLauncher
+import com.stripe.android.core.networking.ApiRequest
+import com.stripe.android.model.StripeIntent
+import com.stripe.android.payments.PaymentFlowResult
+import com.stripe.android.payments.core.authentication.PaymentAuthenticator
+import com.stripe.android.view.AuthActivityStarterHost
+import javax.inject.Singleton
+
+@Singleton
+@Suppress("unused") // Used via reflection in PollingAuthenticatorModule
+internal class PollingAuthenticator : PaymentAuthenticator<StripeIntent> {
+
+    private var pollingLauncher: ActivityResultLauncher<PollingContract.Args>? = null
+
+    override suspend fun authenticate(
+        host: AuthActivityStarterHost,
+        authenticatable: StripeIntent,
+        requestOptions: ApiRequest.Options
+    ) {
+        val args = PollingContract.Args(
+            clientSecret = requireNotNull(authenticatable.clientSecret),
+            statusBarColor = host.statusBarColor
+        )
+        pollingLauncher?.launch(args)
+    }
+
+    override fun onNewActivityResultCaller(
+        activityResultCaller: ActivityResultCaller,
+        activityResultCallback: ActivityResultCallback<PaymentFlowResult.Unvalidated>
+    ) {
+        pollingLauncher = activityResultCaller.registerForActivityResult(
+            PollingContract(),
+            activityResultCallback
+        )
+    }
+
+    override fun onLauncherInvalidated() {
+        pollingLauncher?.unregister()
+    }
+}
