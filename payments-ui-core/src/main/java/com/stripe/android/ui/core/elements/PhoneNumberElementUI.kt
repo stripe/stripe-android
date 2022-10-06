bee9cc76c0b3f29f0a9d21e27bf093bf9d133830
brnunes-stripe <brnunes@stripe.com>
2022-10-05 22:14:53 -0700

    Fix dropdown icon

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/PhoneNumberElementUI.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/PhoneNumberElementUI.kt
@@ -25,8 +25,18 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.stripe.android.ui.core.R
+
+@Preview
+@Composable
+private fun PhoneNumberCollectionPreview() {
+    PhoneNumberCollectionSection(
+        enabled = true,
+        phoneNumberController = PhoneNumberController.createPhoneNumberController("6508989787")
+    )
+}
 
 @Composable
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
