7162c896d6bf66cbc499b6613f886d44bfa631f6
Till Hellmund <tillh@stripe.com>
2022-10-12 17:46:15 -0400

    Update tests

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/paymentdatacollection/polling/PollingViewModelTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/paymentdatacollection/polling/PollingViewModelTest.kt
@@ -5,11 +5,14 @@ import com.google.common.truth.Truth.assertThat
 import com.stripe.android.model.StripeIntent
 import com.stripe.android.polling.IntentStatusPoller
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
@@ -19,7 +22,7 @@ class PollingViewModelTest {
     private val testDispatcher = UnconfinedTestDispatcher()
 
     @Test
-    fun `Emits provided time limit as remaining duration`() = runBlocking {
+    fun `Emits provided time limit as remaining duration`() = runTest(testDispatcher) {
         val timeLimit = 5.minutes
 
         val viewModel = createPollingViewModel(
@@ -31,7 +34,7 @@ class PollingViewModelTest {
     }
 
     @Test
-    fun `Remaining time is updated every second`() = runBlocking {
+    fun `Remaining time is updated every second`() = runTest(testDispatcher) {
         val timeLimit = 5.minutes
 
         val viewModel = createPollingViewModel(
@@ -41,17 +44,17 @@ class PollingViewModelTest {
 
         assertThat(viewModel.uiState.value.durationRemaining).isEqualTo(timeLimit)
 
-        testDispatcher.scheduler.advanceTimeBy(1.seconds.inWholeMilliseconds + 1)
+        advanceTimeBy(1.seconds + 1.milliseconds)
 
         assertThat(viewModel.uiState.value.durationRemaining).isEqualTo(timeLimit - 1.seconds)
 
-        testDispatcher.scheduler.advanceTimeBy(1.seconds.inWholeMilliseconds)
+        advanceTimeBy(1.seconds)
 
         assertThat(viewModel.uiState.value.durationRemaining).isEqualTo(timeLimit - 2.seconds)
     }
 
     @Test
-    fun `Reflects cancellation in UI state`() = runBlocking {
+    fun `Reflects cancellation in UI state`() = runTest(testDispatcher) {
         val fakePoller = FakeIntentStatusPoller()
 
         val viewModel = createPollingViewModel(
@@ -68,7 +71,7 @@ class PollingViewModelTest {
     }
 
     @Test
-    fun `Reflects failure in UI state`() = runBlocking {
+    fun `Reflects failure in UI state`() = runTest(testDispatcher) {
         val fakePoller = FakeIntentStatusPoller()
 
         val viewModel = createPollingViewModel(
@@ -84,7 +87,7 @@ class PollingViewModelTest {
     }
 
     @Test
-    fun `Performs one-off poll when time limit has been exceeded`() {
+    fun `Performs one-off poll when time limit has been exceeded`() = runTest(testDispatcher) {
         val fakePoller = FakeIntentStatusPoller().apply {
             emitNextPollResult(StripeIntent.Status.RequiresAction)
         }
@@ -99,17 +102,17 @@ class PollingViewModelTest {
 
         fakePoller.enqueueForcePollResult(StripeIntent.Status.RequiresCapture)
 
-        testDispatcher.scheduler.advanceTimeBy(10.seconds.inWholeMilliseconds + 1)
+        advanceTimeBy(10.seconds + 1.milliseconds)
 
         assertThat(fakePoller.isActive).isFalse()
 
-        testDispatcher.scheduler.advanceTimeBy(3.seconds.inWholeMilliseconds + 1)
+        advanceTimeBy(3.seconds)
 
         assertThat(viewModel.uiState.value.pollingState).isEqualTo(PollingState.Failed)
     }
 
     @Test
-    fun `Pausing stops the poller and doesn't emit new state`() = runBlocking {
+    fun `Pausing stops the poller and doesn't emit new state`() = runTest(testDispatcher) {
         val fakePoller = FakeIntentStatusPoller()
 
         val viewModel = createPollingViewModel(
@@ -117,21 +120,21 @@ class PollingViewModelTest {
             dispatcher = testDispatcher,
         )
 
-        testDispatcher.scheduler.advanceTimeBy(5.seconds.inWholeMilliseconds + 1)
+        advanceTimeBy(5.seconds + 1.milliseconds)
 
         assertThat(fakePoller.isActive).isTrue()
         assertThat(viewModel.uiState.value.pollingState).isEqualTo(PollingState.Active)
 
         viewModel.pausePolling()
 
-        testDispatcher.scheduler.advanceTimeBy(1.minutes.inWholeMilliseconds + 1)
+        advanceTimeBy(1.minutes)
 
         assertThat(fakePoller.isActive).isFalse()
         assertThat(viewModel.uiState.value.pollingState).isEqualTo(PollingState.Active)
     }
 
     @Test
-    fun `Continues polling after pausing and resuming`() = runBlocking {
+    fun `Continues polling after pausing and resuming`() = runTest(testDispatcher) {
         val fakePoller = FakeIntentStatusPoller()
 
         val viewModel = createPollingViewModel(
@@ -139,7 +142,7 @@ class PollingViewModelTest {
             dispatcher = testDispatcher,
         )
 
-        testDispatcher.scheduler.advanceTimeBy(5.seconds.inWholeMilliseconds + 1)
+        advanceTimeBy(5.seconds + 1.milliseconds)
 
         assertThat(fakePoller.isActive).isTrue()
 
@@ -149,22 +152,23 @@ class PollingViewModelTest {
 
         viewModel.resumePolling()
 
-        testDispatcher.scheduler.advanceTimeBy(5.seconds.inWholeMilliseconds + 1)
+        advanceTimeBy(5.seconds + 1.milliseconds)
 
         assertThat(fakePoller.isActive).isTrue()
     }
 
     @Test
-    fun `Emits correct time limit when restoring from process death`() = runBlocking {
+    fun `Emits correct time limit when restoring from process death`() = runTest(testDispatcher) {
         val currentTime = System.currentTimeMillis()
         val timeProvider = FakeTimeProvider(timeInMillis = currentTime)
 
+        val timeLimit = 10.minutes
+        val alreadyPassed = 2.minutes
+
         val savedStateHandle = SavedStateHandle().apply {
-            val mockedStartTime = currentTime - 2.minutes.inWholeMilliseconds
+            val mockedStartTime = currentTime - alreadyPassed.inWholeMilliseconds
             this["KEY_CURRENT_POLLING_START_TIME"] = mockedStartTime
         }
-
-        val timeLimit = 10.minutes
 
         val viewModel = createPollingViewModel(
             timeLimit = timeLimit,
@@ -173,7 +177,8 @@ class PollingViewModelTest {
             dispatcher = testDispatcher,
         )
 
-        assertThat(viewModel.uiState.value.durationRemaining).isEqualTo(8.minutes)
+        val remainingTime = timeLimit - alreadyPassed
+        assertThat(viewModel.uiState.value.durationRemaining).isEqualTo(remainingTime)
     }
 }
 
@@ -199,4 +204,9 @@ private fun createPollingViewModel(
         dispatcher = dispatcher,
         savedStateHandle = savedStateHandle,
     )
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+private fun TestScope.advanceTimeBy(duration: Duration) {
+    advanceTimeBy(duration.inWholeMilliseconds)
 }
