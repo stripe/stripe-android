b460de26bf03b6f935b43321875e00029b5004d0
Carlos Munoz <carlosmuvi@stripe.com>
2022-11-06 14:46:24 -0800

    Fixes partner auth web flow event

--- financial-connections/src/main/java/com/stripe/android/financialconnections/domain/CancelAuthorizationSession.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/domain/CancelAuthorizationSession.kt
@@ -1,13 +1,11 @@
 package com.stripe.android.financialconnections.domain
 
 import com.stripe.android.financialconnections.FinancialConnectionsSheet
-import com.stripe.android.financialconnections.domain.NativeAuthFlowCoordinator.Message
 import com.stripe.android.financialconnections.model.FinancialConnectionsAuthorizationSession
 import com.stripe.android.financialconnections.repository.FinancialConnectionsManifestRepository
 import javax.inject.Inject
 
 internal class CancelAuthorizationSession @Inject constructor(
-    private val coordinator: NativeAuthFlowCoordinator,
     private val repository: FinancialConnectionsManifestRepository,
     private val configuration: FinancialConnectionsSheet.Configuration
 ) {
@@ -18,6 +16,6 @@ internal class CancelAuthorizationSession @Inject constructor(
         return repository.cancelAuthorizationSession(
             clientSecret = configuration.financialConnectionsSessionClientSecret,
             sessionId = authorizationSessionId
-        ).also { coordinator().emit(Message.ClearPartnerWebAuth) }
+        )
     }
 }
--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/partnerauth/PartnerAuthViewModel.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/partnerauth/PartnerAuthViewModel.kt
@@ -20,6 +20,8 @@ import com.stripe.android.financialconnections.domain.CancelAuthorizationSession
 import com.stripe.android.financialconnections.domain.CompleteAuthorizationSession
 import com.stripe.android.financialconnections.domain.GetManifest
 import com.stripe.android.financialconnections.domain.GoNext
+import com.stripe.android.financialconnections.domain.NativeAuthFlowCoordinator
+import com.stripe.android.financialconnections.domain.NativeAuthFlowCoordinator.Message.ClearPartnerWebAuth
 import com.stripe.android.financialconnections.domain.PollAuthorizationSessionOAuthResults
 import com.stripe.android.financialconnections.domain.PostAuthSessionEvent
 import com.stripe.android.financialconnections.domain.PostAuthorizationSession
@@ -45,6 +47,7 @@ internal class PartnerAuthViewModel @Inject constructor(
     private val eventTracker: FinancialConnectionsAnalyticsTracker,
     private val postAuthSessionEvent: PostAuthSessionEvent,
     private val getManifest: GetManifest,
+    private val nativeAuthFlowCoordinator: NativeAuthFlowCoordinator,
     private val goNext: GoNext,
     private val navigationManager: NavigationManager,
     private val pollAuthorizationSessionOAuthResults: PollAuthorizationSessionOAuthResults,
@@ -128,6 +131,7 @@ internal class PartnerAuthViewModel @Inject constructor(
     ) {
         logger.debug("Web AuthFlow status received $webStatus")
         viewModelScope.launch {
+            nativeAuthFlowCoordinator().emit(ClearPartnerWebAuth)
             when (webStatus) {
                 is Uninitialized -> {}
                 is Loading -> setState { copy(authenticationStatus = Loading()) }
