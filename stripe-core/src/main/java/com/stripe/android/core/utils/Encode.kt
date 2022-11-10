89fd5b0a972800a0c666a613bf36a3819989ae80
Adam Wushensky <awush@stripe.com>
2022-11-10 15:38:43 -0500

    Add annotations for restricting encoding utilities

--- stripe-core/src/main/java/com/stripe/android/core/utils/Encode.kt
+++ stripe-core/src/main/java/com/stripe/android/core/utils/Encode.kt
@@ -1,6 +1,7 @@
 package com.stripe.android.core.utils
 
 import android.util.Base64
+import androidx.annotation.RestrictTo
 import com.stripe.android.core.networking.QueryStringFactory
 import com.stripe.android.core.networking.toMap
 import kotlinx.serialization.DeserializationStrategy
@@ -15,9 +16,11 @@ private val json = Json {
     encodeDefaults = true
 }
 
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
 fun b64Encode(s: String): String =
     Base64.encodeToString(s.toByteArray(Charset.defaultCharset()), Base64.NO_WRAP)
 
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
 fun b64Encode(b: ByteArray): String =
     Base64.encodeToString(b, Base64.NO_WRAP)
 
@@ -25,6 +28,7 @@ fun b64Encode(b: ByteArray): String =
  * Encode a serializable object to a x-www-url-encoded string. The source object must convert to a
  * [Map] so that the parameters can be named.
  */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
 fun <T> encodeToXWWWFormUrl(serializer: SerializationStrategy<T>, value: T): String =
     QueryStringFactory.createFromParamsWithEmptyValues(
         json.encodeToJsonElement(serializer, value).toMap()
@@ -33,16 +37,19 @@ fun <T> encodeToXWWWFormUrl(serializer: SerializationStrategy<T>, value: T): Str
 /**
  * Encode a serializable object to a JSON string
  */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
 fun <T> encodeToJson(serializer: SerializationStrategy<T>, value: T): String =
     json.encodeToString(serializer, value)
 
 /**
  * Decode an object from a JSON string
  */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
 fun <T> decodeFromJson(deserializer: DeserializationStrategy<T>, value: String): T =
     json.decodeFromString(deserializer, value)
 
 /**
  * URL-encode a string. This is useful for sanitizing untrusted data for use in URLs.
  */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
 fun urlEncode(value: String): String = URLEncoder.encode(value, Charsets.UTF_8.name())
