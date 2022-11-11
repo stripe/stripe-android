940989dd17ea8e5cbe580ee3438559aed4b463c6
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2022-11-11 08:53:18 -0800

    Update address bar

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/addresselement/AddressLauncherResult.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/addresselement/AddressLauncherResult.kt
@@ -1,29 +1,24 @@
 package com.stripe.android.paymentsheet.addresselement
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Parcelable
-import com.stripe.android.paymentsheet.PaymentOptionResult
-import com.stripe.android.view.ActivityStarter
 import kotlinx.parcelize.Parcelize
 
-sealed class AddressLauncherResult(
-    val resultCode: Int
-) : Parcelable {
+sealed class AddressLauncherResult : Parcelable {
+
+    internal abstract val resultCode: Int
+
     @Parcelize
     data class Succeeded(
         val address: AddressDetails
-    ) : AddressLauncherResult(Activity.RESULT_OK)
+    ) : AddressLauncherResult() {
+        override val resultCode: Int
+            get() = Activity.RESULT_OK
+    }
 
     @Parcelize
-    object Canceled : AddressLauncherResult(Activity.RESULT_CANCELED)
-
-    internal companion object {
-        private const val EXTRA_RESULT = ActivityStarter.Result.EXTRA
-
-        @JvmSynthetic
-        internal fun fromIntent(intent: Intent?): PaymentOptionResult? {
-            return intent?.getParcelableExtra(EXTRA_RESULT)
-        }
+    object Canceled : AddressLauncherResult() {
+        override val resultCode: Int
+            get() = Activity.RESULT_CANCELED
     }
 }
