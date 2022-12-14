df3381b71f3283bbf50adf2b337b9a806fc7e88c
Carlos Munoz <carlosmuvi@stripe.com>
2022-12-13 19:48:33 -0800

    Tries linkmore.

--- scripts/execute_maestro_tests.sh
+++ scripts/execute_maestro_tests.sh
@@ -1,42 +1,39 @@
 #!/usr/bin/env bash
 
+reports_path="financial-connections-example/build/reports"
+logs_file_path="$reports_path/emulator.log"
+
 # Uninstall existing app on cached AVD.
 adb uninstall com.stripe.android.financialconnections.example || true
 
 # Create folder for reports
-mkdir -p financial-connections-example/build/reports
+mkdir -p $reports_path
 
 # create log file and allow writes.
-rm financial-connections-example/build/reports/emulator.log
-touch financial-connections-example/build/reports/emulator.log
-chmod 777 financial-connections-example/build/reports/emulator.log
+rm $logs_file_path
+touch $logs_file_path
+chmod 777 $logs_file_path
 
 # Compile and install APK.
 ./gradlew -PSTRIPE_FINANCIAL_CONNECTIONS_EXAMPLE_BACKEND_URL=$STRIPE_FINANCIAL_CONNECTIONS_EXAMPLE_BACKEND_URL :financial-connections-example:installDebug
-
-# Clear logs
-adb logcat -c
 
 # Start screen record (adb screenrecord has a 3 min limit, for now take consequent recordings.
 adb shell "screenrecord /sdcard/rec-1.mp4; screenrecord /sdcard/rec-2.mp4; screenrecord /sdcard/rec-3.mp4; screenrecord /sdcard/rec-4.mp4" &
 childpid=$!
 
-# Start collecting logs
-adb logcat >> financial-connections-example/build/reports/emulator.log &
+# Clear and start collecting logs
+adb logcat -c
+adb logcat >> $logs_file_path &
 maestro test --format junit maestro/financial-connections
 result=$?
 
-# Stop video recording and pull record.
+# Stop video recording and pull record parts.
 kill "$childpid"
 wait "$childpid"
-cd financial-connections-example/build/reports/
+cd $reports_path/
 adb pull /sdcard/rec-1.mp4 || true
 adb pull /sdcard/rec-2.mp4 || true
 adb pull /sdcard/rec-3.mp4 || true
 adb pull /sdcard/rec-4.mp4 || true
 
-#echo "TEST $?"
 exit "$result"
-
-
-
