d10533d98122980463eec2358bc221edd7164e5a
Jay Newstrom <jaynewstrom@stripe.com>
2022-12-15 09:33:15 -0600

    Inject PaymentOptionFactory.

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/flowcontroller/FlowControllerFactory.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/flowcontroller/FlowControllerFactory.kt
@@ -9,8 +9,6 @@ import androidx.lifecycle.ViewModelStoreOwner
 import com.stripe.android.paymentsheet.PaymentOptionCallback
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheetResultCallback
-import com.stripe.android.paymentsheet.model.PaymentOptionFactory
-import com.stripe.android.uicore.image.StripeImageLoader
 
 internal class FlowControllerFactory(
     private val viewModelStoreOwner: ViewModelStoreOwner,
@@ -18,7 +16,6 @@ internal class FlowControllerFactory(
     private val appContext: Context,
     private val activityResultCaller: ActivityResultCaller,
     private val statusBarColor: () -> Int?,
-    private val paymentOptionFactory: PaymentOptionFactory,
     private val paymentOptionCallback: PaymentOptionCallback,
     private val paymentResultCallback: PaymentSheetResultCallback
 ) {
@@ -32,10 +29,6 @@ internal class FlowControllerFactory(
         appContext = activity.applicationContext,
         activityResultCaller = activity,
         statusBarColor = { activity.window.statusBarColor },
-        paymentOptionFactory = PaymentOptionFactory(
-            activity.resources,
-            StripeImageLoader(activity.applicationContext)
-        ),
         paymentOptionCallback = paymentOptionCallback,
         paymentResultCallback = paymentResultCallback,
     )
@@ -50,10 +43,6 @@ internal class FlowControllerFactory(
         appContext = fragment.requireContext().applicationContext,
         activityResultCaller = fragment,
         statusBarColor = { fragment.activity?.window?.statusBarColor },
-        paymentOptionFactory = PaymentOptionFactory(
-            fragment.resources,
-            StripeImageLoader(fragment.requireContext().applicationContext)
-        ),
         paymentOptionCallback = paymentOptionCallback,
         paymentResultCallback = paymentResultCallback,
     )
@@ -65,7 +54,6 @@ internal class FlowControllerFactory(
             lifecycleOwner = lifecycleOwner,
             activityResultCaller = activityResultCaller,
             statusBarColor = statusBarColor,
-            paymentOptionFactory = paymentOptionFactory,
             paymentOptionCallback = paymentOptionCallback,
             paymentResultCallback = paymentResultCallback
         )
