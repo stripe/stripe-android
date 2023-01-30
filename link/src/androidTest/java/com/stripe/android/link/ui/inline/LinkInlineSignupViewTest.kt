e4a9d92b7efd87e9f5bf0597d71570057df7ebe7
Carlos M <99293320+carlosmuvi-stripe@users.noreply.github.com>
2023-01-30 14:09:55 -0800

    Moves EmailConfig to stripe-ui-core. (#6139)

--- link/src/androidTest/java/com/stripe/android/link/ui/inline/LinkInlineSignupViewTest.kt
+++ link/src/androidTest/java/com/stripe/android/link/ui/inline/LinkInlineSignupViewTest.kt
@@ -12,7 +12,7 @@ import com.stripe.android.link.theme.DefaultLinkTheme
 import com.stripe.android.link.ui.ErrorMessage
 import com.stripe.android.link.ui.progressIndicatorTestTag
 import com.stripe.android.link.ui.signup.SignUpState
-import com.stripe.android.ui.core.elements.EmailConfig
+import com.stripe.android.uicore.elements.EmailConfig
 import com.stripe.android.ui.core.elements.NameConfig
 import com.stripe.android.uicore.elements.PhoneNumberController
 import com.stripe.android.uicore.elements.SimpleTextFieldController
