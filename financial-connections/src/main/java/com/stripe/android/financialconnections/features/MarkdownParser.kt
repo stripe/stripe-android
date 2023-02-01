a585512b2076f6a48cda1f5fe1940e58a36ddb8e
Carlos Munoz <carlosmuvi@stripe.com>
2023-02-01 13:22:18 -0800

    Extracts regexes to constants.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/MarkdownParser.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/MarkdownParser.kt
@@ -2,13 +2,18 @@ package com.stripe.android.financialconnections.features
 
 internal object MarkdownParser {
 
+    private const val REGEX_BOLD_ASTERISKS = "\\*\\*(.*?)\\*\\*"
+    private const val REGEX_BOLD_UNDERSCORES = "__([^_]+)__"
+
+    private const val REGEX_LINK = "\\[([^]]+)]\\(([^)]+)\\)"
+
     private val markDownToHtmlRegex = listOf<Pair<Regex, (MatchResult) -> CharSequence>>(
         // bold, italics rules
-        "\\*\\*(.*?)\\*\\*".toRegex() to { "<b>${it.groupValues[1]}</b>" },
-        "__([^_]+)__".toRegex() to { "<b>${it.groupValues[1]}</b>" },
+        REGEX_BOLD_ASTERISKS.toRegex() to { "<b>${it.groupValues[1]}</b>" },
+        REGEX_BOLD_UNDERSCORES.toRegex() to { "<b>${it.groupValues[1]}</b>" },
 
         // links
-        "\\[([^]]+)]\\(([^)]+)\\)".toRegex() to { "<a href=\"${it.groupValues[2]}\">${it.groupValues[1]}</a>" }
+        REGEX_LINK.toRegex() to { "<a href=\"${it.groupValues[2]}\">${it.groupValues[1]}</a>" }
     )
 
     internal fun toHtml(string: String): String {
