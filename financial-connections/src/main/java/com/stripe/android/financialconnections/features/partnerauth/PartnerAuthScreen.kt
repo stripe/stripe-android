68edbcb78c38cae794355324c00970e06e558f82
Carlos Munoz <carlosmuvi@stripe.com>
2023-02-01 15:31:01 -0800

    Show loading while browser is opened.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/partnerauth/PartnerAuthScreen.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/partnerauth/PartnerAuthScreen.kt
@@ -1,15 +1,19 @@
 package com.stripe.android.financialconnections.features.partnerauth
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
@@ -178,10 +182,7 @@ private fun LoadedContent(
             )
         }
 
-        is Loading -> LoadingContent(
-            stringResource(id = R.string.stripe_partnerauth_loading_title),
-            stringResource(id = R.string.stripe_partnerauth_loading_desc)
-        )
+        is Loading -> BrowserLoadingContent()
 
         is Success -> LoadingContent(
             stringResource(R.string.stripe_account_picker_loading_title),
@@ -192,6 +193,15 @@ private fun LoadedContent(
             // TODO@carlosmuvi translate error type to specific error screen.
             InstitutionUnknownErrorContent(onSelectAnotherBank)
         }
+    }
+}
+
+@Composable
+private fun BrowserLoadingContent() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator(
+            color = FinancialConnectionsTheme.colors.iconBrand
+        )
     }
 }
 
