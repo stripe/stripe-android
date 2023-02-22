417d78c7676984e4a0f02c61a8d113f2a4a8a8c9
Till Hellmund <tillh@stripe.com>
2023-02-22 11:49:16 -0500

    Fix PaymentOptionTest

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/PaymentOptionTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/PaymentOptionTest.kt
@@ -1,5 +1,6 @@
 package com.stripe.android.paymentsheet.ui
 
+import android.os.Build
 import androidx.compose.ui.test.assertContentDescriptionEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
@@ -8,18 +9,18 @@ import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.paymentsheet.PaymentOptionUi
 import com.stripe.android.paymentsheet.R
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.Q])
 class PaymentOptionTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Ignore("Figure out why this times out when run with other tests")
     @Test
     fun `Turns card label into screen reader-friendly text`() {
         val label = "Card ending in 4242"
