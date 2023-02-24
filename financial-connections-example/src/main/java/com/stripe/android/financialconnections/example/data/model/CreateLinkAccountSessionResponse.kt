a9afe108123fd3924bb02da8107f30c254ee354d
Jay Newstrom <jaynewstrom@stripe.com>
2023-02-24 12:17:51 -0600

    Make proguard work with financial connections.

--- financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/data/model/CreateLinkAccountSessionResponse.kt
+++ financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/data/model/CreateLinkAccountSessionResponse.kt
@@ -1,14 +1,16 @@
 package com.stripe.android.financialconnections.example.data.model
 
+import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 
+@Keep
 data class CreateLinkAccountSessionResponse(
     @SerializedName("client_secret") val clientSecret: String,
     @SerializedName("las_id") val lasId: String,
     @SerializedName("publishable_key") val publishableKey: String
 )
 
-
+@Keep
 data class CreateIntentResponse(
     @SerializedName("secret") val intentSecret: String,
     @SerializedName("publishable_key") val publishableKey: String
