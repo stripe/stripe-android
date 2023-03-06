d9529179e82f5ed283112d4fda14febcb6077d0f
Till Hellmund <tillh@stripe.com>
2023-03-06 16:02:46 -0500

    Switch to different devices for BrowserStack

--- scripts/browserstack.py
+++ scripts/browserstack.py
@@ -135,13 +135,16 @@ def executeTests(appUrl, testUrl):
     # firefox doesn't work on this samsung: Samsung Galaxy S9 Plus-9.0"]
     response = requests.post(url, json={
          "app": appUrl,
-         "devices": ["Google Pixel 3-9.0"],
+         "devices": [
+            "Google Pixel 7-13.0",
+            "Samsung Galaxy S22-12.0",
+            "Google Pixel-7.1",
+         ],
          "testSuite": testUrl,
          "networkLogs": True,
          "deviceLogs": True,
          "video": True,
          "acceptInsecureCerts": True,
-#          "language": "en_US",
          "locale": "en_US",
          "enableSpoonFramework": False,
          "project": "Mobile Payments",
