05d822b4220e20b3f9ec6e42e9797cfe5a444d24
Till Hellmund <tillh@stripe.com>
2023-03-07 15:11:26 -0500

    Make tweaks to ConfirmCallbacks and remove comments for now

--- payments-core/src/main/java/com/stripe/android/ConfirmCallback.kt
+++ payments-core/src/main/java/com/stripe/android/ConfirmCallback.kt
@@ -7,24 +7,12 @@ import kotlin.coroutines.suspendCoroutine
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 interface ConfirmCallback {
 
-    /**
-     * 🚧 Under construction 🚧
-     * The model for the confirmation response from the integrators servers.
-     */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     sealed interface Result {
 
-        /**
-         * 🚧 Under construction 🚧
-         * @param clientSecret the client secret returned by the integrators server
-         */
         @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
         data class Success(val clientSecret: String) : Result
 
-        /**
-         * 🚧 Under construction 🚧
-         * @param error the error returned while retrieving the confirmation response
-         */
         @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
         data class Failure(val error: Throwable) : Result
     }
@@ -33,32 +21,18 @@ interface ConfirmCallback {
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 fun interface ConfirmCallbackForServerSideConfirmation : ConfirmCallback {
 
-    /**
-     * 🚧 Under construction 🚧
-     * The callback to implement to retrieve the intent client secret in the deferred flow.
-     *
-     * @param paymentMethodId the payment method ID to create the intent with
-     * @param shouldSavePaymentMethod whether or not the payment method should be saved
-     *
-     * @return a [Result] which contains the intent client secret and whether or not the
-     * intent was confirmed on the integrators server.
-     */
-    suspend fun onConfirmResponse(paymentMethodId: String, shouldSavePaymentMethod: Boolean): ConfirmCallback.Result
+    suspend fun onConfirmResponse(
+        paymentMethodId: String,
+        shouldSavePaymentMethod: Boolean,
+    ): ConfirmCallback.Result
 }
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 fun interface ConfirmCallbackForClientSideConfirmation : ConfirmCallback {
 
-    /**
-     * 🚧 Under construction 🚧
-     * The callback to implement to retrieve the intent client secret in the deferred flow.
-     *
-     * @param paymentMethodId the payment method ID to create the intent with
-     *
-     * @return a [Result] which contains the intent client secret and whether or not the
-     * intent was confirmed on the integrators server.
-     */
-    suspend fun onConfirmResponse(paymentMethodId: String): ConfirmCallback.Result
+    suspend fun onConfirmResponse(
+        paymentMethodId: String,
+    ): ConfirmCallback.Result
 }
 
 internal fun interface LegacyConfirmCallbackForServerSideConfirmation : ConfirmCallbackForServerSideConfirmation {
@@ -71,9 +45,7 @@ internal fun interface LegacyConfirmCallbackForServerSideConfirmation : ConfirmC
             onConfirmResponse(
                 paymentMethodId = paymentMethodId,
                 shouldSavePaymentMethod = shouldSavePaymentMethod,
-                onResult = {
-                    continuation.resume(it)
-                }
+                resultListener = continuation::resume,
             )
         }
     }
@@ -81,8 +53,12 @@ internal fun interface LegacyConfirmCallbackForServerSideConfirmation : ConfirmC
     fun onConfirmResponse(
         paymentMethodId: String,
         shouldSavePaymentMethod: Boolean,
-        onResult: (ConfirmCallback.Result) -> Unit
+        resultListener: ResultListener,
     )
+
+    fun interface ResultListener {
+        fun onResult(result: ConfirmCallback.Result)
+    }
 }
 
 internal fun interface LegacyConfirmCallbackForClientSideConfirmation : ConfirmCallbackForClientSideConfirmation {
@@ -91,15 +67,17 @@ internal fun interface LegacyConfirmCallbackForClientSideConfirmation : ConfirmC
         return suspendCoroutine { continuation ->
             onConfirmResponse(
                 paymentMethodId = paymentMethodId,
-                onResult = {
-                    continuation.resume(it)
-                }
+                resultListener = continuation::resume,
             )
         }
     }
 
     fun onConfirmResponse(
         paymentMethodId: String,
-        onResult: (ConfirmCallback.Result) -> Unit
+        resultListener: ResultListener,
     )
+
+    fun interface ResultListener {
+        fun onResult(result: ConfirmCallback.Result)
+    }
 }
