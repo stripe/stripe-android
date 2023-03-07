c293f1f2072b5d244e9d4fb1a198f3d2eb37a0fe
Till Hellmund <tillh@stripe.com>
2023-03-07 09:33:23 -0500

    Add retrying to BrowserStack tests and extend test devices (#6325)
    * Add RetryRule and combine all rules in TestRules
    
    * Switch to different devices for BrowserStack
    
    * Tweak log statement
    
    * Remove redundant TestGooglePay
    
    This is covered in PaymentSheetViewModelTest thanks to the new-ish WalletsContainerState.
    
    * Replace Pixel with Pixel 2 for BrowserStack tests
    
    * Fix issues with animations rule
    
    Fix rule not being used and re-enable animations after use.
    
    * Address code review feedback
    
    - Make TestRules constructor private

---
+++ paymentsheet-example/src/androidTestDebug/java/com/stripe/android/utils/RetryRule.kt
@@ -0,0 +1,33 @@
+package com.stripe.android.utils
+
+import android.util.Log
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class RetryRule(private val attempts: Int) : TestRule {
+
+    override fun apply(base: Statement, description: Description): Statement {
+        val logTag = description.className
+
+        return object : Statement() {
+
+            @Throws
+            override fun evaluate() {
+                for (attempt in 1..attempts) {
+                    val isLast = attempts == attempt
+                    val error = runCatching { base.evaluate() }.exceptionOrNull()
+
+                    if (error != null && isLast) {
+                        throw error
+                    } else if (error != null) {
+                        Log.d(logTag, "Failed attempt $attempt out of $attempts with error")
+                    } else {
+                        // The test succeeded
+                        return
+                    }
+                }
+            }
+        }
+    }
+}
