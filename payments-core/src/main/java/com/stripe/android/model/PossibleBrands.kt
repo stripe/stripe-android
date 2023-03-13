c2911ece70058dfae5074b97d7781aeaca748eec
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-03-13 14:33:45 -0700

    Revert "Restrict possible brands" (#6359)
    This reverts commit 107800234ef9e7760e6e67657eb89246394565ae.

--- payments-core/src/main/java/com/stripe/android/model/PossibleBrands.kt
+++ payments-core/src/main/java/com/stripe/android/model/PossibleBrands.kt
@@ -1,11 +1,9 @@
 package com.stripe.android.model
 
-import androidx.annotation.RestrictTo
 import com.stripe.android.core.model.StripeModel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 data class PossibleBrands(
     val brands: List<CardBrand>
 ) : StripeModel
