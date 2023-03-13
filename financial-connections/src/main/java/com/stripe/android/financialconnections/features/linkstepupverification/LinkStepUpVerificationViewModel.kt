51e50077a47a16165ffc18f99978c43b0d27d6c0
Carlos Munoz <carlosmuvi@stripe.com>
2023-03-13 09:57:42 -0700

    Adds events to start and confirm verification.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/linkstepupverification/LinkStepUpVerificationViewModel.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/linkstepupverification/LinkStepUpVerificationViewModel.kt
@@ -1,27 +1,37 @@
 package com.stripe.android.financialconnections.features.linkstepupverification
 
 import com.airbnb.mvrx.Async
+import com.airbnb.mvrx.Fail
+import com.airbnb.mvrx.Loading
 import com.airbnb.mvrx.MavericksState
 import com.airbnb.mvrx.MavericksViewModel
 import com.airbnb.mvrx.MavericksViewModelFactory
+import com.airbnb.mvrx.Success
 import com.airbnb.mvrx.Uninitialized
 import com.airbnb.mvrx.ViewModelContext
 import com.stripe.android.core.Logger
 import com.stripe.android.financialconnections.analytics.FinancialConnectionsAnalyticsTracker
 import com.stripe.android.financialconnections.analytics.FinancialConnectionsEvent.Error
-import com.stripe.android.financialconnections.analytics.FinancialConnectionsEvent.PaneLoaded
+import com.stripe.android.financialconnections.analytics.FinancialConnectionsEvent.VerificationStepUpError
+import com.stripe.android.financialconnections.analytics.FinancialConnectionsEvent.VerificationStepUpError.Error.ConsumerNotFoundError
+import com.stripe.android.financialconnections.analytics.FinancialConnectionsEvent.VerificationStepUpError.Error.LookupConsumerSession
+import com.stripe.android.financialconnections.analytics.FinancialConnectionsEvent.VerificationStepUpError.Error.MarkLinkVerifiedError
+import com.stripe.android.financialconnections.analytics.FinancialConnectionsEvent.VerificationStepUpError.Error.StartVerificationError
+import com.stripe.android.financialconnections.analytics.FinancialConnectionsEvent.VerificationStepUpSuccess
 import com.stripe.android.financialconnections.domain.ConfirmVerification
 import com.stripe.android.financialconnections.domain.GetCachedAccounts
 import com.stripe.android.financialconnections.domain.GetManifest
 import com.stripe.android.financialconnections.domain.GoNext
-import com.stripe.android.financialconnections.domain.LookupAccount
+import com.stripe.android.financialconnections.domain.LookupConsumerAndStartVerification
 import com.stripe.android.financialconnections.domain.MarkLinkStepUpVerified
 import com.stripe.android.financialconnections.domain.SelectNetworkedAccount
-import com.stripe.android.financialconnections.domain.StartVerification
 import com.stripe.android.financialconnections.domain.UpdateCachedAccounts
 import com.stripe.android.financialconnections.domain.UpdateLocalManifest
+import com.stripe.android.financialconnections.features.linkstepupverification.LinkStepUpVerificationState.Payload
 import com.stripe.android.financialconnections.model.FinancialConnectionsSessionManifest.Pane
 import com.stripe.android.financialconnections.ui.FinancialConnectionsSheetNativeActivity
+import com.stripe.android.model.ConsumerSession
+import com.stripe.android.model.VerificationType
 import com.stripe.android.uicore.elements.IdentifierSpec
 import com.stripe.android.uicore.elements.OTPController
 import com.stripe.android.uicore.elements.OTPElement
@@ -33,36 +43,61 @@ internal class LinkStepUpVerificationViewModel @Inject constructor(
     initialState: LinkStepUpVerificationState,
     private val eventTracker: FinancialConnectionsAnalyticsTracker,
     private val getManifest: GetManifest,
-    private val lookupAccount: LookupAccount,
-    private val startVerification: StartVerification,
+    private val lookupConsumerAndStartVerification: LookupConsumerAndStartVerification,
     private val confirmVerification: ConfirmVerification,
     private val selectNetworkedAccount: SelectNetworkedAccount,
     private val getCachedAccounts: GetCachedAccounts,
     private val updateLocalManifest: UpdateLocalManifest,
     private val markLinkStepUpVerified: MarkLinkStepUpVerified,
     private val updateCachedAccounts: UpdateCachedAccounts,
+    private val analyticsTracker: FinancialConnectionsAnalyticsTracker,
     private val goNext: GoNext,
     private val logger: Logger
 ) : MavericksViewModel<LinkStepUpVerificationState>(initialState) {
 
     init {
         logErrors()
-        suspend {
-            val email = requireNotNull(getManifest().accountholderCustomerEmailAddress)
-            val consumerSession = requireNotNull(lookupAccount(email).consumerSession)
-            startVerification.email(consumerSessionClientSecret = consumerSession.clientSecret)
-            eventTracker.track(PaneLoaded(Pane.LINK_STEP_UP_VERIFICATION))
-            LinkStepUpVerificationState.Payload(
-                email = consumerSession.emailAddress,
-                phoneNumber = consumerSession.redactedPhoneNumber,
-                consumerSessionClientSecret = consumerSession.clientSecret,
-                otpElement = OTPElement(
-                    IdentifierSpec.Generic("otp"),
-                    OTPController()
-                )
-            )
-        }.execute { copy(payload = it) }
+        viewModelScope.launch { lookupAndStartVerification() }
     }
+
+    private suspend fun lookupAndStartVerification() = runCatching {
+        requireNotNull(getManifest().accountholderCustomerEmailAddress)
+    }
+        .onFailure { setState { copy(payload = Fail(it)) } }
+        .onSuccess { email ->
+            setState { copy(payload = Loading()) }
+            lookupConsumerAndStartVerification(
+                email = email,
+                verificationType = VerificationType.EMAIL,
+                onConsumerNotFound = {
+                    analyticsTracker.track(VerificationStepUpError(PANE, ConsumerNotFoundError))
+                    goNext(Pane.INSTITUTION_PICKER)
+                },
+                onLookupError = { error ->
+                    analyticsTracker.track(VerificationStepUpError(PANE, LookupConsumerSession))
+                    setState { copy(payload = Fail(error)) }
+                },
+                onStartVerification = { /* no-op */ },
+                onVerificationStarted = { consumerSession ->
+                    val payload = buildPayload(consumerSession)
+                    setState { copy(payload = Success(payload)) }
+                },
+                onStartVerificationError = { error ->
+                    analyticsTracker.track(VerificationStepUpError(PANE, StartVerificationError))
+                    setState { copy(payload = Fail(error)) }
+                }
+            )
+        }
+
+    private fun buildPayload(consumerSession: ConsumerSession) = Payload(
+        email = consumerSession.emailAddress,
+        phoneNumber = consumerSession.redactedPhoneNumber,
+        consumerSessionClientSecret = consumerSession.clientSecret,
+        otpElement = OTPElement(
+            IdentifierSpec.Generic("otp"),
+            OTPController()
+        )
+    )
 
     private fun logErrors() {
         onAsync(
@@ -86,10 +121,22 @@ internal class LinkStepUpVerificationViewModel @Inject constructor(
             consumerSessionClientSecret = payload.consumerSessionClientSecret,
             verificationCode = otp
         )
+
+        // Get accounts selected in networked accounts picker.
         val selectedAccount = getCachedAccounts().first()
 
         // Mark session as verified.
-        markLinkStepUpVerified()
+        runCatching { markLinkStepUpVerified() }
+            .onFailure {
+                analyticsTracker.track(
+                    VerificationStepUpError(
+                        PANE,
+                        MarkLinkVerifiedError
+                    )
+                )
+            }
+            .onSuccess { analyticsTracker.track(VerificationStepUpSuccess(PANE)) }
+            .getOrThrow()
 
         // Mark networked account as selected.
         val activeInstitution = selectNetworkedAccount(
@@ -107,19 +154,36 @@ internal class LinkStepUpVerificationViewModel @Inject constructor(
 
     fun onClickableTextClick(text: String) {
         when (text) {
-            CLICKABLE_TEXT_RESEND_CODE -> onResendOtp()
+            CLICKABLE_TEXT_RESEND_CODE -> viewModelScope.launch { onResendOtp() }
             else -> logger.error("Unknown clicked text $text")
         }
     }
 
-    private fun onResendOtp() = suspend {
-        val email = requireNotNull(getManifest().accountholderCustomerEmailAddress)
-        val consumerSession = requireNotNull(lookupAccount(email).consumerSession)
-        startVerification.email(consumerSessionClientSecret = consumerSession.clientSecret)
-        Unit
-    }.execute {
-        copy(resendOtp = it)
+    private suspend fun onResendOtp() = runCatching {
+        requireNotNull(getManifest().accountholderCustomerEmailAddress)
     }
+        .onFailure { setState { copy(resendOtp = Fail(it)) } }
+        .onSuccess { email ->
+            setState { copy(resendOtp = Loading()) }
+            lookupConsumerAndStartVerification(
+                email = email,
+                verificationType = VerificationType.EMAIL,
+                onConsumerNotFound = {
+                    analyticsTracker.track(VerificationStepUpError(PANE, ConsumerNotFoundError))
+                    goNext(Pane.INSTITUTION_PICKER)
+                },
+                onLookupError = { error ->
+                    analyticsTracker.track(VerificationStepUpError(PANE, LookupConsumerSession))
+                    setState { copy(resendOtp = Fail(error)) }
+                },
+                onStartVerification = { /* no-op */ },
+                onVerificationStarted = { setState { copy(resendOtp = Success(Unit)) } },
+                onStartVerificationError = { error ->
+                    analyticsTracker.track(VerificationStepUpError(PANE, StartVerificationError))
+                    setState { copy(resendOtp = Fail(error)) }
+                }
+            )
+        }
 
     companion object :
         MavericksViewModelFactory<LinkStepUpVerificationViewModel, LinkStepUpVerificationState> {
@@ -138,13 +202,14 @@ internal class LinkStepUpVerificationViewModel @Inject constructor(
         }
 
         private const val CLICKABLE_TEXT_RESEND_CODE = "resend_code"
+        internal val PANE = Pane.LINK_STEP_UP_VERIFICATION
     }
 }
 
 internal data class LinkStepUpVerificationState(
     val payload: Async<Payload> = Uninitialized,
     val confirmVerification: Async<Unit> = Uninitialized,
-    val resendOtp: Async<Unit> = Uninitialized
+    val resendOtp: Async<Unit> = Uninitialized,
 ) : MavericksState {
 
     data class Payload(
