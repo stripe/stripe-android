f69699b8261ef73ecbb3dc477ed38854087b23e4
Carlos Munoz <carlosmuvi@stripe.com>
2023-03-17 10:14:33 -0700

    Adds middle size ellipsis text component.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/accountpicker/AccountPickerScreen.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/accountpicker/AccountPickerScreen.kt
@@ -61,6 +61,7 @@ import com.stripe.android.financialconnections.ui.TextResource
 import com.stripe.android.financialconnections.ui.components.FinancialConnectionsButton
 import com.stripe.android.financialconnections.ui.components.FinancialConnectionsScaffold
 import com.stripe.android.financialconnections.ui.components.FinancialConnectionsTopAppBar
+import com.stripe.android.financialconnections.ui.components.MiddleEllipsisText
 import com.stripe.android.financialconnections.ui.theme.FinancialConnectionsTheme
 import com.stripe.android.financialconnections.ui.theme.FinancialConnectionsTheme.colors
 
@@ -407,10 +408,8 @@ private fun AccountItem(
             Column(
                 Modifier.weight(0.7f)
             ) {
-                Text(
+                MiddleEllipsisText(
                     text = title,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1,
                     color = if (accountUI.account.allowSelection) {
                         colors.textPrimary
                     } else {
