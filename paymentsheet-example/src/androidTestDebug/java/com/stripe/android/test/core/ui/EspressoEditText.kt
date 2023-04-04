7c5ccfc421208734f209ab4f7383e953bec17135
Jay Newstrom <jaynewstrom@stripe.com>
2023-04-04 10:03:50 -0500

    Fix espresso tests.

--- paymentsheet-example/src/androidTestDebug/java/com/stripe/android/test/core/ui/EspressoEditText.kt
+++ paymentsheet-example/src/androidTestDebug/java/com/stripe/android/test/core/ui/EspressoEditText.kt
@@ -3,14 +3,15 @@ package com.stripe.android.test.core.ui
 import androidx.annotation.IdRes
 import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.matcher.ViewMatchers.withId
 
 class EspressoEditText(
     @IdRes private val id: Int,
 ) {
     fun enter(text: String) {
-        onView(withId(id)).perform(typeText(text))
+        onView(withId(id)).perform(scrollTo(), replaceText(text))
         closeSoftKeyboard()
     }
 }
