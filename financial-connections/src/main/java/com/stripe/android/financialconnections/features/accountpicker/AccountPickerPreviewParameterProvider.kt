e5b805f7a2cd5b4a1ad87cfc9023255357653c84
Carlos Munoz <carlosmuvi@stripe.com>
2023-04-04 15:33:56 -0700

    Updates preview parameter provider for signup pane.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/accountpicker/AccountPickerPreviewParameterProvider.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/accountpicker/AccountPickerPreviewParameterProvider.kt
@@ -4,17 +4,16 @@ package com.stripe.android.financialconnections.features.accountpicker
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.airbnb.mvrx.Success
-import com.stripe.android.financialconnections.features.accountpicker.AccountPickerState.PartnerAccountUI
 import com.stripe.android.financialconnections.features.common.AccessibleDataCalloutModel
 import com.stripe.android.financialconnections.model.FinancialConnectionsAccount
 import com.stripe.android.financialconnections.model.PartnerAccount
 
-internal class AccountPickerPreviewParameterProvider : PreviewParameterProvider<AccountPickerState> {
+internal class AccountPickerPreviewParameterProvider :
+    PreviewParameterProvider<AccountPickerState> {
     override val values = sequenceOf(
         multiSelect(),
         singleSelect(),
-        singleSelectWithConfirm(),
-
+        singleSelectWithConfirm()
     )
 
     override val count: Int
@@ -72,83 +71,63 @@ internal class AccountPickerPreviewParameterProvider : PreviewParameterProvider<
     )
 
     private fun partnerAccountList() = listOf(
-        PartnerAccountUI(
-            PartnerAccount(
-                authorization = "Authorization",
-                category = FinancialConnectionsAccount.Category.CASH,
-                id = "id1",
-                name = "With balance",
-                balanceAmount = 1000,
-                displayableAccountNumbers = "1234",
-                currency = "$",
-                _allowSelection = true,
-                allowSelectionMessage = "",
-                subcategory = FinancialConnectionsAccount.Subcategory.CHECKING,
-                supportedPaymentMethodTypes = emptyList()
-            ),
-            institutionIcon = null,
-            formattedBalance = "$1,000"
+        PartnerAccount(
+            authorization = "Authorization",
+            category = FinancialConnectionsAccount.Category.CASH,
+            id = "id1",
+            name = "With balance",
+            balanceAmount = 1000,
+            displayableAccountNumbers = "1234",
+            currency = "$",
+            _allowSelection = true,
+            allowSelectionMessage = "",
+            subcategory = FinancialConnectionsAccount.Subcategory.CHECKING,
+            supportedPaymentMethodTypes = emptyList()
         ),
-        PartnerAccountUI(
-            PartnerAccount(
-                authorization = "Authorization",
-                category = FinancialConnectionsAccount.Category.CASH,
-                id = "id2",
-                name = "With balance disabled",
-                balanceAmount = 1000,
-                _allowSelection = true,
-                allowSelectionMessage = "",
-                subcategory = FinancialConnectionsAccount.Subcategory.SAVINGS,
-                supportedPaymentMethodTypes = emptyList()
-            ),
-            institutionIcon = null,
-            formattedBalance = "$1,000"
+        PartnerAccount(
+            authorization = "Authorization",
+            category = FinancialConnectionsAccount.Category.CASH,
+            id = "id2",
+            name = "With balance disabled",
+            balanceAmount = 1000,
+            _allowSelection = true,
+            allowSelectionMessage = "",
+            subcategory = FinancialConnectionsAccount.Subcategory.SAVINGS,
+            supportedPaymentMethodTypes = emptyList()
         ),
-        PartnerAccountUI(
-            PartnerAccount(
-                authorization = "Authorization",
-                category = FinancialConnectionsAccount.Category.CASH,
-                id = "id3",
-                name = "No balance",
-                displayableAccountNumbers = "1234",
-                subcategory = FinancialConnectionsAccount.Subcategory.CREDIT_CARD,
-                _allowSelection = false,
-                allowSelectionMessage = "Cannot be selected",
-                supportedPaymentMethodTypes = emptyList()
-            ),
-            institutionIcon = null,
-            formattedBalance = null
+        PartnerAccount(
+            authorization = "Authorization",
+            category = FinancialConnectionsAccount.Category.CASH,
+            id = "id3",
+            name = "No balance",
+            displayableAccountNumbers = "1234",
+            subcategory = FinancialConnectionsAccount.Subcategory.CREDIT_CARD,
+            _allowSelection = false,
+            allowSelectionMessage = "Cannot be selected",
+            supportedPaymentMethodTypes = emptyList()
         ),
-        PartnerAccountUI(
-            PartnerAccount(
-                authorization = "Authorization",
-                category = FinancialConnectionsAccount.Category.CASH,
-                id = "id4",
-                name = "No balance disabled",
-                displayableAccountNumbers = "1234",
-                subcategory = FinancialConnectionsAccount.Subcategory.CHECKING,
-                _allowSelection = false,
-                allowSelectionMessage = "Cannot be selected",
-                supportedPaymentMethodTypes = emptyList()
-            ),
-            institutionIcon = null,
-            formattedBalance = null
+        PartnerAccount(
+            authorization = "Authorization",
+            category = FinancialConnectionsAccount.Category.CASH,
+            id = "id4",
+            name = "No balance disabled",
+            displayableAccountNumbers = "1234",
+            subcategory = FinancialConnectionsAccount.Subcategory.CHECKING,
+            _allowSelection = false,
+            allowSelectionMessage = "Cannot be selected",
+            supportedPaymentMethodTypes = emptyList()
         ),
-        PartnerAccountUI(
-            PartnerAccount(
-                authorization = "Authorization",
-                category = FinancialConnectionsAccount.Category.CASH,
-                id = "id5",
-                name = "Very long institution that is already linked",
-                displayableAccountNumbers = "1234",
-                linkedAccountId = "linkedAccountId",
-                _allowSelection = true,
-                subcategory = FinancialConnectionsAccount.Subcategory.CHECKING,
-                supportedPaymentMethodTypes = emptyList()
-            ),
-            institutionIcon = null,
-            formattedBalance = null
-        )
+        PartnerAccount(
+            authorization = "Authorization",
+            category = FinancialConnectionsAccount.Category.CASH,
+            id = "id5",
+            name = "Very long institution that is already linked",
+            displayableAccountNumbers = "1234",
+            linkedAccountId = "linkedAccountId",
+            _allowSelection = true,
+            subcategory = FinancialConnectionsAccount.Subcategory.CHECKING,
+            supportedPaymentMethodTypes = emptyList()
+        ),
     )
 
     private fun accessibleCallout() = AccessibleDataCalloutModel(
