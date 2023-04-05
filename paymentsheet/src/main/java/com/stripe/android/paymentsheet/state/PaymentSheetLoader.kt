df75d64bb29e20f44179b05830878e5bc96137aa
Till Hellmund <tillh@stripe.com>
2023-04-05 13:08:19 -0400

    Use `Result<T>` for ElementsSession endpoint (#6456)
    * Use Result<T> in ElementsSessionRepository
    
    * Add test
    
    * Tweak method name
    
    * Make further code improvements
    
    * Use `mapCatching` to catch validation exception

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/state/PaymentSheetLoader.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/state/PaymentSheetLoader.kt
@@ -73,12 +73,10 @@ internal class DefaultPaymentSheetLoader @Inject constructor(
     ): PaymentSheetLoader.Result = withContext(workContext) {
         val isGooglePayReady = isGooglePayReady(paymentSheetConfiguration)
 
-        runCatching {
-            retrieveElementsSession(
-                initializationMode = initializationMode,
-                configuration = paymentSheetConfiguration,
-            )
-        }.fold(
+        retrieveElementsSession(
+            initializationMode = initializationMode,
+            configuration = paymentSheetConfiguration,
+        ).fold(
             onSuccess = { stripeIntent ->
                 create(
                     stripeIntent = stripeIntent,
@@ -226,21 +224,22 @@ internal class DefaultPaymentSheetLoader @Inject constructor(
     private suspend fun retrieveElementsSession(
         initializationMode: PaymentSheet.InitializationMode,
         configuration: PaymentSheet.Configuration?,
-    ): StripeIntent {
-        val elementsSession = elementsSessionRepository.get(initializationMode)
+    ): Result<StripeIntent> {
+        return elementsSessionRepository.get(initializationMode).mapCatching { elementsSession ->
+            lpmRepository.update(
+                stripeIntent = elementsSession.stripeIntent,
+                serverLpmSpecs = elementsSession.paymentMethodSpecs,
+                billingDetailsCollectionConfiguration =
+                configuration?.billingDetailsCollectionConfiguration
+                    ?: BillingDetailsCollectionConfiguration(),
+            )
 
-        lpmRepository.update(
-            stripeIntent = elementsSession.stripeIntent,
-            serverLpmSpecs = elementsSession.paymentMethodSpecs,
-            billingDetailsCollectionConfiguration =
-            configuration?.billingDetailsCollectionConfiguration ?: BillingDetailsCollectionConfiguration(),
-        )
+            if (lpmRepository.serverSpecLoadingState is ServerSpecState.ServerNotParsed) {
+                eventReporter.onLpmSpecFailure()
+            }
 
-        if (lpmRepository.serverSpecLoadingState is ServerSpecState.ServerNotParsed) {
-            eventReporter.onLpmSpecFailure()
+            stripeIntentValidator.requireValid(elementsSession.stripeIntent)
         }
-
-        return stripeIntentValidator.requireValid(elementsSession.stripeIntent)
     }
 
     private suspend fun loadLinkState(
