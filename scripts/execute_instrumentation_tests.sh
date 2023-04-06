6cfc6727cf4af19e8db3162cf74da46522a25213
Jay Newstrom <jaynewstrom@stripe.com>
2023-04-06 10:18:40 -0500

    Ensure all instrumentation tests pass in CI.

--- scripts/execute_instrumentation_tests.sh
+++ scripts/execute_instrumentation_tests.sh
@@ -4,6 +4,8 @@
 # adb logcat -c
 # adb logcat &
 
+set -e # Fail on error.
+
 # Exclude any modules with screenshot tests here. Then run them with the screenshot test package excluded.
 ./gradlew connectedAndroidTest -x :paymentsheet-example:connectedAndroidTest -x :paymentsheet:connectedAndroidTest
 
