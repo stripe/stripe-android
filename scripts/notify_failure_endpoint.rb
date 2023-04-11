79e8e79d9d7242ef69e3fdcdcac6e5b3e5ea9da9
Carlos Munoz <carlosmuvi@stripe.com>
2023-04-11 15:50:38 -0700

    Pass entire failure url to job.

--- scripts/notify_failure_endpoint.rb
+++ scripts/notify_failure_endpoint.rb
@@ -7,17 +7,16 @@ require 'base64'
 
 env_sdk_failure_notif_endpoint = ARGV[0]
 env_sdk_failure_notif_endpoint_hmac_key = ARGV[1]
-github_run_id = ARGV[2]
+failing_run_url = ARGV[2]
 jira_project = ARGV[3]
-
 
 if !env_sdk_failure_notif_endpoint || !env_sdk_failure_notif_endpoint_hmac_key
   puts "SDK_FAILURE_NOTIFICATION_ENDPOINT` or `SDK_FAILURE_NOTIFICATION_ENDPOINT_HMAC_KEY` not found"
   exit 102
 end
 
-if !github_run_id
-  puts "github_run_id not found"
+if !failing_run_url
+  puts "failing_run_url not found"
   exit 102
 end
 
@@ -39,7 +38,7 @@ params = {
 }
 
 params[:summary] = "stripe-android E2E test failed"
-params[:description] = "Please ACK this ticket and investigate the failure. See https://github.com/stripe/stripe-android/actions/runs/#{github_run_id}"
+params[:description] = "Please ACK this ticket and investigate the failure. See #{failing_run_url}"
 params[:components] = %w[Android]
 
 req.body = params.to_json
@@ -50,4 +49,4 @@ header_data = OpenSSL::HMAC.digest(digest, hmac_key, req.body)
 header_data_64 = Base64.strict_encode64(header_data)
 req.add_field 'X-TM-Signature', header_data_64
 
-res = http.request(req)
+# res = http.request(req)
