c23a5fa373718c4dbf6a62f35d9fcde32dc4b9b1
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-04-20 09:04:30 -0700

    Add recordings to maestro tests (#6571)

--- scripts/execute_maestro_paymentsheet_tests.sh
+++ scripts/execute_maestro_paymentsheet_tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-set -e
 set -o pipefail
 set -x
+
+now=$(date +%F_%H-%M-%S)
+echo $now
 
 # Install Maestro
 export MAESTRO_VERSION=1.21.3; curl -Ls "https://get.maestro.mobile.dev" | bash
@@ -11,13 +13,28 @@ maestro -v
 # Compile and install APK.
 ./gradlew :paymentsheet-example:installDebug
 
-# Run tests with retry.
-retry=1
-while [ $retry -le 3 ]; do
-  echo "Running Maestro tests. Attempt $retry"
-  maestro test --format junit --output maestroReport.xml maestro/paymentsheet
-  if [ $? -eq 0 ]; then
-    break
-  fi
-  retry=$((retry+1))
-done
+# Start screen record (adb screenrecord has a 3 min limit). Can append more recordings as needed.
+# adb shell "screenrecord /sdcard/$now-1.mp4" &
+# adb shell "screenrecord /sdcard/$now-2.mp4" & ...
+adb shell "screenrecord /sdcard/$now-1.mp4" &
+
+# Store the process ID
+childpid=$!
+
+# Clear and start collecting logs
+maestro test --format junit --output maestroReport.xml maestro/paymentsheet
+result=$?
+
+# Wait for the recording process to finish
+kill -2 "$childpid"
+wait "$childpid"
+
+# Sleep for a short duration to allow the process to finalize the video file
+sleep 3
+
+# Pull the video file from the device
+mkdir -p /tmp/test_results
+cd /tmp/test_results
+adb pull "/sdcard/$now-1.mp4" || true
+
+exit "$result"
