21b7b7cc81e4771ad8d11e1c4e7258474110a162
Chen Cen <ccen@stripe.com>
2023-05-15 16:55:00 -0700

    [Identity] Example app UX change for phone verification

--- identity-example/src/main/java/com/stripe/android/identity/example/IdentityExampleViewModel.kt
+++ identity-example/src/main/java/com/stripe/android/identity/example/IdentityExampleViewModel.kt
@@ -6,10 +6,13 @@ import androidx.lifecycle.liveData
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.coroutines.awaitStringResult
 import com.github.kittinunf.result.Result
+import com.stripe.android.identity.example.ui.IdentitySubmissionState
 import com.stripe.android.identity.example.ui.VerificationType
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
 internal class IdentityExampleViewModel(application: Application) : AndroidViewModel(application) {
+    @OptIn(ExperimentalSerializationApi::class)
     private val json by lazy {
         Json {
             ignoreUnknownKeys = true
@@ -20,14 +23,7 @@ internal class IdentityExampleViewModel(application: Application) : AndroidViewM
     }
 
     fun postForResult(
-        type: VerificationType,
-        allowDrivingLicense: Boolean,
-        allowPassport: Boolean,
-        allowId: Boolean,
-        requireLiveCapture: Boolean,
-        requireId: Boolean,
-        requireSelfie: Boolean,
-        requireAddress: Boolean
+        submissionState: IdentitySubmissionState
     ) = liveData {
         val result = Fuel.post(EXAMPLE_BACKEND_URL)
             .header("content-type", "application/json")
@@ -35,24 +31,136 @@ internal class IdentityExampleViewModel(application: Application) : AndroidViewM
                 json.encodeToString(
                     VerificationSessionCreationRequest.serializer(),
                     VerificationSessionCreationRequest(
-                        type = type.value,
+                        type = submissionState.verificationType.value,
                         options =
-                        if (type == VerificationType.DOCUMENT) {
-                            VerificationSessionCreationRequest.Options(
-                                document = VerificationSessionCreationRequest.Document(
-                                    requireIdNumber = requireId,
-                                    requireMatchingSelfie = requireSelfie,
-                                    requireLiveCapture = requireLiveCapture,
-                                    requireAddress = requireAddress,
-                                    allowedTypes = mutableListOf<String>().also {
-                                        if (allowDrivingLicense) it.add(DRIVING_LICENSE)
-                                        if (allowPassport) it.add(PASSPORT)
-                                        if (allowId) it.add(ID_CARD)
-                                    }
-                                )
-                            )
-                        } else {
-                            null
+                        when (submissionState.verificationType) {
+                            VerificationType.DOCUMENT -> {
+                                if (submissionState.requirePhoneVerification == true) {
+                                    VerificationSessionCreationRequest.Options(
+                                        document = VerificationSessionCreationRequest.Options.Document(
+                                            requireIdNumber = submissionState.requireId,
+                                            requireMatchingSelfie = submissionState.requireSelfie,
+                                            requireLiveCapture = submissionState.requireLiveCapture,
+                                            requireAddress = submissionState.requireAddress,
+                                            allowedTypes = mutableListOf<DocumentType>().also {
+                                                if (submissionState.allowDrivingLicense) {
+                                                    it.add(
+                                                    DocumentType.DrivingLicense
+                                                )
+                                                }
+                                                if (submissionState.allowPassport) {
+                                                    it.add(
+                                                    DocumentType.Passport
+                                                )
+                                                }
+                                                if (submissionState.allowId) it.add(DocumentType.IdCard)
+                                            }
+                                        ),
+                                        phone = VerificationSessionCreationRequest.Options.Phone(
+                                            requireVerification = true
+                                        )
+                                    )
+                                } else {
+                                    VerificationSessionCreationRequest.Options(
+                                        document = VerificationSessionCreationRequest.Options.Document(
+                                            requireIdNumber = submissionState.requireId,
+                                            requireMatchingSelfie = submissionState.requireSelfie,
+                                            requireLiveCapture = submissionState.requireLiveCapture,
+                                            requireAddress = submissionState.requireAddress,
+                                            allowedTypes = mutableListOf<DocumentType>().also {
+                                                if (submissionState.allowDrivingLicense) {
+                                                    it.add(
+                                                    DocumentType.DrivingLicense
+                                                )
+                                                }
+                                                if (submissionState.allowPassport) {
+                                                    it.add(
+                                                    DocumentType.Passport
+                                                )
+                                                }
+                                                if (submissionState.allowId) it.add(DocumentType.IdCard)
+                                            }
+                                        )
+                                    )
+                                }
+                            }
+                            VerificationType.PHONE -> {
+                                if (submissionState.useDocumentFallback == true) {
+                                    VerificationSessionCreationRequest.Options(
+                                        document = VerificationSessionCreationRequest.Options.Document(
+                                            allowedTypes = listOf(DocumentType.DrivingLicense)
+                                        ),
+                                        phoneRecords = VerificationSessionCreationRequest.Options.PhoneRecords(
+                                            fallback = Fallback.Document
+                                        ),
+                                        phoneOtp = VerificationSessionCreationRequest.Options.PhoneOTP(
+                                            check = requireNotNull(submissionState.phoneOtpCheck)
+                                        )
+                                    )
+                                } else {
+                                    null
+                                }
+                            }
+                            VerificationType.ID_NUMBER -> {
+                                if (submissionState.requirePhoneVerification == true) {
+                                    VerificationSessionCreationRequest.Options(
+                                        phone = VerificationSessionCreationRequest.Options.Phone(
+                                            requireVerification = true
+                                        )
+                                    )
+                                } else {
+                                    null
+                                }
+                            }
+                            VerificationType.ADDRESS -> {
+                                if (submissionState.requirePhoneVerification == true) {
+                                    VerificationSessionCreationRequest.Options(
+                                        phone = VerificationSessionCreationRequest.Options.Phone(
+                                            requireVerification = true
+                                        )
+                                    )
+                                } else {
+                                    null
+                                }
+                            }
+                        },
+                        providedDetails = when (submissionState.verificationType) {
+                            VerificationType.DOCUMENT -> {
+                                if (submissionState.requirePhoneVerification == true) {
+                                    VerificationSessionCreationRequest.ProvidedDetails(
+                                        phone = requireNotNull(
+                                            submissionState.providedPhoneNumber
+                                        )
+                                    )
+                                } else {
+                                    null
+                                }
+                            }
+                            VerificationType.ADDRESS -> {
+                                if (submissionState.requirePhoneVerification == true) {
+                                    VerificationSessionCreationRequest.ProvidedDetails(
+                                        phone = requireNotNull(
+                                            submissionState.providedPhoneNumber
+                                        )
+                                    )
+                                } else {
+                                    null
+                                }
+                            }
+                            VerificationType.ID_NUMBER -> {
+                                if (submissionState.requirePhoneVerification == true) {
+                                    VerificationSessionCreationRequest.ProvidedDetails(
+                                        phone = requireNotNull(
+                                            submissionState.providedPhoneNumber
+                                        )
+                                    )
+                                } else {
+                                    null
+                                }
+                            }
+                            VerificationType.PHONE -> {
+                                null
+                            }
                         }
                     )
                 )
@@ -75,11 +183,7 @@ internal class IdentityExampleViewModel(application: Application) : AndroidViewM
     }
 
     private companion object {
-        const val DRIVING_LICENSE = "driving_license"
-        const val PASSPORT = "passport"
-        const val ID_CARD = "id_card"
-
         const val EXAMPLE_BACKEND_URL =
-            "https://stripe-mobile-identity-verification-playground.glitch.me/create-verification-session"
+            "https://reflective-fossil-rib.glitch.me/create-verification-session"
     }
 }
