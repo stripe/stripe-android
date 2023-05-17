568e3d38741dd035692722629c630681c011d2bc
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-05-17 16:09:48 -0700

    Add class docs

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/repositories/CustomerAdapter.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/repositories/CustomerAdapter.kt
@@ -4,6 +4,15 @@ import androidx.annotation.RestrictTo
 import com.stripe.android.ExperimentalSavedPaymentMethodsApi
 import com.stripe.android.model.PaymentMethod
 
+/**
+ * [CustomerAdapter] A "bridge" from wallet mode to your backend to fetch Customer-related
+ * information. Typically, you will not need to implement this interface yourself. You should
+ * instead use [StripeCustomerAdapter], which implements [CustomerAdapter] and manages retrieving
+ * and updating a Stripe customer for you.
+ *
+ * Implement this interface if you would prefer retrieving and updating your Stripe customer object
+ * via your own backend instead of using [StripeCustomerAdapter].
+ */
 @ExperimentalSavedPaymentMethodsApi
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 interface CustomerAdapter {
@@ -44,18 +53,28 @@ interface CustomerAdapter {
 }
 
 /**
- * A persistable payment method
+ * A representation of a saved payment method, used for persisting the user's default payment
+ * method.
  */
 @ExperimentalSavedPaymentMethodsApi
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 sealed class PersistablePaymentMethodOption(open val id: String) {
 
+    /**
+     * GooglePay is the customer's default payment method.
+     */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     object GooglePay : PersistablePaymentMethodOption("google_pay")
 
+    /**
+     * Link is the customer's default payment method.
+     */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     object Link : PersistablePaymentMethodOption("link")
 
+    /**
+     * A Stripe payment method backed by a Stripe PaymentMethod ID.
+     */
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     data class StripeId(override val id: String) : PersistablePaymentMethodOption(id)
 
