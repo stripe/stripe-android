c79a4913b0174fafc18f85b2b7a6003581129633
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-05-17 11:09:19 -0700

    Add CustomerAdapter interface

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/repositories/CustomerAdapter.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/repositories/CustomerAdapter.kt
@@ -2,7 +2,71 @@ package com.stripe.android.paymentsheet.repositories
 
 import androidx.annotation.RestrictTo
 import com.stripe.android.ExperimentalSavedPaymentMethodsApi
+import com.stripe.android.model.PaymentMethod
 
 @ExperimentalSavedPaymentMethodsApi
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-interface CustomerAdapter
+interface CustomerAdapter {
+    /**
+     * Retrieves a list of payment methods attached to a customer
+     */
+    suspend fun fetchPaymentMethods(): Result<List<PaymentMethod>>
+
+    /**
+     * Adds a payment method to a customer
+     * @param paymentMethodId, the payment method to attach to the customer
+     */
+    suspend fun attachPaymentMethod(paymentMethodId: String)
+
+    /**
+     * Deletes the given payment method from the customer
+     * @param paymentMethodId, the payment method to detach from the customer
+     */
+    suspend fun detachPaymentMethod(paymentMethodId: String)
+
+    /**
+     * Set the selected payment method option.
+     * @param paymentOption, the [PersistablePaymentMethodOption] to save in the [DataStore]
+     */
+    suspend fun setSelectedPaymentMethodOption(paymentOption: PersistablePaymentMethodOption)
+
+    /**
+     * Fetch the persisted payment method option from the [DataStore]
+     */
+    suspend fun fetchSelectedPaymentMethodOption(): Result<PersistablePaymentMethodOption?>
+
+    /**
+     * Returns a client secret configured to the attach a new payment method to a customer.
+     * This will call your backend to retrieve a client secret if you have provided a
+     * setupIntentClientSecretProvider in the init call.
+     */
+    suspend fun setupIntentClientSecretForCustomerAttach(): Result<String>
+}
+
+/**
+ * A persistable payment method
+ */
+@ExperimentalSavedPaymentMethodsApi
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+sealed class PersistablePaymentMethodOption(open val id: String) {
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    object GooglePay : PersistablePaymentMethodOption("google_pay")
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    object Link : PersistablePaymentMethodOption("link")
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    data class StripeId(override val id: String) : PersistablePaymentMethodOption(id)
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    companion object {
+        fun fromId(id: String): PersistablePaymentMethodOption {
+            return when (id) {
+                "google_pay" -> GooglePay
+                "link" -> Link
+                else -> StripeId(id)
+            }
+        }
+    }
+}
