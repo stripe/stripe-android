2fbfed1b91d1ea384702b2d001f9d2c3a4e7abcd
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-05-18 12:06:44 -0700

    Fix comments

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/repositories/CustomerAdapter.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/repositories/CustomerAdapter.kt
@@ -8,11 +8,11 @@ import com.stripe.android.model.PaymentMethod
 /**
  * [CustomerAdapter] A "bridge" from wallet mode to your backend to fetch Customer-related
  * information. Typically, you will not need to implement this interface yourself. You should
- * instead use [StripeCustomerAdapter], which implements [CustomerAdapter] and manages retrieving
- * and updating a Stripe customer for you.
+ * instead use [CustomerAdaper.create], which manages retrieving and updating a Stripe customer for
+ * you.
  *
  * Implement this interface if you would prefer retrieving and updating your Stripe customer object
- * via your own backend instead of using [StripeCustomerAdapter].
+ * via your own backend instead of using the default implementation.
  *
  * Use [CustomerAdapter.create] to create an instance of a [CustomerAdapter].
  */
