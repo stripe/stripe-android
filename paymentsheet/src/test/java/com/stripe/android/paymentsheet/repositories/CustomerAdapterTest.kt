9a2b7936b533c2bd4d364121573e2501b09ff731
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-05-19 12:27:05 -0700

    Add caching to StripeCustomerAdapter (#6747)
    * Add caching to StripeCustomerAdapter

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/repositories/CustomerAdapterTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/repositories/CustomerAdapterTest.kt
@@ -5,6 +5,11 @@ import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.ExperimentalSavedPaymentMethodsApi
 import com.stripe.android.PaymentConfiguration
+import com.stripe.android.paymentsheet.FakePrefsRepository
+import com.stripe.android.paymentsheet.repositories.StripeCustomerAdapter.Companion.CACHED_CUSTOMER_MAX_AGE_MILLIS
+import com.stripe.android.utils.FakeCustomerRepository
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,6 +20,7 @@ import org.robolectric.RobolectricTestRunner
 class CustomerAdapterTest {
 
     private val context = ApplicationProvider.getApplicationContext<Context>()
+    private val customerRepository = FakeCustomerRepository()
 
     @Before
     fun setup() {
@@ -47,5 +53,86 @@ class CustomerAdapterTest {
         )
 
         assertThat(adapter).isNotNull()
+    }
+
+    @Test
+    fun `CustomerEphemeralKey is cached`() = runTest {
+        val adapter = createAdapter(
+            customerEphemeralKeyProvider = {
+                Result.success(
+                    CustomerEphemeralKey(
+                        customerId = testScheduler.currentTime.toString(),
+                        ephemeralKey = "ek_123"
+                    )
+                )
+            },
+            timeProvider = { testScheduler.currentTime }
+        )
+        assertThat(adapter.cachedCustomer).isNull()
+
+        var customer = adapter.getCustomer()
+        assertThat(customer.getOrNull()?.customerId).isEqualTo("0")
+        assertThat(adapter.cachedCustomer).isNotNull()
+
+        customer = adapter.getCustomer()
+        assertThat(customer.getOrNull()?.customerId).isEqualTo("0")
+    }
+
+    @Test
+    fun `CustomerEphemeralKey is expired and updated`() = runTest {
+        val adapter = createAdapter(
+            customerEphemeralKeyProvider = {
+                Result.success(
+                    CustomerEphemeralKey(
+                        customerId = testScheduler.currentTime.toString(),
+                        ephemeralKey = "ek_123"
+                    )
+                )
+            },
+            timeProvider = { testScheduler.currentTime }
+        )
+        assertThat(adapter.cachedCustomer).isNull()
+
+        var customer = adapter.getCustomer()
+        assertThat(customer.getOrNull()?.customerId).isEqualTo("0")
+        assertThat(adapter.cachedCustomer).isNotNull()
+
+        advanceTimeBy(CACHED_CUSTOMER_MAX_AGE_MILLIS)
+        customer = adapter.getCustomer()
+        assertThat(customer.getOrNull()?.customerId)
+            .isEqualTo("0")
+
+        advanceTimeBy(1)
+        customer = adapter.getCustomer()
+        assertThat(customer.getOrNull()?.customerId)
+            .isEqualTo("${CACHED_CUSTOMER_MAX_AGE_MILLIS + 1}")
+    }
+
+    private fun createAdapter(
+        customerEphemeralKeyProvider: CustomerEphemeralKeyProvider =
+            CustomerEphemeralKeyProvider {
+                Result.success(
+                    CustomerEphemeralKey(
+                        customerId = "cus_123",
+                        ephemeralKey = "ek_123"
+                    )
+                )
+            },
+        setupIntentClientSecretProvider: SetupIntentClientSecretProvider =
+            SetupIntentClientSecretProvider {
+                Result.success("seti_123")
+            },
+        canCreateSetupIntents: Boolean = true,
+        timeProvider: () -> Long = { 1L }
+    ): StripeCustomerAdapter {
+        return StripeCustomerAdapter(
+            context = context,
+            customerEphemeralKeyProvider = customerEphemeralKeyProvider,
+            setupIntentClientSecretProvider = setupIntentClientSecretProvider,
+            canCreateSetupIntents = canCreateSetupIntents,
+            timeProvider = timeProvider,
+            customerRepository = customerRepository,
+            prefsRepositoryFactory = { FakePrefsRepository() }
+        )
     }
 }
