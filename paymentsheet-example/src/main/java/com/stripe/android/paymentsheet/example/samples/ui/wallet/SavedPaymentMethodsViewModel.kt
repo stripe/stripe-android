ff292d3bb3c336782686d64c3eace2f85aa28f5d
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-05-19 13:28:52 -0700

    Wallet mode example SavedPaymentMethodsSheet instantiation (#6748)
    * Wallet mode example SavedPaymentMethodsSheet instantiation

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/samples/ui/wallet/SavedPaymentMethodsViewModel.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/samples/ui/wallet/SavedPaymentMethodsViewModel.kt
@@ -4,20 +4,22 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.core.requests.suspendable
-import com.github.kittinunf.result.Result
 import com.stripe.android.ExperimentalSavedPaymentMethodsApi
 import com.stripe.android.PaymentConfiguration
 import com.stripe.android.paymentsheet.example.samples.networking.ExampleSavedPaymentMethodRequest
 import com.stripe.android.paymentsheet.example.samples.networking.ExampleSavedPaymentMethodResponse
 import com.stripe.android.paymentsheet.example.samples.networking.awaitModel
+import com.stripe.android.paymentsheet.repositories.CustomerAdapter
 import com.stripe.android.paymentsheet.repositories.CustomerEphemeralKey
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
+import com.github.kittinunf.result.Result as FuelResult
 
 @OptIn(ExperimentalSavedPaymentMethodsApi::class)
 class SavedPaymentMethodsViewModel(
@@ -28,49 +30,73 @@ class SavedPaymentMethodsViewModel(
     )
     val state: StateFlow<SavedPaymentMethodsViewState> = _state
 
+    internal val customerAdapter: CustomerAdapter = CustomerAdapter.create(
+        context = getApplication(),
+        customerEphemeralKeyProvider = {
+            fetchCustomerEphemeralKey().fold(
+                success = {
+                    Result.success(
+                        CustomerEphemeralKey.create(
+                            customerId = it.customerId,
+                            ephemeralKey = it.customerEphemeralKeySecret
+                        )
+                    )
+                },
+                failure = {
+                    Result.failure(it.exception)
+                }
+            )
+        },
+        setupIntentClientSecretProvider = null,
+    )
+
     init {
-        initialize()
+        viewModelScope.launch {
+            initialize()
+        }
     }
 
-    private fun initialize() {
-        viewModelScope.launch {
-            val request = ExampleSavedPaymentMethodRequest(
-                customerType = "returning"
-            )
-            val requestBody = Json.encodeToString(
-                ExampleSavedPaymentMethodRequest.serializer(),
-                request
-            )
-            val apiResult = Fuel
-                .post("$backendUrl/customer_ephemeral_key")
-                .jsonBody(requestBody)
-                .suspendable()
-                .awaitModel(ExampleSavedPaymentMethodResponse.serializer())
-
-            when (apiResult) {
-                is Result.Success -> {
-                    PaymentConfiguration.init(
-                        context = getApplication(),
-                        publishableKey = apiResult.value.publishableKey,
+    private suspend fun initialize() {
+        when (val result = fetchCustomerEphemeralKey()) {
+            is FuelResult.Success -> {
+                PaymentConfiguration.init(
+                    context = getApplication(),
+                    publishableKey = result.value.publishableKey,
+                )
+                _state.update {
+                    SavedPaymentMethodsViewState.Data(
+                        customerEphemeralKey = CustomerEphemeralKey.create(
+                            customerId = result.value.customerId,
+                            ephemeralKey = result.value.customerEphemeralKeySecret,
+                        )
                     )
-                    _state.update {
-                        SavedPaymentMethodsViewState.Data(
-                            customerEphemeralKey = CustomerEphemeralKey.create(
-                                customerId = apiResult.value.customerId,
-                                ephemeralKey = apiResult.value.customerEphemeralKeySecret,
-                            )
-                        )
-                    }
                 }
-                is Result.Failure -> {
-                    _state.update {
-                        SavedPaymentMethodsViewState.FailedToLoad(
-                            message = apiResult.error.message.toString()
-                        )
-                    }
+            }
+            is FuelResult.Failure -> {
+                _state.update {
+                    SavedPaymentMethodsViewState.FailedToLoad(
+                        message = result.error.message.toString()
+                    )
                 }
             }
         }
+    }
+
+    private suspend fun fetchCustomerEphemeralKey():
+        FuelResult<ExampleSavedPaymentMethodResponse, FuelError> {
+        val request = ExampleSavedPaymentMethodRequest(
+            customerType = "returning"
+        )
+        val requestBody = Json.encodeToString(
+            ExampleSavedPaymentMethodRequest.serializer(),
+            request
+        )
+
+        return Fuel
+            .post("$backendUrl/customer_ephemeral_key")
+            .jsonBody(requestBody)
+            .suspendable()
+            .awaitModel(ExampleSavedPaymentMethodResponse.serializer())
     }
 
     private companion object {
