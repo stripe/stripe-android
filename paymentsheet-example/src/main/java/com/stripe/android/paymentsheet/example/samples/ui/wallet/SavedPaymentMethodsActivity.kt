339286d4c4b0dd7c531b91e1b2c51aa8b690bf67
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-05-19 09:22:34 -0700

    Add UI and fetch customer ephemeral keys

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/samples/ui/wallet/SavedPaymentMethodsActivity.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/samples/ui/wallet/SavedPaymentMethodsActivity.kt
@@ -1,19 +1,92 @@
 package com.stripe.android.paymentsheet.example.samples.ui.wallet
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.stripe.android.ExperimentalSavedPaymentMethodsApi
 import com.stripe.android.paymentsheet.example.samples.ui.shared.PaymentSheetExampleTheme
 
+@OptIn(ExperimentalSavedPaymentMethodsApi::class)
 internal class SavedPaymentMethodsActivity : AppCompatActivity() {
+    private val viewModel by viewModels<SavedPaymentMethodsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             PaymentSheetExampleTheme {
-                Text("Hello world!")
+                val uiState by viewModel.state.collectAsState()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = "Payment Methods",
+                        fontSize = 18.sp
+                    )
+                    if (uiState.isProcessing) {
+                        LinearProgressIndicator(
+                            Modifier
+                                .fillMaxWidth()
+                                .height(4.dp)
+                        )
+                    } else {
+                        uiState.customerEphemeralKey?.let {
+                            PaymentDefaults()
+                        }
+                    }
+                }
             }
+        }
+    }
+}
+
+@Composable
+private fun PaymentDefaults() {
+    val context = LocalContext.current
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            "Payment default",
+            fontWeight = FontWeight.Bold,
+        )
+        TextButton(
+            onClick = {
+                Toast.makeText(
+                    context,
+                    "Wallet mode under construction",
+                    Toast.LENGTH_LONG,
+                ).show()
+            }
+        ) {
+            Text(
+                text = "Visa 4242",
+            )
         }
     }
 }
--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/samples/ui/wallet/SavedPaymentMethodsViewState.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/samples/ui/wallet/SavedPaymentMethodsViewState.kt
@@ -1,5 +1,10 @@
 package com.stripe.android.paymentsheet.example.samples.ui.wallet
 
+import com.stripe.android.ExperimentalSavedPaymentMethodsApi
+import com.stripe.android.paymentsheet.repositories.CustomerEphemeralKey
+
+@OptIn(ExperimentalSavedPaymentMethodsApi::class)
 data class SavedPaymentMethodsViewState(
     val isProcessing: Boolean = false,
+    val customerEphemeralKey: CustomerEphemeralKey? = null
 )
