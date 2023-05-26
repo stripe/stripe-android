de41b560edeb2c1c431bbc3ba7a6d8fb310ee9d5
Jay Newstrom <jaynewstrom@stripe.com>
2023-05-26 12:59:09 -0500

    Start removing Link UI. (#6790)

--- link/src/main/java/com/stripe/android/link/injection/LinkComponent.kt
+++ link/src/main/java/com/stripe/android/link/injection/LinkComponent.kt
@@ -1,12 +1,8 @@
 package com.stripe.android.link.injection
 
 import com.stripe.android.link.LinkActivityContract
-import com.stripe.android.link.LinkActivityViewModel
-import com.stripe.android.link.ui.cardedit.CardEditViewModel
-import com.stripe.android.link.ui.paymentmethod.PaymentMethodViewModel
 import com.stripe.android.link.ui.signup.SignUpViewModel
 import com.stripe.android.link.ui.verification.VerificationViewModel
-import com.stripe.android.link.ui.wallet.WalletViewModel
 import dagger.BindsInstance
 import dagger.Subcomponent
 
@@ -19,12 +15,8 @@ import dagger.Subcomponent
     ]
 )
 internal abstract class LinkComponent {
-    abstract fun inject(factory: LinkActivityViewModel.Factory)
     abstract fun inject(factory: SignUpViewModel.Factory)
     abstract fun inject(factory: VerificationViewModel.Factory)
-    abstract fun inject(factory: WalletViewModel.Factory)
-    abstract fun inject(factory: PaymentMethodViewModel.Factory)
-    abstract fun inject(factory: CardEditViewModel.Factory)
 
     @Subcomponent.Builder
     interface Builder {
