de40ef4c43de0eeb42ba2e68ff15e037cb43d40a
Jay Newstrom <jaynewstrom@stripe.com>
2023-06-15 09:40:29 -0500

    Fix serialization of defaults in Link PopupPayload. (#6880)

--- link/src/test/java/com/stripe/android/link/serialization/PopupPayloadTest.kt
+++ link/src/test/java/com/stripe/android/link/serialization/PopupPayloadTest.kt
@@ -1,7 +1,6 @@
 package com.stripe.android.link.serialization
 
 import com.google.common.truth.Truth.assertThat
-import kotlinx.serialization.json.Json
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -11,13 +10,16 @@ import org.robolectric.RobolectricTestRunner
 internal class PopupPayloadTest {
     @Test
     fun testJsonSerialization() {
-        val json = Json.encodeToString(PopupPayload.serializer(), createPayload())
-        assertThat(json).isEqualTo("""{"publishableKey":"pk_test_abc","stripeAccount":"123","merchantInfo":{"businessName":"Jay's Taco Stand","country":"US"},"customerInfo":{"email":"jaystacostandfake@gmail.com","country":"US"},"paymentInfo":{"currency":"USD","amount":5555},"returnUrl":"stripesdk://link_return_url","locale":"US"}""")
+        val json = PopupPayload.PopupPayloadJson.encodeToString(
+            serializer = PopupPayload.serializer(),
+            value = createPayload(),
+        )
+        assertThat(json).isEqualTo("""{"publishableKey":"pk_test_abc","stripeAccount":"123","merchantInfo":{"businessName":"Jay's Taco Stand","country":"US"},"customerInfo":{"email":"jaystacostandfake@gmail.com","country":"US"},"paymentInfo":{"currency":"USD","amount":5555},"returnUrl":"stripesdk://link_return_url","locale":"US","path":"mobile_pay","integrationType":"mobile"}""")
     }
 
     @Test
     fun testToUrl() {
-        assertThat(createPayload().toUrl()).isEqualTo("https://checkout.link.com/link-popup.html#eyJwdWJsaXNoYWJsZUtleSI6InBrX3Rlc3RfYWJjIiwic3RyaXBlQWNjb3VudCI6IjEyMyIsIm1lcmNoYW50SW5mbyI6eyJidXNpbmVzc05hbWUiOiJKYXkncyBUYWNvIFN0YW5kIiwiY291bnRyeSI6IlVTIn0sImN1c3RvbWVySW5mbyI6eyJlbWFpbCI6ImpheXN0YWNvc3RhbmRmYWtlQGdtYWlsLmNvbSIsImNvdW50cnkiOiJVUyJ9LCJwYXltZW50SW5mbyI6eyJjdXJyZW5jeSI6IlVTRCIsImFtb3VudCI6NTU1NX0sInJldHVyblVybCI6InN0cmlwZXNkazovL2xpbmtfcmV0dXJuX3VybCIsImxvY2FsZSI6IlVTIn0=")
+        assertThat(createPayload().toUrl()).isEqualTo("https://checkout.link.com/link-popup.html#eyJwdWJsaXNoYWJsZUtleSI6InBrX3Rlc3RfYWJjIiwic3RyaXBlQWNjb3VudCI6IjEyMyIsIm1lcmNoYW50SW5mbyI6eyJidXNpbmVzc05hbWUiOiJKYXkncyBUYWNvIFN0YW5kIiwiY291bnRyeSI6IlVTIn0sImN1c3RvbWVySW5mbyI6eyJlbWFpbCI6ImpheXN0YWNvc3RhbmRmYWtlQGdtYWlsLmNvbSIsImNvdW50cnkiOiJVUyJ9LCJwYXltZW50SW5mbyI6eyJjdXJyZW5jeSI6IlVTRCIsImFtb3VudCI6NTU1NX0sInJldHVyblVybCI6InN0cmlwZXNkazovL2xpbmtfcmV0dXJuX3VybCIsImxvY2FsZSI6IlVTIiwicGF0aCI6Im1vYmlsZV9wYXkiLCJpbnRlZ3JhdGlvblR5cGUiOiJtb2JpbGUifQ==")
     }
 
     private fun createPayload(): PopupPayload = PopupPayload(
