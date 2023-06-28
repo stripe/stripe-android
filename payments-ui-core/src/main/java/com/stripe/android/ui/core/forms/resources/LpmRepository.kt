f62bde9837dbe7b3122a4225e61e41760125db4f
Till Hellmund <tillh@stripe.com>
2023-06-28 16:47:59 -0400

    Enable ACH v2 in decoupled flow (#6889)

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/forms/resources/LpmRepository.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/forms/resources/LpmRepository.kt
@@ -446,11 +446,8 @@ class LpmRepository constructor(
         PaymentMethod.Type.USBankAccount.code -> {
             val pmo = stripeIntent.getPaymentMethodOptions()[PaymentMethod.Type.USBankAccount.code]
             val verificationMethod = (pmo as? Map<*, *>)?.get("verification_method") as? String
-            val supportsVerificationMethod = verificationMethod == "instant" ||
-                verificationMethod == "automatic"
-            val supported =
-                (stripeIntent.clientSecret != null || arguments.enableACHV2InDeferredFlow) &&
-                    supportsVerificationMethod
+            val supportsVerificationMethod = verificationMethod in setOf("instant", "automatic")
+
             /**
              * US Bank Account requires the payment method option to have either automatic or
              * instant verification method in order to be a supported payment method. For example,
@@ -463,7 +460,7 @@ class LpmRepository constructor(
              *     }
              * }
              */
-            if (supported) {
+            if (supportsVerificationMethod) {
                 SupportedPaymentMethod(
                     code = "us_bank_account",
                     requiresMandate = true,
@@ -646,9 +643,6 @@ class LpmRepository constructor(
         val resources: Resources?,
         val isFinancialConnectionsAvailable: IsFinancialConnectionsAvailable =
             DefaultIsFinancialConnectionsAvailable(),
-        // Whether to enable ACHv2 in the deferred flow.
-        // To be deleted when https://jira.corp.stripe.com/browse/BANKCON-6731 is completed.
-        val enableACHV2InDeferredFlow: Boolean = false,
     )
 }
 
