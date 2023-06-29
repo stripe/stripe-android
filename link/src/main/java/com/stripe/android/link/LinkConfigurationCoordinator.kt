1b6e895a1c4105c44cfceea4e15e3bb4e53a4412
Jay Newstrom <jaynewstrom@stripe.com>
2023-06-29 09:01:46 -0600

    Reorganize link components.

--- link/src/main/java/com/stripe/android/link/LinkConfigurationCoordinator.kt
+++ link/src/main/java/com/stripe/android/link/LinkConfigurationCoordinator.kt
@@ -1,75 +1,32 @@
 package com.stripe.android.link
 
-import android.content.Context
 import androidx.annotation.RestrictTo
-import com.stripe.android.core.injection.ENABLE_LOGGING
-import com.stripe.android.core.injection.IOContext
-import com.stripe.android.core.injection.NonFallbackInjectable
-import com.stripe.android.core.injection.PUBLISHABLE_KEY
-import com.stripe.android.core.injection.STRIPE_ACCOUNT_ID
-import com.stripe.android.core.injection.UIContext
-import com.stripe.android.core.networking.AnalyticsRequestExecutor
-import com.stripe.android.link.injection.DaggerLinkPaymentLauncherComponent
-import com.stripe.android.link.injection.LinkPaymentLauncherComponent
+import com.stripe.android.link.injection.LinkComponent
 import com.stripe.android.link.ui.inline.UserInput
 import com.stripe.android.model.PaymentMethodCreateParams
-import com.stripe.android.networking.PaymentAnalyticsRequestFactory
-import com.stripe.android.networking.StripeRepository
-import com.stripe.android.payments.core.injection.PRODUCT_USAGE
-import com.stripe.android.uicore.address.AddressRepository
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import javax.inject.Named
 import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 @Singleton
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 class LinkConfigurationCoordinator @Inject internal constructor(
-    context: Context,
-    @Named(PRODUCT_USAGE) private val productUsage: Set<String>,
-    @Named(PUBLISHABLE_KEY) private val publishableKeyProvider: () -> String,
-    @Named(STRIPE_ACCOUNT_ID) private val stripeAccountIdProvider: () -> String?,
-    @Named(ENABLE_LOGGING) private val enableLogging: Boolean,
-    @IOContext ioContext: CoroutineContext,
-    @UIContext uiContext: CoroutineContext,
-    paymentAnalyticsRequestFactory: PaymentAnalyticsRequestFactory,
-    analyticsRequestExecutor: AnalyticsRequestExecutor,
-    stripeRepository: StripeRepository,
-    addressRepository: AddressRepository,
-) : NonFallbackInjectable {
-    private val launcherComponentBuilder = DaggerLinkPaymentLauncherComponent.builder()
-        .context(context)
-        .ioContext(ioContext)
-        .uiContext(uiContext)
-        .analyticsRequestFactory(paymentAnalyticsRequestFactory)
-        .analyticsRequestExecutor(analyticsRequestExecutor)
-        .stripeRepository(stripeRepository)
-        .addressRepository(addressRepository)
-        .enableLogging(enableLogging)
-        .publishableKeyProvider(publishableKeyProvider)
-        .stripeAccountIdProvider(stripeAccountIdProvider)
-        .productUsage(productUsage)
+    private val linkComponentBuilder: LinkComponent.Builder,
+) {
+    private val componentFlow = MutableStateFlow<LinkComponent?>(null)
 
-    private val componentFlow = MutableStateFlow<LinkPaymentLauncherComponent?>(null)
-
-    @OptIn(FlowPreview::class)
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     val emailFlow: Flow<String?> = componentFlow
         .filterNotNull()
-        .flatMapMerge { it.linkAccountManager.linkAccount }
-        .map { it?.email }
+        .map { it.configuration.customerEmail }
 
     /**
      * The dependency injector Component for all injectable classes in Link while in an embedded
      * environment.
      */
-    internal val component: LinkPaymentLauncherComponent?
+    internal val component: LinkComponent?
         get() = componentFlow.value
 
     /**
@@ -106,12 +63,12 @@ class LinkConfigurationCoordinator @Inject internal constructor(
             .createCardPaymentDetails(paymentMethodCreateParams)
 
     /**
-     * Create or get the existing [LinkPaymentLauncherComponent], responsible for injecting all
+     * Create or get the existing [LinkComponent], responsible for injecting all
      * injectable classes in Link while in an embedded environment.
      */
     private fun getLinkPaymentLauncherComponent(configuration: LinkConfiguration) =
         component?.takeIf { it.configuration == configuration }
-            ?: launcherComponentBuilder
+            ?: linkComponentBuilder
                 .configuration(configuration)
                 .build()
                 .also {
--- link/src/main/java/com/stripe/android/link/LinkPaymentLauncher.kt
+++ link/src/main/java/com/stripe/android/link/LinkPaymentLauncher.kt
@@ -4,7 +4,7 @@ import androidx.activity.result.ActivityResultCaller
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.ActivityResultRegistry
 import androidx.annotation.RestrictTo
-import com.stripe.android.link.injection.LinkLauncherSubcomponent
+import com.stripe.android.link.injection.LinkAnalyticsComponent
 import com.stripe.android.link.ui.paymentmethod.SupportedPaymentMethod
 import com.stripe.android.model.PaymentMethodCreateParams
 import javax.inject.Inject
@@ -16,10 +16,10 @@ import javax.inject.Singleton
 @Singleton
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 class LinkPaymentLauncher @Inject internal constructor(
-    linkLauncherSubcomponentBuilder: LinkLauncherSubcomponent.Builder,
+    linkAnalyticsComponentBuilder: LinkAnalyticsComponent.Builder,
     private val linkActivityContract: LinkActivityContract,
 ) {
-    private val analyticsHelper = linkLauncherSubcomponentBuilder.build().linkAnalyticsHelper
+    private val analyticsHelper = linkAnalyticsComponentBuilder.build().linkAnalyticsHelper
 
     private var linkActivityResultLauncher:
         ActivityResultLauncher<LinkActivityContract.Args>? = null
