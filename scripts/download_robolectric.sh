de16fe2e307065876f0dd291d287f7409f967978
Jay Newstrom <jaynewstrom@stripe.com>
2023-07-24 08:22:12 -0600

    Attempt to fix robolectric.

---
+++ scripts/download_robolectric.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e # Fail on error.
+
+declare -a robolectric_versions=("10-robolectric-5803371-i4" "11-robolectric-6757853-i4" "13-robolectric-9030017-i4" "4.1.2_r1-robolectric-r1-i4" "9-robolectric-4913185-2-i4")
+
+for i in ${robolectric_versions[@]}; do
+  eval "mvn dependency:get -Dartifact=org.robolectric:android-all-instrumented:${i}"
+done
