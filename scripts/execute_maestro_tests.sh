3429543859855232a4da3d101fb880cbad7c4bf2
Carlos M <99293320+carlosmuvi-stripe@users.noreply.github.com>
2023-07-24 01:39:58 -0600

    [FC] Update maestro to 1.30.4 and use Maestro record (#6937)
    * Adds screen record.
    
    * Test.
    
    * Breaks tests.
    
    * Update to .30.
    
    * Revert tests.
    
    * Update to 1.30.4

--- scripts/execute_maestro_tests.sh
+++ scripts/execute_maestro_tests.sh
@@ -18,33 +18,15 @@ now=$(date +%F_%H-%M-%S)
 echo $now
 
 # Install Maestro
-export MAESTRO_VERSION=1.21.3; curl -Ls "https://get.maestro.mobile.dev" | bash
+export MAESTRO_VERSION=1.30.4; curl -Ls "https://get.maestro.mobile.dev" | bash
 export PATH="$PATH":"$HOME/.maestro/bin"
 maestro -v
+
+# Create test results folder.
+mkdir -p /tmp/test_results
 
 # Compile and install APK.
 ./gradlew -PSTRIPE_FINANCIAL_CONNECTIONS_EXAMPLE_BACKEND_URL=$STRIPE_FINANCIAL_CONNECTIONS_EXAMPLE_BACKEND_URL :financial-connections-example:$task
 
-# Start screen record (adb screenrecord has a 3 min limit).
-adb shell "screenrecord /sdcard/$now-1.mp4" &
-
-# Store the process ID
-childpid=$!
-
 # Clear and start collecting logs
 maestro test -e APP_ID=com.stripe.android.financialconnections.example --format junit --output maestroReport.xml maestro/financial-connections/
-result=$?
-
-# Wait for the recording process to finish
-kill -2 "$childpid"
-wait "$childpid"
-
-# Sleep for a short duration to allow the process to finalize the video file
-sleep 3
-
-# Pull the video file from the device
-mkdir -p /tmp/test_results
-cd /tmp/test_results
-adb pull "/sdcard/$now-1.mp4" || true
-
-exit "$result"
