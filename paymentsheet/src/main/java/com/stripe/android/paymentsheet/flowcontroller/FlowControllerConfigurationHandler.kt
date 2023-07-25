bc45fc4b3a3935981945b8cfab1ceef8b50b755c
Till Hellmund <tillh@stripe.com>
2023-07-25 15:51:05 -0400

    Send init event before configuration happens in FC

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/flowcontroller/FlowControllerConfigurationHandler.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/flowcontroller/FlowControllerConfigurationHandler.kt
@@ -3,7 +3,6 @@ package com.stripe.android.paymentsheet.flowcontroller
 import com.stripe.android.core.injection.UIContext
 import com.stripe.android.core.networking.AnalyticsRequestFactory
 import com.stripe.android.paymentsheet.PaymentSheet
-import com.stripe.android.paymentsheet.PaymentSheet.InitializationMode.DeferredIntent
 import com.stripe.android.paymentsheet.analytics.EventReporter
 import com.stripe.android.paymentsheet.state.PaymentSheetLoader
 import com.stripe.android.paymentsheet.state.PaymentSheetState
@@ -85,10 +84,17 @@ internal class FlowControllerConfigurationHandler @Inject constructor(
         }
 
         AnalyticsRequestFactory.regenerateSessionId()
+
+        val isDecoupling = initializationMode is PaymentSheet.InitializationMode.DeferredIntent
+        eventReporter.onInit(
+            configuration = configuration,
+            isDecoupling = isDecoupling,
+        )
+
         when (val result = paymentSheetLoader.load(initializationMode, configuration)) {
             is PaymentSheetLoader.Result.Success -> {
                 viewModel.previousConfigureRequest = configureRequest
-                onInitSuccess(result.state, configureRequest)
+                onInitSuccess(result.state)
                 onConfigured()
             }
             is PaymentSheetLoader.Result.Failure -> {
@@ -99,15 +105,7 @@ internal class FlowControllerConfigurationHandler @Inject constructor(
 
     private fun onInitSuccess(
         state: PaymentSheetState.Full,
-        configureRequest: ConfigureRequest,
     ) {
-        val isDecoupling = configureRequest.initializationMode is DeferredIntent
-
-        eventReporter.onInit(
-            configuration = state.config,
-            isDecoupling = isDecoupling,
-        )
-
         viewModel.paymentSelection = paymentSelectionUpdater(
             currentSelection = viewModel.paymentSelection,
             previousConfig = viewModel.state?.config,
