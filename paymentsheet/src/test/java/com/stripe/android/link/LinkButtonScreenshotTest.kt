a3cbf6532c272567842488cab99cd110e669bc3f
Samer Alabi <samer@stripe.com>
2023-08-11 23:57:00 -0400

    Update `LinkButton` to properly scale for device display & font changes.

--- paymentsheet/src/test/java/com/stripe/android/link/LinkButtonScreenshotTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/link/LinkButtonScreenshotTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.stripe.android.link.ui.LinkButton
+import com.stripe.android.utils.screenshots.FontSize
 import com.stripe.android.utils.screenshots.PaparazziRule
 import com.stripe.android.utils.screenshots.SystemAppearance
 import org.junit.Rule
@@ -14,6 +15,7 @@ internal class LinkButtonScreenshotTest {
     @get:Rule
     val paparazziRule = PaparazziRule(
         arrayOf(SystemAppearance.LightTheme),
+        FontSize.values(),
         boxModifier = Modifier
             .padding(0.dp)
             .fillMaxWidth(),
