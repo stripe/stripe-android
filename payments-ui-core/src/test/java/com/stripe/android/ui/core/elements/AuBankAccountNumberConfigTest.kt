606b1d0a9abb1133862b7981523ac2eab0a2a961
Till Hellmund <tillh@stripe.com>
2023-08-17 10:52:26 -0400

    Remove `totranslate.xml` files (#7166)
    * Remove `totranslate.xml` files
    
    * Update tests

--- payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/AuBankAccountNumberConfigTest.kt
+++ payments-ui-core/src/test/java/com/stripe/android/ui/core/elements/AuBankAccountNumberConfigTest.kt
@@ -1,6 +1,6 @@
 package com.stripe.android.ui.core.elements
 
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import com.stripe.android.uicore.elements.TextFieldStateConstants
 import org.junit.Test
 
@@ -9,34 +9,33 @@ class AuBankAccountNumberConfigTest {
 
     @Test
     fun `verify only numbers are allowed in the field`() {
-        Truth.assertThat(auBankAccountNumberConfig.filter("12345hell6789"))
+        assertThat(auBankAccountNumberConfig.filter("12345hell6789"))
             .isEqualTo("123456789")
     }
 
     @Test
+    fun `verify limits input to accepted length`() {
+        assertThat(auBankAccountNumberConfig.filter("1234567899999")).isEqualTo("123456789")
+    }
+
+    @Test
     fun `verify blank bank account number returns blank state`() {
-        Truth.assertThat(auBankAccountNumberConfig.determineState(""))
+        assertThat(auBankAccountNumberConfig.determineState(""))
             .isEqualTo(TextFieldStateConstants.Error.Blank)
     }
 
     @Test
     fun `verify incomplete bank account number is in incomplete state`() {
-        Truth.assertThat(auBankAccountNumberConfig.determineState("123"))
+        assertThat(auBankAccountNumberConfig.determineState("123"))
             .isInstanceOf(TextFieldStateConstants.Error.Incomplete::class.java)
 
-        Truth.assertThat(auBankAccountNumberConfig.determineState("123456"))
+        assertThat(auBankAccountNumberConfig.determineState("123456"))
             .isInstanceOf(TextFieldStateConstants.Error.Incomplete::class.java)
-    }
-
-    @Test
-    fun `verify invalid bank account number is in invalid state`() {
-        Truth.assertThat(auBankAccountNumberConfig.determineState("1234567890"))
-            .isInstanceOf(TextFieldStateConstants.Error.Invalid::class.java)
     }
 
     @Test
     fun `verify valid bank account number is in valid state`() {
-        Truth.assertThat(auBankAccountNumberConfig.determineState("653987643"))
+        assertThat(auBankAccountNumberConfig.determineState("653987643"))
             .isInstanceOf(TextFieldStateConstants.Valid.Full::class.java)
     }
 }
