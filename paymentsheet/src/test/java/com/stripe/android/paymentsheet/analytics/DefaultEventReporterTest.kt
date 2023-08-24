ca31bf0d1a7aaf976c04a15f4b1159b17b804fe4
Till Hellmund <tillh@stripe.com>
2023-08-23 22:52:50 -0400

    Expand `error_message` to include network and server errors (#7187)
    * Expand `error_message` to include network and server errors
    
    * Update values to match iOS

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/analytics/DefaultEventReporterTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/analytics/DefaultEventReporterTest.kt
@@ -1,8 +1,10 @@
 package com.stripe.android.paymentsheet.analytics
 
 import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
 import com.stripe.android.ApiKeyFixtures
 import com.stripe.android.PaymentConfiguration
+import com.stripe.android.core.networking.AnalyticsRequest
 import com.stripe.android.core.networking.AnalyticsRequestExecutor
 import com.stripe.android.model.PaymentMethodFixtures
 import com.stripe.android.networking.PaymentAnalyticsRequestFactory
@@ -10,12 +12,15 @@ import com.stripe.android.paymentsheet.PaymentSheetFixtures
 import com.stripe.android.paymentsheet.model.PaymentSelection
 import com.stripe.android.utils.FakeDurationProvider
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import org.json.JSONException
 import org.junit.runner.RunWith
 import org.mockito.kotlin.argWhere
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.reset
 import org.mockito.kotlin.verify
 import org.robolectric.RobolectricTestRunner
+import java.io.IOException
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
@@ -237,5 +242,47 @@ class DefaultEventReporterTest {
             durationProvider,
             testDispatcher
         )
+    }
+
+    @Test
+    fun `Send correct error_message for server errors`() {
+        completeEventReporter.onLoadFailed(
+            isDecoupling = false,
+            error = JSONException("Server did something bad"),
+        )
+
+        val argumentCaptor = argumentCaptor<AnalyticsRequest>()
+        verify(analyticsRequestExecutor).executeAsync(argumentCaptor.capture())
+
+        val errorType = argumentCaptor.firstValue.params["error_message"] as String
+        assertThat(errorType).isEqualTo("apiError")
+    }
+
+    @Test
+    fun `Send correct error_message for network errors`() {
+        completeEventReporter.onLoadFailed(
+            isDecoupling = false,
+            error = IOException("Internet no good"),
+        )
+
+        val argumentCaptor = argumentCaptor<AnalyticsRequest>()
+        verify(analyticsRequestExecutor).executeAsync(argumentCaptor.capture())
+
+        val errorType = argumentCaptor.firstValue.params["error_message"] as String
+        assertThat(errorType).isEqualTo("connectionError")
+    }
+
+    @Test
+    fun `Send correct error_message for invalid requests`() {
+        completeEventReporter.onLoadFailed(
+            isDecoupling = false,
+            error = IllegalArgumentException("This ain't valid"),
+        )
+
+        val argumentCaptor = argumentCaptor<AnalyticsRequest>()
+        verify(analyticsRequestExecutor).executeAsync(argumentCaptor.capture())
+
+        val errorType = argumentCaptor.firstValue.params["error_message"] as String
+        assertThat(errorType).isEqualTo("invalidRequestError")
     }
 }
