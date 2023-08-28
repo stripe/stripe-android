5e548b3d65d1ea77270b040f69516235e6342b4c
Till Hellmund <tillh@stripe.com>
2023-08-28 12:42:05 -0400

    Fix FPX test

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestFpx.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestFpx.kt
@@ -2,6 +2,8 @@ package com.stripe.android.lpm
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BaseLpmTest
+import com.stripe.android.test.core.Automatic
+import com.stripe.android.test.core.Currency
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -9,6 +11,9 @@ import org.junit.runner.RunWith
 internal class TestFpx : BaseLpmTest() {
     private val fpx = newUser.copy(
         paymentMethod = lpmRepository.fromCode("fpx")!!,
+        currency = Currency.MYR,
+        merchantCountryCode = "MY",
+        automatic = Automatic.On,
     )
 
     @Test
