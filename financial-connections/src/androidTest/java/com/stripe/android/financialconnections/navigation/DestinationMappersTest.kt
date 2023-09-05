045118b561f2b9cd5d6587b51a7b2cf89590686b
Carlos M <carlosmuvi@stripe.com>
2023-09-05 14:27:58 -0700

    Adds test.

---
+++ financial-connections/src/androidTest/java/com/stripe/android/financialconnections/navigation/DestinationMappersTest.kt
@@ -0,0 +1,28 @@
+package com.stripe.android.financialconnections.navigation
+
+import com.stripe.android.financialconnections.model.FinancialConnectionsSessionManifest.Pane
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class DestinationMappersTest {
+
+    private val nonImplementedPanes = listOf(
+        Pane.UNEXPECTED_ERROR,
+        Pane.BANK_AUTH_REPAIR,
+        Pane.AUTH_OPTIONS,
+        Pane.LINK_CONSENT,
+        Pane.LINK_LOGIN,
+    )
+
+    @Test
+    fun testPaneToDestination() {
+        for (pane in Pane.values()) {
+            if (!nonImplementedPanes.contains(pane)) {
+                assertNotNull("No matching destination for pane: $pane", pane.destination)
+            } else {
+                assertThrows(IllegalArgumentException::class.java) { pane.destination }
+            }
+        }
+    }
+}
