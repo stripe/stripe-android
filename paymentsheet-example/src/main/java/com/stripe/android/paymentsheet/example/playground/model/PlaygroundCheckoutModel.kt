5786a4010995a8117e2abae77ac7cb81354bb09d
Jay Newstrom <jaynewstrom@stripe.com>
2023-09-27 15:25:06 -0600

    Use a builder for checkout request. (#7357)

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/model/PlaygroundCheckoutModel.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/model/PlaygroundCheckoutModel.kt
@@ -103,26 +103,88 @@ sealed class CheckoutCustomer(val value: String) {
 }
 
 @Serializable
-data class CheckoutRequest(
+class CheckoutRequest private constructor(
     @SerialName("initialization")
-    val initialization: String,
+    val initialization: String?,
     @SerialName("customer")
-    val customer: String,
+    val customer: String?,
     @SerialName("currency")
-    val currency: String,
+    val currency: String?,
     @SerialName("mode")
-    val mode: String,
+    val mode: String?,
     @SerialName("set_shipping_address")
-    val set_shipping_address: Boolean,
+    val setShippingAddress: Boolean?,
     @SerialName("automatic_payment_methods")
-    val automatic_payment_methods: Boolean,
+    val automaticPaymentMethods: Boolean?,
     @SerialName("use_link")
-    val use_link: Boolean,
+    val useLink: Boolean?,
     @SerialName("merchant_country_code")
-    val merchant_country_code: String,
+    val merchantCountryCode: String?,
     @SerialName("supported_payment_methods")
-    val supported_payment_methods: List<String>? = null,
-)
+    val supportedPaymentMethods: List<String>?,
+) {
+    class Builder {
+        private var initialization: String? = null
+        private var customer: String? = null
+        private var currency: String? = null
+        private var mode: String? = null
+        private var setShippingAddress: Boolean? = null
+        private var automaticPaymentMethods: Boolean? = null
+        private var useLink: Boolean? = null
+        private var merchantCountryCode: String? = null
+        private var supportedPaymentMethods: List<String>? = null
+
+        fun initialization(initialization: String?) = apply {
+            this.initialization = initialization
+        }
+
+        fun customer(customer: String?) = apply {
+            this.customer = customer
+        }
+
+        fun currency(currency: String?) = apply {
+            this.currency = currency
+        }
+
+        fun mode(mode: String?) = apply {
+            this.mode = mode
+        }
+
+        fun setShippingAddress(setShippingAddress: Boolean?) = apply {
+            this.setShippingAddress = setShippingAddress
+        }
+
+        fun automaticPaymentMethods(automaticPaymentMethods: Boolean?) = apply {
+            this.automaticPaymentMethods = automaticPaymentMethods
+        }
+
+        fun useLink(useLink: Boolean?) = apply {
+            this.useLink = useLink
+        }
+
+        fun merchantCountryCode(merchantCountryCode: String?) = apply {
+            this.merchantCountryCode = merchantCountryCode
+        }
+
+        fun supportedPaymentMethods(supportedPaymentMethods: List<String>?) = apply {
+            this.supportedPaymentMethods = supportedPaymentMethods
+        }
+
+        fun build(): CheckoutRequest {
+            return CheckoutRequest(
+                initialization = initialization,
+                customer = customer,
+                currency = currency,
+                mode = mode,
+                setShippingAddress = setShippingAddress,
+                automaticPaymentMethods = automaticPaymentMethods,
+                useLink = useLink,
+                merchantCountryCode = merchantCountryCode,
+                supportedPaymentMethods = supportedPaymentMethods,
+            )
+        }
+    }
+}
 
 @Serializable
 data class CheckoutResponse(
