7af912976d5a785297a02ae7d61dab600bc5a105
Carlos M <carlosmuvi@stripe.com>
2023-09-28 15:25:56 -0700

    ktlint fixes.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/bankauthrepair/BankAuthRepairScreen.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/bankauthrepair/BankAuthRepairScreen.kt
@@ -6,11 +6,16 @@ package com.stripe.android.financialconnections.features.bankauthrepair
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksViewModel
+import com.stripe.android.financialconnections.features.partnerauth.PartnerAuthState
 
 @Composable
 internal fun BankAuthRepairScreen() {
     // step view model
     val viewModel: BankAuthRepairViewModel = mavericksViewModel()
-    Text(text = "Bank Auth Repair")
+    val state: State<PartnerAuthState> = viewModel.collectAsState()
+
+    Text(text = "Bank Auth Repair ${state.value.payload}")
 }
