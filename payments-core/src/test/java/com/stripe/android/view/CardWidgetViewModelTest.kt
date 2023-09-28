a4a48e471102ff2f899892faf38be4467b950e5e
Till Hellmund <tillh@stripe.com>
2023-09-28 11:37:35 -0400

    Add `FeatureFlags` and use for CBC

--- payments-core/src/test/java/com/stripe/android/view/CardWidgetViewModelTest.kt
+++ payments-core/src/test/java/com/stripe/android/view/CardWidgetViewModelTest.kt
@@ -2,18 +2,29 @@ package com.stripe.android.view
 
 import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
+import com.stripe.android.testing.FeatureFlagTestRule
+import com.stripe.android.utils.FeatureFlags
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 
 class CardWidgetViewModelTest {
+
+    @get:Rule
+    val featureFlagTestRule = FeatureFlagTestRule(
+        featureFlag = FeatureFlags.cardBrandChoice,
+        isEnabled = false,
+    )
 
     private val testDispatcher = UnconfinedTestDispatcher()
 
     @Test
     fun `Emits correct CBC eligibility when the feature is enabled`() = runTest(testDispatcher) {
+        featureFlagTestRule.setEnabled(true)
+
         val viewModel = CardWidgetViewModel(
-            cbcEnabled = { true },
+            cbcEligible = { true },
             dispatcher = testDispatcher,
         )
 
@@ -26,7 +37,7 @@ class CardWidgetViewModelTest {
     @Test
     fun `Emits correct CBC eligibility when the feature is disabled`() = runTest(testDispatcher) {
         val viewModel = CardWidgetViewModel(
-            cbcEnabled = { false },
+            cbcEligible = { true },
             dispatcher = testDispatcher,
         )
 
