328b608c88cc72676d041cf31c95435e829b373c
Jay Newstrom <jaynewstrom@stripe.com>
2023-10-02 12:25:46 -0600

    More enums for playground settings.

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/StringSettingDefinition.kt
+++
@@ -1,9 +0,0 @@
-package com.stripe.android.paymentsheet.example.playground.settings
-
-internal abstract class StringSettingDefinition(
-    key: String,
-    displayName: String,
-) : PlaygroundSettingDefinition<String>(key, displayName) {
-    override fun convertToString(value: String): String = value
-    override fun convertToValue(value: String): String = value
-}
