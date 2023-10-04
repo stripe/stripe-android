9e9ee8feed0e2d776628e60d5657c20450aafed4
Jay Newstrom <jaynewstrom@stripe.com>
2023-10-04 09:39:16 -0600

    Missed this one in the rebase.

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/ShippingAddressSettingsDefinition.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/ShippingAddressSettingsDefinition.kt
@@ -4,26 +4,12 @@ import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.addresselement.AddressDetails
 import com.stripe.android.paymentsheet.example.playground.PlaygroundState
 
-internal object ShippingAddressSettingsDefinition : PlaygroundSettingDefinition<AddressDetails?>(
-    key = "shippingAddress",
-    displayName = "" // Not displayed.
-) {
-    override val defaultValue: AddressDetails? = null
-    override val options: List<Option<AddressDetails?>> = emptyList()
-
-    override fun convertToValue(value: String): AddressDetails? {
-        return null
-    }
-
-    override fun convertToString(value: AddressDetails?): String {
-        return ""
-    }
-
+internal object ShippingAddressSettingsDefinition : PlaygroundSettingDefinition<AddressDetails?> {
     override fun configure(
         value: AddressDetails?,
         configurationBuilder: PaymentSheet.Configuration.Builder,
         playgroundState: PlaygroundState,
-        configurationData: PaymentSheetConfigurationData
+        configurationData: PlaygroundSettingDefinition.PaymentSheetConfigurationData
     ) {
         configurationBuilder.shippingDetails(value)
     }
