0683bbf2ee338a57718aac66223ebfab3bbb610e
Jay Newstrom <jaynewstrom@stripe.com>
2023-10-09 10:15:29 -0600

    Add bottom bar to playground. (#7395)
    ---------
    
    Co-authored-by: Samer Alabi <samer@stripe.com>

--- paymentsheet-example/src/androidTest/java/com/stripe/android/test/core/ui/ComposeButton.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/test/core/ui/ComposeButton.kt
@@ -1,7 +1,10 @@
 package com.stripe.android.test.core.ui
 
+import androidx.compose.ui.semantics.SemanticsNode
 import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasScrollAction
 import androidx.compose.ui.test.isEnabled
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.performClick
@@ -13,7 +16,7 @@ internal class ComposeButton(
     private val matcher: SemanticsMatcher,
 ) {
     fun click() {
-        composeTestRule.onNode(matcher).performScrollTo().performClick()
+        composeTestRule.onNode(matcher).tryPerformScrollTo().performClick()
     }
 
     fun waitForEnabled() {
@@ -21,5 +24,29 @@ internal class ComposeButton(
             val combinedMatcher = matcher.and(isEnabled()).and(hasClickAction())
             composeTestRule.onAllNodes(combinedMatcher).fetchSemanticsNodes().isNotEmpty()
         }
+    }
+
+    private fun SemanticsNodeInteraction.tryPerformScrollTo(): SemanticsNodeInteraction {
+        fetchSemanticsNode().findClosestParentNode {
+            hasScrollAction().matches(it)
+        } ?: return this
+        performScrollTo()
+        return this
+    }
+
+    private fun SemanticsNode.findClosestParentNode(
+        includeSelf: Boolean = false,
+        selector: (SemanticsNode) -> Boolean
+    ): SemanticsNode? {
+        var currentParent = if (includeSelf) this else parent
+        while (currentParent != null) {
+            if (selector(currentParent)) {
+                return currentParent
+            } else {
+                currentParent = currentParent.parent
+            }
+        }
+
+        return null
     }
 }
