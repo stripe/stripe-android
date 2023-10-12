7c32d6ace0d68cacc3892e766f289401c615cb45
Chen Cen <ccen@stripe.com>
2023-10-11 17:08:40 -0700

    fix lint

--- identity/src/main/java/com/stripe/android/identity/ui/ComposeLoadingButton.kt
+++ identity/src/main/java/com/stripe/android/identity/ui/ComposeLoadingButton.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
