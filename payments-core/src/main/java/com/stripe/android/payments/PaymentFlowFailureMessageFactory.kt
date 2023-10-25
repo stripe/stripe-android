838fbb375415fb2487bcc0bea35eb8995e13b268
Till Hellmund <tillh@stripe.com>
2023-10-25 12:54:35 -0400

    Fix error mapping when returning from 3DS (#7499)
    * Fix error mapping when returning from 3DS
    
    * Update tests
    
    * Update changelog
    
    * Wrap test helper in try-finally

--- payments-core/src/main/java/com/stripe/android/payments/PaymentFlowFailureMessageFactory.kt
+++ payments-core/src/main/java/com/stripe/android/payments/PaymentFlowFailureMessageFactory.kt
@@ -7,7 +7,7 @@ import com.stripe.android.model.PaymentIntent
 import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.SetupIntent
 import com.stripe.android.model.StripeIntent
-import com.stripe.android.networking.mapErrorCodeToLocalizedMessage
+import com.stripe.android.networking.withLocalizedMessage
 
 internal class PaymentFlowFailureMessageFactory(
     private val context: Context
@@ -52,8 +52,7 @@ internal class PaymentFlowFailureMessageFactory(
             context.resources.getString(R.string.stripe_failure_reason_authentication)
         }
         paymentIntent.lastPaymentError?.type == PaymentIntent.Error.Type.CardError -> {
-            val error = paymentIntent.lastPaymentError
-            context.mapErrorCodeToLocalizedMessage(error.code) ?: error.message
+            paymentIntent.lastPaymentError.withLocalizedMessage(context).message
         }
         else -> {
             null
@@ -67,8 +66,7 @@ internal class PaymentFlowFailureMessageFactory(
             context.resources.getString(R.string.stripe_failure_reason_authentication)
         }
         setupIntent.lastSetupError?.type == SetupIntent.Error.Type.CardError -> {
-            val error = setupIntent.lastSetupError
-            context.mapErrorCodeToLocalizedMessage(error.code) ?: error.message
+            setupIntent.lastSetupError.withLocalizedMessage(context).message
         }
         else -> {
             null
