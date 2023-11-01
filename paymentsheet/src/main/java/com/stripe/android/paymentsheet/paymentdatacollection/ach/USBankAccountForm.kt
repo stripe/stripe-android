10e9060f06b401121aa826ef5593eeab87e678d6
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-11-01 09:15:25 -0700

    Update bank form message for saving

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/ach/USBankAccountForm.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/ach/USBankAccountForm.kt
@@ -83,6 +83,7 @@ internal fun USBankAccountForm(
             is USBankAccountFormScreenState.BillingDetailsCollection -> {
                 BillingDetailsCollectionScreen(
                     formArgs = formArgs,
+                    isPaymentFlow = usBankAccountFormArgs.isPaymentFlow,
                     isProcessing = screenState.isProcessing,
                     nameController = viewModel.nameController,
                     emailController = viewModel.emailController,
@@ -96,6 +97,7 @@ internal fun USBankAccountForm(
                 MandateCollectionScreen(
                     formArgs = formArgs,
                     isProcessing = screenState.isProcessing,
+                    isPaymentFlow = usBankAccountFormArgs.isPaymentFlow,
                     screenState = screenState,
                     nameController = viewModel.nameController,
                     emailController = viewModel.emailController,
@@ -111,6 +113,7 @@ internal fun USBankAccountForm(
                 VerifyWithMicrodepositsScreen(
                     formArgs = formArgs,
                     isProcessing = screenState.isProcessing,
+                    isPaymentFlow = usBankAccountFormArgs.isPaymentFlow,
                     screenState = screenState,
                     nameController = viewModel.nameController,
                     emailController = viewModel.emailController,
@@ -126,6 +129,7 @@ internal fun USBankAccountForm(
                 SavedAccountScreen(
                     formArgs = formArgs,
                     isProcessing = screenState.isProcessing,
+                    isPaymentFlow = usBankAccountFormArgs.isPaymentFlow,
                     screenState = screenState,
                     nameController = viewModel.nameController,
                     emailController = viewModel.emailController,
@@ -145,6 +149,7 @@ internal fun USBankAccountForm(
 internal fun BillingDetailsCollectionScreen(
     formArgs: FormArguments,
     isProcessing: Boolean,
+    isPaymentFlow: Boolean,
     nameController: TextFieldController,
     emailController: TextFieldController,
     phoneController: PhoneNumberController,
@@ -156,6 +161,7 @@ internal fun BillingDetailsCollectionScreen(
         BillingDetailsForm(
             formArgs = formArgs,
             isProcessing = isProcessing,
+            isPaymentFlow = isPaymentFlow,
             nameController = nameController,
             emailController = emailController,
             phoneController = phoneController,
@@ -170,6 +176,7 @@ internal fun BillingDetailsCollectionScreen(
 internal fun MandateCollectionScreen(
     formArgs: FormArguments,
     isProcessing: Boolean,
+    isPaymentFlow: Boolean,
     screenState: USBankAccountFormScreenState.MandateCollection,
     nameController: TextFieldController,
     emailController: TextFieldController,
@@ -184,6 +191,7 @@ internal fun MandateCollectionScreen(
         BillingDetailsForm(
             formArgs = formArgs,
             isProcessing = isProcessing,
+            isPaymentFlow = isPaymentFlow,
             nameController = nameController,
             emailController = emailController,
             phoneController = phoneController,
@@ -206,6 +214,7 @@ internal fun MandateCollectionScreen(
 internal fun VerifyWithMicrodepositsScreen(
     formArgs: FormArguments,
     isProcessing: Boolean,
+    isPaymentFlow: Boolean,
     screenState: USBankAccountFormScreenState.VerifyWithMicrodeposits,
     nameController: TextFieldController,
     emailController: TextFieldController,
@@ -220,6 +229,7 @@ internal fun VerifyWithMicrodepositsScreen(
         BillingDetailsForm(
             formArgs = formArgs,
             isProcessing = isProcessing,
+            isPaymentFlow = isPaymentFlow,
             nameController = nameController,
             emailController = emailController,
             phoneController = phoneController,
@@ -242,6 +252,7 @@ internal fun VerifyWithMicrodepositsScreen(
 internal fun SavedAccountScreen(
     formArgs: FormArguments,
     isProcessing: Boolean,
+    isPaymentFlow: Boolean,
     screenState: USBankAccountFormScreenState.SavedAccount,
     nameController: TextFieldController,
     emailController: TextFieldController,
@@ -256,6 +267,7 @@ internal fun SavedAccountScreen(
         BillingDetailsForm(
             formArgs = formArgs,
             isProcessing = isProcessing,
+            isPaymentFlow = isPaymentFlow,
             nameController = nameController,
             emailController = emailController,
             phoneController = phoneController,
@@ -278,6 +290,7 @@ internal fun SavedAccountScreen(
 internal fun BillingDetailsForm(
     formArgs: FormArguments,
     isProcessing: Boolean,
+    isPaymentFlow: Boolean,
     nameController: TextFieldController,
     emailController: TextFieldController,
     phoneController: PhoneNumberController,
@@ -287,7 +300,11 @@ internal fun BillingDetailsForm(
 ) {
     Column(Modifier.fillMaxWidth()) {
         H6Text(
-            text = stringResource(R.string.stripe_paymentsheet_pay_with_bank_title),
+            text = if (isPaymentFlow) {
+                stringResource(R.string.stripe_paymentsheet_pay_with_bank_title)
+            } else {
+                stringResource(R.string.stripe_paymentsheet_save_bank_title)
+            },
             modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
         )
         if (formArgs.billingDetailsCollectionConfiguration.name != CollectionMode.Never) {
