b920dd2363e72919dc2e5678f96259a3e546cd44
Chen Cen <79880926+ccen-stripe@users.noreply.github.com>
2023-11-02 10:58:05 -0700

    [Identity] Remove all parameters for Scan and Upload destinations  (#7555)

--- identity/src/main/java/com/stripe/android/identity/navigation/CouldNotCaptureDestination.kt
+++ identity/src/main/java/com/stripe/android/identity/navigation/CouldNotCaptureDestination.kt
@@ -3,42 +3,30 @@ package com.stripe.android.identity.navigation
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
-import com.stripe.android.identity.states.IdentityScanState
 
 internal class CouldNotCaptureDestination(
-    scanType: IdentityScanState.ScanType,
-    requireLiveCapture: Boolean
+    fromSelfie: Boolean
 ) : IdentityTopLevelDestination() {
     override val destinationRoute = ROUTE
     override val routeWithArgs = destinationRoute.withParams(
-        ARG_COULD_NOT_CAPTURE_SCAN_TYPE to scanType,
-        ARG_REQUIRE_LIVE_CAPTURE to requireLiveCapture
+        ARG_FROM_SELFIE to fromSelfie
     )
 
     companion object {
         const val COULD_NOT_CAPTURE = "CouldNotCapture"
-        const val ARG_COULD_NOT_CAPTURE_SCAN_TYPE = "scanType"
-        const val ARG_REQUIRE_LIVE_CAPTURE = "requireLiveCapture"
+        const val ARG_FROM_SELFIE = "fromSelfie"
 
         val ROUTE = object : DestinationRoute() {
             override val routeBase = COULD_NOT_CAPTURE
 
             override val arguments = listOf(
-                navArgument(ARG_COULD_NOT_CAPTURE_SCAN_TYPE) {
-                    type = NavType.EnumType(IdentityScanState.ScanType::class.java)
-                },
-                navArgument(ARG_REQUIRE_LIVE_CAPTURE) {
+                navArgument(ARG_FROM_SELFIE) {
                     type = NavType.BoolType
                 }
             )
         }
 
-        fun couldNotCaptureScanType(backStackEntry: NavBackStackEntry) =
-            backStackEntry.arguments?.getSerializable(
-                ARG_COULD_NOT_CAPTURE_SCAN_TYPE
-            ) as IdentityScanState.ScanType
-
-        fun requireLiveCapture(backStackEntry: NavBackStackEntry) =
-            backStackEntry.getBooleanArgument(ARG_REQUIRE_LIVE_CAPTURE)
+        fun fromSelfie(backStackEntry: NavBackStackEntry) =
+            backStackEntry.getBooleanArgument(ARG_FROM_SELFIE)
     }
 }
