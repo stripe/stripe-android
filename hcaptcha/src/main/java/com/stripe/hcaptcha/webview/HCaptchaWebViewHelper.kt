2ecaf36718e9b06675e0bf2777cafc5a172930ec
Adam Wushensky <awush@stripe.com>
2023-12-19 13:50:55 -0500

    Cleanup imports

--- hcaptcha/src/main/java/com/stripe/hcaptcha/webview/HCaptchaWebViewHelper.kt
+++ hcaptcha/src/main/java/com/stripe/hcaptcha/webview/HCaptchaWebViewHelper.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Color
-import android.os.Build
 import android.os.Handler
 import android.util.Log
 import android.view.View
