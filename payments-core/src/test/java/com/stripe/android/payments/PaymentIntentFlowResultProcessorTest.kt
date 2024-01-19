4e45c7349addc1ca2234d8dac21abb0d44a5f1c1
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2024-01-18 16:57:23 -0800

    Revert "Revert "Increase max retry count for Cash App Pay""

--- payments-core/src/test/java/com/stripe/android/payments/PaymentIntentFlowResultProcessorTest.kt
+++ payments-core/src/test/java/com/stripe/android/payments/PaymentIntentFlowResultProcessorTest.kt
@@ -311,6 +311,7 @@ internal class PaymentIntentFlowResultProcessorTest {
             Result.success(processingIntent),
             Result.failure(APIConnectionException()),
             Result.failure(APIConnectionException()),
+            Result.failure(APIConnectionException()),
             Result.success(succeededIntent),
         )
 
