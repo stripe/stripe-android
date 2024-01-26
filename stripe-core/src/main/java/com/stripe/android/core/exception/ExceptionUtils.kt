365f229bdf36cb88d2c16d1c299004b9701df052
Samer Alabi <samer@stripe.com>
2024-01-26 12:15:47 -0500

    Update `safeAnalyticsMessage` to simply return 'unknown' for most exception types.

--- stripe-core/src/main/java/com/stripe/android/core/exception/ExceptionUtils.kt
+++ stripe-core/src/main/java/com/stripe/android/core/exception/ExceptionUtils.kt
@@ -3,10 +3,15 @@
 package com.stripe.android.core.exception
 
 import androidx.annotation.RestrictTo
+import java.io.IOException
+
+private const val IO_EXCEPTION_ANALYTICS_MESSAGE = "ioException"
+private const val DEFAULT_ANALYTICS_MESSAGE = "unknown"
 
 @get:RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 val Throwable.safeAnalyticsMessage: String
     get() = when (this) {
-        is StripeException -> analyticsValue() ?: javaClass.name
-        else -> javaClass.name
+        is StripeException -> analyticsValue()
+        is IOException -> IO_EXCEPTION_ANALYTICS_MESSAGE
+        else -> DEFAULT_ANALYTICS_MESSAGE
     }
