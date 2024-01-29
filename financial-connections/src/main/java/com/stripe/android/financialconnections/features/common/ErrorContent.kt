16a0e5421e867371fd194c45ff36e9111cb0f04d
Carlos M <carlosmuvi@stripe.com>
2024-01-29 11:14:30 -0800

    Detekt fixes.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/common/ErrorContent.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/common/ErrorContent.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
