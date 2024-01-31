4cfe2581c1f01c781d7858fceb9690e873930bcd
Till Hellmund <tillh@stripe.com>
2024-01-31 10:21:15 -0500

    Update `DefaultBillingAddressSettingsDefinition` to allow random email
    This makes testing Link inline signup easier.

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/DefaultBillingAddressSettingsDefinition.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/DefaultBillingAddressSettingsDefinition.kt
@@ -2,36 +2,61 @@ package com.stripe.android.paymentsheet.example.playground.settings
 
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.example.playground.PlaygroundState
-import kotlin.math.absoluteValue
-import kotlin.random.Random
+import java.util.UUID
 
-internal object DefaultBillingAddressSettingsDefinition : BooleanSettingsDefinition(
-    key = "defaultBillingAddress",
-    displayName = "Default Billing Address",
-    defaultValue = true,
-) {
+internal object DefaultBillingAddressSettingsDefinition :
+    PlaygroundSettingDefinition<DefaultBillingAddress>,
+    PlaygroundSettingDefinition.Saveable<DefaultBillingAddress> by EnumSaveable(
+        key = "defaultBillingAddress",
+        values = DefaultBillingAddress.entries.toTypedArray(),
+        defaultValue = DefaultBillingAddress.Off,
+    ),
+    PlaygroundSettingDefinition.Displayable<DefaultBillingAddress> {
+
+    override val displayName: String
+        get() = "Default Billing Address"
+
+    override val options: List<PlaygroundSettingDefinition.Displayable.Option<DefaultBillingAddress>>
+        get() = listOf(
+            option("On", DefaultBillingAddress.On),
+            option("On with random email", DefaultBillingAddress.OnWithRandomEmail),
+            option("Off", DefaultBillingAddress.Off),
+        )
+
     override fun configure(
-        value: Boolean,
+        value: DefaultBillingAddress,
         configurationBuilder: PaymentSheet.Configuration.Builder,
         playgroundState: PlaygroundState,
-        configurationData: PlaygroundSettingDefinition.PaymentSheetConfigurationData,
+        configurationData: PlaygroundSettingDefinition.PaymentSheetConfigurationData
     ) {
-        if (value) {
-            configurationBuilder.defaultBillingDetails(
-                PaymentSheet.BillingDetails(
-                    address = PaymentSheet.Address(
-                        line1 = "354 Oyster Point Blvd",
-                        line2 = null,
-                        city = "South San Francisco",
-                        state = "CA",
-                        postalCode = "94080",
-                        country = "US",
-                    ),
-                    email = "email_${Random.nextInt().absoluteValue}@email.com",
-                    name = "Jenny Rosen",
-                    phone = "+18008675309",
-                )
+        val email = when (value) {
+            DefaultBillingAddress.On -> "email@email.com"
+            DefaultBillingAddress.OnWithRandomEmail -> "email_${UUID.randomUUID().toString()}@email.com"
+            DefaultBillingAddress.Off -> null
+        }
+
+        val billingDetails = email?.let {
+            PaymentSheet.BillingDetails(
+                address = PaymentSheet.Address(
+                    line1 = "354 Oyster Point Blvd",
+                    line2 = null,
+                    city = "South San Francisco",
+                    state = "CA",
+                    postalCode = "94080",
+                    country = "US",
+                ),
+                email = it,
+                name = "Jenny Rosen",
+                phone = "+18008675309",
             )
         }
+
+        configurationBuilder.defaultBillingDetails(billingDetails)
     }
+}
+
+internal enum class DefaultBillingAddress(override val value: String) : ValueEnum {
+    On("on"),
+    OnWithRandomEmail("on_with_random_email"),
+    Off("off"),
 }
