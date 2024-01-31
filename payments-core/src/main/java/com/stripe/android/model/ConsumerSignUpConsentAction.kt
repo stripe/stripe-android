f2262a0f5ed3858b48afa486a5420dd4da069644
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2024-01-31 11:33:57 -0500

    Fix `ConsentAction` event names. (#7853)

--- payments-core/src/main/java/com/stripe/android/model/ConsumerSignUpConsentAction.kt
+++ payments-core/src/main/java/com/stripe/android/model/ConsumerSignUpConsentAction.kt
@@ -4,7 +4,7 @@ import androidx.annotation.RestrictTo
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 enum class ConsumerSignUpConsentAction(val value: String) {
-    Checkbox("clicked_checkbox_no_spm_mobile_v0"),
+    Checkbox("clicked_checkbox_nospm_mobile_v0"),
     CheckboxWithPrefilledEmail("clicked_checkbox_nospm_mobile_v0_0"),
     CheckboxWithPrefilledEmailAndPhone("clicked_checkbox_nospm_mobile_v0_1"),
     Implied("implied_consent_withspm_mobile_v0"),
