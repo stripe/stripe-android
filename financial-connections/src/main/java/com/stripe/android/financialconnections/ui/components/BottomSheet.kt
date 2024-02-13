178e0e6fb93df8f93f529c5c6adee695e0e007b2
Till Hellmund <tillh@stripe.com>
2024-02-13 17:06:19 -0500

    Fix issue with swipe-to-dismiss on bottom sheet

--- financial-connections/src/main/java/com/stripe/android/financialconnections/ui/components/BottomSheet.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/ui/components/BottomSheet.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.unit.dp
+import com.stripe.android.financialconnections.model.FinancialConnectionsSessionManifest.Pane
 import com.stripe.android.financialconnections.navigation.bottomsheet.BottomSheetNavigator
+import com.stripe.android.financialconnections.ui.LocalNavHostController
 import com.stripe.android.financialconnections.ui.theme.FinancialConnectionsTheme.colors
 import com.stripe.android.financialconnections.ui.theme.Neutral900
 
@@ -31,6 +35,28 @@ internal fun FinancialConnectionsModalBottomSheetLayout(
     bottomSheetNavigator: BottomSheetNavigator,
     content: @Composable () -> Unit
 ) {
+    val navController = LocalNavHostController.current
+
+    LaunchedEffect(Unit) {
+        var wasVisible = bottomSheetNavigator.navigatorSheetState.isVisible
+
+        snapshotFlow { bottomSheetNavigator.navigatorSheetState.isVisible }.collect { isVisible ->
+            if (wasVisible && !isVisible) {
+                // The bottom sheet might be dismissed by the user swiping it down.
+                // In this case, we have to manually tell the NavHostController to actually
+                // pop the backstack. Otherwise, an empty overlay stays around and consumes
+                // the next back press.
+                val currentRoute = navController.currentDestination?.route.orEmpty().split("/").firstOrNull()
+                val isBottomSheet = currentRoute in setOf(Pane.EXIT.value, Pane.PARTNER_AUTH_DRAWER.value)
+
+                if (isBottomSheet) {
+                    navController.popBackStack()
+                }
+            }
+            wasVisible = isVisible
+        }
+    }
+
     com.stripe.android.financialconnections.navigation.bottomsheet.ModalBottomSheetLayout(
         bottomSheetNavigator = bottomSheetNavigator,
         sheetBackgroundColor = colors.backgroundSurface,
