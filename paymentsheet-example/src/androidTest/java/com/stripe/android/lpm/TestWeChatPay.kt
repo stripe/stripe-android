829d6e704c810ae876a3819911abd1ddc86bcad2
Till Hellmund <tillh@stripe.com>
2024-02-16 09:51:28 -0500

    Add test

---
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestWeChatPay.kt
@@ -0,0 +1,28 @@
+package com.stripe.android.lpm
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.stripe.android.BasePlaygroundTest
+import com.stripe.android.paymentsheet.example.playground.settings.Country
+import com.stripe.android.paymentsheet.example.playground.settings.CountrySettingsDefinition
+import com.stripe.android.paymentsheet.example.playground.settings.Currency
+import com.stripe.android.paymentsheet.example.playground.settings.CurrencySettingsDefinition
+import com.stripe.android.test.core.TestParameters
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class TestWeChatPay : BasePlaygroundTest() {
+    private val testParameters = TestParameters.create(
+        paymentMethodCode = "wechat_pay",
+    ) { settings ->
+        settings[CountrySettingsDefinition] = Country.CN
+        settings[CurrencySettingsDefinition] = Currency.EUR
+    }
+
+    @Test
+    fun testWeChatPay() {
+        testDriver.confirmNewOrGuestComplete(
+            testParameters = testParameters,
+        )
+    }
+}
