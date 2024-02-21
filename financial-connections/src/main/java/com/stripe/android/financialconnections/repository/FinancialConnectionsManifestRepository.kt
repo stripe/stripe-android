4ebaef6ec665a15520dbb90ccb152ba079b2b784
Till Hellmund <tillh@stripe.com>
2024-02-21 15:18:38 -0500

    Fix lint

--- financial-connections/src/main/java/com/stripe/android/financialconnections/repository/FinancialConnectionsManifestRepository.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/repository/FinancialConnectionsManifestRepository.kt
@@ -258,7 +258,7 @@ private class FinancialConnectionsManifestRepositoryImpl(
             "emit_events" to true,
             "locale" to locale.toLanguageTag(),
             "mobile" to mapOf(
-                //TODO REMOVE BEFORE MERGING INTEGRATION BRANCH
+                // TODO REMOVE BEFORE MERGING INTEGRATION BRANCH
                 "forced_authflow_version" to "v3",
                 PARAMS_FULLSCREEN to true,
                 PARAMS_HIDE_CLOSE_BUTTON to true,
