d32bf351b560d8be0f59d26464445d0958cfc718
Aditya Rajan <adityar@stripe.com>
2024-02-22 16:34:01 +0100

    Add tests

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestTwint.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestTwint.kt
@@ -2,51 +2,32 @@ package com.stripe.android.lpm
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BasePlaygroundTest
-import com.stripe.android.paymentsheet.example.playground.settings.AutomaticPaymentMethodsSettingsDefinition
-import com.stripe.android.paymentsheet.example.playground.settings.CheckoutMode
-import com.stripe.android.paymentsheet.example.playground.settings.CheckoutModeSettingsDefinition
-import com.stripe.android.paymentsheet.example.playground.settings.DelayedPaymentMethodsSettingsDefinition
+import com.stripe.android.paymentsheet.example.playground.settings.Country
+import com.stripe.android.paymentsheet.example.playground.settings.CountrySettingsDefinition
+import com.stripe.android.paymentsheet.example.playground.settings.Currency
+import com.stripe.android.paymentsheet.example.playground.settings.CurrencySettingsDefinition
 import com.stripe.android.test.core.TestParameters
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-internal class TestBancontact : BasePlaygroundTest() {
+internal class TestTwint : BasePlaygroundTest() {
     private val testParameters = TestParameters.create(
-        paymentMethodCode = "bancontact",
-    )
+        paymentMethodCode = "twint",
+    ) { settings ->
+        settings[CountrySettingsDefinition] = Country.GB
+        settings[CurrencySettingsDefinition] = Currency.CHF
+    }
 
     @Test
-    fun testBancontact() {
+    fun testTwint() {
         testDriver.confirmNewOrGuestComplete(
             testParameters = testParameters,
         )
     }
 
     @Test
-    fun testBancontactSfu() {
-        testDriver.confirmNewOrGuestComplete(
-            testParameters = testParameters.copyPlaygroundSettings { settings ->
-                settings[DelayedPaymentMethodsSettingsDefinition] = true
-                settings[AutomaticPaymentMethodsSettingsDefinition] = true
-                settings[CheckoutModeSettingsDefinition] = CheckoutMode.PAYMENT_WITH_SETUP
-            },
-        )
-    }
-
-    @Test
-    fun testBancontactSetup() {
-        testDriver.confirmNewOrGuestComplete(
-            testParameters = testParameters.copyPlaygroundSettings { settings ->
-                settings[DelayedPaymentMethodsSettingsDefinition] = true
-                settings[AutomaticPaymentMethodsSettingsDefinition] = true
-                settings[CheckoutModeSettingsDefinition] = CheckoutMode.SETUP
-            },
-        )
-    }
-
-    @Test
-    fun testBancontactInCustomFlow() {
+    fun testTwintInCustomFlow() {
         testDriver.confirmCustom(
             testParameters = testParameters,
         )
