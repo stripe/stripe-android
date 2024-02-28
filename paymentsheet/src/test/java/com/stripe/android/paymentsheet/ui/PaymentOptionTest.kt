4310ee158ad1b0eeac2f33f1e83b4fff1d01baa5
Jay Newstrom <jaynewstrom@stripe.com>
2024-02-28 15:05:10 -0800

    Add a test.

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/PaymentOptionTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/PaymentOptionTest.kt
@@ -3,8 +3,10 @@ package com.stripe.android.paymentsheet.ui
 import android.os.Build
 import androidx.compose.ui.test.assertContentDescriptionEquals
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onParent
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.paymentsheet.PaymentOptionUi
@@ -42,5 +44,32 @@ class PaymentOptionTest {
             .onNodeWithText(label)
             .onParent()
             .assertContentDescriptionEquals("Card ending in 4 2 4 2 ")
+    }
+
+    @Test
+    fun `remove confirmation dialog is dismissed on confirmation`() {
+        val label = "Card ending in 4242"
+
+        composeTestRule.setContent {
+            PaymentOptionUi(
+                viewWidth = 100.dp,
+                isSelected = false,
+                editState = PaymentOptionEditState.Removable,
+                isEnabled = true,
+                iconRes = R.drawable.stripe_ic_paymentsheet_card_visa,
+                description = label,
+                labelText = label,
+                onItemSelectedListener = {},
+                onRemoveListener = {},
+                onRemoveAccessibilityDescription = "remove"
+            )
+        }
+
+        // remove icon for the payment method
+        composeTestRule.onNodeWithContentDescription("remove").performClick()
+        // remove button in dialog
+        composeTestRule.onNodeWithText("Remove").performClick()
+        // The dialog should be removed.
+        composeTestRule.onNodeWithText("Remove").assertDoesNotExist()
     }
 }
