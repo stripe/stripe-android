c8821112076d552431a114b7121b5897ab0b4f6d
Carlos M <99293320+carlosmuvi-stripe@users.noreply.github.com>
2024-03-05 12:01:26 -0800

    [V3] Adds padding to inst picker search bar on scroll (#8036)
    * [skip ci] Start PR
    
    * Add 8dp padding underneath the institution picker search bar.
    
    * Removes comment.
    
    * Regenerates screenshots
    
    * Regenerates screenshots
    
    * Regenerates screenshots
    
    * Regenerates screenshots
    
    * Extracts to method.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/institutionpicker/InstitutionPickerPreviewParameterProvider.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/institutionpicker/InstitutionPickerPreviewParameterProvider.kt
@@ -9,7 +9,8 @@ import com.stripe.android.financialconnections.model.FinancialConnectionsInstitu
 import com.stripe.android.financialconnections.model.InstitutionResponse
 
 internal class InstitutionPickerPreviewParameterProvider :
-    PreviewParameterProvider<InstitutionPickerState> {
+    PreviewParameterProvider<InstitutionPickerPreviewParameterProvider.InstitutionPreviewState> {
+
     override val values = sequenceOf(
         initialLoading(),
         featured(),
@@ -20,120 +21,171 @@ internal class InstitutionPickerPreviewParameterProvider :
         searchNoResultsNoManualEntry(),
         searchFailed(),
         searchFailedNoManualEntry(),
-        selectedInstitution()
+        selectedInstitution(),
+        partiallyScrolled()
     )
 
-    private fun initialLoading() = InstitutionPickerState(
-        previewText = null,
-        payload = Loading(),
-        searchInstitutions = Uninitialized,
-    )
-
-    private fun featured() = InstitutionPickerState(
-        previewText = null,
-        payload = Success(payload()),
-        searchInstitutions = Uninitialized,
-    )
-
-    private fun searchInProgress() = InstitutionPickerState(
-        previewText = "Some query",
-        payload = Success(payload()),
-        searchInstitutions = Loading(),
-    )
-
-    private fun searchSuccess() = InstitutionPickerState(
-        previewText = "Some query",
-        payload = Success(payload()),
-        searchInstitutions = Success(institutionResponse().copy(showManualEntry = true)),
-    )
-
-    private fun searchSuccessNoManualEntry() = InstitutionPickerState(
-        previewText = "Some query",
-        payload = Success(payload()),
-        searchInstitutions = Success(institutionResponse().copy(showManualEntry = false)),
-    )
-
-    private fun searchNoResults() = InstitutionPickerState(
-        previewText = "Some query",
-        payload = Success(payload()),
-        searchInstitutions = Success(
-            InstitutionResponse(
-                data = emptyList(),
-                showManualEntry = true
-            )
+    private fun initialLoading() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = null,
+            payload = Loading(),
+            searchInstitutions = Uninitialized,
         ),
+        initialScroll = 0
     )
 
-    private fun searchNoResultsNoManualEntry() = InstitutionPickerState(
-        previewText = "Some query",
-        payload = Success(payload()),
-        searchInstitutions = Success(
-            InstitutionResponse(
-                data = emptyList(),
-                showManualEntry = false
-            )
+    private fun featured() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = null,
+            payload = Success(payload()),
+            searchInstitutions = Uninitialized,
         ),
+        initialScroll = 0
     )
 
-    private fun searchFailed() = InstitutionPickerState(
-        previewText = "Some query",
-        payload = Success(payload(manualEntry = true)),
-        searchInstitutions = Fail(java.lang.Exception("Something went wrong")),
+    private fun searchInProgress() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = "Some query",
+            payload = Success(payload()),
+            searchInstitutions = Loading(),
+        ),
+        initialScroll = 0
     )
 
-    private fun searchFailedNoManualEntry() = InstitutionPickerState(
-        previewText = "Some query",
-        payload = Success(payload(manualEntry = false)),
-        searchInstitutions = Fail(java.lang.Exception("Something went wrong")),
+    private fun searchSuccess() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = "Some query",
+            payload = Success(payload()),
+            searchInstitutions = Success(institutionResponse(FEW_INSTITUTIONS).copy(showManualEntry = true)),
+        ),
+        initialScroll = 0
     )
 
-    private fun selectedInstitution() = InstitutionPickerState(
-        previewText = "Some query",
-        payload = Success(payload()),
-        searchInstitutions = Success(institutionResponse()),
-        selectedInstitutionId = "2",
-        createSessionForInstitution = Loading(),
+    private fun searchSuccessNoManualEntry() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = "Some query",
+            payload = Success(payload()),
+            searchInstitutions = Success(institutionResponse(FEW_INSTITUTIONS).copy(showManualEntry = false)),
+        ),
+        initialScroll = 0
     )
+
+    private fun searchNoResults() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = "Some query",
+            payload = Success(payload()),
+            searchInstitutions = Success(
+                InstitutionResponse(
+                    data = emptyList(),
+                    showManualEntry = true
+                )
+            ),
+        ),
+        initialScroll = 0
+    )
+
+    private fun searchNoResultsNoManualEntry() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = "Some query",
+            payload = Success(payload()),
+            searchInstitutions = Success(
+                InstitutionResponse(
+                    data = emptyList(),
+                    showManualEntry = false
+                )
+            ),
+        ),
+        initialScroll = 0
+    )
+
+    private fun searchFailed() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = "Some query",
+            payload = Success(payload(manualEntry = true)),
+            searchInstitutions = Fail(java.lang.Exception("Something went wrong")),
+        ),
+        initialScroll = 0
+    )
+
+    private fun searchFailedNoManualEntry() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = "Some query",
+            payload = Success(payload(manualEntry = false)),
+            searchInstitutions = Fail(java.lang.Exception("Something went wrong")),
+        ),
+        initialScroll = 0
+    )
+
+    private fun selectedInstitution() = InstitutionPreviewState(
+        state = InstitutionPickerState(
+            previewText = "Some query",
+            payload = Success(payload()),
+            searchInstitutions = Success(institutionResponse(FEW_INSTITUTIONS)),
+            selectedInstitutionId = "2",
+            createSessionForInstitution = Loading(),
+        ),
+        initialScroll = 0
+    )
+
+    private fun partiallyScrolled(): InstitutionPreviewState {
+        return InstitutionPreviewState(
+            state = InstitutionPickerState(
+                previewText = "Some query",
+                payload = Success(payload()),
+                searchInstitutions = Success(institutionResponse(MANY_INSTITUTIONS)),
+            ),
+            initialScroll = 1000
+        )
+    }
 
     private fun payload(manualEntry: Boolean = true) = InstitutionPickerState.Payload(
-        featuredInstitutions = institutionResponse().copy(showManualEntry = manualEntry),
+        featuredInstitutions = institutionResponse(institutions = FEW_INSTITUTIONS).copy(showManualEntry = manualEntry),
         searchDisabled = false,
         featuredInstitutionsDuration = 0
     )
 
-    private fun institutionResponse() = InstitutionResponse(
+    @Suppress("MagicNumber")
+    private fun institutionResponse(institutions: Int) = InstitutionResponse(
         showManualEntry = true,
         listOf(
-            FinancialConnectionsInstitution(
-                id = "1",
+            institution(1).copy(
                 name = "Very very long institution content does not fit - 1",
                 url = "https://www.institutionUrl.com/1",
-                featured = false,
-                featuredOrder = null,
-                icon = null,
-                logo = null,
-                mobileHandoffCapable = false
             ),
-            FinancialConnectionsInstitution(
-                id = "2",
-                name = "Institution 2",
-                url = "otherUrl.com",
-                featured = false,
-                featuredOrder = null,
-                icon = null,
-                logo = null,
-                mobileHandoffCapable = false
+            institution(2),
+            institution(3).copy(
+                url = "Unparseable URL"
             ),
-            FinancialConnectionsInstitution(
-                id = "3",
-                name = "Institution 3",
-                url = "Unparseable URL",
-                featured = false,
-                featuredOrder = null,
-                icon = null,
-                logo = null,
-                mobileHandoffCapable = false
-            )
-        )
+            institution(4),
+            institution(5),
+            institution(6),
+            institution(7),
+            institution(8),
+            institution(9),
+            institution(10)
+        ).take(institutions)
     )
+
+    private fun institution(i: Int): FinancialConnectionsInstitution {
+        return FinancialConnectionsInstitution(
+            id = i.toString(),
+            name = "Institution $i",
+            url = "otherUrl.com",
+            featured = false,
+            featuredOrder = null,
+            icon = null,
+            logo = null,
+            mobileHandoffCapable = false
+        )
+    }
+
+    data class InstitutionPreviewState(
+        val state: InstitutionPickerState,
+        val initialScroll: Int
+    )
+
+    companion object {
+        const val FEW_INSTITUTIONS = 3
+        const val MANY_INSTITUTIONS = 10
+    }
 }
