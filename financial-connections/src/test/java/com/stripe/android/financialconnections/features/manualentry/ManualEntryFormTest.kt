fcccd10849c5265966a7993394bd0873b696e97d
Carlos M. <carlosmuvi@stripe.com>
2024-03-06 12:11:12 -0800

    Adds tests.

--- financial-connections/src/test/java/com/stripe/android/financialconnections/features/manualentry/ManualEntryFormTest.kt
+++ financial-connections/src/test/java/com/stripe/android/financialconnections/features/manualentry/ManualEntryFormTest.kt
@@ -32,9 +32,9 @@ class ManualEntryFormTest {
 
     @Test
     fun `routing number validation checks for US routing number pattern`() = runTest {
-        form.routing = "111000025" // Valid US routing number
         scope.advanceUntilIdle()
         assertNull(form.routingError.first())
+
         form.routing = "123456789" // Invalid US routing number
         scope.advanceUntilIdle()
         assertEquals(R.string.stripe_validation_no_us_routing, form.routingError.first())
@@ -42,53 +42,53 @@ class ManualEntryFormTest {
 
     @Test
     fun `routing number must have correct length`() = runTest {
-        form.routing = ""
         scope.advanceUntilIdle()
         assertNull(form.routingError.first())
+
         form.routing = "12345"
         scope.advanceUntilIdle()
         assertEquals(R.string.stripe_validation_routing_too_short, form.routingError.first())
     }
 
-        @Test
+    @Test
     fun `account number validation checks for required and max length`() = runTest {
-        form.account = ""
-        assertEquals(R.string.stripe_validation_account_required, form.accountError.first())
+        scope.advanceUntilIdle()
+        assertNull(form.routingError.first())
 
         form.account = "12345678901234567" // 17 characters
+        scope.advanceUntilIdle()
         assertNull(form.accountError.first())
 
         form.account = "123456789012345678" // more than 17 characters
-        assertEquals(R.string.stripe_validation_account_too_long, manualEntryForm.accountError.first())
+        scope.advanceUntilIdle()
+        assertEquals(R.string.stripe_validation_account_too_long, form.accountError.first())
+    }
+
+    @Test
+    fun `account confirm validation checks for mismatch`() = runTest {
+        scope.advanceUntilIdle()
+        assertNull(form.routingError.first())
+
+        form.account = "correctAccount"
+        form.accountConfirm = "correctAccount"
+        scope.advanceUntilIdle()
+        assertNull(form.accountConfirmError.first())
+
+        form.accountConfirm = "wrongAccount"
+        scope.advanceUntilIdle()
+        assertEquals(R.string.stripe_validation_account_confirm_mismatch, form.accountConfirmError.first())
+    }
+
+    @Test
+    fun `form is valid if all fields are filled and there are no errors`() = runTest {
+        scope.advanceUntilIdle()
+        assertTrue(form.isValid.first().not())
+
+        form.routing = "111000025" // Valid US routing number
+        form.account = "12345678"
+        form.accountConfirm = "12345678"
+        scope.advanceUntilIdle()
+        assertTrue(form.isValid.first())
     }
 
 }
-
-//
-
-//
-
-//
-//
-
-//
-//    @Test
-//    fun `account confirm validation checks for mismatch`() = runTest {
-//        manualEntryForm.account = "correctAccount"
-//        manualEntryForm.accountConfirm = "correctAccount"
-//        assertNull(manualEntryForm.accountConfirmError.first())
-//
-//        manualEntryForm.accountConfirm = "wrongAccount"
-//        assertEquals(R.string.stripe_validation_account_confirm_mismatch, manualEntryForm.accountConfirmError.first())
-//    }
-//
-//    @Test
-//    fun `form is valid if all fields are filled and there are no errors`() = runTest {
-//        manualEntryForm.routing = "111000025" // Valid US routing number
-//        manualEntryForm.account = "12345678"
-//        manualEntryForm.accountConfirm = "12345678"
-//
-//        assertTrue(manualEntryForm.isValid.first())
-//    }
-
-//}
