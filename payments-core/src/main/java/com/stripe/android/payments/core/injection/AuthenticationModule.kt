5e730f044df7beb6835854863652a0769b91bb5a
Annabella Koch <amk@stripe.com>
2024-03-08 13:25:59 -0800

    get everything to build

--- payments-core/src/main/java/com/stripe/android/payments/core/injection/AuthenticationModule.kt
+++ payments-core/src/main/java/com/stripe/android/payments/core/injection/AuthenticationModule.kt
@@ -1,8 +1,16 @@
 package com.stripe.android.payments.core.injection
 
+import android.app.Application
 import android.content.Context
 import com.stripe.android.PaymentBrowserAuthStarter
+import com.stripe.android.PaymentConfiguration
 import com.stripe.android.PaymentRelayStarter
+import com.stripe.android.core.networking.AnalyticsRequestFactory
+import com.stripe.android.core.networking.ErrorReporter
+import com.stripe.android.core.networking.FakeErrorReporter
+import com.stripe.android.core.networking.NetworkTypeDetector
+import com.stripe.android.core.networking.QErrorReporter
+import com.stripe.android.core.utils.ContextUtils.packageInfo
 import com.stripe.android.model.StripeIntent
 import com.stripe.android.model.StripeIntent.NextActionData
 import com.stripe.android.payments.DefaultReturnUrl
@@ -20,6 +28,7 @@ import dagger.Lazy
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
+import javax.inject.Provider
 import javax.inject.Singleton
 
 /**
@@ -95,12 +104,30 @@ internal abstract class AuthenticationModule {
     @Binds
     abstract fun bindsRedirectResolver(impl: RealRedirectResolver): RedirectResolver
 
+    @Binds
+    abstract fun bindsErrorReporter(impl : QErrorReporter) : ErrorReporter
+
+
     companion object {
         @Provides
         @Singleton
         fun provideDefaultReturnUrl(
             context: Context
         ) = DefaultReturnUrl.create(context)
+
+
+        // TODO: I feel like I shouldn't have to rewrite this
+        @Provides
+        internal fun provideAnalyticsRequestFactory(
+            context: Context,
+        ): AnalyticsRequestFactory = AnalyticsRequestFactory(
+            packageManager = context.packageManager,
+            packageName = context.packageName.orEmpty(),
+            packageInfo = context.packageInfo,
+            publishableKeyProvider = { PaymentConfiguration.getInstance(context = context).publishableKey },
+            networkTypeProvider = NetworkTypeDetector(context = context)::invoke,
+        )
+
 
         @Provides
         @Singleton
