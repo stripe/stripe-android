23ad46c4285371b3da437b91149f800aab4fb976
Jay Newstrom <jaynewstrom@stripe.com>
2024-03-14 09:02:25 -0600

    Remove unused LinkActivityContractArgsModule.

--- link/src/main/java/com/stripe/android/link/injection/LinkActivityContractArgsModule.kt
+++ link/src/main/java/com/stripe/android/link/injection/LinkActivityContractArgsModule.kt
@@ -1,23 +1,18 @@
 package com.stripe.android.link.injection
 
-import com.stripe.android.link.LinkActivityContract
-import com.stripe.android.ui.core.injection.FormControllerSubcomponent
-import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
-
-/**
- * Module that extracts variables needed for injection from [LinkActivityContract.Args].
- */
-@Module(
-    subcomponents = [
-        FormControllerSubcomponent::class
-    ]
-)
-internal interface LinkActivityContractArgsModule {
-    companion object {
-        @Provides
-        @Singleton
-        fun provideConfiguration(args: LinkActivityContract.Args) = args.configuration
-    }
-}
+//
+///**
+// * Module that extracts variables needed for injection from [LinkActivityContract.Args].
+// */
+//@Module(
+//    subcomponents = [
+//        FormControllerSubcomponent::class
+//    ]
+//)
+//internal interface LinkActivityContractArgsModule {
+//    companion object {
+//        @Provides
+//        @Singleton
+//        fun provideConfiguration(args: LinkActivityContract.Args) = args.configuration
+//    }
+//}
