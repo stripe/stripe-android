6b7661f4d501544a0a8eb515d4a4305cf684a490
Jay Newstrom <jaynewstrom@stripe.com>
2024-03-21 10:24:04 -0600

    Remove unused field.

--- payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/SharedDataSpec.kt
+++ payments-ui-core/src/main/java/com/stripe/android/ui/core/elements/SharedDataSpec.kt
@@ -13,10 +13,6 @@ data class SharedDataSpec(
     @SerialName("type")
     val type: String,
 
-    /** Ignored by the SDK */
-    @SerialName("async")
-    val async: Boolean = false,
-
     // If a form is empty, it must still have an EmptyFormSpec
     // field to get the form into a complete state (i.e. PayPal).
     @SerialName("fields")
