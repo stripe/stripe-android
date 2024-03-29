ccc9a336681d459922d44dfdacf0d9447e4dbd69
Annabella Koch <amk@stripe.com>
2024-03-29 11:06:10 -0700

    Add request ID to error analytics from RealErrorReporter

--- payments-core/src/test/java/com/stripe/android/payments/core/analytics/RealErrorReporterTest.kt
+++ payments-core/src/test/java/com/stripe/android/payments/core/analytics/RealErrorReporterTest.kt
@@ -4,9 +4,11 @@ import android.app.Application
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.ApiKeyFixtures
+import com.stripe.android.core.StripeError
 import com.stripe.android.core.exception.StripeException
 import com.stripe.android.core.networking.AnalyticsRequestFactory
 import com.stripe.android.core.utils.ContextUtils.packageInfo
+import com.stripe.android.exception.CardException
 import com.stripe.android.testing.FakeAnalyticsRequestExecutor
 import org.junit.Before
 import org.junit.Test
@@ -44,5 +46,23 @@ class RealErrorReporterTest {
         val analyticsRequestParams = executedAnalyticsRequests.get(0).params
         assertThat(analyticsRequestParams.get("analyticsValue")).isEqualTo(expectedAnalyticsValue)
         assertThat(analyticsRequestParams.get("statusCode")).isEqualTo(expectedStatusCode)
+        assertThat(analyticsRequestParams.get("requestId")).isNull()
+    }
+
+    @Test
+    fun `RealErrorReporter logs requestId correctly`() {
+        val expectedRequestId = "some_request_ID"
+        val exception = CardException(StripeError(), requestId = expectedRequestId)
+        val expectedAnalyticsValue = exception.analyticsValue()
+        val expectedStatusCode = exception.statusCode.toString()
+
+        realErrorReporter.report(ErrorReporter.ErrorEvent.GET_SAVED_PAYMENT_METHODS_FAILURE, exception)
+
+        val executedAnalyticsRequests = analyticsRequestExecutor.getExecutedRequests()
+        assertThat(executedAnalyticsRequests.size).isEqualTo(1)
+        val analyticsRequestParams = executedAnalyticsRequests.get(0).params
+        assertThat(analyticsRequestParams.get("analyticsValue")).isEqualTo(expectedAnalyticsValue)
+        assertThat(analyticsRequestParams.get("statusCode")).isEqualTo(expectedStatusCode)
+        assertThat(analyticsRequestParams.get("requestId")).isEqualTo(expectedRequestId)
     }
 }
