5d07f1e5c3ef7bec7ef8fd296bba98ba64e583ae
Annabella Koch <amk@stripe.com>
2024-04-03 15:33:49 -0700

    Revert PaymentSheetConfirmationError changes

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/analytics/PaymentSheetConfirmationError.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/analytics/PaymentSheetConfirmationError.kt
@@ -2,14 +2,14 @@ package com.stripe.android.paymentsheet.analytics
 
 import com.stripe.android.core.exception.StripeException
 
-internal sealed class PaymentSheetConfirmationError : StripeException() {
+internal sealed class PaymentSheetConfirmationError : Throwable() {
 
     abstract val analyticsValue: String
 
     data class Stripe(override val cause: Throwable) : PaymentSheetConfirmationError() {
 
         override val analyticsValue: String
-            get() = create(cause).analyticsValue()
+            get() = StripeException.create(cause).analyticsValue()
     }
 
     data class GooglePay(val errorCode: Int) : PaymentSheetConfirmationError() {
