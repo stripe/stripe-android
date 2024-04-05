ca1a3af28a04e3a3ad1c250bee2653da9d4a3b51
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2024-04-05 19:21:06 -0400

    Add initial `CustomerSheet` network tests. (#8232)

---
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/CustomerSheetPage.kt
@@ -0,0 +1,81 @@
+package com.stripe.android.paymentsheet
+
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isEnabled
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
+import androidx.compose.ui.test.performTextReplacement
+import androidx.test.espresso.Espresso
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.stripe.android.customersheet.ui.CUSTOMER_SHEET_CONFIRM_BUTTON_TEST_TAG
+import com.stripe.android.customersheet.ui.CUSTOMER_SHEET_SAVE_BUTTON_TEST_TAG
+
+internal class CustomerSheetPage(
+    private val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>,
+) {
+    fun waitForText(text: String, substring: Boolean = false) {
+        waitUntil(hasText(text, substring = substring))
+    }
+
+    fun fillOutCardDetails() {
+        replaceText("Card number", CARD_NUMBER)
+        replaceText("MM / YY", "$EXPIRY_MONTH/$${EXPIRY_YEAR.substring(startIndex = 2)}")
+        replaceText("CVC", CVC)
+        replaceText("ZIP Code", ZIP_CODE)
+    }
+
+    fun clickSaveButton() {
+        clickPrimaryButton(CUSTOMER_SHEET_SAVE_BUTTON_TEST_TAG)
+    }
+
+    fun clickConfirmButton() {
+        clickPrimaryButton(CUSTOMER_SHEET_CONFIRM_BUTTON_TEST_TAG)
+    }
+
+    private fun clickPrimaryButton(tag: String) {
+        waitUntil(hasTestTag(tag).and(isEnabled()))
+        click(hasTestTag(tag))
+        waitForIdle()
+    }
+
+    private fun waitForIdle() {
+        Espresso.onIdle()
+        composeTestRule.waitForIdle()
+    }
+
+    private fun waitUntil(matcher: SemanticsMatcher) {
+        waitForIdle()
+
+        composeTestRule.waitUntil(5_000) {
+            composeTestRule
+                .onAllNodes(matcher.and(isEnabled()))
+                .fetchSemanticsNodes().isNotEmpty()
+        }
+    }
+
+    private fun click(matcher: SemanticsMatcher) {
+        composeTestRule.onNode(matcher)
+            .performScrollTo()
+            .performClick()
+    }
+
+    private fun replaceText(label: String, text: String, isLabelSubstring: Boolean = false) {
+        waitForText(label, substring = isLabelSubstring)
+
+        composeTestRule.onNode(hasText(label, substring = isLabelSubstring))
+            .performScrollTo()
+            .performTextReplacement(text)
+    }
+
+    companion object {
+        const val CARD_NUMBER = "4242424242424242"
+        const val EXPIRY_MONTH = "12"
+        const val EXPIRY_YEAR = "2034"
+        const val CVC = "123"
+        const val ZIP_CODE = "12345"
+        const val COUNTRY = "US"
+    }
+}
---
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/CustomerSheetTest.kt
@@ -0,0 +1,200 @@
+package com.stripe.android.paymentsheet
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.stripe.android.customersheet.CustomerSheetResult
+import com.stripe.android.customersheet.ExperimentalCustomerSheetApi
+import com.stripe.android.networktesting.NetworkRule
+import com.stripe.android.networktesting.RequestMatcher
+import com.stripe.android.networktesting.RequestMatchers
+import com.stripe.android.networktesting.RequestMatchers.bodyPart
+import com.stripe.android.networktesting.RequestMatchers.host
+import com.stripe.android.networktesting.RequestMatchers.method
+import com.stripe.android.networktesting.RequestMatchers.path
+import com.stripe.android.networktesting.RequestMatchers.query
+import com.stripe.android.networktesting.testBodyFromFile
+import com.stripe.android.paymentsheet.utils.IntegrationType
+import com.stripe.android.paymentsheet.utils.IntegrationTypeProvider
+import com.stripe.android.paymentsheet.utils.runCustomerSheetTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@OptIn(ExperimentalCustomerSheetApi::class)
+@RunWith(TestParameterInjector::class)
+internal class CustomerSheetTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private val activityScenarioRule = composeTestRule.activityRule
+
+    @get:Rule
+    val networkRule = NetworkRule()
+
+    private val page: CustomerSheetPage = CustomerSheetPage(composeTestRule)
+
+    @TestParameter(valuesProvider = IntegrationTypeProvider::class)
+    lateinit var integrationType: IntegrationType
+
+    @Test
+    fun testSuccessfulCardSave() = activityScenarioRule.runCustomerSheetTest(
+        integrationType = integrationType,
+        resultCallback = { result ->
+            assert(result is CustomerSheetResult.Selected)
+        }
+    ) { context ->
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-requires_payment_method.json")
+        }
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/payment_methods"),
+            query("type", "card")
+        ) { response ->
+            response.testBodyFromFile("payment-methods-get-success-empty.json")
+        }
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/payment_methods"),
+            query("type", "us_bank_account")
+        ) { response ->
+            response.testBodyFromFile("payment-methods-get-success-empty.json")
+        }
+
+        context.presentCustomerSheet()
+
+        page.fillOutCardDetails()
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("POST"),
+            path("/v1/payment_methods"),
+            cardDetails(),
+        ) { response ->
+            response.testBodyFromFile("payment-methods-create.json")
+        }
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/setup_intents/seti_12345"),
+            query("client_secret", "seti_12345_secret_12345"),
+        ) { response ->
+            response.testBodyFromFile("setup-intent-get.json")
+        }
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("POST"),
+            path("/v1/setup_intents/seti_12345/confirm"),
+            confirmParams()
+        ) { response ->
+            response.testBodyFromFile("setup-intent-confirm.json")
+        }
+
+        page.clickSaveButton()
+        page.clickConfirmButton()
+    }
+
+    @Test
+    fun testCardNotSavedOnConnfirmError() = activityScenarioRule.runCustomerSheetTest(
+        integrationType = integrationType,
+        resultCallback = {
+            error("Shouldn't call CustomerSheetResultCallback")
+        }
+    ) { context ->
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-requires_payment_method.json")
+        }
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/payment_methods"),
+            query("type", "card")
+        ) { response ->
+            response.testBodyFromFile("payment-methods-get-success-empty.json")
+        }
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/payment_methods"),
+            query("type", "us_bank_account")
+        ) { response ->
+            response.testBodyFromFile("payment-methods-get-success-empty.json")
+        }
+
+        context.presentCustomerSheet()
+
+        page.fillOutCardDetails()
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("POST"),
+            path("/v1/payment_methods"),
+            cardDetails(),
+        ) { response ->
+            response.testBodyFromFile("payment-methods-create.json")
+        }
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/setup_intents/seti_12345"),
+            query("client_secret", "seti_12345_secret_12345"),
+        ) { response ->
+            response.testBodyFromFile("setup-intent-get.json")
+        }
+
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("POST"),
+            path("/v1/setup_intents/seti_12345/confirm"),
+            confirmParams()
+        ) { response ->
+            response.setResponseCode(402)
+            response.testBodyFromFile("setup-intent-confirm_incorrect_cvc.json")
+        }
+
+        page.clickSaveButton()
+
+        page.waitForText("Your card's security code is incorrect.")
+
+        context.markTestSucceeded()
+    }
+
+    private fun cardDetails(): RequestMatcher {
+        return RequestMatchers.composite(
+            bodyPart("type", "card"),
+            bodyPart("card%5Bnumber%5D", CustomerSheetPage.CARD_NUMBER),
+            bodyPart("card%5Bexp_month%5D", CustomerSheetPage.EXPIRY_MONTH),
+            bodyPart("card%5Bexp_year%5D", CustomerSheetPage.EXPIRY_YEAR),
+            bodyPart("card%5Bcvc%5D", CustomerSheetPage.CVC),
+            bodyPart("billing_details%5Baddress%5D%5Bpostal_code%5D", CustomerSheetPage.ZIP_CODE),
+            bodyPart("billing_details%5Baddress%5D%5Bcountry%5D", CustomerSheetPage.COUNTRY)
+        )
+    }
+
+    private fun confirmParams(): RequestMatcher {
+        return RequestMatchers.composite(
+            bodyPart("payment_method", "pm_12345"),
+            bodyPart("client_secret", "seti_12345_secret_12345"),
+            bodyPart("mandate_data%5Bcustomer_acceptance%5D%5Btype%5D", "online"),
+            bodyPart("mandate_data%5Bcustomer_acceptance%5D%5Bonline%5D%5Binfer_from_client%5D", "true"),
+        )
+    }
+}
