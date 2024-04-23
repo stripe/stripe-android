81c0cd559876c1622330339ec270f4d8ade03550
Samer Alabi <samer@stripe.com>
2024-04-22 20:33:35 -0400

    Generate in-code schema definitions to replace JSON files.

---
+++ stripe-ui-core/src/main/java/com/stripe/android/uicore/address/schemas/MfAddressSchemaDefinition.kt
@@ -0,0 +1,50 @@
+package com.stripe.android.uicore.address.schemas
+
+import com.stripe.android.uicore.address.AddressSchemaDefinition
+import com.stripe.android.uicore.address.CountryAddressSchema
+import com.stripe.android.uicore.address.FieldSchema
+import com.stripe.android.uicore.address.FieldType
+import com.stripe.android.uicore.address.NameType
+import kotlin.String
+import kotlin.collections.List
+
+internal object MfAddressSchemaDefinition : AddressSchemaDefinition {
+    override val countryCode: String = "MF"
+
+    override fun schemaElements(): List<CountryAddressSchema> = listOf(
+        CountryAddressSchema(
+            type = FieldType.AddressLine1,
+            required = true,
+            schema = null,
+        ),
+        CountryAddressSchema(
+            type = FieldType.AddressLine2,
+            required = false,
+            schema = null,
+        ),
+        CountryAddressSchema(
+            type = FieldType.PostalCode,
+            required = true,
+            schema = FieldSchema(
+                nameType = NameType.Postal,
+                isNumeric = false,
+            ),
+        ),
+        CountryAddressSchema(
+            type = FieldType.Locality,
+            required = true,
+            schema = FieldSchema(
+                nameType = NameType.City,
+                isNumeric = false,
+            ),
+        ),
+        CountryAddressSchema(
+            type = FieldType.SortingCode,
+            required = false,
+            schema = FieldSchema(
+                nameType = NameType.Cedex,
+                isNumeric = false,
+            ),
+        ),
+    )
+}
