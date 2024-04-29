d20d39a02cba729c648bc5706f48f330d27b58fe
Annabella Koch <amk@stripe.com>
2024-04-29 15:51:48 -0700

    add tests for AddPaymentMethod

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/AddPaymentMethodTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/AddPaymentMethodTest.kt
@@ -1,24 +1,17 @@
 package com.stripe.android.paymentsheet.ui
 
 import android.content.Context
-import androidx.compose.ui.platform.LocalContext
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadataFactory
-import com.stripe.android.lpmfoundations.paymentmethod.definitions.BlikDefinition
 import com.stripe.android.model.PaymentIntentFixtures
-import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.PaymentMethodCreateParams.Companion.getNameFromParams
 import com.stripe.android.model.PaymentMethodFixtures
-import com.stripe.android.model.PaymentMethodOptionsParams
 import com.stripe.android.paymentsheet.forms.FormFieldValues
 import com.stripe.android.paymentsheet.model.PaymentSelection
 import com.stripe.android.uicore.elements.IdentifierSpec
-import com.stripe.android.uicore.elements.TextFieldStateConstants
 import com.stripe.android.uicore.forms.FormFieldEntry
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import kotlin.test.Test
 
@@ -62,28 +55,52 @@ internal class AddPaymentMethodTest {
 
     @Test
     fun `transformToPaymentSelection transforms external payment methods correctly`() {
-        // TODO: update to be epms
-        val cardBrand = "visa"
-        val name = "Joe"
         val customerRequestedSave = PaymentSelection.CustomerRequestedSave.RequestNoReuse
+        val paypalSpec = PaymentMethodFixtures.PAYPAL_EXTERNAL_PAYMENT_METHOD_SPEC
+        val formFieldValues = FormFieldValues(
+            fieldValuePairs = mapOf(),
+            showsMandate = false,
+            userRequestedReuse = customerRequestedSave,
+        )
+        val externalPaymentMethod = metadata.supportedPaymentMethodForCode(paypalSpec.type)!!
+
+        val externalPaymentSelection = formFieldValues.transformToPaymentSelection(
+            context,
+            externalPaymentMethod,
+            metadata
+        ) as PaymentSelection.ExternalPaymentMethod
+
+        assertThat(externalPaymentSelection.type).isEqualTo(PaymentMethodFixtures.PAYPAL_EXTERNAL_PAYMENT_METHOD_SPEC.type)
+        assertThat(externalPaymentSelection.label).isEqualTo(paypalSpec.label)
+        assertThat(externalPaymentSelection.lightThemeIconUrl).isEqualTo(paypalSpec.lightImageUrl)
+        assertThat(externalPaymentSelection.darkThemeIconUrl).isEqualTo(paypalSpec.darkImageUrl)
+        assertThat(externalPaymentSelection.iconResource).isEqualTo(0)
+    }
+
+    @Test
+    fun `transformToPaymentSelection transforms generic payment methods correctly`() {
+        val customerRequestedSave = PaymentSelection.CustomerRequestedSave.RequestNoReuse
+        val name = "Joe"
         val formFieldValues = FormFieldValues(
             fieldValuePairs = mapOf(
-                IdentifierSpec.CardBrand to FormFieldEntry(cardBrand, true),
                 IdentifierSpec.Name to FormFieldEntry(name, true),
             ),
             showsMandate = false,
             userRequestedReuse = customerRequestedSave,
         )
-        val cardPaymentMethod = metadata.supportedPaymentMethodForCode("card")!!
+        val klarnaPaymentMethod = metadata.supportedPaymentMethodForCode("klarna")!!
 
-        val cardPaymentSelection = formFieldValues.transformToPaymentSelection(
+        val klarnaPaymentSelection = formFieldValues.transformToPaymentSelection(
             context,
-            cardPaymentMethod,
+            klarnaPaymentMethod,
             metadata
-        ) as PaymentSelection.New.Card
+        ) as PaymentSelection.New.GenericPaymentMethod
 
-        assertThat(cardPaymentSelection.brand.code).isEqualTo(cardBrand)
-        assertThat(cardPaymentSelection.customerRequestedSave).isEqualTo(customerRequestedSave)
-        assertThat(getNameFromParams(cardPaymentSelection.paymentMethodCreateParams)).isEqualTo(name)
+        assertThat(klarnaPaymentSelection.paymentMethodCreateParams.typeCode).isEqualTo(klarnaPaymentMethod.code)
+        assertThat(klarnaPaymentSelection.labelResource).isEqualTo(klarnaPaymentMethod.displayName.resolve(context))
+        assertThat(klarnaPaymentSelection.lightThemeIconUrl).isEqualTo(klarnaPaymentMethod.lightThemeIconUrl)
+        assertThat(klarnaPaymentSelection.darkThemeIconUrl).isEqualTo(klarnaPaymentMethod.darkThemeIconUrl)
+        assertThat(klarnaPaymentSelection.iconResource).isEqualTo(klarnaPaymentMethod.iconResource)
+        assertThat(getNameFromParams(klarnaPaymentSelection.paymentMethodCreateParams)).isEqualTo(name)
     }
 }
