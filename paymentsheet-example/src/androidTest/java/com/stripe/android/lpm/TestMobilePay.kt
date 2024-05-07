b6d1f3e2ae2252d6cefad5857921ba7e0d6a3b0b
Carlos M. <carlosmuvi@stripe.com>
2024-05-07 11:49:37 -0700

    ktlint fixes.

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestMobilePay.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestMobilePay.kt
@@ -2,7 +2,6 @@ package com.stripe.android.lpm
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BasePlaygroundTest
-import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.PaymentMethod.Type
 import com.stripe.android.paymentsheet.example.playground.settings.Country
 import com.stripe.android.paymentsheet.example.playground.settings.CountrySettingsDefinition
