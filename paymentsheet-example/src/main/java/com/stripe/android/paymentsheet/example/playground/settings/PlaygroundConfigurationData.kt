cbbbe944b84757a417bcae7b0160e4ecd0efccd9
Samer Alabi <samer@stripe.com>
2024-05-15 11:20:34 -0700

    Add `PlaygroundConfigurationData` for configuring known playground options.

---
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/PlaygroundConfigurationData.kt
@@ -0,0 +1,18 @@
+package com.stripe.android.paymentsheet.example.playground.settings
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PlaygroundConfigurationData(
+    val integrationType: IntegrationType = IntegrationType.PaymentSheet,
+) {
+    @Serializable
+    enum class IntegrationType {
+        @SerialName("paymentSheet")
+        PaymentSheet,
+
+        @SerialName("flowController")
+        FlowController,
+    }
+}
