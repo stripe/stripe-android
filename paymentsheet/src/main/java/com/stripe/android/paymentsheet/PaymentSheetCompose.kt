89874d49c17e5098cacb373e0c6c944a793ec9a0
Annabella Koch <amk@stripe.com>
2024-05-15 11:00:04 -0700

    revert unnecessary change

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentSheetCompose.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentSheetCompose.kt
@@ -105,7 +105,7 @@ private fun UpdateIntentConfirmationInterceptor(
 
 @Composable
 private fun UpdateExternalPaymentMethodConfirmHandler(
-    externalPaymentMethodConfirmHandler: ExternalPaymentMethodConfirmHandler?,
+    externalPaymentMethodConfirmHandler: ExternalPaymentMethodConfirmHandler,
 ) {
     LaunchedEffect(externalPaymentMethodConfirmHandler) {
         ExternalPaymentMethodInterceptor.externalPaymentMethodConfirmHandler = externalPaymentMethodConfirmHandler
