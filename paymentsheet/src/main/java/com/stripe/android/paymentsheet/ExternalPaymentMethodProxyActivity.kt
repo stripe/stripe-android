1210901abc9f2b1f2a6ba625d2d2db701a306518
Annabella Koch <amk@stripe.com>
2024-05-16 16:23:22 -0700

    finish EPM proxy activity if it's restarted

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/ExternalPaymentMethodProxyActivity.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ExternalPaymentMethodProxyActivity.kt
@@ -16,15 +16,18 @@ import com.stripe.android.model.PaymentMethod
  */
 internal class ExternalPaymentMethodProxyActivity : AppCompatActivity() {
 
+    private var hasStarted: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        savedInstanceState?.getBoolean(HAS_STARTED_CONFIRM_STATE_KEY)?.let { hasStarted = it }
 
         val type = intent.getStringExtra(EXTRA_EXTERNAL_PAYMENT_METHOD_TYPE)
 
         @Suppress("DEPRECATION")
         val billingDetails = intent.getParcelableExtra<PaymentMethod.BillingDetails>(EXTRA_BILLING_DETAILS)
 
-        if (type != null) {
+        if (type != null && !hasStarted) {
             ExternalPaymentMethodInterceptor.externalPaymentMethodConfirmHandler?.confirmExternalPaymentMethod(
                 type,
                 billingDetails ?: PaymentMethod.BillingDetails()
@@ -54,6 +57,15 @@ internal class ExternalPaymentMethodProxyActivity : AppCompatActivity() {
             return
         }
 
+        // This calls finish immediately, so whatever is listening for the result (PS or FC) has already received an
+        // event when we get here.
+        if (hasStarted && externalPaymentMethodResult == null) {
+            // When this activity is resumed but the external payment method result is not set, we should return to
+            // the PaymentSheet or FlowController activity to avoid hanging in a "processing" state.
+            finish()
+            return
+        }
+
         externalPaymentMethodResult?.let {
             when (it) {
                 is ExternalPaymentMethodResult.Completed -> setResult(ExternalPaymentMethodResult.Completed.RESULT_CODE)
@@ -68,7 +80,21 @@ internal class ExternalPaymentMethodProxyActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+
+        hasStarted = true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(HAS_STARTED_CONFIRM_STATE_KEY, true)
+
+        super.onSaveInstanceState(outState)
+    }
+
     internal companion object {
+        const val HAS_STARTED_CONFIRM_STATE_KEY = "has_started_confirm"
+
         const val EXTRA_EXTERNAL_PAYMENT_METHOD_TYPE = "external_payment_method_type"
         const val EXTRA_BILLING_DETAILS = "external_payment_method_billing_details"
     }
