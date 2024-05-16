ad2b57a2e75ec53af8f117fcefd672ebc3851ca0
Samer Alabi <samer@stripe.com>
2024-05-16 09:28:13 -0700

    Add `configure` method for building `CustomerSheet.Configuration` and `CustomerEphemeralKeyRequest` in `Playground`.

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/PlaygroundSettingDefinition.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/PlaygroundSettingDefinition.kt
@@ -1,8 +1,11 @@
 package com.stripe.android.paymentsheet.example.playground.settings
 
+import com.stripe.android.customersheet.CustomerSheet
+import com.stripe.android.customersheet.ExperimentalCustomerSheetApi
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.example.playground.PlaygroundState
 import com.stripe.android.paymentsheet.example.playground.model.CheckoutRequest
+import com.stripe.android.paymentsheet.example.playground.model.CustomerEphemeralKeyRequest
 
 internal interface PlaygroundSettingDefinition<T> {
     val defaultValue: T
@@ -15,9 +18,24 @@ internal interface PlaygroundSettingDefinition<T> {
     ) {
     }
 
+    @OptIn(ExperimentalCustomerSheetApi::class)
+    fun configure(
+        value: T,
+        configurationBuilder: CustomerSheet.Configuration.Builder,
+        playgroundState: PlaygroundState,
+        configurationData: CustomerSheetConfigurationData,
+    ) {
+    }
+
     fun configure(
         value: T,
         checkoutRequestBuilder: CheckoutRequest.Builder,
+    ) {
+    }
+
+    fun configure(
+        value: T,
+        customerEphemeralKeyRequestBuilder: CustomerEphemeralKeyRequest.Builder,
     ) {
     }
 
@@ -36,6 +54,27 @@ internal interface PlaygroundSettingDefinition<T> {
 
     data class PaymentSheetConfigurationData(
         private val configurationBuilder: PaymentSheet.Configuration.Builder,
+        var billingDetailsCollectionConfiguration: PaymentSheet.BillingDetailsCollectionConfiguration =
+            PaymentSheet.BillingDetailsCollectionConfiguration()
+    ) {
+        // Billing details is a nested configuration, but we have individual settings for it in the
+        // UI, this helper keeps all of the configurations, rather than just the most recent.
+        fun updateBillingDetails(
+            block: PaymentSheet.BillingDetailsCollectionConfiguration.() ->
+            PaymentSheet.BillingDetailsCollectionConfiguration
+        ) {
+            billingDetailsCollectionConfiguration.apply {
+                billingDetailsCollectionConfiguration = block()
+            }
+            configurationBuilder.billingDetailsCollectionConfiguration(
+                billingDetailsCollectionConfiguration
+            )
+        }
+    }
+
+    @OptIn(ExperimentalCustomerSheetApi::class)
+    data class CustomerSheetConfigurationData(
+        private val configurationBuilder: CustomerSheet.Configuration.Builder,
         var billingDetailsCollectionConfiguration: PaymentSheet.BillingDetailsCollectionConfiguration =
             PaymentSheet.BillingDetailsCollectionConfiguration()
     ) {
