ee96d1e528b241b5d4eca79fda764f4543d504d2
Annabella Koch <amk@stripe.com>
2024-05-22 14:48:28 -0700

    remove unused imports

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/NewPaymentMethodRowButton.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/NewPaymentMethodRowButton.kt
@@ -1,19 +1,13 @@
 package com.stripe.android.paymentsheet.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.stripe.android.lpmfoundations.luxe.SupportedPaymentMethod
 import com.stripe.android.uicore.image.StripeImageLoader
 import com.stripe.android.uicore.strings.resolve
-import com.stripe.android.uicore.stripeColors
 
 @Composable
 internal fun NewPaymentMethodRowButton(
