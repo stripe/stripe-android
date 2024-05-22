9e939d27884603e08ceb2243845790cbd0e82d3c
Annabella Koch <amk@stripe.com>
2024-05-22 11:55:45 -0700

    rebase

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/PaymentMethodRowButton.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/PaymentMethodRowButton.kt
@@ -1,22 +1,29 @@
 package com.stripe.android.paymentsheet.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.stripe.android.uicore.stripeColors
 
 @Composable
-internal fun PaymentMethodRowButton(
+fun PaymentMethodRowButton(
     isEnabled: Boolean,
     isSelected: Boolean,
     iconContent: @Composable RowScope.() -> Unit,
-    textContent: @Composable RowScope.() -> Unit,
+    title: String,
+    subtitle: String?,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     trailingContent: (@Composable RowScope.() -> Unit)? = null,
@@ -31,12 +38,36 @@ internal fun PaymentMethodRowButton(
         Row(horizontalArrangement = Arrangement.spacedBy(12.dp), verticalAlignment = Alignment.CenterVertically) {
             iconContent()
 
-            textContent()
+            TitleContent(title = title, subtitle = subtitle, isEnabled = isEnabled)
 
             if (trailingContent != null) {
                 Spacer(modifier = Modifier.weight(1f))
                 trailingContent()
             }
+        }
+    }
+}
+
+@Composable
+private fun TitleContent(title: String, subtitle: String?, isEnabled: Boolean,) {
+    val textColor = MaterialTheme.stripeColors.onComponent
+
+    Column {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.caption,
+            color = if (isEnabled) textColor else textColor.copy(alpha = 0.6f),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        if (subtitle != null) {
+            val subtitleTextColor = MaterialTheme.stripeColors.subtitle
+            Text(
+                text = subtitle,
+                style = MaterialTheme.typography.subtitle1.copy(fontSize = 10.sp),
+                color = if (isEnabled) subtitleTextColor else subtitleTextColor.copy(alpha = 0.6f),
+            )
         }
     }
 }
--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/SavedPaymentMethodRowButton.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/SavedPaymentMethodRowButton.kt
@@ -1,7 +1,67 @@
 package com.stripe.android.paymentsheet.ui
 
+import android.content.res.Resources
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.stripe.android.model.CardBrand
+import com.stripe.android.model.PaymentMethod
+import com.stripe.android.paymentsheet.DisplayableSavedPaymentMethod
 
 @Composable
-fun SavedPaymentMethodRowButton() {
+fun SavedPaymentMethodRowButton(
+    displayableSavedPaymentMethod: DisplayableSavedPaymentMethod,
+    resources: Resources?,
+    isEnabled: Boolean,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier,
+    trailingContent: (@Composable RowScope.() -> Unit)? = null,
+) {
+    val paymentMethodTitle =
+        resources?.let { displayableSavedPaymentMethod.paymentMethod.getLabel(resources) }
+            ?: displayableSavedPaymentMethod.displayName
+
+    PaymentMethodRowButton(
+        isEnabled = isEnabled,
+        isSelected = isSelected,
+        iconContent = {
+            PaymentMethodIconFromResource(
+                iconRes = displayableSavedPaymentMethod.paymentMethod.getSavedPaymentMethodIcon(forVerticalMode = true),
+                colorFilter = null,
+            )
+        },
+        title = paymentMethodTitle,
+        subtitle = null,
+        onClick = {},
+        modifier = modifier,
+        trailingContent = trailingContent,
+    )
+}
+
+@Preview
+@Composable
+fun PreviewCardSavedPaymentMethodRowButton() {
+    val cardSavedPaymentMethod = DisplayableSavedPaymentMethod(
+        displayName = "4242",
+        paymentMethod = PaymentMethod(
+            id = "001",
+            created = null,
+            liveMode = false,
+            code = PaymentMethod.Type.Card.code,
+            type = PaymentMethod.Type.Card,
+            card = PaymentMethod.Card(
+                brand = CardBrand.Visa,
+                last4 = "4242",
+            )
+        )
+    )
+
+    SavedPaymentMethodRowButton(
+        displayableSavedPaymentMethod = cardSavedPaymentMethod,
+        resources = LocalContext.current.resources,
+        isEnabled = true,
+        isSelected = true,
+    )
 }
