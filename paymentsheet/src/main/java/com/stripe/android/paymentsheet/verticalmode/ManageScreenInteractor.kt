3f88418edcd09eafa99c779bc3a1edcf03a307d2
Annabella Koch <amk@stripe.com>
2024-05-29 15:45:15 -0700

    make ManageScreenInteractor compile again

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/verticalmode/ManageScreenInteractor.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/verticalmode/ManageScreenInteractor.kt
@@ -1,7 +1,10 @@
 package com.stripe.android.paymentsheet.verticalmode
 
+import com.stripe.android.model.PaymentMethod
+import com.stripe.android.model.PaymentMethodCode
 import com.stripe.android.paymentsheet.DisplayableSavedPaymentMethod
 import com.stripe.android.paymentsheet.viewmodels.BaseSheetViewModel
+import com.stripe.android.ui.core.cbc.CardBrandChoiceEligibility
 import com.stripe.android.uicore.utils.mapAsStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -9,12 +12,18 @@ internal interface ManageScreenInteractor {
     val state: StateFlow<State>
 
     data class State(
-        val paymentMethods: List<DisplayableSavedPaymentMethod>?,
+        val paymentMethods: List<PaymentMethod>?,
+        val nameProvider: (PaymentMethodCode?) -> String,
+        val isCbcEligible: Boolean,
     )
 }
 
 internal class DefaultManageScreenInteractor(viewModel: BaseSheetViewModel) : ManageScreenInteractor {
-    override val state = viewModel.displayableSavedPaymentMethods.mapAsStateFlow { paymentMethods ->
-        ManageScreenInteractor.State(paymentMethods)
+    override val state = viewModel.paymentMethods.mapAsStateFlow { paymentMethods ->
+        ManageScreenInteractor.State(
+            paymentMethods,
+            viewModel::providePaymentMethodName,
+            viewModel.paymentMethodMetadata.value?.cbcEligibility is CardBrandChoiceEligibility.Eligible,
+        )
     }
 }
