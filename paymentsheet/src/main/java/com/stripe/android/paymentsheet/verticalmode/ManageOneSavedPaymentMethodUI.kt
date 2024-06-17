f22a8495cd0f95032c5265d475e5e7aba5071637
Annabella Koch <amk@stripe.com>
2024-06-17 16:49:51 -0700

    Remove unnecessary Column

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/verticalmode/ManageOneSavedPaymentMethodUI.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/verticalmode/ManageOneSavedPaymentMethodUI.kt
@@ -1,13 +1,10 @@
 package com.stripe.android.paymentsheet.verticalmode
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.unit.dp
 import com.stripe.android.paymentsheet.R
 
 @Composable
@@ -18,26 +15,21 @@ internal fun ManageOneSavedPaymentMethodUI(interactor: ManageOneSavedPaymentMeth
 
     val paymentMethod = interactor.state.paymentMethod
 
-    Column(
-        modifier = Modifier
-            .padding(horizontal = horizontalPadding),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-    ) {
-        SavedPaymentMethodRowButton(
-            displayableSavedPaymentMethod = paymentMethod,
-            resources = LocalContext.current.resources,
-            isEnabled = true,
-            isSelected = true,
-            trailingContent = {
-                DeleteIcon(
-                    paymentMethod = paymentMethod,
-                    deletePaymentMethod = {
-                        interactor.handleViewAction(
-                            ManageOneSavedPaymentMethodInteractor.ViewAction.DeletePaymentMethod
-                        )
-                    }
-                )
-            }
-        )
-    }
+    SavedPaymentMethodRowButton(
+        displayableSavedPaymentMethod = paymentMethod,
+        resources = LocalContext.current.resources,
+        isEnabled = true,
+        isSelected = true,
+        trailingContent = {
+            DeleteIcon(
+                paymentMethod = paymentMethod,
+                deletePaymentMethod = {
+                    interactor.handleViewAction(
+                        ManageOneSavedPaymentMethodInteractor.ViewAction.DeletePaymentMethod
+                    )
+                }
+            )
+        },
+        modifier = Modifier.padding(horizontal = horizontalPadding)
+    )
 }
