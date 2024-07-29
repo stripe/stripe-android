e735e9a388b023154f0c47ec7e6658c888e49bc2
Annabella Koch <amk@stripe.com>
2024-07-29 16:05:48 -0700

    remove unneeded changes

--- scripts/deploy/deploy.rb
+++ scripts/deploy/deploy.rb
@@ -61,7 +61,6 @@ steps = [
     method(:validate_version_number),
     method(:ensure_clean_repo),
     method(:pull_latest),
-    method(:switch_to_release_branch),
 
     # Update version number
     method(:create_version_bump_pr),
@@ -70,7 +69,6 @@ steps = [
     method(:publish_to_sonatype),
 
     # Create a Github release
-
     method(:create_github_release),
 
     # Do docs updates
