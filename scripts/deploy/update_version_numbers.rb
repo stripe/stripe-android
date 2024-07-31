12077e01dedb5027c6f54b88474b2ac62b692b4c
Annabella Koch <amk@stripe.com>
2024-07-31 10:59:21 -0700

    Update CHANGELOG update step to be idempotent

--- scripts/deploy/update_version_numbers.rb
+++ scripts/deploy/update_version_numbers.rb
@@ -35,13 +35,17 @@ def update_version()
 end
 
 def update_changelog()
-    date = Time.now.strftime("%Y-%m-%d")
+    changelog_contents = File.read("CHANGELOG.md")
 
-    replace_in_file(
-        "CHANGELOG.md",
-        /## XX.XX.XX - 20XX-XX-XX/,
-        "## XX.XX.XX - 20XX-XX-XX\n\n## #{@version} - #{date}"
-    )
+    if (!changelog_contents.include? @version)
+        date = Time.now.strftime("%Y-%m-%d")
+
+        replace_in_file(
+            "CHANGELOG.md",
+            /## XX.XX.XX - 20XX-XX-XX/,
+            "## XX.XX.XX - 20XX-XX-XX\n\n## #{@version} - #{date}"
+        )
+    end
 
     execute_or_fail("git add CHANGELOG.md")
 end
