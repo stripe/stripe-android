3117bdc15ebc5670f9f6a9abbda8346006123419
Bella Koch <160939932+amk-stripe@users.noreply.github.com>
2024-08-08 17:02:37 -0700

    Update LPM tests to not use automatic payment methods (#9025)
    * Update LPM tests to not use automatic payment methods
    
    * Update payment sheet screenshot tests
    
    * Revert "Update payment sheet screenshot tests"
    
    This reverts commit 374d167e30f0d884ac4243ccc9b40fadc272d6a1.
    
    * Update TestFpx, TestTwint, and TestKlarna to use DPMs
    
    * Fix lint issues

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestWeChatPay.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestWeChatPay.kt
@@ -2,13 +2,14 @@ package com.stripe.android.lpm
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BasePlaygroundTest
-import com.stripe.android.paymentsheet.example.playground.settings.AutomaticPaymentMethodsSettingsDefinition
+import com.stripe.android.model.PaymentMethod
 import com.stripe.android.paymentsheet.example.playground.settings.Country
 import com.stripe.android.paymentsheet.example.playground.settings.CountrySettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.Currency
 import com.stripe.android.paymentsheet.example.playground.settings.CurrencySettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.CustomerSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.CustomerType
+import com.stripe.android.paymentsheet.example.playground.settings.SupportedPaymentMethodsSettingsDefinition
 import com.stripe.android.test.core.TestParameters
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,7 +20,10 @@ internal class TestWeChatPay : BasePlaygroundTest() {
         paymentMethodCode = "wechat_pay",
         requiresBrowser = false,
     ) { settings ->
-        settings[AutomaticPaymentMethodsSettingsDefinition] = true
+        settings[SupportedPaymentMethodsSettingsDefinition] = listOf(
+            PaymentMethod.Type.Card,
+            PaymentMethod.Type.WeChatPay
+        ).joinToString(",")
         settings[CountrySettingsDefinition] = Country.CN
         settings[CurrencySettingsDefinition] = Currency.EUR
         settings[CustomerSettingsDefinition] = CustomerType.GUEST
