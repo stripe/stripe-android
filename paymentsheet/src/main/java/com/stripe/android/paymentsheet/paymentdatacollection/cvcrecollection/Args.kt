f64276d474efb101dcc96768ee7eba5d8657d3e6
toluo-stripe <toluo@stripe.com>
2024-08-13 12:56:59 -0400

    Make files internal

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/cvcrecollection/Args.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/cvcrecollection/Args.kt
@@ -2,7 +2,7 @@ package com.stripe.android.paymentsheet.paymentdatacollection.cvcrecollection
 
 import com.stripe.android.model.CardBrand
 
-data class Args(
+internal data class Args(
     val lastFour: String,
     val cardBrand: CardBrand,
     val cvc: String? = null,
--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/cvcrecollection/CvcCompletionState.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/paymentdatacollection/cvcrecollection/CvcCompletionState.kt
@@ -1,8 +1,6 @@
 package com.stripe.android.paymentsheet.paymentdatacollection.cvcrecollection
 
-sealed interface CvcCompletionState {
+internal sealed interface CvcCompletionState {
     data object Incomplete : CvcCompletionState
     data class Completed(val cvc: String) : CvcCompletionState
 }
-
-data class CvcState(val cvc: String = "", val isComplete: Boolean = false)
