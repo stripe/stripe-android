2b830eae9e42eef689e9b56ecd1a9d38d0e0625c
Samer Alabi <samer@stripe.com>
2024-08-29 17:49:45 -0400

    Add `Google Pay` tests

---
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestGooglePay.kt
@@ -0,0 +1,45 @@
+package com.stripe.android.lpm
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.stripe.android.BasePlaygroundTest
+import com.stripe.android.paymentsheet.example.playground.settings.Country
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class TestGooglePay : BasePlaygroundTest() {
+    @Test
+    fun testUnitedStates() {
+        testDriver.confirmWithGooglePay(Country.US)
+    }
+
+    @Test
+    fun testFrance() {
+        testDriver.confirmWithGooglePay(Country.FR)
+    }
+
+    @Test
+    fun testGreatBritain() {
+        testDriver.confirmWithGooglePay(Country.GB)
+    }
+
+    @Test
+    fun testAustralia() {
+        testDriver.confirmWithGooglePay(Country.AU)
+    }
+
+    @Test
+    fun testBrazil() {
+        testDriver.confirmWithGooglePay(Country.BR)
+    }
+
+    @Test
+    fun testJapan() {
+        testDriver.confirmWithGooglePay(Country.JP)
+    }
+
+    @Test
+    fun testMexico() {
+        testDriver.confirmWithGooglePay(Country.MX)
+    }
+}
