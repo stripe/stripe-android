2a2b8df0582e6895a53289e30693daadc7d51ac6
Jay Newstrom <jaynewstrom@stripe.com>
2024-08-29 09:41:03 -0500

    Add screenshot tests for entire vertical mode form UI screen. (#9140)

---
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/analytics/NoOpEventReporter.kt
@@ -0,0 +1,99 @@
+package com.stripe.android.paymentsheet.analytics
+
+import com.stripe.android.model.CardBrand
+import com.stripe.android.model.LinkMode
+import com.stripe.android.model.PaymentMethodCode
+import com.stripe.android.paymentsheet.DeferredIntentConfirmationType
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.model.PaymentSelection
+
+@Suppress("EmptyFunctionBlock")
+internal class NoOpEventReporter : EventReporter {
+    override fun onInit(configuration: PaymentSheet.Configuration, isDeferred: Boolean) {
+    }
+
+    override fun onLoadStarted(initializedViaCompose: Boolean) {
+    }
+
+    override fun onLoadSucceeded(
+        paymentSelection: PaymentSelection?,
+        linkMode: LinkMode?,
+        googlePaySupported: Boolean,
+        currency: String?,
+        initializationMode: PaymentSheet.InitializationMode,
+        orderedLpms: List<String>
+    ) {
+    }
+
+    override fun onLoadFailed(error: Throwable) {
+    }
+
+    override fun onElementsSessionLoadFailed(error: Throwable) {
+    }
+
+    override fun onDismiss() {
+    }
+
+    override fun onShowExistingPaymentOptions() {
+    }
+
+    override fun onShowNewPaymentOptions() {
+    }
+
+    override fun onSelectPaymentMethod(code: PaymentMethodCode) {
+    }
+
+    override fun onPaymentMethodFormShown(code: PaymentMethodCode) {
+    }
+
+    override fun onPaymentMethodFormInteraction(code: PaymentMethodCode) {
+    }
+
+    override fun onCardNumberCompleted() {
+    }
+
+    override fun onSelectPaymentOption(paymentSelection: PaymentSelection) {
+    }
+
+    override fun onPressConfirmButton(paymentSelection: PaymentSelection?) {
+    }
+
+    override fun onPaymentSuccess(
+        paymentSelection: PaymentSelection?,
+        deferredIntentConfirmationType: DeferredIntentConfirmationType?
+    ) {
+    }
+
+    override fun onPaymentFailure(paymentSelection: PaymentSelection?, error: PaymentSheetConfirmationError) {
+    }
+
+    override fun onLpmSpecFailure(errorMessage: String?) {
+    }
+
+    override fun onAutofill(type: String) {
+    }
+
+    override fun onShowEditablePaymentOption() {
+    }
+
+    override fun onHideEditablePaymentOption() {
+    }
+
+    override fun onShowPaymentOptionBrands(source: EventReporter.CardBrandChoiceEventSource, selectedBrand: CardBrand) {
+    }
+
+    override fun onHidePaymentOptionBrands(
+        source: EventReporter.CardBrandChoiceEventSource,
+        selectedBrand: CardBrand?
+    ) {
+    }
+
+    override fun onUpdatePaymentMethodSucceeded(selectedBrand: CardBrand) {
+    }
+
+    override fun onUpdatePaymentMethodFailed(selectedBrand: CardBrand, error: Throwable) {
+    }
+
+    override fun onCannotProperlyReturnFromLinkAndOtherLPMs() {
+    }
+}
