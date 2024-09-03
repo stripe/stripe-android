f712e8ac009c68bc84c1e4d825b5cc5388de1e61
Jay Newstrom <jaynewstrom@stripe.com>
2024-09-03 07:44:40 -0600

    Add screenshots for select saved payment methods.

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/FakeSelectSavedPaymentMethodsInteractor.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/FakeSelectSavedPaymentMethodsInteractor.kt
@@ -1,18 +1,27 @@
 package com.stripe.android.paymentsheet
 
 import com.stripe.android.paymentsheet.ui.SelectSavedPaymentMethodsInteractor
+import com.stripe.android.uicore.utils.stateFlowOf
 import kotlinx.coroutines.flow.StateFlow
 
-internal object FakeSelectSavedPaymentMethodsInteractor : SelectSavedPaymentMethodsInteractor {
+internal class FakeSelectSavedPaymentMethodsInteractor(
+    initialState: SelectSavedPaymentMethodsInteractor.State = SelectSavedPaymentMethodsInteractor.State(
+        paymentOptionsItems = emptyList(),
+        selectedPaymentOptionsItem = null,
+        isEditing = false,
+        isProcessing = false,
+        canEdit = false,
+        canRemove = false,
+    ),
+    private val viewActionRecorder: ViewActionRecorder<SelectSavedPaymentMethodsInteractor.ViewAction> =
+        ViewActionRecorder(),
+) : SelectSavedPaymentMethodsInteractor {
     override val isLiveMode: Boolean = true
 
-    override val state: StateFlow<SelectSavedPaymentMethodsInteractor.State>
-        get() = throw NotImplementedError(
-            "State is not implemented for FakeSelectSavedPaymentMethodsInteractor"
-        )
+    override val state: StateFlow<SelectSavedPaymentMethodsInteractor.State> = stateFlowOf(initialState)
 
     override fun handleViewAction(viewAction: SelectSavedPaymentMethodsInteractor.ViewAction) {
-        // Do nothing.
+        viewActionRecorder.record(viewAction)
     }
 
     override fun close() {
