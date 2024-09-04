c2930649701d12eb2d8059a22eee523ddb489b5c
Carlos M <99293320+carlosmuvi-stripe@users.noreply.github.com>
2024-09-04 14:22:17 -0700

    [Connections] Playground - Adds Payment Element entry point (#9146)
    * Adds payment sheet to connections example.
    
    * Uses correct ephemeral key.
    
    * Deletes comment.
    
    * Updates check.
    
    * Refactors view effect.
    
    * Removes coroutine.
    
    * Fixes instant debits.
    
    * Removes stripe account id.
    
    * Regenerates dependencies.

--- financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/FinancialConnectionsPlaygroundViewModel.kt
+++ financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/FinancialConnectionsPlaygroundViewModel.kt
@@ -26,6 +26,7 @@ import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.StripeIntent
 import com.stripe.android.payments.bankaccount.navigation.CollectBankAccountForInstantDebitsResult
 import com.stripe.android.payments.bankaccount.navigation.CollectBankAccountResult
+import com.stripe.android.paymentsheet.PaymentSheetResult
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -87,6 +88,9 @@ internal class FinancialConnectionsPlaygroundViewModel(
             Experience.InstantDebits -> {
                 startWithPaymentIntent(this)
             }
+            Experience.PaymentElement -> {
+                startWithPaymentIntent(this)
+            }
         }
     }
 
@@ -115,6 +119,8 @@ internal class FinancialConnectionsPlaygroundViewModel(
                         FinancialConnectionsPlaygroundViewEffect.OpenForPaymentIntent(
                             paymentIntentSecret = it.intentSecret,
                             publishableKey = it.publishableKey,
+                            ephemeralKey = it.ephemeralKey,
+                            customerId = it.customerId,
                             experience = settings.get<ExperienceSetting>().selectedOption,
                         )
                     )
@@ -161,8 +167,8 @@ internal class FinancialConnectionsPlaygroundViewModel(
                     _viewEffect.emit(
                         FinancialConnectionsPlaygroundViewEffect.OpenForToken(
                             configuration = FinancialConnectionsSheet.Configuration(
-                                it.clientSecret,
-                                it.publishableKey
+                                financialConnectionsSessionClientSecret = it.clientSecret,
+                                publishableKey = it.publishableKey,
                             )
                         )
                     )
@@ -315,6 +321,31 @@ internal class FinancialConnectionsPlaygroundViewModel(
         }
     }
 
+    fun onPaymentSheetResult(result: PaymentSheetResult) {
+        when (result) {
+            is PaymentSheetResult.Canceled -> {
+                _state.update { it.copy(loading = false, status = it.status + "Cancelled!") }
+            }
+            is PaymentSheetResult.Completed -> {
+                _state.update {
+                    it.copy(
+                        status = it.status + listOf(
+                            "Elements Session Completed"
+                        )
+                    )
+                }
+            }
+            is PaymentSheetResult.Failed -> {
+                _state.update {
+                    it.copy(
+                        loading = false,
+                        status = it.status + "Failed! ${result.error}"
+                    )
+                }
+            }
+        }
+    }
+
     private suspend fun confirmIntentIfNeeded(
         intent: StripeIntent,
     ) {
@@ -412,6 +443,7 @@ enum class Experience(
 ) {
     FinancialConnections("Financial Connections"),
     InstantDebits("Instant Debits"),
+    PaymentElement("Payment Element")
 }
 
 enum class NativeOverride(val apiValue: String) {
@@ -433,6 +465,8 @@ sealed class FinancialConnectionsPlaygroundViewEffect {
 
     data class OpenForPaymentIntent(
         val paymentIntentSecret: String,
+        val ephemeralKey: String?,
+        val customerId: String?,
         val publishableKey: String,
         val experience: Experience,
     ) : FinancialConnectionsPlaygroundViewEffect()
