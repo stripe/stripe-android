add5c8621b9e295b0e0bdd381be90ab904528a98
Simon Duchastel <163092709+simond-stripe@users.noreply.github.com>
2024-09-09 09:10:50 -0700

    Set up Connect SDK Module (#9203)
    * Create skeleton of stripe-connect module
    
    * Add codeowners, add example, push
    
    * Update CODEOWNERS
    
    * Revert change to gradle properties (un-needed)
    
    * Remove last line
    
    * Add dependencies, fix lint
    
    * Fix lint, restrict to library, clarify comment
    
    * Generate new api file
    
    * Fix manifest

---
+++ stripe-connect/src/main/java/com/stripe/android/connectsdk/EmbeddedComponentManager.kt
@@ -0,0 +1,7 @@
+package com.stripe.android.connectsdk
+
+import androidx.annotation.RestrictTo
+
+@PrivateBetaConnectSDK
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+class EmbeddedComponentManager internal constructor()
