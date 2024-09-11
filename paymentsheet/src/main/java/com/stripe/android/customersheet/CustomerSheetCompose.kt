92258d8628c91826fa6a1a85afaf99fe33739317
Jay Newstrom <jaynewstrom@stripe.com>
2024-09-11 15:32:43 -0500

    Update customer sheet to use rememberUpdatedState. (#9239)

--- paymentsheet/src/main/java/com/stripe/android/customersheet/CustomerSheetCompose.kt
+++ paymentsheet/src/main/java/com/stripe/android/customersheet/CustomerSheetCompose.kt
@@ -2,7 +2,9 @@ package com.stripe.android.customersheet
 
 import androidx.activity.compose.LocalActivityResultRegistryOwner
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import com.stripe.android.utils.rememberActivity
 
@@ -31,18 +33,19 @@ fun rememberCustomerSheet(
         "CustomerSheet must be created in the context of an Activity"
     }
 
+    val currentCustomerAdapter by rememberUpdatedState(customerAdapter)
+    val currentCallback by rememberUpdatedState(callback)
+
     return remember(
         configuration,
-        customerAdapter,
-        callback,
     ) {
         CustomerSheet.getInstance(
             application = activity.application,
             lifecycleOwner = lifecycleOwner,
             activityResultRegistryOwner = activityResultRegistryOwner,
             configuration = configuration,
-            customerAdapter = customerAdapter,
-            callback = callback,
+            customerAdapter = currentCustomerAdapter,
+            callback = currentCallback,
             statusBarColor = { activity.window?.statusBarColor },
         )
     }
