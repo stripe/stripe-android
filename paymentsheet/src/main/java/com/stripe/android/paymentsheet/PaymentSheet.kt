ef5ba9107263ee8655374d8fdb375771fb072509
Jay Newstrom <jaynewstrom@stripe.com>
2024-09-12 15:20:02 -0600

    Expose vertical mode for private beta.

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentSheet.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentSheet.kt
@@ -852,6 +852,10 @@ class PaymentSheet internal constructor(
                 this.externalPaymentMethods = externalPaymentMethods
             }
 
+            /**
+             * The layout of payment methods in PaymentSheet. Defaults to [PaymentSheet.PaymentMethodLayout.Horizontal].
+             * @see [PaymentSheet.PaymentMethodLayout] for the list of available layouts.
+             */
             @ExperimentalPaymentMethodLayoutApi
             fun paymentMethodLayout(paymentMethodLayout: PaymentMethodLayout): Builder = apply {
                 this.paymentMethodLayout = paymentMethodLayout
@@ -885,8 +889,21 @@ class PaymentSheet internal constructor(
         }
     }
 
+    /**
+     * Defines the layout orientations available for displaying payment methods in PaymentSheet.
+     */
     enum class PaymentMethodLayout {
-        Horizontal, Vertical;
+        /**
+         * Payment methods are arranged horizontally.
+         * Users can swipe left or right to navigate through different payment methods.
+         */
+        Horizontal,
+
+        /**
+         * Payment methods are arranged vertically.
+         * Users can scroll up or down to navigate through different payment methods.
+         */
+        Vertical;
 
         internal companion object {
             internal val default: PaymentMethodLayout = Horizontal
