d6f81af76c53ffefd957175de9a97466227fd16a
Simon Duchastel <163092709+simond-stripe@users.noreply.github.com>
2024-09-24 10:48:32 -0700

    Update EmbeddedComponentService.kt

--- stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/networking/EmbeddedComponentService.kt
+++ stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/networking/EmbeddedComponentService.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.delay
 
 class EmbeddedComponentService {
     suspend fun fetchClientSecret(): String? {
+        // TODO MXMOBILE-2511 - add backend call
         delay(3000)
         return null
     }
