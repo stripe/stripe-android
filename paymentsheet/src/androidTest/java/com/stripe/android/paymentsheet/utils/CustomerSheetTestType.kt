273ec56a61c2cd30b1fb2d37c1ddc6323e5e2b1d
Jay Newstrom <jaynewstrom@stripe.com>
2024-09-25 11:53:45 -0600

    Fix build.

--- paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/utils/CustomerSheetTestType.kt
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/utils/CustomerSheetTestType.kt
@@ -1,14 +1,14 @@
 package com.stripe.android.paymentsheet.utils
 
-import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterValuesProvider
 
 internal enum class CustomerSheetTestType {
     AttachToCustomer,
     AttachToSetupIntent,
 }
 
-internal object CustomerSheetTestTypeProvider : TestParameter.TestParameterValuesProvider {
-    override fun provideValues(): List<CustomerSheetTestType> {
+internal object CustomerSheetTestTypeProvider : TestParameterValuesProvider() {
+    override fun provideValues(context: Context?): List<CustomerSheetTestType> {
         return CustomerSheetTestType.entries
     }
 }
--- paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/utils/ProductIntegrationType.kt
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/utils/ProductIntegrationType.kt
@@ -1,14 +1,14 @@
 package com.stripe.android.paymentsheet.utils
 
-import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterValuesProvider
 
 internal enum class ProductIntegrationType {
     PaymentSheet,
     FlowController,
 }
 
-internal object ProductIntegrationTypeProvider : TestParameter.TestParameterValuesProvider {
-    override fun provideValues(): List<ProductIntegrationType> {
+internal object ProductIntegrationTypeProvider : TestParameterValuesProvider() {
+    override fun provideValues(context: Context?): List<ProductIntegrationType> {
         return ProductIntegrationType.entries
     }
 }
