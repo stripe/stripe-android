0dedf1fdaa43f210583f3e3c88725d59f6d39b83
Jay Newstrom <jaynewstrom@stripe.com>
2024-09-25 11:02:25 -0500

    Revert "Add `CustomerSheetInitializationDataSource` (#9307)" (#9330)
    This reverts commit 890f99203e1f411cc67cb1ae3b269d8591cdf19e.

--- paymentsheet/src/main/java/com/stripe/android/customersheet/data/CustomerSheetInitializationDataSource.kt
+++
@@ -1,5 +0,0 @@
-package com.stripe.android.customersheet.data
-
-internal interface CustomerSheetInitializationDataSource {
-    suspend fun loadCustomerSheetSession(): CustomerSheetDataResult<CustomerSheetSession>
-}
--- paymentsheet/src/test/java/com/stripe/android/customersheet/utils/CustomerSheetHacksTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/customersheet/utils/CustomerSheetHacksTest.kt
@@ -1,9 +1,6 @@
 package com.stripe.android.customersheet.utils
 
-import android.app.Application
 import androidx.lifecycle.testing.TestLifecycleOwner
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.customersheet.ExperimentalCustomerSheetApi
 import com.stripe.android.customersheet.FakeCustomerAdapter
@@ -12,15 +9,11 @@ import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.withTimeoutOrNull
 import org.junit.After
-import org.junit.runner.RunWith
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalCustomerSheetApi::class)
-@RunWith(AndroidJUnit4::class)
 class CustomerSheetHacksTest {
-    private val application = ApplicationProvider.getApplicationContext<Application>()
-
     @After
     fun teardown() {
         CustomerSheetHacks.clear()
@@ -28,9 +21,8 @@ class CustomerSheetHacksTest {
 
     @Test
     fun `on initialize, should initialize all data sources as expected`() = runTest {
-        CustomerSheetHacks.initialize(application, TestLifecycleOwner(), FakeCustomerAdapter())
+        CustomerSheetHacks.initialize(TestLifecycleOwner(), FakeCustomerAdapter())
 
-        assertThat(CustomerSheetHacks.initializationDataSource.awaitWithTimeout()).isNotNull()
         assertThat(CustomerSheetHacks.intentDataSource.awaitWithTimeout()).isNotNull()
         assertThat(CustomerSheetHacks.paymentMethodDataSource.awaitWithTimeout()).isNotNull()
         assertThat(CustomerSheetHacks.savedSelectionDataSource.awaitWithTimeout()).isNotNull()
@@ -38,10 +30,9 @@ class CustomerSheetHacksTest {
 
     @Test
     fun `on clear, should clear all data sources as expected`() = runTest {
-        CustomerSheetHacks.initialize(application, TestLifecycleOwner(), FakeCustomerAdapter())
+        CustomerSheetHacks.initialize(TestLifecycleOwner(), FakeCustomerAdapter())
         CustomerSheetHacks.clear()
 
-        assertThat(CustomerSheetHacks.initializationDataSource.awaitWithTimeout()).isNull()
         assertThat(CustomerSheetHacks.intentDataSource.awaitWithTimeout()).isNull()
         assertThat(CustomerSheetHacks.paymentMethodDataSource.awaitWithTimeout()).isNull()
         assertThat(CustomerSheetHacks.savedSelectionDataSource.awaitWithTimeout()).isNull()
