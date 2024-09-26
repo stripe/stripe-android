b388616febe2642b328fc8da4a34e9711b925220
Annabella Koch <amk@stripe.com>
2024-09-26 15:51:23 -0700

    Update saved card description to use displayed brand

---
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/DisplayableSavedPaymentMethodTest.kt
@@ -0,0 +1,33 @@
+package com.stripe.android.paymentsheet
+
+import android.content.Context
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import com.google.common.truth.Truth.assertThat
+import com.stripe.android.core.strings.resolvableString
+import com.stripe.android.model.PaymentMethodFixtures
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import kotlin.test.Test
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.Q])
+class DisplayableSavedPaymentMethodTest {
+
+    val context: Context = getApplicationContext()
+
+    @Test
+    fun getDescription_usesDisplayedCardBrand() {
+        val visaCardUsingCartesBancaires = PaymentMethodFixtures.CARD_WITH_NETWORKS_PAYMENT_METHOD
+        val displayableSavedPaymentMethod = DisplayableSavedPaymentMethod(
+            displayName = "unused".resolvableString,
+            paymentMethod = visaCardUsingCartesBancaires
+        )
+
+        val description = displayableSavedPaymentMethod.getDescription().resolve(context)
+
+        assertThat(description).isEqualTo("Cartes Bancaires ending in 4242")
+    }
+
+}
