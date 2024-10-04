4774cb43ead9ebaa0646db6e79530ab922521243
Simon Duchastel <simond@stripe.com>
2024-10-04 13:25:48 -0400

    Fix lint

--- stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/networking/EmbeddedComponentService.kt
+++ stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/networking/EmbeddedComponentService.kt
@@ -5,7 +5,11 @@ import kotlinx.coroutines.delay
 class EmbeddedComponentService {
     suspend fun fetchClientSecret(): String? {
         // TODO MXMOBILE-2511 - add backend call
-        delay(3000)
+        delay(ARTIFICIAL_DELAY_MS)
         return null
+    }
+
+    companion object {
+        private const val ARTIFICIAL_DELAY_MS = 3000L
     }
 }
