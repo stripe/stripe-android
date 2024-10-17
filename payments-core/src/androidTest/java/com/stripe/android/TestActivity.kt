69d22fd46a0898cf08b6c65e24ba059159b9e550
Annabella Koch <amk@stripe.com>
2024-10-17 09:32:29 -0700

    Remove annotations

--- payments-core/src/androidTest/java/com/stripe/android/TestActivity.kt
+++ payments-core/src/androidTest/java/com/stripe/android/TestActivity.kt
@@ -1,9 +1,5 @@
 package com.stripe.android
 
-import androidx.annotation.RestrictTo
-import androidx.annotation.VisibleForTesting
 import androidx.fragment.app.FragmentActivity
 
-@VisibleForTesting
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 internal class TestActivity : FragmentActivity()
