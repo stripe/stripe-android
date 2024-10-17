90c4161f9b3f1313d8fe5c72b7d5c2b003be0817
Till Hellmund <tillh@stripe.com>
2024-10-17 16:31:48 -0400

    Pass billing address in `ElementsSessionContext`

--- financial-connections/src/test/java/com/stripe/android/financialconnections/repository/FinancialConnectionsConsumerSessionRepositoryImplTest.kt
+++ financial-connections/src/test/java/com/stripe/android/financialconnections/repository/FinancialConnectionsConsumerSessionRepositoryImplTest.kt
@@ -163,6 +163,7 @@ class FinancialConnectionsConsumerSessionRepositoryImplTest {
                 amount = 1234,
                 currency = "cad",
                 linkMode = LinkMode.LinkPaymentMethod,
+                billingAddress = null,
             )
         )
 
@@ -427,6 +428,7 @@ class FinancialConnectionsConsumerSessionRepositoryImplTest {
                 consumerSessionClientSecret = anyOrNull(),
                 paymentDetailsId = anyOrNull(),
                 expectedPaymentMethodType = anyOrNull(),
+                billingPhone = anyOrNull(),
                 requestSurface = anyOrNull(),
                 requestOptions = anyOrNull(),
                 extraParams = eq(fraudParams.params),
@@ -441,6 +443,7 @@ class FinancialConnectionsConsumerSessionRepositoryImplTest {
             consumerSessionClientSecret = consumerSessionClientSecret,
             paymentDetailsId = "pd_123",
             expectedPaymentMethodType = "card",
+            billingPhone = null,
         )
 
         verify(fraudDetectionDataRepository, never()).getLatest()
