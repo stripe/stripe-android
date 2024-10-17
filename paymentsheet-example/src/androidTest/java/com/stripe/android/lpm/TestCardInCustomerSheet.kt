0b239e43c449198843245e64904b485b977945cd
reuben-stripe <107846296+reuben-stripe@users.noreply.github.com>
2024-10-17 18:30:00 +0100

    Revert "Ignore 3ds2 tests. (#9455)" (#9466)
    This reverts commit 40d6017ef789a7d2b0af1f20557c07fccc160932.

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestCardInCustomerSheet.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestCardInCustomerSheet.kt
@@ -18,7 +18,6 @@ import com.stripe.android.paymentsheet.example.playground.settings.PaymentMethod
 import com.stripe.android.test.core.AuthorizeAction
 import com.stripe.android.test.core.FieldPopulator
 import com.stripe.android.test.core.TestParameters
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -72,7 +71,6 @@ internal class TestCardInCustomerSheet : BasePlaygroundTest() {
     }
 
     @Test
-    @Ignore
     fun testCardWith3ds2() {
         testDriver.savePaymentMethodInCustomerSheet(
             testParameters.copyPlaygroundSettings { settings ->
