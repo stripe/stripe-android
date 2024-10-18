eb45de74aa9b6fabd0b507d1f37a5fc5d86cef7b
Simon Duchastel <simond@stripe.com>
2024-10-18 19:20:34 -0400

    Add placeholder Fragment UI

--- connect/src/main/java/com/stripe/android/connect/AccountOnboardingFragment.kt
+++ connect/src/main/java/com/stripe/android/connect/AccountOnboardingFragment.kt
@@ -1,11 +1,19 @@
 package com.stripe.android.connect
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.RestrictTo
 import androidx.fragment.app.Fragment
 
 @PrivateBetaConnectSDK
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 class AccountOnboardingFragment internal constructor() : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.stripe_account_onboarding_fragment, container, false)
+    }
 
     companion object {
         @PrivateBetaConnectSDK
--- connect/src/main/java/com/stripe/android/connect/PayoutsFragment.kt
+++ connect/src/main/java/com/stripe/android/connect/PayoutsFragment.kt
@@ -1,11 +1,19 @@
 package com.stripe.android.connect
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.RestrictTo
 import androidx.fragment.app.Fragment
 
 @PrivateBetaConnectSDK
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 class PayoutsFragment internal constructor() : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.stripe_payouts_fragment, container, false)
+    }
 
     companion object {
         @PrivateBetaConnectSDK
