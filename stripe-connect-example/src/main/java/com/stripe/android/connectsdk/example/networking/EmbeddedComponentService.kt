6436e1f02532d9062cf52075d6e5496e2c26ab4e
Simon Duchastel <163092709+simond-stripe@users.noreply.github.com>
2024-10-18 08:56:03 -0700

    [Connect SDK] Integrate with backend calls for client secret (#9287)
    * Integrate with backend calls for client secret
    
    # Conflicts:
    #	dependencies.gradle
    #	stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/networking/EmbeddedComponentService.kt
    #	stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/ui/accountonboarding/AccountOnboardingExampleActivity.kt
    #	stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/ui/accountonboarding/AccountOnboardingExampleViewModel.kt
    #	stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/ui/payouts/PayoutsExampleViewModel.kt
    
    * Fix lint
    
    * add dependencies file
    
    * Move from timber to Logger
    
    * Remove unnecessary import
    
    * fix lint for deps
    
    * Fix deps
    
    * Fix colors, fix merge confligt
    
    * Fix icon lint

--- stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/networking/EmbeddedComponentService.kt
+++ stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/networking/EmbeddedComponentService.kt
@@ -1,15 +1,89 @@
 package com.stripe.android.connectsdk.example.networking
 
-import kotlinx.coroutines.delay
+import com.github.kittinunf.fuel.core.Deserializable
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.awaitResult
+import com.github.kittinunf.result.Result
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 class EmbeddedComponentService {
-    suspend fun fetchClientSecret(): String? {
-        // TODO MXMOBILE-2511 - add backend call
-        delay(ARTIFICIAL_DELAY_MS)
-        return null
+    private val fuel = FuelManager.instance
+        .apply {
+            // add logging
+            addRequestInterceptor(RequestLogger(tag = "EmbeddedComponentService"))
+            addResponseInterceptor(ResponseLogger(tag = "EmbeddedComponentService"))
+
+            // add headers
+            addRequestInterceptor(ApplicationJsonHeaderInterceptor)
+            addRequestInterceptor(UserAgentHeader)
+        }
+
+    /**
+     * Returns the publishable key for use in the Stripe Connect SDK as well as a list
+     * of available merchants. Throws a [FuelError] exception on network issues and other errors.
+     */
+    suspend fun getAccounts(): GetAccountsResponse {
+        return fuel.get(EXAMPLE_BACKEND_URL + "app_info")
+            .awaitModel(GetAccountsResponse.serializer())
+            .get()
+    }
+
+    /**
+     * Returns the client secret for the given merchant account to be used in the Stripe Connect SDK.
+     * Throws a [FuelError] exception on network issues and other errors.
+     */
+    suspend fun fetchClientSecret(account: String): String {
+        return fuel.post(EXAMPLE_BACKEND_URL + "account_session")
+            .header("account", account)
+            .awaitModel(FetchClientSecretResponse.serializer())
+            .get()
+            .clientSecret
     }
 
     companion object {
-        private const val ARTIFICIAL_DELAY_MS = 3000L
+        private const val EXAMPLE_BACKEND_URL = "https://stripe-connect-mobile-example-v1.glitch.me/"
     }
+}
+
+@Serializable
+data class FetchClientSecretResponse(
+    @SerialName("client_secret")
+    val clientSecret: String
+)
+
+@Serializable
+data class GetAccountsResponse(
+    @SerialName("publishable_key")
+    val publishableKey: String,
+    @SerialName("available_merchants")
+    val availableMerchants: List<Merchant>
+)
+
+@Serializable
+data class Merchant(
+    @SerialName("merchant_id")
+    val merchantId: String,
+    @SerialName("display_name")
+    val displayName: String
+)
+
+suspend fun <T : Any> Request.awaitModel(
+    serializer: DeserializationStrategy<T>
+): Result<T, FuelError> {
+    val deserializer = object : Deserializable<T> {
+
+        override fun deserialize(response: Response): T {
+            println(response.toString())
+            val body = response.body().asString("application/json")
+            return Json.decodeFromString(serializer, body)
+        }
+    }
+
+    return awaitResult(deserializer)
 }
---
+++ stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/ui/common/EmbeddedComponentsUi.kt
@@ -0,0 +1,139 @@
+package com.stripe.android.connectsdk.example.ui.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ExposedDropdownMenuBox
+import androidx.compose.material.ExposedDropdownMenuDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.stripe.android.connectsdk.example.networking.Merchant
+
+@Composable
+fun LaunchEmbeddedComponentsScreen(
+    embeddedComponentName: String,
+    selectedAccount: Merchant?,
+    connectSDKAccounts: List<Merchant>,
+    onConnectSDKAccountSelected: (Merchant) -> Unit,
+    onEmbeddedComponentLaunched: () -> Unit,
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            AccountSelector(
+                selectedAccount = selectedAccount,
+                accounts = connectSDKAccounts,
+                onAccountSelected = onConnectSDKAccountSelected,
+            )
+            Button(
+                onClick = onEmbeddedComponentLaunched,
+                enabled = selectedAccount != null,
+            ) {
+                Text("Launch $embeddedComponentName")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AccountSelector(
+    selectedAccount: Merchant? = null,
+    accounts: List<Merchant>,
+    onAccountSelected: (Merchant) -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = it },
+    ) {
+        TextField(
+            readOnly = true,
+            value = selectedAccount?.displayName ?: "No account selected",
+            onValueChange = {},
+            label = { Text("Account") },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            for (account in accounts) {
+                DropdownMenuItem(
+                    onClick = {
+                        onAccountSelected(account)
+                        expanded = false
+                    }
+                ) {
+                    Text(text = account.displayName,)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LaunchEmbeddedComponentsScreenPreviewWithSelectedAccount() {
+    LaunchEmbeddedComponentsScreen(
+        embeddedComponentName = "Payouts",
+        selectedAccount = Merchant(merchantId = "1", displayName = "Selected Merchant"),
+        connectSDKAccounts = listOf(
+            Merchant(merchantId = "2", displayName = "Merchant 1"),
+            Merchant(merchantId = "3", displayName = "Merchant 2"),
+            Merchant(merchantId = "4", displayName = "Merchant 3")
+        ),
+        onConnectSDKAccountSelected = {},
+        onEmbeddedComponentLaunched = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LaunchEmbeddedComponentsScreenPreviewWithNoSelectedAccount() {
+    LaunchEmbeddedComponentsScreen(
+        embeddedComponentName = "Payouts",
+        selectedAccount = null,
+        connectSDKAccounts = listOf(
+            Merchant(merchantId = "2", displayName = "Merchant 1"),
+            Merchant(merchantId = "3", displayName = "Merchant 2"),
+            Merchant(merchantId = "4", displayName = "Merchant 3")
+        ),
+        onConnectSDKAccountSelected = {},
+        onEmbeddedComponentLaunched = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LaunchEmbeddedComponentsScreenPreviewWithEmptyAccounts() {
+    LaunchEmbeddedComponentsScreen(
+        embeddedComponentName = "Payouts",
+        selectedAccount = Merchant(merchantId = "1", displayName = "Selected Merchant"),
+        connectSDKAccounts = emptyList(),
+        onConnectSDKAccountSelected = {},
+        onEmbeddedComponentLaunched = {}
+    )
+}
