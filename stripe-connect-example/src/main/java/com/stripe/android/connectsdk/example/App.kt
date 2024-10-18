cb70b1c4ec7b51aa361ebb3b89d1659f01d77518
Simon Duchastel <163092709+simond-stripe@users.noreply.github.com>
2024-10-18 08:56:03 -0700

    [Connect SDK] Integrate with backend calls for client secret (#9287)
    * Integrate with backend calls for client secret
    
    # Conflicts:
    #	dependencies.gradle
    #	stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/networking/EmbeddedComponentService.kt
    #	stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/ui/accountonboarding/AccountOnboardingExampleActivity.kt
    #	stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/ui/accountonboarding/AccountOnboardingExampleViewModel.kt
    #	stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/ui/payouts/PayoutsExampleViewModel.kt
    
    * Fix lint
    
    * add dependencies file
    
    * Move from timber to Logger
    
    * Remove unnecessary import
    
    * fix lint for deps
    
    * Fix deps
    
    * Fix colors, fix merge confligt
    
    * Fix icon lint

---
+++ stripe-connect-example/src/main/java/com/stripe/android/connectsdk/example/App.kt
@@ -0,0 +1,27 @@
+package com.stripe.android.connectsdk.example
+
+import android.app.Application
+import android.os.StrictMode
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .build()
+        )
+    }
+}
