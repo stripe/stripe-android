cdb260f6bca60daddfdc41f1457149960a7cd4e4
toluo-stripe <toluo@stripe.com>
2024-10-21 13:38:10 -0400

    native link dagger setup (#9465)

--- link/src/main/java/com/stripe/android/link/LinkActivity.kt
+++ link/src/main/java/com/stripe/android/link/LinkActivity.kt
@@ -1,29 +1,34 @@
 package com.stripe.android.link
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.annotation.VisibleForTesting
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.core.os.bundleOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.stripe.android.core.Logger
 import com.stripe.android.link.ui.cardedit.CardEditScreen
 import com.stripe.android.link.ui.paymentmenthod.PaymentMethodScreen
 import com.stripe.android.link.ui.signup.SignUpScreen
 import com.stripe.android.link.ui.verification.VerificationScreen
 import com.stripe.android.link.ui.wallet.WalletScreen
+import com.stripe.android.ui.core.CircularProgressIndicator
 
-internal class LinkActivity : AppCompatActivity() {
-    @VisibleForTesting
-    internal var viewModelFactory: ViewModelProvider.Factory = LinkActivityViewModel.Factory()
-    private val viewModel: LinkActivityViewModel by viewModels { viewModelFactory }
+internal class LinkActivity : ComponentActivity() {
+    internal var viewModel: LinkActivityViewModel? = null
 
     @VisibleForTesting
     internal lateinit var navController: NavHostController
@@ -31,18 +36,36 @@ internal class LinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        try {
+            viewModel = ViewModelProvider(this, LinkActivityViewModel.factory())[LinkActivityViewModel::class.java]
+        } catch (e: NoArgsException) {
+            Logger.getInstance(BuildConfig.DEBUG).error("Failed to create LinkActivityViewModel", e)
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+
         setContent {
             navController = rememberNavController()
 
             LaunchedEffect(Unit) {
-                viewModel.navController = navController
-                viewModel.dismissWithResult = ::dismissWithResult
+                viewModel?.navController = navController
+                viewModel?.dismissWithResult = ::dismissWithResult
             }
 
             NavHost(
                 navController = navController,
-                startDestination = LinkScreen.SignUp.route
+                startDestination = LinkScreen.Loading.route
             ) {
+                composable(LinkScreen.Loading.route) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+
                 composable(LinkScreen.SignUp.route) {
                     SignUpScreen()
                 }
@@ -79,6 +102,22 @@ internal class LinkActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        viewModel.unregisterActivity()
+        viewModel?.unregisterActivity()
+    }
+
+    companion object {
+        internal const val EXTRA_ARGS = "native_link_args"
+
+        internal fun createIntent(
+            context: Context,
+            args: NativeLinkArgs
+        ): Intent {
+            return Intent(context, LinkActivity::class.java)
+                .putExtra(EXTRA_ARGS, args)
+        }
+
+        internal fun getArgs(savedStateHandle: SavedStateHandle): NativeLinkArgs? {
+            return savedStateHandle.get<NativeLinkArgs>(EXTRA_ARGS)
+        }
     }
 }
