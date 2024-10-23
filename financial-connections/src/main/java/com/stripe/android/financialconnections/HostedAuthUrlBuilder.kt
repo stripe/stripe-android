98f590a9212ba1061f92944c272a14dc3ad4a098
Till Hellmund <tillh@stripe.com>
2024-10-23 15:28:39 -0400

    Set `billing_address` parameter in hosted auth URL

---
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/HostedAuthUrlBuilder.kt
@@ -0,0 +1,61 @@
+package com.stripe.android.financialconnections
+
+import com.stripe.android.core.networking.QueryStringFactory
+import com.stripe.android.financialconnections.FinancialConnectionsSheet.ElementsSessionContext.BillingDetails
+import com.stripe.android.financialconnections.FinancialConnectionsSheet.ElementsSessionContext.PrefillDetails
+import com.stripe.android.financialconnections.launcher.FinancialConnectionsSheetActivityArgs
+import com.stripe.android.financialconnections.model.FinancialConnectionsSessionManifest
+import com.stripe.android.financialconnections.utils.toApiParams
+import com.stripe.android.model.LinkMode
+
+internal object HostedAuthUrlBuilder {
+
+    fun create(
+        args: FinancialConnectionsSheetActivityArgs,
+        manifest: FinancialConnectionsSessionManifest,
+    ): String? {
+        return create(
+            hostedAuthUrl = manifest.hostedAuthUrl,
+            isInstantDebits = args is FinancialConnectionsSheetActivityArgs.ForInstantDebits,
+            linkMode = args.elementsSessionContext?.linkMode,
+            billingDetails = args.elementsSessionContext?.billingDetails,
+            prefillDetails = args.elementsSessionContext?.prefillDetails,
+        )
+    }
+
+    private fun create(
+        hostedAuthUrl: String?,
+        isInstantDebits: Boolean,
+        linkMode: LinkMode?,
+        billingDetails: BillingDetails?,
+        prefillDetails: PrefillDetails?,
+    ): String? {
+        if (hostedAuthUrl == null) {
+            return null
+        }
+
+        val queryParams = mutableListOf(hostedAuthUrl)
+        if (isInstantDebits) {
+            // For Instant Debits, add a query parameter to the hosted auth URL so that payment account creation
+            // takes place on the web side of the flow and the payment method ID is returned to the app.
+            queryParams.add("return_payment_method=true")
+            linkMode?.let { queryParams.add("link_mode=${it.value}") }
+            billingDetails?.let { queryParams.add(makeBillingDetailsQueryParams(it)) }
+        }
+
+        prefillDetails?.run {
+            email?.let { queryParams.add("email=$it") }
+            phone?.let { queryParams.add("linkMobilePhone=$it") }
+            phoneCountryCode?.let { queryParams.add("linkMobilePhoneCountry=$it") }
+        }
+
+        return queryParams.joinToString("&")
+    }
+
+    private fun makeBillingDetailsQueryParams(billingAddress: BillingDetails): String {
+        val params = mapOf(
+            "billing_details" to billingAddress.toApiParams(),
+        )
+        return QueryStringFactory.createFromParamsWithEmptyValues(params)
+    }
+}
