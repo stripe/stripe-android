a05aa3e7dd5c549305f4ea82664ac91b5363e1b9
Samer Alabi <samer@stripe.com>
2024-10-24 15:08:33 -0400

    Add tests for `Single` extensions

--- paymentsheet/src/main/java/com/stripe/android/common/coroutines/Single.kt
+++ paymentsheet/src/main/java/com/stripe/android/common/coroutines/Single.kt
@@ -3,9 +3,23 @@ package com.stripe.android.common.coroutines
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.time.Duration
 
 internal fun interface Single<T> {
     suspend fun await(): T
+}
+
+internal suspend fun <T> Single<T>.awaitWithTimeout(
+    timeout: Duration,
+    timeoutMessage: () -> String,
+): Result<T> {
+    val result = withTimeoutOrNull(timeout) { await() }
+    return if (result != null) {
+        Result.success(result)
+    } else {
+        Result.failure(IllegalStateException(timeoutMessage()))
+    }
 }
 
 internal fun <T> StateFlow<T?>.asSingle(): Single<T> {
