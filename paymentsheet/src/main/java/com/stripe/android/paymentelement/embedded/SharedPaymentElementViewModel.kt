18330612aa8e6924b631d3e8ff9768e99b225bda
Jay Newstrom <jaynewstrom@stripe.com>
2024-11-01 10:33:36 -0600

    Add configuration to embedded payment element.

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/SharedPaymentElementViewModel.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/SharedPaymentElementViewModel.kt
@@ -1,6 +1,7 @@
 package com.stripe.android.paymentelement.embedded
 
 import androidx.lifecycle.ViewModel
+import com.stripe.android.paymentelement.EmbeddedPaymentElement
 import com.stripe.android.paymentelement.EmbeddedPaymentElement.ConfigureResult
 import com.stripe.android.paymentelement.EmbeddedPaymentElement.PaymentOptionDisplayData
 import com.stripe.android.paymentelement.ExperimentalEmbeddedPaymentElementApi
@@ -14,7 +15,10 @@ internal class SharedPaymentElementViewModel : ViewModel() {
     private val _paymentOption: MutableStateFlow<PaymentOptionDisplayData?> = MutableStateFlow(null)
     val paymentOption: StateFlow<PaymentOptionDisplayData?> = _paymentOption.asStateFlow()
 
-    suspend fun configure(intentConfiguration: PaymentSheet.IntentConfiguration): ConfigureResult {
-        return ConfigureResult.Failed(IllegalStateException("Not implemented. $intentConfiguration"))
+    suspend fun configure(
+        intentConfiguration: PaymentSheet.IntentConfiguration,
+        configuration: EmbeddedPaymentElement.Configuration,
+    ): ConfigureResult {
+        return ConfigureResult.Failed(IllegalStateException("Not implemented. $intentConfiguration, $configuration"))
     }
 }
