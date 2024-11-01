704d802623cf1c83d75af6aabb8e85ad415a3a01
Carlos Muñoz <99293320+carlosmuvi-stripe@users.noreply.github.com>
2024-11-01 10:19:18 -0700

    [FC] Fixes single account skip account selection flows (#9540)
    * [skip ci] Start PR
    
    * Always caches selected accounts.
    
    * Updates tests.

--- financial-connections/src/test/java/com/stripe/android/financialconnections/features/accountpicker/AccountPickerViewModelTest.kt
+++ financial-connections/src/test/java/com/stripe/android/financialconnections/features/accountpicker/AccountPickerViewModelTest.kt
@@ -78,6 +78,7 @@ internal class AccountPickerViewModelTest {
             )
             givenPollAccountsReturns(
                 partnerAccountList().copy(
+                    data = listOf(partnerAccount()),
                     skipAccountSelection = true
                 )
             )
@@ -101,7 +102,8 @@ internal class AccountPickerViewModelTest {
 
         givenPollAccountsReturns(
             partnerAccountList().copy(
-                skipAccountSelection = null
+                data = listOf(partnerAccount()),
+                skipAccountSelection = true
             )
         )
 
@@ -348,7 +350,6 @@ internal class AccountPickerViewModelTest {
             selectAccounts(
                 selectedAccountIds = any(),
                 sessionId = any(),
-                updateLocalCache = any(),
             )
         ).thenReturn(response)
     }
