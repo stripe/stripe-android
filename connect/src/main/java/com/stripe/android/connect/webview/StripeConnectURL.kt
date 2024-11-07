5e0d150a175e481849a84668c64f2375bff2f1bc
Simon Duchastel <simond@stripe.com>
2024-11-07 17:08:35 -0500

    Update API, make internal objects internal

--- connect/src/main/java/com/stripe/android/connect/webview/StripeConnectURL.kt
+++ connect/src/main/java/com/stripe/android/connect/webview/StripeConnectURL.kt
@@ -1,13 +1,13 @@
 package com.stripe.android.connect.webview
 
-object StripeConnectURL {
-    fun getStripeURL(component: Component, publishableKey: String): String {
+internal object StripeConnectURL {
+    internal fun getStripeURL(component: Component, publishableKey: String): String {
         return "https://connect-js.stripe.com/v1.0/android_webview.html" +
             "#component=${component.urlComponent}" +
             "&publicKey=$publishableKey"
     }
 
-    enum class Component(val urlComponent: String) {
+    internal enum class Component(val urlComponent: String) {
         PAYOUTS("payouts"),
     }
 }
