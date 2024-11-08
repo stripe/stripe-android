2361fcf1916d28463d2ccea751cb673f2e391333
Simon Duchastel <simond@stripe.com>
2024-11-08 15:50:28 -0500

    Fix lint

--- connect-example/src/main/java/com/stripe/android/connect/example/data/EmbeddedComponentManagerWrapper.kt
+++ connect-example/src/main/java/com/stripe/android/connect/example/data/EmbeddedComponentManagerWrapper.kt
@@ -8,9 +8,7 @@ import com.stripe.android.core.BuildConfig
 import com.stripe.android.core.Logger
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import java.io.IOException
