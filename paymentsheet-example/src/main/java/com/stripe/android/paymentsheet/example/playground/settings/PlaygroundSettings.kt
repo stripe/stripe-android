5cd4782c587efd69b642af4cb420a9ce1b68cf29
Till Hellmund <tillh@stripe.com>
2024-11-13 11:30:46 -0500

    Add feature flag setting for IBP + DI (#9613)

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/PlaygroundSettings.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/PlaygroundSettings.kt
@@ -448,6 +448,7 @@ internal class PlaygroundSettings private constructor(
             LayoutSettingsDefinition,
             CardBrandAcceptanceSettingsDefinition,
             FeatureFlagSettingsDefinition(FeatureFlags.useNewUpdateCardScreen),
+            FeatureFlagSettingsDefinition(FeatureFlags.instantDebitsDeferredIntent),
         )
 
         private val nonUiSettingDefinitions: List<PlaygroundSettingDefinition<*>> = listOf(
