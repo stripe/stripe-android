23bd12922a61aca1d798864ef637e1debdd81169
Simon Duchastel <simond@stripe.com>
2024-11-15 14:41:09 -0500

    Remove unused import (lint)

--- connect/src/test/java/com/stripe/android/connect/webview/StripeConnectWebViewClientTest.kt
+++ connect/src/test/java/com/stripe/android/connect/webview/StripeConnectWebViewClientTest.kt
@@ -17,7 +17,6 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.isNull
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
