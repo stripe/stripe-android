8ee22b556f1571407213f747edc5de1e95176120
Simon Duchastel <simond@stripe.com>
2024-11-27 16:16:00 -0500

    Hook onReceivedError into listener

--- connect/src/main/java/com/stripe/android/connect/webview/StripeConnectWebViewContainer.kt
+++ connect/src/main/java/com/stripe/android/connect/webview/StripeConnectWebViewContainer.kt
@@ -3,10 +3,13 @@ package com.stripe.android.connect.webview
 import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.graphics.Bitmap
+import android.os.Build
 import android.view.LayoutInflater
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.FrameLayout
@@ -194,6 +197,25 @@ internal class StripeConnectWebViewContainerImpl<Listener : StripeEmbeddedCompon
         override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
             initJavascriptBridge(view)
             controller?.onPageStarted()
+        }
+
+        override fun onReceivedHttpError(
+            view: WebView,
+            request: WebResourceRequest,
+            errorResponse: WebResourceResponse
+        ) {
+            controller?.onReceivedError(request.url.toString(), errorResponse.statusCode, errorResponse.reasonPhrase)
+        }
+
+        override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
+            // for some reason errorCode and description are only available in API 23+,
+            // so we simply ignore the description for older devices
+            val errorMessage = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                error.description.toString()
+            } else {
+                null
+            }
+            controller?.onReceivedError(request.url.toString(), errorMessage = errorMessage)
         }
 
         private fun initJavascriptBridge(webView: WebView) {
--- connect/src/main/java/com/stripe/android/connect/webview/StripeConnectWebViewContainerController.kt
+++ connect/src/main/java/com/stripe/android/connect/webview/StripeConnectWebViewContainerController.kt
@@ -59,6 +59,20 @@ internal class StripeConnectWebViewContainerController<Listener : StripeEmbedded
         updateState { copy(isNativeLoadingIndicatorVisible = !receivedSetOnLoaderStart) }
     }
 
+    fun onReceivedError(requestUrl: String, httpStatusCode: Int? = null, errorMessage: String? = null) {
+        val errorString = buildString {
+            if (httpStatusCode != null) {
+                append("Received $httpStatusCode loading $requestUrl")
+            } else {
+                append("Received error loading $requestUrl")
+            }
+            if (errorMessage != null) {
+                append(": $errorMessage")
+            }
+        }
+        listener?.onLoadError(RuntimeException(errorString)) // TODO - wrap error better
+    }
+
     fun shouldOverrideUrlLoading(context: Context, request: WebResourceRequest): Boolean {
         val url = request.url
         return if (url.host?.lowercase() in ALLOWLISTED_HOSTS) {
@@ -135,7 +149,7 @@ internal class StripeConnectWebViewContainerController<Listener : StripeEmbedded
             }
             is SetOnLoadError -> {
                 // TODO - wrap error better
-                listener?.onLoadError(RuntimeException("${value.type}: ${value.message}"))
+                listener?.onLoadError(RuntimeException("${value.error.type}: ${value.error.message}"))
             }
             else -> {
                 with(listenerDelegate) {
