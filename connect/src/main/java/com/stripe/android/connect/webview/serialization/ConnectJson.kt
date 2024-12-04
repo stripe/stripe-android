71fa84e510793e74ae91a482b00346f5b618a9a3
Carlos Munoz <carlosmuvi@stripe.com>
2024-12-04 15:19:29 -0800

    Removes connect.

--- connect/src/main/java/com/stripe/android/connect/webview/serialization/ConnectJson.kt
+++
@@ -1,9 +0,0 @@
-package com.stripe.android.connect.webview.serialization
-
-import kotlinx.serialization.json.Json
-
-internal val ConnectJson: Json =
-    Json {
-        ignoreUnknownKeys = true
-        explicitNulls = false
-    }
