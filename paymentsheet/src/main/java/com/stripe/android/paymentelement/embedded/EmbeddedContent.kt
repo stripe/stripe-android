d7bac10691dda8352146982767c2bb6356d32555
Jay Newstrom <jaynewstrom@stripe.com>
2024-12-09 07:05:41 -0700

    Move embedded content into it's own composable.

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/EmbeddedContent.kt
@@ -0,0 +1,9 @@
+package com.stripe.android.paymentelement.embedded
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun EmbeddedContent() {
+    Text("Hello World!")
+}
