b0e99f5b549689445ddcfb047d8207dea026bcbc
Larry Ng <lng@stripe.com>
2024-12-12 10:31:34 -0500

    rm unused

--- connect-example/src/main/java/com/stripe/android/connect/example/ui/appearance/AppearanceView.kt
+++ connect-example/src/main/java/com/stripe/android/connect/example/ui/appearance/AppearanceView.kt
@@ -1,7 +1,6 @@
 package com.stripe.android.connect.example.ui.appearance
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
