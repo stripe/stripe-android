83a9e0b39b43bdc9280f2489bbf1f984c96beead
Annabella Koch <amk@stripe.com>
2024-12-12 12:45:15 -0800

    Fix tests

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/EditPage.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/EditPage.kt
@@ -7,6 +7,8 @@ import androidx.compose.ui.test.performClick
 import com.stripe.android.paymentsheet.ui.PAYMENT_SHEET_EDIT_SCREEN_REMOVE_BUTTON
 import com.stripe.android.paymentsheet.ui.TEST_TAG_EDIT_SCREEN_UPDATE_BUTTON
 import com.stripe.android.paymentsheet.ui.TEST_TAG_PAYMENT_SHEET_EDIT_SCREEN
+import com.stripe.android.paymentsheet.ui.UPDATE_PM_REMOVE_BUTTON_TEST_TAG
+import com.stripe.android.paymentsheet.ui.UPDATE_PM_SAVE_BUTTON_TEST_TAG
 import com.stripe.android.uicore.elements.DROPDOWN_MENU_CLICKABLE_TEST_TAG
 import com.stripe.android.uicore.elements.TEST_TAG_DROP_DOWN_CHOICE
 
@@ -43,11 +45,11 @@ internal class EditPage(
     }
 
     fun update() {
-        composeTestRule.onNodeWithTag(TEST_TAG_EDIT_SCREEN_UPDATE_BUTTON)
+        composeTestRule.onNodeWithTag(UPDATE_PM_SAVE_BUTTON_TEST_TAG)
             .performClick()
     }
 
     fun onRemoveButton(): SemanticsNodeInteraction {
-        return composeTestRule.onNodeWithTag(PAYMENT_SHEET_EDIT_SCREEN_REMOVE_BUTTON)
+        return composeTestRule.onNodeWithTag(UPDATE_PM_REMOVE_BUTTON_TEST_TAG)
     }
 }
