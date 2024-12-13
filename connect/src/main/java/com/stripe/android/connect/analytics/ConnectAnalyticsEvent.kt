3ac75ebbab16ef5f40f55a79f64f818f49544df7
Simon Duchastel <simond@stripe.com>
2024-12-12 19:44:25 -0500

    Add javadoc

--- connect/src/main/java/com/stripe/android/connect/analytics/ConnectAnalyticsEvent.kt
+++ connect/src/main/java/com/stripe/android/connect/analytics/ConnectAnalyticsEvent.kt
@@ -1,23 +1,23 @@
 package com.stripe.android.connect.analytics
 
+/**
+ * Analytics event for the Connect SDK. One subclass per unique analytics event is expected.
+ */
 internal sealed class ConnectAnalyticsEvent(
     val eventName: String,
     val params: Map<String, String?>? = null,
 ) {
-
-    data class PaneLaunched(
-        val pane: String,
-        val referrer: Int?
-    ) : ConnectAnalyticsEvent(
-        "pane.launched",
-        mapOf(
-            "referrer_pane" to "String",
-            "pane" to "String",
-        )
-    )
-
+    /**
+     * A component was instantiated via create{ComponentType}.
+     *
+     * The delta between this and component.viewed could tell us if apps are instantiating
+     * components but never rendering them on screen.
+     */
     data object ComponentCreated : ConnectAnalyticsEvent("component.created")
 
+    /**
+     * The component is viewed on screen (viewDidAppear lifecycle event on iOS)
+     */
     data class ComponentViewed(
         val pageViewId: String?
     ) : ConnectAnalyticsEvent(
@@ -25,6 +25,11 @@ internal sealed class ConnectAnalyticsEvent(
         mapOf("page_view_id" to pageViewId)
     )
 
+    /**
+     * The web page finished loading (didFinish navigation on iOS).
+     *
+     * Note: This should happen before component_loaded, so we won't yet have a page_view_id.
+     */
     data class WebPageLoaded(
         val timeToLoad: Double
     ) : ConnectAnalyticsEvent(
@@ -32,6 +37,10 @@ internal sealed class ConnectAnalyticsEvent(
         mapOf("time_to_load" to timeToLoad.toString())
     )
 
+    /**
+     * The component is successfully loaded within the web view. Triggered from componentDidLoad
+     * message handler from the web view.
+     */
     data class WebComponentLoaded(
         val pageViewId: String,
         val timeToLoad: Double,
@@ -45,6 +54,12 @@ internal sealed class ConnectAnalyticsEvent(
         )
     )
 
+    /**
+     * The SDK receives a non-200 status code loading the web view, other than "Internet connectivity" errors.
+     *
+     * Intent is to alert if the URL the mobile client expects is suddenly unreachable.
+     * The web view should always return a 200, even when displaying an error state.
+     */
     data class WebErrorPageLoad(
         val status: Int?,
         val error: String?,
@@ -58,17 +73,27 @@ internal sealed class ConnectAnalyticsEvent(
         )
     )
 
-    data class WebWarnUnexpectedLoad(
+    /**
+     * If the web view sends an onLoadError that can't be deserialized by the SDK.
+     */
+    data class WebWarnErrorUnexpectedLoad(
         val errorType: String,
         val pageViewId: String?
     ) : ConnectAnalyticsEvent(
-        "component.web.warn.unexpected_load_error_type",
+        "component.web.warnerror.unexpected_load_error_type",
         mapOf(
             "error_type" to errorType,
             "page_view_id" to pageViewId
         )
     )
 
+    /**
+     * If the web view calls onSetterFunctionCalled with a setter argument the SDK doesn't know how to handle.
+     *
+     * Note: It's expected to get this warning when web adds support for new setter functions not handled
+     * in older SDK versions. But logging it could help us debug issues where we expect the SDK to handle
+     * something it isn't.
+     */
     data class WebWarnUnrecognizedSetter(
         val setter: String,
         val pageViewId: String?
@@ -80,6 +105,9 @@ internal sealed class ConnectAnalyticsEvent(
         )
     )
 
+    /**
+     * An error occurred deserializing the JSON payload from a web message.
+     */
     data class WebErrorDeserializeMessage(
         val message: String,
         val error: String,
@@ -95,52 +123,68 @@ internal sealed class ConnectAnalyticsEvent(
         )
     )
 
-    data class SecureWebOpened(
+    /**
+     * A web view was opened when openWebView was called.
+     */
+    data class AuthenticatedWebOpened(
         val pageViewId: String?,
-        val authenticatedSecureViewId: String
+        val authenticatedViewId: String
     ) : ConnectAnalyticsEvent(
-        "component.authenticated_websecureweb.opened",
+        "component.authenticated_web.opened",
         mapOf(
             "page_view_id" to pageViewId,
-            "authenticatedsecure_view_id" to authenticatedSecureViewId
+            "authenticated_view_id" to authenticatedViewId
         )
     )
 
-    data class SecureWebRedirected(
+    /**
+     * The web view successfully redirected back to the app.
+     */
+    data class AuthenticatedWebRedirected(
         val pageViewId: String?,
-        val authenticatedSecureViewId: String
+        val authenticatedViewId: String
     ) : ConnectAnalyticsEvent(
-        "component.authenticated_websecureweb.redirected",
+        "component.authenticated_web.redirected",
         mapOf(
             "page_view_id" to pageViewId,
-            "authenticatedsecure_view_id" to authenticatedSecureViewId
+            "authenticated_view_id" to authenticatedViewId
         )
     )
 
-    data class SecureWebCanceled(
+    /**
+     * The user closed the web view before getting redirected back to the app.
+     */
+    data class AuthenticatedWebCanceled(
         val pageViewId: String?,
-        val secureViewId: String
+        val viewId: String
     ) : ConnectAnalyticsEvent(
-        "component.authenticated_websecureweb.canceled",
+        "component.authenticated_web.canceled",
         mapOf(
             "page_view_id" to pageViewId,
-            "secure_view_id" to secureViewId
+            "view_id" to viewId
         )
     )
 
-    data class SecureWebError(
+    /**
+     * The web view threw an error and was not successfully redirected back to the app.
+     */
+    data class AuthenticatedWebError(
         val error: String,
         val pageViewId: String?,
-        val secureViewId: String
+        val viewId: String
     ) : ConnectAnalyticsEvent(
-        "component.authenticated_websecureweb.error",
+        "component.authenticated_web.error",
         mapOf(
             "error" to error,
             "page_view_id" to pageViewId,
-            "secure_view_id" to secureViewId
+            "view_id" to viewId
         )
     )
 
+    /**
+     * The web page navigated somewhere other than the component wrapper URL
+     * (e.g. https://connect-js.stripe.com/v1.0/ios-webview.html)
+     */
     data class WebErrorUnexpectedNavigation(
         val url: String
     ) : ConnectAnalyticsEvent(
@@ -148,6 +192,9 @@ internal sealed class ConnectAnalyticsEvent(
         mapOf("url" to url)
     )
 
+    /**
+     * Catch-all event for unexpected client-side errors.
+     */
     data class ClientError(
         val domain: String,
         val code: Int,
