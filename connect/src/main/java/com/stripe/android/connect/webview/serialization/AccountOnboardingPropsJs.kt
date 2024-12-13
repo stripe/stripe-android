fb7ddb026cd5fe4e6d3ff7234bfef544147f4afd
Larry Ng <lng@stripe.com>
2024-12-13 12:16:07 -0500

    new onboarding settings wiring

--- connect/src/main/java/com/stripe/android/connect/webview/serialization/AccountOnboardingPropsJs.kt
+++ connect/src/main/java/com/stripe/android/connect/webview/serialization/AccountOnboardingPropsJs.kt
@@ -9,7 +9,15 @@ internal data class AccountOnboardingPropsJs(
     val setFullTermsOfServiceUrl: String?,
     val setRecipientTermsOfServiceUrl: String?,
     val setPrivacyPolicyUrl: String?,
-)
+    val setSkipTermsOfServiceCollection: Boolean?,
+    val setCollectionOptions: CollectionOptionsJs?,
+) {
+    @Serializable
+    data class CollectionOptionsJs(
+        val fields: String?,
+        val futureRequirements: String?,
+    )
+}
 
 @OptIn(PrivateBetaConnectSDK::class)
 internal fun AccountOnboardingProps.toJs(): AccountOnboardingPropsJs {
@@ -17,5 +25,12 @@ internal fun AccountOnboardingProps.toJs(): AccountOnboardingPropsJs {
         setFullTermsOfServiceUrl = fullTermsOfServiceUrl,
         setPrivacyPolicyUrl = privacyPolicyUrl,
         setRecipientTermsOfServiceUrl = recipientTermsOfServiceUrl,
+        setSkipTermsOfServiceCollection = skipTermsOfServiceCollection,
+        setCollectionOptions = collectionOptions?.let {
+            AccountOnboardingPropsJs.CollectionOptionsJs(
+                fields = it.fields?.value,
+                futureRequirements = it.futureRequirements?.value,
+            )
+        }
     )
 }
