a42576916e5e8a417151852dabbb48d4789b09bf
Till Hellmund <tillh@stripe.com>
2024-12-16 23:17:54 +0100

    Remove unused `Serializable` annotation

--- payments-model/src/main/java/com/stripe/android/model/LinkConsumerIncentive.kt
+++ payments-model/src/main/java/com/stripe/android/model/LinkConsumerIncentive.kt
@@ -3,11 +3,9 @@ package com.stripe.android.model
 import androidx.annotation.RestrictTo
 import com.stripe.android.core.model.StripeModel
 import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.Serializable
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 @Parcelize
-@Serializable
 data class LinkConsumerIncentive(
     val incentiveParams: IncentiveParams,
     val incentiveDisplayText: String?,
@@ -15,7 +13,6 @@ data class LinkConsumerIncentive(
 
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     @Parcelize
-    @Serializable
     data class IncentiveParams(
         val paymentMethod: String,
     ) : StripeModel
