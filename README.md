3621f91387fb9e5536a417c8d032bb06f52a5a24
Annabella Koch <amk@stripe.com>
2025-01-02 11:55:41 -0800

    Bump version to 25.100.5

--- README.md
+++ README.md
@@ -67,7 +67,7 @@ Add `stripe-android` to your `build.gradle` dependencies.
 
 ```
 dependencies {
-    implementation 'com.stripe:stripe-android:21.3.0'
+    implementation 'com.stripe:stripe-android:25.100.5'
 }
 ```
 
--- stripe-core/src/main/java/com/stripe/android/core/version/StripeSdkVersion.kt
+++ stripe-core/src/main/java/com/stripe/android/core/version/StripeSdkVersion.kt
@@ -8,6 +8,6 @@ package com.stripe.android.core.version
  *
  */
 object StripeSdkVersion {
-    const val VERSION_NAME = "21.3.0"
+    const val VERSION_NAME = "25.100.5"
     const val VERSION: String = "AndroidBindings/$VERSION_NAME"
 }
