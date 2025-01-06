45e8eea9d5d0afdc83b557942d29be4f57163e2b
Samer Alabi <samer@stripe.com>
2025-01-06 14:26:20 -0500

    Bump version to 21.3.1

--- stripe-core/src/main/java/com/stripe/android/core/version/StripeSdkVersion.kt
+++ stripe-core/src/main/java/com/stripe/android/core/version/StripeSdkVersion.kt
@@ -8,6 +8,6 @@ package com.stripe.android.core.version
  *
  */
 object StripeSdkVersion {
-    const val VERSION_NAME = "21.3.0"
+    const val VERSION_NAME = "21.3.1"
     const val VERSION: String = "AndroidBindings/$VERSION_NAME"
 }
