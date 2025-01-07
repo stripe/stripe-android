c04623c8c16dac7aff478403de8f60c7f685f381
toluo-stripe <toluo@stripe.com>
2025-01-07 17:06:12 -0500

    Lint fix

--- paymentsheet/src/main/java/com/stripe/android/link/ui/menu/LinkMenu.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/menu/LinkMenu.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.stripe.android.link.theme.HorizontalPadding
 import com.stripe.android.link.theme.MinimumTouchTargetSize
