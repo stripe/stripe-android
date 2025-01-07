8a503d4e72126fc31a47685c7941c2e7d49048a5
toluo-stripe <toluo@stripe.com>
2025-01-07 17:35:36 -0500

    Restore Wallet Payment Method Menu (#9866)

---
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/menu/LinkMenuItem.kt
@@ -0,0 +1,14 @@
+package com.stripe.android.link.ui.menu
+
+import com.stripe.android.core.strings.ResolvableString
+
+/**
+ * An item to be displayed in a [LinkMenu].
+ *
+ * @property text The text content of the item
+ * @property isDestructive Whether this item should be rendered with the error text color
+ */
+internal interface LinkMenuItem {
+    val text: ResolvableString
+    val isDestructive: Boolean
+}
---
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/wallet/WalletPaymentMethodMenu.kt
@@ -0,0 +1,58 @@
+package com.stripe.android.link.ui.wallet
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.stripe.android.core.strings.resolvableString
+import com.stripe.android.link.ui.menu.LinkMenu
+import com.stripe.android.model.ConsumerPaymentDetails
+import com.stripe.android.paymentsheet.R
+
+@Composable
+internal fun WalletPaymentMethodMenu(
+    modifier: Modifier = Modifier,
+    paymentDetails: ConsumerPaymentDetails.PaymentDetails,
+    onEditClick: () -> Unit,
+    onSetDefaultClick: () -> Unit,
+    onRemoveClick: () -> Unit,
+    onCancelClick: () -> Unit
+) {
+    val items = remember(paymentDetails) {
+        buildList {
+            if (paymentDetails is ConsumerPaymentDetails.Card) {
+                add(WalletPaymentMethodMenuItem.EditCard)
+            }
+
+            if (!paymentDetails.isDefault) {
+                add(WalletPaymentMethodMenuItem.SetAsDefault)
+            }
+
+            add(
+                element = WalletPaymentMethodMenuItem.RemoveItem(
+                    text = paymentDetails.removeLabel.resolvableString
+                )
+            )
+            add(WalletPaymentMethodMenuItem.Cancel)
+        }
+    }
+
+    LinkMenu(
+        modifier = modifier,
+        items = items,
+        onItemPress = { item ->
+            when (item) {
+                is WalletPaymentMethodMenuItem.EditCard -> onEditClick()
+                is WalletPaymentMethodMenuItem.SetAsDefault -> onSetDefaultClick()
+                is WalletPaymentMethodMenuItem.RemoveItem -> onRemoveClick()
+                is WalletPaymentMethodMenuItem.Cancel -> onCancelClick()
+            }
+        }
+    )
+}
+
+private val ConsumerPaymentDetails.PaymentDetails.removeLabel
+    get() = when (this) {
+        is ConsumerPaymentDetails.Card,
+        is ConsumerPaymentDetails.Passthrough -> R.string.stripe_paymentsheet_remove_card
+        is ConsumerPaymentDetails.BankAccount -> R.string.stripe_wallet_remove_linked_account
+    }
