e2a4daff15f442d99bd6130c26e9c4b0187a151f
tianzhao-stripe <tianzhao@stripe.com>
2025-01-08 13:35:33 -0500

    payment option test

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/PaymentOptionTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/PaymentOptionTest.kt
@@ -3,6 +3,7 @@ package com.stripe.android.paymentsheet.ui
 import android.os.Build
 import androidx.compose.ui.test.assertContentDescriptionEquals
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -20,7 +21,7 @@ class PaymentOptionTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun `Turns card label into screen reader-friendly text`() {
+    fun `SavedPaymentMethodTab created corrected`() {
         val label = "Card ending in 4242"
 
         composeTestRule.setContent {
@@ -40,5 +41,70 @@ class PaymentOptionTest {
         composeTestRule
             .onNodeWithText(label)
             .assertContentDescriptionEquals("Card ending in 4 2 4 2 ")
+        composeTestRule.onNodeWithTag(TEST_TAG_DEFAULT_PAYMENT_METHOD_LABEL).assertDoesNotExist()
+    }
+
+
+    @Test
+    fun `SavedPaymentMethodTab created corrected when edited`() {
+        val label = "Card ending in 4242"
+
+        composeTestRule.setContent {
+            SavedPaymentMethodTab(
+                viewWidth = 100.dp,
+                isSelected = false,
+                shouldShowModifyBadge = true,
+                shouldShowDefaultBadge = false,
+                isEnabled = true,
+                iconRes = R.drawable.stripe_ic_paymentsheet_card_visa,
+                labelText = label,
+                description = label,
+                onItemSelectedListener = {},
+            )
+        }
+
+        composeTestRule.onNodeWithTag(TEST_TAG_DEFAULT_PAYMENT_METHOD_LABEL).assertDoesNotExist()
+    }
+
+    @Test
+    fun `SavedPaymentMethodTab created corrected when not edited and default`() {
+        val label = "Card ending in 4242"
+
+        composeTestRule.setContent {
+            SavedPaymentMethodTab(
+                viewWidth = 100.dp,
+                isSelected = false,
+                shouldShowModifyBadge = false,
+                shouldShowDefaultBadge = true,
+                isEnabled = true,
+                iconRes = R.drawable.stripe_ic_paymentsheet_card_visa,
+                labelText = label,
+                description = label,
+                onItemSelectedListener = {},
+            )
+        }
+
+        composeTestRule.onNodeWithTag(TEST_TAG_DEFAULT_PAYMENT_METHOD_LABEL).assertDoesNotExist()
+    }
+
+    @Test
+    fun `SavedPaymentMethodTab created corrected when edited and default`() {
+        val label = "Card ending in 4242"
+
+        composeTestRule.setContent {
+            SavedPaymentMethodTab(
+                viewWidth = 100.dp,
+                isSelected = false,
+                shouldShowModifyBadge = true,
+                shouldShowDefaultBadge = true,
+                isEnabled = true,
+                iconRes = R.drawable.stripe_ic_paymentsheet_card_visa,
+                labelText = label,
+                description = label,
+                onItemSelectedListener = {},
+            )
+        }
+
+        composeTestRule.onNodeWithTag(TEST_TAG_DEFAULT_PAYMENT_METHOD_LABEL).assertExists()
     }
 }
