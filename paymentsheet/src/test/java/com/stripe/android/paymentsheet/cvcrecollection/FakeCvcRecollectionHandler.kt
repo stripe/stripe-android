03c06d58e8571e28220bb85bf4cff26789c13ddd
Samer Alabi <samer@stripe.com>
2025-01-09 13:23:33 -0500

    Refactor `PaymentSelection` & nullability out of `CvcRecollectionHandler`.

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/cvcrecollection/FakeCvcRecollectionHandler.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/cvcrecollection/FakeCvcRecollectionHandler.kt
@@ -1,7 +1,8 @@
 package com.stripe.android.paymentsheet.cvcrecollection
 
+import com.stripe.android.model.PaymentMethod
+import com.stripe.android.model.PaymentMethodOptionsParams
 import com.stripe.android.model.StripeIntent
-import com.stripe.android.paymentsheet.model.PaymentSelection
 import com.stripe.android.paymentsheet.paymentdatacollection.cvcrecollection.CvcRecollectionData
 import com.stripe.android.paymentsheet.state.PaymentElementLoader
 
@@ -9,20 +10,19 @@ internal class FakeCvcRecollectionHandler : CvcRecollectionHandler {
     var cvcRecollectionEnabled = false
     var requiresCVCRecollection = false
 
-    override fun launch(paymentSelection: PaymentSelection?, launch: (CvcRecollectionData) -> Unit) {
-        val card = (paymentSelection as? PaymentSelection.Saved)?.paymentMethod?.card
-        CvcRecollectionData.fromPaymentSelection(card)?.let(launch)
+    override fun launch(paymentMethod: PaymentMethod, launch: (CvcRecollectionData) -> Unit) {
+        CvcRecollectionData.fromPaymentSelection(paymentMethod.card)?.let(launch)
     }
 
     override fun cvcRecollectionEnabled(
-        stripeIntent: StripeIntent?,
-        initializationMode: PaymentElementLoader.InitializationMode?
+        stripeIntent: StripeIntent,
+        initializationMode: PaymentElementLoader.InitializationMode
     ) = requiresCVCRecollection || cvcRecollectionEnabled
 
     override fun requiresCVCRecollection(
-        stripeIntent: StripeIntent?,
-        paymentSelection: PaymentSelection?,
-        initializationMode: PaymentElementLoader.InitializationMode?,
-        extraRequirements: () -> Boolean
-    ) = requiresCVCRecollection && extraRequirements()
+        stripeIntent: StripeIntent,
+        paymentMethod: PaymentMethod,
+        optionsParams: PaymentMethodOptionsParams?,
+        initializationMode: PaymentElementLoader.InitializationMode,
+    ) = requiresCVCRecollection
 }
