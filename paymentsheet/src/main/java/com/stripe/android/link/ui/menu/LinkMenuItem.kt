604608e7a1e4919c1f1e50bf3a71be30162e538c
toluo-stripe <toluo@stripe.com>
2025-01-13 13:16:15 -0500

    Fix kdoc

--- paymentsheet/src/main/java/com/stripe/android/link/ui/menu/LinkMenuItem.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/menu/LinkMenuItem.kt
@@ -6,7 +6,7 @@ import com.stripe.android.core.strings.ResolvableString
  * An item to be displayed in a [LinkMenu].
  *
  * @property text The text content of the item
- * @property testTag Allow tag to allow ui element to be found in tests
+ * @property testTag Test tag to allow ui element to be found in tests
  * @property isDestructive Whether this item should be rendered with the error text color
  */
 internal interface LinkMenuItem {
