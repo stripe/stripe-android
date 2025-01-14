02b8ec5c03bbc31a4ab9483cb6953e53d2bceca1
toluo-stripe <toluo@stripe.com>
2025-01-14 11:30:06 -0500

    Clean up fake

--- paymentsheet/src/test/java/com/stripe/android/link/ui/paymentmethod/PaymentMethodViewModelTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/link/ui/paymentmethod/PaymentMethodViewModelTest.kt
@@ -10,7 +10,7 @@ import com.stripe.android.link.ui.paymentmenthod.UpdateSelection
 import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.PaymentMethodCode
 import com.stripe.android.model.PaymentMethodFixtures
-import com.stripe.android.paymentsheet.FakeFormHelper
+import com.stripe.android.paymentsheet.FormHelper
 import com.stripe.android.paymentsheet.forms.FormFieldValues
 import com.stripe.android.paymentsheet.model.PaymentSelection
 import com.stripe.android.paymentsheet.paymentdatacollection.FormArguments
@@ -58,13 +58,7 @@ class PaymentMethodViewModelTest {
 
     @Test
     fun `test selection and button state gets updated when form values change`() = runTest {
-        var paramFormValues: FormFieldValues? = null
-        val formHelper = object : PaymentMethodFormHelper() {
-            override fun onFormFieldValuesChanged(formValues: FormFieldValues?, selectedPaymentMethodCode: String) {
-                paramFormValues = formValues
-                super.onFormFieldValuesChanged(formValues, selectedPaymentMethodCode)
-            }
-        }
+        val formHelper = PaymentMethodFormHelper()
         val formValues = FormFieldValues(
             userRequestedReuse = PaymentSelection.CustomerRequestedSave.NoRequest
         )
@@ -77,14 +71,19 @@ class PaymentMethodViewModelTest {
 
         assertThat(vm.state.value.paymentSelection).isEqualTo(PaymentMethodFixtures.CARD_PAYMENT_SELECTION)
         assertThat(vm.state.value.primaryButtonState).isEqualTo(PrimaryButtonState.Enabled)
-        assertThat(paramFormValues).isEqualTo(formValues)
+        assertThat(formHelper.onFormFieldValuesChangedCalls).containsExactly(
+            PaymentMethodFormHelper.OnFormFieldValuesChangedCall(
+                formValues = formValues,
+                selectedPaymentMethodCode = PaymentMethod.Type.Card.code
+            )
+        )
 
         formHelper.paymentSelection = null
         vm.formValuesChanged(null)
 
         assertThat(vm.state.value.paymentSelection).isNull()
         assertThat(vm.state.value.primaryButtonState).isEqualTo(PrimaryButtonState.Disabled)
-        assertThat(paramFormValues).isNull()
+        assertThat(formHelper.onFormFieldValuesChangedCalls.last().formValues).isNull()
     }
 
     private fun createViewModel(
@@ -100,12 +99,23 @@ class PaymentMethodViewModelTest {
     }
 }
 
-private open class PaymentMethodFormHelper : FakeFormHelper() {
+private class PaymentMethodFormHelper : FormHelper {
     var updateSelection: UpdateSelection? = null
     var paymentSelection: PaymentSelection? = PaymentMethodFixtures.CARD_PAYMENT_SELECTION
+    val onFormFieldValuesChangedCalls = arrayListOf<OnFormFieldValuesChangedCall>()
 
     override fun onFormFieldValuesChanged(formValues: FormFieldValues?, selectedPaymentMethodCode: String) {
+        onFormFieldValuesChangedCalls.add(
+            OnFormFieldValuesChangedCall(
+                formValues = formValues,
+                selectedPaymentMethodCode = selectedPaymentMethodCode
+            )
+        )
         updateSelection?.invoke(paymentSelection)
+    }
+
+    override fun requiresFormScreen(selectedPaymentMethodCode: String): Boolean {
+        TODO("Not yet implemented")
     }
 
     override fun formElementsForCode(code: String): List<FormElement> {
@@ -121,4 +131,9 @@ private open class PaymentMethodFormHelper : FakeFormHelper() {
         }
         return TestFactory.CARD_FORM_ARGS
     }
+
+    data class OnFormFieldValuesChangedCall(
+        val formValues: FormFieldValues?,
+        val selectedPaymentMethodCode: String
+    )
 }
