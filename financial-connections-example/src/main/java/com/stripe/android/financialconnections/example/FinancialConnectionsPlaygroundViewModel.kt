12c26c8851d0b6b57a3f055058d6a79b76da8c3e
Till Hellmund <tillh@stripe.com>
2025-01-24 11:41:33 -0500

    Fix Connect behavior in FC playground

--- financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/FinancialConnectionsPlaygroundViewModel.kt
+++ financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/FinancialConnectionsPlaygroundViewModel.kt
@@ -125,10 +125,14 @@ internal class FinancialConnectionsPlaygroundViewModel(
                             }
                         )
                     }
+
+                    val stripeAccount = settings.get<StripeAccountIdSetting>().selectedOption
+
                     _viewEffect.emit(
                         FinancialConnectionsPlaygroundViewEffect.OpenForPaymentIntent(
                             paymentIntentSecret = it.intentSecret,
                             publishableKey = it.publishableKey,
+                            stripeAccountId = stripeAccount.takeIf(String::isNotBlank),
                             ephemeralKey = it.ephemeralKey,
                             customerId = it.customerId,
                             elementsSessionContext = ElementsSessionContext(
@@ -506,6 +510,7 @@ sealed class FinancialConnectionsPlaygroundViewEffect {
         val ephemeralKey: String?,
         val customerId: String?,
         val publishableKey: String,
+        val stripeAccountId: String?,
         val experience: Experience,
         val integrationType: IntegrationType,
         val elementsSessionContext: ElementsSessionContext,
