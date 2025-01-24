a2a3721dded418d23f79f4f4a8ec4415e3354968
Nick Porter <porter@stripe.com>
2025-01-24 09:03:47 -0700

    Update E2E CBC test to assert disabled

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/VerticalModePaymentSheetActivityTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/VerticalModePaymentSheetActivityTest.kt
@@ -472,7 +472,7 @@ internal class VerticalModePaymentSheetActivityTest {
     }
 
     @Test
-    fun `Disallowed brands are hidden in the CBC dropdown`() = runTest(
+    fun `Disallowed brands are disabled in the CBC dropdown`() = runTest(
         cardBrandAcceptance = PaymentSheet.CardBrandAcceptance.disallowed(
             listOf(
 
@@ -502,7 +502,7 @@ internal class VerticalModePaymentSheetActivityTest {
         editPage.assertIsVisible()
 
         // Even though our card is co-branded, Visa should not show up in the dropdown as it is disallowed
-        editPage.assertNotInDropdown("Visa")
+        editPage.assertInDropdownButDisabled("Visa (not available)")
     }
 
     @OptIn(ExperimentalCardBrandFilteringApi::class)
