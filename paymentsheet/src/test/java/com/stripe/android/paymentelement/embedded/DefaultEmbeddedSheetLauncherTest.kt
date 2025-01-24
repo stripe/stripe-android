a6389e9598af24c90a8f7f0a93cb7aa277834535
Tyler Clawson <tjclawson@stripe.com>
2025-01-24 13:34:52 -0500

    Refactor to support DummyActivityResultCaller updates

--- paymentsheet/src/test/java/com/stripe/android/paymentelement/embedded/DefaultEmbeddedSheetLauncherTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentelement/embedded/DefaultEmbeddedSheetLauncherTest.kt
@@ -32,6 +32,10 @@ internal class DefaultEmbeddedSheetLauncherTest {
         sheetLauncher.launchForm(code, paymentMethodMetadata)
         val launchCall = dummyActivityResultCallerScenario.awaitLaunchCall()
         assertThat(launchCall).isEqualTo(expectedArgs)
+
+        // Required to consume all events from turbine
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
     }
 
     @Test
@@ -42,6 +46,9 @@ internal class DefaultEmbeddedSheetLauncherTest {
 
         callback.onActivityResult(result)
         assertThat(selectionHolder.selection.value).isEqualTo(selection)
+
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
     }
 
     @Test
@@ -51,6 +58,9 @@ internal class DefaultEmbeddedSheetLauncherTest {
 
         callback.onActivityResult(result)
         assertThat(selectionHolder.selection.value).isEqualTo(null)
+
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
     }
 
     @Test
@@ -63,6 +73,9 @@ internal class DefaultEmbeddedSheetLauncherTest {
         val launchCall = dummyActivityResultCallerScenario.awaitLaunchCall()
 
         assertThat(launchCall).isEqualTo(expectedArgs)
+
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
     }
 
     @Test
@@ -79,6 +92,9 @@ internal class DefaultEmbeddedSheetLauncherTest {
 
         assertThat(customerStateHolder.customer.value).isEqualTo(customerState)
         assertThat(selectionHolder.selection.value).isEqualTo(selection)
+
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
     }
 
     @Test
@@ -91,6 +107,9 @@ internal class DefaultEmbeddedSheetLauncherTest {
 
         assertThat(customerStateHolder.customer.value).isEqualTo(PaymentSheetFixtures.EMPTY_CUSTOMER_STATE)
         assertThat(selectionHolder.selection.value).isEqualTo(null)
+
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
     }
 
     @Test
@@ -100,16 +119,21 @@ internal class DefaultEmbeddedSheetLauncherTest {
         callback.onActivityResult(result)
 
         assertThat(customerStateHolder.customer.value).isEqualTo(PaymentSheetFixtures.EMPTY_CUSTOMER_STATE)
+
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
     }
 
     @Test
     fun `onDestroy unregisters launchers`() = testScenario {
         lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        val formUnregisterCall = dummyActivityResultCallerScenario.awaitUnregisterCall()
-        val manageUnregisterCall = dummyActivityResultCallerScenario.awaitUnregisterCall()
+        val formLauncher = dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        val manageLauncher = dummyActivityResultCallerScenario.awaitNextRegisteredLauncher()
+        val formUnregisteredLauncher = dummyActivityResultCallerScenario.awaitNextUnregisteredLauncher()
+        val manageUnregisteredLauncher = dummyActivityResultCallerScenario.awaitNextUnregisteredLauncher()
 
-        assertThat(formUnregisterCall).isNotNull()
-        assertThat(manageUnregisterCall).isNotNull()
+        assertThat(formUnregisteredLauncher).isEqualTo(formLauncher)
+        assertThat(manageUnregisteredLauncher).isEqualTo(manageLauncher)
     }
 
     private fun testScenario(
@@ -129,9 +153,6 @@ internal class DefaultEmbeddedSheetLauncherTest {
             )
             val formRegisterCall = awaitRegisterCall()
             val manageRegisterCall = awaitRegisterCall()
-
-            awaitNextRegisteredLauncher()
-            awaitNextRegisteredLauncher()
 
             assertThat(formRegisterCall).isNotNull()
             assertThat(manageRegisterCall).isNotNull()
