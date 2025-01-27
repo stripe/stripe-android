5f411cbfd658090a7ed6ed4088b10db99be41fb5
toluo-stripe <toluo@stripe.com>
2025-01-27 12:32:16 -0500

    Add attestation endpoints to link account manager

--- paymentsheet/src/test/java/com/stripe/android/link/repositories/FakeLinkRepository.kt
+++ paymentsheet/src/test/java/com/stripe/android/link/repositories/FakeLinkRepository.kt
@@ -1,8 +1,10 @@
 package com.stripe.android.link.repositories
 
+import app.cash.turbine.Turbine
 import com.stripe.android.link.TestFactory
 import com.stripe.android.model.ConsumerPaymentDetails
 import com.stripe.android.model.ConsumerPaymentDetailsUpdateParams
+import com.stripe.android.model.ConsumerSessionLookup
 import com.stripe.android.model.ConsumerSessionSignup
 import com.stripe.android.model.ConsumerSignUpConsentAction
 import com.stripe.android.model.EmailSource
@@ -14,6 +16,7 @@ internal open class FakeLinkRepository : LinkRepository {
     var lookupConsumerResult = Result.success(TestFactory.CONSUMER_SESSION_LOOKUP)
     var mobileLookupConsumerResult = Result.success(TestFactory.CONSUMER_SESSION_LOOKUP)
     var consumerSignUpResult = Result.success(TestFactory.CONSUMER_SESSION_SIGN_UP)
+    var mobileConsumerSignUpResult = Result.success(TestFactory.CONSUMER_SESSION_SIGN_UP)
     var createCardPaymentDetailsResult = Result.success(TestFactory.LINK_NEW_PAYMENT_DETAILS)
     var shareCardPaymentDetailsResult = Result.success(TestFactory.LINK_NEW_PAYMENT_DETAILS)
     var logOutResult = Result.success(TestFactory.CONSUMER_SESSION)
@@ -23,6 +26,9 @@ internal open class FakeLinkRepository : LinkRepository {
     var updatePaymentDetailsResult = Result.success(TestFactory.CONSUMER_PAYMENT_DETAILS)
     var deletePaymentDetailsResult = Result.success(Unit)
 
+    private val mobileLookupCalls = Turbine<MobileLookupCall>()
+    private val mobileSignUpCalls = Turbine<MobileSignUpCall>()
+
     override suspend fun lookupConsumer(email: String) = lookupConsumerResult
 
     override suspend fun mobileLookupConsumer(
@@ -31,7 +37,18 @@ internal open class FakeLinkRepository : LinkRepository {
         verificationToken: String,
         appId: String,
         sessionId: String
-    ) = mobileLookupConsumerResult
+    ): Result<ConsumerSessionLookup> {
+        mobileLookupCalls.add(
+            item = MobileLookupCall(
+                email = email,
+                emailSource = emailSource,
+                verificationToken = verificationToken,
+                appId = appId,
+                sessionId = sessionId
+            )
+        )
+        return mobileLookupConsumerResult
+    }
 
     override suspend fun consumerSignUp(
         email: String,
@@ -53,7 +70,21 @@ internal open class FakeLinkRepository : LinkRepository {
         verificationToken: String,
         appId: String
     ): Result<ConsumerSessionSignup> {
-        TODO("Not yet implemented")
+        mobileSignUpCalls.add(
+            item = MobileSignUpCall(
+                name = name,
+                email = email,
+                phoneNumber = phoneNumber,
+                country = country,
+                consentAction = consentAction,
+                amount = amount,
+                currency = currency,
+                incentiveEligibilitySession = incentiveEligibilitySession,
+                verificationToken = verificationToken,
+                appId = appId
+            )
+        )
+        return mobileConsumerSignUpResult
     }
 
     override suspend fun createCardPaymentDetails(
@@ -105,4 +136,38 @@ internal open class FakeLinkRepository : LinkRepository {
         consumerSessionClientSecret: String,
         consumerPublishableKey: String?
     ): Result<ConsumerPaymentDetails> = updatePaymentDetailsResult
+
+    suspend fun awaitMobileLookup(): MobileLookupCall {
+        return mobileLookupCalls.awaitItem()
+    }
+
+    suspend fun awaitMobileSignup(): MobileSignUpCall {
+        return mobileSignUpCalls.awaitItem()
+    }
+
+    fun ensureAllEventsConsumed() {
+        mobileSignUpCalls.ensureAllEventsConsumed()
+        mobileLookupCalls.ensureAllEventsConsumed()
+    }
+
+    data class MobileLookupCall(
+        val email: String,
+        val emailSource: EmailSource,
+        val verificationToken: String,
+        val appId: String,
+        val sessionId: String
+    )
+
+    data class MobileSignUpCall(
+        val name: String?,
+        val email: String,
+        val phoneNumber: String,
+        val country: String,
+        val consentAction: ConsumerSignUpConsentAction,
+        val amount: Long?,
+        val currency: String?,
+        val incentiveEligibilitySession: IncentiveEligibilitySession?,
+        val verificationToken: String,
+        val appId: String
+    )
 }
