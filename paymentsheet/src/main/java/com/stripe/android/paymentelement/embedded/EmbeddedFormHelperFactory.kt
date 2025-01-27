f1673e93352f38d364ea6f0126417531d9ea3308
tjclawson-stripe <163896025+tjclawson-stripe@users.noreply.github.com>
2025-01-27 11:40:00 -0500

    Add EmbeddedFormHelperFactory (#9990)
    * Add EmbeddedFormHelperFactory
    
    * Refactor EmbeddedFormHelperFactory to be injectable
    
    * Fix tests

---
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/EmbeddedFormHelperFactory.kt
@@ -0,0 +1,44 @@
+package com.stripe.android.paymentelement.embedded
+
+import com.stripe.android.cards.CardAccountRangeRepository
+import com.stripe.android.link.LinkConfigurationCoordinator
+import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
+import com.stripe.android.paymentsheet.DefaultFormHelper
+import com.stripe.android.paymentsheet.FormHelper
+import com.stripe.android.paymentsheet.LinkInlineHandler
+import com.stripe.android.paymentsheet.NewOrExternalPaymentSelection
+import com.stripe.android.paymentsheet.model.PaymentSelection
+import kotlinx.coroutines.CoroutineScope
+import javax.inject.Inject
+
+internal class EmbeddedFormHelperFactory @Inject constructor(
+    private val linkConfigurationCoordinator: LinkConfigurationCoordinator,
+    private val embeddedSelectionHolder: EmbeddedSelectionHolder,
+    private val cardAccountRangeRepositoryFactory: CardAccountRangeRepository.Factory,
+) {
+    fun create(
+        coroutineScope: CoroutineScope,
+        paymentMethodMetadata: PaymentMethodMetadata,
+        selectionUpdater: (PaymentSelection?) -> Unit
+    ): FormHelper {
+        return DefaultFormHelper(
+            coroutineScope = coroutineScope,
+            linkInlineHandler = LinkInlineHandler.create(),
+            cardAccountRangeRepositoryFactory = cardAccountRangeRepositoryFactory,
+            paymentMethodMetadata = paymentMethodMetadata,
+            newPaymentSelectionProvider = {
+                when (val currentSelection = embeddedSelectionHolder.selection.value) {
+                    is PaymentSelection.ExternalPaymentMethod -> {
+                        NewOrExternalPaymentSelection.External(currentSelection)
+                    }
+                    is PaymentSelection.New -> {
+                        NewOrExternalPaymentSelection.New(currentSelection)
+                    }
+                    else -> null
+                }
+            },
+            selectionUpdater = selectionUpdater,
+            linkConfigurationCoordinator = linkConfigurationCoordinator,
+        )
+    }
+}
