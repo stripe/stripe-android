ddb722f4604c3876f7ab96f8243925f066f77921
Jay Newstrom <jaynewstrom@stripe.com>
2025-01-27 13:16:03 -0600

    Implement manage navigation. (#9991)

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/manage/ManageActivity.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/manage/ManageActivity.kt
@@ -1,14 +1,35 @@
 package com.stripe.android.paymentelement.embedded.manage
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import com.stripe.android.common.ui.BottomSheetScaffold
 import com.stripe.android.common.ui.ElementsBottomSheetLayout
+import com.stripe.android.paymentsheet.R
+import com.stripe.android.paymentsheet.ui.PaymentSheetTopBar
+import com.stripe.android.ui.core.elements.H4Text
 import com.stripe.android.uicore.StripeTheme
 import com.stripe.android.uicore.elements.bottomsheet.rememberStripeBottomSheetState
+import com.stripe.android.uicore.strings.resolve
+import com.stripe.android.uicore.utils.collectAsState
 import com.stripe.android.uicore.utils.fadeOut
 import kotlin.getValue
 
@@ -27,10 +48,13 @@ internal class ManageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val localArgs = args
-        if (localArgs == null) {
+        if (args == null) {
             finish()
             return
+        }
+
+        onBackPressedDispatcher.addCallback {
+            viewModel.manageNavigator.performAction(ManageNavigator.Action.Back)
         }
 
         setContent {
@@ -43,10 +67,69 @@ internal class ManageActivity : AppCompatActivity() {
                         finish()
                     }
                 ) {
-                    Text("Manage Screen")
+                    val screen by viewModel.manageNavigator.screen.collectAsState()
+                    Box(modifier = Modifier.padding(bottom = 20.dp)) {
+                        ScreenContent(viewModel.manageNavigator, screen)
+                    }
+                    LaunchedEffect(screen) {
+                        viewModel.manageNavigator.result.collect { result ->
+                            if (result.maintainPaymentSelection) {
+                                setManageResult(
+                                    ManageResult.Complete(
+                                        customerState = requireNotNull(viewModel.customerStateHolder.customer.value),
+                                        selection = viewModel.selectionHolder.selection.value,
+                                    )
+                                )
+                            } else {
+                                setManageResult(ManageResult.Cancelled(viewModel.customerStateHolder.customer.value))
+                            }
+                            finish()
+                        }
+                    }
                 }
             }
         }
+    }
+
+    @Composable
+    private fun ScreenContent(
+        navigator: ManageNavigator,
+        screen: ManageNavigator.Screen
+    ) {
+        val density = LocalDensity.current
+        var contentHeight by remember { mutableStateOf(0.dp) }
+        val scrollState = rememberScrollState()
+        BottomSheetScaffold(
+            topBar = {
+                val topBarState by screen.topBarState().collectAsState()
+                PaymentSheetTopBar(
+                    state = topBarState,
+                    canNavigateBack = navigator.canGoBack,
+                    isEnabled = true,
+                    handleBackPressed = { viewModel.manageNavigator.performAction(ManageNavigator.Action.Back) },
+                )
+            },
+            content = {
+                val horizontalPadding = dimensionResource(R.dimen.stripe_paymentsheet_outer_spacing_horizontal)
+                val headerText by screen.title().collectAsState()
+                headerText?.let { text ->
+                    H4Text(
+                        text = text.resolve(),
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                            .padding(horizontal = horizontalPadding),
+                    )
+                }
+
+                Box(modifier = Modifier.animateContentSize()) {
+                    screen.Content()
+                }
+            },
+            modifier = Modifier.onGloballyPositioned {
+                contentHeight = with(density) { it.size.height.toDp() }
+            },
+            scrollState = scrollState,
+        )
     }
 
     override fun finish() {
--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/manage/ManageComponent.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/manage/ManageComponent.kt
@@ -1,18 +1,25 @@
 package com.stripe.android.paymentelement.embedded.manage
 
+import android.content.Context
 import androidx.lifecycle.SavedStateHandle
+import com.stripe.android.core.injection.IOContext
+import com.stripe.android.core.injection.ViewModelScope
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
+import com.stripe.android.paymentelement.embedded.EmbeddedCommonModule
 import com.stripe.android.paymentelement.embedded.EmbeddedSelectionHolder
 import com.stripe.android.paymentsheet.CustomerStateHolder
 import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
 
 @Component(
     modules = [
         ManageModule::class,
+        EmbeddedCommonModule::class,
     ],
 )
 @Singleton
@@ -32,18 +39,35 @@ internal interface ManageComponent {
         @BindsInstance
         fun paymentMethodMetadata(paymentMethodMetadata: PaymentMethodMetadata): Builder
 
+        @BindsInstance
+        fun context(context: Context): Builder
+
         fun build(): ManageComponent
     }
 }
 
 @Module
-internal object ManageModule {
-    @Provides
-    @Singleton
-    fun providesCustomerStateHolder(
-        savedStateHandle: SavedStateHandle,
-        selectionHolder: EmbeddedSelectionHolder,
-    ): CustomerStateHolder {
-        return CustomerStateHolder(savedStateHandle, selectionHolder.selection)
+internal interface ManageModule {
+    companion object {
+        @Provides
+        @Singleton
+        fun provideManageNavigator(
+            @ViewModelScope viewModelScope: CoroutineScope,
+        ): ManageNavigator {
+            lateinit var manageNavigator: ManageNavigator
+            return ManageNavigator(
+                viewModelScope,
+                ManageNavigator.Screen.All {
+                    manageNavigator
+                }
+            ).also { manageNavigator = it }
+        }
+
+        @Provides
+        @Singleton
+        @ViewModelScope
+        fun provideViewModelScope(@IOContext ioContext: CoroutineContext): CoroutineScope {
+            return CoroutineScope(ioContext)
+        }
     }
 }
