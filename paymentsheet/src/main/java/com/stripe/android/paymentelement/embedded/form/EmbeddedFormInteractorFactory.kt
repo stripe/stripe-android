dfca811ca352dcddd14f7143d2c8d394253dbd5b
Tyler Clawson <tjclawson@stripe.com>
2025-01-28 11:50:25 -0500

    Inject all need dependencies to EmbeddedFormInteractorFactory

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/form/EmbeddedFormInteractorFactory.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/form/EmbeddedFormInteractorFactory.kt
@@ -3,35 +3,40 @@ package com.stripe.android.paymentelement.embedded.form
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
 import com.stripe.android.model.PaymentMethodCode
 import com.stripe.android.paymentelement.embedded.EmbeddedFormHelperFactory
+import com.stripe.android.paymentelement.embedded.EmbeddedSelectionHolder
 import com.stripe.android.payments.bankaccount.CollectBankAccountLauncher.Companion.HOSTED_SURFACE_PAYMENT_ELEMENT
-import com.stripe.android.paymentsheet.model.PaymentSelection
 import com.stripe.android.paymentsheet.paymentdatacollection.ach.USBankAccountFormArguments
 import com.stripe.android.paymentsheet.verticalmode.DefaultVerticalModeFormInteractor
 import com.stripe.android.paymentsheet.verticalmode.PaymentMethodIncentiveInteractor
 import com.stripe.android.uicore.utils.stateFlowOf
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Inject
 
 internal class EmbeddedFormInteractorFactory @Inject constructor(
-    private val embeddedFormHelperFactory: EmbeddedFormHelperFactory
+    private val paymentMethodMetadata: PaymentMethodMetadata,
+    private val paymentMethodCode: PaymentMethodCode,
+    private val embeddedSelectionHolder: EmbeddedSelectionHolder,
+    private val embeddedFormHelperFactory: EmbeddedFormHelperFactory,
 ) {
-    fun create(
-        coroutineScope: CoroutineScope,
-        paymentMethodCode: PaymentMethodCode,
-        paymentMethodMetadata: PaymentMethodMetadata,
-        setSelection: (PaymentSelection?) -> Unit,
-    ): DefaultVerticalModeFormInteractor {
+    fun create(): DefaultVerticalModeFormInteractor {
+        val coroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
         val formHelper = embeddedFormHelperFactory.create(
             coroutineScope = coroutineScope,
             paymentMethodMetadata = paymentMethodMetadata,
-            selectionUpdater = setSelection
+            selectionUpdater = {
+                embeddedSelectionHolder.set(it)
+            }
         )
 
         val usBankAccountFormArguments = USBankAccountFormArguments.create(
             paymentMethodMetadata = paymentMethodMetadata,
             selectedPaymentMethodCode = paymentMethodCode,
             hostedSurface = HOSTED_SURFACE_PAYMENT_ELEMENT,
-            setSelection = setSelection,
+            setSelection = {
+                embeddedSelectionHolder.set(it)
+            },
             onMandateTextChanged = { _, _ ->
             },
             onUpdatePrimaryButtonUIState = {
--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/form/FormActivityViewModel.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/form/FormActivityViewModel.kt
@@ -3,34 +3,17 @@ package com.stripe.android.paymentelement.embedded.form
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.stripe.android.core.utils.requireApplication
-import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
-import com.stripe.android.model.PaymentMethodCode
-import com.stripe.android.paymentelement.embedded.EmbeddedSelectionHolder
 import com.stripe.android.paymentsheet.analytics.EventReporter
-import com.stripe.android.paymentsheet.model.PaymentSelection
 import javax.inject.Inject
 
 internal class FormActivityViewModel @Inject constructor(
-    paymentMethodMetadata: PaymentMethodMetadata,
-    selectedPaymentMethodCode: PaymentMethodCode,
     embeddedFormInteractorFactory: EmbeddedFormInteractorFactory,
-    private val selectionHolder: EmbeddedSelectionHolder,
     val eventReporter: EventReporter
 ) : ViewModel() {
 
-    val formInteractor = embeddedFormInteractorFactory.create(
-        coroutineScope = viewModelScope,
-        paymentMethodCode = selectedPaymentMethodCode,
-        paymentMethodMetadata = paymentMethodMetadata,
-        setSelection = ::setSelection
-    )
-
-    private fun setSelection(selection: PaymentSelection?) {
-        selectionHolder.set(selection)
-    }
+    val formInteractor = embeddedFormInteractorFactory.create()
 
     class Factory(
         private val argSupplier: () -> FormContract.Args
