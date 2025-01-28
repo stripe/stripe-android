69fa683e9d481a1be3ee2526526bff46f5fa8d99
Carlos Munoz <carlosmuvi@stripe.com>
2025-01-28 13:44:08 -0800

    Adds comment.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/error/ErrorExt.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/error/ErrorExt.kt
@@ -4,6 +4,14 @@ import com.stripe.android.core.exception.APIException
 import com.stripe.android.financialconnections.FinancialConnectionsSheet.ElementsSessionContext.PrefillDetails
 import com.stripe.attestation.AttestationError
 
+/**
+ * Maps attestation related exceptions from the data layer (Integrity SDK, Stripe API) to a Financial Connections
+ * specific domain exception type, when applicable.
+ *
+ * @param sdkPrefillDetails the prefill details to be used when the attestation error triggers a switch from
+ * native to web flow. If the user entered email / phone on the native side before the exception occurred,
+ * we'll pass this information to the web flow so the user doesn't have to re-enter it.
+ */
 internal fun Throwable.toAttestationErrorIfApplicable(sdkPrefillDetails: PrefillDetails?): Throwable {
     return when {
         this is APIException && stripeError?.code == "link_failed_to_attest_request" -> FinancialConnectionsAttestationError(
