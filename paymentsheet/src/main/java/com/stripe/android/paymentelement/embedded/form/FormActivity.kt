3d419850f0e50174d0a7cdd81eb88107cafff812
Tyler Clawson <tjclawson@stripe.com>
2025-01-28 10:16:53 -0500

    Refactor to use EventReporterProvider

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/form/FormActivity.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/form/FormActivity.kt
@@ -6,13 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.CompositionLocalProvider
 import com.stripe.android.common.ui.ElementsBottomSheetLayout
+import com.stripe.android.paymentsheet.utils.EventReporterProvider
 import com.stripe.android.paymentsheet.verticalmode.VerticalModeFormUI
-import com.stripe.android.ui.core.elements.events.LocalCardBrandDisallowedReporter
-import com.stripe.android.ui.core.elements.events.LocalCardNumberCompletedEventReporter
 import com.stripe.android.uicore.StripeTheme
-import com.stripe.android.uicore.elements.LocalAutofillEventReporter
 import com.stripe.android.uicore.elements.bottomsheet.rememberStripeBottomSheetState
 import com.stripe.android.uicore.utils.fadeOut
 
@@ -51,11 +48,7 @@ internal class FormActivity : AppCompatActivity() {
                         finish()
                     }
                 ) {
-                    CompositionLocalProvider(
-                        LocalAutofillEventReporter provides viewModel.eventReporter::onAutofill,
-                        LocalCardNumberCompletedEventReporter provides viewModel.eventReporter::onCardNumberCompleted,
-                        LocalCardBrandDisallowedReporter provides viewModel.eventReporter::onDisallowedCardBrandEntered
-                    ) {
+                    EventReporterProvider(viewModel.eventReporter) {
                         VerticalModeFormUI(
                             interactor = viewModel.formInteractor,
                             showsWalletHeader = false
