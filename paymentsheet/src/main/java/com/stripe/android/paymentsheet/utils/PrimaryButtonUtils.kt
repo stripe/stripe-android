623e56798083efea52e148a4eafee243bee7cff8
Tyler Clawson <tjclawson@stripe.com>
2025-01-31 13:55:03 -0500

    Add tests

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/utils/PrimaryButtonUtils.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/utils/PrimaryButtonUtils.kt
@@ -4,23 +4,24 @@ import com.stripe.android.core.strings.ResolvableString
 import com.stripe.android.core.strings.resolvableString
 import com.stripe.android.paymentsheet.R
 import com.stripe.android.ui.core.Amount
+import com.stripe.android.ui.core.R as StripeUiCoreR
 
 internal fun buyButtonLabel(
     amount: Amount?,
     primaryButtonLabel: String?,
-    isProcessingPayment: Boolean
+    isForPaymentIntent: Boolean
 ): ResolvableString {
     return primaryButtonLabel?.resolvableString ?: run {
-        if (isProcessingPayment) {
+        if (isForPaymentIntent) {
             val fallback = R.string.stripe_paymentsheet_pay_button_label.resolvableString
             amount?.buildPayButtonLabel() ?: fallback
         } else {
-            com.stripe.android.ui.core.R.string.stripe_setup_button_label.resolvableString
+            StripeUiCoreR.string.stripe_setup_button_label.resolvableString
         }
     }
 }
 
 internal fun continueButtonLabel(primaryButtonLabel: String?): ResolvableString {
     return primaryButtonLabel?.resolvableString
-        ?: com.stripe.android.ui.core.R.string.stripe_continue_button_label.resolvableString
+        ?: StripeUiCoreR.string.stripe_continue_button_label.resolvableString
 }
