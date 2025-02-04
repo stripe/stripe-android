74136b7bff7c46a323aa4248e24dc50cbf9b2505
Carlos Munoz <carlosmuvi@stripe.com>
2025-02-04 15:54:20 -0800

    Adds lookup error handling

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/networkinglinkloginwarmup/NetworkingLinkLoginWarmupViewModel.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/networkinglinkloginwarmup/NetworkingLinkLoginWarmupViewModel.kt
@@ -86,6 +86,17 @@ internal class NetworkingLinkLoginWarmupViewModel @AssistedInject constructor(
                 )
             },
         )
+        onAsync(
+            NetworkingLinkLoginWarmupState::continueAsync,
+            onFail = { error ->
+                handleError(
+                    extraMessage = "Error looking up account",
+                    error = error,
+                    displayErrorScreen = true,
+                    pane = PANE
+                )
+            },
+        )
     }
 
     fun onContinueClick() {
