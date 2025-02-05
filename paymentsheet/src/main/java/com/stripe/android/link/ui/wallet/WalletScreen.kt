24cad74e737e230f5dedbcc55e8f2945a76497f6
toluo-stripe <toluo@stripe.com>
2025-02-05 10:52:46 -0500

    Disable edit card menu item

--- paymentsheet/src/main/java/com/stripe/android/link/ui/wallet/WalletScreen.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/wallet/WalletScreen.kt
@@ -84,7 +84,6 @@ internal fun WalletScreen(
         onPrimaryButtonClick = viewModel::onPrimaryButtonClicked,
         onPayAnotherWayClicked = viewModel::onPayAnotherWayClicked,
         onRemoveClicked = viewModel::onRemoveClicked,
-        onEditPaymentMethodClicked = viewModel::onEditPaymentMethodClicked,
         onSetDefaultClicked = viewModel::onSetDefaultClicked,
         showBottomSheetContent = showBottomSheetContent,
         hideBottomSheetContent = hideBottomSheetContent,
@@ -105,7 +104,6 @@ internal fun WalletBody(
     onPrimaryButtonClick: () -> Unit,
     onPayAnotherWayClicked: () -> Unit,
     onDismissAlert: () -> Unit,
-    onEditPaymentMethodClicked: (ConsumerPaymentDetails.PaymentDetails) -> Unit,
     onSetDefaultClicked: (ConsumerPaymentDetails.PaymentDetails) -> Unit,
     onRemoveClicked: (ConsumerPaymentDetails.PaymentDetails) -> Unit,
     showBottomSheetContent: (BottomSheetContent) -> Unit,
@@ -146,7 +144,6 @@ internal fun WalletBody(
             showBottomSheetContent = showBottomSheetContent,
             onRemoveClicked = onRemoveClicked,
             onSetDefaultClicked = onSetDefaultClicked,
-            onEditPaymentMethodClicked = onEditPaymentMethodClicked,
             onAddNewPaymentMethodClicked = onAddNewPaymentMethodClicked,
             hideBottomSheetContent = hideBottomSheetContent
         )
@@ -228,7 +225,6 @@ private fun PaymentMethodSection(
     onItemSelected: (ConsumerPaymentDetails.PaymentDetails) -> Unit,
     onExpandedChanged: (Boolean) -> Unit,
     onAddNewPaymentMethodClicked: () -> Unit,
-    onEditPaymentMethodClicked: (ConsumerPaymentDetails.PaymentDetails) -> Unit,
     onSetDefaultClicked: (ConsumerPaymentDetails.PaymentDetails) -> Unit,
     onRemoveClicked: (ConsumerPaymentDetails.PaymentDetails) -> Unit,
     showBottomSheetContent: (BottomSheetContent) -> Unit,
@@ -249,10 +245,6 @@ private fun PaymentMethodSection(
                             modifier = Modifier
                                 .testTag(WALLET_SCREEN_MENU_SHEET_TAG),
                             paymentDetails = it,
-                            onEditClick = {
-                                hideBottomSheetContent()
-                                onEditPaymentMethodClicked(it)
-                            },
                             onSetDefaultClick = {
                                 hideBottomSheetContent()
                                 onSetDefaultClicked(it)
