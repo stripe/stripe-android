988a138e1490557f50c5671e01ce1e42ab0774ec
Jay Newstrom <jaynewstrom@stripe.com>
2025-02-05 10:39:46 -0800

    Add checkout mode to embedded playground. (#10085)

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/embedded/EmbeddedPlaygroundActivity.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/embedded/EmbeddedPlaygroundActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,8 +36,12 @@ import com.stripe.android.paymentsheet.example.playground.PlaygroundState
 import com.stripe.android.paymentsheet.example.playground.PlaygroundTheme
 import com.stripe.android.paymentsheet.example.playground.activity.FawryActivity
 import com.stripe.android.paymentsheet.example.playground.network.PlaygroundRequester
+import com.stripe.android.paymentsheet.example.playground.settings.CheckoutMode
+import com.stripe.android.paymentsheet.example.playground.settings.CheckoutModeSettingsDefinition
+import com.stripe.android.paymentsheet.example.playground.settings.DropdownSetting
 import com.stripe.android.paymentsheet.example.playground.settings.EmbeddedViewDisplaysMandateSettingDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.PlaygroundConfigurationData
+import com.stripe.android.paymentsheet.example.playground.settings.PlaygroundSettings
 import com.stripe.android.paymentsheet.example.samples.ui.shared.BuyButton
 import com.stripe.android.paymentsheet.example.samples.ui.shared.PaymentMethodSelector
 import com.stripe.android.uicore.utils.collectAsState
@@ -45,29 +50,33 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalEmbeddedPaymentElementApi::class)
 internal class EmbeddedPlaygroundActivity : AppCompatActivity(), ExternalPaymentMethodConfirmHandler {
     companion object {
+        const val PLAYGROUND_STATE_KEY = "playgroundState"
+
         fun create(context: Context, playgroundState: PlaygroundState.Payment): Intent {
             return Intent(context, EmbeddedPlaygroundActivity::class.java).apply {
-                putExtra("playgroundState", playgroundState)
+                putExtra(PLAYGROUND_STATE_KEY, playgroundState)
             }
         }
     }
 
     private val viewModel: EmbeddedPlaygroundViewModel by viewModels()
+    private lateinit var playgroundState: PlaygroundState.Payment
+    private lateinit var playgroundSettings: PlaygroundSettings
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        @Suppress("DEPRECATION")
-        val playgroundState = intent.getParcelableExtra<PlaygroundState.Payment?>("playgroundState")
-        if (playgroundState == null ||
-            playgroundState.integrationType != PlaygroundConfigurationData.IntegrationType.Embedded
-        ) {
+        val initialPlaygroundState = getPlaygroundState(savedInstanceState).validate()
+        if (initialPlaygroundState == null) {
             finish()
             return
         }
+        this.playgroundState = initialPlaygroundState
+        this.playgroundSettings = initialPlaygroundState.snapshot.playgroundSettings()
 
         val embeddedBuilder = EmbeddedPaymentElement.Builder(
             createIntentCallback = { _, _ ->
+                val playgroundState = playgroundState
                 PlaygroundRequester(playgroundState.snapshot, applicationContext).fetch().fold(
                     onSuccess = { state ->
                         val clientSecret = requireNotNull(state.asPaymentState()).clientSecret
@@ -80,7 +89,8 @@ internal class EmbeddedPlaygroundActivity : AppCompatActivity(), ExternalPayment
             },
             resultCallback = ::handleEmbeddedResult,
         ).externalPaymentMethodConfirmHandler(this)
-        val embeddedViewDisplaysMandateText = playgroundState.snapshot[EmbeddedViewDisplaysMandateSettingDefinition]
+        val embeddedViewDisplaysMandateText =
+            initialPlaygroundState.snapshot[EmbeddedViewDisplaysMandateSettingDefinition]
         setContent {
             val embeddedPaymentElement = rememberEmbeddedPaymentElement(embeddedBuilder)
 
@@ -120,9 +130,40 @@ internal class EmbeddedPlaygroundActivity : AppCompatActivity(), ExternalPayment
                             embeddedViewDisplaysMandateText = embeddedViewDisplaysMandateText,
                         )
                     }
+
+                    ModeUi(::configure)
                 }
             )
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(PLAYGROUND_STATE_KEY, playgroundState)
+    }
+
+    @Composable
+    private fun ModeUi(configure: () -> Unit) {
+        val configurationData by playgroundSettings.configurationData.collectAsState()
+
+        val options = remember(configurationData) {
+            CheckoutModeSettingsDefinition.createOptions(configurationData)
+        }
+        var value by rememberSaveable {
+            mutableStateOf(playgroundSettings.snapshot()[CheckoutModeSettingsDefinition])
+        }
+
+        DropdownSetting<CheckoutMode>(
+            name = "Mode",
+            options = options,
+            value = value,
+            onOptionChanged = {
+                value = it
+                playgroundSettings[CheckoutModeSettingsDefinition] = it
+                playgroundState = playgroundState.copy(amount = 5099, snapshot = playgroundSettings.snapshot())
+                configure()
+            },
+        )
     }
 
     @Composable
@@ -191,6 +232,19 @@ internal class EmbeddedPlaygroundActivity : AppCompatActivity(), ExternalPayment
                 Toast.makeText(this, "Payment Failed.", Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun getPlaygroundState(savedInstanceState: Bundle?): PlaygroundState.Payment? {
+        return savedInstanceState?.getParcelable<PlaygroundState.Payment?>(PLAYGROUND_STATE_KEY)
+            ?: intent.getParcelableExtra<PlaygroundState.Payment?>(PLAYGROUND_STATE_KEY)
+    }
+
+    private fun PlaygroundState.Payment?.validate(): PlaygroundState.Payment? {
+        if (this == null || integrationType != PlaygroundConfigurationData.IntegrationType.Embedded) {
+            return null
+        }
+        return this
     }
 
     private enum class LoadingState {
