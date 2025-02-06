6bd0730444f0316e3a62f1e729468c24f23ef95e
tianzhao-stripe <tianzhao@stripe.com>
2025-02-06 13:40:04 -0800

    Revert "nullable pt 2"
    This reverts commit 58ed734cfe971fa6aab15bfe5a088c77124674f4.

--- paymentsheet/src/main/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/CardDefinition.kt
+++ paymentsheet/src/main/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/CardDefinition.kt
@@ -159,7 +159,7 @@ private object CardUiDefinitionFactory : UiDefinitionFactory.Simple {
             code = PaymentMethod.Type.Card.code,
             intent = metadata.stripeIntent,
             paymentMethodSaveConsentBehavior = metadata.paymentMethodSaveConsentBehavior,
-            hasCustomerConfiguration = metadata.customerMetadata?.hasCustomerConfiguration ?: false,
+            hasCustomerConfiguration = metadata.customerMetadata.hasCustomerConfiguration,
         )
     }
 }
