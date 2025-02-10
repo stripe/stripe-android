e8413c48829fc32f784f0f9b79f9612fb50c9e07
Till Hellmund <tillh@stripe.com>
2025-02-10 11:01:32 -0500

    Add end-to-end tests of bank payments in Link

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestLink.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestLink.kt
@@ -2,7 +2,14 @@ package com.stripe.android.lpm
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BasePlaygroundTest
+import com.stripe.android.paymentsheet.example.playground.settings.Country
+import com.stripe.android.paymentsheet.example.playground.settings.CountrySettingsDefinition
+import com.stripe.android.paymentsheet.example.playground.settings.DefaultBillingAddress
+import com.stripe.android.paymentsheet.example.playground.settings.DefaultBillingAddressSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.LinkSettingsDefinition
+import com.stripe.android.paymentsheet.example.playground.settings.LinkType
+import com.stripe.android.paymentsheet.example.playground.settings.LinkTypeSettingsDefinition
+import com.stripe.android.paymentsheet.example.playground.settings.SupportedPaymentMethodsSettingsDefinition
 import com.stripe.android.test.core.TestParameters
 import org.junit.Ignore
 import org.junit.Test
@@ -20,5 +27,28 @@ internal class TestLink : BasePlaygroundTest() {
     @Ignore("neutral-culminate")
     fun testLinkInlineCustom() {
         testDriver.testLinkCustom(linkNewUser)
+    }
+
+    @Test
+    fun testLinkPaymentWithBankAccountInPaymentMethodMode() {
+        val testParameters = makeLinkTestParameters(passthroughMode = false)
+        testDriver.confirmWithBankAccountInLink(testParameters)
+    }
+
+    @Test
+    fun testLinkPaymentWithBankAccountInPassthroughMode() {
+        val testParameters = makeLinkTestParameters(passthroughMode = true)
+        testDriver.confirmWithBankAccountInLink(testParameters)
+    }
+
+    private fun makeLinkTestParameters(passthroughMode: Boolean): TestParameters {
+        return TestParameters.create(
+            paymentMethodCode = "card",
+        ) { settings ->
+            settings[SupportedPaymentMethodsSettingsDefinition] = if (passthroughMode) "card" else "card,link"
+            settings[CountrySettingsDefinition] = Country.US
+            settings[LinkTypeSettingsDefinition] = LinkType.Native
+            settings[DefaultBillingAddressSettingsDefinition] = DefaultBillingAddress.On
+        }
     }
 }
