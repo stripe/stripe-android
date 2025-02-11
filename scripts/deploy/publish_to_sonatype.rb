8eedfeb33a9166ecfbece9fff6f415748515f8c0
Annabella Koch <amk@stripe.com>
2025-02-10 16:38:50 -0800

    Make sonatype step agnostic to local gpg location

--- scripts/deploy/publish_to_sonatype.rb
+++ scripts/deploy/publish_to_sonatype.rb
@@ -11,58 +11,77 @@ def publish_to_sonatype
     execute_or_fail("git checkout #{@deploy_branch}")
     execute_or_fail("git pull")
 
-    m2_settings = Nokogiri::XML(fetch_password("bindings/java-maven-api-token"))
+    set_gpg_location()
 
-    # Required in order to use xpath sanely (the commands below would
-    # otherwise come up with no results because the top level `<settings>`
-    # has an `xmlns`.
-    m2_settings.remove_namespaces!
+    begin
+        m2_settings = Nokogiri::XML(fetch_password("bindings/java-maven-api-token"))
 
-    # Note that we're looking for the "sonatype-nexus-staging" server here.
-    # Currently hard-coded to the second <server> element in the array but
-    # there's got to be a better way (I tried a few clever xpath tricks, but
-    # Nokogiri didn't like them one bit)..
-    server_element = m2_settings.xpath("//settings/servers/server")[1]
+        # Required in order to use xpath sanely (the commands below would
+        # otherwise come up with no results because the top level `<settings>`
+        # has an `xmlns`.
+        m2_settings.remove_namespaces!
 
-    nexus_user = server_element.xpath("//username").first.content
-    nexus_pass = server_element.xpath("//password").first.content
-    gnupg_key_id = fetch_password("bindings/gnupg/fingerprint").strip
+        # Note that we're looking for the "sonatype-nexus-staging" server here.
+        # Currently hard-coded to the second <server> element in the array but
+        # there's got to be a better way (I tried a few clever xpath tricks, but
+        # Nokogiri didn't like them one bit)..
+        server_element = m2_settings.xpath("//settings/servers/server")[1]
 
-    gradle_opts = \
-    "-Dorg.gradle.project.NEXUS_USERNAME=#{nexus_user} " \
-    "-Dorg.gradle.project.NEXUS_PASSWORD=#{nexus_pass} " \
-    "-Dorg.gradle.project.signing.gnupg.keyName=#{gnupg_key_id}"
+        nexus_user = server_element.xpath("//username").first.content
+        nexus_pass = server_element.xpath("//password").first.content
+        gnupg_key_id = fetch_password("bindings/gnupg/fingerprint").strip
 
-    gnupg_env do |env|
-        env.update(
-            "GRADLE_OPTS" => gradle_opts,
+        gradle_opts = \
+        "-Dorg.gradle.project.NEXUS_USERNAME=#{nexus_user} " \
+        "-Dorg.gradle.project.NEXUS_PASSWORD=#{nexus_pass} " \
+        "-Dorg.gradle.project.signing.gnupg.keyName=#{gnupg_key_id}"
 
-            # Ensure that /usr/bin is in the PATH. This is
-            # where the Java executable is located on a Mac
-            # OS machine.
-            "PATH" => path_with("/usr/bin"),
-        )
+        gnupg_env do |env|
+            env.update(
+                "GRADLE_OPTS" => gradle_opts,
 
-        # See https://github.com/gradle-nexus/publish-plugin for info on these gradle commands.
-        if (@is_dry_run)
-            publish_to_sonatype_commands = ['./gradlew', 'publishToSonatype', 'closeSonatypeStagingRepository', '--stacktrace']
-        else
-            publish_to_sonatype_commands = ['./gradlew', 'publishToSonatype', 'closeAndReleaseSonatypeStagingRepository', '--stacktrace']
-        end
+                # Ensure that /usr/bin is in the PATH. This is
+                # where the Java executable is located on a Mac
+                # OS machine.
+                "PATH" => path_with("/usr/bin"),
+            )
 
-        Subprocess.check_call(
-            publish_to_sonatype_commands,
-            env: env
-        )
+            # See https://github.com/gradle-nexus/publish-plugin for info on these gradle commands.
+            if (@is_dry_run)
+                publish_to_sonatype_commands = ['./gradlew', 'publishToSonatype', 'closeSonatypeStagingRepository', '--stacktrace']
+            else
+                publish_to_sonatype_commands = ['./gradlew', 'publishToSonatype', 'closeAndReleaseSonatypeStagingRepository', '--stacktrace']
+            end
 
-        puts "Release succeeded!"
+            Subprocess.check_call(
+                publish_to_sonatype_commands,
+                env: env
+            )
 
-        if (@is_dry_run)
-            rputs "At the open link, verify that a new release was added to the staging repo. You can log in to sonatype using your credentials found at `fetch-password bindings/java-maven-api-token`"
-            open_url("https://oss.sonatype.org/#stagingRepositories")
-            wait_for_user
+            puts "Release succeeded!"
+
+            if (@is_dry_run)
+                rputs "At the open link, verify that a new release was added to the staging repo. You can log in to sonatype using your credentials found at `fetch-password bindings/java-maven-api-token`"
+                open_url("https://oss.sonatype.org/#stagingRepositories")
+                wait_for_user
+            end
+        ensure
+            reset_gradle_properties()
         end
     end
+end
+
+private def set_gpg_location
+    begin
+        gpg_location = `which gpg`
+    rescue
+        rputs "Could not find gpg location via `which gpg`, do you have gpg installed?"
+    end
+    append_to_file("gradle.properties", "signing.gnupg.executable=#{gpg_location}")
+end
+
+private def reset_gradle_properties
+    execute_or_fail("git checkout origin/master gradle.properties")
 end
 
 # Gets the contents of the PATH environment variable, but before it does
