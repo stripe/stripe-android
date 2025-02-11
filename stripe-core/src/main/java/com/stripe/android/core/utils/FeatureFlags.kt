3fe0c674dae25349d548c362259d5c844e112966
toluo-stripe <toluo@stripe.com>
2025-02-11 16:16:55 -0500

    Releasee changes

--- stripe-core/src/main/java/com/stripe/android/core/utils/FeatureFlags.kt
+++ stripe-core/src/main/java/com/stripe/android/core/utils/FeatureFlags.kt
@@ -20,11 +20,7 @@ class FeatureFlag(
     private var overrideEnabledValue: Boolean? = null
 
     val isEnabled: Boolean
-        get() = if (BuildConfig.DEBUG) {
-            overrideEnabledValue ?: false
-        } else {
-            false
-        }
+        get() = overrideEnabledValue ?: false
 
     fun setEnabled(isEnabled: Boolean) {
         overrideEnabledValue = isEnabled
