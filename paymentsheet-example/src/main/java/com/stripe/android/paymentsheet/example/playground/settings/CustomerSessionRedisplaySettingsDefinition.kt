39b1793fadf0436f5a5c4fcd0bb942d9a712a230
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2025-02-14 10:13:46 -0800

    Fix `Customer Session` settings title for enabling/disabling SPM redisplay (#10167)

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/CustomerSessionRedisplaySettingsDefinition.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/CustomerSessionRedisplaySettingsDefinition.kt
@@ -5,7 +5,7 @@ import com.stripe.android.paymentsheet.example.playground.model.FeatureState
 
 internal object CustomerSessionRedisplaySettingsDefinition : BooleanSettingsDefinition(
     defaultValue = true,
-    displayName = "Customer Session Allow Redisplay",
+    displayName = "Customer Session Redisplay",
     key = "customer_session_payment_method_redisplay"
 ) {
     override fun applicable(configurationData: PlaygroundConfigurationData): Boolean {
