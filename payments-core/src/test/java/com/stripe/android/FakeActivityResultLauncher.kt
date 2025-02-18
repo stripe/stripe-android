7cba72468ce8cc08359f7c169c0fc8cccde6ac10
Jay Newstrom <jaynewstrom@stripe.com>
2025-02-18 09:44:14 -0700

    Update dependencies.

--- payments-core/src/test/java/com/stripe/android/FakeActivityResultLauncher.kt
+++ payments-core/src/test/java/com/stripe/android/FakeActivityResultLauncher.kt
@@ -5,7 +5,7 @@ import androidx.activity.result.contract.ActivityResultContract
 import androidx.core.app.ActivityOptionsCompat
 
 internal class FakeActivityResultLauncher<I>(
-    private val contract: ActivityResultContract<I, *>
+    override val contract: ActivityResultContract<I, *>
 ) : ActivityResultLauncher<I>() {
     val launchArgs = mutableListOf<I>()
     var unregisterInvocations = 0
@@ -19,9 +19,5 @@ internal class FakeActivityResultLauncher<I>(
 
     override fun unregister() {
         unregisterInvocations++
-    }
-
-    override fun getContract(): ActivityResultContract<I, *> {
-        return contract
     }
 }
