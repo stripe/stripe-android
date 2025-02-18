537c64466e253ba9c7daa1e1b0169dc0dba5da34
Annabella Koch <amk@stripe.com>
2025-02-17 16:37:24 -0800

    Update tests

--- paymentsheet/src/test/java/com/stripe/android/customersheet/data/CustomerSessionSavedSelectionDataSourceTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/customersheet/data/CustomerSessionSavedSelectionDataSourceTest.kt
@@ -2,6 +2,7 @@ package com.stripe.android.customersheet.data
 
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.isInstanceOf
+import com.stripe.android.model.PaymentMethodFixtures
 import com.stripe.android.paymentsheet.FakePrefsRepository
 import com.stripe.android.paymentsheet.PrefsRepository
 import com.stripe.android.paymentsheet.model.SavedSelection
@@ -110,18 +111,69 @@ class CustomerSessionSavedSelectionDataSourceTest {
     }
 
     @Test
-    fun `When default payment methods enabled, should get selection from backend`() {
-        // TODO: implement test.
+    fun `When default payment methods enabled, should get selection from backend`() = runTest {
+        val expectedSavedSelectionId = "pm_1"
+        val elementsSessionManager = FakeCustomerSessionElementsSessionManager(
+            isPaymentMethodSyncDefaultEnabled = true,
+            defaultPaymentMethodId = expectedSavedSelectionId,
+            paymentMethods = listOf(
+                PaymentMethodFixtures.CARD_PAYMENT_METHOD.copy(id = "pm_2"),
+                PaymentMethodFixtures.CARD_PAYMENT_METHOD.copy(id = expectedSavedSelectionId),
+            )
+        )
+
+        val dataSource = createDataSource(
+            elementsSessionManager = elementsSessionManager,
+        )
+
+        val result = dataSource.retrieveSavedSelection(customerSessionElementsSession = null)
+
+        assertThat(result).isInstanceOf<CustomerSheetDataResult.Success<SavedSelection?>>()
+
+        val successResult = result.asSuccess()
+
+        assertThat(successResult.value).isEqualTo(SavedSelection.PaymentMethod(id = expectedSavedSelectionId))
     }
 
     @Test
-    fun `When default payment methods enabled and no default PM, should return null selection`() {
-        // TODO: implement test.
+    fun `When default payment methods enabled and no default PM, should return null selection`() = runTest {
+        val elementsSessionManager = FakeCustomerSessionElementsSessionManager(
+            isPaymentMethodSyncDefaultEnabled = true,
+            defaultPaymentMethodId = null,
+        )
+
+        val dataSource = createDataSource(
+            elementsSessionManager = elementsSessionManager,
+        )
+
+        val result = dataSource.retrieveSavedSelection(customerSessionElementsSession = null)
+
+        assertThat(result).isInstanceOf<CustomerSheetDataResult.Success<SavedSelection?>>()
+
+        val successResult = result.asSuccess()
+
+        assertThat(successResult.value).isEqualTo(null)
     }
 
     @Test
-    fun `When elements session passed to retrieveSavedSelection, should not re-query elements session`() {
-        // TODO: implement test.
+    fun `When elements session passed to retrieveSavedSelection, should not re-query elements session`() = runTest {
+        val existingElementsSession = FakeCustomerSessionElementsSessionManager().fetchElementsSession()
+        val failingElementsSessionManager = FakeCustomerSessionElementsSessionManager(
+            elementsSession = Result.failure(
+                IllegalAccessError("Should not re-query elements session in this test!")
+            )
+        )
+
+        val dataSource = createDataSource(
+            elementsSessionManager = failingElementsSessionManager
+        )
+
+        val result = dataSource.retrieveSavedSelection(customerSessionElementsSession = existingElementsSession.getOrThrow())
+
+        assertThat(result).isInstanceOf<CustomerSheetDataResult.Success<SavedSelection?>>()
+
+        // No exception being thrown indicates we did not try to re-query elements session from the
+        // "failingElementsSessionManager".
     }
 
     private suspend fun createDataSource(
