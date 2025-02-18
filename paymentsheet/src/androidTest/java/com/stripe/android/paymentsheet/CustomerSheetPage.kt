9b80729eff40e68e52bc54e575c20a0b8fe2e440
Tyler Clawson <tjclawson@stripe.com>
2025-02-18 18:10:07 -0500

    Fix CustomerSheetTest

--- paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/CustomerSheetPage.kt
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/CustomerSheetPage.kt
@@ -60,7 +60,7 @@ internal class CustomerSheetPage(
         cardNumber: String = CARD_NUMBER,
     ) {
         replaceText("Card number", cardNumber)
-        replaceText("MM / YY", "$EXPIRY_MONTH/$${EXPIRY_YEAR.substring(startIndex = 2)}")
+        fillExpirationDate("$EXPIRY_MONTH/$${EXPIRY_YEAR.substring(startIndex = 2)}")
         replaceText("CVC", CVC)
         replaceText("ZIP Code", ZIP_CODE)
     }
