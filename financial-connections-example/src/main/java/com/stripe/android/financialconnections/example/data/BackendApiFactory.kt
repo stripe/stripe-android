8e85ab37fcdde1bcd6ca1145ce43a2bfe112ccf1
Jay Newstrom <jaynewstrom@stripe.com>
2025-02-18 09:35:06 -0700

    Update retrofit.

--- financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/data/BackendApiFactory.kt
+++ financial-connections-example/src/main/java/com/stripe/android/financialconnections/example/data/BackendApiFactory.kt
@@ -1,6 +1,5 @@
 package com.stripe.android.financialconnections.example.data
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
@@ -8,6 +7,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import java.net.SocketTimeoutException
 import java.util.concurrent.TimeUnit
 
