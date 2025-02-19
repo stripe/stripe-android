566e058e31723b5c08e71a8036c0e44ad80ff65c
tianzhao-stripe <tianzhao@stripe.com>
2025-02-18 17:16:41 -0800

    add extra params to PaymentMethodConfirmationOption.New

--- paymentsheet/src/test/java/com/stripe/android/paymentelement/confirmation/bacs/BacsConfirmationFlowTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentelement/confirmation/bacs/BacsConfirmationFlowTest.kt
@@ -36,6 +36,7 @@ class BacsConfirmationFlowTest {
                 createParams = BACS_CONFIRMATION_OPTION.createParams,
                 optionsParams = BACS_CONFIRMATION_OPTION.optionsParams,
                 shouldSave = false,
+                extraParams = null,
             ),
             parameters = CONFIRMATION_PARAMETERS,
         ),
