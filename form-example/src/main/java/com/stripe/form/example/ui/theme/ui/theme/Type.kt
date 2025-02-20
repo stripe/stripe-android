3458b2c7a221218206ee9e96be6842c14cd9b0be
toluo-stripe <toluo@stripe.com>
2025-02-20 17:14:27 -0500

    Delete Type.kt

--- form-example/src/main/java/com/stripe/form/example/ui/theme/ui/theme/Type.kt
+++
@@ -1,7 +0,0 @@
-package com.stripe.form.example.ui.theme.ui.theme
-
-import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
