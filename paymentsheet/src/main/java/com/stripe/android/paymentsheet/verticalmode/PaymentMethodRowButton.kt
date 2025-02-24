2565a1f26090f3e93f98667a218b791ad8ae66ce
Tyler Clawson <tjclawson@stripe.com>
2025-02-24 16:15:30 -0500

    Add offset to checkmark icon to be flush with end of row

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodRowButton.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodRowButton.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -274,7 +275,8 @@ private fun RowButtonCheckmarkOuterContent(
                 contentDescription = null,
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
-                    .padding(end = style.checkmarkInsetDp.dp),
+                    .padding(end = style.checkmarkInsetDp.dp)
+                    .offset(3.dp),
                 tint = Color(style.checkmarkColor)
             )
         }
