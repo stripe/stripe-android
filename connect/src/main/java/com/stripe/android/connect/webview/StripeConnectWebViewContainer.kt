c19c4e97dde2f2888655f69fc817e01411b0cb67
Larry Ng <lng@stripe.com>
2025-02-26 17:01:25 -0500

    fix StripeConnectWebViewContainerViewModel scoping to support Fragments, etc.

--- connect/src/main/java/com/stripe/android/connect/webview/StripeConnectWebViewContainer.kt
+++ connect/src/main/java/com/stripe/android/connect/webview/StripeConnectWebViewContainer.kt
@@ -13,8 +13,8 @@ import androidx.annotation.RestrictTo
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import com.stripe.android.connect.BuildConfig
@@ -27,7 +27,6 @@ import com.stripe.android.connect.StripeEmbeddedComponent
 import com.stripe.android.connect.StripeEmbeddedComponentListener
 import com.stripe.android.connect.toJsonObject
 import com.stripe.android.connect.util.AndroidClock
-import com.stripe.android.connect.util.findActivity
 import com.stripe.android.core.Logger
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -55,12 +54,13 @@ interface StripeConnectWebViewContainer<Listener, Props>
 internal class StripeConnectWebViewContainerImpl<Listener, Props>(
     private val context: Context,
     private val embeddedComponent: StripeEmbeddedComponent,
-    embeddedComponentManager: EmbeddedComponentManager?,
+    private var embeddedComponentManager: EmbeddedComponentManager?,
     private var listener: Listener?,
     props: Props?,
     private val listenerDelegate: ComponentListenerDelegate<Listener> = ComponentListenerDelegate(),
     private val logger: Logger = Logger.getInstance(enableLogging = BuildConfig.DEBUG),
-) : StripeConnectWebViewContainer<Listener, Props>
+) : StripeConnectWebViewContainer<Listener, Props>,
+    View.OnAttachStateChangeListener
     where Props : ComponentProps,
           Listener : StripeEmbeddedComponentListener {
 
@@ -98,6 +98,7 @@ internal class StripeConnectWebViewContainerImpl<Listener, Props>(
     private var propsJson: JsonObject? = null
 
     init {
+        val embeddedComponentManager = this.embeddedComponentManager
         if (embeddedComponentManager != null) {
             initializeInternal(
                 embeddedComponentManager = embeddedComponentManager,
@@ -107,11 +108,11 @@ internal class StripeConnectWebViewContainerImpl<Listener, Props>(
         }
     }
 
-    internal fun initializeView(view: FrameLayout, cacheKey: String?) {
-        this.containerView = view
+    internal fun initializeView(containerView: FrameLayout, cacheKey: String?) {
+        this.containerView = containerView
         this.webViewCacheKey = cacheKey
 
-        bindViewModel()
+        containerView.addOnAttachStateChangeListener(this)
     }
 
     override fun initialize(
@@ -131,9 +132,7 @@ internal class StripeConnectWebViewContainerImpl<Listener, Props>(
         listener: Listener?,
         propsJson: JsonObject?,
     ) {
-        if (this.viewModel != null) {
-            throw IllegalStateException("Already initialized")
-        }
+        this.embeddedComponentManager = embeddedComponentManager
         this.listener = listener
         val oldProps = this.propsJson
         this.propsJson =
@@ -148,15 +147,45 @@ internal class StripeConnectWebViewContainerImpl<Listener, Props>(
                     }
                 }
             }
-
-        this.viewModel = getViewModelFromActivity(embeddedComponentManager)
-        bindViewModel()
     }
 
-    private fun getViewModelFromActivity(
-        embeddedComponentManager: EmbeddedComponentManager
-    ): StripeConnectWebViewContainerViewModel {
-        val viewModelStoreOwner = requireNotNull(context.findActivity() as? ViewModelStoreOwner)
+    override fun onViewAttachedToWindow(v: View) {
+        val containerView = v as FrameLayout
+
+        val viewModel = checkNotNull(getViewModelFromViewModelStoreOwner(containerView))
+            .also { this.viewModel = it }
+        populateContainerView(
+            containerView = containerView,
+            viewModel = viewModel
+        )
+
+        val lifecycleOwner = checkNotNull(containerView.findViewTreeLifecycleOwner())
+
+        // Bind VM's lifecycle to container view's lifecycle.
+        lifecycleOwner.lifecycle.addObserver(viewModel)
+
+        // Handle VM state and events.
+        lifecycleOwner.lifecycleScope.launch {
+            viewModel.stateFlow.collectLatest(::bindViewModelState)
+            viewModel.eventFlow.collectLatest(::handleEvent)
+        }
+
+        viewModel.onViewAttached()
+    }
+
+    override fun onViewDetachedFromWindow(v: View) {
+        val containerView = v as FrameLayout
+
+        // Clean up.
+        viewModel?.let { containerView.findViewTreeLifecycleOwner()?.lifecycle?.removeObserver(it) }
+        logger.debug("($loggerTag) Removing WebView from container view")
+        containerView.removeView(webView)
+    }
+
+    private fun getViewModelFromViewModelStoreOwner(view: View): StripeConnectWebViewContainerViewModel? {
+        val embeddedComponentManager = this.embeddedComponentManager ?: return null
+        val viewModelStoreOwner = view.findViewTreeViewModelStoreOwner() ?: return null
+
         val viewModelKey = buildString {
             append(embeddedComponent.name)
             append("-")
@@ -184,12 +213,12 @@ internal class StripeConnectWebViewContainerImpl<Listener, Props>(
         return viewModelProvider[viewModelKey, StripeConnectWebViewContainerViewModel::class]
     }
 
-    private fun bindViewModel() {
-        val containerView = this.containerView ?: return
-        val viewModel = this.viewModel ?: return
-
+    private fun populateContainerView(containerView: FrameLayout, viewModel: StripeConnectWebViewContainerViewModel) {
         // Sync props to VM.
         viewModel.propsJson = this.propsJson
+
+        // Start from a clean slate.
+        containerView.removeAllViews()
 
         // Add VM's WebView.
         val webView = viewModel.webView
@@ -212,32 +241,6 @@ internal class StripeConnectWebViewContainerImpl<Listener, Props>(
             }
         this.progressBar = progressBar
         containerView.addView(progressBar)
-
-        containerView.addOnAttachStateChangeListener(
-            object : View.OnAttachStateChangeListener {
-                override fun onViewAttachedToWindow(v: View) {
-                    viewModel.onViewAttached()
-                    val owner = containerView.findViewTreeLifecycleOwner()
-                        ?: return
-
-                    // Bind VM's lifecycle to container view's lifecycle.
-                    owner.lifecycle.addObserver(viewModel)
-
-                    // Handle VM state and events.
-                    owner.lifecycleScope.launch {
-                        viewModel.stateFlow.collectLatest(::bindViewModelState)
-                        viewModel.eventFlow.collectLatest(::handleEvent)
-                    }
-                }
-
-                override fun onViewDetachedFromWindow(v: View) {
-                    // Clean up.
-                    containerView.findViewTreeLifecycleOwner()?.lifecycle?.removeObserver(viewModel)
-                    logger.debug("($loggerTag) Removing WebView from container view")
-                    containerView.removeView(webView)
-                }
-            }
-        )
     }
 
     internal fun setPropsFromXml(props: Props) {
