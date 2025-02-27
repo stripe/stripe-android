bd654765b80514a3f9c2a10346dcacecdbfd1c07
Larry Ng <lng@stripe.com>
2025-02-27 16:14:50 -0500

    lint and api

--- connect/src/main/java/com/stripe/android/connect/StripeComponentDialogFragment.kt
+++ connect/src/main/java/com/stripe/android/connect/StripeComponentDialogFragment.kt
@@ -1,8 +1,6 @@
 package com.stripe.android.connect
 
 import android.content.DialogInterface
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
