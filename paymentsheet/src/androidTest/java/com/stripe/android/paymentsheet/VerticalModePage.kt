27af59209671d908bb0cca4d30265d988f45e792
Annabella Koch <amk@stripe.com>
2025-03-02 12:23:04 -0800

    Add network test class for default payment methods

---
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/VerticalModePage.kt
@@ -0,0 +1,60 @@
+package com.stripe.android.paymentsheet
+
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.hasAnyDescendant
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.isSelected
+import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import com.stripe.android.paymentsheet.ui.PAYMENT_SHEET_PRIMARY_BUTTON_TEST_TAG
+import com.stripe.android.paymentsheet.ui.TEST_TAG_ICON_FROM_RES
+import com.stripe.android.paymentsheet.verticalmode.TEST_TAG_PAYMENT_METHOD_VERTICAL_LAYOUT
+import com.stripe.android.paymentsheet.verticalmode.TEST_TAG_SAVED_PAYMENT_METHOD_ROW_BUTTON
+import com.stripe.android.paymentsheet.verticalmode.TEST_TAG_SAVED_TEXT
+import com.stripe.android.paymentsheet.verticalmode.TEST_TAG_VIEW_MORE
+
+internal class VerticalModePage(
+    private val composeTestRule: ComposeTestRule
+) {
+    fun waitUntilVisible() {
+        composeTestRule.waitUntil {
+            composeTestRule
+                .onAllNodes(hasTestTag(TEST_TAG_PAYMENT_METHOD_VERTICAL_LAYOUT))
+                .fetchSemanticsNodes()
+                .isNotEmpty()
+        }
+    }
+
+    fun assertPrimaryButton(matcher: SemanticsMatcher) {
+        composeTestRule
+            .onNode(hasTestTag(PAYMENT_SHEET_PRIMARY_BUTTON_TEST_TAG).and(matcher))
+            .assertExists()
+    }
+
+    fun assertHasSavedPaymentMethods() {
+        composeTestRule.waitUntil {
+            composeTestRule.onNodeWithTag(TEST_TAG_SAVED_TEXT).isDisplayed()
+        }
+        composeTestRule.onNodeWithTag(TEST_TAG_SAVED_TEXT).assertExists()
+    }
+
+    fun assertHasSelectedSavedPaymentMethod(paymentMethodId: String, cardBrand: String? = null) {
+        composeTestRule.onNode(
+            hasTestTag("${TEST_TAG_SAVED_PAYMENT_METHOD_ROW_BUTTON}_$paymentMethodId").and(isSelected())
+        ).assertExists()
+
+        if (cardBrand != null) {
+            composeTestRule.onNode(
+                hasTestTag("${TEST_TAG_SAVED_PAYMENT_METHOD_ROW_BUTTON}_$paymentMethodId")
+                    .and(hasAnyDescendant(hasTestTag(TEST_TAG_ICON_FROM_RES).and(hasTestMetadata(cardBrand)))),
+                useUnmergedTree = true,
+            ).assertExists()
+        }
+    }
+
+    fun clickViewMore() {
+        composeTestRule.onNodeWithTag(TEST_TAG_VIEW_MORE).performClick()
+    }
+}
