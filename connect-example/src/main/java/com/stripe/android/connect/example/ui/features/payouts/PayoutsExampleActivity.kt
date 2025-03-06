de8cf4b8fb260d655ac7175079340b8853d66de1
Larry Ng <lng@stripe.com>
2025-03-06 17:05:06 -0500

    rm "Use XML views" setting

--- connect-example/src/main/java/com/stripe/android/connect/example/ui/features/payouts/PayoutsExampleActivity.kt
+++ connect-example/src/main/java/com/stripe/android/connect/example/ui/features/payouts/PayoutsExampleActivity.kt
@@ -1,16 +1,13 @@
 package com.stripe.android.connect.example.ui.features.payouts
 
 import android.content.Context
-import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.stripe.android.connect.EmbeddedComponentManager
-import com.stripe.android.connect.EmptyProps
 import com.stripe.android.connect.PayoutsListener
 import com.stripe.android.connect.PrivateBetaConnectSDK
 import com.stripe.android.connect.example.R
-import com.stripe.android.connect.example.databinding.ViewPayoutsExampleBinding
 import com.stripe.android.connect.example.ui.common.BasicExampleComponentActivity
 import com.stripe.android.connect.example.ui.settings.SettingsViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,22 +22,11 @@ class PayoutsExampleActivity : BasicExampleComponentActivity() {
     override fun createComponentView(context: Context, embeddedComponentManager: EmbeddedComponentManager): View {
         val settings = settingsViewModel.state.value
         val listener = Listener()
-        return if (settings.presentationSettings.useXmlViews) {
-            ViewPayoutsExampleBinding.inflate(LayoutInflater.from(context)).root
-                .apply {
-                    initialize(
-                        embeddedComponentManager = embeddedComponentManager,
-                        listener = listener,
-                        props = EmptyProps,
-                    )
-                }
-        } else {
-            embeddedComponentManager.createPayoutsView(
+        return embeddedComponentManager.createPayoutsView(
                 context = context,
                 listener = listener,
                 cacheKey = "PayoutsExampleActivity"
             )
-        }
     }
 
     private inner class Listener : PayoutsListener {
