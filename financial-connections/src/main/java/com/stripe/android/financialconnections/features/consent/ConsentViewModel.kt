e2e9090e733b50e83b45eee58eb591050c306dac
Carlos Munoz <carlosmuvi@stripe.com>
2025-03-06 09:59:50 -0800

    Detekt fixes.

--- financial-connections/src/main/java/com/stripe/android/financialconnections/features/consent/ConsentViewModel.kt
+++ financial-connections/src/main/java/com/stripe/android/financialconnections/features/consent/ConsentViewModel.kt
@@ -37,8 +37,8 @@ import com.stripe.android.financialconnections.utils.Experiment.CONNECTIONS_CONS
 import com.stripe.android.financialconnections.utils.error
 import com.stripe.android.financialconnections.utils.experimentAssignment
 import com.stripe.android.financialconnections.utils.trackExposure
-import com.stripe.android.uicore.navigation.NavigationManager
 import com.stripe.android.model.EmailSource
+import com.stripe.android.uicore.navigation.NavigationManager
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
