cde2c574c1582e60d38ab831672d53aefd1b85d9
Larry Ng <lng@stripe.com>
2025-03-06 12:17:09 -0500

    lint

--- connect-example/src/main/java/com/stripe/android/connect/example/ui/settings/PresentationSettingsView.kt
+++ connect-example/src/main/java/com/stripe/android/connect/example/ui/settings/PresentationSettingsView.kt
@@ -9,17 +9,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
