5820ced53eb159bc7780103c36e55fdb0356cd67
Larry Ng <lng@stripe.com>
2025-03-11 17:10:46 -0400

    init webview tests

---
+++ connect/src/androidTest/java/com/stripe/android/connect/ConnectTestRunner.kt
@@ -0,0 +1,15 @@
+package com.stripe.android.connect
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import com.stripe.android.connect.di.DaggerTestStripeConnectComponent
+import com.stripe.android.connect.di.StripeConnectComponent
+
+@Suppress("unused")
+class ConnectTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
+        StripeConnectComponent.instance = DaggerTestStripeConnectComponent.create()
+        return super.newApplication(cl, className, context)
+    }
+}
