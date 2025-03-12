fca660ec10904f6fd446e857c372e1c0f7828b17
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2025-03-12 09:59:36 -0700

    Fix how `StripeImageLoader` determines the content type (#10378)
    * Fix how `StripeImageLoader` determines the content type
    
    * Address PR comments

---
+++ stripe-ui-core/src/test/java/com/stripe/android/uicore/image/ImageLruMemoryCacheTest.kt
@@ -0,0 +1,39 @@
+package com.stripe.android.uicore.image
+
+import android.graphics.BitmapFactory
+import com.google.common.truth.Truth.assertThat
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.Test
+
+@RunWith(RobolectricTestRunner::class)
+class ImageLruMemoryCacheTest {
+    @Test
+    fun `should be able to store and retrieve saved image`() {
+        val cache = ImageLruMemoryCache()
+
+        assertThat(cache.get(URL)).isNull()
+
+        cache.put(
+            key = URL,
+            image = LoadedImage(
+                contentType = LoadedImage.ContentType.Known.Png,
+                bitmap = BitmapFactory.decodeStream(readImage()),
+            ),
+        )
+
+        val image = cache.get(URL)
+
+        assertThat(image).isNotNull()
+
+        val nonNullImage = requireNotNull(image)
+
+        assertThat(nonNullImage.contentType).isEqualTo(LoadedImage.ContentType.Known.Png)
+    }
+
+    private fun readImage() = Unit::class.java.classLoader!!.getResourceAsStream("example.png")
+
+    private companion object {
+        const val URL = "https://image"
+    }
+}
