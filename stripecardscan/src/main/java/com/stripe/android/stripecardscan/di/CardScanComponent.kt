fcd9935da242dbc2a240edf2daf0a2e7e894e7ce
toluo-stripe <toluo@stripe.com>
2025-03-13 13:18:07 -0400

    Add analytics event reporter for card scan (#10372)

---
+++ stripecardscan/src/main/java/com/stripe/android/stripecardscan/di/CardScanComponent.kt
@@ -0,0 +1,31 @@
+package com.stripe.android.stripecardscan.di
+
+import android.app.Application
+import com.stripe.android.core.injection.CoroutineContextModule
+import com.stripe.android.stripecardscan.cardscan.CardScanConfiguration
+import com.stripe.android.stripecardscan.cardscan.CardScanEventsReporter
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        CardScanModule::class,
+        CoroutineContextModule::class,
+    ]
+)
+internal interface CardScanComponent {
+    val cardScanEventsReporter: CardScanEventsReporter
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        @BindsInstance
+        fun configuration(cardScanConfiguration: CardScanConfiguration): Builder
+
+        fun build(): CardScanComponent
+    }
+}
