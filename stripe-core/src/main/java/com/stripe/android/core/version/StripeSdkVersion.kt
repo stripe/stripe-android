c6fb5fb3f8d1a2bc0f474ccf021756246ae8cf72
Adam Wushensky <84421010+awush-stripe@users.noreply.github.com>
2025-03-17 15:24:54 -0400

    Bump version to 21.7.0 (#10411)
    * Bump version to 21.7.0
    
    * Add note for new feature
    
    * Remove reference to the PR for the new feature

--- stripe-core/src/main/java/com/stripe/android/core/version/StripeSdkVersion.kt
+++ stripe-core/src/main/java/com/stripe/android/core/version/StripeSdkVersion.kt
@@ -8,6 +8,6 @@ package com.stripe.android.core.version
  *
  */
 object StripeSdkVersion {
-    const val VERSION_NAME = "21.6.0"
+    const val VERSION_NAME = "21.7.0"
     const val VERSION: String = "AndroidBindings/$VERSION_NAME"
 }
