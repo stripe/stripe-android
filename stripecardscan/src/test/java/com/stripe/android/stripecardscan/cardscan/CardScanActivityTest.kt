eface192fd4a261c9cc941bab226790bc1f651c2
toluo-stripe <toluo@stripe.com>
2025-03-19 15:58:58 -0400

    Update CardScanActivityTest.kt

--- stripecardscan/src/test/java/com/stripe/android/stripecardscan/cardscan/CardScanActivityTest.kt
+++ stripecardscan/src/test/java/com/stripe/android/stripecardscan/cardscan/CardScanActivityTest.kt
@@ -4,71 +4,78 @@ import androidx.test.core.app.ActivityScenario
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.stripecardscan.payment.card.ScannedCard
 import com.stripe.android.stripecardscan.scanui.CancellationReason
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
-class CardScanActivityTest {
+internal class CardScanActivityTest {
     @Test
-    fun `scanSucceeded event is sent on scan completion`() = runTest {
-        FakeCardScansEventReporter.test {
-            val scenario = ActivityScenario.launch(CardScanActivity::class.java)
-
-            scenario.onActivity { activity ->
-                activity.cardScanEventsReporter = eventsReporter
-                activity.resultListener.cardScanComplete(ScannedCard(""))
-            }
-
-            awaitScanSucceeded()
+    fun `scanSucceeded event is sent on scan completion`() = testScenario {
+        activityScenario.onActivity { activity ->
+            activity.cardScanEventsReporter = eventsReporter
+            activity.resultListener.cardScanComplete(ScannedCard(""))
         }
+
+        eventReporterScenario.awaitScanSucceeded()
     }
 
     @Test
-    fun `scanFailed event is sent on scan failed`() = runTest {
-        FakeCardScansEventReporter.test {
-            val error = Throwable("oops")
-            val scenario = ActivityScenario.launch(CardScanActivity::class.java)
+    fun `scanFailed event is sent on scan failed`() = testScenario {
+        val error = Throwable("oops")
 
-            scenario.onActivity { activity ->
-                activity.cardScanEventsReporter = eventsReporter
-                activity.resultListener.failed(error)
-            }
-
-            assertThat(awaitScanFailed()).isEqualTo(error)
+        activityScenario.onActivity { activity ->
+            activity.cardScanEventsReporter = eventsReporter
+            activity.resultListener.failed(error)
         }
+
+        assertThat(eventReporterScenario.awaitScanFailed()).isEqualTo(error)
     }
 
     @Test
-    fun `scanCancelled event is sent on cancellation`() = runTest {
-        FakeCardScansEventReporter.test {
-            val scenario = ActivityScenario.launch(CardScanActivity::class.java)
-
-            scenario.onActivity { activity ->
-                activity.cardScanEventsReporter = eventsReporter
-                activity.resultListener.userCanceled(CancellationReason.Back)
-            }
-
-            awaitScanCancelled()
+    fun `scanCancelled event is sent on cancellation`() = testScenario {
+        activityScenario.onActivity { activity ->
+            activity.cardScanEventsReporter = eventsReporter
+            activity.resultListener.userCanceled(CancellationReason.Back)
         }
+
+        eventReporterScenario.awaitScanCancelled()
     }
 
     @Test
-    fun `scanStarted event is sent on scan started`() = runTest {
-        FakeCardScansEventReporter.test {
-            val scenario = ActivityScenario.launch(CardScanActivity::class.java)
-
-            scenario.onActivity { activity ->
-                activity.cardScanEventsReporter = eventsReporter
-                this@runTest.launch {
-                    activity.onCameraStreamAvailable(flowOf())
-                }
+    fun `scanStarted event is sent on scan started`() = testScenario {
+        activityScenario.onActivity { activity ->
+            activity.cardScanEventsReporter = eventsReporter
+            scope.launch {
+                activity.onCameraStreamAvailable(flowOf())
             }
-
-            awaitScanStarted()
         }
+
+        eventReporterScenario.awaitScanStarted()
+    }
+
+    private fun testScenario(test: suspend TestParams.() -> Unit) = runTest {
+        FakeCardScansEventReporter.test {
+            val activityScenario = ActivityScenario.launch(CardScanActivity::class.java)
+            val params = TestParams(
+                activityScenario = activityScenario,
+                eventReporterScenario = this,
+                scope = this@runTest
+            )
+            test(params)
+        }
+    }
+
+    private data class TestParams(
+        val activityScenario: ActivityScenario<CardScanActivity>,
+        val eventReporterScenario: FakeCardScansEventReporter.Scenario,
+        val scope: TestScope
+    ) {
+        val eventsReporter = eventReporterScenario.eventsReporter
     }
 }
