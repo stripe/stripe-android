5a8bb60e822fe0db911979dbc835266698d2bed0
Larry Ng <lng@stripe.com>
2025-03-19 21:59:28 -0400

    add tests

--- connect/src/androidTest/java/com/stripe/android/connect/FullScreenComponentTest.kt
+++ connect/src/androidTest/java/com/stripe/android/connect/FullScreenComponentTest.kt
@@ -3,6 +3,7 @@ package com.stripe.android.connect
 import androidx.appcompat.widget.Toolbar
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.NoMatchingViewException
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -13,6 +14,7 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withClassName
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.web.assertion.WebViewAssertions.webMatches
 import androidx.test.espresso.web.model.Atoms.script
 import androidx.test.espresso.web.sugar.Web.onWebView
@@ -22,6 +24,8 @@ import androidx.test.espresso.web.webdriver.DriverAtoms.webClick
 import androidx.test.espresso.web.webdriver.DriverAtoms.webScrollIntoView
 import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.stripe.android.connect.webview.serialization.AlertJs
+import com.stripe.android.connect.webview.serialization.ConnectJson
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.containsString
 import org.hamcrest.Matchers.equalTo
@@ -30,6 +34,8 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 @OptIn(PrivateBetaConnectSDK::class)
 class FullScreenComponentTest {
@@ -127,6 +133,51 @@ class FullScreenComponentTest {
         checkDialogDoesNotExist()
     }
 
+    @Test
+    fun testCustomJsAlertContents() {
+        checkDialogIsDisplayed()
+        val alertJs = AlertJs(
+            title = randomString(),
+            message = randomString(),
+            buttons = AlertJs.ButtonsJs(
+                ok = randomString(),
+                cancel = randomString(),
+            ),
+        )
+        performWebViewAlert(ALERT, alertJs)
+        onView(withText(alertJs.title)).check(matches(isDisplayed()))
+        onView(withText(alertJs.message)).check(matches(isDisplayed()))
+        onView(withText(alertJs.buttons!!.ok)).check(matches(isDisplayed()))
+        onView(withText(alertJs.buttons.cancel)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testCustomJsAlertDismissal() {
+        checkDialogIsDisplayed()
+        val alertJs = AlertJs(
+            title = null,
+            message = "alert message",
+            buttons = AlertJs.ButtonsJs(
+                ok = "ok button",
+                cancel = "cancel button",
+            ),
+        )
+        val dismissActions = listOf(
+            { onView(withText(alertJs.buttons!!.ok)).perform(click()) },
+            { onView(withText(alertJs.buttons!!.cancel)).perform(click()) },
+            { Espresso.pressBack() },
+        )
+        listOf(ALERT, CONFIRM).forEach { method ->
+            dismissActions.forEach { dismiss ->
+                performWebViewAlert(method, alertJs)
+                dismiss()
+                checkDialogIsDisplayed()
+            }
+        }
+    }
+
+    private fun randomString() = UUID.randomUUID().toString()
+
     private fun checkDialogIsDisplayed() {
         onView(rootView)
             .inRoot(RootMatchers.isDialog())
@@ -137,5 +188,23 @@ class FullScreenComponentTest {
         onView(rootView)
             .noActivity()
             .check(doesNotExist())
+    }
+
+    private fun performWebViewAlert(method: String, alertJs: AlertJs) {
+        val alertString = ConnectJson.encodeToString(alertJs)
+        try {
+            onWebView()
+                .withTimeout(500L, TimeUnit.MILLISECONDS)
+                .perform(script("""$method(`$alertString`)"""))
+        } catch (_: NoMatchingViewException) {
+            // HACK: After the JS executes, the alert dialog prevents the WebView from
+            //  being interacted with. Assume the alert was shown -- we'll be verifying
+            //  the dialog below anyway.
+        }
+    }
+
+    companion object {
+        private const val ALERT = "alert"
+        private const val CONFIRM = "confirm"
     }
 }
