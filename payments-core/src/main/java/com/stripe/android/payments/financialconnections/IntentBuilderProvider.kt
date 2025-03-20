cf16f0ca13c33bbce557e7701c58fcea58ee4de3
Carlos Munoz <carlosmuvi@stripe.com>
2025-03-19 19:09:16 -0700

    Adds alias.

--- payments-core/src/main/java/com/stripe/android/payments/financialconnections/IntentBuilderProvider.kt
+++ payments-core/src/main/java/com/stripe/android/payments/financialconnections/IntentBuilderProvider.kt
@@ -2,17 +2,17 @@ package com.stripe.android.payments.financialconnections
 
 import android.content.Context
 import android.content.Intent
-import com.stripe.android.financialconnections.intentBuilder
 import com.stripe.android.financialconnections.launcher.FinancialConnectionsSheetActivityArgs
 import com.stripe.android.financialconnections.lite.liteIntentBuilder
 import com.stripe.android.payments.financialconnections.IntentBuilderProvider.IntentBuilder
+import com.stripe.android.financialconnections.intentBuilder as fullIntentBuilder
 
 interface IntentBuilderProvider {
     fun provide(context: Context, isFinancialConnectionsAvailable: Boolean): IntentBuilder
 
     sealed class IntentBuilder(val provider: (FinancialConnectionsSheetActivityArgs) -> Intent) {
         class Lite(context: Context) : IntentBuilder(liteIntentBuilder(context))
-        class Full(context: Context) : IntentBuilder(intentBuilder(context))
+        class Full(context: Context) : IntentBuilder(fullIntentBuilder(context))
     }
 }
 
