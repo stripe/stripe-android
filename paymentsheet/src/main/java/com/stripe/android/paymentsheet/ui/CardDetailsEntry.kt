18f955db0f5e3f476c637462ba65cfe5689814ac
toluo-stripe <toluo@stripe.com>
2025-03-21 16:04:24 -0400

    kdoc update

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/CardDetailsEntry.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/CardDetailsEntry.kt
@@ -2,16 +2,32 @@ package com.stripe.android.paymentsheet.ui
 
 import com.stripe.android.paymentsheet.CardUpdateParams
 
+/**
+ * Represents the editable details of a card payment method.
+ *
+ * @property cardBrandChoice The currently selected card brand choice.
+ */
 internal data class CardDetailsEntry(
     val cardBrandChoice: CardBrandChoice,
 ) {
+    /**
+     * Determines if the card details have changed compared to the provided values.
+     *
+     * @param originalCardBrandChoice The card brand choice to compare against.
+     * @return True if any of the card details have changed, false otherwise.
+     */
     fun hasChanged(
-        cardBrandChoice: CardBrandChoice,
+        originalCardBrandChoice: CardBrandChoice,
     ): Boolean {
-        return cardBrandChoice != this.cardBrandChoice
+        return originalCardBrandChoice != this.cardBrandChoice
     }
 }
 
+/**
+ * Converts the CardDetailsEntry to CardUpdateParams.
+ *
+ * @return CardUpdateParams containing the updated card brand.
+ */
 internal fun CardDetailsEntry.toUpdateParams(): CardUpdateParams {
     return CardUpdateParams(cardBrand = cardBrandChoice.brand)
 }
