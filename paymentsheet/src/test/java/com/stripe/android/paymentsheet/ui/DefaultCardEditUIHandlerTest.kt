f0ac4f75995ca0c6e0518815776842b2110223d4
toluo-stripe <toluo@stripe.com>
2025-03-21 16:32:22 -0400

    Update DefaultCardEditUIHandlerTest.kt

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/DefaultCardEditUIHandlerTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/ui/DefaultCardEditUIHandlerTest.kt
@@ -58,7 +58,7 @@ internal class DefaultCardEditUIHandlerTest {
 
         handler.brandChanged(CardBrand.Visa)
 
-        assertThat(cardUpdateParams?.cardBrand).isEqualTo(CardBrand.Visa)
+        assertThat(cardUpdateParams).isEqualTo(cardUpdateParams(cardBrand = CardBrand.Visa))
 
         handler.brandChanged(CardBrand.CartesBancaires)
 
@@ -94,6 +94,20 @@ internal class DefaultCardEditUIHandlerTest {
 
     private val DefaultCardEditUIHandler.selectedBrand
         get() = uiState.selectedCardBrand.brand
+
+    private fun cardUpdateParams(
+        expiryMonth: Int? = null,
+        expiryYear: Int? = null,
+        cardBrand: CardBrand? = null,
+        billingDetails: PaymentMethod.BillingDetails? = null
+    ): CardUpdateParams {
+        return CardUpdateParams(
+            expiryMonth = expiryMonth,
+            expiryYear = expiryYear,
+            cardBrand = cardBrand,
+            billingDetails = billingDetails
+        )
+    }
 
     private fun handler(
         card: PaymentMethod.Card = PaymentMethodFixtures.CARD_WITH_NETWORKS,
