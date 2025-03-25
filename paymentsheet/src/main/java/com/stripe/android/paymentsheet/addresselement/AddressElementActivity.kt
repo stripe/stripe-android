873090d70b982da03f3e8f699a610c663e9dcd19
Jay Newstrom <jaynewstrom@stripe.com>
2025-03-25 09:41:58 -0700

    Prevent invalid launches of address element. (#10484)

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/addresselement/AddressElementActivity.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/addresselement/AddressElementActivity.kt
@@ -10,10 +10,12 @@ import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -22,6 +24,7 @@ import androidx.navigation.navArgument
 import com.stripe.android.common.ui.ElementsBottomSheetLayout
 import com.stripe.android.paymentsheet.parseAppearance
 import com.stripe.android.uicore.StripeTheme
+import com.stripe.android.uicore.elements.bottomsheet.StripeBottomSheetState
 import com.stripe.android.uicore.elements.bottomsheet.rememberStripeBottomSheetState
 import com.stripe.android.uicore.utils.fadeOut
 import kotlinx.coroutines.launch
@@ -33,17 +36,23 @@ internal class AddressElementActivity : ComponentActivity() {
     internal var viewModelFactory: ViewModelProvider.Factory =
         AddressElementViewModel.Factory(
             applicationSupplier = { application },
-            starterArgsSupplier = { starterArgs }
+            starterArgsSupplier = { requireNotNull(starterArgs) }
         )
 
     private val viewModel: AddressElementViewModel by viewModels { viewModelFactory }
 
     private val starterArgs by lazy {
-        requireNotNull(AddressElementActivityContract.Args.fromIntent(intent))
+        AddressElementActivityContract.Args.fromIntent(intent)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val starterArgs = starterArgs
+        if (starterArgs == null) {
+            finish()
+            return
+        }
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
         starterArgs.config?.appearance?.parseAppearance()
@@ -68,37 +77,45 @@ internal class AddressElementActivity : ComponentActivity() {
                 }
             }
 
-            StripeTheme {
-                ElementsBottomSheetLayout(
-                    state = bottomSheetState,
-                    onDismissed = viewModel.navigator::dismiss,
-                ) {
-                    Surface(modifier = Modifier.fillMaxSize()) {
-                        NavHost(
-                            navController = navController,
-                            startDestination = AddressElementScreen.InputAddress.route,
-                        ) {
-                            composable(AddressElementScreen.InputAddress.route) {
-                                InputAddressScreen(viewModel.inputAddressViewModelSubcomponentBuilderProvider)
-                            }
-                            composable(
-                                AddressElementScreen.Autocomplete.route,
-                                arguments = listOf(
-                                    navArgument(AddressElementScreen.Autocomplete.countryArg) {
-                                        type = NavType.StringType
-                                    }
+            AddressElementUi(bottomSheetState, navController)
+        }
+    }
+
+    @Composable
+    private fun AddressElementUi(
+        bottomSheetState: StripeBottomSheetState,
+        navController: NavHostController,
+    ) {
+        StripeTheme {
+            ElementsBottomSheetLayout(
+                state = bottomSheetState,
+                onDismissed = viewModel.navigator::dismiss,
+            ) {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = AddressElementScreen.InputAddress.route,
+                    ) {
+                        composable(AddressElementScreen.InputAddress.route) {
+                            InputAddressScreen(viewModel.inputAddressViewModelSubcomponentBuilderProvider)
+                        }
+                        composable(
+                            AddressElementScreen.Autocomplete.route,
+                            arguments = listOf(
+                                navArgument(AddressElementScreen.Autocomplete.countryArg) {
+                                    type = NavType.StringType
+                                }
+                            )
+                        ) { backStackEntry ->
+                            val country = backStackEntry
+                                .arguments
+                                ?.getString(
+                                    AddressElementScreen.Autocomplete.countryArg
                                 )
-                            ) { backStackEntry ->
-                                val country = backStackEntry
-                                    .arguments
-                                    ?.getString(
-                                        AddressElementScreen.Autocomplete.countryArg
-                                    )
-                                AutocompleteScreen(
-                                    viewModel.autoCompleteViewModelSubcomponentBuilderProvider,
-                                    country
-                                )
-                            }
+                            AutocompleteScreen(
+                                viewModel.autoCompleteViewModelSubcomponentBuilderProvider,
+                                country
+                            )
                         }
                     }
                 }
