ffa3166c566436fb88bcfcab443c4dca13316cf7
Larry Ng <lng@stripe.com>
2025-03-26 17:35:04 -0400

    add redirect activity

---
+++ connect/src/main/java/com/stripe/android/connect/StripeConnectRedirectActivity.kt
@@ -0,0 +1,11 @@
+package com.stripe.android.connect
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StripeConnectRedirectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
