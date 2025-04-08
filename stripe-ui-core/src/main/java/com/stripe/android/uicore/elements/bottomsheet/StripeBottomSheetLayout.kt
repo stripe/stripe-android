087c0192982f39ffabae875640dcbdfaa4c23243
Samer Alabi <samer@stripe.com>
2025-04-08 10:25:23 -0400

    Address PR comments

--- stripe-ui-core/src/main/java/com/stripe/android/uicore/elements/bottomsheet/StripeBottomSheetLayout.kt
+++ stripe-ui-core/src/main/java/com/stripe/android/uicore/elements/bottomsheet/StripeBottomSheetLayout.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import com.stripe.android.uicore.elements.bottomsheet.StripeBottomSheetState.DismissalType
