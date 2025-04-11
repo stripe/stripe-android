6a0957244e9411f0898c50f79c9de7c076f1bdd1
tianzhao-stripe <tianzhao@stripe.com>
2025-04-11 14:03:44 -0700

    Update PaymentMethodRowRadioButtonScreenshotTest.kt
    Update PaymentMethodRowRadioButtonScreenshotTest.kt

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodRowRadioButtonScreenshotTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodRowRadioButtonScreenshotTest.kt
@@ -1,19 +1,22 @@
 package com.stripe.android.paymentsheet.verticalmode
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.stripe.android.paymentelement.ExperimentalEmbeddedPaymentElementApi
+import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheet.Appearance.Embedded.RowStyle.FlatWithRadio
 import com.stripe.android.screenshottesting.FontSize
 import com.stripe.android.screenshottesting.PaparazziRule
+import com.stripe.android.screenshottesting.SystemAppearance
 import com.stripe.android.ui.core.R
 import com.stripe.android.uicore.StripeThemeDefaults
+import com.stripe.android.utils.screenshots.PaymentSheetAppearance
 import org.junit.Rule
 import org.junit.Test
 
@@ -22,6 +25,8 @@ internal class PaymentMethodRowRadioButtonScreenshotTest {
 
     @get:Rule
     val paparazziRule = PaparazziRule(
+        SystemAppearance.entries,
+        PaymentSheetAppearance.entries,
         FontSize.entries,
         boxModifier = Modifier
             .padding(16.dp)
@@ -30,138 +35,45 @@ internal class PaymentMethodRowRadioButtonScreenshotTest {
 
     @Test
     fun testInitialState() {
-        paparazziRule.snapshot {
-            PaymentMethodRowButton(
-                isEnabled = true,
-                isSelected = false,
-                iconContent = {
-                    Image(
-                        painter = painterResource(id = R.drawable.stripe_ic_paymentsheet_pm_card),
-                        contentDescription = null
-                    )
-                },
-                title = "**** 4242",
-                subtitle = null,
-                promoText = null,
-                onClick = {},
-                style = FlatWithRadio.default,
-                shouldShowDefaultBadge = false,
-            )
-        }
+        testPaymentMethodRowButton_RadioButton()
     }
 
     @Test
     fun testDisabledState() {
-        paparazziRule.snapshot {
-            PaymentMethodRowButton(
-                isEnabled = false,
-                isSelected = false,
-                iconContent = {
-                    Image(
-                        painter = painterResource(id = R.drawable.stripe_ic_paymentsheet_pm_card),
-                        contentDescription = null
-                    )
-                },
-                title = "**** 4242",
-                subtitle = null,
-                promoText = null,
-                onClick = {},
-                style = FlatWithRadio.default,
-                shouldShowDefaultBadge = false,
-            )
-        }
+        testPaymentMethodRowButton_RadioButton(
+            isEnabled = false,
+        )
     }
 
     @Test
     fun testSelectedState() {
-        paparazziRule.snapshot {
-            PaymentMethodRowButton(
-                isEnabled = true,
-                isSelected = true,
-                iconContent = {
-                    Image(
-                        painter = painterResource(id = R.drawable.stripe_ic_paymentsheet_pm_card),
-                        contentDescription = null
-                    )
-                },
-                title = "**** 4242",
-                subtitle = null,
-                promoText = null,
-                onClick = {},
-                style = FlatWithRadio.default,
-                shouldShowDefaultBadge = false,
-            )
-        }
+        testPaymentMethodRowButton_RadioButton(
+            isSelected = false,
+        )
     }
 
     @Test
     fun testMultilineText() {
-        paparazziRule.snapshot {
-            PaymentMethodRowButton(
-                isEnabled = true,
-                isSelected = false,
-                iconContent = {
-                    Image(
-                        painter = painterResource(id = R.drawable.stripe_ic_paymentsheet_pm_card),
-                        contentDescription = null
-                    )
-                },
-                title = "**** 4242",
-                subtitle = "Please click me, I'm fancy",
-                promoText = null,
-                onClick = {},
-                style = FlatWithRadio.default,
-                shouldShowDefaultBadge = false,
-            )
-        }
+        testPaymentMethodRowButton_RadioButton(
+            subtitle = "Please click me, I'm fancy",
+        )
     }
 
     @Test
     fun testMultilineTextTruncation() {
-        paparazziRule.snapshot {
-            PaymentMethodRowButton(
-                isEnabled = true,
-                isSelected = false,
-                iconContent = {
-                    Image(
-                        painter = painterResource(id = R.drawable.stripe_ic_paymentsheet_pm_card),
-                        contentDescription = null
-                    )
-                },
-                title = "**** 4242",
-                subtitle = "Please click me, I'm fancy, but I shouldn't extend a a a a a a a a a a a a a a a a " +
-                    "forever.",
-                promoText = null,
-                onClick = {},
-                style = FlatWithRadio.default,
-                shouldShowDefaultBadge = false,
-            )
-        }
+        testPaymentMethodRowButton_RadioButton(
+            subtitle = "Please click me, I'm fancy, but I shouldn't extend a a a a a a a a a a a a a a a a " +
+                "forever.",
+        )
     }
 
     @Test
     fun testTailingContent() {
-        paparazziRule.snapshot {
-            PaymentMethodRowButton(
-                isEnabled = true,
-                isSelected = false,
-                iconContent = {
-                    Image(
-                        painter = painterResource(id = R.drawable.stripe_ic_paymentsheet_pm_card),
-                        contentDescription = null
-                    )
-                },
-                title = "**** 4242",
-                subtitle = null,
-                promoText = null,
-                onClick = {},
-                style = FlatWithRadio.default,
-                trailingContent = {
-                    Text(text = "View more")
-                },
-                shouldShowDefaultBadge = false,
-            )
-        }
+        testPaymentMethodRowButton_RadioButton(
+            trailingContent = {
+                Text(text = "View more")
+            },
+        )
     }
 
     @Test
@@ -185,26 +97,61 @@ internal class PaymentMethodRowRadioButtonScreenshotTest {
                 unselectedColor = StripeThemeDefaults.colorsDark.materialColors.primary.toArgb()
             )
         )
-        paparazziRule.snapshot {
-            PaymentMethodRowButton(
-                isEnabled = true,
-                isSelected = false,
-                iconContent = {
-                    Image(
-                        painter = painterResource(id = R.drawable.stripe_ic_paymentsheet_pm_card),
-                        contentDescription = null
-                    )
-                },
-                title = "**** 4242",
-                subtitle = null,
-                promoText = null,
-                onClick = {},
-                style = style,
-                trailingContent = {
-                    Text(text = "View more")
-                },
-                shouldShowDefaultBadge = false,
-            )
-        }
+
+        testPaymentMethodRowButton_RadioButton(
+            trailingContent = {
+                Text(text = "View more")
+            },
+            rowStyle = style,
+        )
+    }
+
+    @Test
+    fun testNonTintedIcon() {
+        testPaymentMethodRowButton_RadioButton(
+            iconContent = {
+                DefaultPaymentMethodRowIcon(
+                    iconRes = R.drawable.stripe_ic_paymentsheet_pm_klarna
+                )
+            },
+            title = "Klarna"
+        )
+    }
+
+    @Test
+    fun testLongTitle() {
+        testPaymentMethodRowButton_RadioButton(
+            iconContent = {
+                DefaultPaymentMethodRowIcon(
+                    iconRes = R.drawable.stripe_ic_paymentsheet_pm_klarna
+                )
+            },
+            title = "Buy now or pay later with Klarna"
+        )
+    }
+
+    private fun testPaymentMethodRowButton_RadioButton(
+        isEnabled: Boolean = true,
+        isSelected: Boolean = false,
+        iconContent: @Composable RowScope.() -> Unit = { DefaultPaymentMethodRowIcon() },
+        rowStyle: PaymentSheet.Appearance.Embedded.RowStyle = FlatWithRadio.default,
+        trailingContent: @Composable RowScope.() -> Unit = {},
+        title: String = "**** 4242",
+        subtitle: String? = null,
+        promoText: String? = null,
+        shouldShowDefaultBadge: Boolean = false,
+    ) {
+        testPaymentMethodRowButton(
+            isEnabled = isEnabled,
+            isSelected = isSelected,
+            iconContent = iconContent,
+            title = title,
+            subtitle = subtitle,
+            promoText = promoText,
+            trailingContent = trailingContent,
+            shouldShowDefaultBadge = shouldShowDefaultBadge,
+            rowStyle = rowStyle,
+            paparazziRule = paparazziRule,
+        )
     }
 }
