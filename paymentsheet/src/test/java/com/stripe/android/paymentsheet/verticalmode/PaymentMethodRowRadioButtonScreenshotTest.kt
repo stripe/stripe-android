f94afb0b5cd212bbc5f638a4ca14e1a9556b6f58
tianzhao-stripe <tianzhao@stripe.com>
2025-04-11 17:23:25 -0700

    trailing content

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodRowRadioButtonScreenshotTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodRowRadioButtonScreenshotTest.kt
@@ -1,16 +1,27 @@
 package com.stripe.android.paymentsheet.verticalmode
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.stripe.android.paymentelement.ExperimentalEmbeddedPaymentElementApi
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheet.Appearance.Embedded.RowStyle.FlatWithRadio
+import com.stripe.android.paymentsheet.verticalmode.UIConstants.iconHeight
+import com.stripe.android.paymentsheet.verticalmode.UIConstants.iconWidth
 import com.stripe.android.screenshottesting.FontSize
 import com.stripe.android.screenshottesting.PaparazziRule
 import com.stripe.android.screenshottesting.SystemAppearance
@@ -68,10 +79,10 @@ internal class PaymentMethodRowRadioButtonScreenshotTest {
     }
 
     @Test
-    fun testTailingContent() {
+    fun testTrailingContent() {
         testPaymentMethodRowButton_RadioButton(
             trailingContent = {
-                Text(text = "View more")
+                TrailingContent()
             },
         )
     }
@@ -100,7 +111,7 @@ internal class PaymentMethodRowRadioButtonScreenshotTest {
 
         testPaymentMethodRowButton_RadioButton(
             trailingContent = {
-                Text(text = "View more")
+                TrailingContent()
             },
             rowStyle = style,
         )
@@ -128,6 +139,26 @@ internal class PaymentMethodRowRadioButtonScreenshotTest {
             },
             title = "Buy now or pay later with Klarna"
         )
+    }
+
+    @Composable
+    private fun TrailingContent() {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(vertical = 4.dp)
+                .wrapContentHeight(),
+        ) {
+            Text(text = "View more")
+            Icon(
+                painter = painterResource(com.stripe.android.paymentsheet.R.drawable.stripe_ic_chevron_right),
+                contentDescription = null,
+                tint = MaterialTheme.colors.primary,
+                modifier = Modifier.padding(start = 4.dp, top = 2.dp)
+                    .height(iconHeight).width(iconWidth)
+                    .defaultMinSize(minWidth = iconWidth, minHeight = iconHeight)
+            )
+        }
     }
 
     private fun testPaymentMethodRowButton_RadioButton(
