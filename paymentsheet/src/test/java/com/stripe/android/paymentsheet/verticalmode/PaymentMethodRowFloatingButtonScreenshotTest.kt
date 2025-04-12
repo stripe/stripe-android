cc521d4299c7467c9aeb7c27efd0c33f1945a4f7
tianzhao-stripe <tianzhao@stripe.com>
2025-04-11 18:14:16 -0700

    trailing content

--- paymentsheet/src/test/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodRowFloatingButtonScreenshotTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodRowFloatingButtonScreenshotTest.kt
@@ -1,15 +1,27 @@
 package com.stripe.android.paymentsheet.verticalmode
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.stripe.android.paymentelement.ExperimentalEmbeddedPaymentElementApi
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheet.Appearance.Embedded.RowStyle.FloatingButton
+import com.stripe.android.paymentsheet.verticalmode.UIConstants.iconHeight
+import com.stripe.android.paymentsheet.verticalmode.UIConstants.iconWidth
 import com.stripe.android.screenshottesting.FontSize
 import com.stripe.android.screenshottesting.PaparazziRule
 import com.stripe.android.screenshottesting.SystemAppearance
@@ -67,10 +79,10 @@ internal class PaymentMethodRowFloatingButtonScreenshotTest {
     }
 
     @Test
-    fun testTailingContent() {
+    fun testTrailingContent() {
         testPaymentMethodRowButton_FloatingButton(
             trailingContent = {
-                Text(text = "View more")
+                TrailingContent()
             }
         )
     }
@@ -84,7 +96,7 @@ internal class PaymentMethodRowFloatingButtonScreenshotTest {
                 )
             },
             trailingContent = {
-                Text(text = "View more")
+                TrailingContent()
             },
             title = "US Bank Account",
             promoText = "$5",
@@ -101,7 +113,7 @@ internal class PaymentMethodRowFloatingButtonScreenshotTest {
                 )
             },
             trailingContent = {
-                Text(text = "View more")
+                TrailingContent()
             },
             title = "US Bank Account",
             promoText = "$5",
@@ -116,7 +128,7 @@ internal class PaymentMethodRowFloatingButtonScreenshotTest {
         )
         testPaymentMethodRowButton_FloatingButton(
             trailingContent = {
-                Text(text = "View more")
+                TrailingContent()
             },
             rowStyle = style,
         )
@@ -151,6 +163,27 @@ internal class PaymentMethodRowFloatingButtonScreenshotTest {
             },
             title = "Buy now or pay later with Klarna"
         )
+    }
+
+    @Composable
+    private fun TrailingContent() {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .testTag(TEST_TAG_VIEW_MORE)
+                .padding(vertical = 4.dp)
+                .wrapContentHeight()
+        ) {
+            Text(text = "View more")
+            Icon(
+                painter = painterResource(com.stripe.android.paymentsheet.R.drawable.stripe_ic_chevron_right),
+                contentDescription = null,
+                tint = MaterialTheme.colors.primary,
+                modifier = Modifier.padding(start = 4.dp, top = 2.dp)
+                    .height(iconHeight).width(iconWidth)
+                    .defaultMinSize(minWidth = iconWidth, minHeight = iconHeight)
+            )
+        }
     }
 
     private fun testPaymentMethodRowButton_FloatingButton(
