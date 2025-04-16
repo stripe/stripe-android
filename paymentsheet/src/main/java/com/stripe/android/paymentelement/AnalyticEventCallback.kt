67cdc895fdc0943b52a55597ba9c6dfc979f963a
Chengtin Tsai <cttsai@stripe.com>
2025-04-16 13:40:50 -0700

    Expose the events

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/AnalyticEventCallback.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/AnalyticEventCallback.kt
@@ -1,6 +1,5 @@
 package com.stripe.android.paymentelement
 
-import androidx.annotation.RestrictTo
 import dev.drewhamilton.poko.Poko
 
 /**
@@ -21,7 +20,6 @@ abstract class AnalyticEvent internal constructor() {
     /**
      * User viewed the payment sheet
      */
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class PresentedSheet internal constructor() : AnalyticEvent() {
         override fun toString(): String =
             javaClass.simpleName
@@ -37,48 +35,41 @@ abstract class AnalyticEvent internal constructor() {
      * User selected a payment method type
      */
     @Poko
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class SelectedPaymentMethodType internal constructor(val paymentMethodType: String) : AnalyticEvent()
 
     /**
      * User viewed a payment method form
      */
     @Poko
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class DisplayedPaymentMethodForm internal constructor(val paymentMethodType: String) : AnalyticEvent()
 
     /**
      * User interacted with a payment method form
      */
     @Poko
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class StartedInteractionWithPaymentMethodForm internal constructor(val paymentMethodType: String) : AnalyticEvent()
 
     /**
      * User completed all required payment form fields
      */
     @Poko
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class CompletedPaymentMethodForm internal constructor(val paymentMethodType: String) : AnalyticEvent()
 
     /**
      * User tapped on the confirm button
      */
     @Poko
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class TappedConfirmButton internal constructor(val paymentMethodType: String) : AnalyticEvent()
 
     /**
      * User selected a saved payment method
      */
     @Poko
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class SelectedSavedPaymentMethod internal constructor(val paymentMethodType: String) : AnalyticEvent()
 
     /**
      * User removed a saved payment method
      */
     @Poko
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class RemovedSavedPaymentMethod internal constructor(val paymentMethodType: String) : AnalyticEvent()
 }
