d63b9eeb0ce868ac2141bb29fcd273f4604d78d4
Carlos Munoz <carlosmuvi@stripe.com>
2025-04-16 11:18:19 -0700

    Reorders fields.

--- paymentsheet/src/main/java/com/stripe/android/customersheet/CustomerSheetViewModel.kt
+++ paymentsheet/src/main/java/com/stripe/android/customersheet/CustomerSheetViewModel.kt
@@ -841,7 +841,6 @@ internal class CustomerSheetViewModel(
             onMandateTextChanged = { mandate, showAbove ->
                 handleViewAction(CustomerSheetViewAction.OnUpdateMandateText(mandate, showAbove))
             },
-            onAnalyticsEvent = { /* no-op, CustomerSheetScreen does not send AnalyticEvent */ },
             onLinkedBankAccountChanged = {
                 handleViewAction(CustomerSheetViewAction.OnBankAccountSelectionChanged(it))
             },
@@ -854,6 +853,7 @@ internal class CustomerSheetViewModel(
                 handleViewAction(CustomerSheetViewAction.OnFormError(error))
             },
             onFormCompleted = { /* no-op, CustomerSheetScreen does not send AnalyticEvent */ },
+            onAnalyticsEvent = { /* no-op, CustomerSheetScreen does not send AnalyticEvent */ },
             setAsDefaultPaymentMethodEnabled = false,
             financialConnectionsAvailability = GetFinancialConnectionsAvailability(elementsSession = null),
             setAsDefaultMatchesSaveForFutureUse = FORM_ELEMENT_SET_DEFAULT_MATCHES_SAVE_FOR_FUTURE_DEFAULT_VALUE,
