ac527fca2806d67e1b51d24ecce53d3dbfa0c78a
Jay Newstrom <jaynewstrom@stripe.com>
2025-04-17 09:30:35 -0600

    Fix screenshot test flakes.

--- paymentsheet-example/src/androidTest/java/com/stripe/android/test/core/PlaygroundTestDriver.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/test/core/PlaygroundTestDriver.kt
@@ -1152,6 +1152,7 @@ internal class PlaygroundTestDriver(
 
         waitForScreenToLoad(testParameters)
         customOperations()
+        composeTestRule.waitForIdle()
 
         currentActivity?.let {
             compareScreenshot(it)
