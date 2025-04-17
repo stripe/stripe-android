5367c37183f29291dfa3f4bb5caea440032d5d8a
Jay Newstrom <jaynewstrom@stripe.com>
2025-04-17 07:48:30 -0600

    FormSheetAction integration tests.

--- paymentsheet/src/androidTest/java/com/stripe/android/paymentelement/EmbeddedPaymentElementTest.kt
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentelement/EmbeddedPaymentElementTest.kt
@@ -32,9 +32,76 @@ internal class EmbeddedPaymentElementTest {
     private val embeddedContentPage = EmbeddedContentPage(testRules.compose)
     private val managePage = ManagePage(testRules.compose)
     private val editPage = EditPage(testRules.compose)
+    private val formPage = EmbeddedFormPage(testRules.compose)
 
     private val card1 = CardPaymentMethodDetails("pm_12345", "4242")
     private val card2 = CardPaymentMethodDetails("pm_67890", "5544")
+
+    @Test
+    fun testSuccessfulCardPayment_withFormSheetActionConfirm() = runEmbeddedPaymentElementTest(
+        networkRule = networkRule,
+        createIntentCallback = { _, shouldSavePaymentMethod ->
+            assertThat(shouldSavePaymentMethod).isFalse()
+            CreateIntentResult.Success("pi_example_secret_12345")
+        },
+        resultCallback = ::assertCompleted,
+    ) { testContext ->
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-requires_payment_method.json")
+        }
+
+        testContext.configure {
+            formSheetAction(EmbeddedPaymentElement.FormSheetAction.Confirm)
+        }
+
+        embeddedContentPage.clickOnLpm("card")
+        formPage.fillOutCardDetails()
+
+        enqueueDeferredIntentConfirmationRequests()
+
+        formPage.clickPrimaryButton()
+        formPage.waitUntilMissing()
+    }
+
+    @Test
+    fun testSuccessfulCardPayment_withFormSheetActionContinue() = runEmbeddedPaymentElementTest(
+        networkRule = networkRule,
+        createIntentCallback = { _, shouldSavePaymentMethod ->
+            assertThat(shouldSavePaymentMethod).isFalse()
+            CreateIntentResult.Success("pi_example_secret_12345")
+        },
+        resultCallback = ::assertCompleted,
+    ) { testContext ->
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-requires_payment_method.json")
+        }
+
+        testContext.configure {
+            formSheetAction(EmbeddedPaymentElement.FormSheetAction.Continue)
+        }
+
+        embeddedContentPage.clickOnLpm("card")
+        formPage.fillOutCardDetails()
+        formPage.clickPrimaryButton()
+        formPage.waitUntilMissing()
+
+        embeddedContentPage.assertHasSelectedLpm("card")
+        embeddedContentPage.clickOnLpm("card")
+        formPage.clickPrimaryButton() // Ensures the form has the previous values.
+        formPage.waitUntilMissing()
+
+        enqueueDeferredIntentConfirmationRequests()
+
+        testContext.confirm()
+    }
 
     @Test
     fun testRemoveCard() = runEmbeddedPaymentElementTest(
@@ -129,6 +196,28 @@ internal class EmbeddedPaymentElementTest {
             embeddedContentPage.clickViewMore()
 
             testContext.markTestSucceeded()
+        }
+    }
+
+    private fun enqueueDeferredIntentConfirmationRequests() {
+        networkRule.enqueue(
+            method("POST"),
+            path("/v1/payment_methods"),
+        ) { response ->
+            response.testBodyFromFile("payment-methods-create.json")
+        }
+        networkRule.enqueue(
+            method("GET"),
+            path("/v1/payment_intents/pi_example"),
+        ) { response ->
+            response.testBodyFromFile("payment-intent-get-requires_payment_method.json")
+        }
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("POST"),
+            path("/v1/payment_intents/pi_example/confirm"),
+        ) { response ->
+            response.testBodyFromFile("payment-intent-confirm.json")
         }
     }
 }
