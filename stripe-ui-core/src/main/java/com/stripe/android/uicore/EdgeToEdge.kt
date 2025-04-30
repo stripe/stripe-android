1b170b92a43a2fd84219705a001229c50a732a91
toluo-stripe <toluo@stripe.com>
2025-04-30 16:31:19 -0400

    Update EdgeToEdge.kt

--- stripe-ui-core/src/main/java/com/stripe/android/uicore/EdgeToEdge.kt
+++ stripe-ui-core/src/main/java/com/stripe/android/uicore/EdgeToEdge.kt
@@ -2,7 +2,9 @@ package com.stripe.android.uicore
 
 import android.app.Activity
 import android.os.Build
+import androidx.annotation.RestrictTo
 
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 fun Activity.disableNavigationBarContrastEnforcement() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
         window.isNavigationBarContrastEnforced = false
