278e425006536390a90dfc7ae693f6a76f3363a2
tianzhao-stripe <tianzhao@stripe.com>
2025-05-19 18:12:37 -0700

    handleImmediateRowActionCallback

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/content/PaymentOptionDisplayDataHolder.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/content/PaymentOptionDisplayDataHolder.kt
@@ -3,15 +3,20 @@
 package com.stripe.android.paymentelement.embedded.content
 
 import com.stripe.android.core.injection.ViewModelScope
+import com.stripe.android.paymentelement.EmbeddedPaymentElement
 import com.stripe.android.paymentelement.EmbeddedPaymentElement.PaymentOptionDisplayData
 import com.stripe.android.paymentelement.ExperimentalEmbeddedPaymentElementApi
+import com.stripe.android.paymentelement.InternalRowSelectionCallback
 import com.stripe.android.paymentelement.embedded.EmbeddedSelectionHolder
+import com.stripe.android.paymentsheet.analytics.code
+import com.stripe.android.paymentsheet.model.PaymentSelection
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import javax.inject.Provider
 import javax.inject.Singleton
 
 internal interface PaymentOptionDisplayDataHolder {
@@ -24,6 +29,7 @@ internal class DefaultPaymentOptionDisplayDataHolder @Inject constructor(
     private val selectionHolder: EmbeddedSelectionHolder,
     private val confirmationStateSupplier: () -> EmbeddedConfirmationStateHolder.State?,
     private val paymentOptionDisplayDataFactory: PaymentOptionDisplayDataFactory,
+    private val internalRowSelectionCallback: Provider<InternalRowSelectionCallback?>,
 ) : PaymentOptionDisplayDataHolder {
     private val _paymentOption = MutableStateFlow<PaymentOptionDisplayData?>(null)
     override val paymentOption: StateFlow<PaymentOptionDisplayData?> = _paymentOption.asStateFlow()
@@ -39,6 +45,31 @@ internal class DefaultPaymentOptionDisplayDataHolder @Inject constructor(
                         selection = selection,
                         paymentMethodMetadata = state.paymentMethodMetadata,
                     )
+                    handleImmediateRowActionCallback(
+                        selection = selection,
+                        state = state,
+                    )
+                }
+            }
+        }
+    }
+
+    private fun handleImmediateRowActionCallback(
+        selection: PaymentSelection?,
+        state: EmbeddedConfirmationStateHolder.State
+    ) {
+        internalRowSelectionCallback.get()?.let { callback ->
+            selection?.code()?.let { code ->
+                val hasForms = state.paymentMethodMetadata.formElementsForCode(
+                    code = code,
+                    uiDefinitionFactoryArgumentsFactory = NullUiDefinitionFactoryHelper.nullEmbeddedUiDefinitionFactory
+                )?.isNotEmpty() ?: false
+
+                if (
+                    !hasForms ||
+                    state.configuration.formSheetAction == EmbeddedPaymentElement.FormSheetAction.Continue
+                ) {
+                    callback.invoke()
                 }
             }
         }
