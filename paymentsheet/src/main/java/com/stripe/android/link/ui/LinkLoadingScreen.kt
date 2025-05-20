b037cf7ed9bdfd57e5dd11692c3aaf116f51df39
Larry Ng <lng@stripe.com>
2025-05-20 14:13:09 -0400

    clean up

--- paymentsheet/src/main/java/com/stripe/android/link/ui/LinkLoadingScreen.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/LinkLoadingScreen.kt
@@ -6,33 +6,62 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import com.stripe.android.link.theme.DefaultLinkTheme
+
+private val LocalLinkScreenSize = compositionLocalOf<DpSize?> { null }
 
 /**
  * Displays a loading spinner in the center of the screen.
- * Tries to match the last screen size using [LocalLinkScreenSize] to minimize size changes.
+ * Will match the last screen size using [ProvideLinkScreenSize] to minimize size changes.
  */
 @Composable
 internal fun LinkLoadingScreen(modifier: Modifier = Modifier) {
     val screenSize = LocalLinkScreenSize.current
-    val screenSizeDp = with(LocalDensity.current) {
-        screenSize?.let { DpSize(it.width.toDp(), it.height.toDp()) }
-    }
     Box(
         modifier = modifier
-            .then(if (screenSizeDp != null) Modifier.heightIn(min = screenSizeDp.height) else Modifier)
+            .then(if (screenSize != null) Modifier.heightIn(min = screenSize.height) else Modifier)
             .fillMaxWidth()
-            .padding(64.dp),
+            .padding(vertical = 64.dp),
         contentAlignment = Alignment.Center
     ) {
         LinkSpinner(modifier = Modifier.size(48.dp))
     }
 }
 
-internal val LocalLinkScreenSize = compositionLocalOf<IntSize?> { null }
+@Composable
+internal fun ProvideLinkScreenSize(size: IntSize?, content: @Composable () -> Unit) {
+    val dpSize = with(LocalDensity.current) {
+        size?.let { DpSize(it.width.toDp(), it.height.toDp()) }
+    }
+    CompositionLocalProvider(
+        LocalLinkScreenSize provides dpSize,
+        content = content
+    )
+}
+
+@Preview
+@Composable
+private fun LinkLoadingScreenPreview() {
+    DefaultLinkTheme {
+        LinkLoadingScreen()
+    }
+}
+
+@Preview
+@Composable
+private fun LinkLoadingScreenWithScreenSizePreview() {
+    DefaultLinkTheme {
+        CompositionLocalProvider(LocalLinkScreenSize provides DpSize(200.dp, 500.dp)) {
+            LinkLoadingScreen()
+        }
+    }
+}
