c6616b4cb7b69a04ad00250a84e2df2cb9f41573
Larry Ng <lng@stripe.com>
2025-05-20 14:13:09 -0400

    clean up

--- paymentsheet/src/main/java/com/stripe/android/link/ui/LinkContent.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/LinkContent.kt
@@ -7,19 +7,16 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imeAnimationSource
 import androidx.compose.foundation.layout.imeAnimationTarget
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -108,7 +105,6 @@ internal fun LinkContent(
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 private fun Screens(
     navController: NavHostController,
@@ -122,15 +118,91 @@ private fun Screens(
     changeEmail: () -> Unit,
     initialDestination: LinkScreen,
 ) {
+    LinkNavHost(
+        navController = navController,
+        initialDestination = initialDestination,
+    ) {
+        composable(LinkScreen.Loading.route) {
+            LinkLoadingScreen()
+        }
+
+        composable(LinkScreen.SignUp.route) {
+            // Keep height fixed to reduce animations caused by IME toggling on both
+            // this screen and Verification screen.
+            MinScreenHeightBox {
+                SignUpRoute(
+                    navigateAndClearStack = navigateAndClearStack,
+                    moveToWeb = moveToWeb
+                )
+            }
+        }
+
+        composable(
+            LinkScreen.UpdateCard.route
+        ) { backStackEntry ->
+            val paymentDetailsId = backStackEntry.arguments?.getString(EXTRA_PAYMENT_DETAILS)
+                ?: return@composable dismissWithResult(noPaymentDetailsResult())
+            UpdateCardRoute(
+                paymentDetailsId = paymentDetailsId
+            )
+        }
+
+        composable(LinkScreen.Verification.route) {
+            // Keep height fixed to reduce animations caused by IME toggling on both
+            // this screen and SignUp screen.
+            MinScreenHeightBox {
+                val linkAccount = getLinkAccount()
+                    ?: return@MinScreenHeightBox dismissWithResult(noLinkAccountResult())
+                VerificationRoute(
+                    linkAccount = linkAccount,
+                    changeEmail = changeEmail,
+                    navigateAndClearStack = navigateAndClearStack,
+                    goBack = goBack
+                )
+            }
+        }
+
+        composable(LinkScreen.Wallet.route) {
+            val linkAccount = getLinkAccount()
+                ?: return@composable dismissWithResult(noLinkAccountResult())
+            WalletRoute(
+                linkAccount = linkAccount,
+                navigateAndClearStack = navigateAndClearStack,
+                showBottomSheetContent = showBottomSheetContent,
+                hideBottomSheetContent = hideBottomSheetContent,
+                dismissWithResult = dismissWithResult
+            )
+        }
+
+        composable(LinkScreen.PaymentMethod.route) {
+            val linkAccount = getLinkAccount()
+                ?: return@composable dismissWithResult(noLinkAccountResult())
+            PaymentMethodRoute(
+                linkAccount = linkAccount,
+                dismissWithResult = dismissWithResult,
+                goBack = goBack
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun LinkNavHost(
+    navController: NavHostController,
+    initialDestination: LinkScreen,
+    modifier: Modifier = Modifier,
+    builder: NavGraphBuilder.() -> Unit
+) {
     // True if the IME (soft keyboard) is animating.
     val isImeAnimating = WindowInsets.imeAnimationSource != WindowInsets.imeAnimationTarget
 
     // Cache the current screen size so we can size `LinkLoadingScreen` to match, reducing
     // screen size animation jumps.
     var screenSize by remember { mutableStateOf<IntSize?>(null) }
-    CompositionLocalProvider(LocalLinkScreenSize provides screenSize) {
+    ProvideLinkScreenSize(screenSize) {
         NavHost(
-            modifier = Modifier.onSizeChanged { screenSize = it },
+            modifier = modifier.onSizeChanged { screenSize = it },
             navController = navController,
             startDestination = initialDestination.route,
             enterTransition = { LinkScreenTransition.targetContentEnter },
@@ -140,72 +212,13 @@ private fun Screens(
             // supposed to be equal height. If the soft keyboard closes immediately after the
             // screen transition starts, the target height of screen B, which is locked in at
             // the start of the animation, will be too short.
-            sizeTransform = if (isImeAnimating) null else {
+            sizeTransform = if (isImeAnimating) {
+                null
+            } else {
                 { LinkScreenTransition.sizeTransform }
             },
-        ) {
-            composable(LinkScreen.Loading.route) {
-                LinkLoadingScreen()
-            }
-
-            composable(LinkScreen.SignUp.route) {
-                // Keep height fixed to reduce animations caused by IME toggling on both
-                // this screen and Verification screen.
-                MinScreenHeightBox {
-                    SignUpRoute(
-                        navigateAndClearStack = navigateAndClearStack,
-                        moveToWeb = moveToWeb
-                    )
-                }
-            }
-
-            composable(
-                LinkScreen.UpdateCard.route
-            ) { backStackEntry ->
-                val paymentDetailsId = backStackEntry.arguments?.getString(EXTRA_PAYMENT_DETAILS)
-                    ?: return@composable dismissWithResult(noPaymentDetailsResult())
-                UpdateCardRoute(
-                    paymentDetailsId = paymentDetailsId
-                )
-            }
-
-            composable(LinkScreen.Verification.route) {
-                // Keep height fixed to reduce animations caused by IME toggling on both
-                // this screen and SignUp screen.
-                MinScreenHeightBox {
-                    val linkAccount = getLinkAccount()
-                        ?: return@MinScreenHeightBox dismissWithResult(noLinkAccountResult())
-                    VerificationRoute(
-                        linkAccount = linkAccount,
-                        changeEmail = changeEmail,
-                        navigateAndClearStack = navigateAndClearStack,
-                        goBack = goBack
-                    )
-                }
-            }
-
-            composable(LinkScreen.Wallet.route) {
-                val linkAccount = getLinkAccount()
-                    ?: return@composable dismissWithResult(noLinkAccountResult())
-                WalletRoute(
-                    linkAccount = linkAccount,
-                    navigateAndClearStack = navigateAndClearStack,
-                    showBottomSheetContent = showBottomSheetContent,
-                    hideBottomSheetContent = hideBottomSheetContent,
-                    dismissWithResult = dismissWithResult
-                )
-            }
-
-            composable(LinkScreen.PaymentMethod.route) {
-                val linkAccount = getLinkAccount()
-                    ?: return@composable dismissWithResult(noLinkAccountResult())
-                PaymentMethodRoute(
-                    linkAccount = linkAccount,
-                    dismissWithResult = dismissWithResult,
-                    goBack = goBack
-                )
-            }
-        }
+            builder = builder,
+        )
     }
 }
 
@@ -301,14 +314,6 @@ private fun WalletRoute(
         showBottomSheetContent = showBottomSheetContent,
         hideBottomSheetContent = hideBottomSheetContent
     )
-}
-
-@Preview
-@Composable
-private fun LinkLoadingScreenPreview() {
-    DefaultLinkTheme {
-        LinkLoadingScreen(Modifier.padding(64.dp))
-    }
 }
 
 private fun noLinkAccountResult(): LinkActivityResult {
