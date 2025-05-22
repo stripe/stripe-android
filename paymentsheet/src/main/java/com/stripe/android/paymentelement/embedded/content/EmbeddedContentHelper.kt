b0fbe33e914860e8d6d0f2a99bace83a465d580c
tianzhao-stripe <tianzhao@stripe.com>
2025-05-22 11:02:28 -0700

    remove path comments form/non form

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/content/EmbeddedContentHelper.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/content/EmbeddedContentHelper.kt
@@ -191,9 +191,8 @@ internal class DefaultEmbeddedContentHelper @Inject constructor(
             canRemove = customerStateHolder.canRemove,
             canUpdateFullPaymentMethodDetails = customerStateHolder.canUpdateFullPaymentMethodDetails,
             onSelectSavedPaymentMethod = {
-                // there is never a form for saved paymentMethods
                 val selection = PaymentSelection.Saved(it)
-                prepareRowSelectionCallbackSavedPaymentRow(selection) //HappyPath1 shouldInvokeSavedPaymentMethod true
+                prepareRowSelectionCallbackSavedPaymentRow(selection)
                 setSelection(selection)
             },
             walletsState = walletsState,
@@ -218,11 +217,11 @@ internal class DefaultEmbeddedContentHelper @Inject constructor(
                 }
             },
             linkRowClicked = { updatedSelection ->
-                prepareRowSelectionCallbackNonFormRows(updatedSelection.code()!!) // HappyPath 1
+                prepareRowSelectionCallbackNonFormRows(updatedSelection.code()!!)
                 setSelection(updatedSelection)
             },
             googlePayRowClicked = { updatedSelection ->
-                prepareRowSelectionCallbackNonFormRows(updatedSelection.code()!!) // HappyPath 1
+                prepareRowSelectionCallbackNonFormRows(updatedSelection.code()!!)
                 setSelection(updatedSelection)
             }
         )
@@ -258,7 +257,7 @@ internal class DefaultEmbeddedContentHelper @Inject constructor(
     }
 
     private fun setSelection(paymentSelection: PaymentSelection?) {
-        selectionHolder.set(paymentSelection) // Weird Paths when savedPMM, is covered by onFormFieldValuesChanged, savedPMSelected, updatePaymentSelection
+        selectionHolder.set(paymentSelection)
     }
 
     private fun prepareRowSelectionCallbackSavedPaymentRow(paymentSelection: PaymentSelection.Saved) {
