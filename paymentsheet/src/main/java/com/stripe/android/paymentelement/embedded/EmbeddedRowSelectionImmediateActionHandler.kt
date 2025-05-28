e49723b46484d21d96b862d3a539338403c6ef7f
tianzhao-stripe <tianzhao@stripe.com>
2025-05-28 10:53:25 -0700

    simplify with coroutines

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/EmbeddedRowSelectionImmediateActionHandler.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/embedded/EmbeddedRowSelectionImmediateActionHandler.kt
@@ -1,11 +1,8 @@
 package com.stripe.android.paymentelement.embedded
 
 import com.stripe.android.core.injection.ViewModelScope
-import com.stripe.android.paymentelement.ExperimentalEmbeddedPaymentElementApi
-import com.stripe.android.paymentelement.embedded.content.DefaultPaymentOptionDisplayDataHolder
 import com.stripe.android.paymentsheet.model.PaymentSelection
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Provider
@@ -18,27 +15,17 @@ internal interface EmbeddedRowSelectionImmediateActionHandler {
     fun handleImmediateRowSelectionCallback(oldSelection: PaymentSelection?, newSelection: PaymentSelection?)
 }
 
-@OptIn(ExperimentalEmbeddedPaymentElementApi::class)
 @Singleton
 internal class DefaultEmbeddedRowSelectionImmediateActionHandler @Inject constructor(
     @ViewModelScope private val coroutineScope: CoroutineScope,
     private val internalRowSelectionCallback: Provider<InternalRowSelectionCallback?>,
-    private val paymentOptionDisplayDataHolder: DefaultPaymentOptionDisplayDataHolder,
 ) : EmbeddedRowSelectionImmediateActionHandler {
 
+    /**
+     * This callback will always be invoked after [DefaultPaymentOptionDisplayDataHolder.paymentOption] updates.
+     * The update paymentOption coroutine is always enqueued first because it is always older, it is launched on init.
+     */
     override fun handleImmediateRowSelectionCallback(oldSelection: PaymentSelection?, newSelection: PaymentSelection?) {
-        internalRowSelectionCallback.get()?.let { callback ->
-            newSelection?.let {
-                if (oldSelection != null && oldSelection == newSelection) {
-                    callback.invoke()
-                } else {
-                    coroutineScope.launch {
-                        paymentOptionDisplayDataHolder.paymentOption.firstOrNull()?.let {
-                            callback.invoke()
-                        }
-                    }
-                }
-            }
-        }
+        internalRowSelectionCallback.get()?.let { callback -> coroutineScope.launch { callback.invoke() } }
     }
 }
