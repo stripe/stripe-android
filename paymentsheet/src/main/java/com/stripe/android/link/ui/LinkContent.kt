5d47ad6df87a89411d469bee1104e3b2203f151b
Larry Ng <lng@stripe.com>
2025-05-29 15:07:39 -0400

    minor refactoring

--- paymentsheet/src/main/java/com/stripe/android/link/ui/LinkContent.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/LinkContent.kt
@@ -2,23 +2,11 @@ package com.stripe.android.link.ui
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imeAnimationSource
-import androidx.compose.foundation.layout.imeAnimationTarget
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.unit.IntSize
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.stripe.android.link.LinkAccountUpdate
 import com.stripe.android.link.LinkAction
@@ -41,7 +29,6 @@ import com.stripe.android.link.ui.verification.VerificationScreen
 import com.stripe.android.link.ui.verification.VerificationViewModel
 import com.stripe.android.link.ui.wallet.WalletScreen
 import com.stripe.android.link.ui.wallet.WalletViewModel
-import com.stripe.android.link.utils.LinkScreenTransition
 
 @SuppressWarnings("LongMethod")
 @Composable
@@ -120,7 +107,7 @@ private fun Screens(
 ) {
     LinkNavHost(
         navController = navController,
-        initialDestination = initialDestination,
+        startDestination = initialDestination.route,
     ) {
         composable(LinkScreen.Loading.route) {
             LinkLoadingScreen()
@@ -183,42 +170,6 @@ private fun Screens(
                 goBack = goBack
             )
         }
-    }
-}
-
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-private fun LinkNavHost(
-    navController: NavHostController,
-    initialDestination: LinkScreen,
-    modifier: Modifier = Modifier,
-    builder: NavGraphBuilder.() -> Unit
-) {
-    // True if the IME (soft keyboard) is animating.
-    val isImeAnimating = WindowInsets.imeAnimationSource != WindowInsets.imeAnimationTarget
-
-    // Cache the current screen size so we can size `LinkLoadingScreen` to match, reducing
-    // screen size animation jumps.
-    var screenSize by remember { mutableStateOf<IntSize?>(null) }
-    ProvideLinkScreenSize(screenSize) {
-        NavHost(
-            modifier = modifier.onSizeChanged { screenSize = it },
-            navController = navController,
-            startDestination = initialDestination.route,
-            enterTransition = { LinkScreenTransition.targetContentEnter },
-            exitTransition = { LinkScreenTransition.initialContentExit },
-            // Workaround a race condition where the route changes while the soft keyboard is
-            // animating in/out. Imagine navigating from screen A to B, where both screens are
-            // supposed to be equal height. If the soft keyboard closes immediately after the
-            // screen transition starts, the target height of screen B, which is locked in at
-            // the start of the animation, will be too short.
-            sizeTransform = if (isImeAnimating) {
-                null
-            } else {
-                { LinkScreenTransition.sizeTransform }
-            },
-            builder = builder,
-        )
     }
 }
 
--- paymentsheet/src/main/java/com/stripe/android/link/ui/LinkLoadingScreen.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/LinkLoadingScreen.kt
@@ -6,46 +6,31 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.stripe.android.link.theme.DefaultLinkTheme
 
-private val LocalLinkScreenSize = compositionLocalOf<DpSize?> { null }
-
 /**
  * Displays a loading spinner in the center of the screen.
- * Will match the last screen size using [ProvideLinkScreenSize] to minimize size changes.
+ * Will match the last screen size as provided in [LocalLinkScreenSize] to minimize size changes.
  */
 @Composable
-internal fun LinkLoadingScreen(modifier: Modifier = Modifier) {
-    val screenSize = LocalLinkScreenSize.current
+internal fun LinkLoadingScreen(
+    modifier: Modifier = Modifier,
+    minHeight: Dp? = LocalLinkScreenSize.current?.height,
+) {
     Box(
         modifier = modifier
-            .then(if (screenSize != null) Modifier.heightIn(min = screenSize.height) else Modifier)
+            .then(minHeight?.let { Modifier.heightIn(min = it) } ?: Modifier)
             .fillMaxWidth()
             .padding(vertical = 64.dp),
         contentAlignment = Alignment.Center
     ) {
         LinkSpinner(modifier = Modifier.size(48.dp))
     }
-}
-
-@Composable
-internal fun ProvideLinkScreenSize(size: IntSize?, content: @Composable () -> Unit) {
-    val dpSize = with(LocalDensity.current) {
-        size?.let { DpSize(it.width.toDp(), it.height.toDp()) }
-    }
-    CompositionLocalProvider(
-        LocalLinkScreenSize provides dpSize,
-        content = content
-    )
 }
 
 @Preview
@@ -60,8 +45,6 @@ private fun LinkLoadingScreenPreview() {
 @Composable
 private fun LinkLoadingScreenWithScreenSizePreview() {
     DefaultLinkTheme {
-        CompositionLocalProvider(LocalLinkScreenSize provides DpSize(200.dp, 500.dp)) {
-            LinkLoadingScreen()
-        }
+        LinkLoadingScreen(minHeight = 500.dp)
     }
 }
