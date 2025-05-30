0cae6b31dbca9305f0d6dddbc37b80cb7a94509d
Larry Ng <lng@stripe.com>
2025-05-30 13:33:54 -0400

    reduce even more size changes

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/PaymentSheetScreen.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/PaymentSheetScreen.kt
@@ -17,7 +17,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredSize
@@ -125,10 +129,18 @@ private fun PaymentSheetScreen(
 
     val density = LocalDensity.current
     var contentHeight by remember { mutableStateOf(0.dp) }
+    val minContentHeight =
+        PaymentSheetTopBarHeight + WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
 
     DismissKeyboardOnProcessing(processing)
 
     BottomSheetScaffold(
+        modifier = Modifier
+            .onGloballyPositioned {
+                contentHeight = with(density) { it.size.height.toDp() }
+            }
+            // Set a min height to reduce size changes during initialization.
+            .heightIn(min = minContentHeight),
         topBar = {
             val currentScreen by viewModel.navigationHandler.currentScreen.collectAsState()
             val topBarState by remember(currentScreen) {
@@ -149,9 +161,6 @@ private fun PaymentSheetScreen(
                 content = { content() }
             )
         },
-        modifier = Modifier.onGloballyPositioned {
-            contentHeight = with(density) { it.size.height.toDp() }
-        },
         scrollState = scrollState,
     )
 
@@ -168,7 +177,10 @@ private fun PaymentSheetScreen(
                 .fillMaxWidth()
                 .background(MaterialTheme.colors.surface.copy(alpha = 0.9f)),
         ) {
-            ProgressOverlay(walletsProcessingState)
+            ProgressOverlay(
+                contentVisible = contentVisible,
+                walletsProcessingState = walletsProcessingState
+            )
         }
     }
 }
@@ -229,17 +241,22 @@ private fun ResetScroll(scrollState: ScrollState, currentScreen: PaymentSheetScr
 
 @Suppress("UnusedReceiverParameter")
 @Composable
-private fun BoxScope.ProgressOverlay(walletsProcessingState: WalletsProcessingState?) {
+private fun BoxScope.ProgressOverlay(
+    contentVisible: Boolean,
+    walletsProcessingState: WalletsProcessingState?
+) {
     AnimatedContent(
         targetState = walletsProcessingState,
         label = "AnimatedProcessingState"
     ) { processingState ->
+        if (!contentVisible) {
+            ProgressOverlayProcessing()
+            return@AnimatedContent
+        }
         when (processingState) {
-            is WalletsProcessingState.Processing -> CircularProgressIndicator(
-                color = MaterialTheme.colors.onSurface,
-                strokeWidth = dimensionResource(R.dimen.stripe_paymentsheet_loading_indicator_stroke_width),
-                modifier = Modifier.requiredSize(48.dp),
-            )
+            is WalletsProcessingState.Processing -> {
+                ProgressOverlayProcessing()
+            }
             is WalletsProcessingState.Completed -> {
                 LaunchedEffect(processingState) {
                     delay(POST_SUCCESS_ANIMATION_DELAY)
@@ -257,6 +274,15 @@ private fun BoxScope.ProgressOverlay(walletsProcessingState: WalletsProcessingSt
             is WalletsProcessingState.Idle -> Unit
         }
     }
+}
+
+@Composable
+private fun ProgressOverlayProcessing() {
+    CircularProgressIndicator(
+        color = MaterialTheme.colors.onSurface,
+        strokeWidth = dimensionResource(R.dimen.stripe_paymentsheet_loading_indicator_stroke_width),
+        modifier = Modifier.requiredSize(48.dp),
+    )
 }
 
 @Composable
