6fa9bbdc1cbfc36b0f8da7339cec6922ba6c61b4
toluo-stripe <toluo@stripe.com>
2025-06-02 11:12:57 -0400

    Update PaymentOptionFactory.kt

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/model/PaymentOptionFactory.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/model/PaymentOptionFactory.kt
@@ -34,6 +34,7 @@ internal val PaymentSelection.shippingDetails: AddressDetails?
         is PaymentSelection.New.GenericPaymentMethod,
         is PaymentSelection.New.LinkInline,
         is PaymentSelection.New.USBankAccount,
+        is PaymentSelection.ShopPay,
         is PaymentSelection.Saved -> {
             null
         }
