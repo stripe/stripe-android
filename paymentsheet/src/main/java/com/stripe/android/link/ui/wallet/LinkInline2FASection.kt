06c05b3369da39fbae1c1f7123714d8ed0f21905
Carlos Munoz <carlosmuvi@stripe.com>
2025-06-10 16:49:20 +0200

    Reverts inline signup changes.

--- paymentsheet/src/main/java/com/stripe/android/link/ui/wallet/LinkInline2FASection.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/wallet/LinkInline2FASection.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
