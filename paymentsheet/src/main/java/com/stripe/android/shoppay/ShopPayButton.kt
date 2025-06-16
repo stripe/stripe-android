345ed6fd6a3787da33e92cb06ba5f0d038601990
toluo-stripe <toluo@stripe.com>
2025-06-16 10:33:35 -0400

    Shop Pay Button UI

---
+++ paymentsheet/src/main/java/com/stripe/android/shoppay/ShopPayButton.kt
@@ -0,0 +1,50 @@
+package com.stripe.android.shoppay
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.stripe.android.paymentsheet.R
+import com.stripe.android.uicore.StripeTheme
+
+private val backgroundColor = Color(0xFF5433EB)
+
+@Composable
+internal fun ShopPayButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        enabled = true,
+        shape = RoundedCornerShape(
+            StripeTheme.primaryButtonStyle.shape.cornerRadius.dp
+        ),
+        elevation = ButtonDefaults.elevation(0.dp, 0.dp, 0.dp, 0.dp, 0.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = backgroundColor,
+        ),
+        contentPadding = PaddingValues(
+            start = 25.dp,
+            top = 10.dp,
+            end = 25.dp,
+            bottom = 10.dp
+        )
+    ) {
+        Image(
+            painter = painterResource(R.drawable.shop_pay_logo_white),
+            contentDescription = "Shop Pay Logo"
+        )
+    }
+}
---
+++ paymentsheet/src/test/java/com/stripe/android/shoppay/ShopPayButtonScreenshotTest.kt
@@ -0,0 +1,29 @@
+package com.stripe.android.shoppay
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.stripe.android.screenshottesting.FontSize
+import com.stripe.android.screenshottesting.PaparazziRule
+import com.stripe.android.screenshottesting.SystemAppearance
+import org.junit.Rule
+import org.junit.Test
+
+internal class ShopPayButtonScreenshotTest {
+    @get:Rule
+    val paparazziRule = PaparazziRule(
+        SystemAppearance.entries,
+        FontSize.entries,
+        boxModifier = Modifier
+            .padding(0.dp)
+            .fillMaxWidth(),
+    )
+
+    @Test
+    fun testButton() {
+        paparazziRule.snapshot {
+            ShopPayButton { }
+        }
+    }
+}
