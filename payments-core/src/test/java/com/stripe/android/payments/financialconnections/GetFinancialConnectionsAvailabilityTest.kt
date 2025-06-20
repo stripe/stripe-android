7b1076851cae176963be9f8df2225c033b8e8203
Carlos Munoz <carlosmuvi@stripe.com>
2025-06-20 11:36:15 +0200

    Adds tests.

--- payments-core/src/test/java/com/stripe/android/payments/financialconnections/GetFinancialConnectionsAvailabilityTest.kt
+++ payments-core/src/test/java/com/stripe/android/payments/financialconnections/GetFinancialConnectionsAvailabilityTest.kt
@@ -3,6 +3,7 @@ package com.stripe.android.payments.financialconnections
 import com.stripe.android.core.utils.FeatureFlags.financialConnectionsFullSdkUnavailable
 import com.stripe.android.model.ElementsSession
 import com.stripe.android.model.ElementsSession.Flag.ELEMENTS_DISABLE_FC_LITE
+import com.stripe.android.model.ElementsSession.Flag.ELEMENTS_PREFER_FC_LITE
 import com.stripe.android.testing.FeatureFlagTestRule
 import org.junit.Rule
 import org.mockito.kotlin.doReturn
@@ -17,6 +18,37 @@ class GetFinancialConnectionsAvailabilityTest {
         featureFlag = financialConnectionsFullSdkUnavailable,
         isEnabled = false
     )
+
+    @Test
+    fun `when prefer lite flag is enabled should return Lite regardless of full SDK availability`() {
+        val elementsSession = createSession(
+            mapOf(ELEMENTS_PREFER_FC_LITE to true)
+        )
+        assertEquals(
+            FinancialConnectionsAvailability.Lite,
+            GetFinancialConnectionsAvailability(
+                elementsSession = elementsSession,
+                isFullSdkAvailable = isFinancialConnectionsFullSdkAvailable(true)
+            )
+        )
+    }
+
+    @Test
+    fun `when prefer lite flag is enabled should take priority over killswitch`() {
+        val elementsSession = createSession(
+            mapOf(
+                ELEMENTS_PREFER_FC_LITE to true,
+                ELEMENTS_DISABLE_FC_LITE to true
+            )
+        )
+        assertEquals(
+            FinancialConnectionsAvailability.Lite,
+            GetFinancialConnectionsAvailability(
+                elementsSession = elementsSession,
+                isFullSdkAvailable = isFinancialConnectionsFullSdkAvailable(false)
+            )
+        )
+    }
 
     @Test
     fun `when full SDK available and not unavailable should return full`() {
