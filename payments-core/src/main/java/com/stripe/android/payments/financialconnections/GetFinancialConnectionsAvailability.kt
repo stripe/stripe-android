2f761181c03b17802239d76f68f83a7c514cadfc
Carlos Munoz <carlosmuvi@stripe.com>
2025-06-20 11:27:05 +0200

    Reads fc lite flag.

--- payments-core/src/main/java/com/stripe/android/payments/financialconnections/GetFinancialConnectionsAvailability.kt
+++ payments-core/src/main/java/com/stripe/android/payments/financialconnections/GetFinancialConnectionsAvailability.kt
@@ -12,6 +12,9 @@ object GetFinancialConnectionsAvailability {
         isFullSdkAvailable: IsFinancialConnectionsSdkAvailable = DefaultIsFinancialConnectionsAvailable,
     ): FinancialConnectionsAvailability? {
         return when {
+            elementsSession.preferLite() -> {
+                FinancialConnectionsAvailability.Lite
+            }
             isFullSdkAvailable() && financialConnectionsFullSdkUnavailable.isEnabled.not() -> {
                 FinancialConnectionsAvailability.Full
             }
@@ -26,4 +29,7 @@ object GetFinancialConnectionsAvailability {
 
     private fun ElementsSession?.fcLiteKillSwitchEnabled(): Boolean =
         this?.flags[ElementsSession.Flag.ELEMENTS_DISABLE_FC_LITE] == true
+
+    private fun ElementsSession?.preferLite(): Boolean =
+        this?.flags?.get(ElementsSession.Flag.ELEMENTS_PREFER_FC_LITE) == true
 }
