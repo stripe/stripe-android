e901d24f8bf2928539616483b30013e4f2e9178f
Jay Newstrom <jaynewstrom@stripe.com>
2025-06-24 08:14:01 -0600

    PaymentMethod id is no longer nullable.

--- paymentsheet/src/main/java/com/stripe/android/customersheet/CustomerSheetViewModel.kt
+++ paymentsheet/src/main/java/com/stripe/android/customersheet/CustomerSheetViewModel.kt
@@ -501,7 +501,7 @@ internal class CustomerSheetViewModel(
 
     private suspend fun removePaymentMethod(paymentMethod: PaymentMethod): CustomerSheetDataResult<PaymentMethod> {
         return awaitPaymentMethodDataSource().detachPaymentMethod(
-            paymentMethodId = paymentMethod.id!!,
+            paymentMethodId = paymentMethod.id,
         ).onSuccess {
             eventReporter.onRemovePaymentMethodSucceeded()
         }.onFailure { cause, _ ->
@@ -518,7 +518,7 @@ internal class CustomerSheetViewModel(
         cardUpdateParams: CardUpdateParams
     ): CustomerSheetDataResult<PaymentMethod> {
         return awaitPaymentMethodDataSource().updatePaymentMethod(
-            paymentMethodId = paymentMethod.id!!,
+            paymentMethodId = paymentMethod.id,
             params = PaymentMethodUpdateParams.createCard(
                 networks = cardUpdateParams.cardBrand?.let {
                     PaymentMethodUpdateParams.Card.Networks(
@@ -612,7 +612,7 @@ internal class CustomerSheetViewModel(
 
     private suspend fun removePaymentMethodFromState(paymentMethod: PaymentMethod) {
         val currentCustomerState = customerState.value
-        val newSavedPaymentMethods = currentCustomerState.paymentMethods.filter { it.id != paymentMethod.id!! }
+        val newSavedPaymentMethods = currentCustomerState.paymentMethods.filter { it.id != paymentMethod.id }
 
         val currentSelection = currentCustomerState.currentSelection
         val originalSelection = originalPaymentSelection
@@ -645,7 +645,7 @@ internal class CustomerSheetViewModel(
                 val savedId = savedMethod.id
                 val updatedId = updatedMethod.id
 
-                if (updatedId != null && savedId != null && updatedId == savedId) {
+                if (updatedId == savedId) {
                     updatedMethod
                 } else {
                     savedMethod
@@ -968,7 +968,7 @@ internal class CustomerSheetViewModel(
             if (awaitIntentDataSource().canCreateSetupIntents) {
                 attachWithSetupIntent(paymentMethod = paymentMethod)
             } else {
-                attachPaymentMethod(id = paymentMethod.id!!)
+                attachPaymentMethod(id = paymentMethod.id)
             }
         }
     }
