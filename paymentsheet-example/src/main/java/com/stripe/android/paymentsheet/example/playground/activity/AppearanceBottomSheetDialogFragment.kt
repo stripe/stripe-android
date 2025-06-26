42188c3e2be2161c4d91f33138a607a7feec3ebd
Chengtin Tsai <cttsai@stripe.com>
2025-06-26 14:20:11 -0700

    Remove copy usage of PaymentSheet.Typography

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/activity/AppearanceBottomSheetDialogFragment.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/activity/AppearanceBottomSheetDialogFragment.kt
@@ -570,8 +570,10 @@ private fun Typography(
     FontScaleSlider(currentAppearance.typography.sizeScaleFactor) {
         updateAppearance(
             currentAppearance.copy(
-                typography = currentAppearance.typography.copy(
-                    sizeScaleFactor = it
+                typography = PaymentSheet.Typography(
+                    sizeScaleFactor = it,
+                    fontResId = currentAppearance.typography.fontResId,
+                    custom = currentAppearance.typography.custom,
                 )
             )
         )
@@ -580,8 +582,10 @@ private fun Typography(
     FontDropDown(currentAppearance.typography.fontResId) {
         updateAppearance(
             currentAppearance.copy(
-                typography = currentAppearance.typography.copy(
-                    fontResId = it
+                typography = PaymentSheet.Typography(
+                    sizeScaleFactor = currentAppearance.typography.sizeScaleFactor,
+                    fontResId = it,
+                    custom = currentAppearance.typography.custom,
                 )
             )
         )
@@ -593,7 +597,9 @@ private fun Typography(
     AppearanceToggle("customH1", h1 != null) {
         updateAppearance(
             currentAppearance.copy(
-                typography = currentAppearance.typography.copy(
+                typography = PaymentSheet.Typography(
+                    sizeScaleFactor = currentAppearance.typography.sizeScaleFactor,
+                    fontResId = currentAppearance.typography.fontResId,
                     custom = PaymentSheet.Typography.Custom(
                         h1 = if (it) {
                             PaymentSheet.Typography.Font(
@@ -615,7 +621,9 @@ private fun Typography(
         FontDropDown(fontFamily) {
             updateAppearance(
                 currentAppearance.copy(
-                    typography = currentAppearance.typography.copy(
+                    typography = PaymentSheet.Typography(
+                        sizeScaleFactor = currentAppearance.typography.sizeScaleFactor,
+                        fontResId = currentAppearance.typography.fontResId,
                         custom = PaymentSheet.Typography.Custom(
                             h1 = PaymentSheet.Typography.Font(
                                 fontFamily = it,
@@ -635,7 +643,9 @@ private fun Typography(
         ) {
             updateAppearance(
                 currentAppearance.copy(
-                    typography = currentAppearance.typography.copy(
+                    typography = PaymentSheet.Typography(
+                        sizeScaleFactor = currentAppearance.typography.sizeScaleFactor,
+                        fontResId = currentAppearance.typography.fontResId,
                         custom = PaymentSheet.Typography.Custom(
                             h1 = PaymentSheet.Typography.Font(
                                 fontFamily = fontFamily,
@@ -656,7 +666,9 @@ private fun Typography(
         ) {
             updateAppearance(
                 currentAppearance.copy(
-                    typography = currentAppearance.typography.copy(
+                    typography = PaymentSheet.Typography(
+                        sizeScaleFactor = currentAppearance.typography.sizeScaleFactor,
+                        fontResId = currentAppearance.typography.fontResId,
                         custom = PaymentSheet.Typography.Custom(
                             h1 = PaymentSheet.Typography.Font(
                                 fontFamily = fontFamily,
@@ -677,7 +689,9 @@ private fun Typography(
         ) {
             updateAppearance(
                 currentAppearance.copy(
-                    typography = currentAppearance.typography.copy(
+                    typography = PaymentSheet.Typography(
+                        sizeScaleFactor = currentAppearance.typography.sizeScaleFactor,
+                        fontResId = currentAppearance.typography.fontResId,
                         custom = PaymentSheet.Typography.Custom(
                             h1 = PaymentSheet.Typography.Font(
                                 fontFamily = fontFamily,
