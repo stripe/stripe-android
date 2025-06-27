ef86a4d7301fe62de3bc567413aa4a34cfa52b16
Jay Newstrom <jaynewstrom@stripe.com>
2025-06-27 12:02:52 -0500

    Remove data modifier from public classes.

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentSheet.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentSheet.kt
@@ -658,7 +658,8 @@ class PaymentSheet internal constructor(
 
     /** Configuration for [PaymentSheet] **/
     @Parcelize
-    data class Configuration internal constructor(
+    @Poko
+    class Configuration internal constructor(
         /**
          * Your customer-facing business name.
          *
@@ -1190,7 +1191,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Appearance
+    @Poko
+    class Appearance
     @OptIn(AppearanceAPIAdditionsPreview::class)
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     constructor(
@@ -2062,7 +2064,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Colors(
+    @Poko
+    class Colors(
         /**
          * A primary color used throughout PaymentSheet.
          */
@@ -2198,7 +2201,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Shapes @AppearanceAPIAdditionsPreview constructor(
+    @Poko
+    class Shapes @AppearanceAPIAdditionsPreview constructor(
         /**
          * The corner radius used for tabs, inputs, buttons, and other components in PaymentSheet.
          */
@@ -2252,7 +2256,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Typography @AppearanceAPIAdditionsPreview constructor(
+    @Poko
+    class Typography @AppearanceAPIAdditionsPreview constructor(
         /**
          * The scale factor for all fonts in PaymentSheet, the default value is 1.0.
          * When this value increases fonts will increase in size and decrease when this value is lowered.
@@ -2291,7 +2296,8 @@ class PaymentSheet internal constructor(
 
         @AppearanceAPIAdditionsPreview
         @Parcelize
-        data class Custom(
+        @Poko
+        class Custom(
             /**
              * The font used for headlines (e.g., "Add your payment information")
              *
@@ -2302,7 +2308,8 @@ class PaymentSheet internal constructor(
 
         @AppearanceAPIAdditionsPreview
         @Parcelize
-        data class Font(
+        @Poko
+        class Font(
             /**
              * The font used in text. This should be a resource ID value.
              */
@@ -2360,7 +2367,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class PrimaryButton(
+    @Poko
+    class PrimaryButton(
         /**
          * Describes the colors used while the system is in light mode.
          */
@@ -2380,7 +2388,8 @@ class PaymentSheet internal constructor(
     ) : Parcelable
 
     @Parcelize
-    data class PrimaryButtonColors(
+    @Poko
+    class PrimaryButtonColors(
         /**
          * The background color of the primary button.
          * Note: If 'null', {@link Colors#primary} is used.
@@ -2465,7 +2474,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class PrimaryButtonShape(
+    @Poko
+    class PrimaryButtonShape(
         /**
          * The corner radius of the primary button.
          * Note: If 'null', {@link Shapes#cornerRadiusDp} is used.
@@ -2515,7 +2525,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class PrimaryButtonTypography(
+    @Poko
+    class PrimaryButtonTypography(
         /**
          * The font used in the primary button.
          * Note: If 'null', Appearance.Typography.fontResId is used.
@@ -2599,7 +2610,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Address(
+    @Poko
+    class Address(
         /**
          * City, district, suburb, town, or village.
          * The value set is displayed in the payment sheet as-is. Depending on the payment method, the customer may be required to edit this value.
@@ -2653,7 +2665,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class BillingDetails(
+    @Poko
+    class BillingDetails(
         /**
          * The customer's billing address.
          */
@@ -2705,7 +2718,8 @@ class PaymentSheet internal constructor(
      * Configuration for how billing details are collected during checkout.
      */
     @Parcelize
-    data class BillingDetailsCollectionConfiguration(
+    @Poko
+    class BillingDetailsCollectionConfiguration(
         /**
          * How to collect the name field.
          */
@@ -2989,7 +3003,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class CustomerConfiguration internal constructor(
+    @Poko
+    class CustomerConfiguration internal constructor(
         /**
          * The identifier of the Stripe Customer object.
          * See [Stripe's documentation](https://stripe.com/docs/api/customers/object#customer_object-id).
@@ -3045,7 +3060,8 @@ class PaymentSheet internal constructor(
      * for more information on button types.
      */
     @Parcelize
-    data class GooglePayConfiguration @JvmOverloads constructor(
+    @Poko
+    class GooglePayConfiguration @JvmOverloads constructor(
         val environment: Environment,
         val countryCode: String,
         val currencyCode: String? = null,
