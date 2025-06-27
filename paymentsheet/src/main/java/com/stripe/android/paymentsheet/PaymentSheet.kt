1da6d8c1b4526c83fb33646a1406a9cf7aa88ae5
Jay Newstrom <jaynewstrom@stripe.com>
2025-06-27 12:02:52 -0500

    Remove data modifier from public classes. GIT_VALID_PII_OVERRIDE

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentSheet.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/PaymentSheet.kt
@@ -689,7 +689,8 @@ class PaymentSheet internal constructor(
 
     /** Configuration for [PaymentSheet] **/
     @Parcelize
-    data class Configuration internal constructor(
+    @Poko
+    class Configuration internal constructor(
         /**
          * Your customer-facing business name.
          *
@@ -1258,7 +1259,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Appearance
+    @Poko
+    class Appearance
     @OptIn(AppearanceAPIAdditionsPreview::class)
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     constructor(
@@ -2139,7 +2141,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Colors(
+    @Poko
+    class Colors(
         /**
          * A primary color used throughout PaymentSheet.
          */
@@ -2275,7 +2278,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Shapes @AppearanceAPIAdditionsPreview constructor(
+    @Poko
+    class Shapes @AppearanceAPIAdditionsPreview constructor(
         /**
          * The corner radius used for tabs, inputs, buttons, and other components in PaymentSheet.
          */
@@ -2329,7 +2333,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Typography @AppearanceAPIAdditionsPreview constructor(
+    @Poko
+    class Typography @AppearanceAPIAdditionsPreview constructor(
         /**
          * The scale factor for all fonts in PaymentSheet, the default value is 1.0.
          * When this value increases fonts will increase in size and decrease when this value is lowered.
@@ -2368,7 +2373,8 @@ class PaymentSheet internal constructor(
 
         @AppearanceAPIAdditionsPreview
         @Parcelize
-        data class Custom(
+        @Poko
+        class Custom(
             /**
              * The font used for headlines (e.g., "Add your payment information")
              *
@@ -2379,7 +2385,8 @@ class PaymentSheet internal constructor(
 
         @AppearanceAPIAdditionsPreview
         @Parcelize
-        data class Font(
+        @Poko
+        class Font(
             /**
              * The font used in text. This should be a resource ID value.
              */
@@ -2437,7 +2444,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class PrimaryButton(
+    @Poko
+    class PrimaryButton(
         /**
          * Describes the colors used while the system is in light mode.
          */
@@ -2457,7 +2465,8 @@ class PaymentSheet internal constructor(
     ) : Parcelable
 
     @Parcelize
-    data class PrimaryButtonColors(
+    @Poko
+    class PrimaryButtonColors(
         /**
          * The background color of the primary button.
          * Note: If 'null', {@link Colors#primary} is used.
@@ -2542,7 +2551,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class PrimaryButtonShape(
+    @Poko
+    class PrimaryButtonShape(
         /**
          * The corner radius of the primary button.
          * Note: If 'null', {@link Shapes#cornerRadiusDp} is used.
@@ -2592,7 +2602,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class PrimaryButtonTypography(
+    @Poko
+    class PrimaryButtonTypography(
         /**
          * The font used in the primary button.
          * Note: If 'null', Appearance.Typography.fontResId is used.
@@ -2676,7 +2687,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class Address(
+    @Poko
+    class Address(
         /**
          * City, district, suburb, town, or village.
          * The value set is displayed in the payment sheet as-is. Depending on the payment method, the customer may be required to edit this value.
@@ -2730,7 +2742,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class BillingDetails(
+    @Poko
+    class BillingDetails(
         /**
          * The customer's billing address.
          */
@@ -2782,7 +2795,8 @@ class PaymentSheet internal constructor(
      * Configuration for how billing details are collected during checkout.
      */
     @Parcelize
-    data class BillingDetailsCollectionConfiguration(
+    @Poko
+    class BillingDetailsCollectionConfiguration(
         /**
          * How to collect the name field.
          */
@@ -2912,6 +2926,19 @@ class PaymentSheet internal constructor(
                 isRequired = collectsFullAddress || collectsPhone,
                 format = format,
                 isPhoneNumberRequired = collectsPhone,
+            )
+        }
+
+        internal fun copy(
+            name: CollectionMode = this.name,
+        ): BillingDetailsCollectionConfiguration {
+            return BillingDetailsCollectionConfiguration(
+                name = name,
+                phone = phone,
+                email = email,
+                address = address,
+                attachDefaultsToPaymentMethod = attachDefaultsToPaymentMethod,
+                allowedCountries = allowedCountries,
             )
         }
 
@@ -3114,7 +3141,8 @@ class PaymentSheet internal constructor(
     }
 
     @Parcelize
-    data class CustomerConfiguration internal constructor(
+    @Poko
+    class CustomerConfiguration internal constructor(
         /**
          * The identifier of the Stripe Customer object.
          * See [Stripe's documentation](https://stripe.com/docs/api/customers/object#customer_object-id).
@@ -3170,7 +3198,8 @@ class PaymentSheet internal constructor(
      * for more information on button types.
      */
     @Parcelize
-    data class GooglePayConfiguration @JvmOverloads constructor(
+    @Poko
+    class GooglePayConfiguration @JvmOverloads constructor(
         val environment: Environment,
         val countryCode: String,
         val currencyCode: String? = null,
