d84629f176f1e578a95311423c96adb002e87849
Yuki <yuki@stripe.com>
2025-07-16 09:38:28 -0700

    line length...

--- paymentsheet/src/test/java/com/stripe/android/paymentelement/embedded/content/DefaultEmbeddedStateHelperTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/paymentelement/embedded/content/DefaultEmbeddedStateHelperTest.kt
@@ -188,11 +188,14 @@ internal class DefaultEmbeddedStateHelperTest {
     }
 
     @Test
-    fun `setState fails when rowSelectionBehavior is immediateAction and embeddedViewDisplaysMandateText is true`() = testScenario(
+    fun `setState fails when rowSelectionBehavior is immediateAction and embeddedViewDisplaysMandateText is true`() =
+    testScenario(
         rowSelectionCallback = { /* no-op */ }
     ) {
         assertFailsWith<IllegalArgumentException>(
-            message = "Your integration must set `embeddedViewDisplaysMandateText` to false and display the mandate (`embeddedPaymentElement.paymentOption.mandateText`) to customer near your buy button and/or before confirmation when `rowSelectionBehavior = .immediateAction`"
+            message = "Your integration must set `embeddedViewDisplaysMandateText` to false and display the mandate " +
+                "(`embeddedPaymentElement.paymentOption.mandateText`) to customer near your buy button and/or before " +
+                "confirmation when `rowSelectionBehavior = .immediateAction`"
         ) {
             setState {
                 embeddedViewDisplaysMandateText(true)
