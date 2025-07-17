ae396d21a266676d13b3c4ff7bde159146907d16
Chengtin Tsai <cttsai@stripe.com>
2025-07-17 08:35:52 -0700

    Migration script prototype

---
+++ migration-rules/src/main/kotlin/com/stripe/android/migration/StripeV25MigrationRuleSetProvider.kt
@@ -0,0 +1,19 @@
+package com.stripe.android.migration
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+
+class StripeV25MigrationRuleSetProvider : RuleSetProvider {
+    override val ruleSetId: String = "stripe-v25-migration"
+
+    override fun instance(config: Config): RuleSet {
+        return RuleSet(
+            ruleSetId,
+            listOf(
+                RememberPaymentSheetMigration(config),
+                RememberPaymentSheetFlowControllerMigration(config),
+            )
+        )
+    }
+}
