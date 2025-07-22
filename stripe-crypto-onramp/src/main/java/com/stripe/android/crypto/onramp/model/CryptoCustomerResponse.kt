82d4d5508487aa31a7ca3705d7360a1222326751
Twig <aharris.1990@gmail.com>
2025-07-22 19:56:22 -0400

    Make it so model doesn't have to be parcelize

--- stripe-crypto-onramp/src/main/java/com/stripe/android/crypto/onramp/model/CryptoCustomerResponse.kt
+++ stripe-crypto-onramp/src/main/java/com/stripe/android/crypto/onramp/model/CryptoCustomerResponse.kt
@@ -1,13 +1,10 @@
 package com.stripe.android.crypto.onramp.model
 
 import androidx.annotation.RestrictTo
-import com.stripe.android.core.model.StripeModel
-import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
-@Parcelize
 @Serializable
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 internal data class CryptoCustomerResponse(
     val id: String
-) : StripeModel
+)
