a9035dfaa53e92b0eb1de6d56edbf6623fd3fd43
Carlos Munoz <carlosmuvi@stripe.com>
2025-07-24 19:50:55 -0700

    Adds import.

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/SignupToLinkToggleStringProvider.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/ui/SignupToLinkToggleStringProvider.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.fromHtml
 import com.stripe.android.link.ui.replaceHyperlinks
+import com.stripe.android.paymentsheet.R
 
 internal interface SignupToLinkToggleStringProvider {
     val title: String
@@ -16,17 +17,16 @@ internal class DefaultSignupToLinkToggleStringProvider(
 ) : SignupToLinkToggleStringProvider {
 
     override val title: String by lazy {
-        application.getString(com.stripe.android.paymentsheet.R.string.stripe_link_signup_toggle_title)
+        application.getString(R.string.stripe_link_signup_toggle_title)
     }
 
     override val description: String by lazy {
-        application.getString(com.stripe.android.paymentsheet.R.string.stripe_link_signup_toggle_description)
+        application.getString(R.string.stripe_link_signup_toggle_description)
     }
 
     override val termsAndConditions: AnnotatedString by lazy {
         AnnotatedString.fromHtml(
-            application.getString(com.stripe.android.paymentsheet.R.string.stripe_link_sign_up_terms)
-                .replaceHyperlinks()
+            application.getString(R.string.stripe_link_sign_up_terms).replaceHyperlinks()
         )
     }
 }
