7ed422a518235bca5eb83211d7455065d08d0505
Larry Ng <lng@stripe.com>
2025-07-25 20:50:00 -0400

    fix after rebase

--- paymentsheet/src/main/java/com/stripe/android/link/LinkControllerViewModel.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/LinkControllerViewModel.kt
@@ -340,7 +340,8 @@ internal class LinkControllerViewModel @Inject constructor(
                     component.linkAuth.lookUp(
                         email = email,
                         emailSource = EmailSource.USER_ACTION,
-                        startSession = false
+                        startSession = false,
+                        customerId = null,
                     )
                         .toResult()
                         .map { it != null }
