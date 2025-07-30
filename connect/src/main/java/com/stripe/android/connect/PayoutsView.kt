3cf8bd01355c670a149f4bc19140aa9f09e02582
Torrance Yang <torrance@stripe.com>
2025-07-30 15:59:36 -0400

    feat: add payouts

--- connect/src/main/java/com/stripe/android/connect/PayoutsView.kt
+++ connect/src/main/java/com/stripe/android/connect/PayoutsView.kt
@@ -6,7 +6,8 @@ import androidx.annotation.RestrictTo
 import androidx.core.content.withStyledAttributes
 import com.stripe.android.connect.webview.StripeConnectWebViewContainer
 
-internal class PayoutsView internal constructor(
+@PrivateBetaConnectSDK
+class PayoutsView internal constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
@@ -48,5 +49,6 @@ internal class PayoutsView internal constructor(
     }
 }
 
+@PrivateBetaConnectSDK
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 interface PayoutsListener : StripeEmbeddedComponentListener
