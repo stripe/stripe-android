bb942a684ebe7eefd7853e719dc3852bef13cc3f
Torrance Yang <torrance@stripe.com>
2025-07-30 16:16:37 -0400

    feat: add Payments

--- connect/src/main/java/com/stripe/android/connect/EmbeddedComponentManager.kt
+++ connect/src/main/java/com/stripe/android/connect/EmbeddedComponentManager.kt
@@ -91,6 +91,27 @@ class EmbeddedComponentManager @JvmOverloads constructor(
         )
     }
 
+    /**
+     * Create a new [PaymentsView] for inclusion in the view hierarchy.
+     *
+     * @param context The [Context] to use for creating the view.
+     * @param listener Optional [PaymentsListener] to use for handling events from the view.
+     * @param cacheKey Key to use for caching the internal WebView within an Activity across configuration changes.
+     */
+    @PrivateBetaConnectSDK
+    fun createPaymentsView(
+        context: Context,
+        listener: PaymentsListener? = null,
+        cacheKey: String? = null,
+    ): PaymentsView {
+        return PaymentsView(
+            context = context,
+            embeddedComponentManager = this,
+            listener = listener,
+            cacheKey = cacheKey,
+        )
+    }
+
     fun update(appearance: Appearance) {
         coordinator.update(appearance)
     }
