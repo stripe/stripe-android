59c59f53758ac6f99ae79a2ef97c0f3fa099b77c
tianzhao-stripe <tianzhao@stripe.com>
2025-08-05 12:05:46 -0700

    missing imports

--- paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/PreparePaymentMethodTest.kt
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/PreparePaymentMethodTest.kt
@@ -19,6 +19,7 @@ import com.stripe.android.paymentelement.assertCompleted
 import com.stripe.android.paymentelement.runEmbeddedPaymentElementTest
 import com.stripe.android.elements.AddressDetails
 import com.stripe.android.elements.payment.IntentConfiguration
+import com.stripe.android.elements.payment.PaymentSheet
 import com.stripe.android.paymentsheet.utils.TestRules
 import com.stripe.android.paymentsheet.utils.assertCompleted
 import com.stripe.android.paymentsheet.utils.runFlowControllerTest
