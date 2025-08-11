1eb285962227ea5cba6d5079dc11a56097944934
Twig <aharris.1990@gmail.com>
2025-08-11 16:34:21 -0400

    Update OnrampSetWalletAddressResult.kt

--- stripe-crypto-onramp/src/main/java/com/stripe/android/crypto/onramp/model/OnrampSetWalletAddressResult.kt
+++ stripe-crypto-onramp/src/main/java/com/stripe/android/crypto/onramp/model/OnrampSetWalletAddressResult.kt
@@ -1,18 +1,23 @@
 package com.stripe.android.crypto.onramp.model
 
+import androidx.annotation.RestrictTo
+
 /**
  * Result of setting a wallet address in Onramp.
  */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 sealed interface OnrampSetWalletAddressResult {
     /**
      * Wallet address was set successfully.
      */
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class Completed internal constructor() : OnrampSetWalletAddressResult
 
     /**
      * Setting wallet address failed (invalid format, etc).
      * @param error The error that caused the failure
      */
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     class Failed internal constructor(
         val error: Throwable
     ) : OnrampSetWalletAddressResult
