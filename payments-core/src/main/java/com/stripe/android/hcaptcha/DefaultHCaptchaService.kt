2c2a802e6332fee221c2d5d910a9a512fb5bf7eb
toluo-stripe <toluo@stripe.com>
2025-08-13 21:16:23 -0400

    More cleanup

--- payments-core/src/main/java/com/stripe/android/hcaptcha/DefaultHCaptchaService.kt
+++ payments-core/src/main/java/com/stripe/android/hcaptcha/DefaultHCaptchaService.kt
@@ -1,6 +1,7 @@
 package com.stripe.android.hcaptcha
 
 import androidx.fragment.app.FragmentActivity
+import com.stripe.hcaptcha.HCaptcha
 import com.stripe.hcaptcha.HCaptchaError
 import com.stripe.hcaptcha.HCaptchaException
 import com.stripe.hcaptcha.HCaptchaTokenResponse
@@ -14,43 +15,59 @@ import kotlin.coroutines.resume
 internal class DefaultHCaptchaService(
     private val hCaptchaProvider: HCaptchaProvider
 ) : HCaptchaService {
-
+    @SuppressWarnings("TooGenericExceptionCaught")
     override suspend fun performPassiveHCaptcha(
         activity: FragmentActivity,
         siteKey: String,
         rqData: String?
     ): HCaptchaService.Result {
         val hCaptcha = hCaptchaProvider.get()
-        try {
-            return suspendCancellableCoroutine { continuation ->
-                continuation.invokeOnCancellation {
-                    hCaptcha.reset()
-                }
-
-                hCaptcha.addOnSuccessListener(object : OnSuccessListener<HCaptchaTokenResponse> {
-                    override fun onSuccess(result: HCaptchaTokenResponse) {
-                        continuation.resume(HCaptchaService.Result.Success(result.tokenResult))
-                    }
-                }).addOnFailureListener(object : OnFailureListener {
-                    override fun onFailure(exception: HCaptchaException) {
-                        continuation.resume(HCaptchaService.Result.Failure(exception))
-                    }
-                })
-
-                val config = HCaptchaConfig(
-                    siteKey = siteKey,
-                    size = HCaptchaSize.INVISIBLE,
-                    rqdata = rqData,
-                    loading = false,
-                    hideDialog = true,
-                    disableHardwareAcceleration = true,
-                    retryPredicate = { _, exception -> exception.hCaptchaError == HCaptchaError.SESSION_TIMEOUT }
-                )
-
-                hCaptcha.setup(activity, config).verifyWithHCaptcha(activity)
-            }
+        return try {
+            startVerification(
+                activity = activity,
+                siteKey = siteKey,
+                rqData = rqData,
+                hCaptcha = hCaptcha
+            )
+        } catch (e: Throwable) {
+            HCaptchaService.Result.Failure(e)
         } finally {
             hCaptcha.reset()
+        }
+    }
+
+    private suspend fun startVerification(
+        activity: FragmentActivity,
+        siteKey: String,
+        rqData: String?,
+        hCaptcha: HCaptcha
+    ): HCaptchaService.Result {
+        return suspendCancellableCoroutine { continuation ->
+            continuation.invokeOnCancellation {
+                hCaptcha.reset()
+            }
+
+            hCaptcha.addOnSuccessListener(object : OnSuccessListener<HCaptchaTokenResponse> {
+                override fun onSuccess(result: HCaptchaTokenResponse) {
+                    continuation.resume(HCaptchaService.Result.Success(result.tokenResult))
+                }
+            }).addOnFailureListener(object : OnFailureListener {
+                override fun onFailure(exception: HCaptchaException) {
+                    continuation.resume(HCaptchaService.Result.Failure(exception))
+                }
+            })
+
+            val config = HCaptchaConfig(
+                siteKey = siteKey,
+                size = HCaptchaSize.INVISIBLE,
+                rqdata = rqData,
+                loading = false,
+                hideDialog = true,
+                disableHardwareAcceleration = true,
+                retryPredicate = { _, exception -> exception.hCaptchaError == HCaptchaError.SESSION_TIMEOUT }
+            )
+
+            hCaptcha.setup(activity, config).verifyWithHCaptcha(activity)
         }
     }
 }
