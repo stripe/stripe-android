8faa25b0f77d263d7478d698620c69f0e5688ae9
Samer Alabi <samer@stripe.com>
2025-08-15 15:59:34 -0400

    Fix deprecation & other issues with API 35

---
+++ stripe-core/src/main/java/com/stripe/android/core/utils/StatusBarCompat.kt
@@ -0,0 +1,26 @@
+package com.stripe.android.core.utils
+
+import android.app.Activity
+import android.os.Build
+import androidx.annotation.RestrictTo
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+object StatusBarCompat {
+    @Suppress("DEPRECATION")
+    fun color(activity: Activity): Int? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            null
+        } else {
+            activity.window?.statusBarColor
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    fun setColor(activity: Activity, color: Int) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            activity.window?.let { window ->
+                window.statusBarColor = color
+            }
+        }
+    }
+}
