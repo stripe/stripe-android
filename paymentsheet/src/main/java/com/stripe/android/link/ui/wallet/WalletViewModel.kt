c633e09af7a01c0ddd305b434bf506011ba5628f
Larry Ng <lng@stripe.com>
2025-08-19 21:08:54 -0400

    use ext fun

--- paymentsheet/src/main/java/com/stripe/android/link/ui/wallet/WalletViewModel.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/wallet/WalletViewModel.kt
@@ -110,7 +110,7 @@ internal class WalletViewModel @Inject constructor(
                 configuration.enableLinkPaymentSelectionHint ||
                     FeatureFlags.forceEnableLinkPaymentSelectionHint.isEnabled
             }
-            ?.let { resolvableString(it) }
+            ?.resolvableString
 
     val uiState: StateFlow<WalletUiState> = _uiState.asStateFlow()
 
@@ -660,12 +660,12 @@ private fun StripeIntent.secondaryButtonLabel(linkLaunchMode: LinkLaunchMode): R
     return when (linkLaunchMode) {
         is LinkLaunchMode.Full,
         is LinkLaunchMode.Confirmation -> when (this) {
-            is PaymentIntent -> resolvableString(R.string.stripe_wallet_pay_another_way)
-            is SetupIntent -> resolvableString(R.string.stripe_wallet_continue_another_way)
+            is PaymentIntent -> R.string.stripe_wallet_pay_another_way.resolvableString
+            is SetupIntent -> R.string.stripe_wallet_continue_another_way.resolvableString
         }
         is LinkLaunchMode.PaymentMethodSelection -> {
             if (linkLaunchMode.shouldShowSecondaryCta) {
-                resolvableString(R.string.stripe_wallet_continue_another_way)
+                R.string.stripe_wallet_continue_another_way.resolvableString
             } else {
                 null
             }
