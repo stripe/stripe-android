bf88d47022990bcaf6414e4453d376ee07eb7d77
Larry Ng <lng@stripe.com>
2025-08-21 17:26:31 -0400

    update UI to be full screen with pinned disclaimer and buttons

--- paymentsheet/src/main/java/com/stripe/android/link/ui/oauth/OAuthConsentScreen.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/oauth/OAuthConsentScreen.kt
@@ -9,16 +9,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,7 +41,7 @@ import com.stripe.android.core.strings.ResolvableString
 import com.stripe.android.link.theme.DefaultLinkTheme
 import com.stripe.android.link.theme.LinkTheme
 import com.stripe.android.link.ui.ErrorText
-import com.stripe.android.link.ui.ScrollableTopLevelColumn
+import com.stripe.android.link.ui.LocalLinkContentScrollHandler
 import com.stripe.android.link.ui.image.LocalStripeImageLoader
 import com.stripe.android.model.ConsentUi
 import com.stripe.android.uicore.image.StripeImage
@@ -68,31 +75,57 @@ internal fun OAuthConsentScreen(
     onAllowClick: () -> Unit,
     onDenyClick: (() -> Unit)? = null,
 ) {
-    ScrollableTopLevelColumn {
-        MerchantLogo(merchantLogoUrl)
-        Spacer(Modifier.height(16.dp))
-        Title(pane.title)
-        Spacer(Modifier.height(24.dp))
-        UserSection(userEmail)
-        Spacer(Modifier.height(24.dp))
-        ScopesSection(pane.scopesSection)
-        Spacer(Modifier.height(8.dp))
-        errorMessage?.let {
+    val contentScrollHandler = LocalLinkContentScrollHandler.current
+    val scrollState = rememberScrollState()
+
+    LaunchedEffect(scrollState.canScrollBackward) {
+        contentScrollHandler?.handleCanScrollBackwardChanged(scrollState.canScrollBackward)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()),
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 20.dp)
+                .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            MerchantLogo(merchantLogoUrl)
             Spacer(Modifier.height(16.dp))
-            ErrorSection(it)
+            Title(pane.title)
+            Spacer(Modifier.height(24.dp))
+            UserSection(userEmail)
+            Spacer(Modifier.height(24.dp))
+            ScopesSection(pane.scopesSection)
+            Spacer(Modifier.height(8.dp))
+            errorMessage?.let {
+                Spacer(Modifier.height(16.dp))
+                ErrorSection(it)
+            }
         }
-        pane.disclaimer?.let {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            pane.disclaimer?.let {
+                Spacer(Modifier.height(16.dp))
+                Disclaimer(it)
+            }
             Spacer(Modifier.height(16.dp))
-            Disclaimer(it)
+            ActionButtons(
+                denyButtonLabel = pane.denyButtonLabel,
+                allowButtonLabel = pane.allowButtonLabel,
+                onAllowClick = onAllowClick,
+                onDenyClick = onDenyClick
+            )
+            Spacer(Modifier.height(24.dp))
         }
-        Spacer(Modifier.height(16.dp))
-        ActionButtons(
-            denyButtonLabel = pane.denyButtonLabel,
-            allowButtonLabel = pane.allowButtonLabel,
-            onAllowClick = onAllowClick,
-            onDenyClick = onDenyClick
-        )
-        Spacer(Modifier.height(4.dp))
     }
 }
 
