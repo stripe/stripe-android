7e57ef9da6edfd828666849f60374c672c886159
Javier de Pedro López <droidpl@stripe.com>
2025-08-21 18:09:24 +0200

    Test example integrations

--- example/src/main/java/com/stripe/example/activity/GooglePayPaymentMethodLauncherComposeActivity.kt
+++ example/src/main/java/com/stripe/example/activity/GooglePayPaymentMethodLauncherComposeActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,24 +21,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.stripe.android.CardBrandFilter
+import com.stripe.android.DefaultCardBrandFilter
 import com.stripe.android.googlepaylauncher.GooglePayEnvironment
 import com.stripe.android.googlepaylauncher.GooglePayPaymentMethodLauncher
 import com.stripe.android.googlepaylauncher.rememberGooglePayPaymentMethodLauncher
+import com.stripe.android.model.CardBrand
+import com.stripe.example.R
 import kotlinx.coroutines.launch
+import kotlinx.parcelize.Parcelize
 
 class GooglePayPaymentMethodLauncherComposeActivity : AppCompatActivity() {
-    private val googlePayConfig = GooglePayPaymentMethodLauncher.Config(
-        environment = GooglePayEnvironment.Test,
-        merchantCountryCode = "US",
-        merchantName = "Widget Store",
-        billingAddressConfig = GooglePayPaymentMethodLauncher.BillingAddressConfig(
-            isRequired = true,
-            format = GooglePayPaymentMethodLauncher.BillingAddressConfig.Format.Full,
-            isPhoneNumberRequired = false
-        ),
-        existingPaymentMethodRequired = false
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,6 +49,20 @@ class GooglePayPaymentMethodLauncherComposeActivity : AppCompatActivity() {
         val scaffoldState = rememberScaffoldState()
         val scope = rememberCoroutineScope()
         var enabled by remember { mutableStateOf(false) }
+        var checked by remember { mutableStateOf(false) }
+
+        val googlePayConfig = GooglePayPaymentMethodLauncher.Config(
+            environment = GooglePayEnvironment.Test,
+            merchantCountryCode = "US",
+            merchantName = "Widget Store",
+            billingAddressConfig = GooglePayPaymentMethodLauncher.BillingAddressConfig(
+                isRequired = true,
+                format = GooglePayPaymentMethodLauncher.BillingAddressConfig.Format.Full,
+                isPhoneNumberRequired = false
+            ),
+            existingPaymentMethodRequired = false,
+            cardBrandFilter = if(checked) GooglePayPaymentMethodLauncherAcceptableBrandsFilter(CardBrand.Visa) else DefaultCardBrandFilter
+        )
 
         val googlePayLauncher = rememberGooglePayPaymentMethodLauncher(
             config = googlePayConfig,
@@ -87,7 +102,9 @@ class GooglePayPaymentMethodLauncherComposeActivity : AppCompatActivity() {
                     currencyCode = "EUR",
                     amount = 2500L,
                 )
-            }
+            },
+            checked,
+            onChecked = { checked = it }
         )
     }
 
@@ -96,20 +113,45 @@ class GooglePayPaymentMethodLauncherComposeActivity : AppCompatActivity() {
     private fun GooglePayPaymentMethodLauncherScreen(
         scaffoldState: ScaffoldState,
         enabled: Boolean,
-        onLaunchGooglePay: () -> Unit
+        onLaunchGooglePay: () -> Unit,
+        checked: Boolean,
+        onChecked: (Boolean) -> Unit
     ) {
         Scaffold(scaffoldState = scaffoldState) {
-            AndroidView(
-                factory = { context ->
-                    GooglePayButton(context)
-                },
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .clickable(
-                        enabled = enabled,
-                        onClick = onLaunchGooglePay
+            Column {
+                AndroidView(
+                    factory = { context ->
+                        GooglePayButton(context)
+                    },
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .clickable(
+                            enabled = enabled,
+                            onClick = onLaunchGooglePay
+                        )
+                )
+                Row {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = onChecked
                     )
-            )
+                    Text(
+                        modifier = Modifier.padding(start = 8.dp, top = 16.dp),
+                        text = stringResource(R.string.allow_only_visa)
+                    )
+                }
+            }
         }
+    }
+}
+
+@Parcelize
+class GooglePayPaymentMethodLauncherAcceptableBrandsFilter(
+    private val cardBrands: Set<CardBrand>
+) : CardBrandFilter {
+    constructor(vararg cardBrands: CardBrand) : this(cardBrands.toSet())
+
+    override fun isAccepted(cardBrand: CardBrand): Boolean {
+        return cardBrand in cardBrands
     }
 }
