30205f841b2abd5154135c198ff6412217a5de37
tianzhao-stripe <tianzhao@stripe.com>
2025-08-25 15:10:29 -0700

    remove extra configure

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/AutomaticallyLaunchCardScanDefinition.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/AutomaticallyLaunchCardScanDefinition.kt
@@ -3,7 +3,6 @@ package com.stripe.android.paymentsheet.example.playground.settings
 import com.stripe.android.customersheet.CustomerSheet
 import com.stripe.android.paymentelement.EmbeddedPaymentElement
 import com.stripe.android.paymentsheet.PaymentSheet
-import com.stripe.android.paymentsheet.example.Settings
 import com.stripe.android.paymentsheet.example.playground.PlaygroundState
 
 internal object AutomaticallyLaunchCardScanDefinition : BooleanSettingsDefinition(
@@ -11,15 +10,6 @@ internal object AutomaticallyLaunchCardScanDefinition : BooleanSettingsDefinitio
     displayName = "Open Card Scan Automatically",
     defaultValue = false
 ) {
-    override fun configure(
-        value: Boolean,
-        configurationBuilder: PaymentSheet.Configuration.Builder,
-        playgroundState: PlaygroundState.Payment,
-        configurationData: PlaygroundSettingDefinition.PaymentSheetConfigurationData,
-        settings: Settings
-    ) {
-        configurationBuilder.opensCardScannerAutomatically(value)
-    }
 
     override fun configure(
         value: Boolean,
