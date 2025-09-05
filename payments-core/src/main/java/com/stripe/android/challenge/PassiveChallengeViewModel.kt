815a9c23b3571b18860d54cf886d09ced7c3c5ec
toluo-stripe <toluo@stripe.com>
2025-09-05 19:44:03 -0400

    Update PassiveChallengeViewModel.kt

--- payments-core/src/main/java/com/stripe/android/challenge/PassiveChallengeViewModel.kt
+++ payments-core/src/main/java/com/stripe/android/challenge/PassiveChallengeViewModel.kt
@@ -22,7 +22,7 @@ internal class PassiveChallengeViewModel @Inject constructor(
     val result: Flow<PassiveChallengeActivityResult> = _result
 
     suspend fun startPassiveChallenge(activity: FragmentActivity) {
-        val result = hCaptchaService.performActiveHCaptcha(
+        val result = hCaptchaService.performPassiveHCaptcha(
             activity = activity,
             siteKey = passiveCaptchaParams.siteKey,
             rqData = passiveCaptchaParams.rqData
