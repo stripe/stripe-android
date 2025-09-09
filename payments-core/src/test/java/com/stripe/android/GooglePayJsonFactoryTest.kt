5117692c99306934680d323c1a07e842391d42b6
Samer Alabi <samer@stripe.com>
2025-09-09 11:19:03 -0400

    Use `isElements` in proper locations.

--- payments-core/src/test/java/com/stripe/android/GooglePayJsonFactoryTest.kt
+++ payments-core/src/test/java/com/stripe/android/GooglePayJsonFactoryTest.kt
@@ -136,11 +136,11 @@ class GooglePayJsonFactoryTest {
                     "phoneNumberRequired": true
                 },
                 "merchantInfo": {
+                    "merchantName": "Widget Store",
                     "softwareInfo": {
                         "id": "android/stripe-launcher",
                         "version": "${StripeSdkVersion.VERSION_NAME}"
-                    },
-                    "merchantName": "Widget Store"
+                    }
                 }
             }
             """.trimIndent()
@@ -185,11 +185,6 @@ class GooglePayJsonFactoryTest {
                 totalPriceStatus = GooglePayJsonFactory.TransactionInfo.TotalPriceStatus.Estimated,
                 countryCode = "us"
             ),
-            merchantInfo = GooglePayJsonFactory.MerchantInfo(
-                softwareInfo = GooglePayJsonFactory.SoftwareInfo(
-                    id = GooglePayJsonFactory.SoftwareInfo.SoftwareId.Launcher
-                )
-            ),
         )
         val countryCode = createPaymentDataRequestJson
             .getJSONObject("transactionInfo")
@@ -204,11 +199,6 @@ class GooglePayJsonFactoryTest {
             transactionInfo = GooglePayJsonFactory.TransactionInfo(
                 currencyCode = "usd",
                 totalPriceStatus = GooglePayJsonFactory.TransactionInfo.TotalPriceStatus.Final
-            ),
-            merchantInfo = GooglePayJsonFactory.MerchantInfo(
-                softwareInfo = GooglePayJsonFactory.SoftwareInfo(
-                    id = GooglePayJsonFactory.SoftwareInfo.SoftwareId.Launcher
-                )
             ),
         )
         val currencyCode = createPaymentDataRequestJson
@@ -231,11 +221,6 @@ class GooglePayJsonFactoryTest {
             shippingAddressParameters = GooglePayJsonFactory.ShippingAddressParameters(
                 isRequired = true,
                 allowedCountryCodes = setOf("us", "de")
-            ),
-            merchantInfo = GooglePayJsonFactory.MerchantInfo(
-                softwareInfo = GooglePayJsonFactory.SoftwareInfo(
-                    id = GooglePayJsonFactory.SoftwareInfo.SoftwareId.Launcher
-                )
             ),
         )
 
@@ -582,11 +567,6 @@ class GooglePayJsonFactoryTest {
                 totalPriceStatus = GooglePayJsonFactory.TransactionInfo.TotalPriceStatus.Final,
                 totalPrice = 100
             ),
-            merchantInfo = GooglePayJsonFactory.MerchantInfo(
-                softwareInfo = GooglePayJsonFactory.SoftwareInfo(
-                    id = GooglePayJsonFactory.SoftwareInfo.SoftwareId.Launcher
-                )
-            ),
         )
 
         val paymentDataAllowedCardNetworks = paymentDataRequestJson
@@ -601,5 +581,55 @@ class GooglePayJsonFactoryTest {
         // Regular payment request should only include filtered card networks
         assertThat(paymentDataAllowedCardNetworks)
             .containsExactly("MASTERCARD", "VISA")
+    }
+
+    @Test
+    fun `'merchantInfo' should have 'stripe-manual-api' by default when software identifier not provided`() {
+        val factory = GooglePayJsonFactory(
+            googlePayConfig = googlePayConfig,
+        )
+
+        val createCardPaymentMethodRequestJson = factory.createPaymentDataRequest(
+            billingAddressParameters = null,
+            allowCreditCards = true,
+            transactionInfo = GooglePayJsonFactory.TransactionInfo(
+                currencyCode = "CAD",
+                totalPriceStatus = GooglePayJsonFactory.TransactionInfo.TotalPriceStatus.Final
+            ),
+            merchantInfo = null,
+        )
+
+        val softwareInfo = createCardPaymentMethodRequestJson
+            .getJSONObject("merchantInfo")
+            .getJSONObject("softwareInfo")
+
+        assertThat(softwareInfo.getString("id")).isEqualTo("android/stripe-manual-api")
+    }
+
+    @Test
+    fun `'merchantInfo' should have 'stripe-elements' when specific software identifier is provided`() {
+        val factory = GooglePayJsonFactory(
+            googlePayConfig = googlePayConfig,
+        )
+
+        val createCardPaymentMethodRequestJson = factory.createPaymentDataRequest(
+            billingAddressParameters = null,
+            allowCreditCards = true,
+            transactionInfo = GooglePayJsonFactory.TransactionInfo(
+                currencyCode = "CAD",
+                totalPriceStatus = GooglePayJsonFactory.TransactionInfo.TotalPriceStatus.Final
+            ),
+            merchantInfo = GooglePayJsonFactory.MerchantInfo(
+                softwareInfo = GooglePayJsonFactory.SoftwareInfo(
+                    id = GooglePayJsonFactory.SoftwareInfo.SoftwareId.Elements,
+                )
+            ),
+        )
+
+        val softwareInfo = createCardPaymentMethodRequestJson
+            .getJSONObject("merchantInfo")
+            .getJSONObject("softwareInfo")
+
+        assertThat(softwareInfo.getString("id")).isEqualTo("android/stripe-elements")
     }
 }
