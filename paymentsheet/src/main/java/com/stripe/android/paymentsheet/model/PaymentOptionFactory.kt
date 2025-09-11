6a5d04d66f9db03b51bd838e271fca61a86dc6dd
Jay Newstrom <jaynewstrom@stripe.com>
2025-09-11 10:12:55 -0600

    Don't reference selection in PaymentOptionFactory lambda. (#11565)

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/model/PaymentOptionFactory.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/model/PaymentOptionFactory.kt
@@ -10,8 +10,12 @@ internal class PaymentOptionFactory @Inject constructor(
     private val context: Context,
 ) {
     fun create(selection: PaymentSelection): PaymentOption {
+        val drawableResourceId = selection.drawableResourceId
+        val lightThemeIconUrl = selection.lightThemeIconUrl
+        val darkThemeIconUrl = selection.darkThemeIconUrl
+
         return PaymentOption(
-            drawableResourceId = selection.drawableResourceId,
+            drawableResourceId = drawableResourceId,
             label = selection.label.resolve(context),
             paymentMethodType = selection.paymentMethodType,
             _labels = PaymentOptionLabelsFactory.create(context, selection),
@@ -19,10 +23,10 @@ internal class PaymentOptionFactory @Inject constructor(
             _shippingDetails = selection.shippingDetails,
             imageLoader = {
                 iconLoader.load(
-                    drawableResourceId = selection.drawableResourceId,
-                    drawableResourceIdNight = selection.drawableResourceId,
-                    lightThemeIconUrl = selection.lightThemeIconUrl,
-                    darkThemeIconUrl = selection.darkThemeIconUrl,
+                    drawableResourceId = drawableResourceId,
+                    drawableResourceIdNight = drawableResourceId,
+                    lightThemeIconUrl = lightThemeIconUrl,
+                    darkThemeIconUrl = darkThemeIconUrl,
                 )
             },
         )
