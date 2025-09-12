66dc71a454e8b88b9491a5422c31af2502a71723
Till Hellmund <tillh@stripe.com>
2025-09-12 13:14:14 -0400

    Hide Link if BDCC is used with web flow (#11562)
    * Hide Link if BDCC is used with web flow
    
    * Update tests and lint
    
    * Add test
    
    * Use `LinkGate` instead of `useAttestationEndpointsForLink`

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/state/PaymentElementLoader.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/state/PaymentElementLoader.kt
@@ -661,9 +661,16 @@ internal class DefaultPaymentElementLoader @Inject constructor(
             elementsSession.linkSettings?.linkSupportedPaymentMethodsOnboardingEnabled.orEmpty(),
         )
 
-        // CBF isn't currently supported in the web flow.
         val useWebLink = !linkGateFactory.create(linkConfiguration).useNativeLink
+
         if (isCardBrandFilteringRequired && useWebLink) {
+            // CBF isn't currently supported in the web flow.
+            return null
+        }
+
+        val collectsExtraBillingDetails = configuration.billingDetailsCollectionConfiguration.collectsAnything
+        if (collectsExtraBillingDetails && useWebLink) {
+            // Extra billing details collection isn't currently supported in the web flow.
             return null
         }
 
