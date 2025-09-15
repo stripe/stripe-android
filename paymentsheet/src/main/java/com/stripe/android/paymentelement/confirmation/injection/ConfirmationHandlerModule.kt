37d00157d168a64d5c56a4e3a7912a23c6597a98
toluo-stripe <toluo@stripe.com>
2025-09-15 15:53:31 -0400

    Move PassiveChallengeWarmerModule inclusion to definition

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/confirmation/injection/ConfirmationHandlerModule.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/confirmation/injection/ConfirmationHandlerModule.kt
@@ -1,6 +1,5 @@
 package com.stripe.android.paymentelement.confirmation.injection
 
-import com.stripe.android.challenge.warmer.PassiveChallengeWarmerModule
 import com.stripe.android.paymentelement.confirmation.ConfirmationDefinition
 import com.stripe.android.paymentelement.confirmation.ConfirmationHandler
 import com.stripe.android.paymentelement.confirmation.ConfirmationRegistry
@@ -9,7 +8,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
-@Module(includes = [PassiveChallengeWarmerModule::class])
+@Module
 internal interface ConfirmationHandlerModule {
     @Binds
     fun bindsConfirmationHandlerFactory(
