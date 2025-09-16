b46c4a7d4ef7b7be5a0b000cccc3e9c291a8b172
Chengtin Tsai <cttsai@stripe.com>
2025-09-16 14:35:40 -0700

    Remove "unspecified by default" description for allowRedisplay

--- payments-core/src/main/java/com/stripe/android/model/ConfirmationToken.kt
+++ payments-core/src/main/java/com/stripe/android/model/ConfirmationToken.kt
@@ -93,7 +93,6 @@ data class ConfirmationToken internal constructor(
          *  This field indicates whether this payment method can be shown again to its customer in a checkout flow.
          *  Stripe products such as Checkout and Elements use this field to determine
          *  whether a payment method can be shown as a saved payment method in a checkout flow.
-         *  The field defaults to “unspecified”.
          */
         @JvmField val allowRedisplay: PaymentMethod.AllowRedisplay? = null,
 
