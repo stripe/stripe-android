f2845771d487c2ad48dcfce6afa0248186439fe6
Jay Newstrom <jaynewstrom@stripe.com>
2025-10-09 08:39:28 -0600

    Allow for deploying old versions.

---
+++ scripts/deploy/common.rb
@@ -0,0 +1,117 @@
+#!/usr/bin/env ruby
+
+require 'open3'
+
+def fetch_password(password)
+    stdout, stderr, status = Open3.capture3("fetch-password --raw #{password}")
+
+    if !stderr.empty?
+        raise("Failed to retrieve password: #{password}")
+    end
+
+    stdout
+end
+
+def rputs(string)
+    puts string.red
+end
+
+def wait_for_user
+    rputs "Press enter to continue..."
+    STDIN.gets
+end
+
+def execute(command)
+    puts "Executing #{command}..."
+    system(command)
+end
+
+def execute_or_fail(command)
+    puts "Executing #{command}..."
+    system(command) or raise "Failed to execute #{command}"
+end
+
+def open_url(url)
+    puts "Opening url #{url}"
+    `open #{url}`
+end
+
+def replace_in_file(filename, pattern, replacement)
+  puts "> Updating #{filename}"
+  content = File.read(filename)
+  new_content = content.sub(pattern, replacement)
+  File.write(filename, new_content)
+end
+
+def append_to_file(filename, new_content)
+    puts "> Updating #{filename}"
+    content = File.read(filename)
+    File.write(filename, "#{content}\n#{new_content}\n")
+end
+
+def switch_to_release_branch()
+    switch_to_new_branch(release_branch, @deploy_branch)
+end
+
+def ensure_directory_is_clean()
+    stdout, stderr, status = Open3.capture3("git status --porcelain")
+
+    if !stdout.empty?
+        abort("You must have a clean working directory to continue.")
+    end
+end
+
+def delete_git_branch(branch_to_delete, main_branch, repo: ".")
+    # Ensure we are not on the same branch that we're trying to delete
+    execute("git -C #{repo} checkout #{main_branch}")
+
+    # Actually delete the branch
+    execute("git -C #{repo} push origin --delete #{branch_to_delete}")
+    execute("git -C #{repo} branch -D #{branch_to_delete}")
+end
+
+def switch_to_new_branch(branch_to_create, main_branch, repo: ".")
+    # Ensure a different version of this branch doesn't already exist.
+    delete_git_branch(branch_to_create, main_branch, repo: repo)
+
+    execute_or_fail("git -C #{repo} checkout -b #{branch_to_create}")
+    execute_or_fail("git -C #{repo} branch -u #{main_branch}")
+end
+
+def create_pr(
+    pr_branch,
+    pr_title,
+    pr_description,
+    user_message
+)
+    response = github_login.create_pull_request(
+        "stripe/stripe-android",
+        @deploy_branch,
+        pr_branch,
+        pr_title,
+        pr_description,
+        draft: @is_dry_run,
+    )
+
+    pr_url = response.html_url
+    open_url(pr_url)
+
+    rputs user_message
+    wait_for_user
+end
+
+# Gets the GnuPG key ID used to sign tagged commits.
+def gnupg_key_id
+    fetch_password("bindings/gnupg/fingerprint").strip
+end
+
+private def github_login
+  token = `fetch-password -q bindings/gh-tokens/$USER`
+  if $?.exitstatus != 0
+    rputs "Couldn't fetch GitHub token"
+    exit(1)
+  end
+  client = Octokit::Client.new(access_token: token)
+  abort('Invalid GitHub token. Follow the wiki instructions for setting up a GitHub token.') unless client.login
+  client
+end
---
+++ scripts/deploy/publish.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+
+require_relative 'common'
+require_relative 'gnupg_utils'
+
+def publish_to_sonatype
+    # Must be run on the deploy branch, because it depends on changes made in
+    # create_version_bump_pr (updating the VERSION file)
+    set_gpg_location()
+
+    begin
+        m2_settings = Nokogiri::XML(fetch_password("bindings/sonatype-central-api-token"))
+
+        # Required in order to use xpath sanely (the commands below would
+        # otherwise come up with no results because the top level `<settings>`
+        # has an `xmlns`.
+        m2_settings.remove_namespaces!
+
+        # Note that we're looking for the "sonatype-nexus-staging" server here.
+        # Currently hard-coded to the second <server> element in the array but
+        # there's got to be a better way (I tried a few clever xpath tricks, but
+        # Nokogiri didn't like them one bit)..
+        server_element = m2_settings.xpath("//settings/servers/server")[0]
+
+        nexus_user = server_element.xpath("//username").first.content
+        nexus_pass = server_element.xpath("//password").first.content
+        gnupg_key_id = fetch_password("bindings/gnupg/fingerprint").strip
+
+        gradle_opts = \
+        "-Dorg.gradle.project.NEXUS_USERNAME=#{nexus_user} " \
+        "-Dorg.gradle.project.NEXUS_PASSWORD=#{nexus_pass} " \
+        "-Dorg.gradle.project.signing.gnupg.keyName=#{gnupg_key_id}"
+
+        gnupg_env do |env|
+            env.update(
+                "GRADLE_OPTS" => gradle_opts,
+
+                # Ensure that /usr/bin is in the PATH. This is
+                # where the Java executable is located on a Mac
+                # OS machine.
+                "PATH" => path_with("/usr/bin"),
+            )
+
+            publish_to_sonatype_commands = ['./gradlew', 'publishToSonatype', 'closeAndReleaseSonatypeStagingRepository', '--stacktrace']
+
+            Subprocess.check_call(
+                publish_to_sonatype_commands,
+                env: env
+            )
+
+            puts "Release succeeded!"
+        ensure
+            reset_gradle_properties()
+        end
+    end
+end
+
+private def set_gpg_location
+    begin
+        gpg_location = `which gpg`
+    rescue
+        rputs "Could not find gpg location via `which gpg`, do you have gpg installed?"
+    end
+    append_to_file("gradle.properties", "signing.gnupg.executable=#{gpg_location}")
+end
+
+private def reset_gradle_properties
+    execute_or_fail("git checkout origin/master gradle.properties")
+end
+
+# Gets the contents of the PATH environment variable, but before it does
+# ensures that it includes the given `path_to_include` path. So for example
+# we might request $PATH, but make sure that it includes `/usr/bin'.
+private def path_with(path_to_include)
+    path = ENV["PATH"] || ""
+    path = path.split(":")
+    path << path_to_include unless path.include?(path_to_include)
+    path.join(":")
+end
+
+publish_to_sonatype
