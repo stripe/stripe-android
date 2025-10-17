dc76ee7976b4045bfd18954345708414dd12b761
cttsai-stripe <cttsai@stripe.com>
2025-10-17 11:19:37 -0700

    Revert "Block Konbini and Blik payment methods with ConfirmationTokens in tes…"
    This reverts commit 3a3e2e101e68ee32f0dae470186e27a9947addcf.

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/confirmation/intent/ConfirmationTokenConfirmationInterceptor.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/confirmation/intent/ConfirmationTokenConfirmationInterceptor.kt
@@ -12,7 +12,6 @@ import com.stripe.android.model.ConfirmationTokenClientContextParams
 import com.stripe.android.model.ConfirmationTokenParams
 import com.stripe.android.model.DeferredIntentParams
 import com.stripe.android.model.MandateDataParams
-import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.PaymentMethodOptionsParams
 import com.stripe.android.model.RadarOptions
 import com.stripe.android.model.StripeIntent
@@ -49,8 +48,6 @@ internal class ConfirmationTokenConfirmationInterceptor @AssistedInject construc
         confirmationOption: PaymentMethodConfirmationOption.New,
         shippingValues: ConfirmPaymentIntentParams.Shipping?
     ): ConfirmationDefinition.Action<Args> {
-        failIfUnsupportedPaymentMethod(confirmationOption.createParams.typeCode)
-
         return stripeRepository.createConfirmationToken(
             confirmationTokenParams = prepareConfirmationTokenParams(
                 confirmationOption,
@@ -84,8 +81,6 @@ internal class ConfirmationTokenConfirmationInterceptor @AssistedInject construc
         shippingValues: ConfirmPaymentIntentParams.Shipping?
     ): ConfirmationDefinition.Action<Args> {
         val paymentMethod = confirmationOption.paymentMethod
-        failIfUnsupportedPaymentMethod(paymentMethod.type?.code)
-
         return stripeRepository.createConfirmationToken(
             confirmationTokenParams = prepareConfirmationTokenParams(
                 confirmationOption,
@@ -254,21 +249,6 @@ internal class ConfirmationTokenConfirmationInterceptor @AssistedInject construc
                 customer = customerId,
                 paymentMethodOptions = paymentMethodOptions,
                 requireCvcRecollection = intentConfiguration.requireCvcRecollection
-            )
-        }
-    }
-
-    private fun failIfUnsupportedPaymentMethod(paymentMethodCode: String?) {
-        val unsupportedPaymentMethods = setOf(
-            PaymentMethod.Type.Konbini.code,
-            PaymentMethod.Type.Blik.code,
-        )
-
-        if (paymentMethodCode in unsupportedPaymentMethods && !requestOptions.apiKeyIsLiveMode) {
-            throw IllegalStateException(
-                "(Test-mode only error) The payment method '$paymentMethodCode' is not yet supported with " +
-                    "confirmation tokens. Please contact us if you'd like to use this feature via a GitHub " +
-                    "issue on stripe-android."
             )
         }
     }
