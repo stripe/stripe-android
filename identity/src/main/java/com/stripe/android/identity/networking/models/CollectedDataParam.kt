1a087bff48da4532244abf2348dcdcaa0bc82e31
pearapps <Pearapps@gmail.com>
2025-10-24 15:17:34 -0400

    [Identity] Adding in camera_lens_model to document capture camera info & selfie camera info (#11738)
    * adding in camera lens model
    
    * Update CollectedDataParam.kt
    
    * remove unused parameter
    
    * do selfie too
    
    * Update IdentityViewModel.kt
    
    ---------
    
    Co-authored-by: Ken Ackerson <ken@lickability.net>

--- identity/src/main/java/com/stripe/android/identity/networking/models/CollectedDataParam.kt
+++ identity/src/main/java/com/stripe/android/identity/networking/models/CollectedDataParam.kt
@@ -78,7 +78,8 @@ internal data class CollectedDataParam(
 
         fun createFromFrontUploadedResultsForAutoCapture(
             frontHighResResult: UploadedResult,
-            frontLowResResult: UploadedResult
+            frontLowResResult: UploadedResult,
+            cameraLensModel: String? = null
         ): CollectedDataParam =
             CollectedDataParam(
                 idDocumentFront = DocumentUploadParam(
@@ -96,13 +97,15 @@ internal data class CollectedDataParam(
                     ) {
                         "front low res image id is null"
                     },
-                    uploadMethod = DocumentUploadParam.UploadMethod.AUTOCAPTURE
+                    uploadMethod = DocumentUploadParam.UploadMethod.AUTOCAPTURE,
+                    cameraLensModel = cameraLensModel
                 )
             )
 
         fun createFromBackUploadedResultsForAutoCapture(
             backHighResResult: UploadedResult,
-            backLowResResult: UploadedResult
+            backLowResResult: UploadedResult,
+            cameraLensModel: String? = null
         ): CollectedDataParam =
             CollectedDataParam(
                 idDocumentBack = DocumentUploadParam(
@@ -120,7 +123,8 @@ internal data class CollectedDataParam(
                     ) {
                         "back low res image id is null"
                     },
-                    uploadMethod = DocumentUploadParam.UploadMethod.AUTOCAPTURE
+                    uploadMethod = DocumentUploadParam.UploadMethod.AUTOCAPTURE,
+                    cameraLensModel = cameraLensModel
                 )
             )
 
@@ -134,7 +138,8 @@ internal data class CollectedDataParam(
             trainingConsent: Boolean,
             bestFaceScore: Float,
             faceScoreVariance: Float,
-            numFrames: Int
+            numFrames: Int,
+            bestCameraLensModel: String? = null
         ) = CollectedDataParam(
             face = FaceUploadParam(
                 bestHighResImage = requireNotNull(bestHighResResult.uploadedStripeFile.id),
@@ -146,7 +151,8 @@ internal data class CollectedDataParam(
                 bestFaceScore = bestFaceScore,
                 faceScoreVariance = faceScoreVariance,
                 numFrames = numFrames,
-                trainingConsent = trainingConsent
+                trainingConsent = trainingConsent,
+                bestCameraLensModel = bestCameraLensModel
             )
         )
 
