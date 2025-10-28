f4357eb7a4403f19f12e3d0b4f874cdff9ce1233
toluo-stripe <toluo@stripe.com>
2025-10-28 16:45:10 -0400

    Prepare integrity request manager in AttestationConfirmationDefinition (#11817)

--- paymentsheet/src/main/java/com/stripe/android/paymentelement/confirmation/attestation/AttestationConfirmationModule.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentelement/confirmation/attestation/AttestationConfirmationModule.kt
@@ -1,11 +1,16 @@
 package com.stripe.android.paymentelement.confirmation.attestation
 
+import com.stripe.android.link.injection.PaymentsIntegrityModule
 import com.stripe.android.paymentelement.confirmation.ConfirmationDefinition
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.multibindings.IntoSet
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 
-@Module
+@Module(includes = [PaymentsIntegrityModule::class])
 internal interface AttestationConfirmationModule {
     @JvmSuppressWildcards
     @Binds
@@ -13,4 +18,11 @@ internal interface AttestationConfirmationModule {
     fun bindsAttestationConfirmationDefinition(
         definition: AttestationConfirmationDefinition
     ): ConfirmationDefinition<*, *, *, *>
+
+    companion object {
+        @OptIn(DelicateCoroutinesApi::class)
+        @Provides
+        @AttestationScope
+        fun providesCoroutineScope(): CoroutineScope = GlobalScope
+    }
 }
