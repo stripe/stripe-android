d30759f7bcb6061c2de011fcede0108ff874660a
tjclawson-stripe <163896025+tjclawson-stripe@users.noreply.github.com>
2025-10-28 11:32:49 -0400

    Add PaymentMethodMessaging example app (#11806)
    * Add PaymentMethodMessaging example app

---
+++ payment-method-messaging-example/src/main/java/com/stripe/android/paymentmethodmessaging/example/Settings.kt
@@ -0,0 +1,52 @@
+package com.stripe.android.paymentmethodmessaging.example
+
+import android.content.Context
+import android.content.pm.PackageManager
+
+/**
+ * See [Configure the app](https://github.com/stripe/stripe-android/tree/master/example#configure-the-app)
+ * for instructions on how to configure the example app before running it.
+ */
+class Settings(context: Context) {
+    private val appContext = context.applicationContext
+    private val publishableKeyMetadata = getMetadata(METADATA_KEY_PUBLISHABLE_KEY)
+    private val stripeAccountIdMetadata = getMetadata(METADATA_KEY_STRIPE_ACCOUNT_ID)
+
+    val publishableKey: String
+        get() {
+            return publishableKeyMetadata ?: PUBLISHABLE_KEY
+        }
+
+    val stripeAccountId: String?
+        get() {
+            return stripeAccountIdMetadata ?: STRIPE_ACCOUNT_ID
+        }
+
+    private fun getMetadata(key: String): String? {
+        return appContext.packageManager
+            .getApplicationInfo(appContext.packageName, PackageManager.GET_META_DATA)
+            .metaData
+            .getString(key)
+            .takeIf { it?.isNotBlank() == true }
+    }
+
+    private companion object {
+        /**
+         * Set to publishable key from https://dashboard.stripe.com/test/apikeys
+         */
+        private const val PUBLISHABLE_KEY = "pk_test_51HvTI7Lu5o3P18Zp6t5AgBSkMvWoTtA0nyA7pVYDqpfLkRtWun7qZTYCOHCRep" +
+            "rfLM464yaBeF72UFfB7cY9WG4a00ZnDtiC2C"
+
+        /**
+         * Optionally, set to a Connect Account id to use for API requests to test Connect
+         *
+         * See https://dashboard.stripe.com/test/connect/accounts/overview
+         */
+        private val STRIPE_ACCOUNT_ID: String? = null
+
+        private const val METADATA_KEY_PUBLISHABLE_KEY =
+            "com.stripe.android.paymentmethodmessaging.example.metadata.publishable_key"
+        private const val METADATA_KEY_STRIPE_ACCOUNT_ID =
+            "com.stripe.android.paymentmethodmessaging.example.metadata.stripe_account_id"
+    }
+}
