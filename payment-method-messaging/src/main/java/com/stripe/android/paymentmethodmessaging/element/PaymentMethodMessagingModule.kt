65ac96cc8ee58207e662469aa8148fa0c1ebcb28
tjclawson-stripe <163896025+tjclawson-stripe@users.noreply.github.com>
2025-10-29 13:24:48 -0400

    Update PaymentMethodMessagingModule (#11844)

--- payment-method-messaging/src/main/java/com/stripe/android/paymentmethodmessaging/element/PaymentMethodMessagingModule.kt
+++ payment-method-messaging/src/main/java/com/stripe/android/paymentmethodmessaging/element/PaymentMethodMessagingModule.kt
@@ -6,56 +6,55 @@ import com.stripe.android.BuildConfig
 import com.stripe.android.PaymentConfiguration
 import com.stripe.android.core.injection.ENABLE_LOGGING
 import com.stripe.android.core.injection.PUBLISHABLE_KEY
-import com.stripe.android.networking.StripeRepository
 import com.stripe.android.payments.core.injection.PRODUCT_USAGE
 import com.stripe.android.uicore.image.StripeImageLoader
 import com.stripe.android.uicore.isSystemDarkTheme
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
 
 @Module
-internal object PaymentMethodMessagingModule {
-    @Provides
-    fun providesAppContext(application: Application): Context = application
+internal interface PaymentMethodMessagingModule {
 
-    @Provides
-    @Named(PRODUCT_USAGE)
-    fun providesProductUsage(): Set<String> = emptySet()
-
-    @Provides
-    @Named(ENABLE_LOGGING)
-    fun providesEnableLogging(): Boolean = BuildConfig.DEBUG
-
-    @Provides
-    fun providesStripeImageLoader(
-        application: Application
-    ): StripeImageLoader = StripeImageLoader(application)
-
-    @Provides
-    fun providesIsDarkTheme(
-        application: Application
-    ): () -> Boolean {
-        return application::isSystemDarkTheme
-    }
-
-    @Provides
-    @Named(PUBLISHABLE_KEY)
-    fun providePublishableKey(
-        configuration: PaymentConfiguration
-    ): () -> String = { configuration.publishableKey }
-
-    @Provides
-    fun paymentConfiguration(application: Application): PaymentConfiguration {
-        return PaymentConfiguration.getInstance(application)
-    }
-
-    @Provides
+    @Binds
     fun providesPaymentMethodMessagingCoordinator(
-        stripeRepository: StripeRepository,
-        paymentConfiguration: PaymentConfiguration
-    ): PaymentMethodMessagingCoordinator = DefaultPaymentMethodMessagingCoordinator(
-        stripeRepository,
-        paymentConfiguration
-    )
+        impl: DefaultPaymentMethodMessagingCoordinator
+    ): PaymentMethodMessagingCoordinator
+
+    companion object {
+        @Provides
+        fun providesAppContext(application: Application): Context = application
+
+        @Provides
+        @Named(PRODUCT_USAGE)
+        fun providesProductUsage(): Set<String> = emptySet()
+
+        @Provides
+        @Named(ENABLE_LOGGING)
+        fun providesEnableLogging(): Boolean = BuildConfig.DEBUG
+
+        @Provides
+        fun providesStripeImageLoader(
+            application: Application
+        ): StripeImageLoader = StripeImageLoader(application)
+
+        @Provides
+        fun providesIsDarkTheme(
+            application: Application
+        ): () -> Boolean {
+            return application::isSystemDarkTheme
+        }
+
+        @Provides
+        @Named(PUBLISHABLE_KEY)
+        fun providePublishableKey(
+            configuration: PaymentConfiguration
+        ): () -> String = { configuration.publishableKey }
+
+        @Provides
+        fun paymentConfiguration(application: Application): PaymentConfiguration {
+            return PaymentConfiguration.getInstance(application)
+        }
+    }
 }
