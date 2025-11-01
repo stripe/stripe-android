c26459686ddca6db5784d704c33f64520364e68d
toluo-stripe <toluo@stripe.com>
2025-11-01 01:51:14 -0400

    Confirmation challenge bridge handler

---
+++ payments-core/src/main/java/com/stripe/android/challenge/confirmation/ConfirmationChallengeBridgeHandler.kt
@@ -0,0 +1,24 @@
+package com.stripe.android.challenge.confirmation
+
+import android.webkit.JavascriptInterface
+import kotlinx.coroutines.flow.Flow
+
+internal interface ConfirmationChallengeBridgeHandler {
+
+    val event: Flow<ConfirmationChallengeBridgeEvent>
+
+    @JavascriptInterface
+    fun getInitParams(): String
+
+    @JavascriptInterface
+    fun onReady()
+
+    @JavascriptInterface
+    fun onSuccess(paymentIntentJson: String)
+
+    @JavascriptInterface
+    fun onError(errorMessage: String)
+
+    @JavascriptInterface
+    fun logConsole(logData: String)
+}
---
+++ payments-core/src/test/java/com/stripe/android/challenge/confirmation/FakeConfirmationChallengeBridgeHandler.kt
@@ -0,0 +1,30 @@
+package com.stripe.android.challenge.confirmation
+
+import android.webkit.JavascriptInterface
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+
+internal class FakeConfirmationChallengeBridgeHandler : ConfirmationChallengeBridgeHandler {
+
+    private val _event = MutableSharedFlow<ConfirmationChallengeBridgeEvent>(replay = 1)
+    override val event: Flow<ConfirmationChallengeBridgeEvent> = _event
+
+    suspend fun emitEvent(event: ConfirmationChallengeBridgeEvent) {
+        _event.emit(event)
+    }
+
+    @JavascriptInterface
+    override fun getInitParams() = "{}"
+
+    @JavascriptInterface
+    override fun onReady() = Unit
+
+    @JavascriptInterface
+    override fun onSuccess(paymentIntentJson: String) = Unit
+
+    @JavascriptInterface
+    override fun onError(errorMessage: String) = Unit
+
+    @JavascriptInterface
+    override fun logConsole(logData: String) = Unit
+}
