22929eeca1038d81c69a453e744cc220af2c2b45
toluo-stripe <toluo@stripe.com>
2025-11-02 23:11:37 -0500

    Fix IntentConfirmationChallenge visibility

--- payments-core/src/main/java/com/stripe/android/challenge/confirmation/IntentConfirmationChallengeActivityContract.kt
+++ payments-core/src/main/java/com/stripe/android/challenge/confirmation/IntentConfirmationChallengeActivityContract.kt
@@ -3,12 +3,10 @@ package com.stripe.android.challenge.confirmation
 import android.content.Context
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
-import androidx.annotation.RestrictTo
 import androidx.core.os.BundleCompat
 import com.stripe.android.model.StripeIntent
 
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-class IntentConfirmationChallengeActivityContract :
+internal class IntentConfirmationChallengeActivityContract :
     ActivityResultContract<
         IntentConfirmationChallengeActivityContract.Args,
         IntentConfirmationChallengeActivityResult
@@ -32,14 +30,12 @@ class IntentConfirmationChallengeActivityContract :
         return result ?: IntentConfirmationChallengeActivityResult.Failed(Throwable("No result"))
     }
 
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     data class Args(
         val publishableKey: String,
         val productUsage: Set<String>,
         val intent: StripeIntent
     )
 
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     companion object {
         const val EXTRA_RESULT =
             "com.stripe.android.challenge.confirmation.IntentConfirmationChallengeActivityContract.extra_result"
--- payments-core/src/main/java/com/stripe/android/challenge/confirmation/IntentConfirmationChallengeActivityResult.kt
+++ payments-core/src/main/java/com/stripe/android/challenge/confirmation/IntentConfirmationChallengeActivityResult.kt
@@ -1,16 +1,12 @@
 package com.stripe.android.challenge.confirmation
 
 import android.os.Parcelable
-import androidx.annotation.RestrictTo
 import kotlinx.parcelize.Parcelize
 
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-sealed interface IntentConfirmationChallengeActivityResult : Parcelable {
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+internal sealed interface IntentConfirmationChallengeActivityResult : Parcelable {
     @Parcelize
     data class Success(val clientSecret: String) : IntentConfirmationChallengeActivityResult
 
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     @Parcelize
     data class Failed(val error: Throwable) : IntentConfirmationChallengeActivityResult
 }
