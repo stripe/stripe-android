fb261df75a16e5c5b9313ff69b3918575c9554a4
Twig <aharris.1990@gmail.com>
2025-11-05 14:42:14 -0500

    Fix bottom bar padding

--- paymentsheet/src/main/java/com/stripe/android/link/onramp/ui/OnrampKYCRefreshScreen.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/onramp/ui/OnrampKYCRefreshScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -172,6 +173,7 @@ fun OnrampKYCRefreshScreen(
                             modifier = Modifier
                                 .padding(horizontal = 24.dp)
                                 .align(Alignment.BottomCenter)
+                                .navigationBarsPadding()
                         )
                 }
 
