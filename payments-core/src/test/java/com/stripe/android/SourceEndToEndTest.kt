873dd8f0a494e9e8e7c9aee1f362d8169111290c
Tyler Clawson <tjclawson@stripe.com>
2025-11-06 17:49:57 -0500

    Remove Klarna SourceEndToEnd tests

--- payments-core/src/test/java/com/stripe/android/SourceEndToEndTest.kt
+++ payments-core/src/test/java/com/stripe/android/SourceEndToEndTest.kt
@@ -2,12 +2,9 @@ package com.stripe.android
 
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
-import com.stripe.android.model.Address
 import com.stripe.android.model.CardBrand
 import com.stripe.android.model.CardParams
-import com.stripe.android.model.DateOfBirth
 import com.stripe.android.model.KlarnaSourceParams
-import com.stripe.android.model.SourceOrder
 import com.stripe.android.model.SourceParams
 import com.stripe.android.model.SourceTypeModel
 import org.junit.runner.RunWith
@@ -16,90 +13,6 @@ import kotlin.test.Test
 
 @RunWith(RobolectricTestRunner::class)
 internal class SourceEndToEndTest {
-    @Test
-    fun createKlarnaParams_createsExpectedSourceOrderItems() {
-        val sourceParams = SourceParams.createKlarna(
-            returnUrl = RETURN_URL,
-            currency = "eur",
-            klarnaParams = KlarnaSourceParams(
-                purchaseCountry = "DE",
-                lineItems = LINE_ITEMS,
-                billingDob = DateOfBirth(1, 1, 1990)
-            )
-        )
-        val stripe = createStripe(ApiKeyFixtures.KLARNA_PUBLISHABLE_KEY)
-
-        val source = requireNotNull(stripe.createSourceSynchronous(sourceParams))
-        assertThat(source.redirect?.returnUrl)
-            .isEqualTo(RETURN_URL)
-        assertThat(source.amount)
-            .isEqualTo(31999)
-
-        val items = requireNotNull(source.sourceOrder?.items)
-        assertThat(items)
-            .hasSize(4)
-        assertThat(items.first())
-            .isEqualTo(
-                SourceOrder.Item(
-                    type = SourceOrder.Item.Type.Sku,
-                    amount = 10000,
-                    currency = "eur",
-                    description = "towel",
-                    quantity = 1
-                )
-            )
-    }
-
-    @Test
-    fun createFullKlarnaParams() {
-        val sourceParams = SourceParams.createKlarna(
-            returnUrl = RETURN_URL,
-            currency = "GBP",
-            klarnaParams = KlarnaSourceParams(
-                purchaseCountry = "GB",
-                lineItems = LINE_ITEMS,
-                billingPhone = "02012267709",
-                billingEmail = "test@example.com",
-                billingAddress = Address(
-                    line1 = "29 Arlington Avenue",
-                    city = "London",
-                    country = "UK",
-                    postalCode = "N1 7BE"
-                ),
-                billingFirstName = "Arthur",
-                billingLastName = "Dent",
-                billingDob = DateOfBirth(11, 3, 1952)
-            )
-        )
-        val stripe = createStripe(ApiKeyFixtures.KLARNA_PUBLISHABLE_KEY)
-
-        val source = requireNotNull(stripe.createSourceSynchronous(sourceParams))
-        assertThat(source.redirect?.returnUrl)
-            .isEqualTo(RETURN_URL)
-    }
-
-    @Test
-    fun createKlarnaParamsWithCustomPaymentMethods() {
-        val sourceParams = SourceParams.createKlarna(
-            returnUrl = RETURN_URL,
-            currency = "EUR",
-            klarnaParams = KlarnaSourceParams(
-                purchaseCountry = "DE",
-                lineItems = LINE_ITEMS,
-                customPaymentMethods = setOf(
-                    KlarnaSourceParams.CustomPaymentMethods.Installments,
-                    KlarnaSourceParams.CustomPaymentMethods.PayIn4
-                ),
-                billingDob = DateOfBirth(1, 1, 1990)
-            )
-        )
-
-        val stripe = createStripe(ApiKeyFixtures.KLARNA_PUBLISHABLE_KEY)
-        val source = requireNotNull(stripe.createSourceSynchronous(sourceParams))
-        assertThat(source.redirect?.returnUrl)
-            .isEqualTo(RETURN_URL)
-    }
-
     @Test
     fun `Source objects should be populated with the expected CardBrand value`() {
         val stripe = createStripe(ApiKeyFixtures.DEFAULT_PUBLISHABLE_KEY)
@@ -132,27 +45,6 @@ internal class SourceEndToEndTest {
                 cardModel.brand == cardBrand
             }
         ).isTrue()
-    }
-
-    @Test
-    fun createKlarnaParamsWithPageOptions() {
-        val sourceParams = SourceParams.createKlarna(
-            returnUrl = RETURN_URL,
-            currency = "eur",
-            klarnaParams = KlarnaSourceParams(
-                purchaseCountry = "DE",
-                lineItems = LINE_ITEMS,
-                pageOptions = KlarnaSourceParams.PaymentPageOptions(
-                    pageTitle = "Very cool checkout page",
-                    purchaseType = KlarnaSourceParams.PaymentPageOptions.PurchaseType.Order
-                ),
-                billingDob = DateOfBirth(1, 1, 1990)
-            )
-        )
-        val stripe = createStripe(ApiKeyFixtures.KLARNA_PUBLISHABLE_KEY)
-        val source = requireNotNull(stripe.createSourceSynchronous(sourceParams))
-        assertThat(source.redirect?.returnUrl)
-            .isEqualTo(RETURN_URL)
     }
 
     private fun createStripe(publishableKey: String): Stripe {
