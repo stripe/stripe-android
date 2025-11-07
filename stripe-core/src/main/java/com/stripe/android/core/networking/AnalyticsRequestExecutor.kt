46d01b87bed3107c9d12ea9407e4a9ae31768c19
Jay Newstrom <jaynewstrom@stripe.com>
2025-11-07 10:26:53 -0700

    Don't send analytics during network tests.

--- stripe-core/src/main/java/com/stripe/android/core/networking/AnalyticsRequestExecutor.kt
+++ stripe-core/src/main/java/com/stripe/android/core/networking/AnalyticsRequestExecutor.kt
@@ -8,4 +8,9 @@ fun interface AnalyticsRequestExecutor {
      * Execute the fire-and-forget request asynchronously.
      */
     fun executeAsync(request: AnalyticsRequest)
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    companion object {
+        var ENABLED: Boolean = true
+    }
 }
