2dc98b819eded6ad56dd10ef0d93a566dd166989
Tyler Clawson <tjclawson@stripe.com>
2025-11-11 13:09:10 -0500

    Add PaymentMethodMessageAnalyticsTest

--- payment-method-messaging/src/test/java/com/stripe/android/paymentmethodmessaging/element/analytics/FakeEventReporter.kt
+++ payment-method-messaging/src/test/java/com/stripe/android/paymentmethodmessaging/element/analytics/FakeEventReporter.kt
@@ -2,35 +2,74 @@
 
 package com.stripe.android.paymentmethodmessaging.element.analytics
 
+import app.cash.turbine.ReceiveTurbine
+import app.cash.turbine.Turbine
 import com.stripe.android.paymentmethodmessaging.element.PaymentMethodMessagingContent
 import com.stripe.android.paymentmethodmessaging.element.PaymentMethodMessagingElement
 import com.stripe.android.paymentmethodmessaging.element.PaymentMethodMessagingElementPreview
 
 internal class FakeEventReporter : PaymentMethodMessagingEventReporter {
+    private val _initTurbine = Turbine<Unit>()
+    val initTurbine: ReceiveTurbine<Unit> = _initTurbine
+
+    private val _loadStartedTurbine = Turbine<PaymentMethodMessagingElement.Configuration.State>()
+    val loadStartedTurbine: ReceiveTurbine<PaymentMethodMessagingElement.Configuration.State> = _loadStartedTurbine
+
+    private val _loadSucceededTurbine = Turbine<LoadSucceededCall>()
+    val loadSucceededTurbine: ReceiveTurbine<LoadSucceededCall> = _loadSucceededTurbine
+
+    private val _loadFailedTurbine = Turbine<Throwable>()
+    val loadFailedTurbine: ReceiveTurbine<Throwable> = _loadFailedTurbine
+
+    private val _elementDisplayedTurbine = Turbine<Unit>()
+    val elementDisplayedTurbine: ReceiveTurbine<Unit> = _elementDisplayedTurbine
+
+    private val _elementTappedTurbine = Turbine<Unit>()
+    val elementTappedTurbine: ReceiveTurbine<Unit> = _elementTappedTurbine
+
+    fun validate() {
+        _initTurbine.ensureAllEventsConsumed()
+        _loadStartedTurbine.ensureAllEventsConsumed()
+        _loadSucceededTurbine.ensureAllEventsConsumed()
+        _loadFailedTurbine.ensureAllEventsConsumed()
+        _elementDisplayedTurbine.ensureAllEventsConsumed()
+        _elementTappedTurbine.ensureAllEventsConsumed()
+    }
+
     override fun onInit() {
-        // NO-OP
+        _initTurbine.add(Unit)
     }
 
     override fun onLoadStarted(configuration: PaymentMethodMessagingElement.Configuration.State) {
-        // NO-OP
+        _loadStartedTurbine.add(configuration)
     }
 
     override fun onLoadSucceeded(
         paymentMethods: List<String>,
         content: PaymentMethodMessagingContent
     ) {
-        // NO-OP
+        _loadSucceededTurbine.add(
+            LoadSucceededCall(
+                paymentMethods = paymentMethods,
+                content = content
+            )
+        )
     }
 
     override fun onLoadFailed(error: Throwable) {
-        // NO-OP
+        _loadFailedTurbine.add(error)
     }
 
     override fun onElementDisplayed(appearance: PaymentMethodMessagingElement.Appearance.State) {
-        // NO-OP
+        _elementDisplayedTurbine.add(Unit)
     }
 
     override fun onElementTapped() {
-        // NO-OP
+        _elementTappedTurbine.add(Unit)
     }
+
+    class LoadSucceededCall(
+        val paymentMethods: List<String>,
+        val content: PaymentMethodMessagingContent
+    )
 }
---
+++ payment-method-messaging/src/test/java/com/stripe/android/paymentmethodmessaging/element/analytics/PaymentMethodMessageAnalyticsTest.kt
@@ -0,0 +1,184 @@
+package com.stripe.android.paymentmethodmessaging.element.analytics
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.google.common.truth.Truth.assertThat
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.model.PaymentMethod
+import com.stripe.android.paymentmethodmessaging.element.DefaultPaymentMethodMessagingCoordinator
+import com.stripe.android.paymentmethodmessaging.element.FakeStripeRepository
+import com.stripe.android.paymentmethodmessaging.element.PaymentMethodMessagingContent
+import com.stripe.android.paymentmethodmessaging.element.PaymentMethodMessagingElement
+import com.stripe.android.paymentmethodmessaging.element.PaymentMethodMessagingElementPreview
+import com.stripe.android.testing.CoroutineTestRule
+import com.stripe.android.testing.createComposeCleanupRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@OptIn(ExperimentalCoroutinesApi::class, PaymentMethodMessagingElementPreview::class)
+@RunWith(RobolectricTestRunner::class)
+internal class PaymentMethodMessageAnalyticsTest {
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @get:Rule
+    val composeCleanupRule = createComposeCleanupRule()
+
+    private val testDispatcher = UnconfinedTestDispatcher()
+
+    @get:Rule
+    val coroutineTestRule = CoroutineTestRule(testDispatcher)
+
+    @Test
+    fun `onLoadStarted called with correct params when element is configured successfully`() = runScenario {
+        element.configure(
+            configuration = PaymentMethodMessagingElement.Configuration()
+                .currency("usd")
+                .countryCode("US")
+                .amount(1000L)
+                .locale("en")
+        )
+        val loadStartedCall = eventReporter.loadStartedTurbine.awaitItem()
+        assertThat(loadStartedCall.amount).isEqualTo(1000L)
+        assertThat(loadStartedCall.currency).isEqualTo("usd")
+        assertThat(loadStartedCall.countryCode).isEqualTo("US")
+        assertThat(loadStartedCall.locale).isEqualTo("en")
+
+        eventReporter.loadSucceededTurbine.awaitItem()
+    }
+
+    @Test
+    fun `onLoadSucceeded called when element is configured successfully MultiPartner`() = runScenario {
+        element.configure(
+            configuration = PaymentMethodMessagingElement.Configuration()
+                .currency("usd")
+                .countryCode("US")
+                .amount(1000L)
+                .locale("en")
+        )
+        eventReporter.loadStartedTurbine.awaitItem()
+
+        val loadSucceededCall = eventReporter.loadSucceededTurbine.awaitItem()
+        assertThat(loadSucceededCall.content).isInstanceOf(PaymentMethodMessagingContent.MultiPartner::class.java)
+        assertThat(loadSucceededCall.paymentMethods).isNotNull()
+    }
+
+    @Test
+    fun `onLoadSucceeded called when element is configured successfully SinglePartner`() = runScenario {
+        element.configure(
+            configuration = PaymentMethodMessagingElement.Configuration()
+                .currency("usd")
+                .countryCode("US")
+                .amount(1000L)
+                .locale("en")
+                .paymentMethodTypes(listOf(PaymentMethod.Type.Klarna))
+        )
+        eventReporter.loadStartedTurbine.awaitItem()
+
+        val loadSucceededCall = eventReporter.loadSucceededTurbine.awaitItem()
+        assertThat(loadSucceededCall.content).isInstanceOf(PaymentMethodMessagingContent.SinglePartner::class.java)
+        assertThat(loadSucceededCall.paymentMethods).isNotNull()
+    }
+
+    @Test
+    fun `onLoadSucceeded called when element is configured successfully NoContent`() = runScenario {
+        element.configure(
+            configuration = PaymentMethodMessagingElement.Configuration()
+                .currency("usd")
+                .countryCode("US")
+                .amount(0L)
+                .locale("en")
+        )
+        eventReporter.loadStartedTurbine.awaitItem()
+
+        val loadSucceededCall = eventReporter.loadSucceededTurbine.awaitItem()
+        assertThat(loadSucceededCall.content).isInstanceOf(PaymentMethodMessagingContent.NoContent::class.java)
+        assertThat(loadSucceededCall.paymentMethods).isNotNull()
+    }
+
+    @Test
+    fun `onLoadFailed called when configuration fails`() = runScenario {
+        element.configure(
+            configuration = PaymentMethodMessagingElement.Configuration()
+                .currency("usd")
+                .countryCode("US")
+                .amount(-1L)
+                .locale("en")
+        )
+        eventReporter.loadStartedTurbine.awaitItem()
+
+        val loadSucceededCall = eventReporter.loadFailedTurbine.awaitItem()
+        assertThat(loadSucceededCall.message).isEqualTo("Price must be non negative")
+    }
+
+    @Test
+    fun `onElementDisplayed sent when content called`() = runScenario {
+        element.configure(
+            configuration = PaymentMethodMessagingElement.Configuration()
+                .currency("usd")
+                .countryCode("US")
+                .amount(1000L)
+        )
+        assertThat(eventReporter.loadStartedTurbine.awaitItem()).isNotNull()
+        assertThat(eventReporter.loadSucceededTurbine.awaitItem()).isNotNull()
+        composeRule.setContent {
+            element.Content()
+        }
+        assertThat(eventReporter.elementDisplayedTurbine.awaitItem()).isNotNull()
+    }
+
+    @Test
+    fun `onElementTapped sent when content tapped`() = runScenario {
+        element.configure(
+            configuration = PaymentMethodMessagingElement.Configuration()
+                .currency("usd")
+                .countryCode("US")
+                .amount(1000L)
+        )
+        assertThat(eventReporter.loadStartedTurbine.awaitItem()).isNotNull()
+        assertThat(eventReporter.loadSucceededTurbine.awaitItem()).isNotNull()
+        composeRule.setContent {
+            element.Content()
+        }
+
+        eventReporter.elementDisplayedTurbine.awaitItem()
+        composeRule.waitForIdle()
+        composeRule.onNodeWithText(text = "buy stuff", substring = true).performClick()
+        assertThat(eventReporter.elementTappedTurbine.awaitItem()).isNotNull()
+    }
+
+    private class Scenario(
+        val element: PaymentMethodMessagingElement,
+        val eventReporter: FakeEventReporter
+    )
+
+    private fun runScenario(
+        testBlock: suspend Scenario.() -> Unit
+    ) = runTest {
+        val eventReporter = FakeEventReporter()
+        val coordinator = DefaultPaymentMethodMessagingCoordinator(
+            stripeRepository = FakeStripeRepository(),
+            paymentConfiguration = { PaymentConfiguration(publishableKey = "pk_123_test") },
+            eventReporter = eventReporter
+        )
+
+        val element = PaymentMethodMessagingElement(
+            messagingCoordinator = coordinator,
+            eventReporter = eventReporter
+        )
+
+        assertThat(eventReporter.initTurbine.awaitItem()).isNotNull()
+
+        Scenario(
+            element = element,
+            eventReporter = eventReporter
+        ).testBlock()
+        eventReporter.validate()
+    }
+}
