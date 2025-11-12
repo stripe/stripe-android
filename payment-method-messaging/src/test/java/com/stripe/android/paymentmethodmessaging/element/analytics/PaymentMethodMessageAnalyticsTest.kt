75c4260694b701e92ea8ba70a6ea979180a32a24
Tyler Clawson <tjclawson@stripe.com>
2025-11-12 13:58:10 -0500

    Validate error reporter

--- payment-method-messaging/src/test/java/com/stripe/android/paymentmethodmessaging/element/analytics/PaymentMethodMessageAnalyticsTest.kt
+++ payment-method-messaging/src/test/java/com/stripe/android/paymentmethodmessaging/element/analytics/PaymentMethodMessageAnalyticsTest.kt
@@ -206,5 +206,6 @@ internal class PaymentMethodMessageAnalyticsTest {
             errorReporter = errorReporter
         ).testBlock()
         eventReporter.validate()
+        errorReporter.ensureAllEventsConsumed()
     }
 }
