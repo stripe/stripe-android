50da68d7ebb85803935282bb530cba985c9ca4a9
Tyler Clawson <tjclawson@stripe.com>
2025-11-13 14:16:54 -0500

    Link

--- payment-method-messaging/src/androidTest/java/com/stripe/android/paymentmethodmessaging/element/PaymentMethodMessagingAnalyticsTest.kt
+++ payment-method-messaging/src/androidTest/java/com/stripe/android/paymentmethodmessaging/element/PaymentMethodMessagingAnalyticsTest.kt
@@ -297,6 +297,3 @@ internal class PaymentMethodMessagingElementAnalyticsTest {
         }
     }
 }
-
-
-
--- payment-method-messaging/src/androidTest/java/com/stripe/android/paymentmethodmessaging/element/PaymentMethodMessagingElementTestRunner.kt
+++ payment-method-messaging/src/androidTest/java/com/stripe/android/paymentmethodmessaging/element/PaymentMethodMessagingElementTestRunner.kt
@@ -62,4 +62,3 @@ internal fun runPaymentMethodMessagingElementTest(
         networkRule.validate()
     }
 }
-
