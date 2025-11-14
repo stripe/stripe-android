1378bd482004d0c26c9a44305e231d9ab2c18acb
Jay Newstrom <jaynewstrom@stripe.com>
2025-11-14 09:03:00 -0700

    Simplify PaymentOptions dagger setup. (#11986)

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/injection/PaymentOptionsViewModelModule.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/injection/PaymentOptionsViewModelModule.kt
@@ -1,18 +1,31 @@
 package com.stripe.android.paymentsheet.injection
 
+import android.app.Application
+import android.content.Context
+import com.stripe.android.paymentelement.callbacks.PaymentElementCallbackIdentifier
+import com.stripe.android.payments.core.injection.PRODUCT_USAGE
+import com.stripe.android.paymentsheet.PaymentOptionContract
 import com.stripe.android.paymentsheet.analytics.EventReporter
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 import javax.inject.Singleton
 
-@Module(
-    subcomponents = [
-        PaymentOptionsViewModelSubcomponent::class,
-    ]
-)
+@Module
 internal class PaymentOptionsViewModelModule {
 
     @Provides
     @Singleton
     fun provideEventReporterMode(): EventReporter.Mode = EventReporter.Mode.Custom
+
+    @Provides
+    fun provideContext(application: Application): Context = application
+
+    @Provides
+    @PaymentElementCallbackIdentifier
+    fun provideCallbackIdentifier(args: PaymentOptionContract.Args): String = args.paymentElementCallbackIdentifier
+
+    @Provides
+    @Named(PRODUCT_USAGE)
+    fun provideProductUsage(args: PaymentOptionContract.Args): Set<String> = args.productUsage
 }
