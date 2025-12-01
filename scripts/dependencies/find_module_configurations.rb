14e434323423d119a6b2096bb033927d32023ab5
Samer Alabi <samer@stripe.com>
2025-12-01 11:46:42 -0500

    Fix transitive dependencies scripts for AGP 8.13

--- scripts/dependencies/find_module_configurations.rb
+++ scripts/dependencies/find_module_configurations.rb
@@ -1,17 +1,17 @@
 def find_module_configurations(module_name)
-    output, stderr, status = Open3.capture3("./gradlew #{module_name}:resolvableConfigurations 2>&1")
+    output, stderr, status = Open3.capture3("./gradlew #{module_name}:printConfigurations --no-configuration-cache 2>&1")
 
     unless status.success?
         puts "Warning: Failed to get configurations for #{module_name}"
-        return []
+        return {}
     end
 
     output = output.force_encoding('UTF-8').scrub
 
     configurations = {}
     output.lines.each do |line|
-        if match = line.match(/^Configuration\s+(\w+ReleaseRuntimeClasspath)\s*$/)
-            config_name = match[1]
+        if match = line.match(/^\w+ReleaseRuntimeClasspath$/)
+            config_name = match[0]
             flavor_name = config_name.sub("ReleaseRuntimeClasspath", "")
             configurations[flavor_name] = config_name
         end
