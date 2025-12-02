84847184b847dfbca3652897b24d6074a3d837c4
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2025-12-01 21:53:04 -0500

    Add `base` & `tap-to-add` product flavors for PS example app (#12067)

--- paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/EnableTapToAddSettingsDefinition.kt
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/EnableTapToAddSettingsDefinition.kt
@@ -1,12 +1,17 @@
 package com.stripe.android.paymentsheet.example.playground.settings
 
 import com.stripe.android.core.utils.FeatureFlags
+import com.stripe.android.paymentsheet.example.BuildConfig
 import com.stripe.android.paymentsheet.example.playground.model.CheckoutRequest
 
 internal object EnableTapToAddSettingsDefinition : FeatureFlagSettingsDefinition(
     featureFlag = FeatureFlags.enableTapToAdd,
     allowedIntegrationTypes = PlaygroundConfigurationData.IntegrationType.paymentFlows().toList()
 ) {
+    override fun applicable(configurationData: PlaygroundConfigurationData): Boolean {
+        return BuildConfig.FLAVOR == "tapToAdd"
+    }
+
     override fun configure(value: Boolean, checkoutRequestBuilder: CheckoutRequest.Builder) {
         checkoutRequestBuilder.allowsTapToAdd(value)
     }
