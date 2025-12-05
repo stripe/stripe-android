69eebbdd935bc815b876b300b85604bc5c5128ce
System Administrator <amk@stripe.com>
2025-12-05 13:20:37 -0800

    fix tests

--- payments-core/src/test/java/com/stripe/android/model/ElementsSessionFixtures.kt
+++ payments-core/src/test/java/com/stripe/android/model/ElementsSessionFixtures.kt
@@ -12,12 +12,6 @@ internal object ElementsSessionFixtures {
             "link_bank_enabled": false,
             "link_bank_onboarding_enabled": false
           },
-          "experiments_data": {
-            "arb_id": "232dd033-0b45-4456-b834-ecdcb02ab1fb",
-            "experiment_assignments": {
-              "ocs_mobile_horizontal_mode_android_aa": "control",
-            }
-          },
           "merchant_country": "US",
           "payment_method_preference": {
             "object": "payment_method_preference",
@@ -289,6 +283,77 @@ internal object ElementsSessionFixtures {
             "link_bank_onboarding_enabled": false
           },
           "merchant_country": "US",
+          "payment_method_preference": {
+            "object": "payment_method_preference",
+            "country_code": "US",
+            "ordered_payment_method_types": [
+              "card",
+              "ideal",
+              "sepa_debit",
+              "bancontact"
+            ],
+            "payment_intent": {
+              "id": "pi_3JTDhYIyGgrkZxL71IDUGKps",
+              "object": "payment_intent",
+              "amount": 973,
+              "canceled_at": null,
+              "cancellation_reason": null,
+              "capture_method": "automatic",
+              "client_secret": "pi_3JTDhYIyGgrkZxL71IDUGKps_secret_aWuzwD4JvF1HM8XJTdUsXG6Za",
+              "confirmation_method": "automatic",
+              "created": 1630103948,
+              "currency": "eur",
+              "description": null,
+              "last_payment_error": null,
+              "livemode": false,
+              "next_action": null,
+              "payment_method": null,
+              "payment_method_types": [
+                "bancontact",
+                "card",
+                "sepa_debit",
+                "ideal"
+              ],
+              "receipt_email": null,
+              "setup_future_usage": null,
+              "shipping": {
+                "address": {
+                  "city": "San Francisco",
+                  "country": "US",
+                  "line1": "510 Townsend St",
+                  "line2": null,
+                  "postal_code": "94102",
+                  "state": "California"
+                },
+                "carrier": null,
+                "name": "Bruno",
+                "phone": null,
+                "tracking_number": null
+              },
+              "source": null,
+              "status": "requires_payment_method"
+            },
+            "type": "payment_intent"
+          }
+        }
+        """.trimIndent()
+    )
+
+    val EXPANDED_PAYMENT_INTENT_WITH_EXPERIMENTS_DATA_JSON = JSONObject(
+        """
+        {
+          "business_name": "Mybusiness",
+          "link_settings": {
+            "link_bank_enabled": false,
+            "link_bank_onboarding_enabled": false
+          },
+          "merchant_country": "US",
+          "experiments_data": {
+              "arb_id": "232dd033-0b45-4456-b834-ecdcb02ab1fb",
+              "experiment_assignments": {
+                "ocs_mobile_horizontal_mode_android_aa": "control",
+              }
+            },
           "payment_method_preference": {
             "object": "payment_method_preference",
             "country_code": "US",
@@ -1356,7 +1421,7 @@ internal object ElementsSessionFixtures {
                 "elements_link_in_payment_element_only": "treatment",
                 "elements_link_in_payment_element_only_holdback": "control",
                 "elements_link_longterm_holdback": "control",
-                "lpm_discoverability_upe_experiment_1": "downward_arrow_treatment",
+                "lpm_discoverability_upe_experiment_1": "downward_arrow_treatment"
               },
               "experiments_data": {
                 "arb_id": "232dd033-0b45-4456-b834-ecdcb02ab1fb",
