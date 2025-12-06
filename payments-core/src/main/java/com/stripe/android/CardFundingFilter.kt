f40462801940c78fcc9ee93012cb03b9e69e7770
toluo-stripe <toluo@stripe.com>
2025-12-06 11:23:36 -0500

    use elements session flag to enable card fund filtering

--- payments-core/src/main/java/com/stripe/android/CardFundingFilter.kt
+++ payments-core/src/main/java/com/stripe/android/CardFundingFilter.kt
@@ -7,4 +7,9 @@ import com.stripe.android.model.CardFunding
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 interface CardFundingFilter : Parcelable {
     fun isAccepted(cardFunding: CardFunding): Boolean
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    interface Factory<T> {
+        operator fun invoke(params: T): CardFundingFilter
+    }
 }
