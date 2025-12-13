ed30310ee0c080b6d67e3a3666fe787d60c321b2
tianzhao-stripe <tianzhao@stripe.com>
2025-12-12 17:34:10 -0800

    klarna definition

--- paymentsheet/src/main/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/KlarnaDefinition.kt
+++ paymentsheet/src/main/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/KlarnaDefinition.kt
@@ -1,6 +1,8 @@
 package com.stripe.android.lpmfoundations.paymentmethod.definitions
 
 import com.stripe.android.core.strings.resolvableString
+import com.stripe.android.core.utils.FeatureFlags
+import com.stripe.android.lpmfoundations.luxe.FormElementsBuilder
 import com.stripe.android.lpmfoundations.luxe.SupportedPaymentMethod
 import com.stripe.android.lpmfoundations.luxe.TransformSpecToElements
 import com.stripe.android.lpmfoundations.paymentmethod.AddPaymentMethodRequirement
@@ -8,10 +10,19 @@ import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodDefinition
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
 import com.stripe.android.lpmfoundations.paymentmethod.UiDefinitionFactory
 import com.stripe.android.model.PaymentMethod
+import com.stripe.android.model.SetupIntent
+import com.stripe.android.paymentsheet.PaymentSheet.BillingDetailsCollectionConfiguration.AddressCollectionMode
 import com.stripe.android.ui.core.R
 import com.stripe.android.ui.core.elements.KlarnaMandateTextSpec
+import com.stripe.android.ui.core.elements.MandateTextElement
 import com.stripe.android.ui.core.elements.SharedDataSpec
+import com.stripe.android.ui.core.elements.StaticTextElement
+import com.stripe.android.uicore.elements.CountryConfig
+import com.stripe.android.uicore.elements.CountryElement
+import com.stripe.android.uicore.elements.DropdownFieldController
 import com.stripe.android.uicore.elements.FormElement
+import com.stripe.android.uicore.elements.IdentifierSpec
+import com.stripe.android.uicore.elements.SectionElement
 
 internal object KlarnaDefinition : PaymentMethodDefinition {
     override val type: PaymentMethod.Type = PaymentMethod.Type.Klarna
@@ -26,10 +37,89 @@ internal object KlarnaDefinition : PaymentMethodDefinition {
         return metadata.hasIntentToSetup(type.code) && metadata.mandateAllowed(type)
     }
 
-    override fun uiDefinitionFactory(): UiDefinitionFactory = KlarnaUiDefinitionFactory
+    override fun uiDefinitionFactory(): UiDefinitionFactory {
+        return if (FeatureFlags.enableKlarnaFormRemoval.isEnabled) {
+            KlarnaRemovedFormUiDefinitionFactory
+        } else {
+            KlarnaSpecUiDefinitionFactory
+        }
+    }
 }
 
-private object KlarnaUiDefinitionFactory : UiDefinitionFactory.RequiresSharedDataSpec {
+private object KlarnaRemovedFormUiDefinitionFactory : UiDefinitionFactory.Simple {
+    override fun createSupportedPaymentMethod(): SupportedPaymentMethod {
+        return SupportedPaymentMethod(
+            paymentMethodDefinition = KlarnaDefinition,
+            displayNameResource = R.string.stripe_paymentsheet_payment_method_klarna,
+            iconResource = R.drawable.stripe_ic_paymentsheet_pm_klarna,
+            iconResourceNight = null,
+            subtitle = R.string.stripe_klarna_pay_later.resolvableString
+        )
+    }
+
+    override fun createFormElements(
+        metadata: PaymentMethodMetadata,
+        arguments: UiDefinitionFactory.Arguments
+    ): List<FormElement> {
+        val formElementsBuilder = FormElementsBuilder(
+            arguments = arguments,
+        )
+
+        if (KlarnaDefinition.requiresMandate(metadata)) {
+            formElementsBuilder.footer(
+                getKlarnaMandateElement(
+                    merchantName = metadata.merchantName
+                )
+            )
+        }
+
+        if (metadata.stripeIntent is SetupIntent &&
+            arguments.billingDetailsCollectionConfiguration.address != AddressCollectionMode.Full
+        ) {
+            formElementsBuilder
+                .element(
+                    StaticTextElement(
+                        IdentifierSpec.Generic("klarna_header_text"),
+                        stringResId = R.string.stripe_klarna_buy_now_pay_later
+                    )
+                )
+                .element(
+                    getKlarnaCountryElement(
+                        allowedCountryCodes = arguments.billingDetailsCollectionConfiguration.allowedBillingCountries,
+                        initialValue = metadata.stripeIntent.countryCode
+                    )
+                )
+        }
+        return formElementsBuilder.build()
+    }
+
+    private fun getKlarnaMandateElement(merchantName: String): MandateTextElement {
+        return MandateTextElement(
+            identifier = IdentifierSpec.Generic("klarna_mandate"),
+            stringResId = R.string.stripe_klarna_mandate,
+            args = listOf(merchantName, merchantName),
+        )
+    }
+
+    private fun getKlarnaCountryElement(
+        allowedCountryCodes: Set<String>,
+        initialValue: String?
+    ): FormElement {
+        val identifier = IdentifierSpec.Generic("billing_details[address][country]")
+
+        return SectionElement.wrap(
+            CountryElement(
+                identifier = identifier,
+                controller = DropdownFieldController(
+                    CountryConfig(allowedCountryCodes),
+                    initialValue = initialValue
+                )
+            )
+        )
+    }
+}
+
+private object KlarnaSpecUiDefinitionFactory : UiDefinitionFactory.RequiresSharedDataSpec {
     override fun createSupportedPaymentMethod(
         metadata: PaymentMethodMetadata,
         sharedDataSpec: SharedDataSpec,
