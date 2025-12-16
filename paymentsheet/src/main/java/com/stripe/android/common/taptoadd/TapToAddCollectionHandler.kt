bf0d98e2530680132acfe30206b617bf02d2aa08
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2025-12-16 11:15:24 -0500

    Collect payment method with Terminal APIs (#12128)
    * Collect payment method with Terminal APIs
    
    * Remove `enableCustomerCancellation`
    
    * Add test functions

--- paymentsheet/src/main/java/com/stripe/android/common/taptoadd/TapToAddCollectionHandler.kt
+++ paymentsheet/src/main/java/com/stripe/android/common/taptoadd/TapToAddCollectionHandler.kt
@@ -4,9 +4,22 @@ import com.stripe.android.common.exception.stripeErrorMessage
 import com.stripe.android.core.strings.ResolvableString
 import com.stripe.android.core.strings.resolvableString
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
+import com.stripe.android.model.PaymentMethod
 import com.stripe.android.paymentelement.TapToAddPreview
 import com.stripe.android.paymentelement.confirmation.intent.CallbackNotFoundException
 import com.stripe.android.paymentsheet.CreateIntentResult
+import com.stripe.android.paymentsheet.model.PaymentSelection
+import com.stripe.stripeterminal.external.callable.Callback
+import com.stripe.stripeterminal.external.callable.Cancelable
+import com.stripe.stripeterminal.external.callable.SetupIntentCallback
+import com.stripe.stripeterminal.external.models.AllowRedisplay
+import com.stripe.stripeterminal.external.models.SetupIntent
+import com.stripe.stripeterminal.external.models.SetupIntentConfiguration
+import com.stripe.stripeterminal.external.models.TerminalException
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.suspendCoroutine
 
 internal interface TapToAddCollectionHandler {
     suspend fun collect(metadata: PaymentMethodMetadata): CollectionState
@@ -24,11 +37,16 @@ internal interface TapToAddCollectionHandler {
         @OptIn(TapToAddPreview::class)
         fun create(
             isStripeTerminalSdkAvailable: IsStripeTerminalSdkAvailable,
+            terminalWrapper: TerminalWrapper,
             connectionManager: TapToAddConnectionManager,
             createCardPresentSetupIntentCallbackRetriever: CreateCardPresentSetupIntentCallbackRetriever,
         ): TapToAddCollectionHandler {
             return if (isStripeTerminalSdkAvailable()) {
-                DefaultTapToAddCollectionHandler(connectionManager, createCardPresentSetupIntentCallbackRetriever)
+                DefaultTapToAddCollectionHandler(
+                    terminalWrapper = terminalWrapper,
+                    connectionManager = connectionManager,
+                    createCardPresentSetupIntentCallbackRetriever = createCardPresentSetupIntentCallbackRetriever,
+                )
             } else {
                 UnsupportedTapToAddCollectionHandler()
             }
@@ -38,6 +56,7 @@ internal interface TapToAddCollectionHandler {
 
 @OptIn(TapToAddPreview::class)
 internal class DefaultTapToAddCollectionHandler(
+    private val terminalWrapper: TerminalWrapper,
     private val connectionManager: TapToAddConnectionManager,
     private val createCardPresentSetupIntentCallbackRetriever: CreateCardPresentSetupIntentCallbackRetriever,
 ) : TapToAddCollectionHandler {
@@ -64,7 +83,7 @@ internal class DefaultTapToAddCollectionHandler(
         }
 
         when (val result = callback.createCardPresentSetupIntent()) {
-            is CreateIntentResult.Success -> TapToAddCollectionHandler.CollectionState.Collected
+            is CreateIntentResult.Success -> collectWithIntent(result.clientSecret, metadata)
             is CreateIntentResult.Failure -> {
                 TapToAddCollectionHandler.CollectionState.FailedCollection(
                     error = result.cause,
@@ -81,6 +100,95 @@ internal class DefaultTapToAddCollectionHandler(
             )
         }
     )
+
+    private suspend fun collectWithIntent(
+        clientSecret: String,
+        metadata: PaymentMethodMetadata,
+    ): TapToAddCollectionHandler.CollectionState {
+        val setupIntent = retrieveSetupIntent(clientSecret)
+        val setupIntentWithAttachedPaymentMethod = collectPaymentMethod(setupIntent, metadata)
+
+        confirmSetupIntent(setupIntentWithAttachedPaymentMethod)
+
+        return TapToAddCollectionHandler.CollectionState.Collected
+    }
+
+    private suspend fun retrieveSetupIntent(clientSecret: String) = suspendCoroutine { continuation ->
+        terminal().retrieveSetupIntent(
+            clientSecret = clientSecret,
+            callback = continuation.createSetupIntentCallback(),
+        )
+    }
+
+    private suspend fun collectPaymentMethod(
+        intent: SetupIntent,
+        metadata: PaymentMethodMetadata,
+    ) = suspendCancellableCoroutine { continuation ->
+        val allowRedisplay = metadata.allowRedisplay(
+            code = PaymentMethod.Type.Card.code,
+            customerRequestedSave = PaymentSelection.CustomerRequestedSave.RequestReuse,
+        )
+
+        val cancellable = terminal().collectSetupIntentPaymentMethod(
+            intent = intent,
+            allowRedisplay = allowRedisplay.toTerminalAllowRedisplay(),
+            config = SetupIntentConfiguration.Builder().build(),
+            callback = continuation.createSetupIntentCallback(),
+        )
+
+        continuation.handleCancellation(cancellable)
+    }
+
+    private suspend fun confirmSetupIntent(
+        intent: SetupIntent,
+    ) = suspendCancellableCoroutine { continuation ->
+        val cancellable = terminal().confirmSetupIntent(
+            intent = intent,
+            callback = continuation.createSetupIntentCallback(),
+        )
+
+        continuation.handleCancellation(cancellable)
+    }
+
+    private fun Continuation<SetupIntent>.createSetupIntentCallback(): SetupIntentCallback {
+        return object : SetupIntentCallback {
+            override fun onSuccess(setupIntent: SetupIntent) {
+                resumeWith(Result.success(setupIntent))
+            }
+
+            override fun onFailure(e: TerminalException) {
+                resumeWith(Result.failure(e))
+            }
+        }
+    }
+
+    private fun CancellableContinuation<SetupIntent>.handleCancellation(
+        cancelable: Cancelable
+    ) {
+        invokeOnCancellation {
+            cancelable.cancel(
+                object : Callback {
+                    override fun onSuccess() {
+                        // No-op
+                    }
+
+                    override fun onFailure(e: TerminalException) {
+                        // No-op
+                    }
+                }
+            )
+        }
+    }
+
+    private fun PaymentMethod.AllowRedisplay.toTerminalAllowRedisplay(): AllowRedisplay {
+        return when (this) {
+            PaymentMethod.AllowRedisplay.UNSPECIFIED -> AllowRedisplay.UNSPECIFIED
+            PaymentMethod.AllowRedisplay.LIMITED -> AllowRedisplay.LIMITED
+            PaymentMethod.AllowRedisplay.ALWAYS -> AllowRedisplay.ALWAYS
+        }
+    }
+
+    private fun terminal() = terminalWrapper.getInstance()
 }
 
 internal class UnsupportedTapToAddCollectionHandler : TapToAddCollectionHandler {
--- paymentsheet/src/test/java/com/stripe/android/common/taptoadd/TapToAddCollectionHandlerTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/common/taptoadd/TapToAddCollectionHandlerTest.kt
@@ -10,10 +10,29 @@ import com.stripe.android.paymentelement.TapToAddPreview
 import com.stripe.android.paymentelement.confirmation.intent.CallbackNotFoundException
 import com.stripe.android.paymentsheet.CreateIntentResult
 import com.stripe.android.paymentsheet.R
+import com.stripe.stripeterminal.Terminal
+import com.stripe.stripeterminal.external.callable.Cancelable
+import com.stripe.stripeterminal.external.callable.SetupIntentCallback
+import com.stripe.stripeterminal.external.models.AllowRedisplay
+import com.stripe.stripeterminal.external.models.SetupIntent
+import com.stripe.stripeterminal.external.models.SetupIntentConfiguration
+import com.stripe.stripeterminal.external.models.TerminalErrorCode
+import com.stripe.stripeterminal.external.models.TerminalException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.kotlin.KStubbing
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.timeout
+import org.mockito.kotlin.verify
 import org.robolectric.RobolectricTestRunner
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.test.fail
 
 @OptIn(TapToAddPreview::class)
 @RunWith(RobolectricTestRunner::class)
@@ -22,6 +41,7 @@ class TapToAddCollectionHandlerTest {
     fun `create returns UnsupportedTapToAddCollectionHandler when terminal SDK is not available`() {
         val handler = TapToAddCollectionHandler.create(
             isStripeTerminalSdkAvailable = { false },
+            terminalWrapper = TestTerminalWrapper.noOp(),
             connectionManager = FakeTapToAddConnectionManager.noOp(isSupported = true, isConnected = false),
             createCardPresentSetupIntentCallbackRetriever = FakeCreateCardPresentSetupIntentCallbackRetriever.noOp(
                 callbackResult = Result.success(DEFAULT_CALLBACK),
@@ -35,6 +55,7 @@ class TapToAddCollectionHandlerTest {
     fun `create returns DefaultTapToAddCollectionHandler when terminal SDK is available`() {
         val handler = TapToAddCollectionHandler.create(
             isStripeTerminalSdkAvailable = { true },
+            terminalWrapper = TestTerminalWrapper.noOp(),
             connectionManager = FakeTapToAddConnectionManager.noOp(isSupported = true, isConnected = false),
             createCardPresentSetupIntentCallbackRetriever = FakeCreateCardPresentSetupIntentCallbackRetriever.noOp(
                 callbackResult = Result.success(DEFAULT_CALLBACK),
@@ -42,31 +63,6 @@ class TapToAddCollectionHandlerTest {
         )
 
         assertThat(handler).isInstanceOf(DefaultTapToAddCollectionHandler::class.java)
-    }
-
-    @Test
-    fun `handler returns Collected when already connected`() = runScenario(
-        isConnected = true,
-    ) {
-        val result = handler.collect(DEFAULT_METADATA)
-
-        assertThat(retrieverScenario.waitForCallbackCalls.awaitItem()).isNotNull()
-
-        assertThat(result).isEqualTo(TapToAddCollectionHandler.CollectionState.Collected)
-    }
-
-    @Test
-    fun `handler calls connect and returns Collected when not connected`() = runScenario(
-        isConnected = false,
-        awaitResult = Result.success(true)
-    ) {
-        val result = handler.collect(DEFAULT_METADATA)
-
-        assertThat(managerScenario.connectCalls.awaitItem()).isNotNull()
-        assertThat(managerScenario.awaitCalls.awaitItem()).isNotNull()
-        assertThat(retrieverScenario.waitForCallbackCalls.awaitItem()).isNotNull()
-
-        assertThat(result).isEqualTo(TapToAddCollectionHandler.CollectionState.Collected)
     }
 
     @Test
@@ -142,7 +138,7 @@ class TapToAddCollectionHandlerTest {
     }
 
     @Test
-    fun `handler returns Collected when callback returns Success`() = runScenario(
+    fun `handler returns Collected when flow is successfully completed`() = runScenario(
         isConnected = true,
         callbackResult = Result.success(
             CreateCardPresentSetupIntentCallback {
@@ -150,19 +146,180 @@ class TapToAddCollectionHandlerTest {
             }
         ),
     ) {
-        val result = handler.collect(DEFAULT_METADATA)
+        val result = testScope.backgroundScope.async {
+            handler.collect(DEFAULT_METADATA)
+        }
 
         assertThat(retrieverScenario.waitForCallbackCalls.awaitItem()).isNotNull()
 
-        assertThat(result).isEqualTo(TapToAddCollectionHandler.CollectionState.Collected)
+        val retrievedSetupIntent = checkRetrieveSetupIntent("si_123_secret")
+        val collectedIntent = checkCollectCall(retrievedSetupIntent)
+        checkConfirmCall(collectedIntent)
+
+        assertThat(result.await()).isEqualTo(TapToAddCollectionHandler.CollectionState.Collected)
+    }
+
+    @Test
+    fun `handler returns FailedCollection when retrieveSetupIntent fails`() = runScenario(
+        isConnected = true,
+        callbackResult = Result.success(
+            CreateCardPresentSetupIntentCallback {
+                CreateIntentResult.Success("si_123_secret")
+            }
+        ),
+    ) {
+        val result = testScope.backgroundScope.async {
+            handler.collect(DEFAULT_METADATA)
+        }
+
+        assertThat(retrieverScenario.waitForCallbackCalls.awaitItem()).isNotNull()
+
+        val terminalException = TerminalException(
+            errorCode = TerminalErrorCode.UNEXPECTED_SDK_ERROR,
+            errorMessage = "Failed to retrieve setup intent"
+        )
+
+        val retrieveSetupIntentCall = terminalScenario.retrieveSetupIntentCalls.awaitItem()
+        retrieveSetupIntentCall.callback.onFailure(terminalException)
+
+        assertThat(result.await()).isEqualTo(
+            TapToAddCollectionHandler.CollectionState.FailedCollection(
+                error = terminalException,
+                displayMessage = R.string.stripe_something_went_wrong.resolvableString
+            )
+        )
+    }
+
+    @Test
+    fun `handler returns FailedCollection when collectSetupIntentPaymentMethod fails`() = runScenario(
+        isConnected = true,
+        callbackResult = Result.success(
+            CreateCardPresentSetupIntentCallback {
+                CreateIntentResult.Success("si_123_secret")
+            }
+        ),
+    ) {
+        val result = testScope.backgroundScope.async {
+            handler.collect(DEFAULT_METADATA)
+        }
+
+        assertThat(retrieverScenario.waitForCallbackCalls.awaitItem()).isNotNull()
+
+        val retrievedSetupIntent = checkRetrieveSetupIntent("si_123_secret")
+
+        val terminalException = TerminalException(
+            errorCode = TerminalErrorCode.CANCELED,
+            errorMessage = "Customer canceled the operation"
+        )
+
+        val collectPaymentMethodCall = terminalScenario.collectPaymentMethodCalls.awaitItem()
+        assertThat(collectPaymentMethodCall.intent).isEqualTo(retrievedSetupIntent)
+        collectPaymentMethodCall.callback.onFailure(terminalException)
+
+        assertThat(result.await()).isEqualTo(
+            TapToAddCollectionHandler.CollectionState.FailedCollection(
+                error = terminalException,
+                displayMessage = R.string.stripe_something_went_wrong.resolvableString
+            )
+        )
+    }
+
+    @Test
+    fun `handler returns FailedCollection when confirmSetupIntent fails`() = runScenario(
+        isConnected = true,
+        callbackResult = Result.success(
+            CreateCardPresentSetupIntentCallback {
+                CreateIntentResult.Success("si_123_secret")
+            }
+        ),
+    ) {
+        val result = testScope.backgroundScope.async {
+            handler.collect(DEFAULT_METADATA)
+        }
+
+        assertThat(retrieverScenario.waitForCallbackCalls.awaitItem()).isNotNull()
+
+        val retrievedSetupIntent = checkRetrieveSetupIntent("si_123_secret")
+        val collectedIntent = checkCollectCall(retrievedSetupIntent)
+
+        val terminalException = TerminalException(
+            errorCode = TerminalErrorCode.DECLINED_BY_STRIPE_API,
+            errorMessage = "Setup intent confirmation failed"
+        )
+
+        val confirmSetupIntentCall = terminalScenario.confirmSetupIntentCalls.awaitItem()
+        assertThat(confirmSetupIntentCall.intent).isEqualTo(collectedIntent)
+        confirmSetupIntentCall.callback.onFailure(terminalException)
+
+        assertThat(result.await()).isEqualTo(
+            TapToAddCollectionHandler.CollectionState.FailedCollection(
+                error = terminalException,
+                displayMessage = R.string.stripe_something_went_wrong.resolvableString
+            )
+        )
+    }
+
+    @Test
+    fun `handler cancels collectSetupIntentPaymentMethod when coroutine is cancelled`() = runScenario(
+        isConnected = true,
+        callbackResult = Result.success(
+            CreateCardPresentSetupIntentCallback {
+                CreateIntentResult.Success("si_123_secret")
+            }
+        ),
+    ) {
+        val job = testScope.backgroundScope.async {
+            handler.collect(DEFAULT_METADATA)
+        }
+
+        assertThat(retrieverScenario.waitForCallbackCalls.awaitItem()).isNotNull()
+
+        val retrievedSetupIntent = checkRetrieveSetupIntent("si_123_secret")
+
+        val collectPaymentMethodCall = terminalScenario.collectPaymentMethodCalls.awaitItem()
+        assertThat(collectPaymentMethodCall.intent).isEqualTo(retrievedSetupIntent)
+
+        job.cancel()
+
+        verify(collectPaymentMethodCall.cancelable, timeout(1000)).cancel(any())
+    }
+
+    @Test
+    fun `handler cancels confirmSetupIntent when coroutine is cancelled`() = runScenario(
+        isConnected = true,
+        callbackResult = Result.success(
+            CreateCardPresentSetupIntentCallback {
+                CreateIntentResult.Success("si_123_secret")
+            }
+        ),
+    ) {
+        val job = testScope.backgroundScope.async {
+            handler.collect(DEFAULT_METADATA)
+        }
+
+        assertThat(retrieverScenario.waitForCallbackCalls.awaitItem()).isNotNull()
+
+        val retrievedSetupIntent = checkRetrieveSetupIntent("si_123_secret")
+        val collectedIntent = checkCollectCall(retrievedSetupIntent)
+
+        val confirmSetupIntentCall = terminalScenario.confirmSetupIntentCalls.awaitItem()
+        assertThat(confirmSetupIntentCall.intent).isEqualTo(collectedIntent)
+
+        job.cancel()
+
+        verify(confirmSetupIntentCall.cancelable, timeout(1000)).cancel(any())
     }
 
     private fun runScenario(
         isConnected: Boolean = true,
         awaitResult: Result<Boolean> = Result.success(true),
         callbackResult: Result<CreateCardPresentSetupIntentCallback> = Result.success(DEFAULT_CALLBACK),
+        coroutineContext: CoroutineContext = EmptyCoroutineContext,
         block: suspend Scenario.() -> Unit,
-    ) = runTest {
+    ) = runTest(coroutineContext) {
+        val terminalScenario = createTerminalScenario()
+        val terminalWrapper = TestTerminalWrapper.noOp(terminalScenario.terminalInstance)
+
         FakeTapToAddConnectionManager.test(
             isSupported = true,
             isConnected = isConnected,
@@ -176,22 +333,199 @@ class TapToAddCollectionHandlerTest {
                 block(
                     Scenario(
                         handler = DefaultTapToAddCollectionHandler(
+                            terminalWrapper = terminalWrapper,
                             connectionManager = managerScenario.tapToAddConnectionManager,
                             createCardPresentSetupIntentCallbackRetriever = retrieverScenario.retriever,
                         ),
                         managerScenario = managerScenario,
                         retrieverScenario = retrieverScenario,
+                        terminalScenario = terminalScenario,
+                        testScope = this@runTest,
                     )
                 )
             }
         }
+
+        terminalScenario.confirmSetupIntentCalls.ensureAllEventsConsumed()
+        terminalScenario.retrieveSetupIntentCalls.ensureAllEventsConsumed()
+        terminalScenario.collectPaymentMethodCalls.ensureAllEventsConsumed()
+    }
+
+    private fun createTerminalScenario(): TerminalScenario {
+        val retrieveSetupIntentCalls = Turbine<TerminalScenario.RetrieveSetupIntentCall>()
+        val collectPaymentMethodCalls = Turbine<TerminalScenario.CollectPaymentMethodCall>()
+        val confirmSetupIntentCalls = Turbine<TerminalScenario.ConfirmSetupIntentCall>()
+
+        val terminalInstance: Terminal = mock {
+            mockRetrieveSetupIntent(retrieveSetupIntentCalls)
+            mockCollectPaymentMethod(collectPaymentMethodCalls)
+            mockConfirmSetupIntent(confirmSetupIntentCalls)
+        }
+
+        return TerminalScenario(
+            terminalInstance = terminalInstance,
+            retrieveSetupIntentCalls = retrieveSetupIntentCalls,
+            collectPaymentMethodCalls = collectPaymentMethodCalls,
+            confirmSetupIntentCalls = confirmSetupIntentCalls,
+        )
+    }
+
+    private suspend fun Scenario.checkRetrieveSetupIntent(clientSecret: String): SetupIntent {
+        val retrievedSetupIntent = mock<SetupIntent>()
+        val retrieveSetupIntentCall = terminalScenario.retrieveSetupIntentCalls.awaitItem()
+        assertThat(retrieveSetupIntentCall.clientSecret).isEqualTo(clientSecret)
+        retrieveSetupIntentCall.callback.onSuccess(retrievedSetupIntent)
+        return retrievedSetupIntent
+    }
+
+    private suspend fun Scenario.checkCollectCall(
+        retrievedSetupIntent: SetupIntent,
+        expectedAllowRedisplay: AllowRedisplay = AllowRedisplay.UNSPECIFIED,
+    ): SetupIntent {
+        val collectedIntent = mock<SetupIntent>()
+        val collectPaymentMethodCall = terminalScenario.collectPaymentMethodCalls.awaitItem()
+        assertThat(collectPaymentMethodCall.intent).isEqualTo(retrievedSetupIntent)
+        assertThat(collectPaymentMethodCall.allowRedisplay).isEqualTo(expectedAllowRedisplay)
+        assertThat(collectPaymentMethodCall.config)
+            .isEqualTo(
+                SetupIntentConfiguration.Builder()
+                    .build()
+            )
+        collectPaymentMethodCall.callback.onSuccess(collectedIntent)
+        return collectedIntent
+    }
+
+    private suspend fun Scenario.checkConfirmCall(collectedSetupIntent: SetupIntent) {
+        val confirmSetupIntentCall = terminalScenario.confirmSetupIntentCalls.awaitItem()
+        assertThat(confirmSetupIntentCall.intent).isEqualTo(collectedSetupIntent)
+        confirmSetupIntentCall.callback.onSuccess(mock())
+    }
+
+    private fun KStubbing<Terminal>.mockRetrieveSetupIntent(
+        retrieveSetupIntentCalls: Turbine<TerminalScenario.RetrieveSetupIntentCall>
+    ) {
+        on {
+            retrieveSetupIntent(
+                clientSecret = any<String>(),
+                callback = any<SetupIntentCallback>(),
+            )
+        } doAnswer { invocation ->
+            val setupIntentClientSecret = invocation.arguments[0] as? String
+                ?: fail("Invalid argument: Not a setup intent!")
+
+            val callback = invocation.arguments[1] as? SetupIntentCallback
+                ?: fail("Invalid argument: Not a setup intent callback!")
+
+            retrieveSetupIntentCalls.add(
+                TerminalScenario.RetrieveSetupIntentCall(
+                    clientSecret = setupIntentClientSecret,
+                    callback = callback,
+                )
+            )
+        }
+    }
+
+    private fun KStubbing<Terminal>.mockCollectPaymentMethod(
+        collectPaymentMethodCalls: Turbine<TerminalScenario.CollectPaymentMethodCall>
+    ) {
+        on {
+            collectSetupIntentPaymentMethod(
+                intent = any<SetupIntent>(),
+                allowRedisplay = any<AllowRedisplay>(),
+                config = any<SetupIntentConfiguration>(),
+                callback = any<SetupIntentCallback>()
+            )
+        } doAnswer { invocation ->
+            val setupIntent = invocation.arguments[0] as? SetupIntent
+                ?: fail("Invalid argument: Not a setup intent!")
+
+            val allowRedisplay = invocation.arguments[1] as? AllowRedisplay
+                ?: fail("Invalid argument: Not an allow redisplay value!")
+
+            val config = invocation.arguments[2] as? SetupIntentConfiguration
+                ?: fail("Invalid argument: Not a setup intent config!")
+
+            val callback = invocation.arguments[3] as? SetupIntentCallback
+                ?: fail("Invalid argument: Not a setup intent callback!")
+
+            val cancelable = mock<Cancelable>()
+
+            collectPaymentMethodCalls.add(
+                TerminalScenario.CollectPaymentMethodCall(
+                    intent = setupIntent,
+                    allowRedisplay = allowRedisplay,
+                    config = config,
+                    callback = callback,
+                    cancelable = cancelable,
+                )
+            )
+
+            cancelable
+        }
+    }
+
+    private fun KStubbing<Terminal>.mockConfirmSetupIntent(
+        confirmSetupIntentCalls: Turbine<TerminalScenario.ConfirmSetupIntentCall>
+    ) {
+        on {
+            confirmSetupIntent(
+                intent = any<SetupIntent>(),
+                callback = any<SetupIntentCallback>()
+            )
+        } doAnswer { invocation ->
+            val setupIntent = invocation.arguments[0] as? SetupIntent
+                ?: fail("Invalid argument: Not a setup intent!")
+
+            val callback = invocation.arguments[1] as? SetupIntentCallback
+                ?: fail("Invalid argument: Not a setup intent callback!")
+
+            val cancelable = mock<Cancelable>()
+
+            confirmSetupIntentCalls.add(
+                TerminalScenario.ConfirmSetupIntentCall(
+                    intent = setupIntent,
+                    callback = callback,
+                    cancelable = cancelable,
+                )
+            )
+
+            cancelable
+        }
     }
 
     private class Scenario(
+        val testScope: TestScope,
         val handler: TapToAddCollectionHandler,
         val managerScenario: FakeTapToAddConnectionManager.Scenario,
         val retrieverScenario: FakeCreateCardPresentSetupIntentCallbackRetriever.Scenario,
+        val terminalScenario: TerminalScenario,
     )
+
+    private class TerminalScenario(
+        val terminalInstance: Terminal,
+        val retrieveSetupIntentCalls: ReceiveTurbine<RetrieveSetupIntentCall>,
+        val collectPaymentMethodCalls: ReceiveTurbine<CollectPaymentMethodCall>,
+        val confirmSetupIntentCalls: ReceiveTurbine<ConfirmSetupIntentCall>,
+    ) {
+        class RetrieveSetupIntentCall(
+            val clientSecret: String,
+            val callback: SetupIntentCallback,
+        )
+
+        class CollectPaymentMethodCall(
+            val intent: SetupIntent,
+            val allowRedisplay: AllowRedisplay,
+            val config: SetupIntentConfiguration,
+            val callback: SetupIntentCallback,
+            val cancelable: Cancelable,
+        )
+
+        class ConfirmSetupIntentCall(
+            val intent: SetupIntent,
+            val callback: SetupIntentCallback,
+            val cancelable: Cancelable,
+        )
+    }
 
     private class FakeCreateCardPresentSetupIntentCallbackRetriever private constructor(
         private val callbackResult: Result<CreateCardPresentSetupIntentCallback>,
