e1c72b4bae39a137d4c4ec2254b04ef7f62b095f
System Administrator <amk@stripe.com>
2026-01-13 14:08:37 -0800

    Add network tests

---
+++ paymentsheet/src/androidTest/java/com/stripe/android/paymentsheet/HorizontalModeExperimentsTest.kt
@@ -0,0 +1,112 @@
+package com.stripe.android.paymentsheet
+
+import androidx.test.espresso.intent.rule.IntentsRule
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.stripe.android.core.networking.AnalyticsRequestV2
+import com.stripe.android.core.networking.ApiRequest
+import com.stripe.android.core.utils.urlEncode
+import com.stripe.android.networktesting.NetworkRule
+import com.stripe.android.networktesting.RequestMatchers.bodyPart
+import com.stripe.android.networktesting.RequestMatchers.host
+import com.stripe.android.networktesting.RequestMatchers.method
+import com.stripe.android.networktesting.RequestMatchers.path
+import com.stripe.android.networktesting.testBodyFromFile
+import com.stripe.android.paymentsheet.utils.ProductIntegrationType
+import com.stripe.android.paymentsheet.utils.ProductIntegrationTypeProvider
+import com.stripe.android.paymentsheet.utils.TestRules
+import com.stripe.android.paymentsheet.utils.assertCompleted
+import com.stripe.android.paymentsheet.utils.runProductIntegrationTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+internal class HorizontalModeExperimentsTest {
+    private val networkRule = NetworkRule(
+        hostsToTrack = listOf(
+            ApiRequest.API_HOST,
+            AnalyticsRequestV2.ANALYTICS_HOST
+        ),
+    )
+
+    @get:Rule
+    val testRules: TestRules = TestRules.create(
+        networkRule = networkRule,
+    ) {
+        around(IntentsRule())
+    }
+
+    @TestParameter(valuesProvider = ProductIntegrationTypeProvider::class)
+    lateinit var integrationType: ProductIntegrationType
+
+    private val composeTestRule = testRules.compose
+    private val page: PaymentSheetPage = PaymentSheetPage(composeTestRule)
+
+    @Test
+    fun testHorizontalModeExperiment() = runProductIntegrationTest(
+        networkRule = networkRule,
+        integrationType = integrationType,
+        resultCallback = ::assertCompleted,
+    ) { testContext ->
+        networkRule.enqueue(
+            host("api.stripe.com"),
+            method("GET"),
+            path("/v1/elements/sessions"),
+        ) { response ->
+            response.testBodyFromFile("elements-sessions-requires_payment_method_with_horizontal_mode_experiment.json")
+        }
+
+        val expectedIntegrationType = when (integrationType) {
+            ProductIntegrationType.FlowController -> "custom"
+            ProductIntegrationType.PaymentSheet -> "complete"
+        }
+
+        networkRule.enqueue(
+            host("r.stripe.com"),
+            method("POST"),
+            bodyPart("event_name", "elements.experiment_exposure"),
+            bodyPart("experiment_retrieved", "ocs_mobile_horizontal_mode_aa"),
+            bodyPart("dimensions-in_app_elements_integration_type", expectedIntegrationType),
+            bodyPart("dimensions-has_saved_payment_method", "false"),
+            bodyPart("dimensions-displayed_payment_method_types", urlEncode("card,afterpay_clearpay,klarna")),
+            bodyPart("dimensions-displayed_payment_method_types_including_wallets", urlEncode("card,afterpay_clearpay,klarna,link")),
+        ) { }
+
+        enqueueLogLinkHoldbackExperiments()
+
+        testContext.launch(
+            configuration = PaymentSheet.Configuration(
+                merchantDisplayName = "Example, Inc.",
+                paymentMethodLayout = PaymentSheet.PaymentMethodLayout.Automatic,
+            ),
+        )
+
+        page.assertIsInVerticalMode()
+
+        testContext.markTestSucceeded()
+    }
+
+    private fun enqueueLogLinkHoldbackExperiments() {
+        networkRule.enqueue(
+            host("r.stripe.com"),
+            method("POST"),
+            bodyPart("event_name", "elements.experiment_exposure"),
+            bodyPart("experiment_retrieved", "link_global_holdback")
+        ) { }
+
+        networkRule.enqueue(
+            host("r.stripe.com"),
+            method("POST"),
+            bodyPart("event_name", "elements.experiment_exposure"),
+            bodyPart("experiment_retrieved", "link_global_holdback_aa")
+        ) { }
+
+        networkRule.enqueue(
+            host("r.stripe.com"),
+            method("POST"),
+            bodyPart("event_name", "elements.experiment_exposure"),
+            bodyPart("experiment_retrieved", "link_ab_test")
+        ) { }
+    }
+}
