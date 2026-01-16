022a20af3891ff72b362db8d92a01366293e39ca
Igor Escodro <escodro@outlook.com>
2026-01-16 14:06:45 -0500

    Implement `PaymentMethodCreateParams.createTwint()` function
    Adding `createTwint()` function inside `PaymentMethodCreateParams`,
    allowing the payment provider to be confirmed by the Android app using
    `PaymentLauncher.confirm()`. Test and example classes were created to
    cover this new implementation.

--- payments-core/src/main/java/com/stripe/android/model/PaymentMethodCreateParams.kt
+++ payments-core/src/main/java/com/stripe/android/model/PaymentMethodCreateParams.kt
@@ -1398,6 +1398,20 @@ constructor(
                 allowRedisplay = allowRedisplay,
             )
         }
+        @JvmStatic
+        @JvmOverloads
+        fun createTwint(
+            billingDetails: PaymentMethod.BillingDetails? = null,
+            metadata: Map<String, String>? = null,
+            allowRedisplay: PaymentMethod.AllowRedisplay? = null,
+        ): PaymentMethodCreateParams {
+            return PaymentMethodCreateParams(
+                type = PaymentMethod.Type.Twint,
+                billingDetails = billingDetails,
+                metadata = metadata,
+                allowRedisplay = allowRedisplay,
+            )
+        }
 
         @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
         fun createLink(
