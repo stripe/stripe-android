6b525c5a8b182d8942ae6e19249885d803cd69ee
Twig <Twigz@users.noreply.github.com>
2026-01-20 12:17:10 -0500

    Adds some missing documentation to various onramp types

--- crypto-onramp/src/main/java/com/stripe/android/crypto/onramp/model/KycInfo.kt
+++ crypto-onramp/src/main/java/com/stripe/android/crypto/onramp/model/KycInfo.kt
@@ -6,6 +6,15 @@ import com.stripe.android.model.DateOfBirth
 import com.stripe.android.paymentsheet.PaymentSheet
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Represents the full list of KYC information to be collected.
+ *
+ * @property firstName The user’s given name as it appears on official documents.
+ * @property lastName The user’s family name as it appears on official documents.
+ * @property idNumber The full  identification number.
+ * @property dateOfBirth The user’s date of birth.
+ * @property address The user’s billing address.
+ */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 data class KycInfo(
     val firstName: String,
@@ -15,6 +24,18 @@ data class KycInfo(
     val address: PaymentSheet.Address
 )
 
+/**
+ * Represents a set of KYC information used when refreshing or revalidating
+ * an existing user’s identity.
+ *
+ * @property firstName The user’s given name.
+ * @property lastName The user’s family name.
+ * @property idNumberLastFour The last four digits of the user’s identification number.
+ * @property idType The type of government-issued identification.
+ * Currently only social_security_number is accepted.
+ * @property dateOfBirth The user’s date of birth.
+ * @property address The user’s billing address.
+ */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 @Parcelize
 data class RefreshKycInfo(
