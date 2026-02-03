eff6273acaedecdfe749cee1e6e6fa89531cb491
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2026-02-03 15:46:43 -0800

    Move `FeatureFlags.enableTapToAdd` to default `TapToAddConnectionManager` (#12301)

--- paymentsheet/src/main/java/com/stripe/android/common/taptoadd/IsStripeTerminalSdkAvailable.kt
+++ paymentsheet/src/main/java/com/stripe/android/common/taptoadd/IsStripeTerminalSdkAvailable.kt
@@ -1,6 +1,5 @@
 package com.stripe.android.common.taptoadd
 
-import com.stripe.android.core.utils.FeatureFlags
 import javax.inject.Inject
 
 internal fun interface IsStripeTerminalSdkAvailable {
@@ -9,10 +8,6 @@ internal fun interface IsStripeTerminalSdkAvailable {
 
 internal class DefaultIsStripeTerminalSdkAvailable @Inject constructor() : IsStripeTerminalSdkAvailable {
     override fun invoke(): Boolean {
-        if (!FeatureFlags.enableTapToAdd.isEnabled) {
-            return false
-        }
-
         return try {
             // If found, 'core' library was imported
             Class.forName("com.stripe.stripeterminal.TerminalApplicationDelegate")
