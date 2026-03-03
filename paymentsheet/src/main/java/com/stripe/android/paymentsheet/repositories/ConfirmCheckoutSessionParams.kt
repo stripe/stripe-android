ac42178dd901b41bbc4258ec8f1bcf69faa67ccc
Jay Newstrom <jaynewstrom@stripe.com>
2026-03-03 09:43:11 -0700

    Make checkout types internal. (#12510)

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/repositories/ConfirmCheckoutSessionParams.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/repositories/ConfirmCheckoutSessionParams.kt
@@ -1,14 +1,12 @@
 package com.stripe.android.paymentsheet.repositories
 
-import androidx.annotation.RestrictTo
 import com.stripe.android.model.ClientAttributionMetadata
 
 /**
  * Parameters for confirming a checkout session via the confirm API
  * (`/v1/payment_pages/{cs_id}/confirm`).
  */
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-data class ConfirmCheckoutSessionParams(
+internal data class ConfirmCheckoutSessionParams(
     private val paymentMethodId: String,
     private val clientAttributionMetadata: ClientAttributionMetadata,
     private val returnUrl: String,
