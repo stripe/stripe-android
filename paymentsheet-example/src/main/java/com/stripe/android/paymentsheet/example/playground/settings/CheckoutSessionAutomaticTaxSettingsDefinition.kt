a5ef6c66cf5afb933fc431165527382bf5c8e465
Jay Newstrom <jaynewstrom@stripe.com>
2026-03-06 10:23:52 -0700

    Add automatic tax option to CheckoutRequest and Playground settings

---
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/CheckoutSessionAutomaticTaxSettingsDefinition.kt
@@ -0,0 +1,20 @@
+package com.stripe.android.paymentsheet.example.playground.settings
+
+import com.stripe.android.paymentsheet.example.playground.model.CheckoutRequest
+
+internal object CheckoutSessionAutomaticTaxSettingsDefinition : BooleanSettingsDefinition(
+    defaultValue = true,
+    displayName = "Automatic Tax",
+    key = "checkout_session_automatic_tax"
+) {
+    override fun applicable(
+        configurationData: PlaygroundConfigurationData,
+        settings: Map<PlaygroundSettingDefinition<*>, Any?>,
+    ): Boolean {
+        return settings[InitializationTypeSettingsDefinition] == InitializationType.CheckoutSession
+    }
+
+    override fun configure(value: Boolean, checkoutRequestBuilder: CheckoutRequest.Builder) {
+        checkoutRequestBuilder.automaticTax(value)
+    }
+}
