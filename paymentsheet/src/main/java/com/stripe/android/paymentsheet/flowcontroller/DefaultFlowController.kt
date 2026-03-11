2bb0b5d26b19d76421d2d80e4b922e6126c4627b
Tyler Clawson <tjclawson@stripe.com>
2026-03-11 14:17:26 -0400

    Fix FlowController configuration handler

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/flowcontroller/DefaultFlowController.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/flowcontroller/DefaultFlowController.kt
@@ -679,6 +679,7 @@ internal class DefaultFlowController @Inject internal constructor(
         if (paymentResult is PaymentResult.Completed && shouldResetOnCompleted) {
             viewModel.paymentSelection = null
             viewModel.state = null
+            viewModel.previousConfigureRequest = null
         }
 
         viewModelScope.launch {
