37e6535bfbb26f1da3d316198759cd772d203feb
David Estes <davidestes@stripe.com>
2026-03-13 15:42:23 -0700

    Switch to google play tflite

--- ml-core/cardscan/src/main/java/com/stripe/android/mlcore/impl/InterpreterInitializerImpl.kt
+++ ml-core/cardscan/src/main/java/com/stripe/android/mlcore/impl/InterpreterInitializerImpl.kt
@@ -2,6 +2,8 @@ package com.stripe.android.mlcore.impl
 
 import android.content.Context
 import androidx.annotation.RestrictTo
+import com.google.android.gms.tasks.Tasks
+import com.google.android.gms.tflite.java.TfLite
 import com.stripe.android.mlcore.base.InterpreterInitializer
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
@@ -11,6 +13,11 @@ object InterpreterInitializerImpl : InterpreterInitializer {
         onSuccess: () -> Unit,
         onFailure: (Exception) -> Unit
     ) {
-        onSuccess()
+        TfLite.initialize(context).also {
+            it.addOnSuccessListener { onSuccess() }
+            it.addOnFailureListener(onFailure)
+        }.let { initializeTask ->
+            Tasks.await(initializeTask)
+        }
     }
 }
