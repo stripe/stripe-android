332ab143daadecb6d3e7f9cb9574dd03b2f02141
Jay Newstrom <jaynewstrom@stripe.com>
2026-03-19 12:02:38 -0600

    Add adaptive pricing option to checkout session settings. (#12720)

---
+++ paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/settings/CheckoutSessionAdaptivePricingSettingsDefinition.kt
@@ -0,0 +1,20 @@
+package com.stripe.android.paymentsheet.example.playground.settings
+
+import com.stripe.android.paymentsheet.example.playground.model.CheckoutRequest
+
+internal object CheckoutSessionAdaptivePricingSettingsDefinition : BooleanSettingsDefinition(
+    defaultValue = false,
+    displayName = "Adaptive Pricing",
+    key = "checkout_session_adaptive_pricing"
+) {
+    override fun applicable(
+        configurationData: PlaygroundConfigurationData,
+        settings: Map<PlaygroundSettingDefinition<*>, Any?>,
+    ): Boolean {
+        return settings[InitializationTypeSettingsDefinition] == InitializationType.CheckoutSession
+    }
+
+    override fun configure(value: Boolean, checkoutRequestBuilder: CheckoutRequest.Builder) {
+        checkoutRequestBuilder.adaptivePricing(value)
+    }
+}
