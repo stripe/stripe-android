70e1ca6223bb7e838485a0d3cf470b78a2cb8e15
Tyler Clawson <tjclawson@stripe.com>
2026-04-10 13:36:26 -0400

    Update

--- paymentsheet/src/test/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/AfterpayClearpayDefinitionTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/AfterpayClearpayDefinitionTest.kt
@@ -1,20 +1,55 @@
 package com.stripe.android.lpmfoundations.paymentmethod.definitions
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.isInstanceOf
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadataFactory
 import com.stripe.android.lpmfoundations.paymentmethod.formElements
+import com.stripe.android.model.PaymentIntentFixtures
 import com.stripe.android.model.PaymentMethodMessageLearnMore
 import com.stripe.android.model.PaymentMethodMessagePromotion
 import com.stripe.android.testing.PaymentIntentFactory
+import com.stripe.android.ui.core.R
 import com.stripe.android.ui.core.elements.PaymentMethodMessageHeaderElement
 import com.stripe.android.utils.FakePaymentMethodMessagePromotionsHelper
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import com.stripe.android.paymentsheet.R as PaymentSheetR
 
 @RunWith(RobolectricTestRunner::class)
 class AfterpayClearpayDefinitionTest {
+
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+    private val res = context.resources
+
+    @Test
+    fun `Test label for afterpay show correctly when clearpay string`() {
+        testDisplayNameReflectsLocale(
+            currency = "GBP",
+            titleResourceId = R.string.stripe_paymentsheet_payment_method_clearpay,
+            subtitleResourceId = PaymentSheetR.string.stripe_clearpay_subtitle,
+        )
+    }
+
+    @Test
+    fun `Test label for afterpay show correctly when afterpay string`() {
+        testDisplayNameReflectsLocale(
+            currency = "EUR",
+            titleResourceId = R.string.stripe_paymentsheet_payment_method_afterpay,
+            subtitleResourceId = PaymentSheetR.string.stripe_afterpay_subtitle,
+        )
+    }
+
+    @Test
+    fun `Test label for afterpay show correctly when cashapp afterpay string`() {
+        testDisplayNameReflectsLocale(
+            currency = "USD",
+            titleResourceId = R.string.stripe_paymentsheet_payment_method_afterpay,
+            subtitleResourceId = PaymentSheetR.string.stripe_cashapp_afterpay_subtitle,
+        )
+    }
 
     @Test
     fun `createFormElements includes promotion if available`() {
@@ -38,7 +73,7 @@ class AfterpayClearpayDefinitionTest {
             )
         )
 
-        assertThat(formElements).hasSize(3)
+        assertThat(formElements).hasSize(2)
 
         val element = formElements[0]
         assertThat(element.identifier.v1).isEqualTo("afterpay_promotion")
@@ -52,5 +87,21 @@ class AfterpayClearpayDefinitionTest {
                 message = "Click me."
             )
         )
+    }
+
+    private fun testDisplayNameReflectsLocale(currency: String, titleResourceId: Int, subtitleResourceId: Int) {
+        val paymentMethodMetadata = PaymentMethodMetadataFactory.create(
+            stripeIntent = PaymentIntentFixtures.PI_WITH_SHIPPING.copy(
+                paymentMethodTypes = listOf("card", "afterpay_clearpay"),
+                currency = currency,
+            )
+        )
+        val supportedPaymentMethod = paymentMethodMetadata.supportedPaymentMethodForCode(
+            code = "afterpay_clearpay",
+        )!!
+        assertThat(supportedPaymentMethod.displayName.resolve(context))
+            .isEqualTo(res.getString(titleResourceId))
+        assertThat(supportedPaymentMethod.subtitle?.resolve(context))
+            .isEqualTo(res.getString(subtitleResourceId))
     }
 }
