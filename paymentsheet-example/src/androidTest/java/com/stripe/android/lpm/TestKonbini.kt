976359dfbd6fecc383b18e33143ca801025dc22c
toluo-stripe <toluo@stripe.com>
2026-04-11 02:08:22 -0400

    enable konbini tests (#12878)

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestKonbini.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestKonbini.kt
@@ -2,8 +2,6 @@ package com.stripe.android.lpm
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BasePlaygroundTest
-import com.stripe.android.paymentsheet.example.playground.settings.Merchant
-import com.stripe.android.paymentsheet.example.playground.settings.MerchantSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.Currency
 import com.stripe.android.paymentsheet.example.playground.settings.CurrencySettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.CustomerSettingsDefinition
@@ -11,13 +9,13 @@ import com.stripe.android.paymentsheet.example.playground.settings.CustomerType
 import com.stripe.android.paymentsheet.example.playground.settings.DefaultBillingAddress
 import com.stripe.android.paymentsheet.example.playground.settings.DefaultBillingAddressSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.DelayedPaymentMethodsSettingsDefinition
+import com.stripe.android.paymentsheet.example.playground.settings.Merchant
+import com.stripe.android.paymentsheet.example.playground.settings.MerchantSettingsDefinition
 import com.stripe.android.test.core.AuthorizeAction
 import com.stripe.android.test.core.TestParameters
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@Ignore("sienna-jurisdiction")
 @RunWith(AndroidJUnit4::class)
 internal class TestKonbini : BasePlaygroundTest() {
     private val testParameters = TestParameters.create(
