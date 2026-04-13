daee56b3fa8e44120982e205cba1bad9a11fb50b
toluo-stripe <toluo@stripe.com>
2026-04-13 14:06:41 -0400

    Wire up card art prefetch in CustomerSheet confirmation module (#12887)

--- paymentsheet/src/main/java/com/stripe/android/customersheet/injection/CustomerSheetViewModelComponent.kt
+++ paymentsheet/src/main/java/com/stripe/android/customersheet/injection/CustomerSheetViewModelComponent.kt
@@ -11,6 +11,7 @@ import com.stripe.android.networking.PaymentElementRequestSurfaceModule
 import com.stripe.android.paymentelement.confirmation.injection.CustomerSheetConfirmationModule
 import com.stripe.android.payments.core.injection.STATUS_BAR_COLOR
 import com.stripe.android.payments.core.injection.StripeRepositoryModule
+import com.stripe.android.paymentsheet.PaymentOptionCardArtModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Named
@@ -24,6 +25,7 @@ import javax.inject.Named
         StripeRepositoryModule::class,
         PaymentElementRequestSurfaceModule::class,
         GooglePayLauncherModule::class,
+        PaymentOptionCardArtModule::class,
     ],
 )
 internal interface CustomerSheetViewModelComponent {
