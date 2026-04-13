a31af377a9e68f663f4f2c2e0fea87bc05e9f222
Bella Koch <160939932+amk-stripe@users.noreply.github.com>
2026-04-13 06:27:14 -0700

    Revert "Revert "Add Pay by Bank as a supported LPM (#12769)" (#12776)"
    This reverts commit 144c0e531bd54892978fc6c100147a71a16f4306.

--- paymentsheet-example/src/androidTest/java/com/stripe/android/test/core/PlaygroundTestDriver.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/test/core/PlaygroundTestDriver.kt
@@ -31,8 +31,10 @@ import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.web.sugar.Web.onWebView
 import androidx.test.espresso.web.webdriver.DriverAtoms.webClick
+import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiSelector
+import androidx.test.uiautomator.Until
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.customersheet.ui.CUSTOMER_SHEET_CONFIRM_BUTTON_TEST_TAG
 import com.stripe.android.customersheet.ui.CUSTOMER_SHEET_SAVE_BUTTON_TEST_TAG
@@ -1503,6 +1505,20 @@ internal class PlaygroundTestDriver(
                         authorizeTestPaymentText.click()
 
                         waitForPollingToFinish()
+                    }
+                    is AuthorizeAction.PayByBank -> {
+                        val webView = device.wait(
+                            Until.findObject(By.clazz("android.webkit.WebView")),
+                            DEFAULT_UI_TIMEOUT.inWholeMilliseconds
+                        )
+                        webView.click()
+
+                        val authorizeTestPaymentText = UiAutomatorText(
+                            "AUTHORIZE TEST PAYMENT",
+                            device = device
+                        )
+                        authorizeTestPaymentText.wait(DEFAULT_UI_TIMEOUT.inWholeMilliseconds)
+                        authorizeTestPaymentText.click()
                     }
                     null -> {}
                 }
