022d6a662f3247f6370a77cd9679aa7fb4c3b958
Samer Alabi <samer@stripe.com>
2026-04-14 01:24:30 -0400

    Limit Tap to Add to 5.4.1

--- paymentsheet/src/main/java/com/stripe/android/common/taptoadd/IsStripeTerminalSdkAvailable.kt
+++ paymentsheet/src/main/java/com/stripe/android/common/taptoadd/IsStripeTerminalSdkAvailable.kt
@@ -6,7 +6,13 @@ internal fun interface IsStripeTerminalSdkAvailable {
     operator fun invoke(): Boolean
 }
 
-internal class DefaultIsStripeTerminalSdkAvailable @Inject constructor() : IsStripeTerminalSdkAvailable {
+internal fun interface StripeTerminalVersionValidator {
+    operator fun invoke(versionName: String): Boolean
+}
+
+internal class DefaultIsStripeTerminalSdkAvailable @Inject constructor(
+    private val versionValidator: StripeTerminalVersionValidator,
+) : IsStripeTerminalSdkAvailable {
     override fun invoke(): Boolean {
         return try {
             // If found, 'core' library was imported
@@ -14,22 +20,35 @@ internal class DefaultIsStripeTerminalSdkAvailable @Inject constructor() : IsStr
             // If found, 'taptopay' library was imported
             Class.forName("com.stripe.stripeterminal.taptopay.TapToPay")
 
-            isValidVersion()
+            versionValidator(versionName = com.stripe.stripeterminal.BuildConfig.SDK_VERSION_NAME)
         } catch (_: Exception) {
             false
         }
     }
+}
 
-    private fun isValidVersion(): Boolean {
-        val versionNumbers = com.stripe.stripeterminal.BuildConfig.SDK_VERSION_NAME.split(".")
+internal class DefaultStripeTerminalVersionValidator @Inject constructor() : StripeTerminalVersionValidator {
+    override operator fun invoke(versionName: String): Boolean {
+        val versionNumbers = versionName.split(".")
 
-        return if (versionNumbers.size == VERSION_SIZE) {
+        if (versionNumbers.size == VERSION_SIZE) {
             val majorVersion = versionNumbers[0].toIntOrNull() ?: 0
+
+            if (majorVersion < MIN_MAJOR_VERSION) {
+                return false
+            }
+
             val minorVersion = versionNumbers[1].toIntOrNull() ?: 0
 
-            majorVersion >= MIN_MAJOR_VERSION && minorVersion >= MIN_MINOR_VERSION
+            if (minorVersion < MIN_MINOR_VERSION) {
+                return false
+            }
+
+            val patchVersion = versionNumbers[2].toIntOrNull() ?: 0
+
+            return patchVersion >= PATCH_VERSION || minorVersion >= MIN_MINOR_VERSION + 1
         } else {
-            false
+            return false
         }
     }
 
@@ -37,5 +56,6 @@ internal class DefaultIsStripeTerminalSdkAvailable @Inject constructor() : IsStr
         const val VERSION_SIZE = 3
         const val MIN_MAJOR_VERSION = 5
         const val MIN_MINOR_VERSION = 4
+        const val PATCH_VERSION = 1
     }
 }
