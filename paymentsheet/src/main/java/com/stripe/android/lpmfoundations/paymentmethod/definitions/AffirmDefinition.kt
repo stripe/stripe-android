6c655ed84dc67d384f186220a68aea822018c84c
System Administrator <amk@stripe.com>
2026-05-29 10:31:42 -0700

    fix lint and test

--- paymentsheet/src/main/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/AffirmDefinition.kt
+++ paymentsheet/src/main/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/AffirmDefinition.kt
@@ -17,8 +17,6 @@ import com.stripe.android.ui.core.R as UiCoreR
 internal object AffirmDefinition : PaymentMethodDefinition {
     override val type: PaymentMethod.Type = PaymentMethod.Type.Affirm
 
-    val supportedAsSavedPaymentMethod: Boolean = false
-
     override fun requirementsToBeUsedAsNewPaymentMethod(
         hasIntentToSetup: Boolean
     ): Set<AddPaymentMethodRequirement> = setOf(
