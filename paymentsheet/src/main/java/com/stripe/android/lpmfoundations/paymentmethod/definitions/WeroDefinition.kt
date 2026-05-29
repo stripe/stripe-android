1996f207961538d34dca4f7c6f768b06f0c212a8
System Administrator <amk@stripe.com>
2026-05-29 11:42:38 -0700

    Clean up

--- paymentsheet/src/main/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/WeroDefinition.kt
+++ paymentsheet/src/main/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/WeroDefinition.kt
@@ -18,8 +18,6 @@ import com.stripe.android.uicore.elements.SectionElement
 internal object WeroDefinition : PaymentMethodDefinition {
     override val type: PaymentMethod.Type = PaymentMethod.Type.Wero
 
-    val supportedAsSavedPaymentMethod: Boolean = false
-
     override fun requirementsToBeUsedAsNewPaymentMethod(
         hasIntentToSetup: Boolean
     ): Set<AddPaymentMethodRequirement> = setOf(
