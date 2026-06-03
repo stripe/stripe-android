9490aa0eb54257b2292741c3fb2b499b37a9ebe2
Tyler Clawson <tjclawson@stripe.com>
2026-06-03 17:49:12 -0400

    Add getPromotionProvider to PMM Promotions helper

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodVerticalLayoutInteractor.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/verticalmode/PaymentMethodVerticalLayoutInteractor.kt
@@ -4,14 +4,12 @@ import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.lifecycle.viewModelScope
 import com.stripe.android.core.strings.ResolvableString
 import com.stripe.android.core.strings.resolvableString
-import com.stripe.android.core.utils.FeatureFlags
 import com.stripe.android.link.ui.LinkButtonState
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
 import com.stripe.android.model.CardBrand
 import com.stripe.android.model.LinkBrand
 import com.stripe.android.model.PaymentMethod
 import com.stripe.android.model.PaymentMethodCode
-import com.stripe.android.model.PaymentMethodMessagePromotion
 import com.stripe.android.paymentsheet.CustomerStateHolder
 import com.stripe.android.paymentsheet.DefaultFormHelper
 import com.stripe.android.paymentsheet.DisplayableSavedPaymentMethod
@@ -380,7 +378,10 @@ internal class DefaultPaymentMethodVerticalLayoutInteractor(
             supportedPaymentMethod.asDisplayablePaymentMethod(
                 customerSavedPaymentMethods = paymentMethods,
                 incentive = paymentMethodIncentive,
-                promotionProvider = getPromotionProvider(supportedPaymentMethod.code),
+                promotionProvider = paymentMethodMessagePromotionsHelper?.getPromotionProvider(
+                    code = supportedPaymentMethod.code,
+                    metadata = paymentMethodMetadata
+                ),
                 shouldExpandOnClick = shouldExpandOnClick(supportedPaymentMethod.code)
             ) {
                 handleViewAction(ViewAction.PaymentMethodSelected(supportedPaymentMethod.code))
@@ -656,21 +657,6 @@ internal class DefaultPaymentMethodVerticalLayoutInteractor(
         }
         is PaymentSelection.New.USBankAccount -> label
         else -> null
-    }
-
-    private fun getPromotionProvider(code: PaymentMethodCode): (() -> PaymentMethodMessagePromotion?)? {
-        return if (FeatureFlags.paymentMethodMessagePromotions.isEnabled &&
-            PromotionSupportedPaymentMethods.supportedPaymentMethods.contains(code)
-        ) {
-            {
-                paymentMethodMessagePromotionsHelper?.getPromotionIfAvailableForCode(
-                    code,
-                    paymentMethodMetadata
-                )
-            }
-        } else {
-            null
-        }
     }
 
     private fun shouldTransitionToFormScreen(paymentMethodCode: PaymentMethodCode): Boolean {
