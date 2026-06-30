9ad936c8362ed33d53b9d8bb6308c0a807f20eea
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2026-06-30 10:46:50 -0700

    Implement `TapZoneResolver` (#13304)

--- paymentsheet/src/main/java/com/stripe/android/common/nfcscan/tapzone/TapZoneModule.kt
+++ paymentsheet/src/main/java/com/stripe/android/common/nfcscan/tapzone/TapZoneModule.kt
@@ -1,10 +1,26 @@
 package com.stripe.android.common.nfcscan.tapzone
 
+import android.os.Build
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 
 @Module
 internal interface TapZoneModule {
     @Binds
     fun bindsTapZoneResolver(resolver: DefaultTapZoneResolver): TapZoneResolver
+
+    companion object {
+        @DeviceManufacturer
+        @Provides
+        fun providesDeviceManufacturer(): String = Build.MANUFACTURER
+
+        @DeviceModel
+        @Provides
+        fun providesDeviceModel(): String = Build.MODEL
+
+        @SdkVersion
+        @Provides
+        fun providesSdkVersion(): Int = Build.VERSION.SDK_INT
+    }
 }
--- paymentsheet/src/test/java/com/stripe/android/common/nfcscan/hardware/NfcHardwareDelegateTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/common/nfcscan/hardware/NfcHardwareDelegateTest.kt
@@ -1,8 +1,11 @@
 package com.stripe.android.common.nfcscan.hardware
 
-import android.content.Context
+import android.app.Application
 import android.content.pm.PackageManager
+import android.nfc.AvailableNfcAntenna
 import android.nfc.NfcAdapter
+import android.nfc.NfcAntennaInfo
+import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.test.runTest
@@ -10,10 +13,11 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows.shadowOf
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 internal class NfcHardwareDelegateTest {
-    private val context: Context = ApplicationProvider.getApplicationContext()
+    private val application: Application = ApplicationProvider.getApplicationContext()
 
     @Test
     fun `isAvailable returns false when device has no NFC hardware`() = test(
@@ -38,23 +42,61 @@ internal class NfcHardwareDelegateTest {
         assertThat(delegate.isAvailable()).isTrue()
     }
 
+    @Config(sdk = [Build.VERSION_CODES.UPSIDE_DOWN_CAKE])
+    @Test
+    fun `antenna returns null when device has no NFC hardware`() = test(
+        hasNfcFeature = false,
+    ) {
+        assertThat(delegate.antenna()).isNull()
+    }
+
+    @Config(sdk = [Build.VERSION_CODES.UPSIDE_DOWN_CAKE])
+    @Test
+    fun `antenna returns null when NFC adapter has no antenna info`() = test(
+        hasNfcFeature = true,
+    ) {
+        assertThat(delegate.antenna()).isNull()
+    }
+
+    @Config(sdk = [Build.VERSION_CODES.UPSIDE_DOWN_CAKE])
+    @Test
+    fun `antenna returns antenna info from NFC adapter`() = test(
+        hasNfcFeature = true,
+        nfcAntennaInfo = NfcAntennaInfo(
+            1000,
+            2000,
+            false,
+            listOf(AvailableNfcAntenna(300, 400)),
+        ),
+    ) {
+        assertThat(delegate.antenna()).isEqualTo(nfcAntennaInfo)
+    }
+
     private fun test(
         hasNfcFeature: Boolean = true,
         isNfcEnabled: Boolean = true,
+        nfcAntennaInfo: NfcAntennaInfo? = null,
         block: suspend Scenario.() -> Unit,
     ) = runTest {
-        shadowOf(context.packageManager)
+        shadowOf(application.packageManager)
             .setSystemFeature(PackageManager.FEATURE_NFC, hasNfcFeature)
 
         if (hasNfcFeature) {
-            val nfcAdapter = NfcAdapter.getDefaultAdapter(context)
+            val nfcAdapter = NfcAdapter.getDefaultAdapter(application)
             shadowOf(nfcAdapter).setEnabled(isNfcEnabled)
+            nfcAntennaInfo?.let { shadowOf(nfcAdapter).setNfcAntennaInfo(it) }
         }
 
-        block(Scenario(DefaultNfcHardwareDelegate(context)))
+        block(
+            Scenario(
+                delegate = DefaultNfcHardwareDelegate(application),
+                nfcAntennaInfo = nfcAntennaInfo,
+            )
+        )
     }
 
     private class Scenario(
         val delegate: NfcHardwareDelegate,
+        val nfcAntennaInfo: NfcAntennaInfo?,
     )
 }
