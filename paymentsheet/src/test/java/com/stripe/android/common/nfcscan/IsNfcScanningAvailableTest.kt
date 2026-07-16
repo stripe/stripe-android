5c2a31ffa68616205b866e78ebea55cf36ef2ad9
Samer Alabi <samer@stripe.com>
2026-07-16 17:22:07 -0400

    Use remote NFC scanning feature flag

--- paymentsheet/src/test/java/com/stripe/android/common/nfcscan/IsNfcScanningAvailableTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/common/nfcscan/IsNfcScanningAvailableTest.kt
@@ -6,16 +6,15 @@ import com.stripe.android.common.nfcscan.security.FakeIsDeviceSecureForNfc
 import com.stripe.android.core.utils.FeatureFlags
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadataFixtures
 import com.stripe.android.model.ElementsSession
+import com.stripe.android.model.PaymentIntentFixtures
 import com.stripe.android.paymentsheet.state.FakeTapToAddAvailabilityFactory
 import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.kotlin.mock
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 internal class IsNfcScanningAvailableTest {
-    private val elementsSession = mock<ElementsSession>()
     private val customerMetadata = PaymentMethodMetadataFixtures.DEFAULT_CUSTOMER_METADATA
 
     @After
@@ -24,7 +23,7 @@ internal class IsNfcScanningAvailableTest {
     }
 
     @Test
-    fun `IsNfcScanningAvailableForPaymentElement returns false when NFC feature flag is disabled`() {
+    fun `IsNfcScanningAvailableForPaymentElement returns false when local NFC feature flag is disabled`() {
         FeatureFlags.enableNfcScanning.setEnabled(false)
 
         val isNfcScanningAvailable = DefaultIsNfcScanningAvailable(
@@ -33,7 +32,30 @@ internal class IsNfcScanningAvailableTest {
             nfcHardwareDelegate = FakeNfcHardwareDelegate(result = true),
         )
 
-        assertThat(isNfcScanningAvailable.get(elementsSession, customerMetadata)).isFalse()
+        assertThat(
+            isNfcScanningAvailable.get(
+                elementsSession = createElementsSession(isNfcScanningEnabled = true),
+                customerMetadata = customerMetadata,
+            )
+        ).isFalse()
+    }
+
+    @Test
+    fun `IsNfcScanningAvailableForPaymentElement returns false when remote feature flag is disabled`() {
+        FeatureFlags.enableNfcScanning.setEnabled(true)
+
+        val isNfcScanningAvailable = DefaultIsNfcScanningAvailable(
+            isDeviceSecureForNfc = FakeIsDeviceSecureForNfc(result = true),
+            tapToAddAvailabilityFactory = FakeTapToAddAvailabilityFactory(isAvailableResult = false),
+            nfcHardwareDelegate = FakeNfcHardwareDelegate(result = true),
+        )
+
+        assertThat(
+            isNfcScanningAvailable.get(
+                elementsSession = createElementsSession(isNfcScanningEnabled = false),
+                customerMetadata = customerMetadata,
+            )
+        ).isFalse()
     }
 
     @Test
@@ -46,7 +68,12 @@ internal class IsNfcScanningAvailableTest {
             nfcHardwareDelegate = FakeNfcHardwareDelegate(result = true),
         )
 
-        assertThat(isNfcScanningAvailable.get(elementsSession, customerMetadata)).isFalse()
+        assertThat(
+            isNfcScanningAvailable.get(
+                elementsSession = createElementsSession(isNfcScanningEnabled = true),
+                customerMetadata = customerMetadata,
+            )
+        ).isFalse()
     }
 
     @Test
@@ -59,7 +86,12 @@ internal class IsNfcScanningAvailableTest {
             nfcHardwareDelegate = FakeNfcHardwareDelegate(result = true),
         )
 
-        assertThat(isNfcScanningAvailable.get(elementsSession, customerMetadata)).isFalse()
+        assertThat(
+            isNfcScanningAvailable.get(
+                elementsSession = createElementsSession(isNfcScanningEnabled = true),
+                customerMetadata = customerMetadata,
+            )
+        ).isFalse()
     }
 
     @Test
@@ -72,7 +104,12 @@ internal class IsNfcScanningAvailableTest {
             nfcHardwareDelegate = FakeNfcHardwareDelegate(result = false),
         )
 
-        assertThat(isNfcScanningAvailable.get(elementsSession, customerMetadata)).isFalse()
+        assertThat(
+            isNfcScanningAvailable.get(
+                elementsSession = createElementsSession(isNfcScanningEnabled = true),
+                customerMetadata = customerMetadata,
+            )
+        ).isFalse()
     }
 
     @Test
@@ -85,6 +122,37 @@ internal class IsNfcScanningAvailableTest {
             nfcHardwareDelegate = FakeNfcHardwareDelegate(result = true),
         )
 
-        assertThat(isNfcScanningAvailable.get(elementsSession, customerMetadata)).isTrue()
+        assertThat(
+            isNfcScanningAvailable.get(
+                elementsSession = createElementsSession(isNfcScanningEnabled = true),
+                customerMetadata = customerMetadata,
+            )
+        ).isTrue()
+    }
+
+    private fun createElementsSession(isNfcScanningEnabled: Boolean): ElementsSession {
+        return ElementsSession(
+            linkSettings = null,
+            paymentMethodSpecs = null,
+            externalPaymentMethodData = null,
+            stripeIntent = PaymentIntentFixtures.PI_REQUIRES_PAYMENT_METHOD,
+            orderedPaymentMethodTypesAndWallets = emptyList(),
+            flags = mapOf(
+                ElementsSession.Flag.ELEMENTS_MOBILE_ANDROID_NFC_SCANNING_ENABLED to isNfcScanningEnabled,
+            ),
+            experimentsData = null,
+            customer = null,
+            merchantCountry = null,
+            merchantLogoUrl = null,
+            cardBrandChoice = null,
+            isGooglePayEnabled = false,
+            sessionsError = null,
+            customPaymentMethods = emptyList(),
+            elementsSessionId = "elements_session_test",
+            passiveCaptcha = null,
+            elementsSessionConfigId = null,
+            accountId = null,
+            merchantId = null,
+        )
     }
 }
