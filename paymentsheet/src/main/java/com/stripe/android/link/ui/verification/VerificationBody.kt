5119e78c328409adabd6740fe4ee8441dc93bb2f
Jeans Patel <jeans@stripe.com>
2026-07-17 16:09:55 -0400

    Lint fix

--- paymentsheet/src/main/java/com/stripe/android/link/ui/verification/VerificationBody.kt
+++ paymentsheet/src/main/java/com/stripe/android/link/ui/verification/VerificationBody.kt
@@ -1,7 +1,6 @@
 package com.stripe.android.link.ui.verification
 
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
