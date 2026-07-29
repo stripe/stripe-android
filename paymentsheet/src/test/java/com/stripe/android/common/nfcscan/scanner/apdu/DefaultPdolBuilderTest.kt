ae690c11d40bc7375ea7bd575eb22d07ef0e99b7
Samer Alabi <samer@stripe.com>
2026-07-29 13:28:59 -0400

    Update tests

--- paymentsheet/src/test/java/com/stripe/android/common/nfcscan/scanner/apdu/DefaultPdolBuilderTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/common/nfcscan/scanner/apdu/DefaultPdolBuilderTest.kt
@@ -23,7 +23,7 @@ internal class DefaultPdolBuilderTest {
     }
 
     @Test
-    fun `fromTemplate encodes producer value for matching template tag`() = runScenario(
+    fun `fromTemplate encodes producer value when value length matches template`() = runScenario(
         tagValueProducers = setOf(
             FakeTagValueProducer(
                 tag = "9F66",
@@ -40,16 +40,11 @@ internal class DefaultPdolBuilderTest {
             template = template,
         )
 
-        assertThat(result).isEqualTo(
-            commandTemplateResponse(TERMINAL_TRANSACTION_QUALIFIERS),
-        )
-
-        val producer = tagValueProducers.single() as FakeTagValueProducer
-        assertThat(producer.produceCalls.awaitItem()).isEqualTo(paymentMethodMetadata)
+        assertThat(result).isEqualTo(byteArrayOf(0x83.toByte(), 0x04, 0x26, 0x00, 0x00, 0x00))
     }
 
     @Test
-    fun `fromTemplate truncates producer value to template length`() = runScenario(
+    fun `fromTemplate zero pads when producer value length does not match template`() = runScenario(
         tagValueProducers = setOf(
             FakeTagValueProducer(
                 tag = "9F37",
@@ -66,14 +61,7 @@ internal class DefaultPdolBuilderTest {
             template = template,
         )
 
-        assertThat(result).isEqualTo(
-            commandTemplateResponse(
-                byteArrayOf(0x01, 0x02),
-            ),
-        )
-
-        assertThat(tagValueProducers.single().produceCalls.awaitItem())
-            .isEqualTo(paymentMethodMetadata)
+        assertThat(result).isEqualTo(byteArrayOf(0x83.toByte(), 0x02, 0x00, 0x00))
     }
 
     @Test
@@ -92,14 +80,7 @@ internal class DefaultPdolBuilderTest {
             template = template,
         )
 
-        assertThat(result).isEqualTo(
-            commandTemplateResponse(
-                byteArrayOf(0x00, 0x00, 0x00, 0x00),
-            ),
-        )
-
-        assertThat(tagValueProducers.single().produceCalls.awaitItem())
-            .isEqualTo(paymentMethodMetadata)
+        assertThat(result).isEqualTo(byteArrayOf(0x83.toByte(), 0x04, 0x00, 0x00, 0x00, 0x00))
     }
 
     @Test
@@ -125,15 +106,26 @@ internal class DefaultPdolBuilderTest {
             template = template,
         )
 
-        assertThat(result).isEqualTo(
-            commandTemplateResponse(
-                TERMINAL_TRANSACTION_QUALIFIERS + UNPREDICTABLE_NUMBER,
-            ),
+        assertThat(result)
+            .isEqualTo(byteArrayOf(0x83.toByte(), 0x08, 0x26, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04))
+    }
+
+    @Test
+    fun `fromTemplate uses long form length encoding for large pdol data`() = runScenario(
+        tagValueProducers = emptySet(),
+    ) {
+        val template = byteArrayOf(
+            0x9F.toByte(), 0x15, 0x80.toByte(),
         )
 
-        tagValueProducers.forEach { producer ->
-            assertThat(producer.produceCalls.awaitItem()).isEqualTo(paymentMethodMetadata)
-        }
+        val result = builder.fromTemplate(
+            paymentMethodMetadata = paymentMethodMetadata,
+            template = template,
+        )
+
+        assertThat(result).isEqualTo(
+            byteArrayOf(0x83.toByte(), 0x81.toByte(), 0x80.toByte()) + ByteArray(0x80),
+        )
     }
 
     private fun runScenario(
@@ -141,7 +133,7 @@ internal class DefaultPdolBuilderTest {
         block: suspend Scenario.() -> Unit,
     ) = runTest {
         val paymentMethodMetadata = PaymentMethodMetadataFactory.create(
-            stripeIntent = PaymentIntentFactory.create(amount = 5502)
+            stripeIntent = PaymentIntentFactory.create(amount = 5502),
         )
         val builder = DefaultPdolBuilder(
             tagValueProducers = tagValueProducers,
@@ -150,8 +142,11 @@ internal class DefaultPdolBuilderTest {
         Scenario(
             builder = builder,
             paymentMethodMetadata = paymentMethodMetadata,
-            tagValueProducers = tagValueProducers,
         ).apply { block() }
+
+        tagValueProducers.forEach { producer ->
+            assertThat(producer.produceCalls.awaitItem()).isEqualTo(paymentMethodMetadata)
+        }
 
         tagValueProducers.forEach {
             it.ensureAllEventsConsumed()
@@ -161,16 +156,9 @@ internal class DefaultPdolBuilderTest {
     private class Scenario(
         val builder: DefaultPdolBuilder,
         val paymentMethodMetadata: PaymentMethodMetadata,
-        val tagValueProducers: Set<FakeTagValueProducer>,
     )
 
-    private fun commandTemplateResponse(data: ByteArray): ByteArray {
-        return byteArrayOf(COMMAND_TEMPLATE_TAG, data.size.toByte()) + data
-    }
-
     private companion object {
-        const val COMMAND_TEMPLATE_TAG = 0x83.toByte()
-
         val TERMINAL_TRANSACTION_QUALIFIERS = byteArrayOf(0x26, 0x00, 0x00, 0x00)
         val UNPREDICTABLE_NUMBER = byteArrayOf(0x01, 0x02, 0x03, 0x04)
     }
