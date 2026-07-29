9c3480f96556f0fdcd6890b3d28d0677452a06c3
Jay Newstrom <jaynewstrom@stripe.com>
2026-07-29 07:48:30 -0600

    Add configurable payment method layout for PaymentElement.

--- paymentsheet/src/main/java/com/stripe/android/checkout/PaymentElement.kt
+++ paymentsheet/src/main/java/com/stripe/android/checkout/PaymentElement.kt
@@ -191,4 +191,28 @@ class PaymentElement @Inject internal constructor(
          */
         Automatic
     }
+
+    /**
+     * The layout of payment methods.
+     */
+    @CheckoutSessionPreview
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    enum class PaymentMethodLayout {
+        /**
+         * Payment methods are arranged horizontally.
+         * Users can swipe left or right to navigate through different payment methods.
+         */
+        Horizontal,
+
+        /**
+         * Payment methods are arranged vertically.
+         * Users can scroll up or down to navigate through different payment methods.
+         */
+        Vertical,
+
+        /**
+         * This lets Stripe choose the best layout for payment methods.
+         */
+        Automatic
+    }
 }
