31ed231d32910d4085e38dd45608bce707af1d8a
Jay Newstrom <jaynewstrom@stripe.com>
2026-08-03 11:18:47 -0600

    Add configurable payment method layout for PaymentElement. (#13652)

--- paymentsheet/src/main/java/com/stripe/android/checkout/PaymentElement.kt
+++ paymentsheet/src/main/java/com/stripe/android/checkout/PaymentElement.kt
@@ -32,6 +32,7 @@ class PaymentElement @Inject internal constructor(
         private var embeddedViewDisplaysMandateText: Boolean = true
         private var billingDetailsCollectionConfiguration: BillingDetailsCollectionConfiguration =
             BillingDetailsCollectionConfiguration()
+        private var paymentMethodLayout: PaymentMethodLayout = PaymentMethodLayout.Automatic
 
         fun embeddedViewDisplaysMandateText(
             embeddedViewDisplaysMandateText: Boolean
@@ -45,15 +46,30 @@ class PaymentElement @Inject internal constructor(
             this.billingDetailsCollectionConfiguration = billingDetailsCollectionConfiguration
         }
 
+        /**
+         * The layout of payment methods in the sheet. Defaults to [PaymentMethodLayout.Automatic].
+         *
+         * Note: Only used if you call `presentPaymentOptions`.
+         *
+         * @see [PaymentMethodLayout] for the list of available layouts.
+         */
+        fun paymentMethodLayout(
+            paymentMethodLayout: PaymentMethodLayout
+        ): Configuration = apply {
+            this.paymentMethodLayout = paymentMethodLayout
+        }
+
         @Parcelize
         internal data class State(
             val embeddedViewDisplaysMandateText: Boolean,
             val billingDetailsCollectionConfiguration: BillingDetailsCollectionConfiguration.State,
+            val paymentMethodLayout: PaymentMethodLayout,
         ) : Parcelable
 
         internal fun build(): State = State(
             embeddedViewDisplaysMandateText = embeddedViewDisplaysMandateText,
             billingDetailsCollectionConfiguration = billingDetailsCollectionConfiguration.build(),
+            paymentMethodLayout = paymentMethodLayout,
         )
 
         /**
@@ -150,5 +166,29 @@ class PaymentElement @Inject internal constructor(
                 // It can be added at public preview/GA if that use case is supported.
             }
         }
+    }
+
+    /**
+     * The layout of payment methods.
+     */
+    @CheckoutSessionPreview
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    enum class PaymentMethodLayout {
+        /**
+         * Payment methods are arranged horizontally.
+         * Users can swipe left or right to navigate through different payment methods.
+         */
+        Horizontal,
+
+        /**
+         * Payment methods are arranged vertically.
+         * Users can scroll up or down to navigate through different payment methods.
+         */
+        Vertical,
+
+        /**
+         * This lets Stripe choose the best layout for payment methods.
+         */
+        Automatic
     }
 }
