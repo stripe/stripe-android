9a161bcf6ffbc2794bac45f3ae816d833b62fc0b
Jay Newstrom <jaynewstrom@stripe.com>
2026-08-03 10:59:00 -0600

    Move BillingDetailsCollectionConfiguration into PaymentElement.Configuration. (#13646)

--- paymentsheet/src/test/java/com/stripe/android/checkout/CheckoutControllerTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/checkout/CheckoutControllerTest.kt
@@ -9,6 +9,7 @@ import app.cash.turbine.test
 import app.cash.turbine.turbineScope
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.checkout.CheckoutController.Address
+import com.stripe.android.checkout.PaymentElement.Configuration.BillingDetailsCollectionConfiguration
 import com.stripe.android.checkouttesting.DEFAULT_CHECKOUT_SESSION_ID
 import com.stripe.android.checkouttesting.checkoutInit
 import com.stripe.android.checkouttesting.checkoutUpdate
