73b5c558e87489e09a0888a158309363c8ce6d6b
Samer Alabi <141707240+samer-stripe@users.noreply.github.com>
2026-08-20 14:29:21 -0700

    Remove synchronization map & ensure register & deregister happen on the main thread. (#14099)

--- payments-core/src/main/java/com/stripe/android/googlepaylauncher/GooglePayPaymentDataUpdateCallbackRegistry.kt
+++ payments-core/src/main/java/com/stripe/android/googlepaylauncher/GooglePayPaymentDataUpdateCallbackRegistry.kt
@@ -1,16 +1,18 @@
 package com.stripe.android.googlepaylauncher
 
+import androidx.annotation.MainThread
 import androidx.annotation.RestrictTo
-import java.util.Collections.synchronizedMap
 
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 object GooglePayPaymentDataUpdateCallbackRegistry {
-    private val registeredCallbacks = synchronizedMap(mutableMapOf<String, GooglePayPaymentDataUpdateCallback>())
+    private val registeredCallbacks = mutableMapOf<String, GooglePayPaymentDataUpdateCallback>()
 
+    @MainThread
     fun register(key: String, callback: GooglePayPaymentDataUpdateCallback) {
         registeredCallbacks[key] = callback
     }
 
+    @MainThread
     fun deregister(key: String) {
         registeredCallbacks.remove(key)
     }
