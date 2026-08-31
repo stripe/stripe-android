5790a07981c51547d11d98860cd34797b5a330bb
System Administrator <amk@stripe.com>
2026-08-31 14:55:45 -0700

    make more readable (to me) and set ECE config

--- paymentsheet/src/androidTest/java/com/stripe/android/checkout/ExpressCheckoutElementTest.kt
+++ paymentsheet/src/androidTest/java/com/stripe/android/checkout/ExpressCheckoutElementTest.kt
@@ -13,6 +13,7 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import com.google.common.truth.Truth.assertThat
 import com.stripe.android.checkouttesting.DEFAULT_CHECKOUT_SESSION_ID
 import com.stripe.android.checkouttesting.checkoutConfirm
+import com.stripe.android.elements.ExpressCheckoutElement
 import com.stripe.android.googlepaylauncher.GooglePayPaymentMethodLauncher
 import com.stripe.android.networktesting.NetworkRule
 import com.stripe.android.networktesting.RequestMatchers.bodyPart
@@ -45,30 +46,17 @@ internal class ExpressCheckoutElementTest {
 
     @Test
     fun testSuccessfulGooglePayPayment() {
-        val paymentMethod = PaymentMethodFactory.card()
-        var checkoutResult: CheckoutController.Result? = null
-
-        intending(hasComponent(GOOGLE_PAY_ACTIVITY_NAME)).respondWith(
-            Instrumentation.ActivityResult(
-                Activity.RESULT_OK,
-                Intent().putExtra(
-                    "extra_result",
-                    GooglePayPaymentMethodLauncher.Result.Completed(paymentMethod),
-                ),
-            )
-        )
-        networkRule.checkoutConfirm(
-            bodyPart("payment_method", paymentMethod.id.orEmpty()),
-            bodyPart("expected_amount", "5099"),
-        ) { response ->
-            response.testBodyFromFile("checkout-session-confirm.json")
-        }
-
         runExpressCheckoutElementTest(
             networkRule = networkRule,
-            resultCallback = { result -> checkoutResult = result },
+            resultCallback = { result ->
+                assertThat(result).isInstanceOf(CheckoutController.Result.Completed::class.java)
+            },
             setup = { controller ->
-                controller.configure(DEFAULT_CLIENT_SECRET).getOrThrow()
+                controller.configure(
+                    DEFAULT_CLIENT_SECRET,
+                    configuration = CheckoutController.Configuration()
+                        .expressCheckoutElement(ExpressCheckoutElement.Configuration())
+                ).getOrThrow()
                 assertThat(controller.session.value?.availableExpressCheckoutPaymentMethods).hasSize(1)
             },
         ) {
@@ -77,11 +65,30 @@ internal class ExpressCheckoutElementTest {
                     .fetchSemanticsNodes(atLeastOneRootRequired = false)
                     .isNotEmpty()
             }
+
+            val paymentMethod = PaymentMethodFactory.card()
+
+            intending(hasComponent(GOOGLE_PAY_ACTIVITY_NAME)).respondWith(
+                Instrumentation.ActivityResult(
+                    Activity.RESULT_OK,
+                    Intent().putExtra(
+                        "extra_result",
+                        GooglePayPaymentMethodLauncher.Result.Completed(paymentMethod),
+                    ),
+                )
+            )
+
+            networkRule.checkoutConfirm(
+                bodyPart("payment_method", paymentMethod.id),
+                bodyPart("expected_amount", "5099"),
+            ) { response ->
+                response.testBodyFromFile("checkout-session-confirm.json")
+            }
+
             testRules.compose.onNodeWithTag(GOOGLE_PAY_BUTTON_TEST_TAG).performClick()
         }
 
         intended(hasComponent(GOOGLE_PAY_ACTIVITY_NAME))
-        assertThat(checkoutResult).isInstanceOf(CheckoutController.Result.Completed::class.java)
     }
 
     private companion object {
