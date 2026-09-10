ee89d4d4723ac9f4b74113f4c4faeb394d2a66b0
Tyler Clawson <tjclawson@stripe.com>
2026-09-10 11:02:44 -0400

    Remove provider for pm metadata

--- paymentsheet/src/main/java/com/stripe/android/paymentsheet/injection/ApiConfigurationModule.kt
+++ paymentsheet/src/main/java/com/stripe/android/paymentsheet/injection/ApiConfigurationModule.kt
@@ -4,12 +4,11 @@ import com.stripe.android.core.ApiConfiguration
 import com.stripe.android.lpmfoundations.paymentmethod.PaymentMethodMetadata
 import dagger.Module
 import dagger.Provides
-import javax.inject.Provider
 
 @Module
 internal object ApiConfigurationModule {
     @Provides
     fun provideApiConfiguration(
-        paymentMethodMetadata: Provider<PaymentMethodMetadata?>
-    ): ApiConfiguration.State = requireNotNull(paymentMethodMetadata.get()?.apiConfiguration)
+        paymentMethodMetadata: PaymentMethodMetadata?
+    ): ApiConfiguration.State = requireNotNull(paymentMethodMetadata?.apiConfiguration)
 }
