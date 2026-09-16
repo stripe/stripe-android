122f0ca1a83dcd25700fc8f54b61d5e01b5563ab
Nick Porter <porter@stripe.com>
2026-09-16 13:31:59 -0400

    Update MoMo test fixtures

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestMoMo.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestMoMo.kt
@@ -3,6 +3,7 @@ package com.stripe.android.lpm
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BasePlaygroundTest
 import com.stripe.android.model.PaymentMethod
+import com.stripe.android.paymentsheet.example.playground.settings.AmountSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.CheckoutMode
 import com.stripe.android.paymentsheet.example.playground.settings.CheckoutModeSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.Currency
@@ -21,6 +22,7 @@ internal class TestMoMo : BasePlaygroundTest() {
     ) { settings ->
         settings[MerchantSettingsDefinition] = Merchant.US
         settings[CurrencySettingsDefinition] = Currency.VND
+        settings[AmountSettingsDefinition] = "100000"
         settings[SupportedPaymentMethodsSettingsDefinition] = listOf(
             PaymentMethod.Type.Card,
             PaymentMethod.Type.MoMo
