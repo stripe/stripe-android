63ccc114762ee6ff29ec40d8262776cc00b97fe2
Nick Porter <porter@stripe.com>
2026-09-16 13:31:56 -0400

    Update GoPay test fixtures

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestGoPay.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestGoPay.kt
@@ -3,6 +3,7 @@ package com.stripe.android.lpm
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BasePlaygroundTest
 import com.stripe.android.model.PaymentMethod
+import com.stripe.android.paymentsheet.example.playground.settings.AmountSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.CheckoutMode
 import com.stripe.android.paymentsheet.example.playground.settings.CheckoutModeSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.Currency
@@ -21,6 +22,7 @@ internal class TestGoPay : BasePlaygroundTest() {
     ) { settings ->
         settings[MerchantSettingsDefinition] = Merchant.US
         settings[CurrencySettingsDefinition] = Currency.IDR
+        settings[AmountSettingsDefinition] = "10000000"
         settings[SupportedPaymentMethodsSettingsDefinition] = listOf(
             PaymentMethod.Type.Card,
             PaymentMethod.Type.GoPay
