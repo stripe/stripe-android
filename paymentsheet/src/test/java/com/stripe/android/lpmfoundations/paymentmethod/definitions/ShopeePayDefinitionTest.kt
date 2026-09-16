4dbd387b813d4e10186a501eb7fe35697a1e9662
Nick Porter <porter@stripe.com>
2026-09-16 13:32:00 -0400

    Update ShopeePay test fixtures

--- paymentsheet/src/test/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/ShopeePayDefinitionTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/ShopeePayDefinitionTest.kt
@@ -34,9 +34,8 @@ internal class ShopeePayDefinitionTest {
     }
 
     @Test
-    fun `form matches web instructions and mandate`(
-        @TestParameter intentScenario: LpmBillingAddressTestConfiguration.IntentScenario,
-    ) {
+    fun `form matches web instructions and mandate`() {
+        val intentScenario = LpmBillingAddressTestConfiguration.IntentScenario.PaymentIntent
         val metadata = PaymentMethodMetadataFactory.create(
             stripeIntent = intentScenario.stripeIntent(PaymentMethod.Type.ShopeePay),
         )
