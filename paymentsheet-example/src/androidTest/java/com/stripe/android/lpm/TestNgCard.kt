e04b024758f85ad8c18c328dd7f99992b41edfef
Nick Porter <porter@stripe.com>
2026-09-16 13:31:57 -0400

    Update Naira card test fixtures

--- paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestNgCard.kt
+++ paymentsheet-example/src/androidTest/java/com/stripe/android/lpm/TestNgCard.kt
@@ -3,6 +3,7 @@ package com.stripe.android.lpm
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.stripe.android.BasePlaygroundTest
 import com.stripe.android.model.PaymentMethod
+import com.stripe.android.paymentsheet.example.playground.settings.AmountSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.CheckoutMode
 import com.stripe.android.paymentsheet.example.playground.settings.CheckoutModeSettingsDefinition
 import com.stripe.android.paymentsheet.example.playground.settings.Currency
@@ -21,6 +22,7 @@ internal class TestNgCard : BasePlaygroundTest() {
     ) { settings ->
         settings[MerchantSettingsDefinition] = Merchant.US
         settings[CurrencySettingsDefinition] = Currency.NGN
+        settings[AmountSettingsDefinition] = "1000000"
         settings[SupportedPaymentMethodsSettingsDefinition] = listOf(
             PaymentMethod.Type.Card,
             PaymentMethod.Type.NgCard
--- paymentsheet/src/test/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/NgCardDefinitionTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/NgCardDefinitionTest.kt
@@ -1,6 +1,8 @@
 package com.stripe.android.lpmfoundations.paymentmethod.definitions
 
 import android.content.Context
+import android.text.style.URLSpan
+import androidx.core.text.HtmlCompat
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import com.google.testing.junit.testparameterinjector.TestParameter
@@ -86,7 +88,9 @@ internal class NgCardDefinitionTest {
         val notice = context.getString(R.string.stripe_ng_payment_mor_notice, NIGERIAN_PAYMENT_METHOD_TERMS_URL)
 
         assertThat(notice).contains("Global Stack Services Limited as merchant of record")
-        assertThat(notice).contains("""<a href="$NIGERIAN_PAYMENT_METHOD_TERMS_URL">terms of use</a>""")
+        val renderedNotice = HtmlCompat.fromHtml(notice, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        val links = renderedNotice.getSpans(0, renderedNotice.length, URLSpan::class.java)
+        assertThat(links.map { it.url }).containsExactly(NIGERIAN_PAYMENT_METHOD_TERMS_URL)
         assertThat(notice).doesNotContain("%s")
     }
 }
