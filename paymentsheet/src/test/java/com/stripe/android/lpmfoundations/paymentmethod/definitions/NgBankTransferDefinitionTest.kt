7ca45489948d7ec58051214a92a47c0bdb1f119f
Nick Porter <porter@stripe.com>
2026-09-16 13:31:58 -0400

    Update Naira bank transfer test fixtures

--- paymentsheet/src/test/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/NgBankTransferDefinitionTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/lpmfoundations/paymentmethod/definitions/NgBankTransferDefinitionTest.kt
@@ -1,6 +1,8 @@
 package com.stripe.android.lpmfoundations.paymentmethod.definitions
 
 import android.content.Context
+import android.text.style.URLSpan
+import androidx.core.text.HtmlCompat
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 import com.google.testing.junit.testparameterinjector.TestParameter
@@ -33,9 +35,8 @@ internal class NgBankTransferDefinitionTest {
     }
 
     @Test
-    fun `form matches web instructions and mandate`(
-        @TestParameter intentScenario: LpmBillingAddressTestConfiguration.IntentScenario,
-    ) {
+    fun `form matches web instructions and mandate`() {
+        val intentScenario = LpmBillingAddressTestConfiguration.IntentScenario.PaymentIntent
         val metadata = PaymentMethodMetadataFactory.create(
             stripeIntent = intentScenario.stripeIntent(PaymentMethod.Type.NgBankTransfer),
         )
@@ -88,7 +89,9 @@ internal class NgBankTransferDefinitionTest {
         val notice = context.getString(R.string.stripe_ng_payment_mor_notice, NIGERIAN_PAYMENT_METHOD_TERMS_URL)
 
         assertThat(notice).contains("Global Stack Services Limited as merchant of record")
-        assertThat(notice).contains("""<a href="$NIGERIAN_PAYMENT_METHOD_TERMS_URL">terms of use</a>""")
+        val renderedNotice = HtmlCompat.fromHtml(notice, HtmlCompat.FROM_HTML_MODE_LEGACY)
+        val links = renderedNotice.getSpans(0, renderedNotice.length, URLSpan::class.java)
+        assertThat(links.map { it.url }).containsExactly(NIGERIAN_PAYMENT_METHOD_TERMS_URL)
         assertThat(notice).doesNotContain("%s")
     }
 }
