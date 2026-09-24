d0c24c36b2e94d3e813492bd15aafb23112d78d8
Samer Alabi <samer@stripe.com>
2026-09-24 17:09:24 -0400

    Address PR comments

--- paymentsheet/src/test/java/com/stripe/android/common/nfcscan/ui/DeveloperOptionsScreenshotTest.kt
+++ paymentsheet/src/test/java/com/stripe/android/common/nfcscan/ui/DeveloperOptionsScreenshotTest.kt
@@ -3,6 +3,7 @@ package com.stripe.android.common.nfcscan.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.screenshottesting.LayoutDirection
 import com.stripe.android.screenshottesting.PaparazziRule
 import com.stripe.android.screenshottesting.SystemAppearance
 import org.junit.Rule
@@ -11,6 +12,7 @@ import org.junit.Test
 class DeveloperOptionsScreenshotTest {
     @get:Rule
     val paparazziRule = PaparazziRule(
+        LayoutDirection.entries,
         SystemAppearance.entries,
         boxModifier = Modifier.fillMaxSize(),
         includeStripeTheme = false,
